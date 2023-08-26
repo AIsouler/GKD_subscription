6ecfac2a74fa1102832e7233387a8c02836856d4
lisonge <i@songe.li>
2023-08-26 23:59:15 +0800

    chore: sync

--- src/apps/com.ximalaya.ting.lite.ts
+++ src/apps/com.ximalaya.ting.lite.ts
@@ -9,5 +9,13 @@ export default <AppConfig>{
       activityIds: 'com.ximalaya.ting.android.host.activity.MainActivity',
       rules: "[id='com.ximalaya.ting.lite:id/main_ad_top_home_iv_close']",
     },
+    {
+      key: 1,
+      name: `喜马拉雅极速版-开屏广告`,
+      activityIds: `com.ximalaya.ting.android.host.activity.WelComeActivity`,
+      rules: [
+        `[id="com.ximalaya.ting.lite:id/host_common_time_countdown_text_view"]`,
+      ],
+    },
   ],
 };
