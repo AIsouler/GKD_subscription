c0d171e5e21f4ee9e618308e919d651bdafb6d0a
lisonge <i@songe.li>
2023-08-28 20:17:37 +0800

    chore: sync

--- src/apps/com.hunantv.imgo.activity.ts
+++ src/apps/com.hunantv.imgo.activity.ts
@@ -4,6 +4,12 @@ export default <AppConfig>{
   id: 'com.hunantv.imgo.activity',
   groups: [
     {
+      key: -1,
+      name: `芒果TV`,
+      // https://gkd-kit.gitee.io/import/38517192/0d54f18a-eb7a-498d-a458-32980190042b
+      rules: `[id="com.hunantv.imgo.activity:id/layout_boot_skip"]`,
+    },
+    {
       key: 0,
       name: '芒果TV-关闭青少年模式提示',
       activityIds: [
