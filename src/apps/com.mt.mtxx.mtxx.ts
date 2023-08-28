caeb5fdb543fa15e2ebd1fe9972471f7957636ac
lisonge <i@songe.li>
2023-08-28 20:01:27 +0800

    chore: sync

--- src/apps/com.mt.mtxx.mtxx.ts
+++ src/apps/com.mt.mtxx.mtxx.ts
@@ -7,7 +7,7 @@ export default <AppConfig>{
       key: 0,
       name: '美图秀秀-开屏广告',
       activityIds: 'com.meitu.business.ads.core.activity.AdActivity',
-      rules: '[id=`com.mt.mtxx.mtxx:id/0*`][text=`跳过广告`]',
+      rules: '[text=`跳过广告`]',
     },
   ],
 };
--- src/apps/com.ss.android.article.video.ts
+++ src/apps/com.ss.android.article.video.ts
@@ -7,6 +7,7 @@ export default <AppConfig>{
       key: 0,
       name: `西瓜视频-关闭青少年模式弹窗`,
       activityIds: `com.ixigua.commonui.uikit.dialog.XGAlertDialog`,
+      // https://gkd-kit.gitee.io/import/38517192/78f0c1f6-e8da-4bc4-acd3-5e6dc056b044
       rules: [`TextView[text="我知道了"][clickable=true]`],
     },
   ],
