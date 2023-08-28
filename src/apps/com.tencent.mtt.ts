d72a8dad48d8342b7ca68559e9e7bc14c9e06b17
lisonge <i@songe.li>
2023-08-28 19:53:01 +0800

    chore: sync

--- src/apps/com.tencent.mtt.ts
+++ src/apps/com.tencent.mtt.ts
@@ -7,8 +7,12 @@ export default <AppConfig>{
       key: 0,
       name: 'QQ浏览器-开屏广告',
       activityIds: 'com.tencent.mtt.MainActivity',
-      rules:
+      rules: [
         '@View[id=null] + ImageView + FrameLayout TextView[text=`向上滑动或点击查看`]',
+
+        // https://gkd-kit.gitee.io/import/38517192/7d8e9661-c29a-4448-94c2-d7b0a1756107
+        `@LinearLayout[clickable=true] > TextView[text="跳过"]`,
+      ],
     },
   ],
 };
