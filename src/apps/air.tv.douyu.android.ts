dbcf201e2c9b517a83196973789a783ae8abccad
lisonge <i@songe.li>
2023-08-29 11:45:34 +0800

    chore: sync

--- src/apps/air.tv.douyu.android.ts
+++ src/apps/air.tv.douyu.android.ts
@@ -8,7 +8,7 @@ export default <AppConfig>{
       name: '斗鱼-开屏广告',
       activityIds: [
         'com.douyu.module.home.pages.main.MainActivity',
-        `miuix.appcompat.app.m`,
+        `com.douyu.module.ad.launch.HotStartSplashActivity`,
       ],
       rules: [
         // https://gkd-kit.gitee.io/import/38517192/50c858ee-d331-4d5a-b5db-5eb17323c5ff
@@ -20,7 +20,10 @@ export default <AppConfig>{
       key: 1,
       name: `斗鱼-青少年模式`,
       desc: `关闭青少年模式提醒弹窗`,
-      activityIds: `com.douyu.module.young.view.YoungModeGuideDialog`,
+      activityIds: [
+        `com.douyu.module.young.view.YoungModeGuideDialog`,
+        `com.douyu.module.home.pages.main.MainActivity`,
+      ],
       rules: [
         // https://gkd-kit.gitee.io/import/38517192/1c081a65-688a-406b-b67b-9bfb9aba0fad
         `[text='开启青少年模式'] + [text='我知道了']`, // 1686970236642
