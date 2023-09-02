18097d81fbce65eb9fec55687cdd74cafc997b5c
lisonge <i@songe.li>
2023-09-03 01:38:13 +0800

    chore: miui主题壁纸 desc

--- src/apps/com.android.thememanager.ts
+++ src/apps/com.android.thememanager.ts
@@ -7,6 +7,7 @@ export default <AppConfig>{
     {
       key: 1,
       name: '推荐下广告',
+      desc: `注意如果使用ADB禁用了MIUI广告组件,点击此按钮会无反应,可关闭此规则,避免过多相同点击记录`,
       rules: '[id=`com.android.thememanager:id/ad_close_btn`]',
     },
   ],
