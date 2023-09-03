369f68255c8934b5b44301e601d0f7d029ad628d
lisonge <i@songe.li>
2023-09-03 18:36:56 +0800

    fix: sync

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,8 +1,10 @@
-await fetch(
+const r = await fetch(
   `https://registry-direct.npmmirror.com/@gkd-kit/subscription/sync`,
   {
     method: 'PUT',
   },
 );
+console.log(await r.json());
+console.log(`同步 npmmirror 成功`);
 
 export {};
