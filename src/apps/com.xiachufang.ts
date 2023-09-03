9c8279efbb2e12d01fb9a47df1a2baf8aab0baaa
Ze-Zheng Wu <zzwu@zju.edu.cn>
2023-09-03 21:33:57 +0800

    fix(rules): 合并下厨房规则

--- src/apps/com.xiachufang.ts
+++ src/apps/com.xiachufang.ts
@@ -5,25 +5,15 @@ export default defineAppConfig({
   name: '下厨房',
   groups: [
     {
-      key: 1,
+      key: 0,
       name: '开屏广告',
       activityIds: ['com.xiachufang.startpage.ui.StartPageActivity'],
-      rules: [
-        {
-          matches: '[id$="/tt_splash_skip_btn"]',
-          snapshotUrls: ['https://gkd-kit.songe.li/import/12505985'],
-        },
-        {
-          matches: '[id$="/start_page_count_down_tv"]',
-          snapshotUrls: ['https://gkd-kit.songe.li/import/12506014'],
-        },
-        {
-          matches: '[text$="跳过"]',
-          snapshotUrls: [
-            'https://gkd-kit.songe.li/import/12506014',
-            'https://gkd-kit.songe.li/import/12506041',
-          ],
-        },
+      rules:
+        '[id$="/tt_splash_skip_btn"||id$="/start_page_count_down_tv"||text$="跳过"]',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12505985',
+        'https://gkd-kit.songe.li/import/12506014',
+        'https://gkd-kit.songe.li/import/12506041',
       ],
     },
   ],
