3137e903cac019ec6a8d7c5256f98c73cdea0f4d
lisonge <i@songe.li>
2023-09-03 18:32:45 +0800

    feat: sync

--- scripts/sync.ps1
+++
@@ -1,6 +0,0 @@
-git add '.'
-git commit -m 'chore: sync'
-git push
-npm publish
-sleep 1s
-curl -X PUT 'https://registry-direct.npmmirror.com/@gkd-kit/subscription/sync'
---
+++ scripts/sync.ts
@@ -0,0 +1,8 @@
+await fetch(
+  `https://registry-direct.npmmirror.com/@gkd-kit/subscription/sync`,
+  {
+    method: 'PUT',
+  },
+);
+
+export {};
