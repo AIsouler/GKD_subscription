83fca9fbc7d28860c4796de4ed29bc2567bbe834
lisonge <i@songe.li>
2023-09-04 21:17:25 +0800

    feat: 汽水音乐
    - 开屏广告

---
+++ src/apps/com.luna.music.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.luna.music',
+  name: '汽水音乐',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.luna.biz.ad.AdActivity',
+      rules: '[id$="/tt_splash_skip_btn"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12514049',
+    },
+  ],
+});
