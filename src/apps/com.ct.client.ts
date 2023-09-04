04bc64f1892b7fb3652e3d0db034563ce89df747
Ze-Zheng Wu <zzwu@zju.edu.cn>
2023-09-04 10:55:21 +0800

    fix: 中国电信开屏广告跳过规则

--- src/apps/com.ct.client.ts
+++ src/apps/com.ct.client.ts
@@ -2,7 +2,7 @@ import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
   id: 'com.ct.client',
-  name: `中国电信`,
+  name: '中国电信',
   groups: [
     {
       key: 0,
@@ -11,7 +11,8 @@ export default defineAppConfig({
         'com.ct.client.activity.MainActivity',
         'com.ct.client.activity.SplashActivity',
       ],
-      rules: '[id=`com.ct.client:id/tvSkip`]',
+      rules: '[id="com.ct.client:id/tvSkip"||id="com.ct.client:id/btSkip"]',
+      snapshotUrls: ['https://gkd-kit.songe.li/import/12508958'],
     },
   ],
 });
