2e7bdeb316314e9e3f8938c8cf054a996a6af983
lisonge <i@songe.li>
2023-09-04 10:19:48 +0800

    fix: #8

--- src/apps/com.qidian.QDReader.ts
+++ src/apps/com.qidian.QDReader.ts
@@ -7,8 +7,12 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.qidian.QDReader.ui.activity.SplashADActivity',
+      activityIds: [
+        'com.qidian.QDReader.ui.activity.SplashADActivity',
+        'com.qidian.QDReader.ui.activity.SplashImageActivity',
+      ],
       rules: 'Button[text^=`跳过`]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12508836'],
     },
   ],
 });
