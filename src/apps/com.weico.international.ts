aa0509e4178d2a8777ae1d43017691c1036d2039
lisonge <i@songe.li>
2023-09-04 14:54:30 +0800

    fix: 微博轻享版-开屏广告

--- src/apps/com.weico.international.ts
+++ src/apps/com.weico.international.ts
@@ -8,8 +8,11 @@ export default defineAppConfig({
       key: -1,
       name: '开屏广告',
       activityIds: 'com.weico.international.ui.ad.AdActivity',
-      rules: 'TextView[text="跳过"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12509123',
+      rules: 'TextView[text*="跳过"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12509123',
+        'https://gkd-kit.gitee.io/import/12510132',
+      ],
     },
     {
       key: 0,
