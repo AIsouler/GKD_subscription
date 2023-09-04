99185294dc49d5614c32ec247d1af724998163be
Hacone <mouren1101@outlook.com>
2023-09-04 19:13:22 +0800

    feat: biubiu加速器-开屏广告 (#19)
    close #19

---
+++ src/apps/com.njh.biubiu.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from "../types";
+
+export default defineAppConfig({
+  id: "com.njh.biubiu",
+  name: `biubiu加速器`,
+  groups: [
+    {
+      key: 0,
+      name: "开屏广告",
+      activityIds: "com.njh.ping.business.base.activity.BusinessActivity",
+      rules: [
+        {
+          matches: '[text="跳过"]',
+          snapshotUrls: "https://gkd-kit.gitee.io/import/12512845",
+        },
+      ],
+    },
+  ],
+});
