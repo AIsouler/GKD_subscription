c72dab8b69d39881d9aaed3cd7b1ea040309fad8
lisonge <i@songe.li>
2023-09-04 14:59:43 +0800

    fix: 虎扑-切回APP广告

--- src/apps/com.hupu.games.ts
+++ src/apps/com.hupu.games.ts
@@ -7,9 +7,12 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.hupu.games.main.MainActivity',
+      desc: '开屏广告,任意界面切回APP广告',
       rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12509060',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12509060',
+        'https://gkd-kit.gitee.io/import/12510962',
+      ],
     },
   ],
 });
