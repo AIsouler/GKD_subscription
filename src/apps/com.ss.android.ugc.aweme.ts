61ff5b41a72ffe9f227a9fd43583cf183c7f7919
lisonge <i@songe.li>
2023-09-05 16:34:36 +0800

    feat(抖音): 关闭用户推荐

--- src/apps/com.ss.android.ugc.aweme.ts
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -9,5 +9,24 @@ export default defineAppConfig({
       name: '关闭青少年弹窗',
       rules: 'Button[text=`开启青少年模式`] + * > Button[text!=null]',
     },
+    {
+      key: 1,
+      name: '关闭用户推荐',
+      rules: [
+        {
+          activityIds:
+            'com.google.android.material.bottomsheet.BottomSheetDialog',
+          matches:
+            '[id="com.ss.android.ugc.aweme:id/desc"][text="减少此类推荐"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12520962',
+        },
+        {
+          activityIds: 'com.ss.android.ugc.aweme.main.MainActivity',
+          matches:
+            '[text="换一个"] - FrameLayout[clickable=true] > ImageView[clickable=true]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12520943',
+        },
+      ],
+    },
   ],
 });
