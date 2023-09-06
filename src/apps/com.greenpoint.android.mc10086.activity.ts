fc27e7d23776a81a7220a07570e1ddf795b525c4
lisonge <i@songe.li>
2023-09-06 14:48:33 +0800

    feat(中国移动): 关闭更新弹窗

--- src/apps/com.greenpoint.android.mc10086.activity.ts
+++ src/apps/com.greenpoint.android.mc10086.activity.ts
@@ -11,5 +11,13 @@ export default defineAppConfig({
       rules:
         '[id=`com.greenpoint.android.mc10086.activity:id/video_time_skip`]',
     },
+    {
+      key: 1,
+      name: '关闭更新弹窗',
+      activityIds: 'com.mc10086.cmcc.view.tabs.AppTabFragment',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12534264',
+      rules:
+        'Button[text="以后再说"][id^="com.greenpoint.android.mc10086.activity:id/dialog_btn"]',
+    },
   ],
 });
