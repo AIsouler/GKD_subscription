330bc8c3672164c86d84c01c1ce67aa604389e0e
lisonge <i@songe.li>
2023-09-06 15:50:55 +0800

    fix(虎扑): 推荐流广告不能准确关闭

--- src/apps/com.hupu.games.ts
+++ src/apps/com.hupu.games.ts
@@ -21,8 +21,15 @@ export default defineAppConfig({
       desc: '点击卡片右上角广告文字,出现广告反馈,点击屏蔽该广告',
       rules: [
         {
+          activityIds: [
+            'com.google.android.material.bottomsheet.BottomSheetDialog',
+            'com.hupu.games.main.MainActivity',
+          ],
           matches: '[id="com.hupu.games:id/tv_title"][text="屏蔽该广告"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12511010',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12511010',
+            'https://gkd-kit.gitee.io/import/12534848',
+          ],
         },
         {
           matches: '[id="com.hupu.games:id/tv_tag"][text="广告"]',
