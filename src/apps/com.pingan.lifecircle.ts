e0104e3fb1c33b042a8b1357c53235b446fc54f6
lisonge <i@songe.li>
2023-09-10 15:24:58 +0800

    feat(平安好生活): 开屏广告

---
+++ src/apps/com.pingan.lifecircle.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.pingan.lifecircle',
+  name: '平安好生活',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.pingan.lifecircle.home.SplashActivity',
+      rules: '[id="com.pingan.lifecircle:id/splash_time_tv"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12567724',
+    },
+  ],
+});
