e08aab78432eeec119ac53302eb772326811d4ea
lisonge <i@songe.li>
2023-09-14 16:59:12 +0800

    feat(随便扫): 顶部广告卡片

---
+++ src/apps/com.jim.qrcode.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.jim.qrcode',
+  name: '随便扫',
+  groups: [
+    {
+      key: 0,
+      name: '顶部广告卡片',
+      desc: '点击卡片右上角关闭按钮',
+      activityIds: [
+        'com.jim.qrcode.MainActivity',
+        'com.miui.home.launcher.Launcher',
+      ],
+      rules: 'TextView[text.length>0] < FrameLayout + FrameLayout > ImageView',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12606861',
+        'https://gkd-kit.gitee.io/import/12606862',
+      ],
+    },
+  ],
+});
