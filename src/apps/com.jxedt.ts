d81bbf083a3cf0b38ab76ba99aea633a87b9f22f
lisonge <i@songe.li>
2023-09-14 14:12:48 +0800

    chore: format+lint

--- src/apps/com.jxedt.ts
+++ src/apps/com.jxedt.ts
@@ -1,24 +1,23 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-    id: 'com.jxedt',
-    name: '驾校一点通',
-    groups: [
-      {
-        key: 1,
-        name: '首页广告',
-        activityIds: ['com.jxedt.ui.activitys.GuideActivity'],
-        rules: [
-          {
-            matches: '[id="com.jxedt:id/tv_guide_tiaoguo" || text*="跳过"]',
-            snapshotUrls: [
-              'https://gkd-kit.gitee.io/import/12605051',
-              'https://gkd-kit.gitee.io/import/12605053',
-              'https://gkd-kit.gitee.io/import/12605057',
-            ],
-          },
-        ],
-      },
-    ],
-  }
-);
+  id: 'com.jxedt',
+  name: '驾校一点通',
+  groups: [
+    {
+      key: 1,
+      name: '首页广告',
+      activityIds: ['com.jxedt.ui.activitys.GuideActivity'],
+      rules: [
+        {
+          matches: '[id="com.jxedt:id/tv_guide_tiaoguo" || text*="跳过"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12605051',
+            'https://gkd-kit.gitee.io/import/12605053',
+            'https://gkd-kit.gitee.io/import/12605057',
+          ],
+        },
+      ],
+    },
+  ],
+});
--- src/apps/com.njh.biubiu.ts
+++ src/apps/com.njh.biubiu.ts
@@ -1,17 +1,17 @@
-import { defineAppConfig } from "../types";
+import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-  id: "com.njh.biubiu",
-  name: `biubiu加速器`,
+  id: 'com.njh.biubiu',
+  name: 'biubiu加速器',
   groups: [
     {
       key: 0,
-      name: "开屏广告",
-      activityIds: "com.njh.ping.business.base.activity.BusinessActivity",
+      name: '开屏广告',
+      activityIds: 'com.njh.ping.business.base.activity.BusinessActivity',
       rules: [
         {
           matches: '[text="跳过"]',
-          snapshotUrls: "https://gkd-kit.gitee.io/import/12512845",
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12512845',
         },
       ],
     },
