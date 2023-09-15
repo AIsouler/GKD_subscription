7efb03560c2b01a7ffbd29a1fd895e35ce8d509b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-15 10:03:40 +0800

    feat(美团): 版本更新弹窗 (#79)
    * 新增 美团 版本更新弹窗
    
    * 更新 美团 版本更新弹窗 规则
    
    * 修改快照链接
    
    * fix(美团): 增加弹窗界面范围
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.sankuai.meituan.ts
+++ src/apps/com.sankuai.meituan.ts
@@ -10,5 +10,13 @@ export default defineAppConfig({
       activityIds: 'com.meituan.android.pt.homepage.activity.MainActivity',
       rules: 'TextView[id=`com.sankuai.meituan:id/close_btn`][text^=`跳过`]',
     },
+    {
+      key: 1,
+      name: '版本更新弹窗',
+      activityIds: 'com.meituan.android.upgrade.ui.',
+      rules:
+        'TextView[text^=`新版本`] - Button[id=`com.sankuai.meituan:id/btn_close`]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12614559',
+    },
   ],
 });
