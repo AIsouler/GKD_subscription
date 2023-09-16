2d4091df660c50c56d970962e789d8d011326d5e
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-17 00:17:47 +0800

    feat(起点读书): 更新弹窗, (#95)
    * feat(起点读书)
    
    * Update com.qidian.QDReader.ts
    
    * 修改-更新弹窗-规则-快照链接

--- src/apps/com.qidian.QDReader.ts
+++ src/apps/com.qidian.QDReader.ts
@@ -14,5 +14,44 @@ export default defineAppConfig({
       rules: 'Button[text^=`跳过`]',
       snapshotUrls: ['https://gkd-kit.gitee.io/import/12508836'],
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: ['com.qidian.QDReader.framework.widget.dialog.judian'],
+      rules:
+        'TextView[text$="新版本"] +(2) RelativeLayout >(4) TextView[text="以后再说"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12641026'],
+    },
+    {
+      key: 2,
+      name: '青少年模式弹窗',
+      activityIds: ['com.qidian.QDReader.bll.helper.z0'],
+      rules:
+        '[text$="青少年模式"] < [id="com.qidian.QDReader:id/btnEnterTeen"] + [id="com.qidian.QDReader:id/btnCancel"] >(2) [text="我知道了"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12640241'],
+    },
+    {
+      key: 3,
+      name: '活动弹窗',
+      activityIds: [
+        'com.qidian.QDReader.ui.activity.MainGroupActivity',
+        'com.tencent.mm.plugin.webview.ui.tools.SDKOAuthUI',
+      ],
+      rules:
+        '[id="com.qidian.QDReader:id/imgBKT"] + [id="com.qidian.QDReader:id/imgClose"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12640195',
+        'https://gkd-kit.gitee.io/import/12640158',
+      ],
+    },
+    {
+      enable: false,
+      key: 4,
+      name: '系统推送提示信息',
+      desc: '点击顶部提醒左侧x按钮',
+      activityIds: ['com.afollestad.materialdialogs.MaterialDialog'],
+      rules: '@[id="com.qidian.QDReader:id/ivClose"] + [text^="开启推送"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12640242'],
+    },
   ],
 });
