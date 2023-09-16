7d9b2c5ef69a73facf71467f98e8ebd02a6b8136
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-16 12:12:08 +0800

    feat(米家): 米家智惠广告, 设备固件升级提示弹窗 (#88)
    * feat(米家): 米家智惠广告, 设备固件升级提示弹窗
    
    * perf(米家): 智惠广告-增加准确性
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/com.xiaomi.smarthome.ts
@@ -0,0 +1,29 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xiaomi.smarthome',
+  name: '米家',
+  groups: [
+    {
+      key: 0,
+      name: '米家智惠广告',
+      activityIds: 'com.xiaomi.smarthome.SmartHomeMainActivity',
+      rules: [
+        {
+          matches:
+            'RelativeLayout[id=null] > ImageView[id!=null] + ImageView[clickable=true][id!=null]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12639658'],
+        },
+      ],
+    },
+    {
+      enable: false,
+      key: 1,
+      name: '设备固件升级提示弹窗',
+      activityIds: 'com.xiaomi.smarthome.framework.plugin.rn.PluginRNActivity',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12639671',
+      rules:
+        '[text*="检测到"&&text*="升级"] < ViewGroup + ViewGroup > Button > [text = "取消"]',
+    },
+  ],
+});
