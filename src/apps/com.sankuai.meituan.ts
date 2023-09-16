8da952cc55cf7162cc9c976a1b7331005155f8e8
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-16 12:27:41 +0800

    feat(美团): 买菜活动弹窗,小调查弹窗,抽奖小广告 (#87)
    * feat(美团): 若干弹窗
    
    1. 美团买菜活动弹窗
    2. 小调查弹窗
    3. 抽奖小广告
    
    * 调整代码风格
    
    * 修改 activityIds，修改快照链接
    
    * 增加 activityIds
    
    * 修改代码风格
    
    * 新增 快照链接

--- src/apps/com.sankuai.meituan.ts
+++ src/apps/com.sankuai.meituan.ts
@@ -18,5 +18,33 @@ export default defineAppConfig({
         'TextView[text^=`新版本`] - Button[id=`com.sankuai.meituan:id/btn_close`]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12614559',
     },
+    {
+      enable: false,
+      key: 2,
+      name: '美团买菜活动弹窗',
+      activityIds: 'com.meituan.retail.c.android.mrn.mrn.MallMrnModal',
+      rules: 'ViewGroup > ViewGroup +(5) ViewGroup > ImageView',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12639717',
+    },
+    {
+      key: 3,
+      name: '小调查弹窗',
+      activityIds: 'com.meituan.retail.c.android.mrn.mrn.MallMrnModal',
+      rules: '[text="小调查"] + ViewGroup > ViewGroup > ImageView',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12639723',
+    },
+    {
+      key: 4,
+      name: '首页右侧抽奖小广告',
+      activityIds: [
+        'com.meituan.android.pt.homepage.activity.MainActivity',
+        'com.miui.home.launcher.Launcher',
+      ],
+      rules: 'FrameLayout > @ImageView + ImageView[desc="资质与规则"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12639815',
+        'https://gkd-kit.gitee.io/import/12639734',
+      ],
+    },
   ],
 });
