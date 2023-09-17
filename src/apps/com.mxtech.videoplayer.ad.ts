a0e476369c3ffd02017407f5b772ffcf8af2fbd1
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-17 22:14:36 +0800

    feat(MX播放器): 谷歌广告 (#117)

---
+++ src/apps/com.mxtech.videoplayer.ad.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.mxtech.videoplayer.ad',
+  name: 'MX 播放器',
+  groups: [
+    {
+      key: 1,
+      name: '谷歌广告',
+      activityIds: 'com.mxtech.videoplayer.ad.ActivityMediaList',
+      rules: [
+        {
+          matches:
+            '[desc="Advertisement"] + View >(2) View + Button[clickable=true]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12642204'],
+        },
+      ],
+    },
+  ],
+});
