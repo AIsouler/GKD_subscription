9f65a1ff46dc1c385338b2909cba8542f1e53e72
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-17 22:04:45 +0800

    feat(拼多多): 更新弹窗,首页优惠弹窗,免单活动弹窗,红包弹窗,多多买菜抽奖弹窗,多多视频悬浮窗广告 (#113)

---
+++ src/apps/com.xunmeng.pinduoduo.ts
@@ -0,0 +1,85 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xunmeng.pinduoduo',
+  name: '拼多多',
+  groups: [
+    {
+      enable: false,
+      key: 0,
+      name: '开屏广告',
+      desc: '占位，暂时不起作用',
+      activityIds: ['com.xunmeng.pinduoduo.ui.activity.HomeActivity'],
+      rules: [],
+      snapshotUrls: [],
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: ['com.xunmeng.pinduoduo.activity.NewPageActivity'],
+      rules: [
+        '[text*="新版本"] - ImageView < LinearLayout < LinearLayout + ImageButton[clickable=true]',
+      ],
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12642017'],
+    },
+    {
+      enable: false,
+      key: 2,
+      name: '首页优惠弹窗',
+      activityIds: [
+        'com.xunmeng.pinduoduo.ui.activity.HomeActivity',
+        'com.miui.home.launcher.Launcher',
+      ],
+      rules: [
+        'FrameLayout > FrameLayout > RelativeLayout > @ImageView[id="com.xunmeng.pinduoduo:id/pdd"][text=null][clickable=true] + ImageView[id="com.xunmeng.pinduoduo:id/pdd"][text=null][clickable=true]',
+        'View[id=null] > TextView + View > View +(4) View[clickable=true] > Image[id=null]',
+      ],
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12642015',
+        'https://gkd-kit.gitee.io/import/12642019',
+      ],
+    },
+    {
+      enable: false,
+      key: 3,
+      name: '免单活动弹窗',
+      activityIds: ['com.xunmeng.pinduoduo.activity.NewPageActivity'],
+      rules: [
+        'TextView[text="免单特权"] < View < View +(5) View[clickable=true] > Image[id=null]',
+        'TextView[text$="抽免单"] -(2) Image[id=null][clickable=true]',
+      ],
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12642032',
+        'https://gkd-kit.gitee.io/import/12642038',
+      ],
+    },
+    {
+      enable: false,
+      key: 4,
+      name: '红包弹窗',
+      activityIds: ['com.xunmeng.pinduoduo.ui.activity.HomeActivity'],
+      rules: [
+        '@ImageView[id=null][clickable=true] < ViewGroup + ViewGroup > ImageView + ViewGroup > ImageView +(2) FrameLayout > TextView[text="专属现金红包"]',
+      ],
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12642023'],
+    },
+    {
+      enable: false,
+      key: 5,
+      name: '多多买菜抽奖弹窗',
+      activityIds: ['com.xunmeng.pinduoduo.activity.NewPageActivity'],
+      rules: ['[id=null][text="关闭弹窗"][clickable=true]'],
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12642053'],
+    },
+    {
+      key: 6,
+      name: '多多视频悬浮窗广告',
+      activityIds: ['com.xunmeng.pinduoduo.ui.activity.HomeActivity'],
+      rules: [
+        '@TextView[id=null][clickable=true] + Image[id=null][text="webp"]',
+        '@TextView[id=null][clickable=true] + View > Image[text="webp"]',
+      ],
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12642058'],
+    },
+  ],
+});
