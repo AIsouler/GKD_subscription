7b54adf40e3294dd8cca5ad17853eafd52fbc463
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-17 22:41:10 +0800

    feat(无线魔盒): 更新弹窗 (#126)

---
+++ src/apps/cn.com.ruijie.magicbox.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.com.ruijie.magicbox',
+  name: '无线魔盒',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'cn.com.ruijie.wifibox.activity.MainActivity',
+      rules: [
+        '@ImageView[id="cn.com.ruijie.magicbox:id/imageView_close"] +(2) RelativeLayout > LinearLayout > [text*="新版本"]',
+      ],
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12642359',
+    },
+  ],
+});
