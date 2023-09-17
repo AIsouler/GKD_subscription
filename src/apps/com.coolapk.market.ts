7a40f65ea4169e67a86a647db5e6a4bd97e66a47
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-17 22:10:41 +0800

    feat(酷安): 关闭卡片广告（旧版） (#115)
    * 修改-广告反馈-规则
    
    * feat(酷安)
    
    * Update com.coolapk.market.ts

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -32,9 +32,10 @@ export default defineAppConfig({
             'com.coolapk.market.view.main.MainActivity',
             'com.coolapk.market.view.base.SimpleAlphaActivity',
           ],
-          snapshotUrls:
+          snapshotUrls: [
             'https://gkd-kit.gitee.io/import/38517192/9badc07c-4da2-4066-8af5-d96a86a28315',
-          matches: '@LinearLayout > TextView[id!=null][text=`不感兴趣`]',
+          ],
+          matches: ['[id="com.byted.pangle.m:id/tt_item_tv"][text=`不感兴趣`]'],
         },
         'Button[text$="免广告"] + Button[text="不感兴趣"]', // 1692148279610
         'Button[text$=`去广告`] - Button[text=`不感兴趣`]',
@@ -47,6 +48,35 @@ export default defineAppConfig({
       activityIds: 'com.coolapk.market.view.main.MainActivity',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12503762',
       rules: '[text=`立即更新`] - [text=`取消`]',
+    },
+    {
+      key: 2,
+      name: '关闭卡片广告（旧版）',
+      desc: '点击卡片右上角按钮,然后点击关闭弹窗',
+      activityIds: [
+        'com.coolapk.market.view.feed.FeedDetailActivityV8',
+        'com.bytedance.sdk.openadsdk.core.dislike.ui',
+        'com.coolapk.market.view.node.DynamicNodePageActivity',
+      ],
+      rules: [
+        {
+          preKeys: [1],
+          name: '关闭广告-反馈理由',
+          matches: '[text$="今日免广告"] + [text="不感兴趣"][clickable=true]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12642132',
+            'https://gkd-kit.gitee.io/import/12642155',
+          ],
+        },
+        {
+          key: 1,
+          matches: ['[id="com.coolapk.market:id/close_view"][desc="关闭"]'],
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12642094',
+            'https://gkd-kit.gitee.io/import/12642148',
+          ],
+        },
+      ],
     },
   ],
 });
