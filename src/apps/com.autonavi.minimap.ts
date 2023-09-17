e27a2cd715d42809ce99e6ba4719dde430f58b1f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-17 23:26:07 +0800

    feat(高德地图): 首页-地图页面上方消息提醒, 首页-签到卡片, 加油页面-优惠券弹窗 (#134)

---
+++ src/apps/com.autonavi.minimap.ts
@@ -0,0 +1,37 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.autonavi.minimap',
+  name: '高德地图',
+  groups: [
+    {
+      key: 10,
+      name: '首页-地图页面上方消息提醒',
+      activityIds: 'com.autonavi.map.activity.NewMapActivity',
+      rules:
+        'RelativeLayout[desc="弹窗"] > [id="com.autonavi.minimap:id/msgbox_popup_clear"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12642830',
+    },
+    {
+      enable: false,
+      key: 11,
+      name: '首页-签到卡片',
+      activityIds: 'com.autonavi.map.activity.NewMapActivity',
+      rules:
+        '@ImageView < ViewGroup -(2) ViewGroup > ViewGroup > View[text*="签到"||text*="成长值"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12642842',
+        'https://gkd-kit.gitee.io/import/12642845',
+      ],
+    },
+    {
+      enable: false,
+      key: 12,
+      name: '加油页面-优惠券弹窗',
+      activityIds: 'com.autonavi.map.activity.NewMapActivity',
+      rules:
+        '@ImageView < [desc="关闭"][clickable=true] - ViewGroup > ViewGroup > ViewGroup > View[text="立即领取"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12642857'],
+    },
+  ],
+});
