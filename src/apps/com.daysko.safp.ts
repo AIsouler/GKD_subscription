fb2630a9229f7dd33e1a5c1d3cb9e28fb7204f60
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-18 00:47:26 +0800

    feat(7天澳門統考): 全屏广告, 视频广告, 页面底部广告 (#136)
    * feat(7天澳門統考)
    
    * fix(7天澳門統考): 修正规则
    
    * perf(7天澳門統考): 提升规则准确性和范围
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/com.daysko.safp.ts
@@ -0,0 +1,84 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.daysko.safp',
+  name: '7天澳門統考',
+  groups: [
+    {
+      key: 1,
+      name: '谷歌广告-全屏广告',
+      activityIds: 'com.google.android.gms.ads.AdActivity',
+      rules: [
+        {
+          key: 1,
+          matches: '@[text="关闭"] < View + View > [text="安装"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12642909',
+        },
+        {
+          key: 2,
+          matches: '[id="adchoiceWrap"] + [id="mv_close_btn"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12643032',
+        },
+        {
+          key: 3,
+          name: '右上角白色圆形背景黑色x关闭按钮',
+          matches:
+            'View[clickable=false][childCount=2] > TextView[clickable=true] + View[clickable=true][childCount=1] > Button[clickable=true]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12643039',
+            'https://gkd-kit.gitee.io/import/12643246',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '谷歌广告-视频广告',
+      desc: '点击跳过视频-点击关闭按钮',
+      activityIds: ['com.google.android.gms.ads.AdActivity'],
+      rules: [
+        {
+          key: 1,
+          matches:
+            'TextView[id=null][text="跳过视频"] < View +(2) @View > Button[clickable=true]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12642913',
+            'https://gkd-kit.gitee.io/import/12642932',
+          ],
+        },
+        {
+          key: 3,
+          matches: [
+            'Button[id=null][clickable=true] < @View - TextView < View - View >(n) View[desc="安装"]', // 点中间的View才会关闭广告，最后两个 View 之间的距离不确定
+          ],
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12642952'],
+        },
+        {
+          key: 4,
+          cd: 2500,
+          matches: ['[desc="了解详情"] - View > [text="关闭"]'],
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12642983'],
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '谷歌广告-页面底部广告',
+      activityIds: 'com.daysko.safp.MainActivity',
+      rules: [
+        {
+          matches:
+            '[id!="why_this_ad_btn"] -2 View[id="mys-wrapper"] + View >2 Button[clickable=true]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12642993',
+            'https://gkd-kit.gitee.io/import/12643229',
+          ],
+        },
+        {
+          matches: 'View[desc="Advertisement"] + View >(2) Button',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12643001'],
+        },
+      ],
+    },
+  ],
+});
