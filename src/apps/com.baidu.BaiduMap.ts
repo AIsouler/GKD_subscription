774e3a2045c68bd03bb7ae93642de1ad4e82891e
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-17 22:33:38 +0800

    feat(百度地图): 地图上方黄页横幅, 打车界面-右侧悬浮球, 美食大礼包弹窗, 酒店提前订弹窗 (#124)

--- src/apps/com.baidu.BaiduMap.ts
+++ src/apps/com.baidu.BaiduMap.ts
@@ -10,8 +10,38 @@ export default defineAppConfig({
       activityIds: 'com.baidu.baidumaps.MapsActivity',
       rules: [
         '@TextView[text^=`跳过`] + TextView[text=`广告`]',
-        'ImageView[clickable=false] + TextView[text^=\'跳过\'][clickable=true]', // 1689423368140
+        'ImageView[clickable=false] + TextView[text^="跳过"][clickable=true]', // 1689423368140
       ],
+    },
+    {
+      key: 1,
+      name: '地图上方黄页横幅',
+      activityIds: 'com.baidu.baidumaps.MapsActivity',
+      rules: '[id="com.baidu.BaiduMap:id/yellow_banner_close"][desc="关闭"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12642301',
+    },
+    {
+      key: 2,
+      name: '打车界面-右侧悬浮球',
+      activityIds: 'com.baidu.baidumaps.MapsActivity',
+      rules: 'RelativeLayout[id="com.baidu.BaiduMap:id/rl_close_content"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12642307',
+    },
+    {
+      enable: false,
+      key: 3,
+      name: '美食大礼包弹窗',
+      activityIds: 'com.baidu.baidumaps.MapsActivity',
+      rules: '[text="美食大礼包"] +(6) TextView[id=null][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12642310',
+    },
+    {
+      enable: false,
+      key: 4,
+      name: '酒店提前订弹窗',
+      activityIds: 'com.baidu.baidumaps.MapsActivity',
+      rules: '[id="lottie_box"] + TextView[id=null][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12642319',
     },
   ],
 });
