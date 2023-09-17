8a09ad8c97c2f32f1e138fcb78b03019ded80326
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-17 23:29:19 +0800

    feat(青创网): 更新弹窗 (#123)

---
+++ src/apps/com.m17qcc.mm.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.m17qcc.mm',
+  name: '青创网',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'miuix.appcompat.app.m',
+      rules: '[text*="新版本"] +(n) LinearLayout > TextView[text="取消"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12642297',
+    },
+  ],
+});
