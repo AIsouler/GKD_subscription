da4be4a2440a97a3a9a5e8ddbb8c1d3909e9ec7b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-17 22:26:21 +0800

    feat(京东): 购物车界面-砸金蛋, 我的界面-参与调研 (#121)

--- src/apps/com.jingdong.app.mall.ts
+++ src/apps/com.jingdong.app.mall.ts
@@ -11,5 +11,22 @@ export default defineAppConfig({
       rules: '[desc$="广告"] +2 [desc="跳过"] > [text="跳过"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12519430',
     },
+    {
+      enable: false,
+      key: 1,
+      name: '购物车界面-砸金蛋',
+      activityIds: 'com.jingdong.app.mall.MainFrameActivity',
+      rules: '[id="com.jd.lib.cart.feature:id/iv_egg_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12642266',
+    },
+    {
+      enable: false,
+      key: 2,
+      name: '我的界面-参与调研',
+      activityIds: 'com.jingdong.app.mall.MainFrameActivity',
+      rules:
+        'RelativeLayout > ImageView[text=null][id^="com.jd.lib.personal.feature:id"] + ImageView[text=null][id^="com.jd.lib.personal.feature:id"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12642270',
+    },
   ],
 });
