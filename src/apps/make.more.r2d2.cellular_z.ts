d2fe8a921ede36bfbac9fbf7e4f7981f73edbdc8
CallMeBill <callmebill@billw.cn>
2023-09-19 11:33:47 +0800

    feat(Cellular-Z): 开屏广告 (#158)

---
+++ src/apps/make.more.r2d2.cellular_z.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'make.more.r2d2.cellular_z',
+  name: 'Cellular-Z',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      activityIds: 'make.more.r2d2.cellular_z.activity.SplashGMActivity',
+      rules: [
+        {
+          matches: '[text^="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12648511',
+        },
+      ],
+    },
+  ],
+});
