4346650d929993ebe656e26fb00f89785858f1ba
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-19 11:37:47 +0800

    feat(喵喵折): 更新弹窗 (#162)

---
+++ src/apps/com.henzanapp.miaomiaozhe.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.henzanapp.miaomiaozhe',
+  name: '喵喵折',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.henzanapp.miaomiaozhe.activity.WelcomeActivity',
+      rules: '[text="更新提示"] +(2) LinearLayout > [text="取消"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12649457'],
+    },
+  ],
+});
