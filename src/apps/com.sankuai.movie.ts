b49c8dce1e59798e8c562521aff524b3cfe31be2
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-19 11:45:29 +0800

    feat(猫眼): 开屏广告, 更新弹窗 (#166)

---
+++ src/apps/com.sankuai.movie.ts
@@ -0,0 +1,34 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.sankuai.movie',
+  name: '猫眼',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: [
+        'com.sankuai.movie.welcome.Welcome',
+        'com.miui.home.launcher.Launcher',
+      ],
+      rules: '[text="点击跳转详情页"] < FrameLayout +(2) [text$="跳过"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12649967', // activityId: com.sankuai.movie.welcome.Welcome
+        'https://gkd-kit.gitee.io/import/12649911', // activityId: com.miui.home.launcher.Launcher
+      ],
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: [
+        'com.maoyan.android.adx.popupads.d',
+        'com.miui.home.launcher.Launcher',
+      ],
+      rules: '[text^="更新"] +(3) LinearLayout > [text="取消"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12649942', // activityId: com.maoyan.android.adx.popupads.d
+        'https://gkd-kit.gitee.io/import/12649938', // activityId: com.miui.home.launcher.Launcher
+      ],
+    },
+  ],
+});
