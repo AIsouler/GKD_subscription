0fbc537c277dea2acd35c0081c71bf6dfab26ad2
CallMeBill <callmebill@billw.cn>
2023-09-19 21:21:23 +0800

    feat:(华为主题): 开屏广告,主页面的广告, 首页活动弹窗 (#183)
    * feat(华为主题): 开屏广告
    
    * feat(华为主题): 增加了一种存在于主页面的广告
    
    * feat(华为主题): 增加了一种存在于主页面的广告
    
    * feat(华为主题): 首页活动弹窗

--- src/apps/com.huawei.android.thememanager.ts
+++ src/apps/com.huawei.android.thememanager.ts
@@ -5,6 +5,20 @@ export default defineAppConfig({
   name: '华为主题',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      activityIds: [
+        'com.huawei.android.thememanager.mvp.view.activity.onlinebase.PageActivity',
+      ],
+      rules: [
+        {
+          matches: '[id="com.huawei.hms.ads.uiengine:id/hiad_skip_text"]',
+          action: 'clickCenter',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12657379',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '主题详情页“下应用领主题”横幅',
       activityIds: [
@@ -93,22 +107,36 @@ export default defineAppConfig({
     },
     {
       key: 6,
-      name: '精选页信息流广告',
+      name: '主页面（推荐、分类、我的）广告',
       activityIds: ['com.huawei.android.thememanager.HwThemeManagerActivity'],
       rules: [
         {
           matches:
-            '[id="com.huawei.android.thememanager:id/announce_ad_close_btn"||id="com.huawei.android.thememanager:id/pps_lable_top"||id="com.huawei.android.thememanager:id/pps_img_video_close"]',
+            '[id="com.huawei.android.thememanager:id/announce_ad_close_btn"||id="com.huawei.android.thememanager:id/pps_lable_top"||id="com.huawei.android.thememanager:id/pps_img_video_close"||id="com.huawei.android.thememanager:id/pps_label_ll"]',
           snapshotUrls: [
             'https://gkd-kit.gitee.io/import/12647614',
             'https://gkd-kit.gitee.io/import/12647653',
             'https://gkd-kit.gitee.io/import/12647650',
+            'https://gkd-kit.gitee.io/import/12657822',
           ],
         },
         {
           matches:
             '[id="com.huawei.android.thememanager:id/tv_btn_uninterested"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12647655',
+        },
+      ],
+    },
+    {
+      key: 7,
+      name: '首页活动弹窗-默认禁用',
+      enable: false,
+      activityIds: ['com.huawei.android.thememanager.HwThemeManagerActivity'],
+      rules: [
+        {
+          matches:
+            '[id="com.huawei.android.thememanager:id/campaign_dialog_close_image"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12657939',
         },
       ],
     },
