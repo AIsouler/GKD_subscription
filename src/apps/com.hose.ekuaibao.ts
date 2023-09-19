3c015b8eafc9c59260e93b50bd51277cceef3364
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-19 11:38:25 +0800

    feat(易快报): 首页-横幅广告 (#163)

---
+++ src/apps/com.hose.ekuaibao.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.hose.ekuaibao',
+  name: '易快报',
+  groups: [
+    {
+      key: 1,
+      name: '首页-横幅广告',
+      activityIds: 'com.hose.ekuaibao.view.activity.EkbProActivity',
+      rules:
+        'View[childCount=2] > TextView + TextView[text="不感兴趣"][clickable=true]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12649616'],
+    },
+  ],
+});
