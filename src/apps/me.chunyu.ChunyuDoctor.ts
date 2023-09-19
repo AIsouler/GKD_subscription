a8df26488bb614280a452b77ba056922059f797a
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-20 00:27:32 +0800

    feat(春雨医生): 开屏广告, 应用评分弹窗  (#189)
    * feat(春雨医生)
    
    * 移动 ts 文件

---
+++ src/apps/me.chunyu.ChunyuDoctor.ts
@@ -0,0 +1,44 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'me.chunyu.ChunyuDoctor',
+  name: '春雨医生',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: [
+        'me.chunyu.ChunyuDoctor.Activities.WelcomeActivity',
+        'com.miui.home.launcher.Launcher',
+      ],
+      rules: [
+        {
+          matches:
+            '[id="me.chunyu.ChunyuDoctor:id/tv_ad_label"] +(2) [id="me.chunyu.ChunyuDoctor:id/welcome_tv_timer"][text^="跳过"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12661722',
+            'https://gkd-kit.gitee.io/import/12661937',
+          ],
+        },
+      ],
+    },
+    {
+      key: 10,
+      name: '应用评分弹窗',
+      activityIds: [
+        'me.chunyu.ChunyuDoctor.Activities.WelcomeActivity',
+        'me.chunyu.ChunyuDoctor.Activities.MainActivity',
+      ],
+      rules: [
+        {
+          matches:
+            '[text="给应用评分"] +(2) RelativeLayout > [text="下次再说"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12661907',
+            'https://gkd-kit.gitee.io/import/12661924',
+          ],
+        },
+      ],
+    },
+  ],
+});
