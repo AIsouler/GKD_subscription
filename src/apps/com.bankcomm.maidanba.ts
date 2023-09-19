d12a3d4e2f1d92198a497f3e126adc1f07b8ae33
lisonge <i@songe.li>
2023-09-19 21:49:51 +0800

    feat(买单吧): 开屏广告 (#143)

---
+++ src/apps/com.bankcomm.maidanba.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.bankcomm.maidanba',
+  name: '买单吧',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.bankcomm.maidanba.activity.SplashActivity',
+      rules: '[id="com.bankcomm.maidanba:id/ll_ad_skip"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12660986',
+    },
+  ],
+});
