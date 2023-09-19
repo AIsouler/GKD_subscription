86891f38a5dcc897468eea768926d851cfea738e
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-19 23:55:19 +0800

    feat(FOWL): 开屏广告 (#188)

---
+++ src/apps/vstc.AKCXKJ.client.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'vstc.AKCXKJ.client',
+  name: 'FOWL',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: [
+        'vstc.AKCXKJ.client.SplashActivity',
+        'vstc.AKCXKJ.client.MainActivity',
+      ],
+      rules: [
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12661199'],
+        },
+        {
+          matches: '@[id=null][text^="跳过"] < LinearLayout +(4) ImageView',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12661178'],
+        },
+      ],
+    },
+  ],
+});
