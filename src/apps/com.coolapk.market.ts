cfa1f7d4c66efeaa33650f5f692c350202215100
lisonge <i@songe.li>
2023-09-19 12:06:00 +0800

    feat(酷安): 关闭卡片广告

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -34,11 +34,11 @@ export default defineAppConfig({
           ],
           snapshotUrls: [
             'https://gkd-kit.gitee.io/import/38517192/9badc07c-4da2-4066-8af5-d96a86a28315',
+            'https://gkd-kit.gitee.io/import/12655713',
           ],
-          matches: ['[id="com.byted.pangle.m:id/tt_item_tv"][text=`不感兴趣`]'],
+          matches: ['[id$="id/tt_item_tv"][text=`不感兴趣`]'],
         },
-        'Button[text$="免广告"] + Button[text="不感兴趣"]', // 1692148279610
-        'Button[text$=`去广告`] - Button[text=`不感兴趣`]',
+        'Button[text$="广告"] + Button[text="不感兴趣"]',
         '[id=`com.coolapk.market:id/close_view`]',
       ],
     },
