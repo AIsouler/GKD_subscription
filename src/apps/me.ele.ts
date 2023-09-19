3513ac98615a99eecdab2a72d5a1aa9c318d594a
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-19 11:49:05 +0800

    feat(饿了么): 更新弹窗,开屏红包弹窗, 超市便利页面-红包弹窗 (#168)

--- src/apps/me.ele.ts
+++ src/apps/me.ele.ts
@@ -11,5 +11,30 @@ export default defineAppConfig({
       rules: '[id="me.ele:id/skip_button"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/12534930',
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.afollestad.materialdialogs.MaterialDialog',
+      rules:
+        '[text*="更新应用版本"] < ScrollView + [text="取消"][focusable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12650280',
+    },
+    {
+      enable: false,
+      key: 2,
+      name: '开屏红包弹窗',
+      activityIds: 'me.ele.component.pops2.Pops2MistDialog',
+      rules:
+        '@ImageView[id=null][clickable=true] - ViewGroup >(5) ViewGroup[childCount=5]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12650238',
+    },
+    {
+      enable: false,
+      key: 3,
+      name: '超市便利页面-红包弹窗',
+      activityIds: 'me.ele.newretail.pack.ui.activity.PackActivity',
+      rules: '[desc$="今日红包"] +(n) [desc$="关闭"][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12650713',
+    },
   ],
 });
