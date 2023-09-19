bd6e8fd6c123f0a8a8ed1bd8c19b810bbc98d386
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-20 00:30:31 +0800

    feat(海南一卡通): 开屏广告, 更新弹窗 (#190)
    * feat(海南一卡通)
    
    * 移动 ts 文件

---
+++ src/apps/com.onecard.hainan.ts
@@ -0,0 +1,42 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.onecard.hainan',
+  name: '海南一卡通',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: [
+        'com.hainan.renshe.app.ui.SplashActivity',
+        'com.miui.home.launcher.Launcher',
+      ],
+      rules: [
+        {
+          matches: '[id="com.onecard.hainan:id/tv_jump"][text$="跳过"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12662092',
+            'https://gkd-kit.gitee.io/import/12662129',
+          ],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: [
+        'com.hainan.renshe.app.ui.SplashActivity',
+        'com.miui.home.launcher.Launcher',
+      ],
+      rules: [
+        {
+          matches: '@[text="暂不更新"] +(2) [text="立即更新"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12662114',
+            'https://gkd-kit.gitee.io/import/12662136',
+          ],
+        },
+      ],
+    },
+  ],
+});
