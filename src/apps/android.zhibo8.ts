43a3c66402b36a2ab6113152f1a4cc11310ea8ad
lisonge <i@songe.li>
2023-09-19 21:59:51 +0800

    feat(直播吧): 开屏广告

---
+++ src/apps/android.zhibo8.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'android.zhibo8',
+  name: '直播吧',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      rules: [
+        {
+          matches: '[id="android.zhibo8:id/tv_skip_text"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12605327',
+        },
+        {
+          matches: '[id="android.zhibo8:id/asv_splash"] TextView[text^="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12661109',
+        },
+      ],
+    },
+  ],
+});
