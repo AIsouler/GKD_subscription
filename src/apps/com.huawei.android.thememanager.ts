de2c462e37d02a48934556291440f7f05fcbd0cf
CallMeBill <callmebill@billw.cn>
2023-09-20 11:35:47 +0800

    feat(华为主题): 首页弹窗 (#196)
    * feat(华为主题): 首页弹窗
    
    * feat(华为主题): 首页弹窗

--- src/apps/com.huawei.android.thememanager.ts
+++ src/apps/com.huawei.android.thememanager.ts
@@ -131,12 +131,18 @@ export default defineAppConfig({
       key: 7,
       name: '首页活动弹窗-默认禁用',
       enable: false,
-      activityIds: ['com.huawei.android.thememanager.HwThemeManagerActivity'],
+      activityIds: [
+        'com.huawei.android.thememanager.HwThemeManagerActivity',
+        'com.huawei.android.launcher.unihome.UniHomeLauncher',
+      ],
       rules: [
         {
           matches:
             '[id="com.huawei.android.thememanager:id/campaign_dialog_close_image"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12657939',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12657939',
+            'https://gkd-kit.gitee.io/import/12667815',
+          ],
         },
       ],
     },
