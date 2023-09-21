0743808fcb07440536e5128125e9d6f1c0f0f6ec
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-22 00:03:06 +0800

    feat(CSDN): 开屏广告,更新弹窗,信息流广告,请求推送通知弹窗  (#214)
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/net.csdn.csdnplus.ts
+++ src/apps/net.csdn.csdnplus.ts
@@ -8,27 +8,61 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'net.csdn.csdnplus.activity.SplashActivity',
-      rules: '[id="net.csdn.csdnplus:id/tv_skip_splashimage"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12668884',
+      rules: [
+        {
+          matches: '[id="net.csdn.csdnplus:id/tv_skip_splashimage"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12673680'],
+        },
+      ],
     },
     {
       key: 1,
-      name: '推荐条目广告',
-      activityIds: 'net.csdn.csdnplus.activity.MainActivity',
+      name: '更新弹窗',
+      activityIds: [
+        'net.csdn.csdnplus.activity.MainActivity',
+        'net.csdn.csdnplus.activity.SetActivity',
+      ],
       rules: [
         {
-          key: 0,
           matches:
-            'TextView[text="广告"] + ImageView[id="net.csdn.csdnplus:id/iv_more"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12668903',
+            '@[id="net.csdn.csdnplus:id/cancel"][text="以后再说"] +(2) [id="net.csdn.csdnplus:id/update"][text="立即安装"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12673693',
+            'https://gkd-kit.gitee.io/import/12673654',
+          ],
         },
+      ],
+    },
+    {
+      key: 2,
+      name: '信息流广告',
+      activityIds: ['net.csdn.csdnplus.activity.MainActivity'],
+      rules: [
         {
           key: 1,
           matches:
+            'TextView[id=null][text="广告"] + ImageView[id="net.csdn.csdnplus:id/iv_more"][clickable=true]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12673738'],
+        },
+        {
+          preKeys: [1],
+          key: 2,
+          matches:
             '[id="net.csdn.csdnplus:id/img_feedback_title"][text="虚假广告"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12668923',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12673787'],
         },
       ],
+    },
+    {
+      enable: false,
+      key: 10,
+      name: '请求推送通知弹窗',
+      activityIds: 'net.csdn.csdnplus.activity.MainActivity',
+      rules: {
+        matches:
+          '@[id="net.csdn.csdnplus:id/iv_close"][clickable=true] + LinearLayout > [text="开启推送通知"]',
+        snapshotUrls: ['https://gkd-kit.gitee.io/import/12673638'],
+      },
     },
   ],
 });
