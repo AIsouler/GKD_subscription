080067df4a62dfca7a36d9bf7dac6b1f604c0451
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-21 21:26:18 +0800

    feat(米游社): 开屏广告, 青少年模式, 版本更新提示  (#225)
    * 米游社 版本更新提示
    
    * feat(米游社)
    
    * 完善青少年模式弹窗规则
    
    ---------
    
    Co-authored-by: Cheng <184034037@qq.com>

--- src/apps/com.mihoyo.hyperion.ts
+++ src/apps/com.mihoyo.hyperion.ts
@@ -10,15 +10,29 @@ export default defineAppConfig({
       activityIds: [
         'com.mihoyo.hyperion.ui.SplashActivity',
         'com.mihoyo.hyperion.splash.SplashActivity',
+        'com.miui.home.launcher.Launcher',
       ],
-      rules: '[id=`com.mihoyo.hyperion:id/mSplashBtJump`]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12482738',
+      rules: '[id="com.mihoyo.hyperion:id/mSplashBtJump"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12482738',
+        'https://gkd-kit.gitee.io/import/12675530', // activityId: 'com.miui.home.launcher.Launcher'
+      ],
     },
     {
       key: 1,
       name: '青少年模式',
       desc: '关闭青少年模式提醒弹窗',
-      rules: 'TextView[id=`com.mihoyo.hyperion:id/tv_dialog_i_know`]',
+      activityIds: 'com.mihoyo.hyperion.main.HyperionMainActivity',
+      rules:
+        'TextView[id="com.mihoyo.hyperion:id/tv_dialog_go_to_teenage_mode"] + TextView[id="com.mihoyo.hyperion:id/tv_dialog_i_know"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12675547',
+    },
+    {
+      key: 2,
+      name: '版本更新提示',
+      desc: '关闭版本更新提示弹窗',
+      rules: 'TextView[id="com.mihoyo.hyperion:id/mUpgradeDialogCancel"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12675513',
     },
   ],
 });
