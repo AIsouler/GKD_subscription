0301328dcba4c1fd104a62e14b8c563f3411b720
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-21 21:45:48 +0800

    feat(阿里巴巴): 更新弹窗, 开屏广告 (#246)

--- src/apps/com.alibaba.wireless.ts
+++ src/apps/com.alibaba.wireless.ts
@@ -8,7 +8,22 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.alibaba.wireless.launch.home.V5HomeActivity',
-      rules: '[id=`com.alibaba.wireless:id/v5_splash_over`]',
+      rules: '[id="com.alibaba.wireless:id/v5_splash_over"][text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12684411',
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: [
+        'com.alibaba.wireless.launch.home.V5HomeActivity',
+        'com.alibaba.wireless.workbench.myali.MyAliSettingActivity',
+      ],
+      rules:
+        '[id="com.alibaba.wireless:id/update_title"] +(2) LinearLayout >(2) [text="取消"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12684422',
+        'https://gkd-kit.gitee.io/import/12684426',
+      ],
     },
   ],
 });
