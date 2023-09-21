3bd2b6632f68be8859f219a091f28e469f5e026a
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-21 20:17:30 +0800

    feat(Emobile7): 更新弹窗 (#210)
    * feat(EMobile7)
    
    * feat(EMobile7)
    
    * 增加 id

---
+++ src/apps/com.weaver.emobile7.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.weaver.emobile7',
+  name: 'EMobile7',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'weaver.fw.com.MainActivity',
+      rules: [
+        {
+          matches:
+            '@[id="com.weaver.emobile7:id/sdl__negative_button"][text="取消"] +(2) [id="com.weaver.emobile7:id/sdl__positive_button"][text="更新"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12673154'],
+        },
+      ],
+    },
+  ],
+});
