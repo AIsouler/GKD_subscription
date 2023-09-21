fb154f66232f975b76f72ee18248b06b659fced8
CallMeBill <callmebill@billw.cn>
2023-09-21 21:30:45 +0800

    feat(华为浏览器): 开屏广告 (#230)

---
+++ src/apps/com.huawei.browser.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.huawei.browser',
+  name: '华为浏览器',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['com.huawei.browser.BrowserMainActivity'],
+      rules: [
+        {
+          matches: '[id="com.huawei.hms.ads.uiengine:id/hiad_skip_text"]',
+          action: 'clickCenter',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12681995'],
+        },
+      ],
+    },
+  ],
+});
