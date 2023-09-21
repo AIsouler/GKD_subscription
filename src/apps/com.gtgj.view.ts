09b1dfc9d12c590da4a56dfd4f0b42b8ae653670
CallMeBill <callmebill@billw.cn>
2023-09-21 22:01:56 +0800

    feat(高铁管家): 开屏广告 (#247)
    * feat(高铁管家): 开屏
    
    * feat(高铁管家): 开屏
    
    * chore: 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/com.gtgj.view.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.gtgj.view',
+  name: '高铁管家',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.gtgj.view.LaunchActivity',
+      rules:
+        '[id="com.gtgj.view:id/logo_img_container"] >2 FrameLayout[clickable=true] > TextView[text=`跳过`]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12684583'],
+    },
+  ],
+});
