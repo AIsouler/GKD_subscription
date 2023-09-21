f5892e853888b2f62c5b847a1520a2effe27228d
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-21 21:06:25 +0800

    feat(应用商店): 忽略升级（默认关闭） (#217)
    * feat(应用商店)
    
    * 新增示例图片

--- src/apps/com.xiaomi.market.ts
+++ src/apps/com.xiaomi.market.ts
@@ -8,7 +8,37 @@ export default defineAppConfig({
       key: 0,
       name: '首页悬浮窗广告',
       activityIds: 'com.xiaomi.market.ui.FloatWebActivity',
-      rules: 'Button[text=\'关闭\']',
+      rules: 'Button[text="关闭"]',
+    },
+    {
+      enable: false,
+      key: 10,
+      name: '忽略升级',
+      desc: '应用升级界面-自动点击忽略',
+      exampleUrls:
+        'https://github.com/gkd-kit/subscription/assets/45487685/a3a61df9-7757-428e-b4fe-a960e09a0bbe',
+      activityIds: 'com.xiaomi.market.ui.UpdateListActivity',
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[id="com.xiaomi.market:id/fold_text"][text^="查看全部升级"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12674261',
+        },
+        {
+          key: 1,
+          matches:
+            '[id="com.xiaomi.market:id/expandable_textview"] + [id="com.xiaomi.market:id/expand_button"][desc="更多"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12674264',
+        },
+        {
+          preKeys: [1],
+          key: 2,
+          matches:
+            '@[id="com.xiaomi.market:id/ignore_this"][text="忽略本次"] + [id="com.xiaomi.market:id/ignore_permanently"][text="永久忽略"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12674269',
+        },
+      ],
     },
   ],
 });
