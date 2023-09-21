2ec69fa9d344d6974d3883395a4fee592d224103
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-21 23:28:31 +0800

    feat(云端学习): 开屏广告 (#253)
    * feat(云端学习)
    
    * 规则放宽

---
+++ src/apps/com.tbc.android.defaults.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tbc.android.defaults',
+  name: '云端学习',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['com.tbc.android.defaults.MainActivity'],
+      rules: [
+        {
+          matches: '[id="com.tbc.android.defaults:id/tvLoginAdSkip"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12685253'],
+        },
+      ],
+    },
+  ],
+});
