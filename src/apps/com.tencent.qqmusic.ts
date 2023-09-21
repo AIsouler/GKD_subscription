269b1fa3a8a0c6c8dedb0d9eeb2310c07b9f6157
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-21 20:40:49 +0800

    feat(QQ音乐): 开屏广告 (#215)

--- src/apps/com.tencent.qqmusic.ts
+++ src/apps/com.tencent.qqmusic.ts
@@ -1,3 +1,4 @@
+import { matches } from 'lodash';
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
@@ -7,18 +8,29 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: [
-        'com.tencent.qqmusic.activity.AppStarterActivity',
-        'com.tencent.qqmusic.business.splash.thirdpartsplash.tme.union.SplashDialog',
+      rules: [
+        {
+          activityIds: [
+            'com.tencent.qqmusic.activity.AppStarterActivity',
+            'com.tencent.qqmusic.business.splash.thirdpartsplash.tme.union.SplashDialog',
+            'com.miui.home.launcher.Launcher',
+          ],
+          matches: 'TextView[text=`跳过`][clickable=true]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12673960', // text="互动广告"
+            'https://gkd-kit.gitee.io/import/12674023', // text="互动广告 | "
+            'https://gkd-kit.gitee.io/import/12673998', // text="广告"
+            'https://gkd-kit.gitee.io/import/12674074', // text="运营推广"
+          ],
+        },
       ],
-      rules: 'TextView[text=`跳过`][clickable=true]',
     },
     {
       key: 1,
       name: '推荐页-广告卡片',
       activityIds: 'com.tencent.qqmusic.activity.AppStarterActivity',
       rules: [
-        '@LinearLayout[clickable=true] > TextView[text=\'广告\'] + ImageView', // 1688045698846
+        '@LinearLayout[clickable=true] > TextView[text="广告"] + ImageView', // 1688045698846
         'TextView[text="广告 | 关闭"][clickable=true]',
       ],
     },
