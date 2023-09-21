e9555ab76dc935fcb96f32e30277322741c1910d
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-21 23:30:13 +0800

    feat(中国农业银行): 更新弹窗, 切换乡村版弹窗 (#255)

--- src/apps/com.android.bankabc.ts
+++ src/apps/com.android.bankabc.ts
@@ -12,5 +12,23 @@ export default defineAppConfig({
       snapshotUrls:
         'https://gkd-kit.gitee.io/import/38517192/3653218a-e0e0-4a60-8308-dea5fd4179b3',
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.android.bankabc.homepage.UpgradeDialog',
+      rules: [
+        '[id="com.android.bankabc:id/upgrade_bottom"] > [id="com.android.bankabc:id/negativeTextView"][text="残忍拒绝"]',
+      ],
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12685502',
+    },
+    {
+      key: 10,
+      name: '切换乡村版弹窗',
+      activityIds: 'com.android.bankabc.MainActivity',
+      rules: [
+        '[id="com.android.bankabc:id/iv_switch"] + [id="com.android.bankabc:id/iv_skip"]',
+      ],
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12685764',
+    },
   ],
 });
