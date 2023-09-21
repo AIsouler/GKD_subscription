1a484adbb153f76edec43fc448a51418c4de5d2f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-21 21:01:26 +0800

    feat(GIF助手): 开屏广告 (#219)
    * feat(GIF助手)
    
    * chore: 移除注释
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/com.didikee.gifparser.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.didikee.gifparser',
+  name: 'GIF助手',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.gif.SplashAdActivity',
+      rules: [
+        {
+          matches:
+            '[id="com.didikee.gifparser:id/skip_view"][text^="跳过"][clickable=true]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12674390'],
+        },
+      ],
+    },
+  ],
+});
