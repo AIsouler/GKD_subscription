8559128f61738916f030be5102c0433dde6ee465
lisonge <i@songe.li>
2023-09-22 19:54:03 +0800

    fix(直播吧): 开屏广告-快照链接错误

--- src/apps/android.zhibo8.ts
+++ src/apps/android.zhibo8.ts
@@ -10,7 +10,7 @@ export default defineAppConfig({
       rules: [
         {
           matches: '[id="android.zhibo8:id/tv_skip_text"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12605327',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12700387',
         },
         {
           matches: '[id="android.zhibo8:id/asv_splash"] TextView[text^="跳过"]',
