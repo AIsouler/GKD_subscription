3f2882d83acbd21cf8eab135ce33237e01f2516d
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-23 21:32:52 +0800

    feat(涨乐财富通): 开屏广告,更新弹窗 (#276)
    * feat(涨乐财富通)
    
    * feat(涨乐财富通)

--- src/apps/com.lphtsccft.ts
+++ src/apps/com.lphtsccft.ts
@@ -5,8 +5,15 @@ export default defineAppConfig({
   name: '涨乐财富通',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['com.lphtsccft.zhangle.startup.SplashScreenActivity'],
+      rules: '[id="com.lphtsccft:id/countdown_skip"][text="跳过"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12699286'],
+    },
+    {
       key: 1,
-      name: '首页弹窗',
+      name: '首页广告弹窗',
       activityIds: [
         'com.lphtsccft.zhangle.startup.SplashScreenActivity',
         'com.lphtsccft.zhangle.main.MainActivity',
@@ -16,6 +23,14 @@ export default defineAppConfig({
         'https://gkd-kit.gitee.io/import/12646899',
         'https://gkd-kit.gitee.io/import/12646925',
       ],
+    },
+    {
+      key: 2,
+      name: '更新弹窗',
+      activityIds: ['com.lphtsccft.zhangle.main.MainActivity'],
+      rules:
+        '@[id="com.lphtsccft:id/dialog_btn_left"][text="取消"] + [id="com.lphtsccft:id/dialog_btn_right"][text="确定"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12699308'],
     },
   ],
 });
