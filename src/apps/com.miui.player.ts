69071c194c83aa95a1d401ca70d824bd14b46af6
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-23 22:41:15 +0800

    feat(音乐): 会员过期续费弹窗, 我的页面-顶部 banner 广告 (#288)

--- src/apps/com.miui.player.ts
+++ src/apps/com.miui.player.ts
@@ -9,7 +9,25 @@ export default defineAppConfig({
       name: '开屏广告',
       activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
       rules:
-        '@TextView[text$=`跳过`] + TextView[id=`com.miui.player:id/ad_view`]',
+        '@TextView[text$=`跳过`] + TextView[id="com.miui.player:id/ad_view"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12700962',
+    },
+    {
+      enable: false,
+      key: 10,
+      name: '会员过期续费弹窗',
+      activityIds: 'com.tencent.qqmusiccommon.hybrid.HybridViewActivity',
+      rules: '[id=null][desc="关闭弹框按钮"][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12700955',
+    },
+    {
+      enable: false,
+      key: 11,
+      name: '我的页面-顶部 banner 广告',
+      activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
+      rules:
+        '[id="com.miui.player:id/banner_image"] + [id="com.miui.player:id/close_banner"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12700984',
     },
   ],
 });
