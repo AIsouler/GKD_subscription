62fd5c50421b636922cf1b82af57109641b86212
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-24 00:04:47 +0800

    feat(东方财富): 开屏广告, 更新弹窗, 请求通知权限提示信息（默认关闭）(#311)

---
+++ src/apps/com.eastmoney.android.berlin.ts
@@ -0,0 +1,36 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.eastmoney.android.berlin',
+  name: '东方财富',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['com.eastmoney.android.berlin.activity.MainActivity'],
+      rules: '[id="com.eastmoney.android.berlin:id/btn_skip"][text^="跳过"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12706071'],
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: [
+        'com.eastmoney.android.module.launcher.internal.home.HomeActivity',
+      ],
+      rules:
+        '@[id="com.eastmoney.android.berlin:id/tv_update_next_time"] + [id="com.eastmoney.android.berlin:id/tv_update_now"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12706070'],
+    },
+    {
+      enable: true,
+      key: 10,
+      name: '请求通知权限提示信息',
+      activityIds: [
+        'com.eastmoney.android.module.launcher.internal.home.HomeActivity',
+      ],
+      rules:
+        '[id="com.eastmoney.android.berlin:id/push_on_notify_on"] + [id="com.eastmoney.android.berlin:id/push_on_notify_delete"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12706065'],
+    },
+  ],
+});
