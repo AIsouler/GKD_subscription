3e9d5c2e0c3716e27d510c70032aeb91310115f6
longm21 <145310263+longm21@users.noreply.github.com>
2023-09-23 23:46:21 +0800

    feat(Gspace): 开屏广告(增加快照)，主页面上方广告 (#301)
    * chore(网易邮箱): 规则更新
    
    * feat(网易邮箱): 开屏广告-优化
    
    * feat(Gspace): 增加开屏广告场景，增加主页面上方广告
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/com.gspace.android.ts
+++ src/apps/com.gspace.android.ts
@@ -8,14 +8,23 @@ export default defineAppConfig({
       key: 0,
       name: '跳过开屏广告',
       activityIds: [
-        'com.gspace.android.ui.activity.SplashActivity',
-        'com.gspace.android.util.ShortcutHandleActivity',
+        'com.gspace.android.ui.activity.SplashActivity', // Gspace主应用开屏
+        'com.gspace.android.util.ShortcutHandleActivity', // Gspace子应用桌面快捷方式开屏
+        'com.gspace.android.ui.activity.VappLoadingActivity', // Gspace子应用开屏
       ],
       rules: '[id="com.gspace.android:id/hiad_skip_text"]',
       snapshotUrls: [
         'https://gkd-kit.gitee.io/import/12668986',
         'https://gkd-kit.gitee.io/import/12668985',
+        'https://gkd-kit.gitee.io/import/12703037',
       ],
+    },
+    {
+      key: 1,
+      name: '主页面上方广告',
+      activityIds: 'com.gspace.android.ui.activity.main.MainActivity',
+      rules: '[id="com.gspace.android:id/hiad_banner_close_button"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12705339',
     },
   ],
 });
