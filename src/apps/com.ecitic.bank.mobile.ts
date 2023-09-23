66c72c6875631d73792a64a619c9c27241b449ee
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-23 22:56:19 +0800

    feat(中信银行): 开屏广告,更新弹窗,广告弹窗  (#290)
    * feat(中信银行)
    
    * feat(中信银行): 开屏广告-优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/com.ecitic.bank.mobile.ts
@@ -0,0 +1,36 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.ecitic.bank.mobile',
+  name: '中信银行',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['com.ecitic.bank.mobile.ui.SplashActivity'],
+      rules: '[id="com.ecitic.bank.mobile:id/skip_adv_layout"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12701203'],
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: [
+        'com.ecitic.bank.mobile.ui.MainActivity',
+        'com.miui.home.launcher.Launcher',
+      ],
+      rules: '@[text="暂不更新"] + [text="立即更新"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12701217',
+        'https://gkd-kit.gitee.io/import/12701250', // activityId: 'com.miui.home.launcher.Launcher'
+      ],
+    },
+    {
+      key: 2,
+      name: '广告弹窗',
+      activityIds: ['com.ecitic.bank.mobile.ui.webview.CordovaWebViewActivity'],
+      rules:
+        '[id="com.ecitic.bank.mobile:id/id_cordova_view"] + [id="com.ecitic.bank.mobile:id/close_product_send"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12701230'],
+    },
+  ],
+});
