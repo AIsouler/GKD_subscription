812f309c37f7b78b0189df04caa86bae69a05098
CallMeBill <callmebill@billw.cn>
2023-09-23 21:26:21 +0800

    feat(什么值得买): 弹窗广告 (#266)
    close #265

--- src/apps/com.smzdm.client.android.ts
+++ src/apps/com.smzdm.client.android.ts
@@ -14,5 +14,12 @@ export default defineAppConfig({
       rules: '[id="com.smzdm.client.android:id/tv_skip"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/12535072',
     },
+    {
+      key: 1,
+      name: 'APP内弹窗广告',
+      activityIds: 'com.smzdm.client.android.app.HomeActivity',
+      rules: '[id="com.smzdm.client.android:id/dialog_home_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12695751',
+    },
   ],
 });
