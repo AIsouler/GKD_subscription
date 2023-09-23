f6e71886fadff9c348fb59c4097398a150c5d86a
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-24 00:03:35 +0800

    feat(金十数据): 更新弹窗,首页广告弹窗, 快讯页面-广告弹窗, 会员页面-顶部广告（默认关闭）  (#310)

---
+++ src/apps/com.jin10.ts
@@ -0,0 +1,37 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.jin10',
+  name: '金十数据',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: ['com.jin10.lgd.update.UpdateActivity'],
+      rules: '@[id="com.jin10:id/update_cancel"] + [id="com.jin10:id/iv_pic"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12706043'],
+    },
+    {
+      key: 2,
+      name: '首页广告弹窗',
+      activityIds: ['com.jin10.lgd.biz.MainActivity'],
+      rules: '[id="com.jin10:id/iv_body"] + [id="com.jin10:id/iv_close"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12706045'],
+    },
+    {
+      key: 3,
+      name: '快讯页面-广告弹窗',
+      activityIds: ['com.jin10.lgd.biz.MainActivity'],
+      rules: '@[id="com.jin10:id/iv_close"] + [id="com.jin10:id/iv_pic"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12706047'],
+    },
+    {
+      enable: false,
+      key: 10,
+      name: '会员页面-顶部广告',
+      activityIds: ['com.jin10.lgd.biz.MainActivity'],
+      rules: '[id="com.jin10:id/iv_header"] + [id="com.jin10:id/iv_close"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12706051'],
+    },
+  ],
+});
