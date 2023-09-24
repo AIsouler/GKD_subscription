7cdd25c9ffcd393ae39b957027063077d668a7f0
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-25 00:41:21 +0800

    feat(联通智家): 更新弹窗 (#332)

---
+++ src/apps/com.woapp.hebei.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.woapp.hebei',
+  name: '联通智家',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'net.smartont.we.wohome.ui.main.view.MainActivity',
+      rules: '@[text="忽略更新"] + [text="立即更新"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12707746'],
+    },
+  ],
+});
