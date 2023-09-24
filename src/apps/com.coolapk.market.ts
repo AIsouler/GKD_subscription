12fe8d531eeec8a72b0d5c175c40ef0aea5e8cb5
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-25 00:38:04 +0800

    fix(酷安): 关闭卡片广告 (#325)
    * feat(酷安)
    
    * fix(酷安)
    
    * fix(酷安)
    
    * 删除注释
    
    * fix(酷安)

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -22,26 +22,39 @@ export default defineAppConfig({
       name: '关闭卡片广告',
       desc: '点击卡片右上角按钮,然后点击关闭弹窗',
       activityIds: [
-        'com.coolapk.market.view.main.MainActivity',
-        'com.coolapk.market.view.base.SimpleAlphaActivity',
+        'com.coolapk.market.view.main.MainActivity', // 缺少快照
+        'com.coolapk.market.view.base.SimpleAlphaActivity', // 缺少快照
+        'com.coolapk.market.view.node.DynamicNodePageActivity',
       ],
       rules: [
         {
-          activityIds: [
-            'com.bytedance.sdk.openadsdk.core.dislike.ui',
-            'com.coolapk.market.view.main.MainActivity',
-            'com.coolapk.market.view.base.SimpleAlphaActivity',
-            'com.coolapk.market.view.node.DynamicNodePageActivity',
+          key: 1,
+          name: '点击右上角x按钮',
+          matches:
+            '[id="com.coolapk.market:id/ad_time_view"] + [id="com.coolapk.market:id/close_view"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12707506',
+        },
+        {
+          preKeys: [1],
+          key: 2,
+          name: '点击不感兴趣',
+          matches: 'Button[text$="广告"] + Button[text="不感兴趣"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12707509',
+        },
+        {
+          // preKeys: [2], // 取消前置 key，防止快速滑动时停在最后一个界面而不进行点击
+          key: 3,
+          name: '再次点击不感兴趣',
+          activityIds: ['com.bytedance.sdk.openadsdk.core.dislike.ui'],
+          matches: [
+            '@LinearLayout[clickable=true] > [id$="id/tt_item_tv"][text="不感兴趣"]',
           ],
           snapshotUrls: [
             'https://gkd-kit.gitee.io/import/38517192/9badc07c-4da2-4066-8af5-d96a86a28315',
             'https://gkd-kit.gitee.io/import/12655713',
             'https://gkd-kit.gitee.io/import/12660759',
           ],
-          matches: ['[id$="id/tt_item_tv"][text=`不感兴趣`]'],
         },
-        'Button[text$="广告"] + Button[text="不感兴趣"]',
-        '[id="com.coolapk.market:id/close_view"]',
       ],
     },
     {
