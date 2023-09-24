71656344f62650b701d4fb44125ef77dc647980f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-25 00:46:54 +0800

    feat(365时政): 首页广告弹窗 (#340)

---
+++ src/apps/cn.net.shizheng.study.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.net.shizheng.study',
+  name: '365时政',
+  groups: [
+    {
+      key: 1,
+      name: '首页广告弹窗',
+      activityIds: 'cn.dingwei.cloud_classroom.MainActivity',
+      rules:
+        'View[childCount=2] > ImageView[id=""] + Button[id=""][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12708731',
+    },
+  ],
+});
