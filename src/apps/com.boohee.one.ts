0d1909855e47f4ac16ea8b698b8e7545fe3a1582
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-25 23:58:31 +0800

    feat(薄荷健康): 开屏广告,更新弹窗,发现页广告弹窗, 通知权限弹窗 (#376)

---
+++ src/apps/com.boohee.one.ts
@@ -0,0 +1,41 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.boohee.one',
+  name: '薄荷健康',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.boohee.one.app.home.ui.activity.SplashActivity',
+      rules: '[id="com.boohee.one:id/anythink_myoffer_splash_skip"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12716929',
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.boohee.library.update.UpdateDialogActivity',
+      rules:
+        '@[id="com.boohee.one:id/tv_ignore"] + [id="com.boohee.one:id/tv_update"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12716918',
+    },
+    {
+      key: 2,
+      name: '发现页广告弹窗',
+      activityIds: 'com.sensorsdata.sf.ui.view.DialogActivity',
+      rules:
+        'LinearLayout[childCount=2] > @ImageView[id=null][clickable=true] + ImageView',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12716970',
+    },
+    {
+      enable: true,
+      key: 10,
+      name: '请求开启通知权限弹窗',
+      desc: '自动点击x按钮',
+      activityIds:
+        'com.boohee.one.app.account.ui.activity.MsgCategoryActivityV2',
+      rules: '[id="com.boohee.one:id/img_close_push"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12716950',
+    },
+  ],
+});
