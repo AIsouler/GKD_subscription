91198e45ae0fc364d03f54c28fd6895a54563dfd
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-25 21:57:55 +0800

    feat(公考雷达): 更新弹窗,首页右侧悬浮广告, 通知权限弹窗（默认关闭） (#365)

---
+++ src/apps/com.maidu.gkld.ts
@@ -0,0 +1,32 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.maidu.gkld',
+  name: '公考雷达',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.maidu.gkld.ui.main.MainActivity',
+      rules: '[id="com.maidu.gkld:id/tv_update_next"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715215',
+    },
+    {
+      key: 2,
+      name: '首页右侧悬浮广告',
+      activityIds: 'com.maidu.gkld.ui.main.MainActivity',
+      rules:
+        '[id="com.maidu.gkld:id/fl_float"] + [id="com.maidu.gkld:id/close_image_view"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715291',
+    },
+    {
+      enable: false,
+      key: 10,
+      name: '请求通知权限弹窗',
+      activityIds: 'com.maidu.gkld.ui.main.MainActivity',
+      rules:
+        '[id="com.maidu.gkld:id/ll_open_app_notice"] + [id="com.maidu.gkld:id/iv_cancel"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715250',
+    },
+  ],
+});
