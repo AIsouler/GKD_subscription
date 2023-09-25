0d63f5128be6fcf64266d38dd034ebbdd43e7af7
lisonge <i@songe.li>
2023-09-25 14:04:29 +0800

    feat(懂车帝): 首页推荐卡片广告 (#140)

--- src/apps/com.ss.android.auto.ts
+++ src/apps/com.ss.android.auto.ts
@@ -15,10 +15,21 @@ export default defineAppConfig({
       key: 1,
       name: '首页推荐卡片广告',
       activityIds: 'com.ss.android.auto.activity.SplashActivity',
-      rules:
-        '[text="广告"] < LinearLayout + @FrameLayout[clickable=true][id!=null] > TextView[text.length=1][id=null][clickable=false]',
-      // 貌似快照存在延迟导致屏幕与节点不对应
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12660816',
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[text="广告"] < LinearLayout + @FrameLayout[clickable=true][id!=null] > TextView[text.length=1][id=null][clickable=false]',
+          // 貌似快照存在延迟导致屏幕与节点不对应
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12660816',
+        },
+        {
+          preKeys: 1,
+          matches:
+            '[text$="精准屏蔽"] + [text="不感兴趣"][id!=null][clickable=true]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12711589',
+        },
+      ],
     },
   ],
 });
