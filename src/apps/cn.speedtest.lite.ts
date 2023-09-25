68efba257b0888bc2f3ed1a3fb233f672f5aa3a3
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-25 21:59:42 +0800

    feat(网速管家极速版): 更新弹窗, 新人专享优惠弹窗 (#367)

---
+++ src/apps/cn.speedtest.lite.ts
@@ -0,0 +1,25 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.speedtest.lite',
+  name: '网速管家极速版',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'cn.speedtest.lite.main.MainActivity',
+      rules:
+        '@[id="cn.speedtest.lite:id/iv_close"] - [id="cn.speedtest.lite:id/ll_main_pane"] >2 [id="cn.speedtest.lite:id/tv_upgrade_content"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715483',
+    },
+    {
+      enable: false,
+      key: 10,
+      name: '新人专享优惠弹窗',
+      activityIds: 'cn.speedtest.lite.main.MainActivity',
+      rules:
+        '@[id="cn.speedtest.lite:id/iv_close"] - RelativeLayout >n [id="cn.speedtest.lite:id/ad_banner"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715511',
+    },
+  ],
+});
