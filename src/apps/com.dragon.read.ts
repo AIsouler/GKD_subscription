6b1e1b6bcb8f8810ecad84657fa6da6697c8ae11
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-25 23:56:45 +0800

    feat(番茄免费小说): 更新弹窗, 右侧悬浮红包, 通知权限弹窗, 阅读页面底部广告 (#374)

--- src/apps/com.dragon.read.ts
+++ src/apps/com.dragon.read.ts
@@ -10,13 +10,44 @@ export default defineAppConfig({
       rules: [
         {
           activityIds: 'com.dragon.read.ad.banner.ui',
-          matches: '@[clickable=true] TextView[text=`关闭此条广告`]',
+          matches: '@[clickable=true] TextView[text="关闭此条广告"]',
         },
         {
           activityIds: 'com.dragon.read.reader.ReaderActivity',
-          matches: '@ImageView - LinearLayout TextView[text=`广告`]',
+          matches: '@ImageView - LinearLayout TextView[text="广告"]',
+        },
+        {
+          activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
+          matches:
+            '[id="com.dragon.read:id/root_view"] >n ViewGroup > @FrameLayout[id!=null][clickable=true] > ImageView',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12716444',
         },
       ],
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.dragon.read.update',
+      rules: '@[text="以后再说"] + [text="优先体验"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12716477',
+    },
+    {
+      key: 2,
+      name: '右侧悬浮红包',
+      activityIds: 'com.dragon.read.pages.main.MainFragmentActivity',
+      rules:
+        '[id="com.dragon.read:id/action_bar_root"] >n RelativeLayout > ImageView[id!=null][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12716506',
+    },
+    {
+      enable: false,
+      key: 10,
+      name: '请求通知权限弹窗',
+      desc: '自动点击【取消】',
+      activityIds: 'com.dragon.read.widget.ConfirmDialogBuilder',
+      rules:
+        '@[text="取消"] < LinearLayout -2 LinearLayout > [text="开启推送提醒"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12716592',
     },
   ],
 });
