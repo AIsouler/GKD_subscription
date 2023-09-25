e084f0456ffdaff01dc9f987dfed6d5488f1308c
lisonge <i@songe.li>
2023-09-25 21:36:47 +0800

    feat(系统更新): miui更新弹窗 (#359)

---
+++ src/apps/com.android.updater.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.android.updater',
+  name: '系统更新',
+  groups: [
+    {
+      enable: false,
+      key: 0,
+      name: 'MIUI更新弹窗',
+      activityIds: 'com.android.updater.MainActivity',
+      rules:
+        '[id="com.android.updater:id/alertTitle"][text="开启智能更新"] < * +2 * > Button[id="android:id/button2"][text="暂不开启"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715712',
+    },
+  ],
+});
