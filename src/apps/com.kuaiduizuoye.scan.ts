ea825709b2723a28697b4cecb5e4406493a1b8d8
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-25 23:55:47 +0800

    feat(快对): 开屏广告, 首页广告弹窗 (#373)

---
+++ src/apps/com.kuaiduizuoye.scan.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.kuaiduizuoye.scan',
+  name: '快对',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.kuaiduizuoye.scan.activity.init.InitActivity',
+      rules:
+        '[id="com.kuaiduizuoye.scan:id/fl_ad_container"] >n [text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12716275',
+    },
+    {
+      key: 1,
+      name: '首页广告弹窗',
+      activityIds: 'com.hihonor.android.launcher.drawer.DrawerLauncher',
+      rules: '[id="com.kuaiduizuoye.scan:id/iv_advertisement_widget_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12716285',
+    },
+  ],
+});
