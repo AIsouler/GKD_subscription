e7edb7beced7770edb719eaa8ef99d5c9accab24
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-25 22:01:57 +0800

    feat(华图在线): 开屏广告,首页广告弹窗, 通知权限弹窗 (#370)

---
+++ src/apps/com.huatu.handheld_huatu.ts
@@ -0,0 +1,31 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.huatu.handheld_huatu',
+  name: '华图在线',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.huatu.handheld_huatu.business.guide.SplashActivity',
+      rules: '[id="com.huatu.handheld_huatu:id/splash_tv_jump"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715734',
+    },
+    {
+      key: 1,
+      name: '首页广告弹窗',
+      activityIds: 'com.huatu.handheld_huatu.view.MainPopDialog',
+      rules:
+        '[id="com.huatu.handheld_huatu:id/main_img_adv_bg"] + [id="com.huatu.handheld_huatu:id/main_img_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715702',
+    },
+    {
+      enable: false,
+      key: 10,
+      name: '请求通知权限弹窗',
+      activityIds: 'com.huatu.handheld_huatu.business.main.MainTabActivity',
+      rules: '[text*="开启通知提示"] +2 LinearLayout > [text="取消"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715719',
+    },
+  ],
+});
