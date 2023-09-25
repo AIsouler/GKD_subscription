9315d5baefa5417bd69b08df6df0758b9c7bec3a
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-25 22:02:49 +0800

    feat(小米游戏中心): 开屏广告, 首页右侧悬浮广告 (#371)

---
+++ src/apps/com.xiaomi.gamecenter.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xiaomi.gamecenter',
+  name: '小米游戏中心',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.xiaomi.gamecenter.ui.MainTabActivity',
+      rules: '[id="com.xiaomi.gamecenter:id/skip"][text="跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715833',
+    },
+    {
+      key: 1,
+      name: '首页右侧悬浮广告',
+      activityIds: 'com.xiaomi.gamecenter.ui.MainTabActivity',
+      rules:
+        '[id="com.xiaomi.gamecenter:id/ad_view"] + [id="com.xiaomi.gamecenter:id/close_btn"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715800',
+    },
+  ],
+});
