d08e5a0c226476956bc98fc952bc3d75dc63be05
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-26 23:50:18 +0800

    feat(网速管家): 更新弹窗 (#405)

---
+++ src/apps/cn.lezhi.speedtest.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.lezhi.speedtest',
+  name: '网速管家',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'cn.lezhi.speedtest.main5.MainV5Activity',
+      rules:
+        '[id="cn.lezhi.speedtest:id/rl_update_dialog_layout"] >n [id="cn.lezhi.speedtest:id/iv_close"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12727619',
+    },
+  ],
+});
