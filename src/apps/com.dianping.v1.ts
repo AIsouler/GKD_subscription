312dcf429ea090cb0599f5e0ad3bbe5df91a1da9
longm21 <145310263+longm21@users.noreply.github.com>
2023-09-26 23:48:58 +0800

    feat(大众点评): 主页推荐关注弹窗,通知提醒,关闭[签到开红包] (#403)
    * feat(大众点评): 补充事件
    
    * feat(大众点评): 补充一些场景
    
    * feat(大众点评): 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.dianping.v1.ts
+++ src/apps/com.dianping.v1.ts
@@ -7,8 +7,42 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.dianping.v1.NovaMainActivity',
+      activityIds: [
+        'com.dianping.v1.NovaMainActivity',
+        'com.dianping.main.guide.SplashActivity',
+      ],
       rules: '[id=`com.dianping.v1:id/new_skip`]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12726928', // 对应第二条activityIds
+      // 缺第一条activityIds对应的快照
+      // 上面两条activityIds，在截快照时交替出现，故都放在判定条件里
+    },
+    {
+      key: 1,
+      name: '主页推荐关注弹窗',
+      activityIds: ['com.dianping.main.guide.SplashActivity'],
+      rules:
+        'FrameLayout[id="android:id/content"] >2 FrameLayout > ImageView + FrameLayout[id=null][desc="关闭"][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727011',
+    },
+    {
+      key: 2,
+      name: '消息页上方开启系统通知提醒',
+      activityIds: ['com.dianping.main.guide.SplashActivity'],
+      rules: {
+        matches:
+          '[text="去开启"][clickable=true] +2 @FrameLayout[clickable=true] > ImageView',
+        action: 'clickCenter', // clickNode无响应
+      },
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727070',
+    },
+    {
+      key: 3,
+      name: '关闭[签到开红包]',
+      desc: '点击右下角悬浮图片右上角的x图标',
+      activityIds: ['com.dianping.v1.NovaMainActivity'],
+      rules:
+        '[id="com.dianping.v1:id/float_image"] + [id="com.dianping.v1:id/close"][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727366',
     },
   ],
 });
