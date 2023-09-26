bf4f605645d15a7e54a889a4e41d5a5ff728ec23
lisonge <i@songe.li>
2023-09-26 10:45:32 +0800

    feat(Wind金融终端): 开屏广告 (#201)

---
+++ src/apps/windinfo.android.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'windinfo.android',
+  name: 'Wind金融终端',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'windinfo.android.LogoAdActivity',
+      rules: '[id="windinfo.android:id/ivAdPass"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12721428',
+    },
+  ],
+});
