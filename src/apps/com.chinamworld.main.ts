7484689017ce7d238fb018cbf801642ac9d8949a
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-26 23:33:20 +0800

    feat(中国建设银行): 我的页面-广告弹窗 (#393)

---
+++ src/apps/com.chinamworld.main.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.chinamworld.main',
+  name: '中国建设银行',
+  groups: [
+    {
+      key: 1,
+      name: '我的页面-广告弹窗',
+      activityIds: 'com.ccb.start.view.startdialog.StartDialogActivity',
+      rules:
+        '[id="com.chinamworld.main:id/indicator_container_container"] + [id="com.chinamworld.main:id/close"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12726961',
+    },
+  ],
+});
