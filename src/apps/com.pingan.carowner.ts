3c0057fe41706ad56e314fdcb575c0139f9bbe20
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-26 23:51:22 +0800

    feat(平安好车主): 开屏广告 (#407)

---
+++ src/apps/com.pingan.carowner.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.pingan.carowner',
+  name: '平安好车主',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.pingan.carowner.activity.SplashActivity',
+      rules: '[id="com.pingan.carowner:id/splash_time_tv"][text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727845',
+    },
+  ],
+});
