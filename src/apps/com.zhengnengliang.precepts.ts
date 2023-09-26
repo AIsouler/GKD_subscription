06fd9ed88b77b1dae65f4cd22470b5ffe41c8e81
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-26 23:50:59 +0800

    feat(正气): 更新弹窗, SDK 广告 (#406)
    * feat(正气)
    
    * feat(正气)

--- src/apps/com.zhengnengliang.precepts.ts
+++ src/apps/com.zhengnengliang.precepts.ts
@@ -7,9 +7,22 @@ export default defineAppConfig({
     {
       key: 1,
       name: '更新弹窗',
-      activityIds: 'com.zhengnengliang.precepts.ui.dialog.DialogTwoButton',
+      activityIds: [
+        'com.zhengnengliang.precepts.ui.activity.MainActivity',
+        'com.zhengnengliang.precepts.ui.dialog.DialogTwoButton',
+      ],
       rules: '@[text="取消"] +2 [text="升级"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715352',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12727650',
+        'https://gkd-kit.gitee.io/import/12715352',
+      ],
+    },
+    {
+      key: 2,
+      name: '第三方 SDK 广告',
+      activityIds: 'com.zhengnengliang.precepts.ui.dialog.DialogTwoButton',
+      rules: '[text="点此去广告"] + ImageButton[clickable=true]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12727705',
     },
   ],
 });
