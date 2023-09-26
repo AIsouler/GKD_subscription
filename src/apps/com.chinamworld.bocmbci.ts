964a9f0e11a4786e8c9bc8ef9b0d593101a444ad
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-26 23:33:49 +0800

    feat(中国银行): 开屏广告 (#394)

---
+++ src/apps/com.chinamworld.bocmbci.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.chinamworld.bocmbci',
+  name: '中国银行',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds:
+        'com.boc.bocsoft.mobile.bocmobile.buss.system.splash.SplashActivity',
+      rules: '[id="com.chinamworld.bocmbci:id/tv_timer"][text*="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12727106',
+    },
+  ],
+});
