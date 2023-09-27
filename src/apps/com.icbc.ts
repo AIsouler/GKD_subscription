3d3ad7cd8e64e5d076e55a8d9c841c01e4b1014d
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-27 21:53:03 +0800

    feat(中国工商银行): 开屏广告 (#422)

--- src/apps/com.icbc.ts
+++ src/apps/com.icbc.ts
@@ -5,10 +5,17 @@ export default defineAppConfig({
   name: '中国工商银行',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      activityIds: 'com.icbc.activity.init.LoadingAdvActivity',
+      rules: '[id="com.icbc:id/tv_skip_adv"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12739088',
+    },
+    {
       key: 0,
       name: '第一次启动提示',
       activityIds: 'com.icbc.activity.init.SplashActivity',
-      rules: '[id=`com.icbc:id/close_btn`]',
+      rules: '[id="com.icbc:id/close_btn"]',
     },
   ],
 });
