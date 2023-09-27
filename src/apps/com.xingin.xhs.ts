56d7942b099874e16cf902efece37d75e9fbb617
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-27 21:52:43 +0800

    feat(小红书): 开屏广告 (#421)

---
+++ src/apps/com.xingin.xhs.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xingin.xhs',
+  name: '小红书',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.xingin.xhs.index.v2.IndexActivityV2',
+      rules: '[text="广告"] - @FrameLayout > [text="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12739065',
+    },
+  ],
+});
