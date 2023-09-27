c83f7baf3bb46ecc798d818dacc51ebd226fa8f9
lisonge <i@songe.li>
2023-09-27 20:20:34 +0800

    feat(百度网盘-三星版本): 各类广告 (#390)

---
+++ src/apps/com.baidu.netdisk.samsung.ts
@@ -0,0 +1,49 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.baidu.netdisk.samsung',
+  name: '百度网盘-三星版本',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.baidu.netdisk.ui.Navigate',
+      rules: '[id="com.baidu.netdisk.samsung:id/countdown"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12738323',
+    },
+    {
+      key: 1,
+      name: '弹窗广告',
+      desc: '点击右上角x图标按钮',
+      activityIds: 'com.baidu.netdisk.ui.MainActivity',
+      rules:
+        '[id="com.baidu.netdisk.samsung:id/cl_root"] > [id="com.baidu.netdisk.samsung:id/iv_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12738331',
+    },
+    {
+      key: 2,
+      name: '关闭购买套餐卡片广告',
+      activityIds: 'com.baidu.netdisk.ui.MainActivity',
+      rules:
+        '[id="com.baidu.netdisk.samsung:id/buy_product_root"] > [id="com.baidu.netdisk.samsung:id/iv_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12738388',
+    },
+    {
+      key: 3,
+      name: '关闭[专属福利]卡片广告',
+      activityIds: 'com.baidu.netdisk.ui.MainActivity',
+      rules:
+        'View[childCount=2] > @TextView[text=""][clickable=true] + TextView[text="专属福利"][clickable=false]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12738404',
+    },
+    {
+      key: 4,
+      name: '关闭[今日福利]卡片广告',
+      desc: '会员-我的会员-底部卡片广告,点击[去使用]右侧的x关闭图标',
+      activityIds: 'com.baidu.netdisk.ui.MainActivity',
+      rules:
+        '@TextView[text=""][clickable=true] - TextView[text="去使用"][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12738449',
+    },
+  ],
+});
