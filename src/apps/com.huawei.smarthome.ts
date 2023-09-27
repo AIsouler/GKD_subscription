d931c5ace01c2d78abd63aca6f14946bdeb89646
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-27 20:00:53 +0800

    feat(智慧生活): 更新弹窗 (#419)

---
+++ src/apps/com.huawei.smarthome.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.huawei.smarthome',
+  name: '智慧生活',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.huawei.android.launcher.unihome.UniHomeLauncher',
+      rules: '@[text="以后再说"] +2 [text="立即更新"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12738253',
+    },
+  ],
+});
