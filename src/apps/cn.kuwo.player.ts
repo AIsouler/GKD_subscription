6e4b1d31744b17b1d2f17e2fe241647072b369bc
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-28 10:34:26 +0800

    feat(酷我音乐): 切屏广告 (#448)
    * feat(酷我音乐)
    
    * feat(酷我音乐)

--- src/apps/cn.kuwo.player.ts
+++ src/apps/cn.kuwo.player.ts
@@ -6,10 +6,23 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '开屏广告',
-      activityIds: 'cn.kuwo.player.activities.EntryActivity',
-      rules: '[id="cn.kuwo.player:id/skip_button"][text="跳过"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12727887',
+      name: '开屏、切屏广告',
+      rules: [
+        {
+          key: 0,
+          name: '开屏广告',
+          activityIds: 'cn.kuwo.player.activities.EntryActivity',
+          matches: '[id="cn.kuwo.player:id/skip_button"][text="跳过"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12727887',
+        },
+        {
+          key: 1,
+          name: '切屏广告',
+          activityIds: 'cn.kuwo.player.activities.EntryActivity',
+          matches: '[id="cn.kuwo.player:id/btnSkip"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12740634',
+        },
+      ],
     },
   ],
 });
