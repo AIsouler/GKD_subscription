d640ff69256caa76bef225015a978ece9b64da54
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-28 10:20:34 +0800

    feat(倒计日): SDK 广告弹窗  (#430)
    * feat(倒计日)
    
    * Update com.zhizhou.days.ts

--- src/apps/com.zhizhou.days.ts
+++ src/apps/com.zhizhou.days.ts
@@ -9,13 +9,23 @@ export default defineAppConfig({
       name: '第三方 SDK 广告弹窗',
       rules: [
         {
-          name: '穿山甲',
+          key: 0,
+          name: '穿山甲-第一类广告',
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
           matches: '[desc^="webview-close"] > View[clickable=true]',
           snapshotUrls: 'https://gkd-kit.songe.li/import/12727499',
         },
         {
+          key: 1,
+          name: '穿山甲-第二类广告',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: ['[text="广告"]', '@Image < View + [text="反馈"]'],
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12739678',
+        },
+        {
+          key: 10,
           name: '优量汇-第一类广告',
           activityIds: 'com.zhizhou.days.activity.MainActivity',
           matches:
@@ -23,11 +33,18 @@ export default defineAppConfig({
           snapshotUrls: 'https://gkd-kit.songe.li/import/12727528',
         },
         {
+          key: 11,
           name: '优量汇-第二类广告',
-          activityIds: 'com.huawei.android.launcher.unihome.UniHomeLauncher',
+          activityIds: [
+            'com.zhizhou.days.activity.MainActivity',
+            'com.huawei.android.launcher.unihome.UniHomeLauncher',
+          ],
           matches:
             'ImageView - FrameLayout > FrameLayout > FrameLayout > ImageView[visibleToUser=true]',
-          snapshotUrls: 'https://gkd-kit.songe.li/import/12727546',
+          snapshotUrls: [
+            'https://gkd-kit.songe.li/import/12739703',
+            'https://gkd-kit.songe.li/import/12727546', // activityId: 'com.huawei.android.launcher.unihome.UniHomeLauncher'
+          ],
         },
       ],
     },
