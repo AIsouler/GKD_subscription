3b7777f6b2cc4e7f101ba1e90906b78763fc1dfb
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-28 20:09:23 +0800

    chore(讯飞AI学): 额外说明 (#481)
    * feat(讯飞AI学)
    
    * 根据 #429 更新注释

--- src/apps/com.iflytek.aistudyclient.parentcontrol.ts
+++ src/apps/com.iflytek.aistudyclient.parentcontrol.ts
@@ -8,7 +8,7 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.iflytek.aistudyclient.parentcontrol.MainActivity',
-      rules: 'View > @[desc^="跳过"] + ImageView', // 选中的目标是快照中的一片区域，不确定能否达到预期目的
+      rules: 'View > @[desc^="跳过"] + ImageView', // 点击目标是快照中的一片区域，https://github.com/gkd-kit/subscription/issues/429#issuecomment-1738990131 反馈可以成功跳过
       snapshotUrls: 'https://gkd-kit.songe.li/import/12740402',
     },
   ],
