3b895cf10be764af370fe83257d83f0fdd15bfde
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-28 10:29:04 +0800

    feat(巧虎官方): 开屏广告 (#444)

---
+++ src/apps/com.qh.tesla.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.qh.tesla',
+  name: '巧虎官方',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.qh.tesla.splash.SplashActivity',
+      rules: '[id="com.qh.tesla:id/ad_timer"][text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12740377',
+    },
+  ],
+});
