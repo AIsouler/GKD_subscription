34b9ae4728461afba5d26a1d4ec97493a60c2b71
lisonge <i@songe.li>
2023-09-28 20:18:53 +0800

    feat(苏e行): 开屏广告 (#476)

---
+++ src/apps/com.bwton.szfreego.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.bwton.szfreego',
+  name: '苏e行',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.bwton.szfreego.business.LaunchActivity',
+      rules: '[id="com.bwton.szfreego:id/tvSkip"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12749225',
+    },
+  ],
+});
