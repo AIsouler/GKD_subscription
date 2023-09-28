736a08b3a40a87c1ae451b68546e9652702685ab
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-28 10:27:45 +0800

    feat(上海农商银行): 开屏广告 (#441)

---
+++ src/apps/com.srcb.pmbank.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.srcb.pmbank',
+  name: '上海农商银行',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.yitong.mobile.biz.launcher.app.SplashActivity',
+      rules: '[id="com.srcb.pmbank:id/chronometer"][text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12740293',
+    },
+  ],
+});
