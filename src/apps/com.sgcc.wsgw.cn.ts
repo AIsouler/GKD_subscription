d9e34196c22d43dd1342d51242ee419736c44bc8
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-28 19:54:31 +0800

    feat(网上国网): 首页弹窗广告 (#454)

---
+++ src/apps/com.sgcc.wsgw.cn.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.sgcc.wsgw.cn',
+  name: '网上国网',
+  groups: [
+    {
+      key: 1,
+      name: '首页弹窗广告',
+      activityIds: 'com.sgcc.wsgw.mainbundle.NewsSyRemind2Activity',
+      rules:
+        '[id="com.sgcc.wsgw.cn:id/news_remind_laytout"] > RelativeLayout > [id="com.sgcc.wsgw.cn:id/btn_remind_close"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12745042',
+    },
+  ],
+});
