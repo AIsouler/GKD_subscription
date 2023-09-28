203c1f05e16bbc7f543f1552f8a2f99b3ea8f3f3
lisonge <i@songe.li>
2023-09-28 19:52:35 +0800

    feat(Cellular-Z): 开屏广告-适配其它 (#475)

--- src/apps/make.more.r2d2.cellular_z.ts
+++ src/apps/make.more.r2d2.cellular_z.ts
@@ -13,6 +13,10 @@ export default defineAppConfig({
           matches: '[text^="跳过"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12648511',
         },
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12747314',
+        },
       ],
     },
   ],
