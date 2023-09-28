7c52560b92b892f0aa751360da77362db668419f
lisonge <i@songe.li>
2023-09-28 21:01:34 +0800

    feat(人民日报): 开屏广告 (#484)

---
+++ src/apps/com.peopledailychina.activity.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.peopledailychina.activity',
+  name: '人民日报',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.peopledailychina.activity.activity.WelcomeActivity',
+      rules: '[id="com.peopledailychina.activity:id/act_welcome_skip"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12749658',
+    },
+  ],
+});
