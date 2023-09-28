b1de5ad65d97c029ca09b96bd005ee2819a7a788
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-28 20:04:27 +0800

    feat(交通银行): 首页弹窗广告 (#466)

--- src/apps/com.bankcomm.Bankcomm.ts
+++ src/apps/com.bankcomm.Bankcomm.ts
@@ -6,13 +6,19 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      activityIds: 'com.bankcomm.module.biz.home.MainActivity',
-      name: '弹窗广告',
+      activityIds: [
+        'com.bankcomm.module.biz.home.MainActivity',
+        'com.hihonor.android.launcher.unihome.UniHomeLauncher',
+      ],
+      name: '首页弹窗广告',
       desc: '点击右上角x',
       rules:
         '[id="com.bankcomm.Bankcomm:id/popup_ad_image"] - [id="com.bankcomm.Bankcomm:id/popup_close"]',
       // 貌似快照里位置和截图不对应
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12671987',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12671987',
+        'https://gkd-kit.songe.li/import/12745293', // activityId: 'com.hihonor.android.launcher.unihome.UniHomeLauncher'
+      ],
     },
   ],
 });
