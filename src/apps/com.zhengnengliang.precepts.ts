365de83ae64a8a08390d1d0c214adff1fb7dd775
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-28 10:21:20 +0800

    feat(正气): SDK 广告 (#431)
    * feat(正气)
    
    * feat(正气)
    
    * Update com.zhengnengliang.precepts.ts

--- src/apps/com.zhengnengliang.precepts.ts
+++ src/apps/com.zhengnengliang.precepts.ts
@@ -20,9 +20,15 @@ export default defineAppConfig({
     {
       key: 2,
       name: '第三方 SDK 广告',
-      activityIds: 'com.zhengnengliang.precepts.ui.dialog.DialogTwoButton',
+      activityIds: [
+        'com.zhengnengliang.precepts.ui.activity.MainActivity',
+        'com.zhengnengliang.precepts.ui.dialog.DialogTwoButton',
+      ],
       rules: '[text="点此去广告"] + ImageButton[clickable=true]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12727705',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12739767',
+        'https://gkd-kit.songe.li/import/12727705', // activityId: 'com.zhengnengliang.precepts.ui.dialog.DialogTwoButton'
+      ],
     },
   ],
 });
