0da5877f7455de395b4c2bd88cb1b3235b0964ac
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-29 23:52:24 +0800

    feat(百度百科): 开屏广告,通知权限弹窗 (#498)

---
+++ src/apps/com.baidu.baike.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.baidu.baike',
+  name: '百度百科',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.baidu.baike.activity.SplashActivity',
+      rules: '[id="com.baidu.baike:id/btn_skip"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12755714',
+    },
+    {
+      key: 10,
+      name: '请求通知权限弹窗',
+      desc: '自动点击【不允许】',
+      activityIds: 'com.baidu.baike.component.push.NotificationEnableDialog',
+      rules: '[text^="打开推送通知"] +2 [text="不允许"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12755717',
+    },
+  ],
+});
