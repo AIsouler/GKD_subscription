052f791edd7ef36efc9ebf83f59bcd98c8b1266e
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-29 23:54:30 +0800

    feat(腾讯微云): 开屏广告 (#500)

---
+++ src/apps/com.qq.qcloud.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.qq.qcloud',
+  name: '腾讯微云',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.qq.qcloud.activity.WeiyunRootActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '[id="com.qq.qcloud:id/gdt_ad_text"] + ImageView',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12755789',
+        },
+        {
+          key: 1,
+          matches:
+            '[text^="互动广告"] + @LinearLayout[clickable=true] > ImageView',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12755799',
+        },
+      ],
+    },
+  ],
+});
