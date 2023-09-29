87ab48cb057917a5d0397682407ed352da9450f7
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-29 23:42:14 +0800

    feat(微博): 首页顶部签到卡片,首页广告弹窗,悬浮弹窗 (#487)
    * feat(微博)
    
    * 修改 key
    
    * 新增 【首页顶部签到卡片】
    
    * 新增 【首页广告弹窗】
    
    * 新增 【右下角刷微博领现金悬浮广告】
    
    * 修改 key12->key13

--- src/apps/com.sina.weibo.ts
+++ src/apps/com.sina.weibo.ts
@@ -125,5 +125,33 @@ export default defineAppConfig({
         '[text^="打开通知"] < LinearLayout + ImageView[id="com.sina.weibo:id/right_icon"]',
       snapshotUrls: ['https://gkd-kit.gitee.io/import/12705986'],
     },
+    {
+      enable: false,
+      key: 11,
+      name: '首页顶部签到卡片',
+      desc: '自动点击x按钮',
+      activityIds: ['com.sina.weibo.MainTabActivity'],
+      rules:
+        '[id="com.sina.weibo:id/ll_check_in_container"] < FrameLayout - FrameLayout > TextView + [id="com.sina.weibo:id/v_close"]',
+      snapshotUrls: ['https://gkd-kit.songe.li/import/12749876'],
+    },
+    {
+      key: 12,
+      name: '首页广告弹窗',
+      activityIds: ['com.sina.weibo.business'],
+      rules:
+        '@[id="com.sina.weibo:id/btn_close"] - FrameLayout > [id="com.sina.weibo:id/ad_tag"]',
+      snapshotUrls: ['https://gkd-kit.songe.li/import/12750090'],
+    },
+    {
+      enable: false,
+      key: 13,
+      name: '右下角刷微博领现金悬浮广告',
+      desc: '自动点击x按钮',
+      activityIds: ['com.sina.weibo.MainTabActivity'],
+      rules:
+        '[id="com.sina.weibo:id/floating_window"] >2 [id="com.sina.weibo:id/close"]',
+      snapshotUrls: ['https://gkd-kit.songe.li/import/12750118'],
+    },
   ],
 });
