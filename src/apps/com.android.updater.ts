c7e7ad0f1a4c58a649813d7b413531dbe9748bf0
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-09-29 23:43:28 +0800

    feat(系统更新): 适配其它界面 (#488)

--- src/apps/com.android.updater.ts
+++ src/apps/com.android.updater.ts
@@ -8,10 +8,16 @@ export default defineAppConfig({
       enable: false,
       key: 0,
       name: 'MIUI更新弹窗',
-      activityIds: 'com.android.updater.MainActivity',
+      activityIds: [
+        'com.android.updater.MainActivity',
+        'com.android.settings.SubSettings',
+      ],
       rules:
         '[id="com.android.updater:id/alertTitle"][text="开启智能更新"] < * +2 * > Button[id="android:id/button2"][text="暂不开启"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12715712',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12715712',
+        'https://gkd-kit.songe.li/import/12749906',
+      ],
     },
   ],
 });
