68c16bb1630ee0edc074ae901a0dbcd7b7ea4aa2
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-01 02:31:59 +0800

    feat(通达信): 开屏广告 (#527)

---
+++ src/apps/com.tdx.AndroidNew.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tdx.AndroidNew',
+  name: '通达信',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.tdx.Android.TdxAndroidActivity',
+      rules: '[id=null][text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12774846',
+    },
+  ],
+});
