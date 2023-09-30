5d3a286b3efcf3ba1cf0a790bbacca6aa7c4a3d6
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-01 02:33:28 +0800

    feat(涨乐全球通): 开屏广告 (#531)
    * 新创建 App 规则
    
    * 修改 App 名称

---
+++ src/apps/com.lphtsccft.zlqqt2.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.lphtsccft.zlqqt2',
+  name: '涨乐全球通',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.lphtsccft.zhangle.startup.SplashScreenActivity',
+      rules: '[id="com.lphtsccft.zlqqt2:id/countdown_skip"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12774860',
+    },
+  ],
+});
