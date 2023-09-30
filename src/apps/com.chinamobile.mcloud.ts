b5f84c0cd7832c0965b52d66cde2c9cba0b31ab6
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-01 02:30:34 +0800

    feat(中国移动云盘): 开屏广告,更新弹窗 (#524)

---
+++ src/apps/com.chinamobile.mcloud.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.chinamobile.mcloud',
+  name: '中国移动云盘',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.chinamobile.mcloud.client.ui.logo.LogoActivity',
+      rules: '[id="com.chinamobile.mcloud:id/btn_skip"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12774830',
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.chinamobile.mcloud.client.ui.setting.UpgradeActivity',
+      rules:
+        '@[id="com.chinamobile.mcloud:id/bn_cancel"] + [id="com.chinamobile.mcloud:id/upgrade_title"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12774833',
+    },
+  ],
+});
