ad1d3d208aaa6dbebeecd2e756667270e0359cff
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-01 02:31:02 +0800

    feat(央视频): 开屏广告 (#525)

---
+++ src/apps/com.cctv.yangshipin.app.androidp.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.cctv.yangshipin.app.androidp',
+  name: '央视频',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.tencent.videolite.android.ui.HomeActivity',
+      rules:
+        '[id="com.cctv.yangshipin.app.androidp:id/ad_container"] >n [text$="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12774838',
+    },
+  ],
+});
