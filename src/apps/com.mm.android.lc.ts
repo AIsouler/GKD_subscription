038151c56d2aff859dc80f02c6021b6f96ae2be2
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-01 02:29:10 +0800

    feat(乐橙): 开屏广告 (#522)

---
+++ src/apps/com.mm.android.lc.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.mm.android.lc',
+  name: '乐橙',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.mm.android.lc.login.LCAdvertActivity',
+      rules: '[id="com.mm.android.lc:id/advert_skip_view"][text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12774801',
+    },
+  ],
+});
