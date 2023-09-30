b852d54149f9b0ed7d676afd7f214d54e9e8a04f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-01 02:34:46 +0800

    fix(京东): 我的界面-参与调研-规则优化 (#533)
    * fix(京东)--我的界面-参与调研
    
    * fix 使用 [desc!="返回"] 进行限定，防止在进入商品详情页时点击返回按钮

--- src/apps/com.jingdong.app.mall.ts
+++ src/apps/com.jingdong.app.mall.ts
@@ -29,8 +29,11 @@ export default defineAppConfig({
       name: '我的界面-参与调研',
       activityIds: 'com.jingdong.app.mall.MainFrameActivity',
       rules:
-        'FrameLayout + RelativeLayout > RelativeLayout > ImageView + ImageView[visibleToUser=true]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12642270',
+        'FrameLayout + RelativeLayout > RelativeLayout > ImageView + ImageView[desc!="返回"][visibleToUser=true]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12642270',
+        'https://gkd-kit.songe.li/import/12774910', // 使用 [desc!="返回"] 进行限定，防止在进入商品详情页时点击返回按钮
+      ],
     },
   ],
 });
