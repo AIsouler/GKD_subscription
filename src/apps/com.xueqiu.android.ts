0e35db7f9bb728f44824d284d028648d369fbf52
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-02 22:52:55 +0800

    feat(雪球): 开屏广告 (#548)

---
+++ src/apps/com.xueqiu.android.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xueqiu.android',
+  name: '雪球',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.xueqiu.android.community.FakeAdActivity',
+      rules: '[id="com.xueqiu.android:id/tv_skip"][text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12776009',
+    },
+  ],
+});
