34bb6eb9029c8de29ef6a26b5ae0721571972642
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-02 22:52:19 +0800

    feat(虎嗅): 开屏广告 (#547)

---
+++ src/apps/com.huxiu.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.huxiu',
+  name: '虎嗅',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds:
+        'com.huxiu.ui.activity.HXRegistrationAgreementPrivacyPolicyActivity',
+      rules: '[id="com.huxiu:id/tv_skip_ad"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12776004',
+    },
+  ],
+});
