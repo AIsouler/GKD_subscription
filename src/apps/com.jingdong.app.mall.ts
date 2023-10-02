6a1973ae0689450d8d417f6d580e3163b4b0b07d
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-02 22:55:35 +0800

    feat(京东): 开屏广告-规则优化,浮层广告 (#558)
    * fix(京东)--我的界面-参与调研
    
    * fix 使用 [desc!="返回"] 进行限定，防止在进入商品详情页时点击返回按钮
    
    * fix 优化【开屏广告】，切换点击目标
    
    * feat 新增【浮层广告】

--- src/apps/com.jingdong.app.mall.ts
+++ src/apps/com.jingdong.app.mall.ts
@@ -9,7 +9,7 @@ export default defineAppConfig({
       name: '开屏广告',
       activityIds: 'com.jingdong.app.mall.MainFrameActivity',
       rules:
-        'RelativeLayout[desc="跳过"][id!=null] > TextView[text="跳过"][id!=null]',
+        '@RelativeLayout[desc="跳过"][id!=null] > TextView[text="跳过"][id!=null]',
       snapshotUrls: [
         'https://gkd-kit.gitee.io/import/12668289',
         'https://gkd-kit.gitee.io/import/12519430',
@@ -33,6 +33,19 @@ export default defineAppConfig({
       snapshotUrls: [
         'https://gkd-kit.gitee.io/import/12642270',
         'https://gkd-kit.songe.li/import/12774910', // 使用 [desc!="返回"] 进行限定，防止在进入商品详情页时点击返回按钮
+      ],
+    },
+    {
+      enable: false,
+      key: 3,
+      name: '浮层广告',
+      activityIds: 'com.jingdong.app.mall.MainFrameActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '@[desc^="关闭浮层"] + [desc="浮层活动"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12740073',
+        },
       ],
     },
   ],
