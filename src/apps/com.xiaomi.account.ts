aee52709cb761c359c788e5b989dd0407f76b45b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-02 23:06:05 +0800

    feat(小米账号): 更新弹窗 (#573)

---
+++ src/apps/com.xiaomi.account.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xiaomi.account',
+  name: '小米账号',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.xiaomi.account.upgrade.AccountApkUpdateDialogActivity',
+      rules: '@[text="以后再说"] + [text="立即升级"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12781280',
+    },
+  ],
+});
