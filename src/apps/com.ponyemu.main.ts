4099f843406ae0a4cb9f4ccff767d127cab87978
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-02 23:32:41 +0800

    feat(小马模拟器): 开屏广告 (#585)

---
+++ src/apps/com.ponyemu.main.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.ponyemu.main',
+  name: '小马模拟器',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.ponyemu.main.activitys.AdLaunchActivity',
+      rules: '[id="com.ponyemu.main:id/time"] +2 [text="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12783200',
+    },
+  ],
+});
