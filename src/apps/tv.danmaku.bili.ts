fe2bd67ea9d2613448a3ac550aed2b49be4dc0cf
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-02 22:54:04 +0800

    fix(哔哩哔哩): 视频底部广告-优化规则 (#552)
    * feat(哔哩哔哩)
    
    * feat(哔哩哔哩)
    
    * 删除开屏广告的 activityId
    
    * revert 点击屏蔽广告原因-回退上一版本的规则
    
    * feat(哔哩哔哩): 视频底部广告-优化
    
    * fix 【视频底部广告】优化规则
    
    * chore 【视频底部广告】-【点击屏蔽广告】补充 key 字段
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/tv.danmaku.bili.ts
+++ src/apps/tv.danmaku.bili.ts
@@ -63,21 +63,25 @@ export default defineAppConfig({
       activityIds: [
         'com.bilibili.video.videodetail.VideoDetailsActivity',
         'com.bilibili.ship.theseus.all.UnitedBizDetailsActivity',
+        'com.bilibili.ship.theseus.detail.UnitedBizDetailsActivity',
+        'tv.danmaku.bili.MainActivityV2',
       ],
       rules: [
         {
           key: 0,
           name: '点击广告卡片右侧菜单图标',
           matches:
-            'FrameLayout[id="tv.danmaku.bili:id/ad_tint_frame"] >n [id="tv.danmaku.bili:id/more"]',
+            'FrameLayout[id="tv.danmaku.bili:id/ad_tint_frame"] >n [id^="tv.danmaku.bili:id/more"]',
           snapshotUrls: [
             'https://gkd-kit.gitee.io/import/12642260', // n = 2
             'https://gkd-kit.gitee.io/import/12705266', // n = 3
+            'https://gkd-kit.songe.li/import/12776568', // id="tv.danmaku.bili:id/more_layout"
             'https://gkd-kit.gitee.io/import/12707070', // 由于 activityId 切换延迟导致规则仍然运行, 使用 FrameLayout 避免误触
           ],
         },
         {
           preKeys: 0,
+          key: 1,
           name: '点击屏蔽广告',
           matches:
             '[id="tv.danmaku.bili:id/menu_text"][text="屏蔽广告"] < [id="tv.danmaku.bili:id/title"] + [id="tv.danmaku.bili:id/dislike_reasons"] >2 [id="tv.danmaku.bili:id/reason1_layout"]',
