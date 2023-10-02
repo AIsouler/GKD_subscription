74c1289f72073b8eb04900e1b9389eb5fca59dcd
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-02 23:31:56 +0800

    feat(3Dbody解剖): 开屏广告 (#584)

---
+++ src/apps/com.HaoTuBang.H3DBodyFree.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.HaoTuBang.H3DBodyFree',
+  name: '3Dbody解剖',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds:
+        'com.HaoTuBang.H3DBodyFree.modules.login.activity.SpalashActivity',
+      rules: '[id="com.HaoTuBang.H3DBodyFree:id/adsParent"] >n [text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12783176',
+    },
+  ],
+});
