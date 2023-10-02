dea24f6efbacf2b278d5756aff4484c6996fa3c3
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-02 22:45:28 +0800

    feat(快递100): 开屏广告 (#545)

---
+++ src/apps/com.Kingdee.Express.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.Kingdee.Express',
+  name: '快递100',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.Kingdee.Express.module.splash.SplashActivity',
+      rules:
+        '[id="com.Kingdee.Express:id/rl_splash_container"] >n [text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12775998',
+    },
+  ],
+});
