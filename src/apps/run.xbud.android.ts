54f58dcf4ba0a447060d2fc929d730c452488348
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-02 22:58:27 +0800

    feat(小步点): 开屏广告,首页广告弹窗 (#563)

---
+++ src/apps/run.xbud.android.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'run.xbud.android',
+  name: '小步点',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'run.xbud.android.mvp.ui.other.SplashActivity',
+      rules: '[id="run.xbud.android:id/bodyLayout"] >n [text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12777125',
+    },
+    {
+      key: 1,
+      name: '首页广告弹窗',
+      activityIds: 'run.xbud.android.mvp.ui.other.MainActivity',
+      rules:
+        '[id="run.xbud.android:id/ksad_tk_view"] >n @ViewGroup[clickable=true] > ImageView',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12777133',
+        'https://gkd-kit.songe.li/import/12777134',
+      ],
+    },
+  ],
+});
