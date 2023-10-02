14523b67ccf951fb738647677bb18eb8d77d5985
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-02 22:32:34 +0800

    feat(QQ邮箱): 开屏广告,广告邮件-列表卡片广告 (#542)
    * 新增 【开屏广告】
    
    * 优化【广告邮件-列表卡片广告】

--- src/apps/com.tencent.androidqqmail.ts
+++ src/apps/com.tencent.androidqqmail.ts
@@ -5,12 +5,34 @@ export default defineAppConfig({
   name: 'qq邮箱',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      activityIds: 'com.tencent.qqmail.launcher.desktop.LauncherActivity',
+      rules: 'RelativeLayout > ImageView[id!=null] + ImageView[id!=null]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12775855',
+    },
+    {
       key: 0,
       name: '广告邮件-列表卡片广告',
       activityIds: 'com.tencent.qqmail.fragment.base.MailFragmentActivity',
       rules: [
-        'TextView[text=`赞助商提供的广告`] <n FrameLayout <n ListView[id=`com.tencent.androidqqmail:id/pop_up_list`] TextView[text=`不感兴趣`]',
-        '[id=`com.tencent.androidqqmail:id/advertise_view_ad`]',
+        {
+          key: 0,
+          matches: '[id="com.tencent.androidqqmail:id/advertise_view_ad"]', // 需要补充快照
+        },
+        {
+          key: 1,
+          matches:
+            'LinearLayout > TextView + @LinearLayout[id!=null][clickable=true] > [text="广告"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12775857',
+        },
+        {
+          preKeys: [1],
+          key: 9,
+          matches:
+            '[text="赞助商提供的广告"] < FrameLayout + @FrameLayout > [text="不感兴趣"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12775862',
+        },
       ],
     },
   ],
