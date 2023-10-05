dde1c021b71f86ac8b8a5d4eede0fbc0f88498fb
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-05 14:20:21 +0800

    feat(爱壹帆): 开屏广告 (#606)

---
+++ src/apps/com.cqcsy.ifvod.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.cqcsy.ifvod',
+  name: '爱壹帆',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.cqcsy.lgsp.main.SplashActivity',
+      rules: '[id="com.cqcsy.ifvod:id/skipTips"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12793228',
+    },
+  ],
+});
