39f5fd19003937389e636cc246100e71c9d25cde
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-05 15:20:02 +0800

    feat(QQ阅读): 开屏广告 (#643)

---
+++ src/apps/com.qq.reader.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.qq.reader',
+  name: 'QQ阅读',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.qq.reader.activity.SplashActivity',
+      rules: '[id="com.qq.reader:id/splash_container"] + [text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12811930',
+    },
+  ],
+});
