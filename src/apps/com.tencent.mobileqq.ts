70a19a927185f429e74a694c5e26a5b18ef03545
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-05 14:22:16 +0800

    feat(QQ): 消息页面顶部-SVIP 广告  - 优化规则 (#608)
    * feat(QQ)
    
    * feat(QQ)
    
    * feat(QQ): 频道页面广告-规则优化
    
    * 【消息页面-顶部广告】补充快照
    
    * improvement 【消息页面顶部-SVIP 广告】优化规则
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -64,12 +64,15 @@ export default defineAppConfig({
     {
       enable: false,
       key: 4,
-      name: '新人专享1元购SVIP',
+      name: '消息页面顶部-SVIP 广告',
       desc: '消息界面-搜索框和消息记录直接的广告卡片,点击关闭右侧x',
       activityIds: 'com.tencent.mobileqq.activity.SplashActivity',
       rules:
-        '@ImageView[id!=null][clickable=true] -2 TextView[id!=null][text^="新人专享1元购SVIP"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12706907',
+        'LinearLayout > TextView[text*="SVIP"] + FrameLayout + ImageView[id!=null]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12706907',
+        'https://gkd-kit.songe.li/import/12793359',
+      ],
     },
     {
       enable: false,
