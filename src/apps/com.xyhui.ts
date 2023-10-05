9207540b369a9e9088c4e7ddced789053fe8b135
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-05 14:19:20 +0800

    feat(PU口袋校园): 优量汇广告-规则优化 (#604)
    * feat(PU口袋校园)
    
    * feat 【优量汇广告】完善规则

--- src/apps/com.xyhui.ts
+++ src/apps/com.xyhui.ts
@@ -23,20 +23,23 @@ export default defineAppConfig({
         {
           key: 1,
           matches:
-            '[text="查看详情"||text="立即打开"||text="立即抢购"] - View < FrameLayout - FrameLayout >(2) ImageView[id=null]',
+            '[text="查看详情"||text="立即打开"||text="立即抢购"||text="立即安装"||text="立即下载"] - View < FrameLayout - FrameLayout >(2) ImageView[id=null]',
           snapshotUrls: [
             'https://gkd-kit.gitee.io/import/12642482', // 查看详情
             'https://gkd-kit.gitee.io/import/12646347', // 立即打开
             'https://gkd-kit.gitee.io/import/12646519', // 立即抢购
+            'https://gkd-kit.songe.li/import/12793157', // 立即安装
+            'https://gkd-kit.songe.li/import/12793177', // 立即下载
             'https://gkd-kit.gitee.io/import/12646541', // 华为手机的快照，activityId = 'com.huawei.permissioncontroller.hwcust.appjump.AppJumpActivity'
           ],
         },
         {
           key: 2,
           matches:
-            '[text="查看详情"||text="立即打开"||text="立即抢购"] - View < FrameLayout -(4) FrameLayout > ImageView[id=null]',
+            '[text="查看详情"||text="立即打开"||text="立即抢购"||text="立即下载"] - View < FrameLayout -(4) FrameLayout > ImageView[id=null]',
           snapshotUrls: [
-            'https://gkd-kit.gitee.io/import/12643276',
+            'https://gkd-kit.gitee.io/import/12643276', // 查看详情
+            'https://gkd-kit.songe.li/import/12793180', // 立即下载
             'https://gkd-kit.gitee.io/import/12646420', // 华为手机的快照，activityId = 'com.huawei.android.launcher.unihome.UniHomeLauncher'
           ],
         },
