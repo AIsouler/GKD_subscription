64f19d6c86659ff9c3d58b77ce169b44518dfcc8
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-05 15:12:24 +0800

    feat(Android 系统文件选择器): 授权第三方应用访问文件夹 (#625)

---
+++ src/apps/com.google.android.documentsui.ts
@@ -0,0 +1,28 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.google.android.documentsui',
+  name: 'Android 系统文件选择器',
+  groups: [
+    {
+      enable: false,
+      key: 10,
+      name: '授权第三方应用访问文件夹',
+      desc: '自动点击【使用此文件夹】->【允许】',
+      activityIds: 'com.android.documentsui.picker.PickActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '[text="使用此文件夹"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12799610',
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          matches: '[text="取消"] + [text="允许"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12799603',
+        },
+      ],
+    },
+  ],
+});
