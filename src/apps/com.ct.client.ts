cf119491a3cfdd77a647b8f20e869d9c0e26908d
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-06 17:08:02 +0800

    feat(中国电信): 开屏广告,首页-取消升级,首页-弹窗广告 (#667)
    * feat(QQ): 扫码登录确认 (gkd-kit#593)
    
    * fix(QQ): 优化规则
    使用Button[text="拒绝"] - Button[text="登录"]
    防止不同版本的 QQ 控件 id 不一致
    
    * feat(支付宝): 开启定位提示 (gkd-kit#269)
    
    * Revert "feat(支付宝): 开启定位提示 (gkd-kit#269)"
    
    This reverts commit 6426c5d7280e4b463d43d68af6681b93034ccdfe.
    
    * fix(中国电信): 【开屏广告】完善规则 (gkd-kit#639)
    
    * feat(中国电信): 首页-取消升级、首页-弹窗广告(gkd-kit#666)

--- src/apps/com.ct.client.ts
+++ src/apps/com.ct.client.ts
@@ -9,10 +9,14 @@ export default defineAppConfig({
       name: '开屏广告',
       activityIds: [
         'com.ct.client.activity.MainActivity',
+        'com.miui.home.launcher.Launcher',
         'com.ct.client.activity.SplashActivity',
       ],
       rules: '[id="com.ct.client:id/tvSkip"||id="com.ct.client:id/btSkip"]',
-      snapshotUrls: ['https://gkd-kit.songe.li/import/12508958'],
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12508958',
+        'https://gkd-kit.songe.li/import/12819736',
+      ],
     },
     {
       key: 1,
@@ -21,6 +25,23 @@ export default defineAppConfig({
       activityIds: 'com.ct.client.activity.UserGuideActivity',
       rules: '[id="com.ct.client:id/tvSkip"]',
       snapshotUrls: ['https://gkd-kit.songe.li/import/12508971'],
+    },
+    {
+      key: 2,
+      name: '首页-取消升级',
+      activityIds: [
+        'com.ct.client.activity.MainActivity',
+        'com.ct.client.common.ConfirmDialogActivity',
+      ],
+      rules: 'LinearLayout > TextView[text="取消升级"]',
+      snapshotUrls: ['https://gkd-kit.songe.li/import/12819594'],
+    },
+    {
+      key: 3,
+      name: '首页-弹窗广告',
+      activityIds: ['com.ct.client.common.ConfirmDialogActivity'],
+      rules: 'RelativeLayout > ImageView[desc="关闭"]',
+      snapshotUrls: ['https://gkd-kit.songe.li/import/12819676'],
     },
   ],
 });
