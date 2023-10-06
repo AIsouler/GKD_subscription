a97586d9ce5573c5a5d398c35c96cf24b3b78e22
lisonge <i@songe.li>
2023-10-06 18:46:55 +0800

    feat(朴朴超市): 开屏广告 (#685)

---
+++ src/apps/com.pupumall.customer.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.pupumall.customer',
+  name: '朴朴超市',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.pupumall.customer.activity.SplashActivity',
+      rules: '[id="com.pupumall.customer:id/tv_skip"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12830126',
+    },
+  ],
+});
