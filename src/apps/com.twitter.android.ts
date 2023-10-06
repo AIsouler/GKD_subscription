f76a2bdd0c6086fb453271c3070a1b1072e67bed
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-06 16:07:21 +0800

    feat(X): 信息流广告-规则优化 (#672)
    * 新创建 App 规则
    
    * feat(X): 信息流广告-规则优化
    
    * feat 【信息流广告】规则优化
    
    * feat 【信息流广告】规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.twitter.android.ts
+++ src/apps/com.twitter.android.ts
@@ -10,26 +10,41 @@ export default defineAppConfig({
       activityIds: 'com.twitter.app.main.MainActivity',
       rules: [
         {
-          name: '视频广告',
           key: 0,
+          name: '视频广告',
           matches:
             '@[id="com.twitter.android:id/tweet_curation_action"] <2 LinearLayout + LinearLayout >n [id="com.twitter.android:id/upper_video_attribution"][text="视频将在广告后播放"]',
           snapshotUrls: 'https://gkd-kit.songe.li/import/12798795',
         },
         {
-          name: '推荐广告',
           key: 1,
+          name: '推荐广告',
           matches:
             '@[id="com.twitter.android:id/tweet_curation_action"] <2 * + [id="com.twitter.android:id/tweet_auto_playable_content_parent"] > [id="com.twitter.android:id/tweet_promoted_badge_bottom"][text="推荐"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12813235',
         },
         {
-          name: '点击屏蔽',
-          preKeys: [0, 1],
           key: 2,
+          name: '评论区广告',
+          activityIds: 'com.twitter.tweetdetail.TweetDetailActivity',
+          matches:
+            '@[id="com.twitter.android:id/tweet_curation_action"] +n [id="com.twitter.android:id/tweet_promoted_badge_bottom"][text="推荐"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12825969',
+        },
+        {
+          preKeys: [0, 1, 2],
+          key: 10,
+          name: '点击屏蔽',
           matches:
             '@ViewGroup > [id="com.twitter.android:id/action_sheet_item_title"][text^="屏蔽"]', // 点击“我不喜欢”会返回主页，应当点击“屏蔽”。https://github.com/gkd-kit/subscription/issues/609#issuecomment-1745172531
           snapshotUrls: 'https://gkd-kit.songe.li/import/12798810',
+        },
+        {
+          preKeys: 10,
+          key: 11,
+          name: '二次确认-点击屏蔽',
+          matches: '[text="取消"] + [text="屏蔽"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12825947',
         },
       ],
     },
