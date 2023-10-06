722d3ecf5f4a2e9d53f0d3b0fb2fed3cd84f7f55
lisonge <i@songe.li>
2023-10-06 18:18:55 +0800

    feat(步道乐跑): 弹窗广告 (#633)

---
+++ src/apps/com.lptiyu.tanke.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.lptiyu.tanke',
+  name: '步道乐跑',
+  groups: [
+    {
+      key: 0,
+      name: '弹窗广告',
+      activityIds: 'com.lptiyu.tanke.activities.main.MainActivity',
+      rules:
+        '[id="com.lptiyu.tanke:id/interact_ad_root"] > [id="com.lptiyu.tanke:id/iv_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12829816',
+    },
+  ],
+});
