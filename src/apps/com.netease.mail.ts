60b4e39befc17b62dc169b6b50933713b6804deb
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-06 17:04:23 +0800

    feat(网易邮箱大师): 开屏广告 (#657)
    * feat(网易邮箱大师)
    
    * feat 【开屏广告】

--- src/apps/com.netease.mail.ts
+++ src/apps/com.netease.mail.ts
@@ -1,10 +1,16 @@
-import { matches } from 'lodash';
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
   id: 'com.netease.mail',
   name: '网易邮箱大师',
   groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.netease.mail.biz.main.MainITabActivity',
+      rules: '[id="com.netease.mail:id/ad_skip"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12818335',
+    },
     {
       key: 1,
       name: '更新弹窗',
