337466cd1fa112de9807709c261a9e76e21b0b09
shanlanCoding <44717382+shanlanCoding@users.noreply.github.com>
2023-10-06 17:22:57 +0800

    feat(摩托范): 信息流广告 (#681)
    * feat(摩托范): 新增弹窗匹配规则
    
    * feat(小熊油耗): 新增开屏广告
    
    * feat(摩托范): 信息流广告-规则优化
    
    * feat(小熊油耗): 回退代码
    
    ---------
    
    Co-authored-by: shanlan <hi@gobyte.cn>
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.jdd.motorfans.ts
+++ src/apps/com.jdd.motorfans.ts
@@ -22,6 +22,22 @@ export default defineAppConfig({
           exampleUrls:
             'https://user-images.githubusercontent.com/44717382/270852019-b0296eaa-a378-49b3-877b-acefca2a7d58.gif',
         },
+        {
+          matches: '@ImageView -n RelativeLayout > TextView[text*="广告"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12826288'],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '信息流广告',
+      activityIds: ['com.jdd.motorfans.MTMainActivity'],
+      rules: [
+        {
+          matches:
+            '[id="com.jdd.motorfans:id/recyclerView"] > FrameLayout[id="com.jdd.motorfans:id/container"] > FrameLayout[childCount=3] > ImageView[clickable=true][id=null] + ImageView[clickable=true][id=null]', //虽然在审查页面能匹配上，但是在“执行选择器”功能下一直处于搜索中，先添加后到真机测试
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12826382'],
+        },
       ],
     },
   ],
