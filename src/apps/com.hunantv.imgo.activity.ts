b84b260e3325c7bf6e490410443aa076891ace48
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-06 17:05:16 +0800

    feat(芒果TV): 首页推荐流-卡片广告,应用内广告弹窗,右侧悬浮广告 (#659)
    * improvement 【首页推荐流-卡片广告】添加 prekeys
    
    * feat 【应用内广告弹窗】
    
    * feat 【右侧悬浮广告】

--- src/apps/com.hunantv.imgo.activity.ts
+++ src/apps/com.hunantv.imgo.activity.ts
@@ -36,15 +36,55 @@ export default defineAppConfig({
       activityIds: 'com.hunantv.imgo.activity.MainActivity',
       rules: [
         {
-          matches: '[id="com.hunantv.imgo.activity:id/close_ad"]',
-          snapshotUrls:
-            'https://gkd-kit.gitee.io/import/38517192/40fb71ad-01a5-4420-9150-88172ff8a3bf',
-        },
-        {
+          key: 0,
           matches:
             '@[id="com.hunantv.imgo.activity:id/layout_logo"] > [id="com.hunantv.imgo.activity:id/tv_ad_logo"]',
           snapshotUrls:
             'https://gkd-kit.gitee.io/import/38517192/b74150b5-8e9f-4cbb-86a7-722fc739a1b8',
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          matches: '[id="com.hunantv.imgo.activity:id/close_ad"]',
+          snapshotUrls:
+            'https://gkd-kit.gitee.io/import/38517192/40fb71ad-01a5-4420-9150-88172ff8a3bf',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '应用内广告弹窗',
+      activityIds: 'com.hunantv.imgo.activity.MainActivity',
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[id="com.hunantv.imgo.activity:id/imgPromotion"] + [id="com.hunantv.imgo.activity:id/vClosePromotionView"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12818464',
+        },
+        {
+          key: 1,
+          matches: '@TextView[id=null][text=""] - View > Image[id="hotGif"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12818528',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '右侧悬浮广告',
+      activityIds: 'com.hunantv.imgo.activity.MainActivity',
+      rules: [
+        {
+          key: 0,
+          matches:
+            '@[id="com.hunantv.imgo.activity:id/apperience_close"] + [id="com.hunantv.imgo.activity:id/apperience_enter"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12818430',
+        },
+        {
+          key: 1,
+          matches:
+            '@[id="com.hunantv.imgo.activity:id/iv_float_close"] + [id="com.hunantv.imgo.activity:id/iv_float_content"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12818481',
         },
       ],
     },
