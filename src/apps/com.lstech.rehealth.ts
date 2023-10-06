ab0a3fb88f2d94a42eaabafca6d34ce22f38aa37
lisonge <i@songe.li>
2023-10-06 18:39:34 +0800

    feat(派健康): 开屏广告 (#646)

---
+++ src/apps/com.lstech.rehealth.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.lstech.rehealth',
+  name: '派健康',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.lstech.rehealth.splash.SplashActivity',
+      rules:
+        '[id="com.lstech.rehealth:id/ksad_splash_root_container"] [text="跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12830071',
+    },
+  ],
+});
