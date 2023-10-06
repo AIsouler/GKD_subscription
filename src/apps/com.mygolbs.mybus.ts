8f3202803f31fdd106806f897e34c7c30a738938
qianjiu <90247377+w4616@users.noreply.github.com>
2023-10-06 13:45:45 +0800

    feat(掌上公交): 开屏广告,广告卡片,广告弹窗,顶栏小广告 (#599)
    * Update com.mygolbs.mybus.ts
    
    * Update com.mygolbs.mybus.ts
    
    * feat(掌上公交): 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.mygolbs.mybus.ts
+++ src/apps/com.mygolbs.mybus.ts
@@ -8,8 +8,113 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.mygolbs.mybus.LoginActivity',
-      rules: '[id="com.mygolbs.mybus:id/splash_container"] >n [text^="跳过"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12745634',
+      rules: [
+        {
+          matches:
+            '[id="com.mygolbs.mybus:id/splash_container"||id="com.mygolbs.mybus:id/adsRl"] [text^="跳过"]',
+          snapshotUrls: [
+            'https://gkd-kit.songe.li/import/12745634',
+            'https://gkd-kit.vercel.app/import/12790377',
+          ],
+        },
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790485',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '广告卡片',
+      activityIds: [
+        'com.mygolbs.mybus.RTimeActivity',
+        'com.mygolbs.mybus.NewHomePageActivity',
+        'com.mygolbs.mybus.StationsResultActivityNew',
+        'com.mygolbs.mybus.mapsearch.poisearch.PoiSearchActivity',
+      ],
+      rules: [
+        {
+          name: '点击底部中间x关闭图标',
+          matches:
+            '[id="com.mygolbs.mybus:id/ad_frameLayout"] RelativeLayout[childCount=4] > TextView[text.length>0] + ImageView + ImageView[clickable=true][id=null]',
+          snapshotUrls: [
+            'https://gkd-kit.vercel.app/import/12790521',
+            'https://gkd-kit.vercel.app/import/12790706',
+            'https://gkd-kit.vercel.app/import/12790841',
+            'https://gkd-kit.vercel.app/import/12790887',
+          ],
+        },
+        {
+          name: '点击右上/下角x关闭图标',
+          matches:
+            '[id="com.mygolbs.mybus:id/ad_frameLayout"] View[childCount=2||childCount=3] > View[childCount=4] > View[childCount=1] > Image[text=""]',
+          snapshotUrls: [
+            'https://gkd-kit.vercel.app/import/12790656',
+            'https://gkd-kit.vercel.app/import/12790903',
+          ],
+        },
+        {
+          name: '点击右上角x关闭图标-2',
+          matches:
+            '[id="com.mygolbs.mybus:id/ad_frameLayout"] View[childCount=5] > View[childCount=2][index=4] > View[childCount=1] > Image[text=""]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790610',
+        },
+        {
+          name: '点击右上角x关闭图标-3',
+          matches:
+            '[id="com.mygolbs.mybus:id/ad_frameLayout"] View[childCount=2] > View[childCount=6][index=0] > View[childCount=1][index=3] > Image[text=""]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790941',
+        },
+        {
+          name: '点击右下角x关闭按钮',
+          matches:
+            '[text="广告"] <3 View + @View[clickable=true] >2 [text="×"][visibleToUser=true]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791122',
+        },
+        {
+          name: '点击不感兴趣',
+          matches:
+            '@LinearLayout[clickable=true][childCount<=2] > [text="不感兴趣"]',
+          snapshotUrls: [
+            'https://gkd-kit.vercel.app/import/12790671',
+            'https://gkd-kit.vercel.app/import/12790551',
+            'https://gkd-kit.vercel.app/import/12790616',
+            'https://gkd-kit.vercel.app/import/12790707',
+            'https://gkd-kit.vercel.app/import/12790717',
+          ],
+        },
+        {
+          name: '点击暂不领取',
+          matches:
+            '[id="com.mygolbs.mybus:id/ll_ad"] [id="com.mygolbs.mybus:id/btn_cancel"]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791579',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '广告弹窗',
+      activityIds: ['com.mygolbs.mybus.mapsearch.poisearch.PoiSearchActivity'],
+      rules: [
+        {
+          name: '点击右上角x关闭图标',
+          matches:
+            '[id="android:id/content"] >3 FrameLayout[childCount=6][index=0] > FrameLayout[childCount=1][index=1] > ImageView[id=null]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790762',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '顶栏小广告',
+      activityIds: ['com.mygolbs.mybus.RTimeActivity'],
+      rules: [
+        {
+          name: '点击右侧x关闭按钮',
+          matches: '[id="com.mygolbs.mybus:id/ad_close2"]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790841',
+        },
+      ],
     },
     {
       enable: false,
@@ -20,8 +125,8 @@ export default defineAppConfig({
       rules: [
         {
           matches: [
-            '[text$="开启通知权限"]',
-            '@[id="com.mygolbs.mybus:id/view_neg"] > [text="取消"]',
+            '[id="com.mygolbs.mybus:id/message"][text$="请开启通知权限"]',
+            '[id="com.mygolbs.mybus:id/view_neg"] > [id="com.mygolbs.mybus:id/negativeButton"][text="取消"]',
           ],
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12715980',
         },
