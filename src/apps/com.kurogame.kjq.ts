205563d67d2ab0add848f11d59fee21c92b7b127
dlh <22029832+dlh996@users.noreply.github.com>
2023-10-08 00:06:27 +0800

    feat(库街区): 开屏广告 (#705)
    Co-authored-by: dlh <du__th@163.com>

---
+++ src/apps/com.kurogame.kjq.ts
@@ -0,0 +1,25 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.kurogame.kjq',
+  name: '库街区',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: [
+        'com.kurogame.kjq.userlogin.ui.activity.SplashActivity',
+        'com.android.launcher.Launcher',
+      ],
+      rules: [
+        {
+          matches: '[id="com.kurogame.kjq:id/jump"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12836250',
+            'https://gkd-kit.gitee.io/import/12836248',
+          ],
+        },
+      ],
+    },
+  ],
+});
