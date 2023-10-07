5998011410cf12abcfe5bdb47fb8e7b0f36a397d
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-07 23:51:05 +0800

    feat(中国移动浙江): 开屏广告 (#697)
    * feat(中国移动浙江)
    
    - 开屏广告
    
    * 格式订正
    
    删除末尾空行
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.example.businesshall.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.example.businesshall',
+  name: '中国移动浙江',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['com.businesshall.activity.NewLogoActivity'],
+      rules: [
+        {
+          matches: '[id="com.example.businesshall:id/jump_layout"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12830978'],
+        },
+      ],
+    },
+  ],
+});
