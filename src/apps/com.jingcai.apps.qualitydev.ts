fd4e82388249b57b2d311ee90e9aacd03c4df304
Adam <3246057843@qq.com>
2023-10-08 00:29:34 +0800

    feat(到梦空间): 开屏广告 (#730) (#735)

---
+++ src/apps/com.jingcai.apps.qualitydev.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.jingcai.apps.qualitydev',
+  name: '到梦空间',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      activityIds: 'com.jingcai.apps.qualitydev.qualitync.SplashActivity',
+      rules: [
+        {
+          matches: ['[id="com.jingcai.apps.qualitydev:id/tv_toapp"]'],
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12838092',
+        },
+      ],
+    },
+  ],
+});
