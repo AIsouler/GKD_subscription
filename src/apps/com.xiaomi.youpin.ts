df9d5b0fefe9c3859c2807d57737f7baaca0c162
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-08 00:16:32 +0800

    feat(小米有品): 开屏广告,广告弹窗,版本升级弹窗 (#716)
    * feat(小米有品)
    
    - 开屏广告
    - 广告弹窗
    - 版本升级弹窗
    
    * chore(actions): check_format_lint
    
    * feat(小米有品): 广告弹窗-规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.xiaomi.youpin.ts
@@ -0,0 +1,46 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xiaomi.youpin',
+  name: '小米有品',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['com.xiaomi.youpin.activity.YouPinMainTabActivity'],
+      rules: [
+        {
+          matches: '[id="com.xiaomi.youpin:id/yp_splash_jump_image"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12836717'],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '广告弹窗',
+      activityIds: [
+        'com.xiaomi.youpin.globalpopwindow.popwindow.PopWindowActivity',
+      ],
+      rules: [
+        {
+          matches:
+            '@Image <2 View[childCount=2] < [id="app"] < [text="有品"] < * < [id="android:id/content"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12836727'],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '版本升级弹窗',
+      activityIds: [
+        'com.xiaomi.youpin.globalpopwindow.popwindow.PopWindowActivity',
+      ],
+      rules: [
+        {
+          matches: '[id="com.xiaomi.youpin:id/ypd_alert_close_area"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12836775'],
+        },
+      ],
+    },
+  ],
+});
