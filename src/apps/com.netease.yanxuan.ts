09ea3aad0d6ee3ba79e1e8576ca9eac2bb05a5bd
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-08 23:20:12 +0800

    feat(网易严选): 开屏广告,弹窗广告 (#761)
    * feat(网易严选)
    
    - 开屏广告
    - 首页-弹窗广告
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.netease.yanxuan.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.netease.yanxuan',
+  name: '网易严选',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.netease.yanxuan.module.splash.SplashActivity',
+      rules: '[id = "com.netease.yanxuan:id/splash_pic_jump_container"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12840637',
+    },
+    {
+      key: 1,
+      name: '首页-弹窗广告',
+      activityIds:
+        'com.netease.yanxuan.module.mainpage.activity.MainPageActivity',
+      rules: '[id="com.netease.yanxuan:id/trans_cancel"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12840641',
+    },
+  ],
+});
