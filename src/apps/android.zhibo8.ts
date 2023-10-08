f446b76b81054a0008f3dc0b45a098e935fbfaec
二刺螈 <i@songe.li>
2023-10-09 00:11:04 +0800

    feat(直播吧): 信息流广告 (#692)

--- src/apps/android.zhibo8.ts
+++ src/apps/android.zhibo8.ts
@@ -18,5 +18,24 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '信息流广告',
+      rules: [
+        {
+          key: 0,
+          name: '点击[查看详情]右侧x',
+          matches:
+            '@[id="android.zhibo8:id/iv_tip"] - [id="android.zhibo8:id/tv_app_download_2"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12841134',
+        },
+        {
+          preKeys: 0,
+          name: '点击[不感兴趣]',
+          matches: '[id="android.zhibo8:id/tv_title"][text="不感兴趣"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12841135',
+        },
+      ],
+    },
   ],
 });
