baa5920bcb66323a54734bad657e5485a4401041
二刺螈 <i@songe.li>
2023-10-08 23:47:12 +0800

    feat(爱玩机工具箱): 忽略授权提示 (#640)

---
+++ src/apps/com.byyoung.setting.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.byyoung.setting',
+  name: '爱玩机工具箱',
+  groups: [
+    {
+      key: 1,
+      name: '忽略授权提示',
+      activityIds: 'com.byyoung.setting.Welcome.PermissionActivity',
+      rules: [
+        {
+          matches: '[id="com.byyoung.setting:id/tv_name"][text*="忽略授权"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12829909',
+        },
+      ],
+    },
+  ],
+});
