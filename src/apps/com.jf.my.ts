277a9982e6b7aa5aec65174c59ce3db86c19d571
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-08 23:18:05 +0800

    feat(蜜源): 版本升级弹窗,广告弹窗 (#759)
    * 【版本升级弹窗】 - 规则优化
    
    * 首页-广告弹窗

--- src/apps/com.jf.my.ts
+++ src/apps/com.jf.my.ts
@@ -7,14 +7,27 @@ export default defineAppConfig({
     {
       key: 0,
       name: '版本升级弹窗',
-      activityIds: ['com.lbe.security.ui.CountdownDialogActivity'],
+      activityIds: [
+        'com.lbe.security.ui.CountdownDialogActivity',
+        'com.jf.my.MainActivity',
+      ],
       rules: [
         {
           matches:
             '[id="com.jf.my:id/btn_ok"][text="更新"] - [id="com.jf.my:id/btn_cancel"][text="关闭"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12838034',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12838034',
+            'https://gkd-kit.gitee.io/import/12840591',
+          ],
         },
       ],
+    },
+    {
+      key: 1,
+      name: '首页-广告弹窗',
+      activityIds: 'com.jf.my.main.ui.dialog.HomeRedPackageDialog',
+      rules: '[id="com.jf.my:id/iv_back"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12840619',
     },
   ],
 });
