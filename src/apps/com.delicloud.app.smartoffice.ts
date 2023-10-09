0eded6c33f277d0fc5442022156044d7753d39c1
dlh <22029832+dlh996@users.noreply.github.com>
2023-10-10 00:06:23 +0800

    feat(得力e+): 开屏广告 (#797)
    Signed-off-by: dlh <du__th@163.com>
    Co-authored-by: dlh <du__th@163.com>

---
+++ src/apps/com.delicloud.app.smartoffice.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.delicloud.app.smartoffice',
+  name: '得力e+',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.delicloud.app.smartoffice.mvp.ui.JiGuangSpalshActivity',
+      rules: [
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12843738'],
+        },
+      ],
+    },
+  ],
+});
