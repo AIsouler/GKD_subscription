2914d7400f89f9acf94fbabbe3202fc1deb5e412
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-10 00:38:58 +0800

    feat(智安星): 开屏广告 (#838)
    * feat(智安星)
    
    - 开屏广告
    
    * Update com.tutu.zhianxing.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.tutu.zhianxing.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tutu.zhianxing',
+  name: '智安星',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['com.xlxapp.MainActivity'],
+      rules: '[id="com.tutu.zhianxing:id/skip_title"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12847474',
+    },
+  ],
+});
