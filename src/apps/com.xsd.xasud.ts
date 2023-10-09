8d2035a995f7db788d0dc039124c29310acc2d29
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-10 00:40:11 +0800

    feat(末忆-铁锈盒子): 开屏广告 (#839)
    * feat(末忆-铁锈盒子)
    
    - 开屏广告
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.xsd.xasud.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xsd.xasud',
+  name: '末忆-铁锈盒子',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['io.dcloud.PandoraEntryActivity'],
+      rules: '[text = "广告启动页"] [text = "跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12847496',
+    },
+  ],
+});
