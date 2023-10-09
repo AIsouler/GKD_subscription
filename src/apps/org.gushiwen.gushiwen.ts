2c3dd6247c92418214be0432010797fd7c06fc1a
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-10 00:12:19 +0800

    feat(古诗文网): 应用内广告弹窗-规则优化 (#806)
    * feat 【应用内广告弹窗】
    
    * feat 【应用内广告弹窗】完善规则

--- src/apps/org.gushiwen.gushiwen.ts
+++ src/apps/org.gushiwen.gushiwen.ts
@@ -15,13 +15,28 @@ export default defineAppConfig({
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12813618',
         },
         {
-          preKeys: [0],
+          key: 1,
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: '@Image < View +3 View > View > Image',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12837225',
+        },
+        {
+          preKeys: [0, 1],
           key: 10,
           desc: '打赏页面-点击返回按钮',
           activityIds: 'local.z.androidshared.vip.RewardActivity',
           matches:
             '[id="org.gushiwen.gushiwen:id/btn_back"][desc="返回"] + [text="打赏我们"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12813638',
+        },
+        {
+          preKeys: [0, 1],
+          key: 11,
+          desc: '荣誉会员-点击返回按钮',
+          activityIds: 'local.z.androidshared.vip.member.VipActivity',
+          matches: '[id="org.gushiwen.gushiwen:id/backBtn"][desc="返回"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12837229',
         },
       ],
     },
