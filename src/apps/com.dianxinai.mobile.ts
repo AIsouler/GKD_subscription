c6cc8e15201dc195f150036d6990ac9b63518317
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-10 00:42:05 +0800

    feat(点心云): 开屏广告 (#843)
    * feat(点心云)
    
    - 开屏广告
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.dianxinai.mobile.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.dianxinai.mobile',
+  name: '点心云',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['com.dianxinai.mobile.MainActivity'],
+      rules:
+        '@ViewGroup[clickable = true] > TextView[text $="s" && text.length=2]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12847518',
+    },
+  ],
+});
