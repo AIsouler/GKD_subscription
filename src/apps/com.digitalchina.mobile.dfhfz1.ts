bed3938c2bfa5e758caa03de3640e8c8f8bdadbe
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-10 00:09:04 +0800

    feat(e福州): 开屏广告 (#803)
    * feat 【开屏广告】
    
    * feat 【关闭花呗升级弹窗】完善规则
    
    * Revert "feat 【关闭花呗升级弹窗】完善规则"
    
    This reverts commit 41a0799e01513a8e90d206174f6f0d9802f1a5af.

---
+++ src/apps/com.digitalchina.mobile.dfhfz1.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.digitalchina.mobile.dfhfz1',
+  name: 'e福州',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.systoon.toon.user.login.view.WelcomeActivity',
+      matchLauncher: true,
+      quickFind: true,
+      rules: '[id="com.digitalchina.mobile.dfhfz1:id/tv_skip"][text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12846137',
+    },
+  ],
+});
