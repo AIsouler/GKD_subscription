59aae4299ad8e235fd7c545a196ea6d3e185e749
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-09 23:56:03 +0800

    feat(网心云): 开屏广告,广告弹窗 (#776)
    * feat(网心云)
    
    - 开屏广告
    - 广告弹窗
    
    * chore(actions): check_format_lint
    
    * feat(网心云)
    
    规则优化，避免误匹配
    
    * Update com.onethingcloud.android.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.onethingcloud.android.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.onethingcloud.android',
+  name: '网心云',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.onethingcloud.android.MainActivity',
+      rules: 'View[desc ^="跳过" && desc.length=6]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12841174',
+    },
+    {
+      key: 1,
+      name: '广告弹窗',
+      activityIds: 'com.onethingcloud.android.MainActivity',
+      rules:
+        'FrameLayout >8 View > ImageView + ImageView + ImageView[clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12841171',
+    },
+  ],
+});
