dfc6f379be459b114eab2709e2dd2e79c89f9c80
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-10 00:11:13 +0800

    feat(钉钉): 开屏广告-规则优化 (#805)

--- src/apps/com.alibaba.android.rimet.ts
+++ src/apps/com.alibaba.android.rimet.ts
@@ -7,10 +7,24 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.alibaba.android.rimet.biz.SplashActivity',
-      rules:
-        '[id="com.alibaba.android.rimet:id/btn_check_detail"||text^="跳过"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12506211',
+      matchLauncher: true,
+      quickFind: true,
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.alibaba.android.rimet.biz.SplashActivity',
+          matches:
+            '[id="com.alibaba.android.rimet:id/btn_check_detail"||text^="跳过"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12506211',
+        },
+        {
+          key: 1,
+          activityIds: 'com.alibaba.android.rimet.biz.SplashAdvertiseActivity',
+          matches:
+            '[id="com.alibaba.android.rimet:id/splash_advertise_pass_text"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12837220',
+        },
+      ],
     },
   ],
 });
