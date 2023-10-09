c4b5e9720522b2ec0d54c7b4ba0b8f65c6802d4f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-10 00:08:07 +0800

    feat(轻音社): 开屏广告,应用内广告弹窗 (#801)
    * chore(main): release 1.39.0 (#7)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * chore(main): release 1.41.0 (#9)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * feat 【信息流广告】规则优化
    
    * Revert "feat 【信息流广告】规则优化"
    
    This reverts commit 469384e18c965c0cb4a1042f48776c11b71c54d5.
    
    * feat 轻音社
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.facetech.konking.ts
@@ -0,0 +1,36 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.facetech.konking',
+  name: '人民日报',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.facetech.konking.MainActivity',
+      matchLauncher: true,
+      quickFind: true,
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[id="com.facetech.konking:id/ksad_splash_circle_skip_view"] > [text="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12841078',
+        },
+        {
+          key: 1,
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12841079',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '应用内广告弹窗',
+      activityIds:
+        'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+      rules: '@Image[text=""] < View +n View > View > TextView[text="广告"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12841081',
+    },
+  ],
+});
