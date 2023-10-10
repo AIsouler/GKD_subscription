1d5527c9c105d323b4b45da20294b62df1484c84
Mr.X <58155247+ZhReimu@users.noreply.github.com>
2023-10-11 00:28:21 +0800

    feat(米游社): 自动打卡 (#888)
    * feat(米游社): 自动签到
    
    * feat(米游社): 自动打卡-规则优化
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/com.mihoyo.hyperion.ts
+++ src/apps/com.mihoyo.hyperion.ts
@@ -40,5 +40,17 @@ export default defineAppConfig({
       rules: 'TextView[id="com.mihoyo.hyperion:id/mUpgradeDialogCancel"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12675513',
     },
+    {
+      key: 3,
+      name: '自动打卡',
+      desc: '点击顶部搜索框右侧[打卡],切换板块自动签到',
+      activityIds: 'com.mihoyo.hyperion.main.HyperionMainActivity',
+      rules:
+        'TextView[id="com.mihoyo.hyperion:id/mTvText"][text="打卡"][visibleToUser=true]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12857503', // 未打卡
+        'https://gkd-kit.gitee.io/import/12859518', // 已打卡
+      ],
+    },
   ],
 });
