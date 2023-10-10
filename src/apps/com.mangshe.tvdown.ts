516ef7a711bd5b70785764a3de61f9a3c9519a82
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-10 23:52:14 +0800

    feat(蟒蛇下载): 开屏广告 (#871)
    * feat(蟒蛇下载): 开屏广告
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.mangshe.tvdown.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.mangshe.tvdown',
+  name: '蟒蛇下载',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.mangshe.tvdown.ad.chuanshanjia.SplashActivity_csj',
+      rules: [
+        {
+          matches: '[id="com.mangshe.tvdown:id/skip_view2"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12854596',
+        },
+      ],
+    },
+  ],
+});
