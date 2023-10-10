578f87f36d297d563daf1696cf1e6af5a006302d
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-10 23:40:02 +0800

    feat(咪咕音乐): 开屏广告 (#861)
    * feat(咪咕音乐): 开屏广告
    
    * chore(actions): check_format_lint
    
    * feat(咪咕音乐): 开屏广告-规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/cmccwm.mobilemusic.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cmccwm.mobilemusic',
+  name: '咪咕音乐',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'cmccwm.mobilemusic.ui.SplashActivity',
+      rules: '[id = "cmccwm.mobilemusic.base_runtime:id/bt_skip_ad"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12852498',
+    },
+  ],
+});
