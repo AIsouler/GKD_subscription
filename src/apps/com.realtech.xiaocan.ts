3d1ef16fe7fe17fec73be75a42715df05986cbe5
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-10 23:44:08 +0800

    feat(小蚕霸王餐): 开屏广告 - 规则优化 (#868)
    * feat(小蚕霸王餐): 开屏广告 - 规则优化
    
    【开屏广告】 - 规则优化
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.realtech.xiaocan.ts
+++ src/apps/com.realtech.xiaocan.ts
@@ -8,9 +8,17 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.realtech.adscope.SplashActivity',
-      rules:
-        '[id = "com.realtech.xiaocan:id/splash_ad_container"] [text ^= "跳过"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12843368',
+      rules: [
+        {
+          matches:
+            '[id = "com.realtech.xiaocan:id/splash_ad_container"] [text ^= "跳过"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12843368',
+        },
+        {
+          matches: '[id = "com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12854728',
+        },
+      ],
     },
   ],
 });
