a1b3c3f460e1c2e409449993a49237174975f590
ylxz95 <106958787+ylxz95@users.noreply.github.com>
2023-10-11 00:22:16 +0800

    feat(搜书大师): 开屏广告 (#886)
    * Create com.flyersoft.seekbooks.ts
    
    * feat(搜书大师): 开屏广告-规则优化
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.flyersoft.seekbooks.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.flyersoft.seekbooks',
+  name: '搜书大师',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: ['com.flyersoft.seekbooks.ActivityMain'],
+      rules: [
+        {
+          matches:
+            '[id="com.flyersoft.seekbooks:id/splash_container1"] >5 [text^="跳过"][text.length<=3]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12857275',
+        },
+      ],
+    },
+  ],
+});
