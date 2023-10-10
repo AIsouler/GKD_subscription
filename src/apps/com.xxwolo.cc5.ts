5df3620a1619ccaa34e144ddac789a7cc0cd1a0e
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-10 23:35:24 +0800

    feat(测测): 开屏广告 (#855)

---
+++ src/apps/com.xxwolo.cc5.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xxwolo.cc5',
+  name: '测测',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: 'com.cece.app.MainActivity',
+      rules: '[text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12852352',
+    },
+  ],
+});
