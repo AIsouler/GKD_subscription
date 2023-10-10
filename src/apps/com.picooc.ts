8ec99fa66f0f108193779743cfd47bb18e92fbd7
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-11 00:40:31 +0800

    feat(PICOOC): 开屏广告 (#894)

---
+++ src/apps/com.picooc.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.picooc',
+  name: 'PICOOC',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.picooc.activity.start.WelcomeActivity',
+      rules: 'TextView[text = "广告"] <2 ViewGroup - ViewGroup [text = "跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12859002',
+    },
+  ],
+});
