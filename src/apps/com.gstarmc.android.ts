5741f4c9b7f2c7474fd10b8f5c901d2b84bde939
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-11 00:03:31 +0800

    feat(CAD看图王): 开屏广告 (#882)
    * feat(CAD看图王): 开屏广告
    
    * chore(actions): check_format_lint
    
    * feat(CAD看图王): 开屏广告-规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.gstarmc.android.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.gstarmc.android',
+  name: 'CAD看图王',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.stone.app.ui.activity.AppSplashActivity',
+      rules: [
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12855855',
+        },
+      ],
+    },
+  ],
+});
