13c8b8d1ab0f2946751d6b9ccc9184086ad9b008
walklty <47232102+walklty@users.noreply.github.com>
2023-10-11 23:26:53 +0800

    fix(Taptap): 推荐广告-减少误触 (#912)
    * Update com.taptap.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.taptap.ts
+++ src/apps/com.taptap.ts
@@ -13,10 +13,12 @@ export default defineAppConfig({
           key: 0,
           activityIds: 'com.taptap.other.basic.impl.TapMainActivity',
           matchLauncher: true,
-          matches: '[id="com.taptap.app.game:id/app_menu"][visibleToUser=true]',
+          matches:
+            '[id="com.taptap.app.middle:id/decision_layout_mask"] + @[id="com.taptap.app.game:id/app_menu"][visibleToUser=true]',
           snapshotUrls: [
             'https://gkd-kit.songe.li/import/12840903',
             'https://gkd-kit.songe.li/import/12842279',
+            'https://gkd-kit.gitee.io/import/12864810', //需避免点击正常情况下的app menu
           ],
         },
         {
