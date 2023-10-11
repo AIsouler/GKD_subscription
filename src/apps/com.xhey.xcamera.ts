f7a62ae27ba44bd2ec151037784f8a9cf21fce5b
walklty <47232102+walklty@users.noreply.github.com>
2023-10-11 23:25:15 +0800

    feat(今日相机): 开屏广告 (#909)
    * Create com.xhey.xcamera.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.xhey.xcamera.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xhey.xcamera',
+  name: '今日相机',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      activityIds: ['com.xhey.xcamera.ui.camera.picNew.PreviewActivity'],
+      rules: [
+        {
+          matches: '@LinearLayout > TextView[text^="跳过"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12864144'],
+        },
+      ],
+    },
+  ],
+});
