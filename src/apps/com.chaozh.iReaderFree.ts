e84b118765166f4e2fea8b0a81ce87f5256e2be8
二刺螈 <i@songe.li>
2023-10-12 00:23:19 +0800

    feat(掌阅): 开屏广告 (#783)

---
+++ src/apps/com.chaozh.iReaderFree.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.chaozh.iReaderFree',
+  name: '掌阅',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.chaozh.iReader.ui.activity.WelcomeActivity',
+      rules:
+        'RelativeLayout[childCount=1] > [text^="跳过"][text.length<=4][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12872346',
+    },
+  ],
+});
