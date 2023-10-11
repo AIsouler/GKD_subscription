3565ff1f4370d981c897a351bf390845bb15e651
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-12 00:04:37 +0800

    feat(好游快爆): 更新弹窗 (#932)
    * 新创建 App 规则
    
    * feat 更新弹窗

--- src/apps/com.xmcy.hykb.ts
+++ src/apps/com.xmcy.hykb.ts
@@ -11,5 +11,13 @@ export default defineAppConfig({
       rules: '[id="com.xmcy.hykb:id/skip"][text^="跳过"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/12774856',
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.xmcy.hykb.app.dialog.UpdateDialog',
+      rules:
+        '@[id="com.xmcy.hykb:id/iv_inner_button_close"] - RelativeLayout >n [id="com.xmcy.hykb:id/text_update_tip"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12870604',
+    },
   ],
 });
