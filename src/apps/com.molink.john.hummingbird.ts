e0ff34049fe55f9fb2abd14274f4661ad4858cc3
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-11 23:49:26 +0800

    feat(bebird): 开屏广告 (#923)
    * feat(bebird): 开屏广告(gkd-kit#922)
    
    * feat(bebird): 开屏广告-规则优化
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.molink.john.hummingbird.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.molink.john.hummingbird',
+  name: 'bebird',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      activityIds: 'com.molink.john.hummingbird.activity.WellComeActivity',
+      rules: '[id="com.molink.john.hummingbird:id/rl_timer"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12867595',
+    },
+  ],
+});
