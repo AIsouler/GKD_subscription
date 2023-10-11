58fe6c28b7bef69299e0c9711e903057968cb28a
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-11 23:57:24 +0800

    feat(大学搜题酱): 开屏广告,推送通知,弹窗广告 (#925)
    * feat(大学搜题酱): 一些规则 (gkd-kit#924)
    - 开屏广告
    - 首页-推送通知
    - 我的-弹窗广告
    
    * feat(大学搜题酱): 开屏广告-规则优化
    
    * feat(大学搜题酱): 开屏广告-规则优化
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.zmzx.college.search.ts
@@ -0,0 +1,37 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.zmzx.college.search',
+  name: '大学搜题酱',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      activityIds: 'com.zmzx.college.search.activity.init.InitActivity',
+      rules:
+        '[id="com.zmzx.college.search:id/ksad_splash_root_container"] [text="跳过"]',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12867875',
+        'https://gkd-kit.songe.li/import/12867970',
+      ],
+    },
+    {
+      enable: false,
+      key: 1,
+      name: '首页-推送通知',
+      activityIds:
+        'com.zmzx.college.search.activity.main.activity.MainActivity',
+      rules: 'ImageView[id="com.zmzx.college.search:id/siv_dialog_close"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12867853',
+    },
+    {
+      key: 2,
+      name: '我的-弹窗广告',
+      activityIds:
+        'com.zmzx.college.search.activity.main.activity.MainActivity',
+      rules: 'ImageView[id="com.zmzx.college.search:id/iv_close"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12867751',
+    },
+  ],
+});
