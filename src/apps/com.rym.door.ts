ee5db8ce65fd4fd4a7d52b13f904a97f90566081
lisonge <i@songe.li>
2023-10-11 13:46:43 +0800

    feat(日淘任意门): 开屏广告 (#719)

---
+++ src/apps/com.rym.door.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.rym.door',
+  name: '日淘任意门',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      rules:
+        'View[childCount=4] > [desc^="跳过（"][desc.length<=6][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12865344',
+    },
+  ],
+});
