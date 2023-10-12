7f82acd95091a480881fce01c0ac3ada35606668
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-13 00:43:58 +0800

    feat(QQ): QQ小程序开屏广告 (#950)
    close #943

--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -173,5 +173,18 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 13,
+      name: 'QQ小程序开屏广告',
+      desc: '点击右下角跳过',
+      activityIds: 'com.tencent.mobileqq.mini.appbrand.ui.AppBrandUI',
+      rules: [
+        {
+          matches:
+            'TextView[text = "广告"] < RelativeLayout + RelativeLayout TextView[text = "跳过"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12877215',
+        },
+      ],
+    },
   ],
 });
