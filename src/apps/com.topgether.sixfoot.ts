fb95734b3ca9ea4753e6e4134fd93aeb9a38db20
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-13 01:09:58 +0800

    feat(六只脚): 开屏广告 (#968)
    #957

---
+++ src/apps/com.topgether.sixfoot.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.topgether.sixfoot',
+  name: '六只脚',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.topgether.sixfootPro.biz.splash.SplashActivity',
+      matchLauncher: true,
+      quickFind: true,
+      rules: '[id="com.topgether.sixfoot:id/mAdsContainer"] >n [text="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12882503',
+    },
+  ],
+});
