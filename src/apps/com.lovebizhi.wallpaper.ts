42fb85c03a0f83f3902330d19cb48aa4539212b8
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-13 01:29:11 +0800

    feat(爱壁纸): 开屏广告-规则优化 (#988)
    * feat(爱壁纸): 开屏广告-规则优化
    
    * Update com.lovebizhi.wallpaper.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #979

--- src/apps/com.lovebizhi.wallpaper.ts
+++ src/apps/com.lovebizhi.wallpaper.ts
@@ -8,6 +8,9 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.adesk.picasso.view.MainActivity',
+
+      // 来自 #979 的优化建议，反馈会匹配到桌面活动ID
+      matchLauncher: true,
       rules: '[id = "com.byted.pangle:id/tt_splash_skip_btn"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/12859492',
     },
