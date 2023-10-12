079da71fc55703d4db157d4b7bdf2e9f32154ea1
walklty <47232102+walklty@users.noreply.github.com>
2023-10-13 00:29:18 +0800

    feat(Youtube): 赞助商广告, Premium广告 (#944)
    * Update com.google.android.youtube.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #938

--- src/apps/com.google.android.youtube.ts
+++ src/apps/com.google.android.youtube.ts
@@ -12,5 +12,22 @@ export default defineAppConfig({
         'com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity',
       rules: '[id=`com.google.android.youtube:id/skip_ad_button`]',
     },
+    {
+      key: 1,
+      name: '视频播放-赞助商广告',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12877346',
+      activityIds:
+        'com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity',
+      rules: 'ImageView[desc="更多选项"] + @ImageView[desc="关闭广告面板"]',
+    },
+    {
+      key: 2,
+      name: '首页-会员广告',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12877357',
+      activityIds:
+        'com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity',
+      rules:
+        'ImageView < @Button < ViewGroup +3 Button[desc*="YouTube Premium"]',
+    },
   ],
 });
