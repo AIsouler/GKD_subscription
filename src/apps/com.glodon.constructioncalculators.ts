f1a36b83276d3af77864047ef348b9fd37217f6f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-13 01:18:34 +0800

    feat(建工计算器): 开屏广告 (#973)
    close #899

---
+++ src/apps/com.glodon.constructioncalculators.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.glodon.constructioncalculators',
+  name: '建工计算器',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.glodon.constructioncalculators.main.SplashActivityTt',
+      matchLauncher: true,
+      rules: '@[text^="跳过"] < LinearLayout +n ImageView',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12882803',
+    },
+  ],
+});
