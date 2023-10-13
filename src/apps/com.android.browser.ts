72a8adb09874e672fa57eb164de104f26b1c782e
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-14 01:43:13 +0800

    feat(小米浏览器): 主页信息流广告 (#1026)
    close #1000

--- src/apps/com.android.browser.ts
+++ src/apps/com.android.browser.ts
@@ -16,5 +16,28 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '主页信息流广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.android.browser.BrowserActivity',
+          matches:
+            '@[id!=null][desc="负反馈"][visibleToUser=true] -n * > * > [text="广告"]',
+          snapshotUrls: [
+            'https://gkd-kit.songe.li/import/12894221',
+            'https://gkd-kit.songe.li/import/12893649',
+          ],
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          activityIds: 'com.android.browser.flow.view.FeedbackDialog',
+          matches: '@FrameLayout > ViewGroup > [text="不感兴趣"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12894234',
+        },
+      ],
+    },
   ],
 });
