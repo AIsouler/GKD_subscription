aa10721774068ba98e26f8ac559d6d9faec9d1a8
walklty <47232102+walklty@users.noreply.github.com>
2023-10-14 01:26:07 +0800

    feat(滴滴出行): 开屏广告-规则优化 (#1004)
    * Update com.sdu.didi.psnger.ts
    
    * Update com.sdu.didi.psnger.ts
    
    * chore(actions): check_format_lint
    
    * feat(滴滴): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>
    
    close #992

--- src/apps/com.sdu.didi.psnger.ts
+++ src/apps/com.sdu.didi.psnger.ts
@@ -7,8 +7,18 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.didi.sdk.app.launch.splash.SplashActivity',
-      rules: '[id=`com.sdu.didi.psnger:id/skip_ad_ll`]',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: [
+        'com.didi.sdk.app.launch.splash.SplashActivity',
+        'com.didi.sdk.app.MainActivity',
+      ],
+      rules: [
+        {
+          matches: '[id=`com.sdu.didi.psnger:id/skip_ad_ll`]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12888394',
+        },
+      ],
     },
   ],
 });
