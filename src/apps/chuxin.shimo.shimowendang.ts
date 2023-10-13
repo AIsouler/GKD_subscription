0f7af7dca15c043b486d20f1f5d97a749daad60f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-14 01:42:27 +0800

    feat(石墨文档): 开屏广告 (#1025)

---
+++ src/apps/chuxin.shimo.shimowendang.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'chuxin.shimo.shimowendang',
+  name: '石墨文档',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: [
+        'chuxin.shimo.shimowendang.launchad.mediatom.MediatomActivity',
+        'chuxin.shimo.shimowendang.MainActivity',
+      ],
+      matchLauncher: true,
+      quickFind: true,
+      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12894158', // chuxin.shimo.shimowendang.launchad.mediatom.MediatomActivity
+        'https://gkd-kit.songe.li/import/12894163', // chuxin.shimo.shimowendang.MainActivity
+      ],
+    },
+  ],
+});
