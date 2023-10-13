063b8562e11722f886ce1ab59e96bc2422cc2194
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-14 01:52:28 +0800

    feat(追漫大全): 广告弹窗-规则优化 (#1034)
    * feat(追漫大全): 广告弹窗-规则优化
    
    * chore(actions): check_format_lint
    
    * Update com.mj.mjyd.ts
    
    * chore(actions): check_format_lint
    
    * Update com.mj.mjyd.ts
    
    * feat(追漫大全): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>
    
    close #949

--- src/apps/com.mj.mjyd.ts
+++ src/apps/com.mj.mjyd.ts
@@ -8,9 +8,20 @@ export default defineAppConfig({
       key: 1,
       name: '广告弹窗',
       quickFind: true,
-      rules:
-        '@ImageView < FrameLayout[childCount=1] <2 FrameLayout[childCount=6] >3 [text="立即下载"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12877729',
+      activityIds:
+        'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Activity_T',
+      rules: [
+        {
+          matches:
+            '@ImageView < FrameLayout[childCount=1] <2 FrameLayout[childCount=6] >3 [text="立即下载"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12877729',
+        },
+        {
+          matches:
+            '@ImageView < FrameLayout[childCount=1] <3 FrameLayout[childCount=5] <3 FrameLayout[childCount=5] >3 [text="立即下载"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12895086',
+        },
+      ],
     },
   ],
 });
