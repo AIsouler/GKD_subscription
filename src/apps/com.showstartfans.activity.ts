de94a24ff98220eb2808aa46cf03b6e2dac2a471
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-14 01:43:54 +0800

    feat(秀动): 开屏广告 (#1027)
    close #1006

---
+++ src/apps/com.showstartfans.activity.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.showstartfans.activity',
+  name: '秀动',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds:
+        'com.showstartfans.activity.activitys.welcome.AdvertActivity',
+      matchLauncher: true,
+      quickFind: true,
+      rules: '[id="com.showstartfans.activity:id/txt_jump"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12894279',
+    },
+  ],
+});
