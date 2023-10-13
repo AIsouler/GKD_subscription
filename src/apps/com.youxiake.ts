ba9d8679524a9be067491473993c6225622a1d92
walklty <47232102+walklty@users.noreply.github.com>
2023-10-14 01:23:36 +0800

    feat(游侠客旅行): 开屏广告 (#1003)
    * Create com.youxiake.ts
    
    * chore(actions): check_format_lint
    
    * feat(游侠客旅行): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>
    
    close #1001

---
+++ src/apps/com.youxiake.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.youxiake',
+  name: '游侠客旅行',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.youxiake.ui.main.MainActivity',
+      matchLauncher: true,
+      quickFind: true,
+      rules: [
+        {
+          matches: '[id="com.youxiake:id/start_skip_txt"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12891979',
+        },
+      ],
+    },
+  ],
+});
