13df822b9932c1172af2647a4efedd7a109cf4a2
shanlanCoding <44717382+shanlanCoding@users.noreply.github.com>
2023-10-14 01:01:02 +0800

    feat(微信): 朋友圈广告-规则优化 (#991)
    * feat(微信): 朋友圈广告匹配
    
    * feat(微信): 朋友圈广告-规则优化
    
    ---------
    
    Co-authored-by: shanlan <hi@gobyte.cn>
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -15,8 +15,12 @@ export default defineAppConfig({
       rules: [
         {
           name: '点击广告卡片右上角',
-          matches: 'ImageView - TextView[text="广告"][id!=null][index=0]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12642588'],
+          matches:
+            'ImageView - TextView[text="广告"][clickable=true][id!=null]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12642588',
+            'https://gkd-kit.gitee.io/import/12888129', //ImageView - TextView[text="广告"][id!=null][index=0]这个规则无法匹配该广告，需要删除[index=0]
+          ],
         },
         // 以下是[确认关闭按钮]出现的情况
         {
