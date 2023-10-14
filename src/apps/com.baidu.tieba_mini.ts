e291f3d65d905fa4d8ccdad85d5e924a47bc9550
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-15 02:34:10 +0800

    feat(贴吧极速版): 首页-信息流广告 (#1072)
    close #1067

---
+++ src/apps/com.baidu.tieba_mini.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.baidu.tieba_mini',
+  name: '贴吧极速版',
+  groups: [
+    {
+      key: 1,
+      name: '首页-信息流广告',
+      activityIds: ['com.baidu.tieba.tblauncher.MainTabActivity'],
+      rules: [
+        {
+          key: 0,
+          matches: 'LinearLayout > [text="广告"] + ImageView',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12905039',
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          matches: '[text="屏蔽广告"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12904633'],
+        },
+      ],
+    },
+  ],
+});
