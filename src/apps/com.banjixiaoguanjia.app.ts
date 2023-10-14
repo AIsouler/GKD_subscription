df81fc8f097aaae997deb6e10411b43afbdecc07
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-15 02:32:43 +0800

    feat(班级小管家) 开屏广告,应用内广告卡片 (#1071)
    * feat 开屏广告
    
    * feat 应用内广告卡片
    
    close #1069

---
+++ src/apps/com.banjixiaoguanjia.app.ts
@@ -0,0 +1,35 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.banjixiaoguanjia.app',
+  name: '班级小管家',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.zero.flutter_gromore_ads.page.AdSplashActivity',
+      matchLauncher: true,
+      quickFind: true,
+      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12904614',
+    },
+    {
+      key: 1,
+      name: '应用内广告卡片',
+      activityIds: 'com.banjixiaoguanjia.app.MainActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '[id="com.byted.pangle:id/tt_bu_close"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12904612',
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          matches: '@LinearLayout > [text="不感兴趣"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12906196',
+        },
+      ],
+    },
+  ],
+});
