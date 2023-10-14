ec4ed04231c8a9285c3f3e17cd791ce2f924a115
CallMeBill <callmebill@billw.cn>
2023-10-15 02:31:09 +0800

    feat(维护云): 开屏广告 (#1064)
    close #1063

---
+++ src/apps/com.shunwang.weihuyun.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.shunwang.weihuyun',
+  name: '维护云',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['com.shunwang.maintaincloud.welcome.SplashActivity'],
+      rules: [
+        {
+          matches: '[id="com.shunwang.weihuyun:id/tv_jump"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12903874'],
+        },
+      ],
+    },
+  ],
+});
