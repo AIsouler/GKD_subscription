d5ba29987e6dfe3d6a3b4b059f93fdd4dcf39fef
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-15 02:23:53 +0800

    feat(胖乖生活): 开屏广告 (#1056)
    close #1033

---
+++ src/apps/com.qiekj.user.ts
@@ -0,0 +1,34 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.qiekj.user',
+  name: '胖乖生活',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.qiekj.user.MainActivity',
+      matchLauncher: true,
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[id="com.qiekj.user:id/native_splash_view"] >n [text^="跳过"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12903088',
+        },
+        {
+          key: 1,
+          matches:
+            '[id="com.kwad.dy.sdk:id/ksad_splash_root_container"] >n [text="跳过"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12903086',
+        },
+        {
+          key: 2,
+          matches:
+            '[id="com.kwad.dy.sdk:id/ksad_splash_root_container"] >n ViewGroup > TextView + TextView + ImageView[clickable=true]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12903095',
+        },
+      ],
+    },
+  ],
+});
