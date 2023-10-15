b709f9f94a7577276dd795e46a51237781745510
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-16 00:41:07 +0800

    feat(Pods King): 开屏广告 (#1089)
    close #1083

---
+++ src/apps/com.msnothing.airpodsking.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.msnothing.airpodsking',
+  name: 'Pods King',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.msnothing.ad.splash.HomeSplashActivity',
+      matchLauncher: true,
+      quickFind: true,
+      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12908449',
+    },
+  ],
+});
