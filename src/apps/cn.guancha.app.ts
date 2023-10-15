f355820686ca53875108c4cfeac18b32a4357a0f
walklty <47232102+walklty@users.noreply.github.com>
2023-10-16 00:32:31 +0800

    feat(观察者网): 开屏广告 (#1078)
    close #1075

---
+++ src/apps/cn.guancha.app.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.guancha.app',
+  name: '观察者',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      desc: '点击开屏右上角“跳过”按钮',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: 'cn.guancha.app.ui.activity.WelcomeActivity',
+      rules: [
+        {
+          matches: 'TextView[id="cn.guancha.app:id/tv_jump_btn"][text="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12907063',
+        },
+      ],
+    },
+  ],
+});
