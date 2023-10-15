1665e00a161ec186d4a97dc1d601f9108cb835ec
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-16 00:47:44 +0800

    feat(必剪): 通知权限请求规则优化 (#1094)

--- src/apps/com.bilibili.studio.ts
+++ src/apps/com.bilibili.studio.ts
@@ -7,10 +7,24 @@ export default defineAppConfig({
     {
       enable: false,
       key: 0,
-      name: '首页-开启通知权限请求',
+      name: '通知权限请求',
+      desc: '会出现在创作、模板、我的页面，自动点击不开启。',
+      quickFind: true,
       activityIds: 'com.bcut.homepage.widget.MainActivity',
-      rules: 'TextView[text="开启设置"] - TextView[text="取消"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12867401',
+      rules: [
+        {
+          matches: 'TextView[text="开启设置"] - TextView[text="取消"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12867401', // 创作页面
+        },
+        {
+          matches: 'ImageView[id="com.bilibili.studio:id/iv_close"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12908880', // 模板页面
+        },
+        {
+          matches: 'ImageView[id="com.bilibili.studio:id/iv_push_tips_close"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12908886', // 我的页面
+        },
+      ],
     },
   ],
 });
