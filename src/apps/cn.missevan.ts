8b4c28ac6e61bef3c1dff3d31883b55019398ba1
walklty <47232102+walklty@users.noreply.github.com>
2023-10-16 00:33:48 +0800

    feeat(猫耳FM): 开屏广告,青少年弹窗 (#1082)
    * Create cn.missevan.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1081

---
+++ src/apps/cn.missevan.ts
@@ -0,0 +1,35 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.missevan',
+  name: '猫耳FM',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      desc: '点击：跳过',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: 'cn.missevan.activity.MainActivity',
+      rules: [
+        {
+          matches: '[id="cn.missevan:id/skip_title"][text="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12908434',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '青少年提示',
+      desc: '点击：知道了',
+      activityIds: 'cn.missevan.activity.MainActivity',
+      quickFind: true,
+      rules: [
+        {
+          matches: '[id="cn.missevan:id/iKnowTextView"][text="我知道了"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12908433',
+        },
+      ],
+    },
+  ],
+});
