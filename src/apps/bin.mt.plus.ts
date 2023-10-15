40630165decd3ae5098f374e3c0448b50392ba69
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-16 00:43:16 +0800

    feat(MT管理器): 更新弹窗 (#1092)

---
+++ src/apps/bin.mt.plus.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'bin.mt.plus',
+  name: 'MT管理器',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'bin.mt.plus.Main',
+      rules: '@[text="取消"] + [text="更新"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12908784',
+    },
+  ],
+});
