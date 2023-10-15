58da3fb073681227d43a03649180e1c37d45d2a5
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-16 00:42:06 +0800

    feat(QQ浏览器): 小说阅读页面广告 (#1090)
    * feat 小说阅读页面广告
    
    * feat 小说阅读页面-视频广告
    
    close #1077

--- src/apps/com.tencent.mtt.ts
+++ src/apps/com.tencent.mtt.ts
@@ -16,5 +16,56 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 10,
+      name: '小说阅读页面-卡片广告',
+      activityIds: 'com.tencent.mtt.MainActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '@ViewGroup[visibleToUser=true] > ViewGroup > [desc="广告"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12907446',
+            'https://gkd-kit.gitee.io/import/12907445', // 限定 visibleToUser=true，防止在节点不可见时触发规则
+          ],
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          matches: '@ViewGroup > [desc="屏蔽此广告"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12907654',
+            'https://gkd-kit.gitee.io/import/12907651',
+          ],
+        },
+        {
+          preKeys: 1,
+          key: 2,
+          matches: '@ViewGroup > [desc="不感兴趣"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12907655',
+            'https://gkd-kit.gitee.io/import/12907653',
+          ],
+        },
+      ],
+    },
+    {
+      key: 11,
+      name: '小说阅读页面-视频广告',
+      activityIds: 'com.qq.e.tg.RewardvideoPortraitADActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '[text="腾讯广告"] >n [id^="button_close"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12909822',
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          matches: '[id="com.tencent.mtt:id/reward_dialog_close"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12908955',
+        },
+      ],
+    },
   ],
 });
