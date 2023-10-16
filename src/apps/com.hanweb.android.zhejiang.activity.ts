67fa53a265500d62579f1917b8d18063a10573a6
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-17 00:08:20 +0800

    feat(浙里办): 开屏广告 (#1128)
    * feat(浙里办): 【开屏广告】
    
    * chore(actions): check_format_lint
    
    * Update com.hanweb.android.zhejiang.activity.ts
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1126

---
+++ src/apps/com.hanweb.android.zhejiang.activity.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.hanweb.android.zhejiang.activity',
+  name: '浙里办',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds:
+        'com.alibaba.gov.android.welcome.activity.DefaultWelcomeActivity',
+      rules:
+        '[id="com.hanweb.android.zhejiang.activity:id/gov_splash_tv_count_down"][text ^= "跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12913138',
+    },
+  ],
+});
