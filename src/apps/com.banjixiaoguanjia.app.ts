dd74a992638310f855c9f1ec74152998486b94e9
二刺螈 <i@songe.li>
2023-10-17 00:47:22 +0800

    feat(班级小管家): 开屏广告-规则优化 (#1088)

--- src/apps/com.banjixiaoguanjia.app.ts
+++ src/apps/com.banjixiaoguanjia.app.ts
@@ -9,7 +9,6 @@ export default defineAppConfig({
       name: '开屏广告',
       activityIds: 'com.zero.flutter_gromore_ads.page.AdSplashActivity',
       matchLauncher: true,
-      quickFind: true,
       rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12904614',
     },
