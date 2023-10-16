9389fdcc1f4e70c9146aace8ed31535060509c67
walklty <47232102+walklty@users.noreply.github.com>
2023-10-17 00:18:48 +0800

    feat(荣耀智慧空间): 关闭更新提示 (#1140)
    close #1138

--- src/apps/com.hihonor.magichome.ts
+++ src/apps/com.hihonor.magichome.ts
@@ -21,5 +21,15 @@ export default defineAppConfig({
         '[text$="推荐服务声明"] < LinearLayout < LinearLayout + LinearLayout > [id="com.hihonor.magichome:id/negative_btn"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12843976',
     },
+    {
+      key: 12,
+      name: '升级提示',
+      desc: '点击以后再说',
+      activityIds: 'com.hihonor.magichome.business.MainActivity',
+      quickFind: true,
+      rules:
+        'TextView[text="立即更新"] -2 [id="com.hihonor.magichome:id/negative_btn"][text="以后再说"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12916700',
+    },
   ],
 });
