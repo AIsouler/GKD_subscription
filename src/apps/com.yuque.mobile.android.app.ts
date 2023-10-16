c711320dcfb1ccefa6fb5da409c12a0f4fd0835f
walklty <47232102+walklty@users.noreply.github.com>
2023-10-16 23:54:52 +0800

    feat(语雀): 关闭横幅提示, 自动原图(默认关闭) (#1113)
    * Create com.yuque.mobile.android.app.ts
    
    * chore(actions): check_format_lint
    
    * feat(语雀): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.yuque.mobile.android.app.ts
@@ -0,0 +1,35 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.yuque.mobile.android.app',
+  name: '语雀',
+  groups: [
+    {
+      key: 0,
+      name: '横幅提示',
+      desc: '点x取消提示',
+      activityIds:
+        'com.yuque.mobile.android.app.rn.activity.ReactNativeMainActivity',
+      rules: [
+        {
+          matches: 'TextView[text="了解更多"] - ViewGroup[clickable=true]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12911014',
+        },
+      ],
+    },
+    {
+      enable: false,
+      key: 1,
+      name: '自动原图',
+      desc: '如果没有勾选原图, 则勾选',
+      activityIds:
+        'com.yuque.mobile.android.app.rn.activity.ReactNativeMainActivity',
+      rules: [
+        {
+          matches: 'ViewGroup[desc="原图未勾选"][clickable=true][childCount=2]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12911013',
+        },
+      ],
+    },
+  ],
+});
