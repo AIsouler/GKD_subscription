b29a7fa4f5df6d934555d5f216e9ebb1bc6e4ec2
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-16 23:45:50 +0800

    feat(心动日常): 开屏广告 (#1111)
    * feat(心动日常): 开屏广告
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1107

---
+++ src/apps/com.youloft.icloser.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.youloft.icloser',
+  name: '心动日常',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.youloft.icloser.splash.AdSplashActivity',
+      rules:
+        '[id="com.youloft.icloser:id/sdk_container"] TextView[text="跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12910405',
+    },
+  ],
+});
