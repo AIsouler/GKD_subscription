87341d56ebb203f9983499f7a82333eebd669b00
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-17 00:02:27 +0800

    feat(小周便签) 开屏广告-规则优化 (#1121)
    * 新创建 App 规则
    
    * feat 开屏广告（补充 activityId）
    
    * fix app 名称错误

--- src/apps/me.zhouzhuo810.zznote.ts
+++ src/apps/me.zhouzhuo810.zznote.ts
@@ -2,15 +2,21 @@ import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
   id: 'me.zhouzhuo810.zznote',
-  name: '上海交警',
+  name: '小周便签',
   groups: [
     {
       key: 0,
-      name: '小周便签',
-      activityIds: 'me.zhouzhuo810.zznote.view.act.QiDongActivity',
+      name: '开屏广告',
+      activityIds: [
+        'me.zhouzhuo810.zznote.view.act.QiDongActivity',
+        'me.zhouzhuo810.zznote.view.act.edit.immersive.SpanPicWordDetailImmersiveActivity',
+      ],
       rules:
         '[id="me.zhouzhuo810.zznote:id/native_ad_container"] + [id^="me.zhouzhuo810.zznote:id/btn_next"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12798528',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12798528',
+        'https://gkd-kit.songe.li/import/12912217',
+      ],
     },
   ],
 });
