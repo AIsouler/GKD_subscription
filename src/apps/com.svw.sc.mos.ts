c37a6e2faef7db8966de1cae9fec2d6473d332c2
walklty <47232102+walklty@users.noreply.github.com>
2023-10-17 00:03:10 +0800

    feat(上汽大众): 开屏广告 (#1123)
    close #1122

---
+++ src/apps/com.svw.sc.mos.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.svw.sc.mos',
+  name: '上汽大众',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      desc: '点击"跳过广告"',
+      matchLauncher: true,
+      quickFind: false,
+      activityIds: 'com.svw.sc.mos.views.SplashActivity',
+      rules: [
+        {
+          matches: '[id="com.svw.sc.mos:id/launch_jump"][text^="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12912850',
+        },
+      ],
+    },
+  ],
+});
