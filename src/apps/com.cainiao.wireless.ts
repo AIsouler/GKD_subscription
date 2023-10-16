10868e15a63c97fd02c6fa89357de6df78224282
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-17 00:12:01 +0800

    feat(菜鸟): 包裹页面-弹窗广告、包裹页面-地图底部横条广告 (#1134)
    * feat(菜鸟): 包裹页面-弹窗广告、包裹页面-地图底部横条广告
    
    * fix(菜鸟): 更正包裹页面-地图底部横条广告快照

--- src/apps/com.cainiao.wireless.ts
+++ src/apps/com.cainiao.wireless.ts
@@ -19,5 +19,24 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '包裹页面-弹窗广告',
+      quickFind: true,
+      activityIds: [
+        'com.taobao.cainiao.logistic.ui.view.LogisticDetailActivity',
+      ],
+      rules: 'RelativeLayout > ImageView[desc="关闭"]',
+      snapshotUrls: ['https://gkd-kit.songe.li/import/12914371'],
+    },
+    {
+      key: 2,
+      name: '包裹页面-地图底部横条广告',
+      activityIds: [
+        'com.taobao.cainiao.logistic.ui.view.LogisticDetailActivity',
+      ],
+      rules: 'View[desc="立即查看"] +2 ImageView[id=null]',
+      snapshotUrls: ['https://gkd-kit.songe.li/import/12914450'],
+    },
   ],
 });
