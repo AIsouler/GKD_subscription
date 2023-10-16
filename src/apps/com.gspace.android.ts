2978c3644f60d89fe0f87e54a8a696092bdc2b9f
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-17 00:07:30 +0800

    feat(gspace): 开屏广告,主页面广告-规则优化 (#1124)
    * feat(Gspace): 规则优化
    
    - 【跳过开屏广告】 - 规则优化
    - 【主页面上方广告】 - 规则优化
    
    * Update com.gspace.android.ts
    
    * Update com.gspace.android.ts
    
    * chore(actions): check_format_lint
    
    * Update com.gspace.android.ts
    
    * chore(actions): check_format_lint
    
    * feat(Gspace): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>
    
    close #1109

--- src/apps/com.gspace.android.ts
+++ src/apps/com.gspace.android.ts
@@ -7,24 +7,55 @@ export default defineAppConfig({
     {
       key: 0,
       name: '跳过开屏广告',
+      matchLauncher: true,
       activityIds: [
         'com.gspace.android.ui.activity.SplashActivity', // Gspace主应用开屏
         'com.gspace.android.util.ShortcutHandleActivity', // Gspace子应用桌面快捷方式开屏
         'com.gspace.android.ui.activity.VappLoadingActivity', // Gspace子应用开屏
       ],
-      rules: '[id="com.gspace.android:id/hiad_skip_text"]',
-      snapshotUrls: [
-        'https://gkd-kit.gitee.io/import/12668986',
-        'https://gkd-kit.gitee.io/import/12668985',
-        'https://gkd-kit.gitee.io/import/12703037',
+      rules: [
+        {
+          matches: '[id="com.gspace.android:id/hiad_skip_text"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12668986',
+            'https://gkd-kit.gitee.io/import/12668985',
+            'https://gkd-kit.gitee.io/import/12703037',
+          ],
+        },
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12910422',
+            'https://gkd-kit.gitee.io/import/12910417',
+          ],
+        },
       ],
     },
     {
       key: 1,
       name: '主页面上方广告',
-      activityIds: 'com.gspace.android.ui.activity.main.MainActivity',
-      rules: '[id="com.gspace.android:id/hiad_banner_close_button"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12705339',
+      matchLauncher: true,
+      activityIds: ['com.gspace.android.ui.activity.main.MainActivity'],
+      rules: [
+        {
+          key: 0,
+          matches: '[id="com.gspace.android:id/hiad_banner_close_button"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12705339',
+        },
+        {
+          key: 1,
+          matches: '@Image < View -2 View > View > View TextView[text="广告"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12910419', //activityIds: 'com.gspace.android.ui.activity.main.MainActivity',
+            'https://gkd-kit.gitee.io/import/12910935', //activityIds: 'com.huawei.android.launcher.unihome.UniHomeLauncher',
+          ],
+        },
+        {
+          preKeys: 1,
+          matches: '[id="com.byted.pangle:id/tt_item_tv" && text= "不感兴趣"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12910420',
+        },
+      ],
     },
     {
       key: 2,
