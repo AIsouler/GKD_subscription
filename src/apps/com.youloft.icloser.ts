2e1e003e8b19ff82c41f171db2b1108046db572e
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-17 00:29:35 +0800

    feat(心动日常): 开屏广告 (#1145)
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/com.youloft.icloser.ts
+++ src/apps/com.youloft.icloser.ts
@@ -8,6 +8,7 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.youloft.icloser.splash.AdSplashActivity',
+      matchLauncher: true,
       rules:
         '[id="com.youloft.icloser:id/sdk_container"] TextView[text="跳过"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12910405',
