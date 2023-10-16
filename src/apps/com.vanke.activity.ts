c771d6a5a679ae5d2b2f46168aaba1a32a106c2e
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-17 00:21:57 +0800

    feat(住这儿): 开屏广告 (#1142)
    * feat(住这儿): 开屏广告
    
    * chore(actions): check_format_lint
    
    * feat(住这儿): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>
    
    close #1141

---
+++ src/apps/com.vanke.activity.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.vanke.activity',
+  name: '住这儿',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      activityIds: 'com.vanke.activity.module.common.SplashNewAct',
+      rules: 'Button[text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12917007',
+    },
+  ],
+});
