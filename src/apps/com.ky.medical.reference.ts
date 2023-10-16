c58c3b5926367916b3478fb0992b535e52806c30
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-17 00:33:11 +0800

    feat(用药参考): 开屏广告 (#1154)
    * feat(用药参考): 开屏广告
    
    * chore(actions): check_format_lint
    
    * Update com.ky.medical.reference.ts
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1146

--- src/apps/com.ky.medical.reference.ts
+++ src/apps/com.ky.medical.reference.ts
@@ -5,6 +5,13 @@ export default defineAppConfig({
   name: '用药参考',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      activityIds: 'com.ky.medical.reference.activity.InitActivity',
+      rules: '[id="com.ky.medical.reference:id/btn_go"][text^="关闭"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12918049',
+    },
+    {
       key: 0,
       name: '弹窗广告',
       activityIds: 'com.ky.medical.reference.promotion.AdDialogActivity',
