305ef4d9f2b265ee959dfe4dd6890eec6a4601c7
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-17 00:32:25 +0800

    feat(酷狗概念版): 开屏广告 (#1155)

---
+++ src/apps/com.kugou.android.lite.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.kugou.android.lite',
+  name: '酷狗概念版',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      activityIds:
+        'com.kugou.android.app.splash.adcontainer.AdContainerActivity',
+      rules: '@[desc="跳过"] - FrameLayout >n [text="互动广告"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12919282',
+    },
+  ],
+});
