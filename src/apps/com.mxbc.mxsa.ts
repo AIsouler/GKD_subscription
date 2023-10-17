16f561d4d06f3fe7136ddaec5cdd2c75650c1b3e
Adam <3246057843@qq.com>
2023-10-18 00:25:33 +0800

    feat(蜜雪冰城): 主界面弹窗广告 (#1179)

--- src/apps/com.mxbc.mxsa.ts
+++ src/apps/com.mxbc.mxsa.ts
@@ -10,8 +10,21 @@ export default defineAppConfig({
       activityIds: ['com.mxbc.mxsa.modules.splash.SplashActivity'],
       rules: [
         {
+          key: 1,
           matches: '[id="com.mxbc.mxsa:id/count_down"]',
           snapshotUrls: 'https://gkd-kit.songe.li/import/12840093',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '主界面弹窗广告',
+      activityIds: ['com.mxbc.mxsa.modules.main.MainActivity'],
+      rules: [
+        {
+          key: 1,
+          matches: '[id="com.mxbc.mxsa:id/close"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12929345',
         },
       ],
     },
