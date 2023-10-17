b4ded34fa65c756a24b3555ecf0fb092bda1c4ea
walklty <47232102+walklty@users.noreply.github.com>
2023-10-18 00:05:24 +0800

    feat(瑞幸咖啡): 超值换购, quickFind, matchLauncher (#1157)
    * feat(瑞幸咖啡): 超值换购, 增加quickFind和matchLauncher
    
    * feat(瑞幸咖啡): 超值换购, quickFind, matchLauncher

--- src/apps/com.lucky.luckyclient.ts
+++ src/apps/com.lucky.luckyclient.ts
@@ -8,6 +8,8 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.luckin.client.main.FirstActivity',
+      matchLauncher: true,
+      quickFind: true,
       rules: '[id="com.lucky.luckyclient:id/tv_skip"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/12508764',
     },
@@ -18,6 +20,16 @@ export default defineAppConfig({
       rules:
         '@ImageView[id="com.lucky.luckyclient:id/dismiss_btn"] - LinearLayout > ImageView[id="com.lucky.luckyclient:id/update_btn"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/12846499',
+    },
+    {
+      key: 2,
+      name: '超值换购',
+      desc: '关闭超值换购弹窗',
+      enable: false,
+      activityIds: 'com.lucky.luckyclient.preview2.OrderPreviewActivity2',
+      quickFind: true,
+      rules: '[id="com.lucky.luckyclient:id/close_iv"][visibleToUser=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12922834',
     },
   ],
 });
