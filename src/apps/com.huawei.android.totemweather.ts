9ed4d404ec0ccad2c3481d429bb721c911d0f4b8
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-19 00:46:20 +0800

    feat(华为天气): 开屏广告-规则优化 (#1204)
    close #1178

--- src/apps/com.huawei.android.totemweather.ts
+++ src/apps/com.huawei.android.totemweather.ts
@@ -9,8 +9,13 @@ export default defineAppConfig({
       name: '开屏广告',
       activityIds: 'com.huawei.android.totemweather.WeatherHome',
       matchLauncher: true,
-      rules: '[id="com.huawei.hms.ads.uiengine:id/hiad_skip_text"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12928975',
+      rules: [
+        {
+          matches: '[id="com.huawei.hms.ads.uiengine:id/hiad_skip_text"]',
+          action: 'clickCenter', // 根据 https://github.com/gkd-kit/subscription/pull/1180#issuecomment-1767735683 反馈，此处使用 clickCenter 事件。
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12928975',
+        },
+      ],
     },
   ],
 });
