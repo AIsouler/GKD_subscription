05b759a64457d4e1fd10ae05f97cb79259f20987
walklty <47232102+walklty@users.noreply.github.com>
2023-10-19 00:46:54 +0800

    feat(天天跳绳): 开屏广告-规则优化 (#1196)
    * Create com.gkid.crazyrope.ts
    
    * feat(天天跳绳): 规则优化
    
    * feat(天天跳绳): 新的开屏广告id
    
    * feat(天天跳绳): 新的开屏广告id
    
    * Update com.gkid.crazyrope.ts
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/com.gkid.crazyrope.ts
+++ src/apps/com.gkid.crazyrope.ts
@@ -9,11 +9,22 @@ export default defineAppConfig({
       name: '开屏广告',
       matchLauncher: true,
       quickFind: true,
-      activityIds: 'com.gkid.crazyrope.ui.splash.StartPageActivity',
+      activityIds: [
+        'com.gkid.crazyrope.ui.splash.StartPageActivity',
+        'com.gkid.crazyrope.ui.splash.SplashActivity',
+      ],
       rules: [
         {
           matches: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12916417',
+        },
+        {
+          matches: '[id="com.gkid.crazyrope:id/splash_tv_skip"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12985488',
+        },
+        {
+          matches: 'LinearLayout[childCount=2] > [text^="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13028151',
         },
       ],
     },
