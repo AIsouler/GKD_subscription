7942888958e255333594989267f9589002f7db96
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-19 00:53:36 +0800

    feat(中国移动河南): 开屏广告、应用内广告弹窗、右侧悬浮广告 (#1219)
    * feat: 开屏广告
    
    * feat: 应用内广告弹窗
    
    * feat: 右侧悬浮广告
    
    close #1218

---
+++ src/apps/com.xinhang.mobileclient.ts
@@ -0,0 +1,45 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xinhang.mobileclient',
+  name: '中国移动河南',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds:
+        'com.xinhang.mobileclient.launcher.activity.LauncherActivity',
+      matchLauncher: true,
+      rules: '[id="com.xinhang.mobileclient:id/cdp_skip"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13024858',
+    },
+    {
+      key: 1,
+      name: '应用内广告弹窗',
+      activityIds: 'com.xinhang.mobileclient.home.activity.HomeActivity',
+      rules: [
+        {
+          key: 0,
+          matches: 'View[desc^="close"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13024867',
+        },
+        {
+          key: 1,
+          matches: 'View > View > TextView + TextView[text=""]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13024869',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '右侧悬浮广告',
+      activityIds: 'com.xinhang.mobileclient.home.activity.HomeActivity',
+      rules:
+        '@[id="com.xinhang.mobileclient:id/iv_close"] + [id="com.xinhang.mobileclient:id/iv_advert"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/13024863',
+        'https://gkd-kit.gitee.io/import/13024872',
+      ],
+    },
+  ],
+});
