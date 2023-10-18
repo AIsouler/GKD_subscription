e1f0cef43de1ab9062f3aec0d60e15dd88b1e717
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-19 00:56:40 +0800

    feat(行者骑行): 开屏广告 (#1226)
    close #1220

---
+++ src/apps/im.xingzhe.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'im.xingzhe',
+  name: '行者骑行',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'im.xingzhe.activity.LaunchActivity',
+      matchLauncher: true,
+      rules: '[id="im.xingzhe:id/count_text"][text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/13031282',
+    },
+  ],
+});
