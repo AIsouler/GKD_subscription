eff19811207ec9f87edd93302b60c6325339cfe0
walklty <47232102+walklty@users.noreply.github.com>
2023-10-19 00:49:54 +0800

    feat(youtube): 规则优化 (#1207)
    * feat(youtube): matchLauncher
    
    * Update com.google.android.youtube.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.google.android.youtube.ts
+++ src/apps/com.google.android.youtube.ts
@@ -7,27 +7,34 @@ export default defineAppConfig({
     {
       key: 0,
       name: '视频播放-跳过广告',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12565261',
       activityIds:
         'com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity',
+      matchLauncher: true,
+      quickFind: true,
       rules: '[id=`com.google.android.youtube:id/skip_ad_button`]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12565261',
+        'https://gkd-kit.gitee.io/import/13024980', // matchLauncher
+      ],
     },
     {
       key: 1,
       name: '视频播放-赞助商广告',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12877346',
       activityIds:
         'com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity',
+      matchLauncher: true,
       rules: 'ImageView[desc="更多选项"] + @ImageView[desc="关闭广告面板"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12877346',
     },
     {
       key: 2,
       name: '首页-会员广告',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12877357',
       activityIds:
         'com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity',
+      matchLauncher: true,
       rules:
         'ImageView < @Button < ViewGroup +3 Button[desc*="YouTube Premium"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12877357',
     },
   ],
 });
