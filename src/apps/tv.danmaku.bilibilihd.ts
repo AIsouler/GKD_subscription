9981c3ffc0b7b6ff763daea6d5f3b3a7ed55821b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-19 00:55:38 +0800

    feat(哔哩哔哩HD): 开屏广告 (#1224)
    close #1220

---
+++ src/apps/tv.danmaku.bilibilihd.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'tv.danmaku.bilibilihd',
+  name: '哔哩哔哩HD',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'tv.danmaku.bili.MainActivityV2',
+      matchLauncher: true,
+      rules: '[id="tv.danmaku.bilibilihd:id/count_down"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/13031261',
+    },
+  ],
+});
