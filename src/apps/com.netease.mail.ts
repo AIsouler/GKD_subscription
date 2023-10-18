c9a0ea0ee0a19b20f732e875112a5305a762ad48
lliiooll <44719954+lliioollcn@users.noreply.github.com>
2023-10-19 00:50:51 +0800

    feat(网易邮箱大师): 规则优化 (#1212)
    * feat(网易邮箱大师): 开屏广告-规则优化
    
    * feat(网易邮箱大师): 邮件列表广告
    
    close #1188

--- src/apps/com.netease.mail.ts
+++ src/apps/com.netease.mail.ts
@@ -12,6 +12,8 @@ export default defineAppConfig({
       activityIds: [
         'com.netease.mail.biz.main.MainITabActivity',
         'com.netease.mail.biz.main.SplashActivity',
+        'com.netease.mail.ad.launch.ui.SplashAdActivity',
+        'com.netease.mail.biz.main.MainITabActivity',
       ],
       rules: [
         {
@@ -28,6 +30,11 @@ export default defineAppConfig({
             'https://gkd-kit.songe.li/import/12923776', // com.netease.mail.biz.main.SplashActivity
           ],
         },
+        {
+          key: 2,
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12999739',
+        },
       ],
     },
     {
@@ -38,6 +45,24 @@ export default defineAppConfig({
       ],
       rules:
         '[id="com.netease.mail:id/tv_ignore_this_version"][text="暂不更新"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12664070',
+    },
+    {
+      key: 2,
+      name: '邮件列表广告',
+      activityIds: ['com.netease.mail.biz.main.MainITabActivity'],
+      rules: [
+        {
+          key: 0,
+          matches: '[id="com.netease.mail:id/ad_arrow"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12999833',
+        },
+        {
+          key: 1,
+          matches: '[id="com.netease.mail:id/ll_delete"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12999841',
+        },
+      ],
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12664070',
     },
   ],
