bb51eb117356f94a8f94958bec46379085e3127c
董红鹏 <1140049363@qq.com>
2023-10-19 00:52:20 +0800

    feat(喵上漫画): 增加跳过开屏广告 (#1215)
    * fix(喵上漫画):增加跳过开屏广告
    
    1.跳过开屏广告
    2.应用内广告增加规则
    
    * Update com.aster.zhbj.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.aster.zhbj.ts
+++ src/apps/com.aster.zhbj.ts
@@ -5,6 +5,24 @@ export default defineAppConfig({
   name: '喵上漫画',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      desc: '点击右上角跳过',
+      rules: [
+        {
+          activityIds: [
+            'com.aster.comic.app.ad.view.mediation.MediationSplashAdActivity',
+            'com.aster.comic.app.ad.view.mediation.MediationAppResumeSplashAdActivity',
+          ],
+          matches: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12981243',
+            'https://gkd-kit.gitee.io/import/13029855',
+          ],
+        },
+      ],
+    },
+    {
       key: 1,
       name: '应用内广告弹窗',
       desc: '点击右上角x',
@@ -21,8 +39,8 @@ export default defineAppConfig({
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Activity_T',
           matches:
-            'ImageView - FrameLayout > FrameLayout > FrameLayout > ImageView',
-          snapshotUrls: 'https://gkd-kit.songe.li/import/12777329',
+            'FrameLayout[childCount=5] > FrameLayout[childCount=1] > ImageView',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13029880',
         },
         {
           key: 2,
@@ -31,21 +49,35 @@ export default defineAppConfig({
             'ImageView - LinearLayout - FrameLayout > FrameLayout > ImageView',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12872249',
         },
-
         // 字节广告
         {
-          key: 10,
+          key: 3,
           activityIds: 'com.aster.comic.app.view.MainActivity',
           matches: '[desc^="webview-close"] > View',
           snapshotUrls: 'https://gkd-kit.songe.li/import/12903062',
+        },
+        {
+          key: 4,
+          activityIds: [
+            'com.aster.comic.app.view.MainActivity',
+            'ImageView - FrameLayout > FrameLayout > ImageView',
+          ],
+          matches: 'ImageView - FrameLayout > FrameLayout > ImageView',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12984767',
+            'https://gkd-kit.gitee.io/import/12998908',
+          ],
         },
         {
           key: 11,
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
           matches:
-            '@Image < View + View +n View > View > TextView[text="广告"]',
-          snapshotUrls: 'https://gkd-kit.songe.li/import/12903072',
+            '@Image[text=""] < View + View +n View > View > TextView[text="广告"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12996953',
+            'https://gkd-kit.gitee.io/import/13003644',
+          ],
         },
       ],
     },
