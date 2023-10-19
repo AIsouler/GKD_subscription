3f54aafd63e9a4d9f37a5072a616d26ed5fe3f55
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-20 00:41:27 +0800

    feat(作业帮家长版): 赏好评弹窗 (#1245)

---
+++ src/apps/com.zuoyebang.knowledge.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.zuoyebang.knowledge',
+  name: '作业帮家长版',
+  groups: [
+    {
+      key: 3,
+      name: '赏好评弹窗',
+      quickFind: true,
+      activityIds: 'com.baidu.homework.activity.init.InitActivity',
+      rules: 'TextView[id="com.zuoyebang.knowledge:id/btn_cancle"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/13043228',
+    },
+  ],
+});
