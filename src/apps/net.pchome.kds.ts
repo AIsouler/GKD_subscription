0baca1450b015c9cd4fd5b8ec7613c5f188e194b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-20 00:41:06 +0800

    feat(kds宽带山): 应用内广告卡片 (#1244)
    close #1231

---
+++ src/apps/net.pchome.kds.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'net.pchome.kds',
+  name: 'kds宽带山',
+  groups: [
+    {
+      key: 1,
+      name: '应用内广告卡片',
+      activityIds: 'net.pchome.kds.View.activity.TopicActivity2',
+      rules: [
+        {
+          key: 0,
+          matches: '@Image[text=""] < View +2 View > View > View > TextView',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/13043221',
+        },
+      ],
+    },
+  ],
+});
