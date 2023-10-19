135360830cdfccbbe983e4be5b915efaa0cac088
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-20 00:29:16 +0800

    feat(盯盯拍): 开屏广告 (#1240)
    * feat(盯盯拍): 开屏广告(gkd-kit#1060)
    
    * feat(qq邮箱): 开启通知提醒
    
    * Revert "feat(qq邮箱): 开启通知提醒"
    
    This reverts commit 0cd22f0dee89ee4845791aca615d532491ea46e2.
    
    close #1060

---
+++ src/apps/com.vyou.vcameraclient.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.vyou.vcameraclient',
+  name: '盯盯拍',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      // quickFind: true, 开启后无法跳过
+      activityIds: 'com.vyou.app.ui.activity.SplashActivity',
+      rules: [
+        {
+          matches: 'View[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
+          snapshotUrls: [
+            'https://gkd-kit.songe.li/import/13042916',
+            'https://gkd-kit.songe.li/import/13042963',
+          ],
+        },
+      ],
+    },
+  ],
+});
