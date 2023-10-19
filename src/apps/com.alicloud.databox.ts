36ef78bef2573bfec5b3abb45a70d47dbddbb3c2
Adam <3246057843@qq.com>
2023-10-20 00:27:22 +0800

    feat(阿里网盘): 自动签到-规则优化 (#1235)

--- src/apps/com.alicloud.databox.ts
+++ src/apps/com.alicloud.databox.ts
@@ -8,8 +8,23 @@ export default defineAppConfig({
       key: 0,
       name: '自动签到',
       activityIds: 'com.alicloud.databox.MainActivity',
-      rules: '[id="com.alicloud.databox:id/tvTaskAction"][text="领取"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12929318',
+      rules: [
+        {
+          key: 0,
+          desc: '自动签到',
+          matches: ['[id="com.alicloud.databox:id/tvTaskAction"][text="领取"]'],
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12929318',
+          quickFind: true,
+        },
+        {
+          key: 1,
+          preKeys: [0],
+          desc: '在签到后，关闭弹窗',
+          matches: ['[id="com.alicloud.databox:id/ivClose"]'],
+          snapshotUrls: 'https://gkd-kit.songe.li/import/13038304',
+          quickFind: true,
+        },
+      ],
     },
   ],
 });
