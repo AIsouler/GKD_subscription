fab2263d09d3c11e0c40f7ac83cdd709598dba3d
walklty <47232102+walklty@users.noreply.github.com>
2023-10-21 23:05:01 +0800

    feat(闲鱼): 开屏广告-规则优化 (#1251)

--- src/apps/com.taobao.idlefish.ts
+++ src/apps/com.taobao.idlefish.ts
@@ -7,9 +7,19 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.taobao.idlefish.maincontainer.activity.MainActivity',
-      rules: '[id="com.taobao.idlefish:id/advert_close"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12668670',
+      activityIds: [
+        'com.taobao.idlefish.maincontainer.activity.MainActivity',
+        'com.taobao.fleamarket.home.activity.InitActivity',
+      ],
+      rules: [
+        {
+          matches: '[id="com.taobao.idlefish:id/advert_close"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12668670', //com.taobao.idlefish.maincontainer.activity.MainActivity
+            'https://gkd-kit.gitee.io/import/13049015', //com.taobao.fleamarket.home.activity.InitActivity
+          ],
+        },
+      ],
     },
   ],
 });
