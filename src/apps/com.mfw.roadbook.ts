d23bac9d821425b67e7d56135882d6689721ef1a
walklty <47232102+walklty@users.noreply.github.com>
2023-10-21 23:15:38 +0800

    feat(马蜂窝): 开屏广告 (#1257)
    * feat(马蜂窝): 开屏广告
    
    * feat(马蜂窝): 开屏广告-规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>
    
    close #1237

---
+++ src/apps/com.mfw.roadbook.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.mfw.roadbook',
+  name: '马蜂窝',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: 'com.mfw.roadbook.business.launch.SplashAdActivity',
+      rules: [
+        {
+          matches: '[id="com.mfw.roadbook:id/skipLayout"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13049046',
+        },
+      ],
+    },
+  ],
+});
