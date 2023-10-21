4561b1a27631eaf4f0865889b17ee486e1014073
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-21 23:59:38 +0800

    feat(我的连云港): 开屏广告 (#1276)
    close #1272

---
+++ src/apps/com.unicde.smartcity_lyg.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.unicde.smartcity_lyg',
+  name: '我的连云港',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.unicde.smartcity.features.welcome.WelcomeActivity',
+      matchLauncher: true,
+      rules: '[id="com.unicde.smartcity_lyg:id/tv_down"][text$="跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13059584',
+    },
+  ],
+});
