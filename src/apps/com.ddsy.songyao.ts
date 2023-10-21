f5939d9f1b2b5d8df108c7151f2d98d33bda5be3
walklty <47232102+walklty@users.noreply.github.com>
2023-10-21 23:04:31 +0800

    feat(叮当快药): 开屏广告, 优惠券提示 (#1250)
    * feat(叮当快药): 开屏广告, 优惠卷提示
    
    * feat(叮当快药): 开屏广告, 优惠卷提示

---
+++ src/apps/com.ddsy.songyao.ts
@@ -0,0 +1,34 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.ddsy.songyao',
+  name: '叮当快药',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: 'com.ddsy.songyao.activity.SplashActivity',
+      rules: [
+        {
+          matches: '[id="com.ddsy.songyao:id/time"][text*="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13048719',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '优惠卷提示',
+      quickFind: true,
+      activityIds: 'com.ddsy.songyao.activity.SplashActivity',
+      rules: [
+        {
+          matches:
+            '[id="com.ddsy.songyao:id/iv_coupon_bottom"] + [id="com.ddsy.songyao:id/iv_close"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13048720',
+        },
+      ],
+    },
+  ],
+});
