4842bcf29f39e202a64d6a548926b224d56859d7
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-22 00:18:31 +0800

    feat(荣耀商城): 应用内广告弹窗 (#1290)
    close #1285

---
+++ src/apps/com.hihonor.vmall.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.hihonor.vmall',
+  name: '荣耀商城',
+  groups: [
+    {
+      key: 1,
+      name: '应用内广告弹窗',
+      activityIds: 'com.vmall.client.base.fragment.VmallWapActivity',
+      rules: '[id="com.hihonor.vmall:id/gift_close_iv"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13060881',
+    },
+  ],
+});
