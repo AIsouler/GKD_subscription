98a2d7ace1259e29d90d40df08d790849e1e74cc
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-23 00:49:31 +0800

    feat(小天才): 开启通知提醒 (#1313)

---
+++ src/apps/com.xtc.watch.ts
@@ -0,0 +1,29 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xtc.watch',
+  name: '小天才',
+  groups: [
+    {
+      key: 3,
+      name: '开启通知提醒',
+      quickFind: true,
+      activityIds: [
+        'com.xtc.widget.phone.popup.activity.CustomActivity13',
+        'com.xtc.watch.view.home.activity.XtcHomeActivity',
+      ],
+      rules: [
+        {
+          matches: [
+            '[id="com.xtc.watch:id/iv_notify_permission_close"]',
+            '[id="com.xtc.watch:id/iv_close"]',
+          ],
+          snapshotUrls: [
+            'https://gkd-kit.songe.li/import/13059965',
+            'https://gkd-kit.songe.li/import/13063274',
+          ],
+        },
+      ],
+    },
+  ],
+});
