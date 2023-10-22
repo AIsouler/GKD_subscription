f5a8e7e093d7ce2ed3878fe6b1b23be84a1bf1e6
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-23 00:44:53 +0800

    feat(华为生活服务): 开屏广告 (#1308)

---
+++ src/apps/com.huawei.lives.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.huawei.lives',
+  name: '华为生活服务',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.huawei.lives.ui.SplashADActivity',
+      matchLauncher: true,
+      rules: '[id="com.huawei.lives:id/hiad_skip_text"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/13063001',
+    },
+  ],
+});
