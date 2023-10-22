1b98eed9ec18f5ddeac94d31e63dba92185f7a7d
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-23 00:51:14 +0800

    feat(智联招聘): 社区-弹窗广告、消息-开启消息通知 (#1315)
    * feat(智联招聘): 社区-弹窗广告、消息-开启消息通知
    
    * fix(智联招聘): 更新快照地址

--- src/apps/com.zhaopin.social.ts
+++ src/apps/com.zhaopin.social.ts
@@ -27,5 +27,23 @@ export default defineAppConfig({
       rules: '[id="com.zhaopin.social:id/rl_banner_close"][clickable=true]',
       snapshotUrls: ['https://gkd-kit.gitee.io/import/12706181'],
     },
+    {
+      key: 3,
+      name: '社区-弹窗广告',
+      activityIds: ['com.zhaopin.social.homepage.ZSC_MainTabActivity'],
+      rules:
+        'ImageView[clickable=true&&focusable=true] < FrameLayout + FrameLayout > ImageView[clickable=false&&focusable=false]',
+      snapshotUrls: 'https://inspect.gkd.li/import/13063442',
+    },
+    {
+      key: 4,
+      name: '消息-开启消息通知',
+      desc: '自动点击关闭',
+      enable: false,
+      quickFind: true,
+      activityIds: ['com.zhaopin.social.homepage.ZSC_MainTabActivity'],
+      rules: '[id="com.zhaopin.social:id/message_pushlayout_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13063427',
+    },
   ],
 });
