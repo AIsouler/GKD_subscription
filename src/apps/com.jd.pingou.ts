e60e87c4c8ace9f4eca2c05280613b1b1bc7b16e
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-23 00:41:52 +0800

    feat(京喜): 开屏广告-规则优化 (#1306)

--- src/apps/com.jd.pingou.ts
+++ src/apps/com.jd.pingou.ts
@@ -7,9 +7,16 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.jd.jdlite.ad.ADActivity',
+      activityIds: [
+        'com.jd.jdlite.ad.ADActivity',
+        'com.jd.jdlite.MainActivity',
+      ],
+      matchLauncher: true,
       rules: '[desc="闪屏广告"] + [text$="跳过"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12727342',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12727342',
+        'https://gkd-kit.songe.li/import/13062974',
+      ],
     },
   ],
 });
