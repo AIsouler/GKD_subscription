b58dfe345fda8d85d639157f14fd20b7df1ba3d3
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-23 00:41:12 +0800

    feat(京喜特价): 开屏广告-规则优化;通知权限弹窗 (#1305)
    * feat: 请求通知权限弹窗
    
    * feat: 开屏广告（补充 activityId）

--- src/apps/com.jd.jdlite.ts
+++ src/apps/com.jd.jdlite.ts
@@ -7,9 +7,16 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.jd.jdlite.ad.ADActivity',
+      activityIds: [
+        'com.jd.jdlite.ad.ADActivity',
+        'com.jd.jdlite.MainActivity',
+      ],
+      matchLauncher: true,
       rules: '[desc="闪屏广告"] + [text$="跳过"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12727385',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12727385',
+        'https://gkd-kit.songe.li/import/13062972',
+      ],
     },
     {
       key: 1,
@@ -17,6 +24,14 @@ export default defineAppConfig({
       activityIds: 'com.jd.jdlite.MainFrameActivity',
       rules: '@[desc="关闭页面"] - FrameLayout >n [text="极速版弹窗"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/12727396',
+    },
+    {
+      key: 10,
+      name: '请求通知权限弹窗',
+      activityIds: 'com.jd.jdlite.MainFrameActivity',
+      rules:
+        '@ImageView[clickable=true] - LinearLayout > [text="开启消息通知"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/13062969',
     },
   ],
 });
