526a5677ea45bc7cc8d461b6c54874843d46352b
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-23 00:40:25 +0800

    feat(GPS工具箱): 开屏广告; 首页占位广告 (#1304)
    - 【开屏广告】
    - 【首页占位广告】
    
    close #1298

---
+++ src/apps/com.leduoworks.gpstoolbox.ts
@@ -0,0 +1,25 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.leduoworks.gpstoolbox',
+  name: 'GPS工具箱',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      activityIds: 'com.leduoworks.gpstoolbox.newest.StarterActivity',
+      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/13062600',
+    },
+    {
+      key: 1,
+      name: '首页占位广告',
+      activityIds: 'com.leduoworks.gpstoolbox.frmt.MainFrmtActivity',
+      matchLauncher: true,
+      quickFind: true,
+      rules: '[id="com.leduoworks.gpstoolbox:id/iv_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13062612',
+    },
+  ],
+});
