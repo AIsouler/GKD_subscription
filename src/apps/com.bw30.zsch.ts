f30b8a9d3fdbe46598d3cb1209568962d7e1a46a
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-24 00:52:54 +0800

    feat(四川航空): 开屏广告(补充activityIds)、升级弹窗 (#1329)

--- src/apps/com.bw30.zsch.ts
+++ src/apps/com.bw30.zsch.ts
@@ -9,12 +9,25 @@ export default defineAppConfig({
       name: '开屏广告',
       matchLauncher: true,
       quickFind: true,
+      activityIds: [
+        'com.bw.zsch.home.PrivacyPolicyActivity',
+        'com.bw.zsch.FirstDataLoadActivity',
+      ],
       rules: 'TextView[id="com.bw30.zsch:id/timer_text"]',
       snapshotUrls: [
         'https://i.gkd.li/import/12882959',
         'https://i.gkd.li/import/12882967',
         'https://i.gkd.li/import/12882991',
       ],
+    },
+    {
+      key: 1,
+      name: '升级弹窗',
+      quickFind: true,
+      enable: false,
+      activityIds: ['com.bw.zsch.MainActivityTab'],
+      rules: '[id="com.bw30.zsch:id/tv_cancel"]',
+      snapshotUrls: ['https://i.gkd.li/import/13068699'],
     },
   ],
 });
