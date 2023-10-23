9ca8955d7c55557bdd6ed90bb67458cab45e0128
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-24 01:08:46 +0800

    feat(心动日常): 开屏广告规则优化、购买会员-弹窗广告 (#1351)
    close #1326

--- src/apps/com.youloft.icloser.ts
+++ src/apps/com.youloft.icloser.ts
@@ -7,11 +7,30 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.youloft.icloser.splash.AdSplashActivity',
+      quickFind: true,
       matchLauncher: true,
-      rules:
-        '[id="com.youloft.icloser:id/sdk_container"] TextView[text="跳过"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12910405',
+      activityIds: ['com.youloft.icloser.splash.AdSplashActivity'],
+      rules: [
+        {
+          matches: [
+            'TextView[text^="跳过"]',
+            '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          ],
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12910405',
+            'https://i.gkd.li/import/13072365',
+          ],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '购买会员-弹窗广告',
+      enable: false,
+      quickFind: true,
+      activityIds: 'com.youloft.icloser.view.dialog.VipRetainPacketDialog',
+      rules: '[id="com.youloft.icloser:id/tv_later_buy"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13067183',
     },
   ],
 });
