181ebc0a0a752b377f769071041139893f09bd08
javabcde <volatile@126.com>
2023-10-24 00:49:25 +0800

    feat(飞猪): 开屏广告 (#1323)
    * feat:新增飞猪开屏去广告
    
    * chore(actions): check_format_lint
    
    * feat(飞猪): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.taobao.trip.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.taobao.trip',
+  name: '飞猪',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      activityIds: ['com.taobao.trip.splash.ui.ImageSplashActivity'],
+      rules: [
+        {
+          matches: '[id="com.taobao.trip:id/splash_biz_skip_ad"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13065579',
+        },
+      ],
+    },
+  ],
+});
