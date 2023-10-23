ed990af31be5f613fd7e525b94f87460c0b6ab10
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-24 01:02:24 +0800

    feat(步道乐跑): 开屏广告、弹窗广告（补充规则） (#1340)
    * feat: 开屏广告
    
    * feat: 弹窗广告（补充规则）

--- src/apps/com.lptiyu.tanke.ts
+++ src/apps/com.lptiyu.tanke.ts
@@ -5,14 +5,33 @@ export default defineAppConfig({
   name: '步道乐跑',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      activityIds: 'com.lptiyu.tanke.activities.splash.SplashActivity',
+      matchLauncher: true,
+      rules:
+        '[id="com.lptiyu.tanke:id/splash_full_tk_play_card_view"] >n [text="跳过"]',
+      snapshotUrls: 'https://i.gkd.li/import/13070420',
+    },
+    {
       key: 0,
       name: '弹窗广告',
       activityIds: 'com.lptiyu.tanke.activities.main.MainActivity',
-      rules:
-        '[id="com.lptiyu.tanke:id/interact_ad_root"] >n [id="com.lptiyu.tanke:id/iv_close"]',
-      snapshotUrls: [
-        'https://gkd-kit.gitee.io/import/12829816',
-        'https://i.gkd.li/import/12869344',
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[id="com.lptiyu.tanke:id/interact_ad_root"] >n [id="com.lptiyu.tanke:id/iv_close"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12829816',
+            'https://i.gkd.li/import/12869344',
+          ],
+        },
+        {
+          key: 1,
+          matches: 'ImageView - FrameLayout > FrameLayout > ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13070801',
+        },
       ],
     },
   ],
