3119a52082106359ee04f6a9a6878157d07b66ae
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-25 00:21:54 +0800

    feat(起点读书): 更新弹窗-规则优化 (#1383)
    * feat(起点读书): 【更新弹窗】
    
    * feat(起点读书): 更新弹窗
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>
    
    close #1373

--- src/apps/com.qidian.QDReader.ts
+++ src/apps/com.qidian.QDReader.ts
@@ -17,10 +17,22 @@ export default defineAppConfig({
     {
       key: 1,
       name: '更新弹窗',
-      activityIds: ['com.qidian.QDReader.framework.widget.dialog.judian'],
-      rules:
-        'TextView[text$="新版本"] +(2) RelativeLayout >(4) TextView[text="以后再说"]',
-      snapshotUrls: ['https://gkd-kit.gitee.io/import/12641026'],
+      quickFind: true,
+      activityIds: [
+        'com.qidian.QDReader.framework.widget.dialog.judian',
+        'com.tencent.upgrade.ui.UpgradeDialogActivity',
+      ],
+      rules: [
+        {
+          matches:
+            'TextView[text$="新版本"] +(2) RelativeLayout >(4) TextView[text="以后再说"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12641026'],
+        },
+        {
+          matches: '[id="com.qidian.QDReader:id/upgrade_dialog_close_btn"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/13116821'],
+        },
+      ],
     },
     {
       key: 2,
