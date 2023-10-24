9d4ea6fc8425e4bb987b85f7228cc49ac8f0e969
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-25 00:15:26 +0800

    feat(bilibili): 评论区-满意度评价 (#1381)
    - 【评论区-满意度评价】
    
    close #1375

---
+++ src/apps/com.bilibili.app.in.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.bilibili.app.in',
+  name: 'bilibili',
+  groups: [
+    {
+      key: 5,
+      name: '评论区-满意度评价',
+      quickFind: true,
+      activityIds: 'com.bilibili.video.story.StoryVideoActivity',
+      rules:
+        '[text="对当前的评论区满意吗？"] + [id="com.bilibili.app.in:id/close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13115189',
+    },
+  ],
+});
