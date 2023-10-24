bcc9412f0f4aefa798fe9703bd1d24d9a80e8a2d
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-25 00:17:10 +0800

    feat(荣耀应用市场): 开屏广告; 广告弹窗; 悬浮窗小广告; 推送通知 (#1382)
    * feat(荣耀应用市场):
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1376

---
+++ src/apps/com.hihonor.appmarket.ts
@@ -0,0 +1,42 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.hihonor.appmarket',
+  name: '荣耀应用市场',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchLauncher: true,
+      activityIds: 'com.hihonor.appmarket.module.main.MainActivity',
+      rules: '[id="com.hihonor.appmarket:id/tv_operation_skip"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13063925',
+    },
+    {
+      key: 1,
+      name: '广告弹窗',
+      quickFind: true,
+      activityIds: 'com.hihonor.android.launcher.unihome.UniHomeLauncher',
+      rules: '[id="com.hihonor.appmarket:id/iv_dialog_operation_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13063815',
+    },
+    {
+      key: 2,
+      name: '悬浮窗小广告',
+      quickFind: true,
+      activityIds: 'com.hihonor.appmarket.module.main.MainActivity',
+      rules: '[id="com.hihonor.appmarket:id/iv_floating_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13063928',
+    },
+    {
+      key: 3,
+      name: '推送通知',
+      quickFind: true,
+      activityIds: 'com.hihonor.appmarket.module.main.MainActivity',
+      rules:
+        'TextView[text="接收通知"] < LinearLayout < LinearLayout +n [id="android:id/buttonPanel"] Button[text="否"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13073319',
+    },
+  ],
+});
