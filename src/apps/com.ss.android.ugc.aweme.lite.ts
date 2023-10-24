00debb9abd1b7c52ec7a1ae0ec56b6385a7a09f6
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-25 00:06:19 +0800

    feat(抖音极速版): 青少年弹窗 (#1363)
    * feat(抖音极速版): 【青少年弹窗】
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1362

---
+++ src/apps/com.ss.android.ugc.aweme.lite.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.ss.android.ugc.aweme.lite',
+  name: '抖音极速版',
+  groups: [
+    {
+      key: 1,
+      name: '青少年弹窗',
+      desc: '点击【我知道了】',
+      quickFind: true,
+      activityIds: 'com.ss.android.ugc.aweme.main.MainActivity',
+      rules: 'Button[desc="开启青少年模式"] + Button[text="我知道了"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13111607',
+    },
+  ],
+});
