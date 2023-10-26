47b1fabb302bb2bf197de56245e745552a69c5b8
walklty <47232102+walklty@users.noreply.github.com>
2023-10-26 22:58:22 +0800

    feat(1号会员店): 开屏广告, 促销弹窗 (#1409)
    * Create com.thestore.main.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1402

---
+++ src/apps/com.thestore.main.ts
@@ -0,0 +1,35 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.thestore.main',
+  name: '1号会员店',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: 'com.thestore.main.LoadingActivity',
+      rules: [
+        {
+          matches: '[id="com.thestore.main:id/ll_skip"] > [text="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13163668',
+        },
+      ],
+    },
+    {
+      key: 1,
+      enable: false,
+      name: '促销广告',
+      desc: '关闭促销弹窗，也许有用呢',
+      activityIds: 'com.thestore.main.app.home.HomeActivity',
+      rules: [
+        {
+          matches: '[id="com.thestore.main.home.feature:id/ad"] > TextView',
+          //TextView有id类似".../j"担心这个id是随机的，所以用了TextView而不是id
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13163686',
+        },
+      ],
+    },
+  ],
+});
