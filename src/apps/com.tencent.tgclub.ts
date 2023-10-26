1f1b3ab9458f6d8f06942f1c617562a6b18e1de9
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-26 22:56:15 +0800

    feat(心悦俱乐部): 开屏广告 (#1405)
    close #1400

---
+++ src/apps/com.tencent.tgclub.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tencent.tgclub',
+  name: '心悦俱乐部',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchLauncher: true,
+      activityIds: 'com.tencent.tgclub.business.appframe.WelcomeActivity',
+      rules: '[id="com.tencent.tgclub:id/jump_time"][text$="跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13163289',
+    },
+  ],
+});
