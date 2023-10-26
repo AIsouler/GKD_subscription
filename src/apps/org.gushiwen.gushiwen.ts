6f51d683d1ae44e56a8ac1682173f596fce639c7
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-26 23:20:44 +0800

    feat(古诗文网): 应用内广告弹窗规则优化 (#1436)
    close #1404

--- src/apps/org.gushiwen.gushiwen.ts
+++ src/apps/org.gushiwen.gushiwen.ts
@@ -7,38 +7,64 @@ export default defineAppConfig({
     {
       key: 1,
       name: '应用内广告弹窗',
+      matchLauncher: true,
       rules: [
         {
           delay: 500,
           key: 0,
           activityIds: 'org.gushiwen.gushiwen.HomeActivity',
-          matches: 'ImageView - FrameLayout > FrameLayout > ImageView',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12813618',
+          matches:
+            '@[width>1 && width<65] < FrameLayout - [name="android.widget.ImageView"||name="android.widget.FrameLayout"||name="android.widget.LinearLayout"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12813618',
+            'https://gkd-kit.gitee.io/import/12903835',
+            'https://gkd-kit.gitee.io/import/12903885',
+            'https://gkd-kit.gitee.io/import/12876077',
+            'https://gkd-kit.gitee.io/import/13059294',
+          ],
         },
         {
           delay: 500,
           key: 1,
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
-          matches: '@Image < View +3 View > View > Image',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12837225',
+          matches: '@Image < View[width<100] <n View > [text="反馈"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12837225',
+            'https://gkd-kit.gitee.io/import/12876086',
+            'https://gkd-kit.gitee.io/import/12876067',
+            'https://gkd-kit.gitee.io/import/12837225',
+            'https://gkd-kit.gitee.io/import/12912911',
+            'https://gkd-kit.gitee.io/import/13171930',
+          ],
         },
         {
           preKeys: [0, 1],
           key: 10,
           desc: '打赏页面-点击返回按钮',
-          activityIds: 'local.z.androidshared.vip.RewardActivity',
-          matches:
-            '[id="org.gushiwen.gushiwen:id/btn_back"][desc="返回"] + [text="打赏我们"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12813638',
+          activityIds: [
+            'local.z.androidshared.vip.RewardActivity',
+            'local.z.androidshared.pay.RewardActivity',
+          ],
+          matches: '[id="org.gushiwen.gushiwen:id/btn_back"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12813638',
+            'https://gkd-kit.gitee.io/import/13111042',
+          ],
         },
         {
           preKeys: [0, 1],
           key: 11,
           desc: '荣誉会员-点击返回按钮',
-          activityIds: 'local.z.androidshared.vip.member.VipActivity',
-          matches: '[id="org.gushiwen.gushiwen:id/backBtn"][desc="返回"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12837229',
+          activityIds: [
+            'local.z.androidshared.vip.member.VipActivity',
+            'local.z.androidshared.pay.vip.VipActivity',
+          ],
+          matches: '[id="org.gushiwen.gushiwen:id/backBtn"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12837229',
+            'https://gkd-kit.gitee.io/import/13111057',
+          ],
         },
       ],
     },
