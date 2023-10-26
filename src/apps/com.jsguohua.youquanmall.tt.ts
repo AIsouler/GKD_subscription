25499e6f12679dd26dc8214694aa6fd3bc77e8ad
walklty <47232102+walklty@users.noreply.github.com>
2023-10-26 23:27:53 +0800

    feat(图图影视): 开屏, 弹窗 (#1413)
    * Create com.jsguohua.youquanmall.tt.ts
    
    * feat(图图影视): 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>
    
    close #1401

---
+++ src/apps/com.jsguohua.youquanmall.tt.ts
@@ -0,0 +1,33 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.jsguohua.youquanmall.tt',
+  name: '图图影视',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: 'com.zero.flutter_qq_ads.page.AdSplashActivity',
+      rules: [
+        {
+          matches: '[text^="跳过"][text.length<=3]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13163305',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '应用内弹窗',
+      activityIds: 'com.zero.flutter_qq_ads.page.AdSplashActivity',
+      rules: [
+        {
+          matches:
+            '[text="去微信看看"] < FrameLayout <n FrameLayout[childCount=2] -n FrameLayout > ImageView',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13163314',
+        },
+      ],
+    },
+  ],
+});
