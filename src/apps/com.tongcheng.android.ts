18651bb0ff6bf4dbed72ea18082d47a6397646b1
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-26 23:03:48 +0800

    feat(同程旅行): 应用内卡片广告 (#1419)

---
+++ src/apps/com.tongcheng.android.ts
@@ -0,0 +1,29 @@
+import { machine } from 'os';
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tongcheng.android',
+  name: '同程旅行',
+  groups: [
+    {
+      key: 1,
+      name: '应用内卡片广告',
+      activityIds: 'com.tongcheng.android.TongchengMainActivity',
+      rules: [
+        {
+          key: 0,
+          matches:
+            'ViewGroup[childCount=2] > ImageView + ViewGroup[childCount=1] > ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13167070',
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          matches:
+            '@* > * > [id="com.tongcheng.android:id/tv_home_banner_pop_up"][text="关闭"]',
+          snapshotUrls: 'https://i.gkd.li/import/13167149',
+        },
+      ],
+    },
+  ],
+});
