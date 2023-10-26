07e5ff9a740104601552c1cdf0c390955022747f
shanlanCoding <44717382+shanlanCoding@users.noreply.github.com>
2023-10-26 23:09:54 +0800

    feat(网络万用表): 开屏广告 (#1429)
    * feat(网络万用表): 开屏广告
    
    * feat(网络万用表): 开屏广告
    
    ---------
    
    Co-authored-by: shanlan <hi@gobyte.cn>
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/org.daai.netcheck.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'org.daai.netcheck',
+  name: '网络万用表',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'org.daai.netcheck.MainActivity',
+      rules: '[id="org.daai.netcheck:id/skip_view"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13174128',
+    },
+  ],
+});
