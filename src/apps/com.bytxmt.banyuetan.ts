dd85bb10425e3f92a7eb2595fa315aee56c892a0
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-26 23:03:19 +0800

    feat(半月谈): 开屏广告 (#1418)

---
+++ src/apps/com.bytxmt.banyuetan.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.bytxmt.banyuetan',
+  name: '半月谈',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.bytxmt.banyuetan.activity.LauncherActivity',
+      matchLauncher: true,
+      rules: '[id="com.bytxmt.banyuetan:id/activity_launcher_ad_time_bt"]',
+      snapshotUrls: 'https://i.gkd.li/import/13166668',
+    },
+  ],
+});
