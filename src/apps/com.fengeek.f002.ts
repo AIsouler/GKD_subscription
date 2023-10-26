701fd71d53b1a29a1f256ab635ef79e4c23fddee
Nebulafalls <53670448+Nebulafalls@users.noreply.github.com>
2023-10-26 22:50:10 +0800

    feat(fiil+): 自动连接耳机 (#1393)
    * 添加自动连接耳机按钮
    
    * chore(actions): check_format_lint
    
    * feat(fiil+): 默认关闭
    
    * feat(fiil+): 增加说明
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.fengeek.f002.ts
+++ src/apps/com.fengeek.f002.ts
@@ -15,5 +15,32 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      enable: false,
+      key: 2,
+      name: '自动连接耳机',
+      desc: '点击连接耳机按钮,点击扫描到的第一个设备',
+      activityIds: ['com.fengeek.f002.MainActivity'],
+      rules: [
+        {
+          key: 0,
+          name: '点击连接耳机按钮',
+          quickFind: true,
+          action: 'clickCenter',
+          matches: '[clickable=true][id="com.fengeek.f002:id/av_elc"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13161277',
+        },
+        {
+          preKeys: [0],
+          key: 1,
+          name: '点击扫描到的设备',
+          quickFind: true,
+          action: 'clickNode',
+          matches:
+            '[clickable=true][id="com.fengeek.f002:id/device_container"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13161365',
+        },
+      ],
+    },
   ],
 });
