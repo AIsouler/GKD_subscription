8b1104c81ba6a9fa5570cc5080ff25a5628bcb18
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-26 23:02:52 +0800

    feat(哔哩哔哩HD): 青少年模式弹窗 (#1417)

--- src/apps/tv.danmaku.bilibilihd.ts
+++ src/apps/tv.danmaku.bilibilihd.ts
@@ -12,5 +12,12 @@ export default defineAppConfig({
       rules: '[id="tv.danmaku.bilibilihd:id/count_down"]',
       snapshotUrls: 'https://i.gkd.li/import/13031261',
     },
+    {
+      key: 1,
+      name: '青少年模式弹窗',
+      activityIds: 'com.bilibili.teenagersmode.ui.TeenagersModeDialogActivity',
+      rules: '[text*="青少年模式"] + [text="我知道了"]',
+      snapshotUrls: 'https://i.gkd.li/import/13166639',
+    },
   ],
 });
