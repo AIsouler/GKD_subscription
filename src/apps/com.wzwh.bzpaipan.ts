0166f6a4cb61d90fb404b95cad0fe7b626c03425
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-26 22:55:30 +0800

    feat(问真八字排盘): 全屏广告 (#1399)
    * feat(问真八字排盘)
    
    * feat(问真八字排盘): 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>
    
    close #1392

---
+++ src/apps/com.wzwh.bzpaipan.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wzwh.bzpaipan',
+  name: '问真八字排盘',
+  groups: [
+    {
+      key: 3,
+      name: '全屏广告',
+      quickFind: true,
+      activityIds: 'com.qq.e.ads.PortraitADActivity',
+      rules:
+        '@ImageView[clickable=true] < LinearLayout[childCount=1] <2 FrameLayout[childCount=2] < [id="android:id/content"][childCount=1]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13162912',
+    },
+  ],
+});
