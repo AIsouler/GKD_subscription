ad737752e0575a27d8c40648387cbbff9dd84fd3
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-26 22:57:21 +0800

    feat(热剧猫): 广告弹窗 (#1408)

---
+++ src/apps/com.tuying.kuimg.rjm.ts
@@ -0,0 +1,27 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tuying.kuimg.rjm',
+  name: '热剧猫',
+  groups: [
+    {
+      key: 1,
+      name: '广告弹窗',
+      activityIds: 'com.tuying.kuimg.rjm.MainActivity',
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[id="com.tuying.kuimg.rjm:id/ksad_tk_view"] >n @ViewGroup[childCount=1][clickable=true] > ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13166088',
+        },
+        {
+          key: 1,
+          matches:
+            '[id="com.tuying.kuimg.rjm:id/ksad_tk_view"] >n @ViewGroup > [text="跳过"]',
+          snapshotUrls: 'https://i.gkd.li/import/13166086',
+        },
+      ],
+    },
+  ],
+});
