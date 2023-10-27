e7d169d483bdbad70c0d7e45350f216e0fce38a5
lisonge <i@songe.li>
2023-10-27 21:08:07 +0800

    feat(GKD): 移除 gkd 规则

--- src/apps/li.songe.gkd.ts
+++ src/apps/li.songe.gkd.ts
@@ -3,34 +3,5 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'li.songe.gkd',
   name: 'GKD',
-  groups: [
-    {
-      key: 0,
-      name: 'GKD-空规则组',
-    },
-    {
-      enable: false,
-      key: 1,
-      name: 'GKD-批量删除快照',
-      desc: '删除完成记得回来关闭这条规则哦~',
-      activityIds: 'li.songe.gkd.MainActivity',
-      rules: [
-        {
-          key: 1,
-          matches:
-            'ComposeView > View > View > View > @View[childCount=3] > TextView + TextView + TextView',
-          snapshotUrls: [
-            'https://gkd-kit.gitee.io/import/12706723',
-            'https://gkd-kit.gitee.io/import/12706741', // 必须加上 TextView + TextView + TextView，否则在这个快照会误触
-          ],
-        },
-        {
-          preKeys: [1],
-          key: 2,
-          matches: 'View[childCount=3] > [text="删除"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12706723',
-        },
-      ],
-    },
-  ],
+  groups: [],
 });
