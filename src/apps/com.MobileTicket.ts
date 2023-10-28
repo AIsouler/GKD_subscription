7c23af9d1d27d99f5cd1232cc75613a35792483a
Otis0227 <106000651+Otis0227@users.noreply.github.com>
2023-10-29 02:34:43 +0800

    feat(铁路12306): 开屏广告-规则优化 (#1531)
    * feat(铁路12306):开屏广告跳过优化
    
    铁路12306，开屏广告跳过优化
    
    * chore(actions): check_format_lint
    
    * feat(铁路12306): 开屏广告-规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>
    
    close #1529

--- src/apps/com.MobileTicket.ts
+++ src/apps/com.MobileTicket.ts
@@ -7,8 +7,12 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.MobileTicket.ui.dialog.SplashAdDialog',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
       rules: '[id=`com.MobileTicket:id/tv_skip`]',
+      snapshotUrls: 'https://i.gkd.li/import/13196243',
     },
   ],
 });
