7e0075aae3d9ba92028850412335dcb1dc2477ea
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-29 02:14:30 +0800

    feat(向日葵远程控制): 瓜子会员弹窗-规则优化 (#1523)
    * feat 开屏广告
    
    * feat(向日葵远程控制): 瓜子会员弹窗-规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.oray.sunlogin.ts
+++ src/apps/com.oray.sunlogin.ts
@@ -6,13 +6,15 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '会员弹窗',
+      name: '瓜子会员弹窗',
       desc: '点击右上角[关闭]',
       quickFind: true,
-      activityIds: 'com.oray.sunlogin.dialog.ShareDialog',
-      rules:
-        '@[id="com.oray.sunlogin:id/close"] <2 RelativeLayout[childCount=2] < [id="android:id/content"][childCount=1]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12910411',
+      activityIds: 'com.oray.sunlogin.application.Main',
+      rules: '[id="com.oray.sunlogin:id/close"]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13195950',
+        'https://i.gkd.li/import/12910411',
+      ],
       exampleUrls:
         'https://github.com/gkd-kit/inspect/assets/38517192/61d335f0-a85a-4e26-80fe-6bc0d1742bc0',
     },
