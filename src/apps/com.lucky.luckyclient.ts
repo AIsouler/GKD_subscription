175e77bfb8a1f7e1c7c811f1f1109faf5068e31f
walklty <47232102+walklty@users.noreply.github.com>
2023-10-28 23:53:25 +0800

    feat(瑞幸咖啡): 优惠卷返回 (#1484)
    * Update com.lucky.luckyclient.ts
    
    * Update com.lucky.luckyclient.ts
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>
    
    close #1406

--- src/apps/com.lucky.luckyclient.ts
+++ src/apps/com.lucky.luckyclient.ts
@@ -31,5 +31,17 @@ export default defineAppConfig({
       rules: '[id="com.lucky.luckyclient:id/close_iv"][visibleToUser=true]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12922834',
     },
+    {
+      key: 3,
+      name: '优惠卷领取提示',
+      desc: '按下返回键关闭优惠卷领取提示',
+      enable: false,
+      activityIds: 'com.luckin.client.main.FirstActivity',
+      rules: {
+        matches: '[id="com.lucky.luckyclient:id/webcontent"]',
+        action: 'back',
+      },
+      snapshotUrls: 'https://i.gkd.li/import/13164114',
+    },
   ],
 });
