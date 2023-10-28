4f72cc8cf90dc98c0764ea96450c5511231356a1
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-28 23:04:38 +0800

    feat(稀饭动漫): 开屏广告 (#1462)
    * feat 开屏广告
    
    * feat 广告弹窗

---
+++ src/apps/com.xfani.app.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xfani.app',
+  name: '稀饭动漫',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.xfani.app.MainActivity',
+      matchLauncher: true,
+      rules: 'TextView[text^="跳过"]',
+      snapshotUrls: 'https://i.gkd.li/import/13188526',
+    },
+    {
+      key: 1,
+      name: '广告弹窗',
+      activityIds: 'com.xfani.app.MainActivity',
+      rules: 'ImageView - LinearLayout - FrameLayout > FrameLayout > ImageView',
+      snapshotUrls: 'https://i.gkd.li/import/13188550',
+    },
+  ],
+});
