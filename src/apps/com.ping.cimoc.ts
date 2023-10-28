74e8ed2f0f67d5c5048ab8ebd848abe53794856d
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-28 23:00:44 +0800

    feat(芝士漫画) 开屏广告、广告弹窗 (#1456)
    * feat 开屏广告
    
    * feat 广告弹窗

---
+++ src/apps/com.ping.cimoc.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.ping.cimoc',
+  name: '芝士漫画',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.ping.cimoc.ui.activity.SplashActivity',
+      matchLauncher: true,
+      rules: '[id="com.ping.cimoc:id/splash_container"] >n [text="跳过"]',
+      snapshotUrls: 'https://i.gkd.li/import/13187773',
+    },
+    {
+      key: 1,
+      name: '广告弹窗',
+      activityIds: 'com.ping.cimoc.ui.activity.MainActivity',
+      rules:
+        'ImageView < FrameLayout + FrameLayout > FrameLayout[childCount=1] > ImageView',
+      snapshotUrls: 'https://i.gkd.li/import/13187751',
+    },
+  ],
+});
