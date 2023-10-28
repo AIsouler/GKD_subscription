5c19d6363eed1cbd4fbc816e2a960667a74ee0c1
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-28 23:02:26 +0800

    feat(嘀嘀动漫): 开屏广告 (#1458)

---
+++ src/apps/info.zzjdev.musicdownload.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'info.zzjdev.musicdownload',
+  name: '嘀嘀动漫',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'info.zzjdev.musicdownload.start.SplashActivity1',
+      matchLauncher: true,
+      rules: '[id="info.zzjdev.musicdownload:id/skip_view"]',
+      snapshotUrls: 'https://i.gkd.li/import/13187827',
+    },
+  ],
+});
