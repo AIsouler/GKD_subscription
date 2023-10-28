4acfbe13ab653b646f133004d2dbb1fabcd2c60e
walklty <47232102+walklty@users.noreply.github.com>
2023-10-28 22:29:16 +0800

    feat(哔哩哔哩): App评价弹窗 (#1447)
    * Update com.bilibili.app.in.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1443

--- src/apps/com.bilibili.app.in.ts
+++ src/apps/com.bilibili.app.in.ts
@@ -13,5 +13,13 @@ export default defineAppConfig({
         '[text="对当前的评论区满意吗？"] + [id="com.bilibili.app.in:id/close"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/13115189',
     },
+    {
+      key: 6,
+      name: 'APP评分',
+      quickFind: true,
+      activityIds: 'tv.danmaku.bili.MainActivityV2',
+      rules: '[id="com.bilibili.app.in:id/cancel"][text="下次再说"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13180746',
+    },
   ],
 });
