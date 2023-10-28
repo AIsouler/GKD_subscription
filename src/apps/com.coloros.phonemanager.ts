b5eae111b02c5406efaf2c0bd282a3df20b9beda
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-29 00:43:23 +0800

    feat(OPPO手机管家): 版本更新 (#1508)
    * feat(OPPO手机管家): 版本更新
    
    * chore(actions): check_format_lint
    
    * feat(OPPO手机管家): 版本更新
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>
    
    close #1498

---
+++ src/apps/com.coloros.phonemanager.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.coloros.phonemanager',
+  name: 'OPPO手机管家',
+  groups: [
+    {
+      key: 3,
+      name: '版本更新',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches:
+            '@[text="取消"] < [id="com.coloros.phonemanager:id/buttonPanel"] > [text="更新"]',
+          snapshotUrls: 'https://i.gkd.li/import/13194979',
+        },
+      ],
+    },
+  ],
+});
