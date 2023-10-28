d60df8d6dd9a3620823843bb70fc06538470baeb
walklty <47232102+walklty@users.noreply.github.com>
2023-10-29 01:04:03 +0800

    feat(腾讯动漫): 开屏广告-规则优化;青少年模式弹窗-规则优化 (#1513)
    * Update com.qq.ac.android.ts
    
    * chore(actions): check_format_lint
    
    * feat(腾讯动漫): 开屏广告
    
    * feat(腾讯动漫): 青少年模式弹窗-规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>
    
    close #1505

--- src/apps/com.qq.ac.android.ts
+++ src/apps/com.qq.ac.android.ts
@@ -7,16 +7,26 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.qq.ac.android.splash.SplashActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: '[id="com.qq.ac.android:id/text_ad_logo"] + [text="跳过"]',
-      snapshotUrls: ['https://gkd-kit.gitee.io/import/12707750'],
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12707750', //com.qq.ac.android.splash.SplashActivity
+        'https://gkd-kit.gitee.io/import/13195632', //com.qq.ac.android.splash.SplashHotActivity
+      ],
     },
     {
       key: 1,
       name: '青少年模式弹窗',
-      activityIds: 'com.qq.ac.android.view.fragment.dialog',
-      rules: '@[id="com.qq.ac.android:id/close_btn"] + [text="青少年守护模式"]',
-      snapshotUrls: ['https://gkd-kit.gitee.io/import/12707752'],
+      activityIds: ['com.qq.ac.android.main.MainActivity'],
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text="青少年守护模式"] - [id="com.qq.ac.android:id/close_btn"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12707752',
+        'https://gkd-kit.gitee.io/import/13195636',
+      ],
     },
     {
       key: 2,
