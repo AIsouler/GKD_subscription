20ec166351b7cce8a84b71c1c336f0ca32fbb7a9
walklty <47232102+walklty@users.noreply.github.com>
2023-10-28 23:00:13 +0800

    feat(奇妙应用): 开屏广告, 下方横幅 (#1455)
    * Create com.magicalstory.AppStore.ts
    
    * chore(actions): check_format_lint
    
    * Update com.magicalstory.AppStore.ts
    
    * feat(奇妙应用): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>
    
    close #1451
    close #1479

---
+++ src/apps/com.magicalstory.AppStore.ts
@@ -0,0 +1,39 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.magicalstory.AppStore',
+  name: '奇妙应用',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[text="跳过"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13185745',
+            'https://i.gkd.li/import/13191546',
+          ],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '下方横幅',
+      activityIds: 'com.magicalstory.AppStore.search.searchActivity',
+      rules: [
+        {
+          matches: [
+            '[id="com.magicalstory.AppStore:id/banner"]',
+            'FrameLayout[childCount=5] > FrameLayout[childCount=1] > ImageView',
+          ],
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13185746',
+        },
+      ],
+    },
+  ],
+});
