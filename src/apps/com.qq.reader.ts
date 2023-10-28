6cfb4a669510bcd168a0170bce09831c1650f534
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-29 00:02:48 +0800

    feat(QQ阅读): 青少年弹窗 (#1494)
    * feat(QQ阅读): 青少年弹窗
    
    * feat(QQ阅读): 青少年弹窗
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>
    
    close #1490

--- src/apps/com.qq.reader.ts
+++ src/apps/com.qq.reader.ts
@@ -11,5 +11,14 @@ export default defineAppConfig({
       rules: '[id="com.qq.reader:id/splash_container"] + [text^="跳过"]',
       snapshotUrls: 'https://i.gkd.li/import/12811930',
     },
+    {
+      key: 2,
+      name: '青少年弹窗',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.qq.reader:id/tv_i_know"]',
+      snapshotUrls: 'https://i.gkd.li/import/13194867',
+    },
   ],
 });
