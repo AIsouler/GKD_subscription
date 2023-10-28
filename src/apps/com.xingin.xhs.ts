faf7bf23e5c75ae7975c27085ae90086f617a154
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-29 02:05:14 +0800

    feat(小红书):申请开启通知 (#1521)
    * feat(小红书):申请开启通知
    
    * feat(小红书): 关闭开启通知弹窗
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>
    
    close #1520

--- src/apps/com.xingin.xhs.ts
+++ src/apps/com.xingin.xhs.ts
@@ -12,5 +12,13 @@ export default defineAppConfig({
       rules: '[text="广告"] - @FrameLayout > [text="跳过"]',
       snapshotUrls: 'https://i.gkd.li/import/12739065',
     },
+    {
+      key: 1,
+      name: '关闭开启通知弹窗',
+      quickFind: true,
+      activityIds: 'com.xingin.xhs.index.v2.IndexActivityV2',
+      rules: '@ImageView <2 FrameLayout >2 [text^="打开通知"]',
+      snapshotUrls: 'https://i.gkd.li/import/13195753',
+    },
   ],
 });
