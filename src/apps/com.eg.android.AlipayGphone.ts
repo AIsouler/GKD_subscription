f05dcb8d4e57311209636cb4f3eac01aa012d952
walklty <47232102+walklty@users.noreply.github.com>
2023-10-28 22:30:13 +0800

    feat(花呗): 升级弹窗ActivityId (#1448)
    * Update com.eg.android.AlipayGphone.ts
    
    * Update com.eg.android.AlipayGphone.ts
    
    close #1446

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -8,14 +8,16 @@ export default defineAppConfig({
       enable: false,
       key: 0,
       name: '关闭花呗升级弹窗',
-      activityIds:
+      activityIds: [
         'com.alipay.mobile.nebulax.integration.mpaas.activity.NebulaActivity$Main',
+        'com.alipay.android.msp.ui.views', //views.MspContainerActivity & views.MspUniRenderActivity
+      ],
       matchLauncher: true,
       rules: [
         {
           key: 0,
           matches: 'View[childCount=3] > @Image - View[text="花呗服务升级"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12737055',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12737055', //com.alipay.mobile.nebulax.integration.mpaas.activity.NebulaActivity$Main
         },
         {
           key: 1,
@@ -23,7 +25,11 @@ export default defineAppConfig({
             '[text="根据相关法律法规要求，请尽快完成花呗升级"]',
             'FrameLayout > FrameLayout  > FrameLayout > [text="关闭"]',
           ],
-          snapshotUrls: 'https://i.gkd.li/import/12826077',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/13183946', //com.alipay.android.msp.ui.views.MspContainerActivity
+            'https://i.gkd.li/import/12826077', //com.alipay.android.msp.ui.views.MspUniRenderActivity
+            'https://gkd-kit.gitee.io/import/12915864', //matchLauncher
+          ],
         },
       ],
     },
