0993eb2837e24dba795d99f280e831d03450bdac
Otis0227 <106000651+Otis0227@users.noreply.github.com>
2023-10-29 02:39:04 +0800

    feat(快手极速版): 开屏广告-规则优化; 青少年弹窗跳过 (#1532)
    * feat(快手极速版):青少年弹窗跳过
    
    快手极速版，青少年弹窗跳过
    
    * Update com.kuaishou.nebula.ts
    
    * Update com.kuaishou.nebula.ts
    
    * feat(快手极速版): 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.kuaishou.nebula.ts
+++ src/apps/com.kuaishou.nebula.ts
@@ -7,9 +7,20 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.yxcorp.gifshow.HomeActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: '[id="com.kuaishou.nebula:id/splash_skip_text"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12519389',
+    },
+    {
+      key: 1,
+      name: '青少年弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.yxcorp.gifshow.HomeActivity',
+      rules: '[id="com.kuaishou.nebula:id/positive"][text="我知道了"]',
+      snapshotUrls: 'https://i.gkd.li/import/13196316',
     },
   ],
 });
