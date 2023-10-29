6f3b9f5ae7063d0d558e050d375792d0fcb8454b
walklty <47232102+walklty@users.noreply.github.com>
2023-10-29 23:18:09 +0800

    feat(向日葵远程控制): 瓜子会员弹窗-解决误触 (#1549)
    close #1539

--- src/apps/com.oray.sunlogin.ts
+++ src/apps/com.oray.sunlogin.ts
@@ -10,10 +10,11 @@ export default defineAppConfig({
       desc: '点击右上角[关闭]',
       quickFind: true,
       activityIds: 'com.oray.sunlogin.application.Main',
-      rules: '[id="com.oray.sunlogin:id/close"]',
+      rules: '[id="com.oray.sunlogin:id/close"][text="关闭"]',
       snapshotUrls: [
         'https://i.gkd.li/import/13195950',
         'https://i.gkd.li/import/12910411',
+        'https://gkd-kit.gitee.io/import/13197454', //如果没有text条件则会和这一条误触
       ],
       exampleUrls:
         'https://github.com/gkd-kit/inspect/assets/38517192/61d335f0-a85a-4e26-80fe-6bc0d1742bc0',
