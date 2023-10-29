8958c39650ff644238647b78fa11a97b6e9542e8
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-30 00:25:15 +0800

    feat(淘宝): 弹窗规则优化 (#1557)
    * feat(淘宝): 各级页面-添加到首页弹窗、限时福利弹窗规则优化(gkd-kit#1444)
    
    * feat(淘宝): 开启系统通知提示(gkd-kit#1502)
    
    * feat(淘宝): 消息页面-热门活动卡片规则优化(gkd-kit#1567)
    
    close #1444 
    close #1502 
    close #1567

--- src/apps/com.taobao.taobao.ts
+++ src/apps/com.taobao.taobao.ts
@@ -20,12 +20,14 @@ export default defineAppConfig({
       name: '限时福利弹窗',
       activityIds: [
         'com.taobao.tao.welcome.Welcome',
+        'com.taobao.tao.TBMainActivity',
         'com.taobao.android.tbabilitykit.pop.StdPopContainerActivity',
         'com.taobao.android.detail.wrapper.activity.DetailActivity',
       ],
       rules: '[desc^="限时福利"] < FrameLayout + [desc="关闭按钮"]',
       snapshotUrls: [
         'https://gkd-kit.gitee.io/import/12642792', // activityId: com.taobao.tao.welcome.Welcome
+        'https://gkd-kit.gitee.io/import/13180826', // activityId: com.taobao.tao.TBMainActivity
         'https://gkd-kit.gitee.io/import/12648734', // activityId: com.taobao.android.tbabilitykit.pop.StdPopContainerActivity
         'https://gkd-kit.gitee.io/import/12648746', // activityId: com.taobao.android.detail.wrapper.activity.DetailActivity
       ],
@@ -34,8 +36,36 @@ export default defineAppConfig({
       key: 2,
       name: '消息页面-热门活动卡片',
       activityIds: 'com.taobao.tao.welcome.Welcome',
-      rules: '[desc="热门活动抢先看"] +(6) FrameLayout > TextView',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12642795',
+      rules: 'View[desc.length>0] +2n FrameLayout > TextView[text="퀺"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12642795',
+        'https://i.gkd.li/import/13197877',
+      ],
+    },
+    {
+      enable: false,
+      key: 8,
+      name: '开启系统通知提示',
+      desc: '自动点击关闭',
+      activityIds: ['com.taobao.tao.welcome.Welcome'],
+      rules: 'View[text^="开启系统通知"] + Image[clickable=true]',
+      snapshotUrls: ['https://i.gkd.li/import/13197594'],
+    },
+    {
+      enable: false,
+      key: 9,
+      name: '各级页面-添加到首页弹窗',
+      desc: '自动点击退出',
+      quickFind: true,
+      activityIds: [
+        'com.taobao.themis.container.app.TMSActivity',
+        'com.alibaba.triver.container.TriverMainActivity',
+      ],
+      rules: 'TextView[text="去首页"] + TextView[text="退出"]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13197553',
+        'https://i.gkd.li/import/13197546',
+      ],
     },
     {
       key: 10,
