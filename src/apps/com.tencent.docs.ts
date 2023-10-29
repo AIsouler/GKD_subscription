1511bbd40f2c257fe34b6a816aa8a5686ea6e6f5
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-30 01:07:57 +0800

    feat(腾讯文档): 开屏广告、更新弹窗、广告卡片 (#1576)
    * feat 开屏广告
    
    * feat 更新弹窗
    
    * feat 广告卡片
    
    * feat(腾讯文档): 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/com.tencent.docs.ts
@@ -0,0 +1,33 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tencent.docs',
+  name: '腾讯文档',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@LinearLayout > [id="com.tencent.docs:id/tv_gdt_ad_text"]',
+      snapshotUrls: 'https://i.gkd.li/import/13198082',
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      activityIds: 'com.tencent.docs.DocsFlutterActivity',
+      rules: '@[desc="暂不更新"] + [desc="现在更新"]',
+      snapshotUrls: 'https://i.gkd.li/import/13198091',
+    },
+    {
+      key: 2,
+      name: '广告卡片',
+      activityIds: 'com.tencent.docs.DocsFlutterActivity',
+      rules: '[desc="限时购买"] + ImageView[clickable=true]',
+      snapshotUrls: 'https://i.gkd.li/import/13198097',
+    },
+  ],
+});
