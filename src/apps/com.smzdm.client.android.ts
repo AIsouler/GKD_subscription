c26b413f9dce14c57398fa595a2424ac5d8765fc
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-30 00:55:01 +0800

    feat(什么值得买): 开屏广告（优化规则）、更新弹窗、请求读取剪贴板权限弹窗 (#1572)
    * feat 开屏广告（优化规则）
    
    * feat 更新弹窗
    
    * feat 请求读取剪贴板权限弹窗
    
    * feat(什么值得买): 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.smzdm.client.android.ts
+++ src/apps/com.smzdm.client.android.ts
@@ -7,19 +7,38 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: [
-        'com.smzdm.client.android.app.WelComeActivity',
-        'com.miui.home.launcher.Launcher',
-      ],
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: '[id="com.smzdm.client.android:id/tv_skip"]',
       snapshotUrls: 'https://i.gkd.li/import/12535072',
     },
     {
       key: 1,
+      quickFind: true,
       name: 'APP内弹窗广告',
       activityIds: 'com.smzdm.client.android.app.HomeActivity',
       rules: '[id="com.smzdm.client.android:id/dialog_home_close"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12695751',
+    },
+    {
+      key: 2,
+      name: '更新弹窗',
+      activityIds: 'com.smzdm.client.android.app.HomeActivity',
+      quickFind: true,
+      actionMaximum: 1,
+      rules:
+        '[id="com.smzdm.client.android:id/tv_version"] +2 [id="com.smzdm.client.android:id/iv_close"]',
+      snapshotUrls: 'https://i.gkd.li/import/13198016',
+    },
+    {
+      key: 10,
+      name: '请求读取剪贴板权限弹窗',
+      quickFind: true,
+      activityIds: 'com.smzdm.client.android.app.HomeActivity',
+      rules: '@[text="暂不允许"] + [text="允许"]',
+      snapshotUrls: 'https://i.gkd.li/import/13198020',
     },
   ],
 });
