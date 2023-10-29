a0dcc96b2aea9da4332a0962c974e6cead002058
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-30 00:00:58 +0800

    feat(小天才): 弹窗提醒、开启通知提醒规则优化 (#1545)

--- src/apps/com.xtc.watch.ts
+++ src/apps/com.xtc.watch.ts
@@ -5,23 +5,31 @@ export default defineAppConfig({
   name: '小天才',
   groups: [
     {
-      key: 3,
-      name: '开启通知提醒',
+      key: 2,
+      name: '弹窗提醒',
+      desc: '已知包括APP升级提醒与开启通知提醒，自动点击关闭。',
+      enable: false,
       quickFind: true,
       activityIds: [
+        'com.xtc.widget.phone.popup.activity.CustomActivity14',
         'com.xtc.widget.phone.popup.activity.CustomActivity13',
-        'com.xtc.watch.view.home.activity.XtcHomeActivity',
       ],
+      rules: '[id="com.xtc.watch:id/iv_close"]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13197390',
+        'https://i.gkd.li/import/13063274',
+      ],
+    },
+    {
+      key: 3,
+      name: '开启通知提醒',
+      enable: false,
+      quickFind: true,
+      activityIds: ['com.xtc.watch.view.home.activity.XtcHomeActivity'],
       rules: [
         {
-          matches: [
-            '[id="com.xtc.watch:id/iv_notify_permission_close"]',
-            '[id="com.xtc.watch:id/iv_close"]',
-          ],
-          snapshotUrls: [
-            'https://i.gkd.li/import/13059965',
-            'https://i.gkd.li/import/13063274',
-          ],
+          matches: ['[id="com.xtc.watch:id/iv_notify_permission_close"]'],
+          snapshotUrls: ['https://i.gkd.li/import/13059965'],
         },
       ],
     },
