88dabf9e0e3214398d1c015f95ceb7a3de00f711
aoguai <34203474+aoguai@users.noreply.github.com>
2023-10-29 23:51:11 +0800

    feat(学习通): 开启消息通知、规则优化(使用quickFind) (#1543)

--- src/apps/com.chaoxing.mobile.ts
+++ src/apps/com.chaoxing.mobile.ts
@@ -7,9 +7,20 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       activityIds: 'com.chaoxing.mobile.activity.SplashActivity',
       rules: '[id="com.chaoxing.mobile:id/jump_view"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12708472',
+    },
+    {
+      key: 10,
+      name: '开启消息通知',
+      desc: '自动点击关闭',
+      enable: false,
+      quickFind: true,
+      activityIds: 'com.chaoxing.mobile.main.ui.MainTabActivity',
+      rules: '[id="com.chaoxing.mobile:id/vNotificationItemClose"]',
+      snapshotUrls: 'https://i.gkd.li/import/13197374',
     },
   ],
 });
