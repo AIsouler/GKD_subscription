0b51988c7a0f1d2713b55c6edec0a2fdd6f34e7b
walklty <47232102+walklty@users.noreply.github.com>
2023-10-29 23:14:57 +0800

    feat(比翼多开): 开屏广告, 弹窗广告 (#1534)
    * Create com.svm.hbyy.ts
    
    * Update com.svm.hbyy.ts
    
    * Update com.svm.hbyy.ts
    
    * chore(actions): check_format_lint
    
    * feat(比翼多开): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>
    
    close #1527

---
+++ src/apps/com.svm.hbyy.ts
@@ -0,0 +1,36 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.svm.hbyy',
+  name: '比翼多开',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13185376',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '弹窗广告',
+      activityIds: 'com.svm.core.pro.view.activity.MainActivityEx',
+      rules: [
+        {
+          matches: [
+            '[text="打开或下载第三方应用"]',
+            'FrameLayout[childCount=3] + FrameLayout > ImageView',
+          ],
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13185377',
+        },
+      ],
+    },
+  ],
+});
