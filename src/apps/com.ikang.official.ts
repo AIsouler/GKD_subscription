b29b355a6735200320ec86e476dc898b4d119bf7
yanyu2023 <130368710+yanyu2023@users.noreply.github.com>
2023-10-29 23:33:33 +0800

    feat(爱康约体检查报告): 开屏广告 (#1537)
    * Create com.ikang.official.ts
    
    * chore(actions): check_format_lint
    
    * feat(爱康约体检查报告): 开屏广告
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/com.ikang.official.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.ikang.official',
+  name: '爱康约体检查报告',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[id="com.ikang.official:id/llAdvertCountdown"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13197061',
+        },
+      ],
+    },
+  ],
+});
