41ee1d08cfe79167033395a33b7685fd28430fa8
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-30 01:25:41 +0800

    feat(vivo应用商店): 请求消息通知; 开屏广告-规则优化 (#1584)
    * feat(vivo应用商店): 请求消息通知
    
    * Update com.bbk.appstore.ts
    
    * Update com.bbk.appstore.ts
    
    * chore(actions): check_format_lint
    
    * Update com.bbk.appstore.ts
    
    * feat(vivo应用商店): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>
    
    close #1581

--- src/apps/com.bbk.appstore.ts
+++ src/apps/com.bbk.appstore.ts
@@ -8,10 +8,30 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       quickFind: true,
-      activityIds: ['com.bbk.appstore.ui.AppStore'],
-      rules:
-        '[id="com.bbk.appstore:id/launch_activity_root_layout"] >6 [text^="跳过"]',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12847395',
+    },
+    {
+      key: 1,
+      name: '请求消息通知',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.bbk.appstore.upgrade.UpgradeNecessaryActivity',
+          matches: '[id="com.bbk.appstore:id/upgrade_necessary_btn_high_new"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13198101',
+        },
+        {
+          activityIds: 'com.bbk.appstore.ui.AppStoreTabActivity',
+          matches: '[id="com.bbk.appstore:id/common_dialog_netgtive_btn"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13198234',
+        },
+      ],
     },
   ],
 });
