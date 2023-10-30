9b5bf591bba6fd816b1ffc347634e4dbc936124e
二刺螈 <i@songe.li>
2023-10-31 01:03:47 +0800

    feat(向日葵远程控制): 设备列表-卡片广告 (#1617)

--- src/apps/com.oray.sunlogin.ts
+++ src/apps/com.oray.sunlogin.ts
@@ -34,6 +34,7 @@ export default defineAppConfig({
         'https://github.com/gkd-kit/inspect/assets/38517192/61d335f0-a85a-4e26-80fe-6bc0d1742bc0',
     },
     {
+      matchTime: 30000,
       key: 1,
       name: '版本更新',
       quickFind: true,
@@ -44,6 +45,16 @@ export default defineAppConfig({
           snapshotUrls: 'https://i.gkd.li/import/13195560',
         },
       ],
+    },
+    {
+      key: 2,
+      name: '设备列表-卡片广告',
+      activityIds: 'com.oray.sunlogin.application.Main',
+      quickFind: true,
+      rules: '[id="com.oray.sunlogin:id/fl_close_advertise"]',
+      snapshotUrls: 'https://i.gkd.li/import/13207785',
+      exampleUrls:
+        'https://github.com/gkd-kit/inspect/assets/38517192/071e5512-e1aa-4a8c-8440-009d6f0f35ff',
     },
   ],
 });
