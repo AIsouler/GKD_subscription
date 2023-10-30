219a5d80980cd6f07f2628248486ad602bc36ecc
OldCoding <85047508+OldCoding@users.noreply.github.com>
2023-10-30 23:38:59 +0800

    feat(甜糖): 开屏广告 (#1591)
    * Create com.tt.turbo.ts
    
    * feat(甜糖): 开屏广告
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.tt.turbo.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tt.turbo',
+  name: '甜糖',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules: 'View[desc^="跳过"][desc.length<=4]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13200355',
+    },
+  ],
+});
