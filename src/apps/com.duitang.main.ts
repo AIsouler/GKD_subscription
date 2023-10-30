a34bd17169728c8a8be9e0ebbf7b02e0b5eeadc8
世上僅有的榮光之路 <a846930164@gmail.com>
2023-10-30 23:45:27 +0800

    feat(堆糖): 开屏广告，青少年模式，首页-推荐浏览广告，去商店评分 (#1599)
    * feat(堆糖): 开屏广告，青少年模式，首页-推荐浏览广告，去商店评分
    
    * feat(堆糖): 规则优化
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.duitang.main.ts
@@ -0,0 +1,61 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.duitang.main',
+  name: '堆糖',
+  groups: [
+    {
+      name: '开屏广告',
+      key: 0,
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules: [
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13202185',
+        },
+      ],
+    },
+    {
+      name: '青少年模式',
+      desc: '关闭青少年模式提醒弹窗',
+      key: 1,
+      actionMaximum: 1,
+      activityIds: ['com.duitang.main.business.main.NAMainActivity'],
+      rules: [
+        {
+          matches: '[id="com.duitang.main:id/teenGuideKnown"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13202230',
+        },
+      ],
+    },
+    {
+      name: '首页-推荐浏览广告',
+      desc: '关闭推荐浏览页面广告',
+      key: 2,
+      activityIds: ['com.duitang.main.business.main.NAMainActivity'],
+      quickFind: true,
+      rules: [
+        {
+          matches: '[id="com.duitang.main:id/adOptionEntry"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13202725',
+        },
+      ],
+    },
+    {
+      name: '去商店评分',
+      desc: '点击[下次再说]',
+      key: 3,
+      actionMaximum: 1,
+      quickFind: true,
+      activityIds: ['com.miui.home.launcher.Launcher'],
+      rules: [
+        {
+          matches: '[id="com.duitang.main:id/scoreDuitangCancel"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13203217',
+        },
+      ],
+    },
+  ],
+});
