4bec1022b0bcfa1fc4bf0006cdf182d91b4b1e67
yanyu2023 <130368710+yanyu2023@users.noreply.github.com>
2023-10-30 23:49:46 +0800

    feat(闪送): 开屏广告 (#1603)
    * Create com.ikang.official.ts
    
    * Delete src/apps/com.ikang.official.ts
    
    * feat(闪送): 开屏广告
    
    * feat(闪送): 开屏广告
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/cn.ishansong.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.ishansong',
+  name: '闪送',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13205475',
+        },
+      ],
+    },
+  ],
+});
