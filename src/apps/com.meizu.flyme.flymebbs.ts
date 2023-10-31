6b77856d37fa8890fa3778f91dc8f4d52ad18a98
世上僅有的榮光之路 <a846930164@gmail.com>
2023-10-31 23:37:40 +0800

    feat(魅族社区): 开屏广告 (#1638)

---
+++ src/apps/com.meizu.flyme.flymebbs.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.meizu.flyme.flymebbs',
+  name: '魅族社区',
+  groups: [
+    {
+      name: '开屏广告',
+      key: 0,
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules: [
+        {
+          matches: '[id="com.meizu.flyme.flymebbs:id/tv_skip"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13213487',
+        },
+      ],
+    },
+  ],
+});
