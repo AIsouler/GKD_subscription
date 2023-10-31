f3a973b40919957826222cc25a55b0ce83a9aa42
yanyu2023 <130368710+yanyu2023@users.noreply.github.com>
2023-11-01 00:17:30 +0800

    feat(叮咚买菜): 开屏广告 (#1648)
    * Create com.ikang.official.ts
    
    * Delete src/apps/com.ikang.official.ts
    
    * feat(叮咚买菜): 开屏广告
    
    * feat(叮咚买菜): 开屏广告
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>
    
    close #1650

---
+++ src/apps/com.yaya.zone.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.yaya.zone',
+  name: '叮咚买菜',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[id="com.yaya.zone:id/tv_skip"][text^="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13215214',
+        },
+      ],
+    },
+  ],
+});
