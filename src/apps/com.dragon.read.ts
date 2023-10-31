d125dd6bf5e76b84d5f467bb16e5904adad7770b
walklty <47232102+walklty@users.noreply.github.com>
2023-10-31 23:00:00 +0800

    feat(番茄免费小说): 开屏广告 (#1622)
    * Update com.dragon.read.ts
    
    * chore(actions): check_format_lint
    
    * feat(番茄免费小说): 开屏广告
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>
    
    close #1621

--- src/apps/com.dragon.read.ts
+++ src/apps/com.dragon.read.ts
@@ -5,6 +5,20 @@ export default defineAppConfig({
   name: '番茄免费小说',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          matches: '[text="跳过广告"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13210844',
+        },
+      ],
+    },
+    {
       key: 0,
       name: '阅读页面底部广告',
       activityIds: [
