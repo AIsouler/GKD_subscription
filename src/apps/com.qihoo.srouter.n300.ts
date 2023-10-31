515749d4d79dda11e85a62ea115c36ced89779c9
walklty <47232102+walklty@users.noreply.github.com>
2023-11-01 00:28:24 +0800

    feat(360家庭防火墙): 开屏广告 (#1655)
    * Create com.qihoo.srouter.n300.ts
    
    * chore(actions): check_format_lint
    
    * feat(360家庭防火墙): 开屏广告
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>
    
    close #1653

---
+++ src/apps/com.qihoo.srouter.n300.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.qihoo.srouter.n300',
+  name: '360家庭防火墙',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          matches: '[id="com.qihoo.srouter.n300:id/tv_countdown"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13216607',
+        },
+      ],
+    },
+  ],
+});
