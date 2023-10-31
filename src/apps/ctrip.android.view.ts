817da7612cb0b6ab856107a9bfff3ebbd3cb3d98
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-01 00:37:30 +0800

    feat(携程旅行): 更新弹窗 (#1666)

--- src/apps/ctrip.android.view.ts
+++ src/apps/ctrip.android.view.ts
@@ -16,5 +16,12 @@ export default defineAppConfig({
         'https://i.gkd.li/import/12511071',
       ],
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'ctrip.android.publicproduct.home.view.CtripHomeActivity',
+      rules: '[text^="升级新版本"] <2 LinearLayout + [desc="关闭"]',
+      snapshotUrls: 'https://i.gkd.li/import/13218347',
+    },
   ],
 });
