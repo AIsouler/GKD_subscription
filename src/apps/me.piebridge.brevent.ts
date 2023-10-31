4845864aa5a930701ecc631d0ac531e4afffea50
Lynn <352281674@qq.com>
2023-11-01 00:43:30 +0800

    feat(黑阈): 权限不足弹窗 (#1667)
    * feat(黑阈): 权限不足取消
    
    * feat(黑阈): 权限不足弹窗
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/me.piebridge.brevent.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'me.piebridge.brevent',
+  name: '黑阈',
+  groups: [
+    {
+      enable: false,
+      key: 1,
+      name: '权限不足弹窗',
+      desc: '点击取消',
+      activityIds: ['me.piebridge.brevent.ui.BreventActivity'],
+      actionMaximum: 1,
+      quickFind: true,
+      rules: [
+        {
+          matches: [
+            '[id="android:id/message"][text^=`黑阈服务权限不足`]',
+            '[text=`取消`][id^=`android:id/button`]',
+          ],
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13218439',
+        },
+      ],
+    },
+  ],
+});
