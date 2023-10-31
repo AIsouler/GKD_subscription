b92220628330091269b90ab7d1f869a636f9a654
yanyu2023 <130368710+yanyu2023@users.noreply.github.com>
2023-11-01 00:26:45 +0800

    feat(豆果美食): 开屏广告 (#1654)
    * Create com.ikang.official.ts
    
    * Delete src/apps/com.ikang.official.ts
    
    * feat(豆果美食): 开屏广告
    
    * feat(豆果美食): 开屏广告
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.douguo.recipe.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.douguo.recipe',
+  name: '豆果美食',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[id="com.douguo.recipe:id/ad_jump"] > [text^="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13215621',
+        },
+      ],
+    },
+  ],
+});
