17614bf81ab2d9014fe89e8e12c8a6a4c735383e
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-10-31 23:34:13 +0800

    feat(优衣库 ): 开屏广告; 弹窗广告 (#1635)
    * feat(优衣库 ): 开屏广告及弹窗广告
    
    * 修订语法
    
    * feat(优衣库): 开屏广告
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.yek.android.uniqlo.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.yek.android.uniqlo',
+  name: '优衣库',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: 'TextView[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13212257',
+    },
+    {
+      key: 1,
+      name: '弹窗广告',
+      quickFind: true,
+      activityIds: 'com.yek.android.uniqlo.uiux.Home.activity.HomeActivity',
+      rules: 'ImageView[id="com.yek.android.uniqlo:id/pop_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13212320',
+    },
+  ],
+});
