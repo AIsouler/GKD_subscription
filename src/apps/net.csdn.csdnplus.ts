776534e35ae3d8cf0fef21884a7583d3f338113d
walklty <47232102+walklty@users.noreply.github.com>
2023-11-01 15:58:40 +0800

    feat(CSDN): 规则优化 (#1637)
    * Update net.csdn.csdnplus.ts
    
    * chore(actions): check_format_lint
    
    * Update net.csdn.csdnplus.ts
    
    * feat(CSDN): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/net.csdn.csdnplus.ts
+++ src/apps/net.csdn.csdnplus.ts
@@ -7,21 +7,29 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'net.csdn.csdnplus.activity.SplashActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
-          matches: '[id="net.csdn.csdnplus:id/tv_skip_splashimage"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12673680',
+          quickFind: true,
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12673680'],
         },
         {
+          // 无法使用 quickFind
           matches: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12838461',
+          snapshotUrls: 'https://i.gkd.li/import/13224627',
         },
       ],
     },
     {
       key: 1,
       name: '更新弹窗',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       activityIds: [
         'net.csdn.csdnplus.activity.MainActivity',
         'net.csdn.csdnplus.activity.SetActivity',
@@ -29,7 +37,7 @@ export default defineAppConfig({
       rules: [
         {
           matches:
-            '@[id="net.csdn.csdnplus:id/cancel"][text="以后再说"] +(2) [id="net.csdn.csdnplus:id/update"][text="立即安装"]',
+            '[id="net.csdn.csdnplus:id/update"] -2 [id="net.csdn.csdnplus:id/cancel"]',
           snapshotUrls: [
             'https://gkd-kit.gitee.io/import/12673693',
             'https://gkd-kit.gitee.io/import/12673654',
@@ -40,31 +48,43 @@ export default defineAppConfig({
     {
       key: 2,
       name: '信息流广告',
+      quickFind: true,
       activityIds: ['net.csdn.csdnplus.activity.MainActivity'],
       rules: [
         {
           key: 1,
-          matches:
-            'TextView[id=null][text="广告"] + ImageView[id="net.csdn.csdnplus:id/iv_more"][clickable=true]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12673738'],
+          matches: '[text="广告"] + [id="net.csdn.csdnplus:id/iv_more"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12673738',
+            'https://i.gkd.li/import/13224538',
+          ],
         },
         {
-          preKeys: [1],
+          preKeys: 1,
           key: 2,
           matches:
-            '[id="net.csdn.csdnplus:id/img_feedback_title"][text="虚假广告"]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12673787'],
+            '@[clickable=true] > [id="net.csdn.csdnplus:id/img_feedback_title"][text="重复推荐该广告"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12673787',
+            'https://i.gkd.li/import/13224551',
+          ],
         },
       ],
     },
     {
       enable: false,
       key: 10,
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       name: '请求推送通知弹窗',
       activityIds: 'net.csdn.csdnplus.activity.MainActivity',
       rules: {
-        matches:
-          '@[id="net.csdn.csdnplus:id/iv_close"][clickable=true] + LinearLayout > [text="开启推送通知"]',
+        matches: [
+          '[text="开启推送通知"]',
+          '[id="net.csdn.csdnplus:id/iv_close"]',
+        ],
         snapshotUrls: ['https://gkd-kit.gitee.io/import/12673638'],
       },
     },
