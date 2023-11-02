8f57f8d105070759cd08a4fec3a895cd54b38e53
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-02 21:14:37 +0800

    feat(华为应用市场): 更新弹窗、开屏广告（优化规则） (#1717)
    * feat 更新弹窗
    
    * feat 开屏广告（优化规则）
    
    * feat 广告卡片
    
    * Revert "feat 广告卡片"
    
    This reverts commit a764cc2c3e9875b532fd25098f72f5e0b14c0db6.

--- src/apps/com.huawei.appmarket.ts
+++ src/apps/com.huawei.appmarket.ts
@@ -7,13 +7,19 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: ['com.huawei.appmarket.MainActivity'],
-      rules: [
-        {
-          matches: '[id="com.huawei.appmarket:id/skip_textview"]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12683196'],
-        },
-      ],
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.huawei.appmarket:id/skip_textview"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12683196',
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.huawei.appmarket.MarketActivity',
+      rules: '@[text="以后再说"] + [text="立即更新"]',
+      snapshotUrls: 'https://i.gkd.li/import/13228520',
     },
   ],
 });
