d6d350dd2ec5a4fe8eb99d0aa1159ed1fc435876
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-02 20:38:09 +0800

    feat(小猿口算): 开屏广告+评分弹窗 (#1699)
    * feat(小猿口算): 开屏广告+评分弹窗
    
    * feat(小猿口算): 开屏广告
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/com.fenbi.android.leo.ts
@@ -0,0 +1,29 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.fenbi.android.leo',
+  name: '小猿口算',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13226136',
+    },
+    {
+      enable: false,
+      key: 1,
+      name: '评分弹窗',
+      quickFind: true,
+      actionMaximum: 1,
+      activityIds:
+        'com.fenbi.android.leo.imgsearch.sdk.activity.NewCheckCameraActivity',
+      rules: '@[text="残忍拒绝"] + [text*="好评"][text.length<=10]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13226140',
+    },
+  ],
+});
