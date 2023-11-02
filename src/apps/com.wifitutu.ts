00700e0c558450a3bcf16c02dc3ad989a490f9bb
董红鹏 <1140049363@qq.com>
2023-11-02 22:22:43 +0800

    feat(WiFi万能钥匙): 开屏广告; 首页-WiFi列表广告 (#1745)
    * fix(喵上漫画):补充activeid
    
    * feat(喵上漫画): 开屏广告-规则优化
    
    * fix(微信):补充朋友圈广告activityId
    
    * 添加wifi万能钥匙(com.wifitutu)规则
    
    * Update com.wifitutu.ts
    
    * chore(actions): check_format_lint
    
    * feat(WiFi万能钥匙): 规则优化
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.wifitutu.ts
@@ -0,0 +1,38 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wifitutu',
+  name: 'WiFi万能钥匙',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      desc: '点击右上角跳过',
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12992451',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '首页-WiFi列表广告',
+      activityIds: ['com.wifitutu.ui.main.MainActivity'],
+      rules: [
+        {
+          matches: 'View < [desc^="dislike"][visibleToUser=true]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13233916',
+        },
+        {
+          matches:
+            '@Image[text=""] < View +n View > View > TextView[text="广告"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13234048',
+        },
+      ],
+    },
+  ],
+});
