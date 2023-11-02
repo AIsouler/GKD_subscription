700c2ca2cd0bc1700ecc927d1822bce919feb3be
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-02 21:34:31 +0800

    feat(途虎养车): 开屏广告（优化规则）、请求通知权限弹窗、广告弹窗 (#1724)
    * feat 开屏广告（优化规则）
    
    * feat 请求通知权限弹窗
    
    * feat 广告弹窗
    
    * feat(途虎养车): 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/cn.TuHu.android.ts
+++ src/apps/cn.TuHu.android.ts
@@ -7,11 +7,29 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'cn.TuHu.Activity.tuhutab.TuHuTabActivity',
-      matchLauncher: true,
       quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: '[id="cn.TuHu.android:id/rl_activity_welcome_jump"]',
       snapshotUrls: 'https://i.gkd.li/import/12846408',
+    },
+    {
+      key: 1,
+      name: '广告弹窗',
+      quickFind: true,
+      activityIds: 'cn.TuHu.Activity.tuhutab.TuHuTabActivity',
+      rules:
+        '@[id="cn.TuHu.android:id/imgClose"] - [id="cn.TuHu.android:id/rlContent"] > [id="cn.TuHu.android:id/img_jump"]',
+      snapshotUrls: 'https://i.gkd.li/import/13228818',
+    },
+    {
+      key: 10,
+      quickFind: true,
+      name: '请求通知权限弹窗',
+      activityIds: 'cn.TuHu.Activity.tuhutab.TuHuTabActivity',
+      rules: '[text="开启推送提醒"] <2 * + [id="cn.TuHu.android:id/imgClose"]',
+      snapshotUrls: 'https://i.gkd.li/import/13228796',
     },
   ],
 });
