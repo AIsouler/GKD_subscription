c990f6b939018b4595e1cc36fc7494d72ac45a89
walklty <47232102+walklty@users.noreply.github.com>
2023-11-02 20:11:42 +0800

    feat(车来了): 开屏广告 (#1696)
    * Update com.ygkj.chelaile.standard.ts
    
    * chore(actions): check_format_lint
    
    * feat(车来了): 开屏广告
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.ygkj.chelaile.standard.ts
+++ src/apps/com.ygkj.chelaile.standard.ts
@@ -7,10 +7,14 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'dev.xesam.chelaile.app.module.func.SplashActivity',
-      matchLauncher: true,
-      rules: '[id="com.ygkj.chelaile.standard:id/cll_splash_ad_skip"]',
-      snapshotUrls: 'https://i.gkd.li/import/13062992',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13062992',
+        'https://gkd-kit.gitee.io/import/13225111', //无ID
+      ],
     },
     {
       key: 1,
