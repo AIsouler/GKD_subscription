2096020b95db1a8191ae6acbed741fc72d043732
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-02 22:14:12 +0800

    feat(全球上网): 广告弹窗 (#1759)

---
+++ src/apps/com.miui.virtualsim.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.miui.virtualsim',
+  name: '全球上网',
+  groups: [
+    {
+      key: 1,
+      name: '广告弹窗',
+      activityIds: 'com.mobile.businesshall.ui.common.RecommendPopupActivity',
+      rules: '[id="com.miui.virtualsim:id/img_close_btn"]',
+      snapshotUrls: 'https://i.gkd.li/import/13238467',
+    },
+  ],
+});
