096a5c0aa6979daad94bede6c2a64c1ccdc4515f
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-02 20:38:45 +0800

    feat(实用工具箱): 广告弹窗 (#1700)
    * feat(实用工具箱):广告弹窗
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1684

---
+++ src/apps/com.hj119.sygjx.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.hj119.sygjx',
+  name: '实用工具箱',
+  groups: [
+    {
+      key: 2,
+      name: '广告弹窗',
+      quickFind: true,
+      rules:
+        '@ImageView < FrameLayout +2 FrameLayout[childCount=2] > FrameLayout > TextView[text="立即下载"]',
+      snapshotUrls: 'https://i.gkd.li/import/13226595',
+    },
+  ],
+});
