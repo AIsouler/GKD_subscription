21e26b81d4cd13cfa9a7b9944e8961fe49fc6eda
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-02 21:54:29 +0800

    feat(剪映): 开屏广告 (#1729)
    * feat 开屏广告
    
    * feat(剪映): 开屏广告
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.lemon.lv.ts
+++ src/apps/com.lemon.lv.ts
@@ -5,6 +5,15 @@ export default defineAppConfig({
   name: '剪映',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+      snapshotUrls: 'https://i.gkd.li/import/13229094',
+    },
+    {
       key: 0,
       name: '导出界面底部广告',
       desc: '关闭完成导出后的下方广告',
