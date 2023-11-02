0ac4b7eca7de0132602f4fed35094e5659fbc864
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-02 19:51:23 +0800

    feat(miui主题壁纸): 广告弹窗 (#1669)
    * feat
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1645

--- src/apps/com.android.thememanager.ts
+++ src/apps/com.android.thememanager.ts
@@ -10,5 +10,13 @@ export default defineAppConfig({
       desc: '注意如果使用ADB禁用了MIUI广告组件,点击此按钮会无反应,可关闭此规则,避免过多相同点击记录',
       rules: '[id=`com.android.thememanager:id/ad_close_btn`]',
     },
+    {
+      key: 2,
+      name: '广告弹窗',
+      quickFind: true,
+      activityIds: 'com.android.thememanager.ThemeResourceTabActivity',
+      rules: 'ImageView[id="com.android.thememanager:id/cancel"][desc="关闭"]',
+      snapshotUrls: 'https://i.gkd.li/import/13215038',
+    },
   ],
 });
