4c70833c1ddb847b294b425d15cc4738f6af406b
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-02 19:59:25 +0800

    feat(手机天猫): 弹窗广告 (#1677)
    * feat(手机天猫): 弹窗广告
    
    * feat(手机天猫): 弹窗广告
    
    * Update com.tmall.wireless.ts
    
    * 修正单行过长问题
    
    * Update com.tmall.wireless.ts

--- src/apps/com.tmall.wireless.ts
+++ src/apps/com.tmall.wireless.ts
@@ -21,5 +21,24 @@ export default defineAppConfig({
       rules: '[id="com.tmall.wireless:id/update_button_cancel"] [text="取消"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/13162603',
     },
+    {
+      enable: false,
+      key: 2,
+      name: '弹窗广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      activityIds: [
+        'com.tmall.wireless.maintab.module.TMMainTabActivity',
+        'com.tmall.wireless.turboweb.container.TurboWebViewActivity',
+        'com.tmall.wireless.webview.TMCommonWebViewActivity',
+      ],
+      rules:
+        'Image[id=null][clickable=false] < View + Image[id=null][clickable=true]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/13222394', //com.tmall.wireless.maintab.module.TMMainTabActivity
+        'https://gkd-kit.gitee.io/import/13222500', //com.tmall.wireless.turboweb.container.TurboWebViewActivity
+        'https://gkd-kit.gitee.io/import/13222510', //com.tmall.wireless.webview.TMCommonWebViewActivity
+      ],
+    },
   ],
 });
