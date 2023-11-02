8c9229df6da5a5c80d3f19b0414e1296438c7fe4
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-02 19:55:48 +0800

    feat(小红书):关闭开启通知弹窗新ActivityId (#1673)
    feat(小红书):关闭开启通知弹窗新ActivityId

--- src/apps/com.xingin.xhs.ts
+++ src/apps/com.xingin.xhs.ts
@@ -22,9 +22,15 @@ export default defineAppConfig({
       name: '关闭开启通知弹窗',
       quickFind: true,
       actionMaximum: 1,
-      activityIds: 'com.xingin.xhs.index.v2.IndexActivityV2',
+      activityIds: [
+        'com.xingin.xhs.index.v2.IndexActivityV2',
+        'com.xingin.xhs.notification.NotificationAuthorizationTranslucentActivity',
+      ],
       rules: '@ImageView <2 FrameLayout >2 [text^="打开通知"]',
-      snapshotUrls: 'https://i.gkd.li/import/13195753',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13195753', //com.xingin.xhs.index.v2.IndexActivityV2
+        'https://gkd-kit.gitee.io/import/13222356', //com.xingin.xhs.notification.NotificationAuthorizationTranslucentActivity
+      ],
     },
   ],
 });
