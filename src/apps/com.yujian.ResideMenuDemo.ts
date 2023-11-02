6e0e51b748fb39ab0f56d6cf50b83a67648a9777
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-02 20:09:23 +0800

    feat(雨见): 更新提示 (#1692)

---
+++ src/apps/com.yujian.ResideMenuDemo.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.yujian.ResideMenuDemo',
+  name: '雨见',
+  groups: [
+    {
+      enable: false,
+      key: 1,
+      name: '更新提示',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: 'TextView[id="com.yujian.ResideMenuDemo:id/btn_selectOther"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13224234',
+    },
+  ],
+});
