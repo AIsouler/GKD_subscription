3bb21f055a21cda89def47fbda4b83410c768753
walklty <47232102+walklty@users.noreply.github.com>
2023-11-02 21:59:00 +0800

    feat(微音乐): 弹窗广告 (#1737)
    * Update com.zhouyu.music.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1682

--- src/apps/com.zhouyu.music.ts
+++ src/apps/com.zhouyu.music.ts
@@ -8,11 +8,11 @@ export default defineAppConfig({
       key: 1,
       name: '广告弹窗',
       activityIds: 'com.zhouyu.music.activities.MainActivity',
-      rules:
-        '@TextView[id=null][clickable=true] + View > View > View [text$="广告"]',
+      rules: '@[id=null][clickable=true] + View >n View [text$="广告"]',
       snapshotUrls: [
         'https://i.gkd.li/import/13062330',
         'https://i.gkd.li/import/13068583',
+        'https://gkd-kit.gitee.io/import/13231850',
       ],
       exampleUrls:
         'https://github.com/gkd-kit/inspect/assets/38517192/798afc53-4aaf-481f-acb1-7c193bd8e6d6',
