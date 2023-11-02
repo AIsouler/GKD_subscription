c7d7a1722cd8f3d275016afa386c1231341b840c
Frank Zhang <xe5700@outlook.com>
2023-11-02 22:10:41 +0800

    feat(酷家乐): 开屏广告 (#1751)
    * feat(酷家乐): 开屏广告
    
    * chore(actions): check_format_lint
    
    * feat(酷家乐设计师): 开屏广告
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/com.qunhe.designer.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.qunhe.designer',
+  name: '酷家乐设计师',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      quickFind: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13226570',
+        },
+      ],
+    },
+  ],
+});
