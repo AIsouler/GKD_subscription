8d24876233e36a4036c751ab27c2c28255e92084
walklty <47232102+walklty@users.noreply.github.com>
2023-11-02 19:53:53 +0800

    fix(直播吧): 信息流不工作;开屏优化 (#1670)
    * Update android.zhibo8.ts
    
    * chore(actions): check_format_lint
    
    * Update android.zhibo8.ts
    
    * Update android.zhibo8.ts
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1656

--- src/apps/android.zhibo8.ts
+++ src/apps/android.zhibo8.ts
@@ -7,22 +7,24 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      matchLauncher: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       quickFind: true,
       rules: [
         {
-          matches: '[id="android.zhibo8:id/tv_skip_text"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12700387',
-        },
-        {
-          matches: '[id="android.zhibo8:id/asv_splash"] TextView[text^="跳过"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12661109',
+          matches: 'TextView[text^="跳过"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12700387',
+            'https://gkd-kit.gitee.io/import/12661109',
+          ],
         },
       ],
     },
     {
       key: 1,
       name: '信息流广告',
+      quickFind: true,
       activityIds: 'android.zhibo8.ui.contollers.main.MainActivity',
       rules: [
         {
@@ -33,9 +35,10 @@ export default defineAppConfig({
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12841134',
         },
         {
-          preKeys: 0,
+          key: 1, //不重叠不需要preKeys
           name: '点击[不感兴趣]',
-          matches: '[id="android.zhibo8:id/tv_title"][text="不感兴趣"]',
+          matches:
+            '@LinearLayout > [id="android.zhibo8:id/tv_title"][text="不感兴趣"]', // issues/1656, 直接指向text可能不工作
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12841135',
         },
       ],
