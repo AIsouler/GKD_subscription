9753486057c60b7cbf1eae8d52c5bd9853dd7573
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-02 22:02:05 +0800

    feat(微信读书): 少年模式弹窗、系统推送弹窗（补充 activityId） (#1742)
    * feat 少年模式弹窗
    
    * feat 系统推送弹窗（补充 activityId）
    
    * feat(微信读书): 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.tencent.weread.ts
+++ src/apps/com.tencent.weread.ts
@@ -8,15 +8,34 @@ export default defineAppConfig({
       key: 1,
       name: '系统推送弹窗',
       desc: '系统推送弹窗-点击取消',
-      activityIds:
+      quickFind: true,
+      actionMaximum: 1,
+      matchTime: 30000,
+      resetMatch: 'app',
+      activityIds: [
         'com.tencent.weread.module.bottomSheet.BottomSheetForFragment',
+        'com.tencent.weread.WeReadFragmentActivity',
+      ],
       rules: [
         {
-          matches:
-            '[id="com.tencent.weread:id/open_notification_title"] +(n) [id="com.tencent.weread:id/open_notification_close"]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12642247'],
+          matches: '[id="com.tencent.weread:id/open_notification_close"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12642247',
+            'https://i.gkd.li/import/13233735',
+          ],
         },
       ],
+    },
+    {
+      key: 2,
+      name: '青少年模式弹窗',
+      quickFind: true,
+      actionMaximum: 1,
+      matchTime: 30000,
+      resetMatch: 'app',
+      activityIds: 'com.tencent.weread.WeReadFragmentActivity',
+      rules: '[text="设置青少年模式"] - LinearLayout >2 [text="我知道了"]',
+      snapshotUrls: 'https://i.gkd.li/import/13233668',
     },
   ],
 });
