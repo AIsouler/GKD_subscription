1c4d7eb5d5e5e0e597e46653ae4dc972727a5b56
yanyu2023 <130368710+yanyu2023@users.noreply.github.com>
2023-11-02 20:51:47 +0800

    feat(猴子分身): 购买会员弹窗 (#1703)
    * Create com.ikang.official.ts
    
    * Delete src/apps/com.ikang.official.ts
    
    * feat(猴子分身): 购买会员弹窗
    
    * feat(猴子分身): 购买会员弹窗
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/cn.chuci.and.wkfenshen.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.chuci.and.wkfenshen',
+  name: '猴子分身',
+  groups: [
+    {
+      key: 1,
+      name: '购买会员弹窗',
+      activityIds: 'cn.chuci.and.wkfenshen.ActMain',
+      rules: [
+        {
+          quickFind: true,
+          matches: [
+            '[id="cn.chuci.and.wkfenshen:id/dialog_local_buy_btn"]',
+            '[id="cn.chuci.and.wkfenshen:id/action_close"]',
+          ],
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13226988',
+        },
+      ],
+    },
+  ],
+});
