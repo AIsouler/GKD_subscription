f6d65471780dd06c9adb63c6e50bf3d4ccfc94a8
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-04 01:23:29 +0800

    feat(招商银行): 请求定位权限弹窗（优化规则） (#1794)
    * feat 请求定位权限弹窗（优化规则）
    
    * feat(招商银行): 请求定位权限弹窗
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/cmb.pb.ts
+++ src/apps/cmb.pb.ts
@@ -20,14 +20,17 @@ export default defineAppConfig({
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12706022',
     },
     {
-      enable: false,
       key: 11,
       name: '请求定位权限弹窗',
+      quickFind: true,
       activityIds:
         'cmb.pb.app.h5container.webviewcontainer.PBWebContainerActivity',
       rules:
-        '@[id="cmb.pb:id/ivDelete"][desc="关闭"] - FrameLayout > [text*="开启定位"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12706029',
+        '@[id="cmb.pb:id/ivDelete"][desc="关闭"] - FrameLayout > [id="cmb.pb:id/tvDescription"][text*="获取您的位置"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12706029',
+        'https://i.gkd.li/import/13248893',
+      ],
     },
   ],
 });
