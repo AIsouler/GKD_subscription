c5cd0d8367e693c75d986bb4656b77ab061b9f05
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-04 00:36:04 +0800

    feat(网上国网): 开屏广告 (#1770)
    close #1764

--- src/apps/com.sgcc.wsgw.cn.ts
+++ src/apps/com.sgcc.wsgw.cn.ts
@@ -5,6 +5,15 @@ export default defineAppConfig({
   name: '网上国网',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: 'TextView[text*="跳过"][text.length<10]',
+      snapshotUrls: 'https://i.gkd.li/import/13242169',
+    },
+    {
       key: 1,
       name: '首页弹窗广告',
       activityIds: 'com.sgcc.wsgw.mainbundle.NewsSyRemind2Activity',
