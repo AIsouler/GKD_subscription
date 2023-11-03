8cf31619ad500b17143fec2f114995bab8e37e5a
walklty <47232102+walklty@users.noreply.github.com>
2023-11-04 00:45:46 +0800

    feat(小猿搜题): 升级提示 (#1782)
    * Create com.fenbi.android.solar.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #1779

---
+++ src/apps/com.fenbi.android.solar.ts
@@ -0,0 +1,25 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.fenbi.android.solar',
+  name: '小猿搜题',
+  groups: [
+    {
+      key: 1,
+      name: '升级提示',
+      matchLauncher: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          matches: [
+            '[id="com.fenbi.android.solar:id/text_upgrade"]',
+            '[id="com.fenbi.android.solar:id/img_close"]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13246056',
+        },
+      ],
+    },
+  ],
+});
