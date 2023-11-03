56343c1c900ef18f379d7e8ef8ea727449dcae33
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-04 01:24:54 +0800

    feat(京东金融): 通知提醒 (#1799)

--- src/apps/com.jd.jrapp.ts
+++ src/apps/com.jd.jrapp.ts
@@ -17,5 +17,14 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '通知提醒',
+      quickFind: true,
+      actionMaximum: 1,
+      activityIds: 'com.jd.jrapp.bm.mainbox.main.MainActivity',
+      rules: 'TextView[text^="打开通知"] +2 [id="com.jd.jrapp:id/iv_close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13249998',
+    },
   ],
 });
