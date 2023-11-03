3e679b89103613c5bf7a1b3f005c57ac6ff7fc7e
二刺螈 <i@songe.li>
2023-11-04 01:57:32 +0800

    feat(WeiboBig): 流量继续播放视频 (#1795)

---
+++ src/apps/com.sina.weibocare.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.sina.weibocare',
+  name: 'WeiboBig',
+  groups: [
+    {
+      enable: false,
+      key: 1,
+      name: '流量继续播放视频',
+      desc: '点击[播放]',
+      quickFind: true,
+      activityIds: 'com.weico.international.activity.MainFragmentActivity',
+      rules: '[id="com.sina.weibocare:id/ed_btn_positive"][text="播放"]',
+      snapshotUrls: 'https://i.gkd.li/import/13253223',
+    },
+  ],
+});
