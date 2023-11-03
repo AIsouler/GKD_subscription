a088c1200597d033824519539fb800dc32e891ce
walklty <47232102+walklty@users.noreply.github.com>
2023-11-04 00:43:39 +0800

    feat(返利): 应用内弹窗 (#1777)
    * Update com.fanli.android.apps.ts
    
    * chore(actions): check_format_lint
    
    * feat(开屏广告): 应用内弹窗
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>
    
    close #1775

--- src/apps/com.fanli.android.apps.ts
+++ src/apps/com.fanli.android.apps.ts
@@ -14,5 +14,13 @@ export default defineAppConfig({
       rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13227495',
     },
+    {
+      key: 1,
+      name: '应用内弹窗',
+      activityIds:
+        'com.fanli.android.module.webview.ui.activity.TransparentBrowserInnerActivity',
+      rules: '[id="J_poplayer_box"] >n @View + [text="main_bkg"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13245808',
+    },
   ],
 });
