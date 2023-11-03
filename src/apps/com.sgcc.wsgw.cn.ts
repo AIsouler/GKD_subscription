046a86359ec5e5afec905124942c3cccb492e325
walklty <47232102+walklty@users.noreply.github.com>
2023-11-04 01:11:08 +0800

    feat(网上国网): 弹窗, 开屏 (#1788)
    * Update com.sgcc.wsgw.cn.ts
    
    * chore(actions): check_format_lint
    
    * Update com.sgcc.wsgw.cn.ts
    
    * Update com.sgcc.wsgw.cn.ts
    
    * Update com.sgcc.wsgw.cn.ts
    
    * Update com.sgcc.wsgw.cn.ts
    
    * Update com.sgcc.wsgw.cn.ts
    
    * feat(网上国网): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/com.sgcc.wsgw.cn.ts
+++ src/apps/com.sgcc.wsgw.cn.ts
@@ -10,16 +10,50 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: 'TextView[text*="跳过"][text.length<10]',
-      snapshotUrls: 'https://i.gkd.li/import/13242169',
+      quickFind: true,
+      rules: '[text^="跳过"][text.length<10]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/13247957', 'https://i.gkd.li/import/13242169'],
     },
     {
       key: 1,
       name: '首页弹窗广告',
-      activityIds: 'com.sgcc.wsgw.mainbundle.NewsSyRemind2Activity',
-      rules:
-        '[id="com.sgcc.wsgw.cn:id/news_remind_laytout"] > RelativeLayout > [id="com.sgcc.wsgw.cn:id/btn_remind_close"]',
-      snapshotUrls: 'https://i.gkd.li/import/12745042',
+      matchLauncher: true,
+      activityIds: [
+        'com.sgcc.wsgw.mainbundle.NewsSyRemind2Activity',
+        'com.sgcc.wsgw.rnbundle.activity.HomeReactActivity',
+      ],
+      rules: [
+        {
+          key: 1,
+          actionCd: 2000,
+          quickFind: true,
+          matches: '@[focusable=true] + [text="今日不再出现"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12745042',
+            'https://gkd-kit.gitee.io/import/13247655',
+          ],
+        },
+        {
+          preKeys: 1,
+          key: 2,
+          actionDelay: 300, //测试时没有delay点击失效
+          matches:
+            'ViewGroup[childCount=1] > @ViewGroup[childCount=1] > ImageView',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/13247655',
+            'https://gkd-kit.gitee.io/import/13247655',
+          ],
+        },
+        {
+          preKeys: 1,
+          key: 3,
+          actionDelay: 300,
+          quickFind: true,
+          matches:
+            '[id="com.sgcc.wsgw.cn:id/news_remind_laytout"] >n [id="com.sgcc.wsgw.cn:id/btn_remind_close"]',
+          snapshotUrls: ['https://i.gkd.li/import/12745042'],
+        },
+      ],
     },
   ],
 });
