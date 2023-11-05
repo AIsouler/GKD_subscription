3b0e62202884f5973c01d3aeac09e74c4863bfae
yanyu2023 <130368710+yanyu2023@users.noreply.github.com>
2023-11-05 23:07:44 +0800

    feat(麦当劳): 开启通知弹窗 (#1859)
    * Create com.ikang.official.ts
    
    * Delete src/apps/com.ikang.official.ts
    
    * feat(麦当劳): 开启通知弹窗

--- src/apps/com.mcdonalds.gma.cn.ts
+++ src/apps/com.mcdonalds.gma.cn.ts
@@ -11,5 +11,13 @@ export default defineAppConfig({
       rules: '[id="com.mcdonalds.gma.cn:id/tv_skip"]',
       snapshotUrls: 'https://i.gkd.li/import/12740193',
     },
+    {
+      key: 1,
+      name: '开启通知服务',
+      desc: '选择[取消]',
+      activityIds: 'com.mcdonalds.gma.cn.activity.MainActivity',
+      rules: '[id="com.mcdonalds.gma.cn:id/tv_cancel"][text="取消"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13259242',
+    },
   ],
 });
