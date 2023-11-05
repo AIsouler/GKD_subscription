298ddf8bb2c3168bd3ac865b01abb625a63574bf
格子 <8309713+MoeGrid@users.noreply.github.com>
2023-11-05 19:22:34 +0800

    feat(萤石云视频): 首页广告;规则优化 (#1818)
    * feat(萤石云视频): 首页广告
    
    * feat(萤石云视频): 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.videogo.ts
+++ src/apps/com.videogo.ts
@@ -7,11 +7,32 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: ['com.videogo.login.LoadingActivity'],
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           matches: '[id="com.videogo:id/ad_skip_tv"]',
           snapshotUrls: 'https://i.gkd.li/import/12565356',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '主页广告',
+      quickFind: true,
+      activityIds: ['com.videogo.main.MainTabActivity'],
+      rules: [
+        {
+          key: 0,
+          matches: '[id="com.videogo:id/ad_close"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13255684',
+        },
+        {
+          preKeys: 0,
+          matches: '[id="com.videogo:id/ad_closead_nointerest"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13255697',
         },
       ],
     },
