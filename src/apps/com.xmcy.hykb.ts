7ed001b35cc1c3cfea068192430d23808e257348
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-05 23:12:57 +0800

    feat(好游快爆): 广告弹窗 (#1863)
    * feat 广告弹窗
    
    * feat(好游快爆): 规则优化
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.xmcy.hykb.ts
+++ src/apps/com.xmcy.hykb.ts
@@ -7,17 +7,29 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.xmcy.hykb.app.ui.splash.SplashActivity',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: '[id="com.xmcy.hykb:id/skip"][text^="跳过"]',
       snapshotUrls: 'https://i.gkd.li/import/12774856',
     },
     {
       key: 1,
       name: '更新弹窗',
+      quickFind: true,
       activityIds: 'com.xmcy.hykb.app.dialog.UpdateDialog',
       rules:
         '@[id="com.xmcy.hykb:id/iv_inner_button_close"] - RelativeLayout >n [id="com.xmcy.hykb:id/text_update_tip"]',
       snapshotUrls: 'https://i.gkd.li/import/12870604',
+    },
+    {
+      key: 2,
+      name: '广告弹窗',
+      quickFind: true,
+      activityIds: 'com.xmcy.hykb.app.ui.main.MainActivity',
+      rules: '[id="com.xmcy.hykb:id/dialog_home_notice_image_close"]',
+      snapshotUrls: 'https://i.gkd.li/import/13259427',
     },
   ],
 });
