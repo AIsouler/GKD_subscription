cdcaa64493540c1db4b6791b544578f6f7ef8459
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-05 19:11:22 +0800

    feat(抖音): 青少年模式弹窗 (#1809)

---
+++ src/apps/com.ss.android.ugc.aweme.mobile.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.ss.android.ugc.aweme.mobile',
+  name: '抖音',
+  groups: [
+    {
+      key: 1,
+      name: '青少年模式弹窗',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules:
+        '[id="com.ss.android.ugc.aweme.mobile:id/teen_mode_title"] +n [id="com.ss.android.ugc.aweme.mobile:id/bt_i_know"]',
+      snapshotUrls: 'https://i.gkd.li/import/13255513',
+    },
+  ],
+});
