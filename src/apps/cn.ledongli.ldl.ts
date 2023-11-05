39cf18fb8531f2747f0f0fe5c4e93102a1e5fb74
yanyu2023 <130368710+yanyu2023@users.noreply.github.com>
2023-11-05 23:07:20 +0800

    fix(乐动力): 开屏广告 (#1858)
    * Create com.ikang.official.ts
    
    * Delete src/apps/com.ikang.official.ts
    
    * fix(乐动力): 开屏广告

--- src/apps/cn.ledongli.ldl.ts
+++ src/apps/cn.ledongli.ldl.ts
@@ -7,9 +7,14 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'cn.ledongli.ldl.adsdk.activity.AdsOriginalActivity',
-      rules: 'TextView[text="跳过"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12668468',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: 'TextView[text^="跳过"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12668468', //text="跳过"
+        'https://gkd-kit.gitee.io/import/13259199', //text="跳过 "
+      ],
     },
   ],
 });
