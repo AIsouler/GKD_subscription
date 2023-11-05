28ef8f3af387c59e42169a8df0750c9579339288
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-05 19:48:39 +0800

    feat(Gmail): 信息流广告 (#1828)
    * feat(Gmail): 信息流广告
    
    * chore(actions): check_format_lint
    
    * feat(Gmail): 信息流广告
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>
    
    close #1816

---
+++ src/apps/com.google.android.gm.ts
@@ -0,0 +1,43 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.google.android.gm',
+  name: 'Gmail',
+  groups: [
+    {
+      key: 2,
+      name: '信息流广告',
+      rules: [
+        {
+          quickFind: true,
+          key: -1,
+          name: '点击广告扩展-1',
+          matches:
+            '[id="com.google.android.gm:id/button_chip_ad_teaser_info_icon"]',
+          snapshotUrls: 'https://i.gkd.li/import/13255698',
+        },
+        {
+          quickFind: true,
+          key: 0,
+          name: '点击广告扩展-2',
+          matches: '[id="com.google.android.gm:id/basic_ad_teaser_info_icon"]',
+          snapshotUrls: 'https://i.gkd.li/import/13255698',
+        },
+        {
+          preKeys: [-1, 0],
+          key: 1,
+          name: '广告中心-点击屏蔽',
+          matches: 'Button[text="屏蔽此广告"]', // TODO quickFind 优化改进 Button[text="屏蔽此广告"] <<n [id="com.google.android.gm:id/my_ad_center_dialog"]
+          snapshotUrls: 'https://i.gkd.li/import/13255700',
+        },
+        {
+          preKeys: 1,
+          key: 2,
+          name: '点击继续确认屏蔽',
+          matches: 'Button[text="继续"]', // TODO Button[text="继续"] <<n [id="com.google.android.gm:id/design_bottom_sheet"]
+          snapshotUrls: 'https://i.gkd.li/import/13255701',
+        },
+      ],
+    },
+  ],
+});
