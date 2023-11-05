f2e718c04db80c461ef97a2203d4ce0eb7f399d5
OldCoding <85047508+OldCoding@users.noreply.github.com>
2023-11-05 22:55:26 +0800

    feat(咪咕快游): 开屏广告 (#1850)
    * Create com.tt.turbo.ts
    
    * feat(甜糖): 开屏广告
    
    * Update com.coolapk.market.ts
    
    适配新的酷安开屏广告
    
    * chore(actions): check_format_lint
    
    * Update com.coolapk.market.ts
    
    * Update com.coolapk.market.ts
    
    * Update com.coolapk.market.ts
    
    * Update com.coolapk.market.ts
    
    * chore(actions): check_format_lint
    
    * Update com.coolapk.market.ts
    
    * Update com.coolapk.market.ts
    
    * chore(actions): check_format_lint
    
    * Update com.coolapk.market.ts
    
    * Create cn.emagsoftware.gamehall.ts
    
    支持咪咕快游
    
    * feat(咪咕快游): 开屏广告
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/cn.emagsoftware.gamehall.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.emagsoftware.gamehall',
+  name: '咪咕快游',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="cn.emagsoftware.gamehall:id/splash_skip"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13258853',
+    },
+  ],
+});
