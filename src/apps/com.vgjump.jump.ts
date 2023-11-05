73ee021017ab0be8c8b9af07a66c52ddf63a5eea
yanyu2023 <130368710+yanyu2023@users.noreply.github.com>
2023-11-05 23:05:23 +0800

    feat(Jump): 开屏广告 (#1856)
    * Create com.ikang.official.ts
    
    * Delete src/apps/com.ikang.official.ts
    
    * feat(Jump): 开屏广告

---
+++ src/apps/com.vgjump.jump.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.vgjump.jump',
+  name: 'Jump',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: 'TextView[text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13259108',
+    },
+  ],
+});
