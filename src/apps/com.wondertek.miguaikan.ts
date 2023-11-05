10d64126bf0604cea35b42b53798230da5f55d50
OldCoding <85047508+OldCoding@users.noreply.github.com>
2023-11-05 22:58:13 +0800

    feat(咪咕视频爱看版): 开屏广告 (#1851)
    * Create com.tt.turbo.ts
    
    * feat(甜糖): 开屏广告
    
    * Update com.coolapk.market.ts
    
    适配新的酷安开屏广告
    
    * chore(actions): check_format_lint
    
    * Update com.coolapk.market.ts
    
    * Update com.coolapk.market.ts
    
    * Update com.coolapk.market.ts
    
    * Update com.coolapk.market.ts
    
    * chore(actions): check_format_lint
    
    * Update com.coolapk.market.ts
    
    * Update com.coolapk.market.ts
    
    * chore(actions): check_format_lint
    
    * Update com.coolapk.market.ts
    
    * Create com.wondertek.miguaikan.ts
    
    * feat(咪咕视频爱看版): 开屏广告
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.wondertek.miguaikan.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wondertek.miguaikan',
+  name: '咪咕视频爱看版',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.wondertek.miguaikan:id/splash_time"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13258889',
+    },
+  ],
+});
