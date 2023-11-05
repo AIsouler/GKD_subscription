f5deafe937cea51113a44e83d7d15d8027d2abf5
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-05 20:31:41 +0800

    feat(乐看视频): 推荐-广告弹窗 (#1833)
    * feat(乐看视频):推荐页-广告弹窗
    
    * chore(actions): check_format_lint
    
    * feat(乐看视频): 广告弹窗
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/com.zhl.bmi.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.zhl.bmi',
+  name: '乐看视频',
+  groups: [
+    {
+      key: 2,
+      name: '广告弹窗',
+      desc: '点击右上角x',
+      activityIds: 'com.zhl.bmi.MainActivity',
+      quickFind: true,
+      rules:
+        '@ImageView < FrameLayout[childCount=1] <3 FrameLayout <2 FrameLayout < FrameLayout < [id="android:id/content"]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13255787',
+        'https://i.gkd.li/import/13255786',
+      ],
+    },
+  ],
+});
