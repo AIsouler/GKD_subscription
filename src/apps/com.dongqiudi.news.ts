baba2257dea155cc557eb7454237f76cfc10faee
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-06 23:09:22 +0800

    feat(懂球帝): 弹窗广告 (#1872)
    * feat(懂球帝)
    
    * feat(懂球帝): 开屏广告-规则优化
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>
    
    close #1843

--- src/apps/com.dongqiudi.news.ts
+++ src/apps/com.dongqiudi.news.ts
@@ -7,7 +7,9 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.dongqiudi.news.BaseSplashActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           matches:
@@ -19,6 +21,7 @@ export default defineAppConfig({
           ],
         },
         {
+          quickFind: true,
           name: '另一种倒计时广告', // 此广告要点击文字相邻的图片,如果直接点击文字会无反应或者触发广告跳转
           matches:
             '[id="com.dongqiudi.news:id/ad_skip_time"] + [id="com.dongqiudi.news:id/scale_iv"]',
@@ -33,13 +36,21 @@ export default defineAppConfig({
       key: 1,
       name: '青少年模式弹窗',
       activityIds: 'com.dongqiudi.news.MainActivity',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12621980',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: '[text*="青少年模式"] + [id="com.dongqiudi.news:id/tv_know"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12621980',
     },
     {
       key: 2,
       name: '更新弹窗',
       activityIds: 'com.dongqiudi.news.DownloadActivity',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: 'TextView[text*="新版本"] +3 TextView[text="下次提醒"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12620586',
     },
@@ -56,6 +67,7 @@ export default defineAppConfig({
       name: '首页信息流广告',
       desc: '点击广告卡片x关闭按钮-关闭反馈理由弹窗',
       activityIds: 'com.dongqiudi.news.MainActivity',
+      quickFind: true,
       rules: [
         {
           preKeys: [1, 2],
@@ -77,6 +89,15 @@ export default defineAppConfig({
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12620788',
         },
       ],
+    },
+    {
+      key: 5,
+      name: '首页-广告弹窗',
+      desc: '点击底部【x】关闭',
+      activityIds: 'com.dongqiudi.news.MainActivity',
+      quickFind: true,
+      rules: '[id="com.dongqiudi.news:id/iv_close"]',
+      snapshotUrls: 'https://i.gkd.li/import/13260467',
     },
   ],
 });
