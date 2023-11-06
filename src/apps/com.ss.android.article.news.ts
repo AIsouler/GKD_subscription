361353ef5290d9f76053f49901f4d5f7e0bdc752
walklty <47232102+walklty@users.noreply.github.com>
2023-11-06 23:17:30 +0800

    feat(今日头条): 浮窗广告 (#1879)
    * Update com.ss.android.article.news.ts
    
    * feat(今日头条): 浮窗广告
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>
    
    close #1878

--- src/apps/com.ss.android.article.news.ts
+++ src/apps/com.ss.android.article.news.ts
@@ -8,12 +8,9 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       quickFind: true,
-      matchLauncher: true,
-      activityIds: [
-        'com.ss.android.article.news.activity.MainActivity',
-        'com.bytedance.ugc.UgcDetailInfoActivity',
-        'com.ss.android.splashad.splash.view.SplashAdActivity',
-      ],
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           matches: '[childCount=1] > [text="跳过广告"]',
@@ -38,6 +35,20 @@ export default defineAppConfig({
             'https://gkd-kit.gitee.io/import/12685000',
             'https://gkd-kit.gitee.io/import/12840104',
           ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '浮窗广告',
+      quickFind: true,
+      activityIds: ['com.ss.android.article.news.activity.MainActivity'],
+      actionMaximum: 1,
+      rules: [
+        {
+          matches:
+            '@ImageView < FrameLayout[childCount=2] < FrameLayout < FrameLayout <2 FrameLayout <4 [id="android:id/tabhost"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/13262675'],
         },
       ],
     },
