ecf036e21cc9492388358c8c705614e912f2389d
walklty <47232102+walklty@users.noreply.github.com>
2023-11-09 18:15:37 +0800

    feat(全民泡泡超人): 开屏广告 (#1922)
    * Create com.tencent.tmgp.kgame.ppcr.ts
    
    * Update com.tencent.tmgp.kgame.ppcr.ts
    
    close #1920

---
+++ src/apps/com.tencent.tmgp.kgame.ppcr.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tencent.tmgp.kgame.ppcr',
+  name: '全民泡泡超人',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 15000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[id="com.tencent.ysdkcore:id/com_tencent_ysdk_icon_h5_xx"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/13276317'],
+        },
+      ],
+    },
+  ],
+});
