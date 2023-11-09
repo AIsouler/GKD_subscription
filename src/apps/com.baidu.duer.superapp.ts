8007217a43061f61f40194596a7739d6d6c95eba
lisonge <i@songe.li>
2023-11-10 00:51:04 +0800

    feat(小度): 开屏广告 (#1981)

--- src/apps/com.baidu.duer.superapp.ts
+++ src/apps/com.baidu.duer.superapp.ts
@@ -7,9 +7,22 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.baidu.duer.superapp.splash.SplashActivity',
-      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12506571',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          quickFind: true,
+          matches: '[text^="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13310527',
+          exampleUrls:
+            'https://github.com/gkd-kit/inspect/assets/38517192/5f6591b6-220e-4117-9cc1-b07375085f64',
+        },
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12506571',
+        },
+      ],
     },
   ],
 });
