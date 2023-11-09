c924e5d8a3edcf6ec1de48142abf11d4fa962b36
walklty <47232102+walklty@users.noreply.github.com>
2023-11-10 00:32:21 +0800

    feat(ourplay): 开屏, 弹窗 (#1966)
    Co-authored-by: 二刺螈 <i@songe.li>
    
    close #1963

--- src/apps/com.excean.gspace.ts
+++ src/apps/com.excean.gspace.ts
@@ -14,5 +14,17 @@ export default defineAppConfig({
       rules: '[id="com.excean.gspace:id/tv_jump"]',
       snapshotUrls: 'https://i.gkd.li/import/13296144',
     },
+    {
+      key: 1,
+      name: '弹窗广告',
+      activityIds: 'com.excelliance.kxqp.splash.SplashActivity',
+      rules: [
+        {
+          matches:
+            'FrameLayout[childCount=2] > LinearLayout[childCount=2] + FrameLayout[childCount=1] > ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13302890',
+        },
+      ],
+    },
   ],
 });
