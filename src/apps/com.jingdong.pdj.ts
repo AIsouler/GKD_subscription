99006164030c52b3c2531ff02879a69aabda5843
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-09 18:31:51 +0800

    feat(京东到家): 开屏广告 (#1930)
    * feat(京东到家): 开屏广告
    
    * feat(京东到家): 开屏广告
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.jingdong.pdj.ts
+++ src/apps/com.jingdong.pdj.ts
@@ -5,6 +5,16 @@ export default defineAppConfig({
   name: '京东到家',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[id="com.jingdong.pdj:id/adv_exit_bnt"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13293044',
+    },
+    {
       key: 1,
       name: '首页广告弹窗',
       matchTime: 10000,
