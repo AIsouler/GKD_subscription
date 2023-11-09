14a5d5ba5bd750a30acd76adb84dcdce070ba8d4
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-10 00:01:07 +0800

    feat(小步点): 广告弹窗（补充规则）、开屏广告（优化规则） (#1950)
    * feat 广告弹窗（补充规则）
    
    * feat 开屏广告（优化规则）

--- src/apps/run.xbud.android.ts
+++ src/apps/run.xbud.android.ts
@@ -7,19 +7,42 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'run.xbud.android.mvp.ui.other.SplashActivity',
-      rules: '[id="run.xbud.android:id/bodyLayout"] >n [text^="跳过"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12777125',
     },
     {
       key: 1,
-      name: '首页广告弹窗',
+      name: '广告弹窗',
       activityIds: 'run.xbud.android.mvp.ui.other.MainActivity',
-      rules:
-        '[id="run.xbud.android:id/ksad_tk_view"] >n @ViewGroup[clickable=true] > ImageView',
-      snapshotUrls: [
-        'https://i.gkd.li/import/12777133',
-        'https://i.gkd.li/import/12777134',
+      rules: [
+        {
+          key: 0,
+          name: '快手广告',
+          matches:
+            '[id="run.xbud.android:id/ksad_tk_view"] >n @ViewGroup[clickable=true] > ImageView',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12777133',
+            'https://i.gkd.li/import/12777134',
+          ],
+        },
+        {
+          key: 1,
+          name: '腾讯广告1',
+          matches:
+            'ImageView - LinearLayout < FrameLayout - FrameLayout > FrameLayout > ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13296371',
+        },
+        {
+          key: 2,
+          name: '腾讯广告2',
+          matches:
+            'ImageView -n FrameLayout > FrameLayout > FrameLayout > ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13296398',
+        },
       ],
     },
   ],
