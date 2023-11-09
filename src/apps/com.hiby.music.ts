d5a2925eb7e143a04c9777c023043493c78fa7f3
m1saka <admin@misaka.ren>
2023-11-09 23:46:53 +0800

    feat(海贝音乐): 开屏广告 (#1932)
    * feat(海贝音乐)：开屏广告
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.hiby.music.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.hiby.music',
+  name: '海贝音乐',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text="跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13293606',
+    },
+  ],
+});
