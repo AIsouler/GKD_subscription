d706b65fa8caf413c3e78b71b75f8e0ece5d8532
walklty <47232102+walklty@users.noreply.github.com>
2023-11-09 23:48:45 +0800

    feat(智慧搜索): 下拉搜索横幅广告-规则优化 (#1934)
    close #1895

--- src/apps/com.huawei.search.ts
+++ src/apps/com.huawei.search.ts
@@ -2,7 +2,7 @@ import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
   id: 'com.huawei.search',
-  name: '华为智慧搜索',
+  name: '智慧搜索',
   groups: [
     {
       key: 0,
@@ -14,11 +14,12 @@ export default defineAppConfig({
       rules: [
         {
           matches:
-            '[id="com.huawei.search:id/iv_topic_close_in_image"||id="com.huawei.search:id/iv_topic_left_close"]',
+            '[id="com.huawei.search:id/iv_topic_close_in_image"||id="com.huawei.search:id/iv_topic_left_close"||id="com.huawei.search:id/iv_topic_close_in_pps"]',
           snapshotUrls: [
             'https://gkd-kit.gitee.io/import/12667938',
             'https://gkd-kit.gitee.io/import/12745008',
             'https://gkd-kit.gitee.io/import/12841076',
+            'https://i.gkd.li/import/13266095', // iv_topic_close_in_pps
           ],
         },
         {
