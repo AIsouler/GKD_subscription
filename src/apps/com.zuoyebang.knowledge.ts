c449643b9db836ae9dd556d2f8f2d903b9e41fb6
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-09 23:54:33 +0800

    feat(作业帮家长版): 开屏广告 (#1945)

--- src/apps/com.zuoyebang.knowledge.ts
+++ src/apps/com.zuoyebang.knowledge.ts
@@ -5,6 +5,16 @@ export default defineAppConfig({
   name: '作业帮家长版',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.zuoyebang.knowledge:id/ms_skipView"]',
+      snapshotUrls: 'https://i.gkd.li/import/13296260',
+    },
+    {
       key: 3,
       name: '赏好评弹窗',
       quickFind: true,
