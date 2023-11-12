9b8a4bbaae666bfadb61cc93d3ad78048ab5aa0a
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-13 01:29:18 +0800

    feat(探探): 开屏广告（优化规则） (#2051)

--- src/apps/com.p1.mobile.putong.ts
+++ src/apps/com.p1.mobile.putong.ts
@@ -7,9 +7,15 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.p1.mobile.putong.account.ui.welcome.SplashAct',
-      rules: '@LinearLayout > TextView[text="跳过"]',
-      snapshotUrls: 'https://i.gkd.li/import/12866488',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12866488',
+        'https://i.gkd.li/import/13324185',
+      ],
     },
   ],
 });
