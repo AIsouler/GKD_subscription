67cfe09f80d362d2029a628f56c28db5695f8b87
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-13 00:27:37 +0800

    feat(普益基金): 开屏广告、版本更新 (#2028)
    * feat(简书):开屏广告、VIP弹窗
    
    * feat(普益基金):开屏广告、版本更新
    
    * 格式更正
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.pywm.fund.ts
@@ -0,0 +1,34 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.pywm.fund',
+  name: '普益基金',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.pywm.fund:id/tv_countdown"]',
+      snapshotUrls: 'https://i.gkd.li/import/13327274',
+    },
+    {
+      key: 1,
+      name: '版本更新',
+      matchLauncher: true,
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: 'TextView[text="立即更新"]',
+          action: 'back',
+          snapshotUrls: 'https://i.gkd.li/import/13327271',
+        },
+      ],
+    },
+  ],
+});
