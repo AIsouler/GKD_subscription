ddcd9dab42b127f6ad8c7bcd1a874a6d656beb3b
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-13 00:24:43 +0800

    feat(萤石云视频): 开屏广告 (#2024)
    * feat(萤石云视频):开屏广告
    
    * chore(actions): check_format_lint
    
    * feat(萤石云视频): 开屏广告
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.videogo.ts
+++ src/apps/com.videogo.ts
@@ -7,14 +7,22 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
+          quickFind: true,
+          key: 0,
+          name: '【跳过+倒计时】型',
           matches: '[id="com.videogo:id/ad_skip_tv"]',
           snapshotUrls: 'https://i.gkd.li/import/12565356',
+        },
+        {
+          key: 1,
+          name: '【圆圈跳过】型',
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13327309',
         },
       ],
     },
