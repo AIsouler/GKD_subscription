0a4bb8bca4cb99d51b2c7c08a79f9d1b551fe6c1
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-13 00:25:57 +0800

    feat(中国移动江苏): 版本更新 (#2025)

--- src/apps/com.jsmcc.ts
+++ src/apps/com.jsmcc.ts
@@ -7,15 +7,33 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: ['com.jsmcc.ui.MainActivityGroupNew'],
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           matches:
-            '[id="com.jsmcc:id/ad_content"] [text^="跳过"][text.length<=3]',
+            '[id="com.jsmcc:id/ad_content"] [text*="跳过"][text.length<=10]',
           snapshotUrls: [
             'https://i.gkd.li/import/12830488',
             'https://i.gkd.li/import/12830580',
           ],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '版本更新',
+      matchLauncher: true,
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          matches: '[id="com.jsmcc:id/cancelVersionTV"]',
+          snapshotUrls: 'https://i.gkd.li/import/13327322',
         },
       ],
     },
