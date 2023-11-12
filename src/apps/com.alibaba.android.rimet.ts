3ba95b85b0674dfa3cab9bc8112b8cea6639376c
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-13 00:14:38 +0800

    feat(钉钉): 消息列表上方广告+文件即将过期提示 (#2021)
    * feat(钉钉): 消息列表上方广告+文件即将过期提示
    
    * chore(actions): check_format_lint
    
    * feat(钉钉): 规则优化
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.alibaba.android.rimet.ts
+++ src/apps/com.alibaba.android.rimet.ts
@@ -7,22 +7,58 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      matchLauncher: true,
       quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           key: 0,
-          activityIds: 'com.alibaba.android.rimet.biz.SplashActivity',
           matches:
-            '[id="com.alibaba.android.rimet:id/btn_check_detail"||text^="跳过"]',
+            '[id="com.alibaba.android.rimet:id/btn_check_detail"][text^="跳过"]',
           snapshotUrls: 'https://i.gkd.li/import/12506211',
         },
         {
           key: 1,
-          activityIds: 'com.alibaba.android.rimet.biz.SplashAdvertiseActivity',
           matches:
             '[id="com.alibaba.android.rimet:id/splash_advertise_pass_text"]',
           snapshotUrls: 'https://i.gkd.li/import/12837220',
+        },
+      ],
+    },
+    {
+      enable: false,
+      key: 1,
+      name: '消息列表上方广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          matches:
+            'ImageView < FrameLayout + FrameLayout > [id="com.alibaba.android.rimet:id/icft_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13325125',
+          action: 'clickCenter',
+        },
+      ],
+    },
+    {
+      enable: false,
+      key: 2,
+      name: '文件即将过期提示',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          matches:
+            '@FrameLayout[id="com.alibaba.android.rimet:id/layout_close"] -2 TextView[text="文件即将过期请备份"]',
+          action: 'clickCenter',
+          snapshotUrls: 'https://i.gkd.li/import/13325125',
         },
       ],
     },
