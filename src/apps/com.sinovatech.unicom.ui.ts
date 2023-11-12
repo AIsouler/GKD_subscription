6edb9702609ab26e00f33ae2889b916c0752167d
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-13 00:31:12 +0800

    feat(中国联通): 开屏广告 (#2032)
    close #2023

--- src/apps/com.sinovatech.unicom.ui.ts
+++ src/apps/com.sinovatech.unicom.ui.ts
@@ -7,7 +7,10 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.sinovatech.unicom.basic.ui.activity.WelcomeClient',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: '[id="com.sinovatech.unicom.ui:id/welcome_advertise_close"]',
       snapshotUrls: 'https://i.gkd.li/import/12535185',
     },
