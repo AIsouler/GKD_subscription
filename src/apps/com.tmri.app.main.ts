1051d0c4ab7c07a602e7fb8e79a371142169b095
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-12 23:19:07 +0800

    feat(交管12123): 消息通知 (#1996)
    * feat(交管12123): 消息通知
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.tmri.app.main.ts
+++ src/apps/com.tmri.app.main.ts
@@ -11,5 +11,19 @@ export default defineAppConfig({
       rules: '[id="com.tmri.app.main:id/tv_timer"][text^="跳过"]',
       snapshotUrls: 'https://i.gkd.li/import/12767813',
     },
+    {
+      key: 1,
+      name: '消息通知',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      rules: [
+        {
+          activityIds: 'com.tmri.app.ui.activity.main.MainTabHostNewActivity',
+          matches: 'ImageButton[id=\'com.tmri.app.main:id/btn_cancel\']',
+          snapshotUrls: 'https://i.gkd.li/import/13315944',
+        },
+      ],
+    },
   ],
 });
