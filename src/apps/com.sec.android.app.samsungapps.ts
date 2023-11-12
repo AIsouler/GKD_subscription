1db3b6982b164e5cce95b5b42ff120cd66804f4e
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-13 00:08:59 +0800

    feat(三星应用商店): 开屏广告 (#2010)
    close #2008

--- src/apps/com.sec.android.app.samsungapps.ts
+++ src/apps/com.sec.android.app.samsungapps.ts
@@ -7,11 +7,16 @@ export default defineAppConfig({
     {
       key: 1,
       name: '开屏广告',
-      activityIds: ['com.sec.android.app.samsungapps.notipopup.WebViewPopup'],
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           matches: '[id="com.sec.android.app.samsungapps:id/skip"]',
-          snapshotUrls: ['https://i.gkd.li/import/12674484'],
+          snapshotUrls: [
+            'https://i.gkd.li/import/12674484',
+            'https://i.gkd.li/import/13324391',
+          ],
         },
       ],
     },
