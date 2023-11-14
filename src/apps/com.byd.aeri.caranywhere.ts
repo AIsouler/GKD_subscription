c8025a9af67f89c379e35075dec01c25f1c4d8df
Frank Zhang <xe5700@outlook.com>
2023-11-14 20:14:56 +0800

    feat(比亚迪王朝): 更新弹窗 (#2061)
    * feat(比亚迪王朝): 添加跳过自动更新支持
    
    * feat(比亚迪王朝): 替换错误的域名
    
    * feat(比亚迪王朝): 修复快照链接
    
    * feat(比亚迪王朝): 更新弹窗
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

---
+++ src/apps/com.byd.aeri.caranywhere.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.byd.aeri.caranywhere',
+  name: '比亚迪王朝',
+  groups: [
+    {
+      enable: false,
+      key: 1,
+      name: '更新弹窗',
+      resetMatch: 'app',
+      actionMaximum: 1,
+      activityIds: ['com.byd.aeri.splash.module_splash.activity.TabManager'],
+      rules: [
+        {
+          quickFind: true,
+          matches: [
+            '[id="com.byd.aeri.caranywhere:id/tv_determine"][text="立即安装"]',
+            '[id="com.byd.aeri.caranywhere:id/tv_cancel"]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13348383',
+        },
+      ],
+    },
+  ],
+});
