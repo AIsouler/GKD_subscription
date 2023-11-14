b966600a49bea0f82ba40ec03269be44371fd47c
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-14 21:22:08 +0800

    feat(PU口袋校园): 弹窗广告（优化规则） (#2114)
    * feat 弹窗广告（优化规则）
    
    * feat 开屏广告（添加 name）

--- src/apps/com.xyhui.ts
+++ src/apps/com.xyhui.ts
@@ -3,6 +3,7 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'com.xyhui',
   name: 'PU口袋校园',
+  deprecatedKeys: [4],
   groups: [
     {
       key: 0,
@@ -13,6 +14,8 @@ export default defineAppConfig({
       resetMatch: 'app',
       rules: [
         {
+          key: 1,
+          name: '开屏广告-1',
           snapshotUrls: [
             'https://i.gkd.li/import/12642486',
             'https://i.gkd.li/import/12846496',
@@ -21,6 +24,8 @@ export default defineAppConfig({
           matches: '[text^="跳过"][text.length<=10]',
         },
         {
+          key: 2,
+          name: '开屏广告-2',
           matches: '[id="com.xyhui:id/ms_skipView"]',
           snapshotUrls: ['https://i.gkd.li/import/13197417'],
         },
@@ -28,48 +33,84 @@ export default defineAppConfig({
     },
     {
       key: 1,
-      name: '首页-弹窗广告',
+      name: '弹窗广告',
       matchLauncher: true,
-      activityIds: [
-        'com.xyhui.start.PUMainActivity',
-        'com.huawei.permissioncontroller.hwcust.appjump.AppJumpActivity',
-        'com.xyhui.start.LoadingActivity',
-      ],
       rules: [
         {
           key: 1,
+          name: '腾讯广告-1',
+          activityIds: 'com.xyhui.start.PUMainActivity',
           matches:
-            'TextView - View < FrameLayout - FrameLayout >(2) ImageView[id=null]',
+            'ImageView - FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
           snapshotUrls: [
-            'https://i.gkd.li/import/12642482', // 查看详情
-            'https://i.gkd.li/import/12646347', // 立即打开
-            'https://i.gkd.li/import/12646519', // 立即抢购
-            'https://i.gkd.li/import/12793157', // 立即安装
-            'https://i.gkd.li/import/12793177', // 立即下载
-            'https://i.gkd.li/import/12868157', // 立即下载, activityId = 'com.xyhui.start.LoadingActivity'
-            'https://i.gkd.li/import/12868369', // 去微信看看
-            'https://i.gkd.li/import/12646541', // 华为手机的快照，activityId = 'com.huawei.permissioncontroller.hwcust.appjump.AppJumpActivity'
+            'https://i.gkd.li/import/12643276',
+            'https://i.gkd.li/import/12868503',
+            'https://i.gkd.li/import/12646420',
+            'https://i.gkd.li/import/13259194',
           ],
         },
         {
           key: 2,
+          name: '腾讯广告-2',
+          activityIds: [
+            'com.xyhui.start.PUMainActivity',
+            'com.huawei.permissioncontroller.hwcust.appjump.AppJumpActivity',
+          ],
           matches:
-            'TextView - View < FrameLayout -(4) FrameLayout > ImageView[id=null]',
+            'ImageView - FrameLayout > FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
           snapshotUrls: [
-            'https://i.gkd.li/import/12643276', // 查看详情
-            'https://i.gkd.li/import/12793180', // 立即下载
-            'https://i.gkd.li/import/12868503', // 去微信看看
-            'https://i.gkd.li/import/12646420', // 华为手机的快照，activityId = 'com.huawei.android.launcher.unihome.UniHomeLauncher'
+            'https://i.gkd.li/import/12642482',
+            'https://i.gkd.li/import/12646519',
+            'https://i.gkd.li/import/12868369',
+            'https://i.gkd.li/import/12646541', // 华为手机的快照，activityId = 'com.huawei.permissioncontroller.hwcust.appjump.AppJumpActivity'
           ],
         },
         {
           key: 3,
+          name: '腾讯广告-3',
+          activityIds: 'com.xyhui.start.PUMainActivity',
           matches:
-            '[text="广告"] <n * < * < ViewGroup - ViewGroup >n ViewGroup > ImageView',
+            'ImageView - LinearLayout - FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: 'https://i.gkd.li/import/12793180',
+        },
+        {
+          key: 4,
+          name: '腾讯广告-4',
+          activityIds: [
+            'com.xyhui.start.PUMainActivity',
+            'com.xyhui.start.LoadingActivity',
+          ],
+          matches:
+            'ImageView - LinearLayout - FrameLayout > FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12646347',
+            'https://i.gkd.li/import/12793157',
+            'https://i.gkd.li/import/12793177',
+            'https://i.gkd.li/import/12868157', // com.xyhui.start.LoadingActivity
+            'https://i.gkd.li/import/13348807', // com.bbk.launcher2.Launcher
+          ],
+        },
+        {
+          key: 5,
+          name: '快手广告',
+          activityIds: 'com.xyhui.start.LoadingActivity',
+          matches:
+            '[text="广告"] <n * < * < ViewGroup - ViewGroup >n @ViewGroup > ImageView',
           snapshotUrls: [
             'https://i.gkd.li/import/13259196',
             'https://i.gkd.li/import/13259198',
           ],
+        },
+        {
+          key: 6,
+          name: '字节广告',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: [
+            '[desc^="logo-union"] > [desc^="logoad"] > [text="广告"]',
+            '[desc^="webview-close"] > View',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13259183',
         },
       ],
     },
@@ -100,19 +141,6 @@ export default defineAppConfig({
       snapshotUrls: [
         'https://i.gkd.li/import/12908853',
         'https://i.gkd.li/import/12908865',
-      ],
-    },
-    {
-      key: 4,
-      name: '我的-弹窗广告',
-      activityIds: [
-        'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
-      ],
-      rules:
-        '[text="查看详情"] <n FrameLayout -n FrameLayout > [(name="android.widget.ImageView"&&clickable=false)||name="android.view.View"]',
-      snapshotUrls: [
-        'https://i.gkd.li/import/13259183',
-        'https://i.gkd.li/import/13259194',
       ],
     },
   ],
