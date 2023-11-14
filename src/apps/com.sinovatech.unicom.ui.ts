b1b943f212f271e36376c629a1d6fe337152e6af
walklty <47232102+walklty@users.noreply.github.com>
2023-11-14 19:41:02 +0800

    feat(中国联通): 权限申请 (#2064)
    close #2062

--- src/apps/com.sinovatech.unicom.ui.ts
+++ src/apps/com.sinovatech.unicom.ui.ts
@@ -14,5 +14,16 @@ export default defineAppConfig({
       rules: '[id="com.sinovatech.unicom.ui:id/welcome_advertise_close"]',
       snapshotUrls: 'https://i.gkd.li/import/12535185',
     },
+    {
+      key: 1,
+      name: '权限申请',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.sinovatech.unicom.basic.ui.activity.MainActivity',
+      rules:
+        '[text="去开启"] - [id="com.sinovatech.unicom.ui:id/custom_dialog_cancel_button"]',
+      snapshotUrls: 'https://i.gkd.li/import/13331268',
+    },
   ],
 });
