b8cdc59c967a01fe6bae21e87cbae83bcc78f74f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-14 21:05:40 +0800

    feat(下厨房): 开屏广告（优化规则）、卡片式广告 (#2113)
    * feat 开屏广告（优化规则）
    
    * feat 卡片式广告

--- src/apps/com.xiachufang.ts
+++ src/apps/com.xiachufang.ts
@@ -7,13 +7,40 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: ['com.xiachufang.startpage.ui.StartPageActivity'],
-      rules:
-        '[id$="/tt_splash_skip_btn"||id$="/start_page_count_down_tv"||text$="跳过"]',
-      snapshotUrls: [
-        'https://i.gkd.li/import/12505985',
-        'https://i.gkd.li/import/12506014',
-        'https://i.gkd.li/import/12506041',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          name: '开屏广告-1',
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/12505985',
+        },
+        {
+          key: 1,
+          name: '开屏广告-2',
+          matches: '@FrameLayout > [text$="跳过"][text.length<=10]',
+          quickFind: true,
+          snapshotUrls: [
+            'https://i.gkd.li/import/12506014',
+            'https://i.gkd.li/import/12506041',
+          ],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 0,
+          name: '菜谱底部卡片广告',
+          activityIds: 'com.xiachufang.activity.recipe.RecipeDetailActivity',
+          quickFind: true,
+          matches: '[id="com.xiachufang:id/ad_close"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13348710',
+        },
       ],
     },
   ],
