45e769ca9e684e5057f0329f1ef433dc52672626
Zeng Weiyu <zerozwy@gmail.com>
2023-11-14 21:20:06 +0800

    Feat(盯盯拍): 开屏广告规则更新 (#2112)
    * feat(盯盯拍): 开屏广告规则更新
    
    * chore(actions): check_format_lint
    
    * feat(盯盯拍): 开屏广告
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.vyou.vcameraclient.ts
+++ src/apps/com.vyou.vcameraclient.ts
@@ -7,12 +7,19 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      matchLauncher: true,
-      // quickFind: true, 开启后无法跳过
-      activityIds: 'com.vyou.app.ui.activity.SplashActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
-          matches: 'View[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
+          quickFind: true,
+          matches: '[text="跳过"]',
+          exampleUrls:
+            'https://m.gkd.li/6328439/5b974f9d-a7bf-46fa-9ba3-57df81aca131',
+          snapshotUrls: 'https://i.gkd.li/import/13347076',
+        },
+        {
+          matches: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
           snapshotUrls: [
             'https://i.gkd.li/import/13042916',
             'https://i.gkd.li/import/13042963',
