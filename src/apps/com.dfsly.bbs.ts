847f5a2453bedb6d7eff9df11ea26a506e3e1679
walklty <47232102+walklty@users.noreply.github.com>
2023-11-14 20:39:53 +0800

    feat(AC匿名版): 开屏广告; 弹窗广告 (#2091)
    * Create com.dfsly.bbs.ts
    
    * Update com.dfsly.bbs.ts
    
    * feat(AC匿名版): 弹窗广告
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>
    
    close #2083
    close #2057

---
+++ src/apps/com.dfsly.bbs.ts
@@ -0,0 +1,42 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.dfsly.bbs',
+  name: 'AC匿名版',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          matches:
+            '[id="android:id/content"] >7 FrameLayout[childCount=7] > LinearLayout >3 ImageView[text=null][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13330351',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '弹窗广告',
+      matchLauncher: true,
+      activityIds: ['com.dfsly.bbs.ui.NavigationActivity'],
+      quickFind: true,
+      rules: [
+        {
+          matches: [
+            '[id="android:id/content"] >n FrameLayout[childCount=5 || childCount=8] > FrameLayout > ImageView[text=null][visibleToUser=true]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/import/13343675',
+            'https://i.gkd.li/import/13335135',
+            'https://i.gkd.li/import/13335316', // matchLauncher
+          ],
+        },
+      ],
+    },
+  ],
+});
