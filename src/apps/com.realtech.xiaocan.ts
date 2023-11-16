9483732df9ed24dccaafccdbd8edb41bc49331c1
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-16 21:32:30 +0800

    feat(小蚕霸王餐): 开屏广告（优化规则） (#2144)

--- src/apps/com.realtech.xiaocan.ts
+++ src/apps/com.realtech.xiaocan.ts
@@ -7,14 +7,24 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.realtech.adscope.SplashActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
-          matches:
-            '[id = "com.realtech.xiaocan:id/splash_ad_container"] [text ^= "跳过"]',
-          snapshotUrls: 'https://i.gkd.li/import/12843368',
+          key: 1,
+          name: '开屏广告-1',
+          quickFind: true,
+          matches: '[text^="跳过"][text.length<=10]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12843368',
+            'https://i.gkd.li/import/13363222',
+            'https://i.gkd.li/import/13363246',
+          ],
         },
         {
+          key: 2,
+          name: '开屏广告-2',
           matches: '[id = "com.byted.pangle:id/tt_splash_skip_btn"]',
           snapshotUrls: 'https://i.gkd.li/import/12854728',
         },
