c8cb333acbdbc4787408ca6db54bef3fcbfaec09
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-16 21:31:48 +0800

    feat(万能小组件 Top Widgets): 开屏广告（新增规则） (#2143)

--- src/apps/com.growing.topwidgets.ts
+++ src/apps/com.growing.topwidgets.ts
@@ -7,12 +7,24 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[text^="跳过"][text.length<=10]',
-      snapshotUrls: 'https://i.gkd.li/import/13334835',
+      rules: [
+        {
+          key: 1,
+          name: '开屏广告-1',
+          quickFind: true,
+          matches: '[text^="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13334835',
+        },
+        {
+          key: 2,
+          name: '开屏广告-2',
+          matches: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13363115',
+        },
+      ],
     },
   ],
 });
