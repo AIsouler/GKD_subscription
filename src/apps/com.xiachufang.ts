cdabcbd4ac2b7f9ac4db0ca41f6f214f8857e2ce
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-16 21:31:19 +0800

    feat(下厨房): 厨房问卷、卡片式广告（新增规则） (#2141)
    * feat 厨房问卷
    
    * feat 卡片式广告（新增规则）

--- src/apps/com.xiachufang.ts
+++ src/apps/com.xiachufang.ts
@@ -34,12 +34,34 @@ export default defineAppConfig({
       name: '卡片式广告',
       rules: [
         {
-          key: 0,
-          name: '菜谱底部卡片广告',
+          key: 1,
+          name: '菜谱底部卡片广告-1',
           activityIds: 'com.xiachufang.activity.recipe.RecipeDetailActivity',
           quickFind: true,
           matches: '[id="com.xiachufang:id/ad_close"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/import/13348710',
+        },
+        {
+          key: 2,
+          name: '菜谱底部卡片广告-2',
+          activityIds: 'com.xiachufang.activity.recipe.RecipeDetailActivity',
+          quickFind: true,
+          matches:
+            '[id="com.xiachufang:id/jad_feed_close"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13363079',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '厨房问卷',
+      rules: [
+        {
+          key: 1,
+          name: '厨房问卷',
+          activityIds: 'com.xiachufang.activity.recipe.RecipeDetailActivity',
+          matches: '[text^="厨房问卷"] + [desc="关闭"]',
+          snapshotUrls: 'https://i.gkd.li/import/13363042',
         },
       ],
     },
