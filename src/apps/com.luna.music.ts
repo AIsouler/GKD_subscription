4f588afb9d4ffdc7f7da305a3c1de60fddaafc93
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-16 21:33:58 +0800

    feat(汽水音乐): VIP弹窗 (#2147)
    * feat(汽水音乐):VIP弹窗
    
    * Update com.luna.music.ts

--- src/apps/com.luna.music.ts
+++ src/apps/com.luna.music.ts
@@ -7,9 +7,29 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.luna.biz.ad.AdActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: '[id$="/tt_splash_skip_btn"]',
       snapshotUrls: 'https://i.gkd.li/import/12514049',
+    },
+    {
+      key: 2,
+      name: 'VIP弹窗',
+      desc: '直接关闭所有该样式弹窗',
+      matchLauncher: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          action: 'back',
+          matches: '[id="com.luna.music:id/bullet_container"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13365087',
+            'https://i.gkd.li/import/13365085',
+          ],
+        },
+      ],
     },
   ],
 });
