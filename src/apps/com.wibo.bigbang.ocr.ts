686f0881e3a9e2ec558034e0dbd5a5b9d8b1f6c0
walklty <47232102+walklty@users.noreply.github.com>
2023-11-16 21:28:41 +0800

    feat(布丁扫描): 升级提示 (#2132)
    clsoe #2129

---
+++ src/apps/com.wibo.bigbang.ocr.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wibo.bigbang.ocr',
+  name: '布丁扫描',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      matchTime: 20000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          matches:
+            '[id="com.wibo.bigbang.ocr:id/vivo_upgrade_negative_btn"][text="忽略本次"]',
+          snapshotUrls: 'https://i.gkd.li/import/13360281',
+        },
+      ],
+    },
+  ],
+});
