3e4df5c47f8bbc236fc7d704efba714ab388a397
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-16 20:32:34 +0800

    feat(喵上漫画): 开屏广告（优化规则）、弹窗广告（优化规则）、视频广告、看视频解锁任意读弹窗 (#2115)
    * feat 开屏广告（优化规则）
    
    * feat 弹窗广告（优化规则）
    
    * feat 视频广告
    
    * feat 看视频解锁任意读弹窗
    
    * feat(喵上漫画): 开屏广告
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.aster.zhbj.ts
+++ src/apps/com.aster.zhbj.ts
@@ -7,73 +7,64 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      desc: '点击右上角跳过',
-      matchLauncher: true,
-      rules: [
-        {
-          activityIds: [
-            'com.aster.comic.app.ad.view.mediation.MediationSplashAdActivity',
-            'com.aster.comic.app.ad.view.mediation.MediationAppResumeSplashAdActivity',
-            'com.aster.comic.app.view.reader.ReaderActivity',
-          ],
-          matches: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/12981243',
-            'https://i.gkd.li/import/13029855',
-            'https://i.gkd.li/import/13043320',
-            'https://i.gkd.li/import/13043344',
-          ],
-        },
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12981243',
+        'https://i.gkd.li/import/13029855',
+        'https://i.gkd.li/import/13043320',
+        'https://i.gkd.li/import/13043344',
       ],
     },
     {
       key: 1,
-      name: '应用内广告弹窗',
-      desc: '点击右上角x',
+      name: '弹窗广告',
+      matchLauncher: true,
       rules: [
-        // 腾讯广告
-        {
-          key: 0,
-          activityIds: 'com.bbk.launcher2.Launcher',
-          matches: 'ImageView - FrameLayout > FrameLayout > ImageView',
-          snapshotUrls: 'https://i.gkd.li/import/12777325',
-        },
         {
           key: 1,
-          activityIds:
-            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Activity_T',
+          name: '腾讯广告-1',
+          activityIds: 'com.aster.comic.app.view.MainActivity',
           matches:
-            'FrameLayout[childCount=5] > FrameLayout[childCount=1] > ImageView',
-          snapshotUrls: 'https://i.gkd.li/import/13029880',
+            'ImageView - FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12984767',
+            'https://i.gkd.li/import/12998908', // com.huawei.android.launcher.unihome.UniHomeLauncher
+            'https://i.gkd.li/import/12777325', // com.bbk.launcher2.Launcher
+          ],
         },
         {
           key: 2,
-          activityIds: 'com.aster.comic.app.view.MainActivity',
+          name: '腾讯广告-2',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Activity_T',
           matches:
-            'ImageView - LinearLayout - FrameLayout > FrameLayout > ImageView',
-          snapshotUrls: 'https://i.gkd.li/import/12872249',
+            'ImageView < FrameLayout + FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: 'https://i.gkd.li/import/13029880',
         },
-        // 字节广告
         {
           key: 3,
+          name: '腾讯广告-3',
           activityIds: 'com.aster.comic.app.view.MainActivity',
-          matches: '[desc^="webview-close"] > View',
-          snapshotUrls: 'https://i.gkd.li/import/12903062',
+          matches:
+            'ImageView - LinearLayout - FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: 'https://i.gkd.li/import/12872249',
         },
         {
           key: 4,
-          activityIds: [
-            'com.aster.comic.app.view.MainActivity',
-            'ImageView - FrameLayout > FrameLayout > ImageView',
+          name: '字节广告-1',
+          activityIds: 'com.aster.comic.app.view.MainActivity',
+          matches: [
+            '[desc^="logo-union"] > [desc^="logoad"] > [text="广告"]',
+            '[desc^="webview-close"] > View',
           ],
-          matches: 'ImageView - FrameLayout > FrameLayout > ImageView',
-          snapshotUrls: [
-            'https://i.gkd.li/import/12984767',
-            'https://i.gkd.li/import/12998908',
-          ],
+          snapshotUrls: 'https://i.gkd.li/import/12903062',
         },
         {
-          key: 11,
+          key: 5,
+          name: '字节广告-2',
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
           matches:
@@ -84,6 +75,33 @@ export default defineAppConfig({
           ],
         },
       ],
+    },
+    {
+      key: 2,
+      name: '视频广告',
+      rules: [
+        {
+          key: 1,
+          name: '字节广告',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: [
+            'View > View > View > View > [text="广告"]',
+            'View[childCount=2] > [text$="跳过"]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13348662',
+        },
+      ],
+    },
+    {
+      enable: false,
+      key: 3,
+      name: '看视频解锁任意读弹窗',
+      desc: '自动点击【我拒绝】',
+      activityIds: 'com.aster.comic.app.view.reader.ReaderActivity',
+      rules:
+        '[text="看视频解锁任意读"] < LinearLayout +n LinearLayout > [id="com.aster.zhbj:id/btnReject"][text*="拒绝"]',
+      snapshotUrls: 'https://i.gkd.li/import/13348635',
     },
   ],
 });
