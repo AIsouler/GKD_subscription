31542d2298c07522b1fa47686bde99f821b4a48b
aoguai <34203474+aoguai@users.noreply.github.com>
2023-11-16 21:47:54 +0800

    feat(学小易): 开屏广告、首页-底部广告规则优化 (#2162)

--- src/apps/com.xuexiaoyi.xxy.ts
+++ src/apps/com.xuexiaoyi.xxy.ts
@@ -7,12 +7,15 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      quickFind: true,
+      // quickFind: true,  // 会导致无法跳过
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: 'View[id="com.byted.pangle:id/tt_splash_skip_btn"]',
-      snapshotUrls: 'https://i.gkd.li/import/12895001',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12895001',
+        'https://i.gkd.li/import/13266314',
+      ],
     },
     {
       key: 1,
@@ -23,22 +26,38 @@ export default defineAppConfig({
           key: 0,
           name: '点击在下方的X',
           matches: [
-            'View[text="广告"] < View <n View + View > Image[text.length=0]',
+            'View[text="广告"] < View <n View <n View > View > Image[text.length=0]',
           ],
-          snapshotUrls: ['https://i.gkd.li/import/13068836'],
+          snapshotUrls: [
+            'https://i.gkd.li/import/13068836',
+            'https://i.gkd.li/import/13375502',
+            'https://i.gkd.li/import/13375559',
+          ],
         },
         {
           key: 1,
-          name: '点击在上方的X',
-          matches: ['[text="查看详情"] -n View > Image[text.length=0]'],
+          name: '点击在上方的X-类型1',
+          matches: [
+            '[text.length>=2&&text.length<=6] -n View > Image[text.length=0&&childCount=0]',
+          ],
           snapshotUrls: [
             'https://i.gkd.li/import/13068811',
             'https://i.gkd.li/import/13202990',
+            'https://i.gkd.li/import/13266356',
+            'https://i.gkd.li/import/13266362',
           ],
         },
         {
-          preKeys: [0, 1],
           key: 2,
+          name: '点击在上方的X-类型2',
+          matches: [
+            '[text.length>=2&&text.length<=6] < FrameLayout -n FrameLayout > View',
+          ],
+          snapshotUrls: ['https://i.gkd.li/import/13266689'],
+        },
+        {
+          preKeys: [0, 1, 2],
+          key: 10,
           name: '点击不感兴趣',
           matches: 'TextView[text="不感兴趣"]',
           snapshotUrls: 'https://i.gkd.li/import/13068819',
