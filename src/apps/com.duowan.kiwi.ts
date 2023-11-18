3bf46f70119ac5074778565cbb394ed4268c6961
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-18 23:52:45 +0800

    feat(虎牙直播): 直播间悬浮广告 (#2224)
    close #2212

--- src/apps/com.duowan.kiwi.ts
+++ src/apps/com.duowan.kiwi.ts
@@ -7,6 +7,7 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
@@ -30,6 +31,7 @@ export default defineAppConfig({
         'com.duowan.kiwi.homepage.Homepage', // 缺少快照
         'com.duowan.kiwi.immersepage.impl.ImmersePageActivity',
       ],
+      quickFind: true,
       matchLauncher: true,
       rules:
         '[id="com.duowan.kiwi:id/hyui_dialog_button_positive"][text="我知道了"]',
@@ -38,6 +40,7 @@ export default defineAppConfig({
     {
       key: 2,
       name: '直播间悬浮广告',
+      quickFind: true,
       activityIds: 'com.duowan.kiwi.liveroom.ChannelPage',
       rules: [
         {
@@ -46,6 +49,14 @@ export default defineAppConfig({
           snapshotUrls: [
             'https://i.gkd.li/import/12901045',
             'https://i.gkd.li/import/12901044',
+          ],
+        },
+        {
+          key: 1,
+          matches: '[id="com.duowan.kiwi:id/game_header_close"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13395604',
+            'https://i.gkd.li/import/13395606',
           ],
         },
       ],
