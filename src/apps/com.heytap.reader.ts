d5154bf45f39b808757a8863c6f58b9c104e8070
Zeng Weiyu <zerozwy@gmail.com>
2023-11-18 23:36:03 +0800

    feat(oppo阅读): 首页广告、首页浮窗、今日推荐 (#2187)
    * feat(oppo阅读):首页广告、首页浮窗、今日推荐
    
    * chore(actions): check_format_lint
    
    * feat(oppo阅读):首页广告、首页浮窗、今日推荐
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.heytap.reader.ts
+++ src/apps/com.heytap.reader.ts
@@ -20,5 +20,47 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 2,
+      name: '首页广告',
+      rules: [
+        {
+          quickFind: true,
+          matches:
+            '[id="com.zhangyue.commonplugin:id/dialog_welfare_card_close"]',
+          exampleUrls:
+            'https://m.gkd.li/6328439/4feb19d4-f90f-4ed5-b025-9c2a6e4fc479',
+          snapshotUrls: 'https://i.gkd.li/import/13387130',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '首页浮窗',
+      rules: [
+        {
+          activityIds: 'com.zhangyue.iReader.bookshelf.ui.ActivityBookShelf',
+          quickFind: true,
+          matches: '[id="com.heytap.reader:id/float_delete"]',
+          exampleUrls:
+            'https://m.gkd.li/6328439/860371ea-1f09-4f82-8ed6-1436eca4a50d',
+          snapshotUrls: 'https://i.gkd.li/import/13387138',
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '今日推荐',
+      rules: [
+        {
+          activityIds: 'com.zhangyue.iReader.bookshelf.ui.ActivityBookShelf',
+          quickFind: true,
+          matches: '[id="com.heytap.reader:id/iv_float_read_continue_close"]',
+          exampleUrls:
+            'https://m.gkd.li/6328439/3ee7210c-970e-4c9a-acb6-9254245c27a0',
+          snapshotUrls: 'https://i.gkd.li/import/13387159',
+        },
+      ],
+    },
   ],
 });
