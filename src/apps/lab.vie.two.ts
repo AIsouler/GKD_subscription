5b00ebcec9051abb78064e31eb360cec5bbe1e36
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-18 23:09:36 +0800

    feat(LIBVIO): 账号注册说明弹窗 (#2172)

---
+++ src/apps/lab.vie.two.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'lab.vie.two',
+  name: 'LIBVIO',
+  groups: [
+    {
+      key: 1,
+      name: '账号注册说明弹窗',
+      desc: '每次打开app的弹窗提示，本规则自动点击【我知道了】',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[desc="账号注册说明"] +2 [desc="我知道了"]',
+      snapshotUrls: 'https://i.gkd.li/import/13379070',
+    },
+  ],
+});
