6b63f5da06a6e6e1df19b6d93faac48678e0c4a4
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-18 22:50:42 +0800

    feat(MINO 4K): 弹窗广告 (#2166)

---
+++ src/apps/com.qjy.youqulife.io.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.qjy.youqulife.io',
+  name: 'MINO 4K',
+  groups: [
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 1,
+          name: '快手广告',
+          activityIds: 'com.qjy.youqulife.io.MainActivity',
+          quickFind: true,
+          matches:
+            '[text="广告"] <2 ViewGroup -n ViewGroup > ViewGroup > [text="跳过"]',
+          snapshotUrls: 'https://i.gkd.li/import/13378653',
+        },
+      ],
+    },
+  ],
+});
