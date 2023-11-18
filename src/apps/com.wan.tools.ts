dd986aed7eb092a483f1f824e006c76ad9ff84d7
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-18 23:46:18 +0800

    feat(下载工具箱) :公告弹窗、自动识别解析链接、开通会员弹窗 (#2205)
    * feat 公告弹窗
    
    * feat 自动识别解析链接
    
    * feat 开通会员弹窗

---
+++ src/apps/com.wan.tools.ts
@@ -0,0 +1,34 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wan.tools',
+  name: '下载工具箱',
+  groups: [
+    {
+      enable: false,
+      key: 1,
+      name: '公告弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: 'LinearLayout[childCount=2] > @[text="取消"] + [text="确认"]',
+      snapshotUrls: 'https://i.gkd.li/import/13392027',
+    },
+    {
+      enable: false,
+      key: 2,
+      name: '自动识别解析链接',
+      activityIds: 'com.one.downloadtools.ui.activity.HomeActivity',
+      rules: '@[text="识别解析"] -2 * > [text^="检测到你复制链接"]',
+      snapshotUrls: 'https://i.gkd.li/import/13392085',
+    },
+    {
+      enable: false,
+      key: 3,
+      name: '开通会员弹窗',
+      activityIds: 'com.one.downloadtools.ui.activity.HomeActivity',
+      rules: '[text^="开通会员"] < * +n * > [text="暂不"]',
+      snapshotUrls: 'https://i.gkd.li/import/13392112',
+    },
+  ],
+});
