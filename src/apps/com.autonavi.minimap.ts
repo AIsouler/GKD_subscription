684953527b756e2938eb1947a6f102b743f94837
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-18 23:10:00 +0800

    feat(高德地图): 开屏广告（优化规则）、更新弹窗 (#2173)
    * feat 开屏广告（优化规则）
    
    * feat 更新弹窗

--- src/apps/com.autonavi.minimap.ts
+++ src/apps/com.autonavi.minimap.ts
@@ -7,12 +7,36 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.autonavi.map.activity.NewMapActivity',
-      rules:
-        '[id="com.autonavi.minimap:id/fl_splash_container"] >n [text^="跳过"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@LinearLayout > [text^="跳过"][text.length<=10]',
       snapshotUrls: [
         'https://i.gkd.li/import/12667556',
         'https://i.gkd.li/import/12750045',
+      ],
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          key: 1,
+          name: '更新弹窗-1',
+          matches:
+            '@[id="com.autonavi.minimap:id/iv_close"] + * > [text^="立即升级"]',
+          snapshotUrls: 'https://i.gkd.li/import/13379094',
+        },
+        {
+          key: 2,
+          name: '更新弹窗-2',
+          matches: '@[text="取消"] + * + [text="去下载"]',
+          snapshotUrls: 'https://i.gkd.li/import/13379426',
+        },
       ],
     },
     {
