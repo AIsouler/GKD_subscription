a365ab0f7e32342088b8e492d6d510ffb55b790e
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-18 23:42:38 +0800

    feat(皮皮搞笑): 信息流广告 (#2195)
    close #2191

--- src/apps/cn.xiaochuankeji.zuiyouLite.ts
+++ src/apps/cn.xiaochuankeji.zuiyouLite.ts
@@ -7,6 +7,10 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           key: 0,
@@ -26,9 +30,34 @@ export default defineAppConfig({
     {
       key: 1,
       name: '青少年模式弹窗',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       activityIds: 'cn.xiaochuankeji.zuiyouLite.ui.main.MainActivity',
       rules: '[id="cn.xiaochuankeji.zuiyouLite:id/young_close_btn"]',
       snapshotUrls: 'https://i.gkd.li/import/12745083',
+    },
+    {
+      enable: false,
+      key: 2,
+      name: '信息流广告',
+      quickFind: true,
+      activityIds: 'cn.xiaochuankeji.zuiyouLite.ui.slide.ActivitySlideDetail',
+      rules: [
+        {
+          key: 0,
+          name: '点击【x】',
+          matches: '[id="cn.xiaochuankeji.zuiyouLite:id/ad_remove"]',
+          snapshotUrls: 'https://i.gkd.li/import/13387116',
+        },
+        {
+          preKeys: 0,
+          name: '点击【不喜欢广告主】',
+          matches: '@LinearLayout[clickable=true] > [text="不喜欢广告主"]',
+          snapshotUrls: 'https://i.gkd.li/import/13387155',
+        },
+      ],
     },
   ],
 });
