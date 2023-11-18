78de7cd9ed621893c86725d282a128aa503f8a24
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-18 23:12:15 +0800

    feat(中羽在线): 开屏广告（优化规则） (#2177)

--- src/apps/com.badmintoncn.bbs.ts
+++ src/apps/com.badmintoncn.bbs.ts
@@ -11,8 +11,11 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[text="跳过"]',
-      snapshotUrls: 'https://i.gkd.li/import/13258044',
+      rules: '[text$="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13258044',
+        'https://i.gkd.li/import/13379472',
+      ],
     },
   ],
 });
