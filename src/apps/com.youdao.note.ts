024e84d0b69c803c59e3ca1bdc479315fb36921c
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-18 23:12:36 +0800

    feat(有道云笔记): 开屏广告（优化规则）、卡片式广告 (#2178)
    * feat 开屏广告（优化规则）
    
    * feat 卡片式广告

--- src/apps/com.youdao.note.ts
+++ src/apps/com.youdao.note.ts
@@ -7,9 +7,30 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.youdao.note.activity2.NormalSplashActivity',
-      rules: '[id="com.youdao.note:id/count_down_textview"][text^="跳过"]',
-      snapshotUrls: 'https://i.gkd.li/import/12745606',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12745606',
+        'https://i.gkd.li/import/13399002',
+      ],
+    },
+    {
+      key: 1,
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 1,
+          name: '首页上方卡片广告',
+          activityIds: 'com.youdao.note.activity2.MainActivity',
+          quickFind: true,
+          matches:
+            '[id="com.youdao.note:id/recycle_view"] + [id="com.youdao.note:id/close_ad"]',
+          snapshotUrls: 'https://i.gkd.li/import/13379524',
+        },
+      ],
     },
   ],
 });
