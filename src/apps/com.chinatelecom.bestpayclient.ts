3ddddb3a52dafdb603381d46986a91aac540293b
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-18 23:44:40 +0800

    feat(翼支付): 版本更新 (#2201)
    * feat(翼支付):版本更新
    
    * Update com.chinatelecom.bestpayclient.ts
    
    close #2199

--- src/apps/com.chinatelecom.bestpayclient.ts
+++ src/apps/com.chinatelecom.bestpayclient.ts
@@ -14,5 +14,16 @@ export default defineAppConfig({
       rules: '[id="com.chinatelecom.bestpayclient:id/tv_ad_skip"]',
       snapshotUrls: 'https://i.gkd.li/import/13197640',
     },
+    {
+      key: 1,
+      name: '版本更新',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.chinatelecom.bestpayclient.ui.MainActivity',
+      rules: '[id="com.chinatelecom.bestpayclient:id/bupdate_tv_bottom_tip"]',
+      snapshotUrls: 'https://i.gkd.li/import/13391544',
+    },
   ],
 });
