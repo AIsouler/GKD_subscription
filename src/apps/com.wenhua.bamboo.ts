d80333c4d8122420afaf9c1ce5f8e98b3f9a2aab
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-20 21:32:24 +0800

    feat(随身行): 通知弹窗 (#2305)
    * feat(随身行): 通知弹窗
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.wenhua.bamboo.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wenhua.bamboo',
+  name: '随身行',
+  groups: [
+    {
+      key: 1,
+      name: '通知弹窗',
+      desc: '点击稍后再说',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.wenhua.bamboo.screen.activity.MarketOptionActivity',
+          matches: '@Button[text=\'稍后再说\'] +2 Button[text=\'马上开启\']',
+          snapshotUrls: 'https://i.gkd.li/import/13407555',
+        },
+      ],
+    },
+  ],
+});
