99aa08f6a6e33226b462671f16e450f59ec3f1b0
tiygu <99116490+tiygu@users.noreply.github.com>
2023-11-20 22:09:44 +0800

    feat(小卖部逆袭记): 卡片广告 (#2228)
    * Create com.dz.xmbnxj.ts
    
    * Update com.dz.xmbnxj.ts
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.dz.xmbnxj.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.dz.xmbnxj',
+  name: '小卖部逆袭记',
+  groups: [
+    {
+      key: 1,
+      name: '底部卡片广告',
+      rules: [
+        {
+          activityIds: 'com.lynx.boot.LynxActivity',
+          quickFind: true,
+          matches: '[id="com.dz.xmbnxj:id/mix_img_close"]',
+          exampleUrls:
+            'https://m.gkd.li/99116490/40d3c3c9-9443-4c71-9a98-69efa38f8bfd',
+          snapshotUrls: 'https://i.gkd.li/import/13399232',
+        },
+      ],
+    },
+  ],
+});
