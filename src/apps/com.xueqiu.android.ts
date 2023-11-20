c31f12892133170a9f564929952f5a1093065fda
bleach2012 <33366298+bleach2012@users.noreply.github.com>
2023-11-20 22:13:52 +0800

    feat(雪球): 开屏广告 (#2197)
    * 雪球app首页广告及评论区广告
    
    * Update com.xueqiu.android.ts
    
    ---------
    
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/com.xueqiu.android.ts
+++ src/apps/com.xueqiu.android.ts
@@ -7,9 +7,41 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.xueqiu.android.community.FakeAdActivity',
-      rules: '[id="com.xueqiu.android:id/tv_skip"][text^="跳过"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.xueqiu.android:id/tv_skip"]',
       snapshotUrls: 'https://i.gkd.li/import/12776009',
+    },
+    {
+      key: 1,
+      name: '首页广告',
+      rules: [
+        {
+          quickFind: true,
+          activityIds:
+            'com.xueqiu.android.mainnesting.view.MainNestingActivity',
+          matches: '[id=`com.xueqiu.android:id/ad_close`]',
+          exampleUrls:
+            'https://m.gkd.li/33366298/d05d1eec-cda5-4d37-a494-8eef7b7871dd',
+          snapshotUrls: 'https://i.gkd.li/import/13388227',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '评论区广告',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.xueqiu.android.community.StatusDetailActivity',
+          matches: '[id=`com.xueqiu.android:id/ad_card_close`]',
+          exampleUrls:
+            'https://m.gkd.li/33366298/b223cc74-fd4f-45a2-89ab-4a18ddffc01c',
+          snapshotUrls: 'https://i.gkd.li/import/13388361',
+        },
+      ],
     },
   ],
 });
