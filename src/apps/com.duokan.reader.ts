ce5e4e416c1d45c2786218a9be413af2a8a9bbb9
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-20 21:33:14 +0800

    feat(多看): 小说推荐弹窗 (#2309)

--- src/apps/com.duokan.reader.ts
+++ src/apps/com.duokan.reader.ts
@@ -12,5 +12,20 @@ export default defineAppConfig({
       rules: '[id="com.duokan.reader:id/store_feed_layer_close"]',
       snapshotUrls: 'https://i.gkd.li/import/13248773',
     },
+    {
+      key: 3,
+      name: '小说推荐弹窗',
+      rules: [
+        {
+          key: 1,
+          name: '退出阅读时的推荐弹窗',
+          activityIds: 'com.duokan.reader.DkMainActivity',
+          quickFind: true,
+          matches:
+            '[id="com.duokan.reader:id/reading_stop_read_recommend_stop"]',
+          snapshotUrls: 'https://i.gkd.li/import/13413412',
+        },
+      ],
+    },
   ],
 });
