b1e86d1d77c9181f729e29e007607dada474cefc
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-20 20:43:16 +0800

    feat(联想应用中心): 更新弹窗、请求通知权限弹窗 (#2258)

---
+++ src/apps/com.lenovo.leos.appstore.ts
@@ -0,0 +1,27 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.lenovo.leos.appstore',
+  name: '联想应用中心',
+  groups: [
+    {
+      key: 0,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules:
+        '@[id="com.lenovo.leos.appstore:id/dialog_cancel"] + [text$="版本更新"]',
+      snapshotUrls: 'https://i.gkd.li/import/13401992',
+    },
+    {
+      key: 1,
+      name: '请求通知权限弹窗',
+      activityIds:
+        'com.lenovo.leos.appstore.activities.NotificationGuideActivity',
+      quickFind: true,
+      rules: '[text$="打开应用中心通知"] +n [text="知道了"]',
+      snapshotUrls: 'https://i.gkd.li/import/13401991',
+    },
+  ],
+});
