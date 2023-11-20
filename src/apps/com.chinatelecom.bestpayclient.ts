4362d671f6c66b236dd454a46fcb6c39e831de89
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-20 20:49:55 +0800

    feat(翼支付): 广告弹窗、悬浮窗小广告 (#2271)

--- src/apps/com.chinatelecom.bestpayclient.ts
+++ src/apps/com.chinatelecom.bestpayclient.ts
@@ -25,5 +25,28 @@ export default defineAppConfig({
       rules: '[id="com.chinatelecom.bestpayclient:id/bupdate_tv_bottom_tip"]',
       snapshotUrls: 'https://i.gkd.li/import/13391544',
     },
+    {
+      key: 3,
+      name: '广告弹窗',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.chinatelecom.bestpayclient.ui.MainActivity',
+      rules:
+        '[id^="am-modal-container"] Dialog @View[clickable=true][index=1] > Image',
+      snapshotUrls: 'https://i.gkd.li/import/13402692',
+    },
+    {
+      enable: false,
+      key: 4,
+      name: '悬浮窗小广告',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.chinatelecom.bestpayclient.ui.MainActivity',
+      rules:
+        '[id="com.chinatelecom.bestpayclient:id/best_h5_container"] View[childCount=2] > @View[clickable=true] +  View[clickable=true] > Image[text!=null]',
+      snapshotUrls: 'https://i.gkd.li/import/13402711',
+    },
   ],
 });
