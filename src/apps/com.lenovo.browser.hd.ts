453e0589a287474b702aeca68019d5b4d77b56e9
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-20 20:42:43 +0800

    feat(联想浏览器HD): 更新弹窗、请求通知权限弹窗 (#2257)
    * feat 更新弹窗
    
    * feat 请求通知权限弹窗

---
+++ src/apps/com.lenovo.browser.hd.ts
@@ -0,0 +1,27 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.lenovo.browser.hd',
+  name: '联想浏览器HD',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules:
+        '[text="发现新版本"] - * > [id="com.lenovo.browser.hd:id/fl_close"]',
+      snapshotUrls: 'https://i.gkd.li/import/13401982',
+    },
+    {
+      key: 2,
+      name: '请求通知权限弹窗',
+      activityIds: 'com.zui.launcher.drawer.NormalLauncher',
+      quickFind: true,
+      rules:
+        '[text="开启消息通知"] +n * > [id="com.lenovo.browser.hd:id/tv_cancle"]',
+      snapshotUrls: 'https://i.gkd.li/import/13401980',
+    },
+  ],
+});
