c760798cca44f6d0c56a5d91d5b318fbd8294fb4
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-20 21:45:31 +0800

    feat(小步点): 弹窗广告 (#2321)

--- src/apps/run.xbud.android.ts
+++ src/apps/run.xbud.android.ts
@@ -16,12 +16,12 @@ export default defineAppConfig({
     },
     {
       key: 1,
-      name: '广告弹窗',
-      activityIds: 'run.xbud.android.mvp.ui.other.MainActivity',
+      name: '弹窗广告',
       rules: [
         {
           key: 0,
           name: '快手广告',
+          activityIds: 'run.xbud.android.mvp.ui.other.MainActivity',
           matches:
             '[id="run.xbud.android:id/ksad_tk_view"] >n @ViewGroup[clickable=true] > ImageView',
           snapshotUrls: [
@@ -32,6 +32,7 @@ export default defineAppConfig({
         {
           key: 1,
           name: '腾讯广告1',
+          activityIds: 'run.xbud.android.mvp.ui.other.MainActivity',
           matches:
             'ImageView - LinearLayout < FrameLayout - FrameLayout > FrameLayout > ImageView',
           snapshotUrls: 'https://i.gkd.li/import/13296371',
@@ -39,9 +40,27 @@ export default defineAppConfig({
         {
           key: 2,
           name: '腾讯广告2',
+          activityIds: 'run.xbud.android.mvp.ui.other.MainActivity',
           matches:
             'ImageView -n FrameLayout > FrameLayout > FrameLayout > ImageView',
           snapshotUrls: 'https://i.gkd.li/import/13296398',
+        },
+        {
+          key: 3,
+          name: '字节广告-1',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches:
+            '@Image[text=""] < View +n View > View > TextView[text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/import/13414538',
+        },
+        {
+          key: 4,
+          name: '字节广告-2',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: '[id="com.byted.pangle:id/tt_reward_full_count_down_after"]',
+          snapshotUrls: 'https://i.gkd.li/import/13414544',
         },
       ],
     },
