3c62d5871c414276c1c96b1815d27da3c6675ae5
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-20 20:43:47 +0800

    feat(浙里办): 开屏广告（优化规则）、更新弹窗 (#2259)
    * feat 开屏广告（优化规则）
    
    * feat 更新弹窗

--- src/apps/com.hanweb.android.zhejiang.activity.ts
+++ src/apps/com.hanweb.android.zhejiang.activity.ts
@@ -7,11 +7,22 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds:
-        'com.alibaba.gov.android.welcome.activity.DefaultWelcomeActivity',
-      rules:
-        '[id="com.hanweb.android.zhejiang.activity:id/gov_splash_tv_count_down"][text ^= "跳过"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12913138',
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules:
+        '@[id="com.hanweb.android.zhejiang.activity:id/iv_cancel"] -2 * >2 [text="发现新版本"]',
+      snapshotUrls: 'https://i.gkd.li/import/13402048',
     },
   ],
 });
