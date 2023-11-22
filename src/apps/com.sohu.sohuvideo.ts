75ac67a1375d95cc5141d1de90233300e2a4fb9c
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-22 22:18:09 +0800

    feat(搜狐视频):开屏广告、版本更新 (#2394)
    close #2391

---
+++ src/apps/com.sohu.sohuvideo.ts
@@ -0,0 +1,30 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.sohu.sohuvideo',
+  name: '搜狐视频',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13435484',
+    },
+    {
+      key: 1,
+      name: '版本更新',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.sohu.sohuvideo.ui.homepage.MainActivity',
+      rules:
+        '@[id="com.sohu.sohuvideo:id/ivClose"] + LinearLayout [text="新版本上线啦"]',
+      snapshotUrls: 'https://i.gkd.li/import/13435504',
+    },
+  ],
+});
