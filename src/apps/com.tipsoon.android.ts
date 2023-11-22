c7781d56f8ddaf90bcc3742ba14952b7ff457b38
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-22 22:00:54 +0800

    feat(简讯): 弹窗广告 (#2365)

---
+++ src/apps/com.tipsoon.android.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tipsoon.android',
+  name: '简讯',
+  groups: [
+    {
+      key: 0,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '弹窗广告-1',
+          activityIds: 'com.tipsoon.android.activity.MainActivity',
+          matches:
+            '[id="com.tipsoon.android:id/cardview"] + [id="com.tipsoon.android:id/img_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13426997',
+        },
+      ],
+    },
+  ],
+});
