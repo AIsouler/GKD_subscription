9cf22244abfa50922fed0dbf47e79ed1eb5ff8a0
walklty <47232102+walklty@users.noreply.github.com>
2023-11-22 22:05:17 +0800

    feat(网易新闻): 开屏广告 (#2378)
    close #2375

--- src/apps/com.netease.newsreader.activity.ts
+++ src/apps/com.netease.newsreader.activity.ts
@@ -7,8 +7,11 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.netease.nr.biz.ad.newAd.AdActivity',
-      rules: '[text="广告"] < LinearLayout - LinearLayout > [text="跳过"]',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12639864',
     },
     {
