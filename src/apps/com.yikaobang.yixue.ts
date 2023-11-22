997712b0a8571115e272db82ae6c49891063aaa7
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-11-22 21:58:37 +0800

    feat(医考帮): 应用内会员活动弹窗 (#2361)
    close #2357

---
+++ src/apps/com.yikaobang.yixue.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.yikaobang.yixue',
+  name: '医考帮',
+  groups: [
+    {
+      key: 1,
+      name: '应用内会员活动弹窗',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds:
+        'com.psychiatrygarden.activity.online.AnswerQuestionActivity',
+      rules: [
+        {
+          matches: '@ImageView[id="com.yikaobang.yixue:id/close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13425853',
+        },
+      ],
+    },
+  ],
+});
