0535d4064288e66adc7fe0650b7379fee13c2ffa
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-22 21:55:07 +0800

    feat(书法字典大全): 信息流广告 (#2354)
    * feat(书法字典大全):信息流广告
    
    * Update com.zivn.cloudbrush3.ts
    
    close #2328

---
+++ src/apps/com.zivn.cloudbrush3.ts
@@ -0,0 +1,31 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.zivn.cloudbrush3',
+  name: '书法字典大全',
+  groups: [
+    {
+      enable: false,
+      key: 3,
+      name: '信息流广告',
+      quickFind: true,
+      activityIds: 'com.zivn.cloudbrush3.app.HomeActivity',
+      rules: [
+        {
+          key: 0,
+          name: '点击【x】',
+          matches:
+            '@View[clickable=true] < FrameLayout  -2 FrameLayout [text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/import/13425305',
+        },
+        {
+          preKeys: 0,
+          name: '点击【不感兴趣】',
+          matches:
+            '@LinearLayout > [id="com.byted.pangle.m:id/tt_item_tv"][text="不感兴趣"]',
+          snapshotUrls: 'https://i.gkd.li/import/13425306',
+        },
+      ],
+    },
+  ],
+});
