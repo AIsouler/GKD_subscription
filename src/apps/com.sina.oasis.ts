7513075857e77257ad14fe523cf77a03e133c01a
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-11-22 22:17:44 +0800

    feat(绿洲): 开屏广告及首页-推荐广告 (#2393)
    * feat(绿洲): 开屏广告及首页-推荐广告
    
    * feat(绿洲): 开屏广告
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>
    
    close #2389

---
+++ src/apps/com.sina.oasis.ts
@@ -0,0 +1,43 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.sina.oasis',
+  name: '绿洲',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13434430',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '首页-推荐广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          matches:
+            'ImageView[id="com.sina.oasis:id/gdt_v2_banner_ad_close_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13434468',
+        },
+        {
+          preKeys: 0,
+          matches: '[text*="关闭此广告"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13434471',
+        },
+      ],
+    },
+  ],
+});
