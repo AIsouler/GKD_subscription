a6616c7b78cbf8b1dc510449cc033b15867f317a
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-24 00:06:24 +0800

    feat(大淘客联盟): 开启消息通知 (#2446)
    * feat(大淘客联盟):开启消息通知
    
    * Update com.dataoke.union.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #2437

---
+++ src/apps/com.dataoke.union.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.dataoke.union',
+  name: '大淘客联盟',
+  groups: [
+    {
+      key: 1,
+      name: '开启消息通知',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.dtk.plat_home_lib.index.activity.IndexActivity',
+      rules:
+        '[text="取消"] <<n [id="com.dataoke.union:id/parentPanel"] [text^="你还没有打开通知哦"]',
+      snapshotUrls: 'https://i.gkd.li/import/13446826',
+    },
+  ],
+});
