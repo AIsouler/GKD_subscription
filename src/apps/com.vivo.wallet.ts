fb308fa29ba2d0b43f9229a39b36f452ce9a1b1a
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-11-23 23:59:37 +0800

    feat(钱包): 自动禁止通知权限授权（默认关闭） (#2415)
    close #2409

--- src/apps/com.vivo.wallet.ts
+++ src/apps/com.vivo.wallet.ts
@@ -18,5 +18,21 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '通知权限授权弹窗',
+      activityIds: 'com.vivo.wallet.bookkeep.activity.BookKeepMainActivity',
+      enable: false,
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: '@Button[text*="取消"]',
+          snapshotUrls: 'https://i.gkd.li/import/13440881',
+        },
+      ],
+    },
   ],
 });
