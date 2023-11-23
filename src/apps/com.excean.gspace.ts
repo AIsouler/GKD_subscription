ae10734dd22b1dc159599cc0309af7ca93768759
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-24 00:05:36 +0800

    feat(OurPlay): 弹窗广告 (#2443)
    close #2440

--- src/apps/com.excean.gspace.ts
+++ src/apps/com.excean.gspace.ts
@@ -20,12 +20,22 @@ export default defineAppConfig({
     {
       key: 1,
       name: '弹窗广告',
-      activityIds: 'com.excelliance.kxqp.splash.SplashActivity',
+      activityIds: [
+        'com.excelliance.kxqp.splash.SplashActivity',
+        'com.excelliance.kxqp.gs.main.MainActivity',
+      ],
       rules: [
         {
+          name: '类型1',
           matches:
             'FrameLayout[childCount=2] > LinearLayout[childCount=2] + FrameLayout[childCount=1] > ImageView',
           snapshotUrls: 'https://i.gkd.li/import/13302890',
+        },
+        {
+          name: '类型2',
+          matches:
+            'ImageView - LinearLayout - FrameLayout > FrameLayout[childCount=1] > ImageView[width<80][height<80]',
+          snapshotUrls: 'https://i.gkd.li/import/13447122',
         },
       ],
     },
