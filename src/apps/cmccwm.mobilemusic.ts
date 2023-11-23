ae78616116f1b434db78ad1429f72b2576695299
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-24 00:10:22 +0800

    feat(咪咕音乐): 版本更新 (#2462)
    * feat(咪咕音乐):版本更新
    
    * Update cmccwm.mobilemusic.ts
    
    close #2450

--- src/apps/cmccwm.mobilemusic.ts
+++ src/apps/cmccwm.mobilemusic.ts
@@ -7,9 +7,29 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'cmccwm.mobilemusic.ui.SplashActivity',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: '[id = "cmccwm.mobilemusic.base_runtime:id/bt_skip_ad"]',
       snapshotUrls: 'https://i.gkd.li/import/12852498',
+    },
+    {
+      key: 1,
+      name: '版本更新',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'cmccwm.mobilemusic.ui.base.MainActivity',
+      rules: [
+        {
+          action: 'back',
+          matches:
+            '[id="cmccwm.mobilemusic.libraries_feature:id/tv_title"][text="发现新版本"]',
+          snapshotUrls: 'https://i.gkd.li/import/13448898',
+        },
+      ],
     },
   ],
 });
