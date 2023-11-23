2ec19155a721087cd7d8e9ac057445bf71182b43
walklty <47232102+walklty@users.noreply.github.com>
2023-11-24 00:02:02 +0800

    feat(华为应用市场): 更新提示(兼容所有华为系统应用) (#2425)
    close #2417

--- src/apps/com.huawei.appmarket.ts
+++ src/apps/com.huawei.appmarket.ts
@@ -17,7 +17,10 @@ export default defineAppConfig({
     {
       key: 1,
       name: '更新弹窗',
-      activityIds: 'com.huawei.appmarket.MarketActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
       rules: '@[text="以后再说"] + [text="立即更新"]',
       snapshotUrls: 'https://i.gkd.li/import/13228520',
     },
