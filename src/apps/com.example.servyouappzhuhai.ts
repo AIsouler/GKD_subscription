83c53e2cd1db88d5ce7cd3bd293118e06d45fda4
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-11-23 23:58:28 +0800

    feat(广东税务): 通知权限授权弹窗 (#2412)
    * feat(广东税务): 通知权限授权弹窗
    
    * chore(actions): check_format_lint
    
    * fix(广东税务): 通知权限授权规则默认关闭
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #2405

---
+++ src/apps/com.example.servyouappzhuhai.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.example.servyouappzhuhai',
+  name: '广东税务',
+  groups: [
+    {
+      key: 0,
+      name: '通知权限授权弹窗',
+      enable: false,
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: '[id="com.example.servyouappzhuhai:id/btn_dialog_negtive"]',
+          snapshotUrls: 'https://i.gkd.li/import/13440721',
+        },
+      ],
+    },
+  ],
+});
