199ea273e2e17562cc55ca1544b8c7971a088db9
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-24 00:09:57 +0800

    feat(咪咕快游): 版本更新 (#2461)
    * feat(咪咕快游):版本更新
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #2449

--- src/apps/cn.emagsoftware.gamehall.ts
+++ src/apps/cn.emagsoftware.gamehall.ts
@@ -14,5 +14,17 @@ export default defineAppConfig({
       rules: '[id="cn.emagsoftware.gamehall:id/splash_skip"]',
       snapshotUrls: 'https://i.gkd.li/import/13258853',
     },
+    {
+      key: 1,
+      name: '版本更新',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.migugame.home_module.ui.activity.HomeActivity',
+      rules:
+        '[id="cn.emagsoftware.gamehall:id/btn_update"] -2 [id="cn.emagsoftware.gamehall:id/btn_cancle"]',
+      snapshotUrls: 'https://i.gkd.li/import/13448894',
+    },
   ],
 });
