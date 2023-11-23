b52bc0cc37e4c18116cc88b5c131f2a9037efff5
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-24 00:09:12 +0800

    feat(搜狐视频): 青少年模式 (#2459)
    close #2447

--- src/apps/com.sohu.sohuvideo.ts
+++ src/apps/com.sohu.sohuvideo.ts
@@ -26,5 +26,17 @@ export default defineAppConfig({
         '@[id="com.sohu.sohuvideo:id/ivClose"] + LinearLayout [text="新版本上线啦"]',
       snapshotUrls: 'https://i.gkd.li/import/13435504',
     },
+    {
+      key: 2,
+      name: '青少年模式',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.sohu.sohuvideo.ui.homepage.MainActivity',
+      rules:
+        '@[id="com.sohu.sohuvideo:id/tv_ok"] -2 LinearLayout > [text="设置青少年模式"]',
+      snapshotUrls: 'https://i.gkd.li/import/13448876',
+    },
   ],
 });
