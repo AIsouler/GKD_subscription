35b063e86968053744a7d753d99e2beebc916b13
lisonge <i@songe.li>
2023-11-23 15:31:02 +0800

    feat(得力e+): 开屏广告-规则优化

--- src/apps/com.delicloud.app.smartoffice.ts
+++ src/apps/com.delicloud.app.smartoffice.ts
@@ -7,8 +7,17 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.delicloud.app.smartoffice.mvp.ui.JiGuangSpalshActivity',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      matchTime: 10000,
       rules: [
+        {
+          quickFind: true,
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13445578',
+          exampleUrls:
+            'https://m.gkd.li/38517192/52482ac3-a5aa-4559-a006-0103ea0b8238',
+        },
         {
           matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
           snapshotUrls: ['https://i.gkd.li/import/12843738'],
