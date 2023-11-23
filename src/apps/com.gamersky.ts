b575cfedea0eba045852945864f51ace8487b3fd
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-24 00:16:29 +0800

    feat(游民星空): 开屏广告（优化规则）、卡片式广告 (#2470)
    * feat 开屏广告（优化规则）
    
    * feat 卡片式广告

--- src/apps/com.gamersky.ts
+++ src/apps/com.gamersky.ts
@@ -7,15 +7,36 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.gamersky.main.activity.LibMainActivity',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12848922',
+        'https://i.gkd.li/import/13038181',
+      ],
+    },
+    {
+      key: 1,
+      name: '卡片式广告',
       rules: [
         {
-          matches: 'View[clickable= true] + TextView[text ^= "跳过"]',
-          snapshotUrls: 'https://i.gkd.li/import/12848922',
+          key: 0,
+          name: '首页卡片广告',
+          activityIds: 'com.gamersky.main.activity.LibMainActivity',
+          quickFind: true,
+          matches:
+            '[id="com.gamersky:id/tap_video_ad_root"] > [id="com.gamersky:id/delete"]',
+          snapshotUrls: 'https://i.gkd.li/import/13451220',
         },
         {
-          matches: '[id="com.gamersky:id/jump"]',
-          snapshotUrls: 'https://i.gkd.li/import/13038181',
+          key: 1,
+          name: '正文底部卡片广告',
+          activityIds:
+            'com.gamersky.common.activity.LibDetailContentDetailActivity',
+          matches: 'View[childCount=2] > [text="广告"] + View[clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13451258',
         },
       ],
     },
