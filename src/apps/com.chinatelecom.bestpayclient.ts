6d82298227d2a6d653d392ff20269457a0756324
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-26 21:06:25 +0800

    feat(翼支付): 广告弹窗 (#2485)
    * feat(翼支付):广告弹窗
    
    * chore(actions): check_format_lint
    
    * Update com.chinatelecom.bestpayclient.ts
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #2480

--- src/apps/com.chinatelecom.bestpayclient.ts
+++ src/apps/com.chinatelecom.bestpayclient.ts
@@ -33,9 +33,29 @@ export default defineAppConfig({
       actionMaximum: 1,
       resetMatch: 'app',
       activityIds: 'com.chinatelecom.bestpayclient.ui.MainActivity',
-      rules:
-        '[id^="am-modal-container"] Dialog @View[clickable=true][index=1] > Image',
-      snapshotUrls: 'https://i.gkd.li/import/13402692',
+      rules: [
+        {
+          key: 0,
+          name: '权益页',
+          matches:
+            'Image < @View[clickable=true][index=1] <<n Dialog <<n [id="com.chinatelecom.bestpayclient:id/best_h5_container"]',
+          snapshotUrls: 'https://i.gkd.li/import/13402692',
+        },
+        {
+          key: 1,
+          name: '首页',
+          matches:
+            '[id="com.chinatelecom.bestpayclient:id/iv_dialog_close_one"]',
+          snapshotUrls: 'https://i.gkd.li/import/13455790',
+        },
+        {
+          key: 2,
+          name: '借贷页',
+          matches:
+            '@View[clickable=true][text=""] <<n [id="com.chinatelecom.bestpayclient:id/best_h5_container"]',
+          snapshotUrls: 'https://i.gkd.li/import/13455929',
+        },
+      ],
     },
     {
       enable: false,
