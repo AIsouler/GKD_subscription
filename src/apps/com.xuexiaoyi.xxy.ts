66f38eb83444a6eddfe01017300aa3079e2b1968
aoguai <34203474+aoguai@users.noreply.github.com>
2023-11-26 21:09:48 +0800

    feat(学小易): 首页-底部广告规则优化 (#2494)

--- src/apps/com.xuexiaoyi.xxy.ts
+++ src/apps/com.xuexiaoyi.xxy.ts
@@ -20,13 +20,16 @@ export default defineAppConfig({
     {
       key: 1,
       name: '首页-底部广告',
-      activityIds: 'com.xuexiaoyi.entrance.main.MainActivity',
+      activityIds: [
+        'com.xuexiaoyi.entrance.main.MainActivity',
+        'com.xuexiaoyi.platform.base.arch.RouterFragmentActivity',
+      ],
       rules: [
         {
           key: 0,
           name: '点击在下方的X',
           matches: [
-            'View[text="广告"] < View <n View <n View > View > Image[text.length=0]',
+            'View[text="广告"&&longClickable=null] < View <n View <n View > View > Image[text.length=0]', // longClickable=null为了与 key:3 区别防止误触
           ],
           snapshotUrls: [
             'https://i.gkd.li/import/13068836',
@@ -38,13 +41,15 @@ export default defineAppConfig({
           key: 1,
           name: '点击在上方的X-类型1',
           matches: [
-            '[text.length>=2&&text.length<=6] -n View > Image[text.length=0&&childCount=0]',
+            'Image[text.length>=20] < View < View + View > Image[text.length=0]',
           ],
           snapshotUrls: [
             'https://i.gkd.li/import/13068811',
             'https://i.gkd.li/import/13202990',
             'https://i.gkd.li/import/13266356',
             'https://i.gkd.li/import/13266362',
+            'https://i.gkd.li/import/13457114',
+            'https://i.gkd.li/import/13457204',
           ],
         },
         {
@@ -56,16 +61,28 @@ export default defineAppConfig({
           snapshotUrls: ['https://i.gkd.li/import/13266689'],
         },
         {
-          preKeys: [0, 1, 2],
+          key: 3,
+          name: '点击在内部的X',
+          matches: 'View[text.length=4] - View > View > Image[text.length=0]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13457210',
+            'https://i.gkd.li/import/13457302',
+          ],
+        },
+        {
+          preKeys: [0, 1, 2, 3],
           key: 10,
           name: '点击不感兴趣',
           matches: 'TextView[text="不感兴趣"]',
-          snapshotUrls: 'https://i.gkd.li/import/13068819',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13068819',
+            'https://i.gkd.li/import/13457380',
+          ],
         },
       ],
     },
     {
-      key: 2,
+      key: 10,
       name: '升级弹窗',
       desc: '自动点击以后再说。',
       activityIds: 'com.xuexiaoyi.entrance.main.MainActivity',
