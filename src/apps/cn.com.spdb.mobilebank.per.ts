9afc46d092e2f320591efb091ca8d6af328fe667
aoguai <34203474+aoguai@users.noreply.github.com>
2023-11-26 21:10:06 +0800

    feat(浦发银行): 消息中心-系统通知请求 (#2496)

--- src/apps/cn.com.spdb.mobilebank.per.ts
+++ src/apps/cn.com.spdb.mobilebank.per.ts
@@ -12,5 +12,15 @@ export default defineAppConfig({
         '[id="cn.com.spdb.mobilebank.per:id/splash_tb_time"][text$="跳过"]',
       snapshotUrls: 'https://i.gkd.li/import/12740308',
     },
+    {
+      key: 1,
+      name: '消息中心-系统通知请求',
+      desc: '自动点击关闭。',
+      enable: false,
+      activityIds:
+        'cn.com.spdb.mobilebank.per.activity.web.stage.StageCommonWebActivity',
+      rules: '[text="去开启"] + View > Image[text.length=0]',
+      snapshotUrls: 'https://i.gkd.li/import/13458535',
+    },
   ],
 });
