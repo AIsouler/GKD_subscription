1b24a354cce883e70fdfdf3ee418c8d168efda67
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-26 21:06:43 +0800

    feat(OPPO软件商店): 版本更新 (#2486)
    * feat(OPPO软件商店):版本更新
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.heytap.market.ts
+++ src/apps/com.heytap.market.ts
@@ -2,16 +2,35 @@ import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
   id: 'com.heytap.market',
-  name: '软件商店',
+  name: 'OPPO软件商店',
   groups: [
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/12506561',
+    },
+    {
+      key: 1,
+      name: '版本更新',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       activityIds:
         'com.heytap.cdo.client.cards.page.main.maintab.MainTabActivity',
-      rules:
-        '[id="android:id/content"] > RelativeLayout > LinearLayout > [text="跳过"||text="Skip"]',
-      snapshotUrls: 'https://i.gkd.li/import/12506561',
+      rules: [
+        {
+          action: 'back',
+          matches:
+            '[id="com.heytap.market:id/tv_titile"][text="发现软件商店新版本"]',
+          snapshotUrls: 'https://i.gkd.li/import/13455965',
+        },
+      ],
     },
   ],
 });
