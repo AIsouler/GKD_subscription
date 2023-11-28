a4491851f8b53c0de67ddc0a623fa4d874acd81e
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-28 20:03:36 +0800

    feat(得物): 截屏分享 (#2592)
    * feat(得物):截屏分享
    
    * Update com.shizhuang.duapp.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.shizhuang.duapp.ts
+++ src/apps/com.shizhuang.duapp.ts
@@ -7,13 +7,24 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.shizhuang.duapp.modules.home.ui.SplashActivity',
-      rules: [
-        {
-          matches: '[id="com.shizhuang.duapp:id/tv_countdown_bottom"]',
-          snapshotUrls: 'https://i.gkd.li/import/12841026',
-        },
-      ],
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/12841026',
+    },
+    {
+      enable: false,
+      key: 4,
+      name: '截屏分享',
+      desc: '关闭截屏时app弹出的分享弹窗',
+      quickFind: true,
+      activityIds:
+        'com.shizhuang.duapp.modules.product_detail.detailv4.ui.ProductDetailActivityV4',
+      rules:
+        '[id="com.shizhuang.duapp:id/ivClose"] +2 [id="com.shizhuang.duapp:id/ivSave"]',
+      snapshotUrls: 'https://i.gkd.li/import/13473449',
     },
   ],
 });
