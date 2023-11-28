06cd0fa3dc7e5efaa9665f5a8fe0bbc8d1ca7d37
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-28 20:02:43 +0800

    feat(小米手机管家): 禁止获取定位、忽略体检分数 (#2590)
    close #2573
    close #2574
    close #2575

--- src/apps/com.miui.securitycenter.ts
+++ src/apps/com.miui.securitycenter.ts
@@ -2,7 +2,7 @@ import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
   id: 'com.miui.securitycenter',
-  name: '安全服务',
+  name: '小米手机管家',
   groups: [
     {
       enable: false,
@@ -15,6 +15,49 @@ export default defineAppConfig({
         {
           matches: '[text="继续安装"]',
           snapshotUrls: 'https://i.gkd.li/import/13269875',
+        },
+      ],
+    },
+    {
+      enable: false,
+      key: 11,
+      name: '禁止获取定位',
+      desc: '关闭【允许联网及定位】后，每次打开手机管家都会出现',
+      quickFind: true,
+      activityIds: [
+        'com.miui.securityscan.MainActivity', // app版本v8
+        'com.miui.permcenter.permissions.SystemAppPermissionDialogActivity', // app版本v5
+      ],
+      rules: [
+        {
+          matches: [
+            '[id="com.miui.securitycenter:id/title"][text="获取位置信息"]',
+            '[text="不同意"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/import/13474517',
+            'https://i.gkd.li/import/13476592', // activityIds: 'com.miui.permcenter.permissions.SystemAppPermissionDialogActivity',
+          ],
+        },
+      ],
+    },
+    {
+      enable: false,
+      key: 12,
+      name: '忽略体检分数',
+      desc: '退出时忽略体检优化分数',
+      quickFind: true,
+      activityIds: 'com.miui.securityscan.MainActivity',
+      rules: [
+        {
+          matches: [
+            '[text="手机体检分数不足75分，是否立即优化？"][id$="id/message"]',
+            '[text="退出"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/import/13474504', // app版本v5
+            'https://i.gkd.li/import/13476770', // app版本v8
+          ],
         },
       ],
     },
