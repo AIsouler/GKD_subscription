fd4d13a1519d05de81a844cc4d5a85536918221a
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-28 19:54:49 +0800

    feat(酷狗音乐): 开屏广告 (#2564)
    * feat(酷狗音乐):开屏广告
    
    * feat(酷狗音乐): 开屏广告
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>
    
    close #2552

--- src/apps/com.kugou.android.ts
+++ src/apps/com.kugou.android.ts
@@ -10,10 +10,15 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: 'ImageView[desc="miniLogoView"] + View[clickable=true]',
-      snapshotUrls: [
-        'https://i.gkd.li/import/12775410',
-        'https://i.gkd.li/import/13426030',
+      rules: [
+        {
+          matches: '[desc="跳过"||desc="GdtCountDownView"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12775410',
+            'https://i.gkd.li/import/13426030',
+            'https://i.gkd.li/import/13468987',
+          ],
+        },
       ],
     },
   ],
