7f0341d2ae5eaf383689cda4b13620f947b362eb
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-28 19:55:47 +0800

    feat(驾考宝典): 开屏广告（优化规则）、悬浮广告、弹窗广告 (#2576)
    * feat 开屏广告（优化规则）
    
    * feat 悬浮广告
    
    * feat 弹窗广告

--- src/apps/com.handsgo.jiakao.android.ts
+++ src/apps/com.handsgo.jiakao.android.ts
@@ -7,8 +7,41 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.handsgo.jiakao.android.splash.Login',
-      rules: '[id=`com.handsgo.jiakao.android:id/closeLayout`]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id=`com.handsgo.jiakao.android:id/closeLayout`]', // 缺少快照
+    },
+    {
+      key: 1,
+      name: '悬浮广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.handsgo.jiakao.android.main.activity.MainActivity',
+          matches:
+            '@[id="com.handsgo.jiakao.android:id/ivDelete"] + [id="com.handsgo.jiakao.android:id/ivRemote"]',
+          snapshotUrls: 'https://i.gkd.li/import/13475994',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '字节广告',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: [
+            '[desc^="logoad"] > [text="广告"]',
+            '[desc^="webview-close"] > View[clickable=true]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13476039',
+        },
+      ],
     },
   ],
 });
