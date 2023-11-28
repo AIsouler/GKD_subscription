2169ed1e20e5b0d32aca9649f32c8219d75f9918
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-28 19:56:12 +0800

    feat(影视仓): 卡片式广告 (#2577)

---
+++ src/apps/com.movie.store.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.movie.store',
+  name: '影视仓',
+  groups: [
+    {
+      key: 1,
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 0,
+          name: '快手广告',
+          activityIds:
+            'com.github.tvbox.osc.ui.activity.mobile.MobileHomeActivity',
+          matches: '[text="广告"] +2 View > View + TextView',
+          snapshotUrls: 'https://i.gkd.li/import/13476107',
+        },
+      ],
+    },
+  ],
+});
