96e150fe777ddeb23fdd893dbb80e19cd0b29981
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-28 19:57:58 +0800

    feat(湘潭出行): 弹窗广告 (#2581)

---
+++ src/apps/com.whpe.qrcode.hunan_xiangtan.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.whpe.qrcode.hunan_xiangtan',
+  name: '湘潭出行',
+  groups: [
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '快手广告',
+          activityIds: 'com.whpe.qrcode.hunan_xiangtan.MainActivity',
+          quickFind: true,
+          matches:
+            '@[id="com.whpe.qrcode.hunan_xiangtan:id/iv_close"] +n * >n [text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/import/13476265',
+        },
+      ],
+    },
+  ],
+});
