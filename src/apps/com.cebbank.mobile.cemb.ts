59c1756448f8416f91f65b711796e240bc698ca2
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-28 19:55:26 +0800

    feat(光大银行): 开屏广告、首页弹窗广告 (#2565)
    close #2562

--- src/apps/com.cebbank.mobile.cemb.ts
+++ src/apps/com.cebbank.mobile.cemb.ts
@@ -5,8 +5,22 @@ export default defineAppConfig({
   name: '光大银行',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13471079',
+    },
+    {
       key: 1,
       name: '更新弹窗',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       activityIds: 'com.cebbank.mobile.cemb.view.CustomizedDialog',
       rules: '@[text="不再提示"] +2 [text="立即更新"]',
       snapshotUrls: 'https://i.gkd.li/import/12727241',
@@ -14,9 +28,19 @@ export default defineAppConfig({
     {
       key: 2,
       name: '首页广告弹窗',
-      activityIds: 'com.cebbank.mobile.cemb.ui.activity.SplashActivity',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: [
+        'com.cebbank.mobile.cemb.ui.activity.SplashActivity',
+        'com.cebbank.mobile.cemb.ui.activity.HomeActivity',
+      ],
       rules: '[id="com.cebbank.mobile.cemb:id/home_advertising_iv_close"]',
-      snapshotUrls: 'https://i.gkd.li/import/12727248',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12727248',
+        'https://i.gkd.li/import/13471080', // activityIds: 'com.cebbank.mobile.cemb.ui.activity.HomeActivity',
+      ],
     },
   ],
 });
