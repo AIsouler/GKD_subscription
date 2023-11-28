7da9e409845f2cdd058c4769000c399d677d96d8
walklty <47232102+walklty@users.noreply.github.com>
2023-11-28 19:44:41 +0800

    feat(拷贝漫画): 数个弹窗广告, 系统确认, 开屏检测时间延长 (#2550)
    * Update com.copymanga.app.ts
    
    * Update com.copymanga.app.ts
    
    * Update com.copymanga.app.ts

--- src/apps/com.copymanga.app.ts
+++ src/apps/com.copymanga.app.ts
@@ -7,14 +7,13 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      matchLauncher: true,
-      activityIds: [
-        'com.copymanga.app.MainActivity',
-        'com.reaper.flutter.reaper_flutter_plugin.activity.ReaperSplashActivity',
-      ],
+      matchTime: 30000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
       rules: [
         {
-          matches: '[text *= "跳过" && text.length<6]',
+          matches: '[text^="跳过"][text.length<6]',
           snapshotUrls: [
             'https://i.gkd.li/import/12504489',
             'https://i.gkd.li/import/12504507',
@@ -28,52 +27,87 @@ export default defineAppConfig({
       key: 1,
       name: '内部弹窗广告',
       matchLauncher: true,
-      activityIds: ['com.copymanga.app.MainActivity'],
-      delay: 500,
+      activityIds: [
+        'com.copymanga.app.MainActivity',
+        'com.reaper.flutter.reaper_flutter_plugin.activity.ReaperSplashActivity',
+      ],
+      delay: 400,
       rules: [
         {
+          key: 1,
           activityIds: 'com.kwad.components.ad.interstitial',
-          matches: 'TextView[text="跳过"]',
-          snapshotUrls: 'https://i.gkd.li/import/12504486',
-        },
-        {
-          activityIds: 'com.kwad.components.ad.interstitial',
-          matches: 'ViewGroup[clickable=true] > ImageView',
-          snapshotUrls: 'https://i.gkd.li/import/12504488',
-        },
-        {
-          matches:
-            'ImageView < FrameLayout < FrameLayout +2 FrameLayout > ImageView',
-          snapshotUrls: 'https://i.gkd.li/import/12504501',
-        },
-        {
-          matches: 'ImageView + FrameLayout + FrameLayout > ImageView',
+          matches: '@ViewGroup[clickable=true][visibleToUser=true] > ImageView',
           snapshotUrls: [
-            'https://i.gkd.li/import/12504520',
-            'https://i.gkd.li/import/12661019',
+            'https://i.gkd.li/import/12504486',
+            'https://i.gkd.li/import/12504488',
           ],
         },
         {
-          matches:
-            '@ImageView < FrameLayout[childCount=1] +2 FrameLayout[childCount=2] >2 TextView[text$="看看"][text^="去"]',
-          snapshotUrls: 'https://i.gkd.li/import/12504501',
+          key: 2,
+          matches: [
+            '[text^="立即" || text^="领取" || text^="了解" || text*="看"]', //软件正常元素都用的des,只有广告用text
+            '[id="android:id/content"] >2 FrameLayout[childCount=4|| childCount=3||childCount=2] > FrameLayout[childCount=5||childCount=6||childCount=8] > FrameLayout[childCount=1] > [name="android.view.View" || name="android.widget.ImageView"][text=null][visibleToUser=true]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/import/12504520',
+            'https://i.gkd.li/import/12661019',
+            'https://i.gkd.li/import/13193877',
+            'https://i.gkd.li/import/12892156',
+            'https://i.gkd.li/import/12504501',
+            'https://i.gkd.li/import/13246786',
+            'https://i.gkd.li/import/13259082', // text^="领取"
+            'https://i.gkd.li/import/13372542', // View
+          ],
         },
         {
-          matches:
-            'TextView[text.length=4] < FrameLayout[childCount>=1] <2 FrameLayout[childCount>=2] -2 FrameLayout > ImageView',
-          snapshotUrls: 'https://i.gkd.li/import/12892156', //最后是ImageView否则和io/import/12504501误触
+          key: 3,
+          matches: [
+            '[text^="京东"]',
+            '[text^="扭"]',
+            'ImageView[index=0][depth=9]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/import/13233178',
+            'https://i.gkd.li/import/13259085',
+          ],
         },
         {
+          key: 4,
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
           matches: '[text="反馈"] -2 @View - Image',
           snapshotUrls: 'https://i.gkd.li/import/12925052',
         },
         {
+          key: 5,
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
           matches: '[text="反馈"] -4 @View < View[childCount=7]',
           snapshotUrls: 'https://i.gkd.li/import/12925095',
+        },
+        {
+          key: 6,
+          matches:
+            '[id="com.copymanga.app:id/video_view"] + [id="com.copymanga.app:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13332719',
+        },
+        {
+          key: 7,
+          // desc: '二步跳过广告_步骤1',
+          quickFind: true,
+          matches: '[text="跳过"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13344156', // step1
+          ],
+        },
+        {
+          key: 8,
+          preKeys: [7],
+          // desc: '二步跳过广告_步骤2',
+          matches: 'ImageView < ViewGroup[clickable=true][childCount=1]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13344156', // step2
+          ],
         },
       ],
     },
@@ -106,6 +140,32 @@ export default defineAppConfig({
         {
           matches: '@[desc="就这样吧"] + [desc^="赞助免广告"]',
           snapshotUrls: 'https://i.gkd.li/import/12851627',
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '自动确认线路',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[desc="进入拷贝漫画"]',
+          snapshotUrls: 'https://i.gkd.li/import/13233179',
+        },
+      ],
+    },
+    {
+      key: 5,
+      name: '关闭提示',
+      matchTime: 30000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[desc="系统提示"] +n [desc="确定"]',
+          snapshotUrls: 'https://i.gkd.li/import/13233180',
         },
       ],
     },
