6ccffa69d890d0a6f0e1fbc59d92c8cdee6bf793
巴拉巴拉嘿 <83610194+superHao2000@users.noreply.github.com>
2023-11-30 21:58:48 +0800

    feat(多看阅读): 小说页下方广告和全屏广告 (#2651)
    * fix: duokan
    
    * feat(多看): 小说页全屏卡片广告
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>
    
    close #1588

--- src/apps/com.duokan.reader.ts
+++ src/apps/com.duokan.reader.ts
@@ -27,5 +27,38 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 4,
+      name: '小说页广告',
+      desc: '小说页面下方出现的条形广告',
+      quickFind: true,
+      activityIds: 'com.duokan.reader.DkMainActivity',
+      rules:
+        '@ImageView < FrameLayout[id="com.duokan.reader:id/reading__app_ad_view__close"]',
+      snapshotUrls: 'https://i.gkd.li/import/13497902',
+    },
+    {
+      key: 5,
+      name: '小说页全屏卡片广告',
+      desc: '点击关闭和确定按钮',
+      quickFind: true,
+      activityIds: 'com.duokan.reader.DkMainActivity',
+      rules: [
+        {
+          key: 0,
+          matches:
+            '@TextView[id="com.duokan.reader:id/reading__app_ad_view__close"][text="广告"]',
+        },
+        {
+          preKeys: 0,
+          matches:
+            '@TextView[id="com.duokan.reader:id/ad__feedback_close_dialog_button"][text="知道了"]',
+        },
+      ],
+      snapshotUrls: [
+        'https://i.gkd.li/import/13498048',
+        'https://i.gkd.li/import/13497990',
+      ],
+    },
   ],
 });
