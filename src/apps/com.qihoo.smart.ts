ed9f236fc3040616a2f3e4fc92d545a188d15f9b
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-30 21:48:43 +0800

    feat(360智慧生活):开屏广告 (#2691)
    close #2686

---
+++ src/apps/com.qihoo.smart.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.qihoo.smart',
+  name: '360智慧生活',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13424651',
+    },
+  ],
+});
