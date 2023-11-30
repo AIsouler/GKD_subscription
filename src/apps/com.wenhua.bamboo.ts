abd224d930da8b6ef9721d3ff5cffddbaa363de1
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-30 21:43:37 +0800

    feat(随身行):开屏广告 (#2685)

--- src/apps/com.wenhua.bamboo.ts
+++ src/apps/com.wenhua.bamboo.ts
@@ -5,6 +5,16 @@ export default defineAppConfig({
   name: '随身行',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13445602',
+    },
+    {
       key: 1,
       name: '通知弹窗',
       desc: '点击稍后再说',
@@ -15,7 +25,7 @@ export default defineAppConfig({
       rules: [
         {
           activityIds: 'com.wenhua.bamboo.screen.activity.MarketOptionActivity',
-          matches: '@Button[text=\'稍后再说\'] +2 Button[text=\'马上开启\']',
+          matches: '@Button[text="稍后再说"] +2 Button[text="马上开启"]',
           snapshotUrls: 'https://i.gkd.li/import/13407555',
         },
       ],
