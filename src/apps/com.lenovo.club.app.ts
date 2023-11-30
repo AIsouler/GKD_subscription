08ae18cb0cb14689085255f0728324a1d69a9b5a
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-30 21:36:17 +0800

    feat(联想):版本更新 (#2664)
    close #2658

--- src/apps/com.lenovo.club.app.ts
+++ src/apps/com.lenovo.club.app.ts
@@ -7,9 +7,24 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.lenovo.club.app.AdActivity',
-      rules: '[id="com.lenovo.club.app:id/tv_jump"][text$="跳过"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12829678',
+    },
+    {
+      key: 2,
+      name: '版本更新',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.lenovo.club.app.AdActivity',
+      rules:
+        '[id="com.lenovo.club.app:id/tv_title"][text="发现新版本"] < LinearLayout [text="下次再说"]',
+      snapshotUrls: 'https://i.gkd.li/import/13498778',
     },
   ],
 });
