bd91cfde5dc2536dd839c18d06f2e3ee823e6f70
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-30 21:35:54 +0800

    feat(万年历):开屏广告 (#2663)
    close #2659

--- src/apps/com.youloft.calendar.ts
+++ src/apps/com.youloft.calendar.ts
@@ -5,6 +5,16 @@ export default defineAppConfig({
   name: '万年历',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+      snapshotUrls: 'https://i.gkd.li/import/13499542',
+    },
+    {
       key: 1,
       name: '悬浮广告',
       rules: [
