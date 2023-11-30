11f7c781c20b09c1a2dee0810a3a7dd814903a4e
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-30 21:41:54 +0800

    feat(皮皮搞笑):开屏广告、青少年模式 (#2683)
    close #2670

--- src/apps/cn.xiaochuankeji.zuiyouLite.ts
+++ src/apps/cn.xiaochuankeji.zuiyouLite.ts
@@ -14,16 +14,18 @@ export default defineAppConfig({
       rules: [
         {
           key: 0,
-          activityIds: 'cn.xiaochuankeji.zuiyouLite.ui.splash.SplashActivity',
+          name: '【字节穿山甲SDK】',
           matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
           snapshotUrls: 'https://i.gkd.li/import/12745084',
         },
         {
           key: 1,
-          activityIds: 'cn.xiaochuankeji.zuiyouLite.ui.splash.SplashActivity',
-          matches:
-            '[id="cn.xiaochuankeji.zuiyouLite:id/spalsh_ad_view"] >n [text="跳过"]',
-          snapshotUrls: 'https://i.gkd.li/import/12745095',
+          name: '【跳过】',
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12745095',
+            'https://i.gkd.li/import/13387320',
+          ],
         },
       ],
     },
@@ -34,9 +36,12 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      activityIds: 'cn.xiaochuankeji.zuiyouLite.ui.main.MainActivity',
+      activityIds: 'cn.xiaochuankeji.zuiyouLite.ui.main',
       rules: '[id="cn.xiaochuankeji.zuiyouLite:id/young_close_btn"]',
-      snapshotUrls: 'https://i.gkd.li/import/12745083',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12745083',
+        'https://i.gkd.li/import/13446652', //activityIds: 'cn.xiaochuankeji.zuiyouLite.ui.main.MainTest',
+      ],
     },
     {
       enable: false,
