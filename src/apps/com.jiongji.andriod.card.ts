0e8b9b1163322c6f440b8c36f48a2cac569bd564
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-30 21:11:31 +0800

    feat(百词斩): 开屏广告、版本更新 (#2617)
    * feat(百词斩):版本更新
    
    * feat(百词斩):开屏广告
    
    close #2630
    close #2613

--- src/apps/com.jiongji.andriod.card.ts
+++ src/apps/com.jiongji.andriod.card.ts
@@ -5,12 +5,33 @@ export default defineAppConfig({
   name: '百词斩',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13495409',
+    },
+    {
       key: 1,
       name: '活动弹窗',
       activityIds: 'com.baicizhan.main.activity.MainTabActivity',
       rules:
         '[text="反馈"] + ImageView + ImageView[childCount=0][clickable=true]',
       snapshotUrls: 'https://i.gkd.li/import/13415075',
+    },
+    {
+      key: 3,
+      name: '版本更新',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.baicizhan.main.activity.MainTabActivity',
+      rules: '[text="抢先体验"] - [text="取消"]',
+      snapshotUrls: 'https://i.gkd.li/import/13488652',
     },
   ],
 });
