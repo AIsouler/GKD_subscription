afdfe754259c48f098985eca6fa00614028be1ad
dlh <22029832+dlh996@users.noreply.github.com>
2023-11-30 21:40:12 +0800

    fix(钛马星): 设备界面顶部广告 (#2676)
    * feat(钛马星): 规则优化
    
    * fix(钛马星): 修改快照连接
    
    * fix(钛马星): 修改快照连接
    
    ---------
    
    Co-authored-by: dlh <du__th@163.com>

--- src/apps/com.tima.carnet.m.main.ts
+++ src/apps/com.tima.carnet.m.main.ts
@@ -11,7 +11,6 @@ export default defineAppConfig({
         {
           matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
           snapshotUrls: [
-            'https://i.gkd.li/import/22029832/5aa3f016-d33c-4821-91bd-f23235b04ad3',
             'https://i.gkd.li/import/12594568',
             'https://i.gkd.li/import/12594574',
           ],
@@ -23,13 +22,53 @@ export default defineAppConfig({
       name: '每日弹窗广告',
       activityIds: [
         'com.tima.carnet.m.main.lib.common.dialog.TimaLoadingDialog',
+        'com.tima.carnet.m.main.app.main.NoticeActivity',
+        'com.tima.carnet.m.main.app.main.MainActivity',
+        'com.tima.carnet.m.main.LaunchActivity',
       ],
       rules: [
         {
           matches: [
             'CheckBox[id$="today_will_not_shown"] + ImageButton[clickable=true]',
           ],
-          snapshotUrls: ['https://i.gkd.li/import/12654432'],
+          snapshotUrls: [
+            'https://i.gkd.li/import/12654432',
+            'https://i.gkd.li/import/13507746',
+            'https://i.gkd.li/import/13508074',
+            'https://i.gkd.li/import/13508041',
+          ],
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '设备界面顶部广告',
+      activityIds: ['com.tima.carnet.m.main.app.main.MainActivity'],
+      rules: [
+        {
+          key: 1,
+          matches:
+            '[id="com.tima.carnet.m.main:id/fl_ad_banner"] >n Image[text!=null&&text!=""] < View +n View > Image',
+          snapshotUrls: ['https://i.gkd.li/import/13508030'],
+        },
+        {
+          key: 2,
+          matches:
+            '[id="com.tima.carnet.m.main:id/fl_ad_banner"] >n Image[text!=null&&text!=""] + View > Image',
+          snapshotUrls: ['https://i.gkd.li/import/13508115'],
+        },
+        {
+          key: 3,
+          preKeys: [1, 2],
+          matches: '[text="不感兴趣"]',
+          snapshotUrls: ['https://i.gkd.li/import/13508102'],
+        },
+        {
+          key: 4, //有时存在加载不出来的情况
+          preKeys: [1, 2],
+          action: 'back',
+          matches: 'ListView[childCount=0]',
+          snapshotUrls: ['https://i.gkd.li/import/13508072'],
         },
       ],
     },
