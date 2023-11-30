fcd6bb63bb05d38b226fc7f02b1bc1d4afeaf79f
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-30 21:47:41 +0800

    feat(我的听书):卡片式广告 (#2689)
    * feat(我的听书):卡片式广告
    
    * 格式更正

--- src/apps/com.github.eprendre.tingshu.ts
+++ src/apps/com.github.eprendre.tingshu.ts
@@ -11,29 +11,28 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[text^="跳过"][text.length<=10]',
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12783430',
     },
     {
       key: 1,
       name: '卡片式广告',
+      activityIds: [
+        'com.github.eprendre.tingshu.ui.search.AggregateSearchActivity',
+        'com.github.eprendre.tingshu.ui.play.PlayerActivity',
+      ],
       rules: [
         {
           key: 0,
-          name: '搜索结果底部广告卡片',
-          activityIds:
-            'com.github.eprendre.tingshu.ui.search.AggregateSearchActivity',
-          matches:
-            'ImageView - FrameLayout - FrameLayout[childCount=1] > ImageView[childCount=0]',
-          snapshotUrls: 'https://i.gkd.li/import/12783466',
-        },
-        {
-          key: 1,
-          name: '播放页面广告卡片',
+          name: '搜索结果底部/播放页面广告卡片',
           activityIds: 'com.github.eprendre.tingshu.ui.play.PlayerActivity',
           matches:
-            'ImageView + LinearLayout + LinearLayout + FrameLayout[childCount=1] > ImageView[childCount=0]',
-          snapshotUrls: 'https://i.gkd.li/import/13334850',
+            'FrameLayout > FrameLayout[childCount=1] > ImageView[width<80][height<80]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12783466',
+            'https://i.gkd.li/import/13334850',
+            'https://i.gkd.li/import/13446735',
+          ],
         },
       ],
     },
