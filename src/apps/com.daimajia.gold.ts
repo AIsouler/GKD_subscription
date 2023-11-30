f0acc7b023ed44518b068d1bd873bb73c8e23f0a
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-30 21:37:04 +0800

    feat(稀土掘金):版本更新 (#2666)
    close #2656

--- src/apps/com.daimajia.gold.ts
+++ src/apps/com.daimajia.gold.ts
@@ -7,8 +7,22 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'im.juejin.android.ui.SplashActivity',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: '[id=`com.daimajia.gold:id/fl_skip`]',
+    },
+    {
+      key: 1,
+      name: '版本更新',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules:
+        '@[id="com.daimajia.gold:id/iv_close"] -n LinearLayout [id="com.daimajia.gold:id/tv_title"][text="发现新版本"]',
+      snapshotUrls: 'https://i.gkd.li/import/13498703',
     },
   ],
 });
