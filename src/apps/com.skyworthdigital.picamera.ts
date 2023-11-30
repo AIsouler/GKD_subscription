489241561f57af69b2d9136428c7b90061aab7d2
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-30 21:27:02 +0800

    feat(创维智慧云):开屏广告 (#2647)
    close #2643

---
+++ src/apps/com.skyworthdigital.picamera.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.skyworthdigital.picamera',
+  name: '创维智慧云',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13497656',
+    },
+  ],
+});
