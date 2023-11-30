c5835dfa384efc136afdbb6b13252d9e0286c04d
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-30 21:12:00 +0800

    feat(微拾): 我的-卡片式广告、【字节】广告弹窗 (#2618)
    * feat(微拾):我的-卡片式广告、【字节】广告弹窗
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #2600

---
+++ src/apps/com.pleasure.trace_wechat.ts
@@ -0,0 +1,40 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.pleasure.trace_wechat',
+  name: '微拾',
+  groups: [
+    {
+      enable: false,
+      key: 5,
+      name: '我的-卡片式广告',
+      desc: '需点击二次弹窗确认',
+      quickFind: true,
+      activityIds: 'com.camellia.trace.activity.MainActivity',
+      rules: [
+        {
+          key: 0,
+          name: '点击【x】',
+          matches:
+            'View < @FrameLayout[desc^="dislike"] <<n [id="com.pleasure.trace_wechat:id/adsuyi_id_view_response"]',
+          snapshotUrls: 'https://i.gkd.li/import/13479466',
+        },
+        {
+          preKeys: 0,
+          name: '关闭原因【不感兴趣】',
+          matches:
+            '@LinearLayout > [id="com.byted.pangle:id/tt_item_tv"][text="不感兴趣"]',
+          snapshotUrls: 'https://i.gkd.li/import/13479469',
+        },
+      ],
+    },
+    {
+      key: 11,
+      name: '【字节】广告弹窗',
+      activityIds:
+        'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+      rules: '[text="反馈"] -2 @View > Image',
+      snapshotUrls: 'https://i.gkd.li/import/13479468',
+    },
+  ],
+});
