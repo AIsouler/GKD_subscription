0effea207351d9e9f1a4e1c9a20229c54826a636
walklty <47232102+walklty@users.noreply.github.com>
2023-11-30 21:54:21 +0800

    feat(金山文档): 列表内广告 (#2641)
    * Create cn.wps.yun.ts
    
    * feat(金山文档): 列表广告
    
    ---------
    
    Co-authored-by: lisonge <i@songe.li>
    
    close #2628

---
+++ src/apps/cn.wps.yun.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.wps.yun',
+  name: '金山文档',
+  groups: [
+    {
+      key: 1,
+      name: '列表广告',
+      desc: '点击右侧x',
+      rules: [
+        {
+          activityIds: 'cn.wps.yun.ui.IndexActivity',
+          quickFind: true,
+          matches:
+            '[id="cn.wps.yun:id/rootGroup"][childCount=5] >  [id="cn.wps.yun:id/moreIconRight"]',
+          exampleUrls:
+            'https://m.gkd.li/47232102/bc97cb52-aad0-4114-a548-5831edbe342d',
+          snapshotUrls: 'https://i.gkd.li/import/13495062',
+        },
+      ],
+    },
+  ],
+});
