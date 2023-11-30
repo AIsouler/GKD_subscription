9961ab260ebcc872c8c344f59f79a3680aa75f17
walklty <47232102+walklty@users.noreply.github.com>
2023-11-30 21:18:45 +0800

    feat(乐橙): 开屏广告 (#2642)
    close #2629

--- src/apps/com.mm.android.lc.ts
+++ src/apps/com.mm.android.lc.ts
@@ -7,9 +7,20 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.mm.android.lc.login.LCAdvertActivity',
-      rules: '[id="com.mm.android.lc:id/advert_skip_view"][text^="跳过"]',
-      snapshotUrls: 'https://i.gkd.li/import/12774801',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[id="com.mm.android.lc:id/advert_skip_view"][text^="跳过"]',
+          snapshotUrls: 'https://i.gkd.li/import/12774801',
+        },
+        {
+          quickFind: true,
+          matches: '[id="com.mm.android.lc:id/advert_countdown_view"]',
+          snapshotUrls: 'https://i.gkd.li/import/13454719',
+        },
+      ],
     },
   ],
 });
