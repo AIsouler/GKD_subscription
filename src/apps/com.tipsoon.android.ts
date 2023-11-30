7aad9ca2f055581ebf6efacc92ab670765640b93
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-30 21:36:39 +0800

    feat(简讯):开屏广告 (#2665)
    close #2657

--- src/apps/com.tipsoon.android.ts
+++ src/apps/com.tipsoon.android.ts
@@ -5,6 +5,16 @@ export default defineAppConfig({
   name: '简讯',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13498741',
+    },
+    {
       key: 0,
       name: '弹窗广告',
       rules: [
