3328beaa6854001c251b178152d9543e1d7f2683
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-03 16:48:44 +0800

    feat(浙里办):开屏宣传 (#2749)
    * feat(浙里办):开屏宣传
    
    * Update com.hanweb.android.zhejiang.activity.ts

--- src/apps/com.hanweb.android.zhejiang.activity.ts
+++ src/apps/com.hanweb.android.zhejiang.activity.ts
@@ -5,14 +5,18 @@ export default defineAppConfig({
   name: '浙里办',
   groups: [
     {
+      enable: false, // 政务app，首页涉及民生政策宣传及服务快捷入口，默认关闭
       key: 0,
-      name: '开屏广告',
+      name: '开屏宣传',
       quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[text^="跳过"][text.length<=10]',
-      snapshotUrls: 'https://i.gkd.li/import/12913138',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12913138',
+        'https://i.gkd.li/import/13530315',
+      ],
     },
     {
       key: 1,
