c106100e869a83a4a009fc51c6b408fb6e6f5a47
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-03 16:38:25 +0800

    feat(浙里办): 更新弹窗（补充规则） (#2716)

--- src/apps/com.hanweb.android.zhejiang.activity.ts
+++ src/apps/com.hanweb.android.zhejiang.activity.ts
@@ -17,12 +17,32 @@ export default defineAppConfig({
     {
       key: 1,
       name: '更新弹窗',
-      actionMaximum: 1,
       resetMatch: 'app',
       quickFind: true,
-      rules:
-        '@[id="com.hanweb.android.zhejiang.activity:id/iv_cancel"] -2 * >2 [text="发现新版本"]',
-      snapshotUrls: 'https://i.gkd.li/import/13402048',
+      rules: [
+        {
+          key: 0,
+          actionMaximum: 1,
+          matches:
+            '@[id="com.hanweb.android.zhejiang.activity:id/iv_cancel"] -2 * >2 [text="发现新版本"]',
+          snapshotUrls: 'https://i.gkd.li/import/13402048',
+        },
+        // key 1,2 共同发挥作用，先点击[立即升级]，然后返回，即可跳过更新弹窗
+        {
+          key: 1,
+          actionMaximumKey: 0,
+          matches:
+            'LinearLayout[childCount=2] > LinearLayout[childCount=2] > [text="立即升级"]',
+          snapshotUrls: 'https://i.gkd.li/import/13520598',
+        },
+        {
+          preKeys: 1,
+          key: 2,
+          matches:
+            '[text="版本更新"] - [id="com.hanweb.android.zhejiang.activity:id/rl_back"]',
+          snapshotUrls: 'https://i.gkd.li/import/13521059',
+        },
+      ],
     },
   ],
 });
