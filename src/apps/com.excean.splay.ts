709dc59ccdd6679bea0ae13192815beed3c54c29
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-03 16:36:26 +0800

    feat(OurPlay加速器): 开屏广告（优化规则）、更新弹窗（优化规则） (#2713)
    * feat 开屏广告（优化规则）
    
    * feat 更新弹窗（优化规则）

--- src/apps/com.excean.splay.ts
+++ src/apps/com.excean.splay.ts
@@ -7,34 +7,26 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: ['com.excelliance.kxqp.gs.main.MainActivity'],
-      rules: [
-        {
-          matches: 'ImageView -(4) LinearLayout[childCount=2] > [text^="跳过"]',
-          snapshotUrls: ['https://i.gkd.li/import/12684539'],
-        },
-        {
-          matches:
-            '[id="com.excean.splay:id/fl_jump"] - RelativeLayout > [text="跳过"][clickable=true]',
-          snapshotUrls: ['https://i.gkd.li/import/12684565'],
-        },
-        {
-          matches: '[id="com.excean.splay:id/render_ad_skip"][text^="跳过"]',
-          snapshotUrls: ['https://i.gkd.li/import/12684579'],
-        },
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12684539',
+        'https://i.gkd.li/import/12684565',
+        'https://i.gkd.li/import/12684579',
+        'https://i.gkd.li/import/13486087',
+        'https://i.gkd.li/import/13486740',
       ],
     },
     {
       key: 1,
       name: '更新弹窗',
-      activityIds: ['com.excelliance.kxqp.gs.main.MainActivity'],
-      rules: [
-        {
-          matches:
-            '[id="com.excean.splay:id/positive"][text="立即更新"] + [id="com.excean.splay:id/negative"][text="忽略"]',
-          snapshotUrls: ['https://i.gkd.li/import/12684551'],
-        },
-      ],
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text="发现新版本"] +n * > [text="忽略"]',
+      snapshotUrls: 'https://i.gkd.li/import/12684551',
     },
   ],
 });
