b2d32308a9a7bb92d32fc9c9775e735227c31cca
Zeng Weiyu <zerozwy@gmail.com>
2023-12-03 16:33:11 +0800

    feat(AcFun): 青少年模式弹窗 (#2697)
    * Update tv.acfundanmaku.video.ts
    
    * chore(actions): check_format_lint
    
    * Update tv.acfundanmaku.video.ts
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/tv.acfundanmaku.video.ts
+++ src/apps/tv.acfundanmaku.video.ts
@@ -20,5 +20,22 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 2,
+      name: '青少年模式弹窗',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          activityIds: 'tv.acfun.core.module.main.MainActivity',
+          matches: '[id="tv.acfundanmaku.video:id/btnSimpleDialogOne"]',
+          exampleUrls:
+            'https://m.gkd.li/6328439/8229e6ec-f1d6-4162-96ad-c3a6b74877c7',
+          snapshotUrls: 'https://i.gkd.li/import/13518737',
+        },
+      ],
+    },
   ],
 });
