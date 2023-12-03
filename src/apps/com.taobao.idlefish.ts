9ed3a480f6c8dce6e238e620fd1446234e31d66f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-03 17:05:13 +0800

    feat(闲鱼): 开屏广告（优化规则）、请求开启通知权限弹窗 (#2784)
    * feat 开屏广告（优化规则）
    
    * feat 请求开启通知权限弹窗

--- src/apps/com.taobao.idlefish.ts
+++ src/apps/com.taobao.idlefish.ts
@@ -7,19 +7,22 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: [
-        'com.taobao.idlefish.maincontainer.activity.MainActivity',
-        'com.taobao.fleamarket.home.activity.InitActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.taobao.idlefish:id/advert_close"]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12668670',
+        'https://i.gkd.li/import/13049015',
       ],
-      rules: [
-        {
-          matches: '[id="com.taobao.idlefish:id/advert_close"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/12668670', //com.taobao.idlefish.maincontainer.activity.MainActivity
-            'https://i.gkd.li/import/13049015', //com.taobao.fleamarket.home.activity.InitActivity
-          ],
-        },
-      ],
+    },
+    {
+      key: 1,
+      name: '请求开启通知权限弹窗',
+      activityIds:
+        'com.idlefish.flutterbridge.flutterboost.boost.FishFlutterBoostTransparencyActivity',
+      rules: '[desc^="开启系统通知"] > ImageView[clickable=true][desc=null]',
+      snapshotUrls: 'https://i.gkd.li/import/13538351',
     },
   ],
 });
