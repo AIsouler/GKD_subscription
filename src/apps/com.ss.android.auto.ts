05fec6b1e674e52b580eb4b58b06d42d406c0557
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-03 16:50:57 +0800

    feat(懂车帝): 规则优化 (#2753)
    * feat(懂车帝):规则优化
    
    * Update com.ss.android.auto.ts
    
    * Update com.ss.android.auto.ts
    
    * Update com.ss.android.auto.ts

--- src/apps/com.ss.android.auto.ts
+++ src/apps/com.ss.android.auto.ts
@@ -7,24 +7,32 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.ss.android.auto.activity.SplashActivity',
-      rules: '[desc="跳过广告"][id!=null]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@FrameLayout[clickable=true] > [text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12605327',
     },
     {
+      enable: false,
       key: 1,
       name: '首页推荐卡片广告',
+      desc: '需二次点击关闭原因',
+      quickFind: true,
       activityIds: 'com.ss.android.auto.activity.SplashActivity',
       rules: [
         {
           key: 0,
+          name: '点击【x】',
           matches:
-            '[text="广告"] < LinearLayout + @FrameLayout[clickable=true][id!=null] > TextView[text.length=1][id=null][clickable=false]',
+            'TextView[text.length=1][id=null][clickable=false] < @FrameLayout[clickable=true][id!=null] - LinearLayout > [text="广告"]',
           // 貌似快照存在延迟导致屏幕与节点不对应
           snapshotUrls: 'https://i.gkd.li/import/12660816',
         },
         {
           preKeys: 1,
+          name: '点击【不感兴趣】',
           matches:
             '[text$="精准屏蔽"] + [text="不感兴趣"][id!=null][clickable=true]',
           snapshotUrls: 'https://i.gkd.li/import/12711589',
@@ -34,22 +42,38 @@ export default defineAppConfig({
     {
       key: 2,
       name: '升级弹窗',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
       activityIds: 'com.ss.android.auto.activity.SplashActivity',
       rules:
         '@TextView[text="以后再说"] - FrameLayout >2 TextView[text$="升级"]',
-      snapshotUrls: 'https://i.gkd.li/import/12711631',
+      snapshotUrls: 'https://i.gkd.li/import/13534445',
     },
     {
       key: 3,
-      name: '首页直播推荐窗口',
-      activityIds: 'com.ss.android.auto.activity.SplashActivity',
+      name: '右下角悬浮窗',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'activity',
+      activityIds: [
+        'com.ss.android.auto.activity.SplashActivity',
+        'com.ss.android.auto.activity.ConcernDetailActivity',
+      ],
       rules:
-        '@ImageView[clickable=true][id!=null] - RelativeLayout >4 [id="com.ss.android.auto:id/at1"][text="特价抢购"]',
-      snapshotUrls: 'https://i.gkd.li/import/12798338',
+        'FrameLayout > RelativeLayout[childCount=2] > RelativeLayout + ImageView[clickable=true]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12798338',
+        'https://i.gkd.li/import/13535531',
+        'https://i.gkd.li/import/13535933',
+        'https://i.gkd.li/import/13535932',
+      ],
     },
     {
       key: 4,
       name: '文章底部广告',
+      quickFind: true,
       activityIds:
         'com.ss.android.article.base.feature.detail2.view.NewDetailActivity',
       rules:
@@ -59,6 +83,7 @@ export default defineAppConfig({
     {
       key: 5,
       name: '评论区广告',
+      quickFind: true,
       activityIds: [
         'com.ss.android.article.base.feature.detail2.view.NewDetailActivity',
         'com.ss.android.auto.ugc.video.activity.UgcLongPostActivity',
@@ -75,6 +100,9 @@ export default defineAppConfig({
     {
       key: 6,
       name: '请求推送通知弹窗',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
       activityIds: 'com.ss.android.auto.activity.SplashActivity',
       rules:
         '@TextView[clickable=true && text.length =1] + [text ^="打开推送通知"]',
