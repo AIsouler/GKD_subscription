ee23a4ebb45503193dad11ee419ee49559b3ebe2
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-03 16:48:14 +0800

    feat(网易云音乐):卡片式广告、播放界面广告 (#2746)
    * feat(网易云音乐):卡片式广告、播放界面广告
    
    * chore(actions): check_format_lint
    
    * Update com.netease.cloudmusic.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    close #2543

--- src/apps/com.netease.cloudmusic.ts
+++ src/apps/com.netease.cloudmusic.ts
@@ -17,13 +17,14 @@ export default defineAppConfig({
     {
       enable: false,
       key: 1,
-      name: '广告卡片',
-      desc: '点击【x】-【直接关闭】',
+      name: '卡片式广告',
+      desc: '有二次确认弹窗',
       quickFind: true,
-      matchDelay: 3000,
+      // matchDelay: 3000, 我又想不起来为什么要有这个了
       rules: [
         {
           key: 0,
+          name: '卡片式广告',
           activityIds: [
             'com.netease.cloudmusic.activity.MainActivity',
             'com.netease.cloudmusic.music.biz.comment.activity.CommentActivity',
@@ -41,8 +42,30 @@ export default defineAppConfig({
         },
         {
           preKeys: 0,
+          activityIds:
+            'com.netease.cloudmusic.module.ad.feedback.AdFeedbackBottomSheet',
           matches: '[text="直接关闭"]',
           snapshotUrls: ['https://i.gkd.li/import/12829967'],
+        },
+        {
+          key: 3,
+          name: '信息流广告-评论区内容推荐', // 考虑位置是否移出
+          activityIds:
+            'com.netease.cloudmusic.music.biz.comment.activity.CommentActivity',
+          matches:
+            '@ViewGroup[clickable=true][childCount=2] > ImageView - ViewGroup > TextView[text.length>0]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13526986',
+            'https://i.gkd.li/import/13526711',
+          ],
+        },
+        {
+          preKeys: 3,
+          activityIds:
+            'com.netease.cloudmusic.music.biz.comment.activity.CommentActivity',
+          matches:
+            '@[id="com.netease.cloudmusic:id/artist_container"] > [text="不感兴趣"]',
+          snapshotUrls: 'https://i.gkd.li/import/13526712',
         },
       ],
     },
@@ -159,15 +182,28 @@ export default defineAppConfig({
     {
       enable: false,
       key: 7,
-      name: '播放界面右上角-免费听歌',
+      name: '播放界面广告',
+      desc: '右上角VIP小悬浮、巨幅卡片式广告',
       quickFind: true,
       activityIds: 'com.netease.cloudmusic.activity.PlayerActivity',
-      rules:
-        'TextView[text!=null] + ImageView[id="com.netease.cloudmusic:id/close"]',
-      snapshotUrls: [
-        'https://i.gkd.li/import/13402634',
-        'https://i.gkd.li/import/13402635',
-        'https://i.gkd.li/import/13402636',
+      rules: [
+        {
+          key: 0,
+          name: '右上角VIP小悬浮',
+          matches:
+            'TextView[text!=null] + ImageView[id="com.netease.cloudmusic:id/close"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13402634',
+            'https://i.gkd.li/import/13402635',
+            'https://i.gkd.li/import/13402636',
+          ],
+        },
+        {
+          key: 1,
+          name: '巨幅卡片式广告',
+          matches: '[text^="跳过广告"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13527105',
+        },
       ],
     },
   ],
