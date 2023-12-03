946bd94bd8dcc53617550666363e4ec339427f4b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-03 16:42:08 +0800

    feat(魔音MORIN): 弹窗广告、卡片式广告 (#2726)
    * feat 弹窗广告
    
    * feat 卡片式广告

---
+++ src/apps/com.feiyu.morin.ts
@@ -0,0 +1,36 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.feiyu.morin',
+  name: '魔音MORIN',
+  groups: [
+    {
+      key: 0,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '腾讯广告',
+          activityIds: 'com.feiyu.morin.view.main.MainActivity',
+          matches:
+            'ImageView - FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: 'https://i.gkd.li/import/13521556',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 0,
+          name: '腾讯广告',
+          activityIds: 'com.feiyu.morin.view.main.MainActivity',
+          matches:
+            'ImageView + LinearLayout + LinearLayout + FrameLayout > ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13521680',
+        },
+      ],
+    },
+  ],
+});
