834ea9ce356ca78cdf771a517ffb45ba4c5b6a51
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-03 16:43:53 +0800

    feat(拷貝漫畫): 弹窗广告（优化规则） (#2728)

--- src/apps/com.copymanga.app.ts
+++ src/apps/com.copymanga.app.ts
@@ -25,7 +25,7 @@ export default defineAppConfig({
     },
     {
       key: 1,
-      name: '内部弹窗广告',
+      name: '弹窗广告',
       matchLauncher: true,
       activityIds: [
         'com.copymanga.app.MainActivity',
@@ -34,80 +34,91 @@ export default defineAppConfig({
       delay: 400,
       rules: [
         {
+          key: 0,
+          name: '快手广告-1',
+          matches: [
+            'ViewGroup > [text="广告"]',
+            'ViewGroup[childCount=3] > TextView + TextView + ImageView[clickable=true]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13259085',
+        },
+        {
           key: 1,
+          name: '快手广告-2',
           activityIds: 'com.kwad.components.ad.interstitial',
-          matches: '@ViewGroup[clickable=true][visibleToUser=true] > ImageView',
+          matches:
+            '[text="广告"] <2 ViewGroup -(2,3) ViewGroup > ViewGroup[clickable=true][visibleToUser=true]',
           snapshotUrls: [
             'https://i.gkd.li/import/12504486',
             'https://i.gkd.li/import/12504488',
+            'https://i.gkd.li/import/13344156',
           ],
         },
         {
           key: 2,
-          matches: [
-            '[text^="立即" || text^="领取" || text^="了解" || text*="看"]', //软件正常元素都用的des,只有广告用text
-            '[id="android:id/content"] >2 FrameLayout[childCount=4|| childCount=3||childCount=2] > FrameLayout[childCount=5||childCount=6||childCount=8] > FrameLayout[childCount=1] > [name="android.view.View" || name="android.widget.ImageView"][text=null][visibleToUser=true]',
-          ],
+          name: '腾讯广告-1',
+          activityIds: 'com.copymanga.app.MainActivity',
+          matches:
+            'ImageView -(1,2) FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
           snapshotUrls: [
             'https://i.gkd.li/import/12504520',
             'https://i.gkd.li/import/12661019',
             'https://i.gkd.li/import/13193877',
             'https://i.gkd.li/import/12892156',
             'https://i.gkd.li/import/12504501',
-            'https://i.gkd.li/import/13246786',
-            'https://i.gkd.li/import/13259082', // text^="领取"
-            'https://i.gkd.li/import/13372542', // View
+            'https://i.gkd.li/import/13259082',
           ],
         },
         {
           key: 3,
-          matches: [
-            '[text^="京东"]',
-            '[text^="扭"]',
-            'ImageView[index=0][depth=9]',
-          ],
-          snapshotUrls: [
-            'https://i.gkd.li/import/13233178',
-            'https://i.gkd.li/import/13259085',
-          ],
+          name: '腾讯广告-2',
+          matches:
+            'ImageView <2 FrameLayout - FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: 'https://i.gkd.li/import/13246786',
         },
         {
           key: 4,
+          name: '腾讯广告-3',
+          matches:
+            '[id="com.copymanga.app:id/interact_ad_root"] > [id="com.copymanga.app:id/iv_close"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13521801',
+            'https://i.gkd.li/import/13332719',
+          ],
+        },
+        {
+          key: 5,
+          name: '腾讯广告-4',
+          matches:
+            'ImageView - ImageView - FrameLayout > FrameLayout > FrameLayout > FrameLayout > ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13233178',
+        },
+        {
+          key: 6,
+          name: '字节广告-1',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: [
+            '[desc^="logoad"] > [text="广告"]',
+            '[desc^="webview-close"] > View[clickable=true]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13372542',
+        },
+        {
+          key: 7,
+          name: '字节广告-2',
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
           matches: '[text="反馈"] -2 @View - Image',
           snapshotUrls: 'https://i.gkd.li/import/12925052',
         },
         {
-          key: 5,
+          key: 8,
+          name: '字节广告-3',
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
           matches: '[text="反馈"] -4 @View < View[childCount=7]',
           snapshotUrls: 'https://i.gkd.li/import/12925095',
-        },
-        {
-          key: 6,
-          matches:
-            '[id="com.copymanga.app:id/video_view"] + [id="com.copymanga.app:id/iv_close"]',
-          snapshotUrls: 'https://i.gkd.li/import/13332719',
-        },
-        {
-          key: 7,
-          // desc: '二步跳过广告_步骤1',
-          quickFind: true,
-          matches: '[text="跳过"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/13344156', // step1
-          ],
-        },
-        {
-          key: 8,
-          preKeys: [7],
-          // desc: '二步跳过广告_步骤2',
-          matches: 'ImageView < ViewGroup[clickable=true][childCount=1]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/13344156', // step2
-          ],
         },
       ],
     },
