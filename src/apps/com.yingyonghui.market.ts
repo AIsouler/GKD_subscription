dd60c9efd4d848ff70099e78e0e5fccf50d0f0de
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-03 17:03:40 +0800

    feat(应用汇): 开屏广告（补充规则）、弹窗广告 (#2779)
    * feat 开屏广告（补充规则）
    
    * feat 弹窗广告

--- src/apps/com.yingyonghui.market.ts
+++ src/apps/com.yingyonghui.market.ts
@@ -11,8 +11,34 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[id = "com.yingyonghui.market:id/splashAdvert_skipText"]',
-      snapshotUrls: 'https://i.gkd.li/import/13196213',
+      rules: [
+        {
+          key: 0,
+          matches: '[id = "com.yingyonghui.market:id/splashAdvert_skipText"]',
+          snapshotUrls: 'https://i.gkd.li/import/13196213',
+        },
+        {
+          key: 1,
+          matches: '@ImageView[clickable=true] + * > [text="跳过"]',
+          snapshotUrls: 'https://i.gkd.li/import/13538315',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '快手广告',
+          activityIds: 'com.yingyonghui.market.ui.MainActivity',
+          matches: [
+            'ViewGroup[childCount=2] > ImageView + [text="广告"]',
+            'ViewGroup[childCount=3] > TextView + TextView + ImageView',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13538316',
+        },
+      ],
     },
   ],
 });
