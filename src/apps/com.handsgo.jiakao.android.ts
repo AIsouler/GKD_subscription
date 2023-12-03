bbcc991247e6b1045585d1840fc115742c53acdf
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-03 16:34:08 +0800

    feat(驾考宝典): 弹窗广告（补充规则）、请求开启通知权限弹窗 (#2707)
    * feat 请求开启通知权限弹窗
    
    * feat 弹窗广告（补充规则）

--- src/apps/com.handsgo.jiakao.android.ts
+++ src/apps/com.handsgo.jiakao.android.ts
@@ -41,7 +41,25 @@ export default defineAppConfig({
           ],
           snapshotUrls: 'https://i.gkd.li/import/13476039',
         },
+        {
+          key: 1,
+          name: '课程广告',
+          activityIds: 'com.handsgo.jiakao.android.main.activity.MainActivity',
+          quickFind: true,
+          matches:
+            '[id="com.handsgo.jiakao.android:id/adsdk__ids_cta_layout"] - * > [id="com.handsgo.jiakao.android:id/close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13523033',
+        },
       ],
+    },
+    {
+      key: 3,
+      name: '请求开启通知权限弹窗',
+      activityIds: [],
+      quickFind: true,
+      rules:
+        '[text="开启专属学习资料推送"] + [id="com.handsgo.jiakao.android:id/closeIv"]',
+      snapshotUrls: 'https://i.gkd.li/import/13520296',
     },
   ],
 });
