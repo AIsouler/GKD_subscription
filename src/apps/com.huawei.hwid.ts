afff0c3b3c67981e8180326ba8ea3e00dca05bbf
lyc452 <108231863+lyc452@users.noreply.github.com>
2023-12-03 17:43:46 +0800

    feat(HMS Core): 开屏广告 (#2714)
    * Add files via upload
    
    * chore(actions): check_format_lint
    
    * feat(HMS Core): 开屏广告
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/com.huawei.hwid.ts
+++ src/apps/com.huawei.hwid.ts
@@ -5,6 +5,20 @@ export default defineAppConfig({
   name: 'HMS Core',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      resetMatch: 'app',
+      matchTime: 10000,
+      actionMaximum: 1,
+      quickFind: true,
+      rules: [
+        {
+          matches: '[id="com.huawei.hwid:id/hiad_skip_text"]',
+          snapshotUrls: ['https://i.gkd.li/import/13520462'],
+        },
+      ],
+    },
+    {
       // 我不知道这个广告从哪里来的，它出乎意料地跳出并覆盖在Gspace上面
       key: 0,
       name: '页面中间跳出广告',
