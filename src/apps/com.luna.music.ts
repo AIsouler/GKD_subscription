1cd72de89e0c6726e1bbc56a86c88244c9749393
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-03 16:50:37 +0800

    feat(汽水音乐): 规则优化 (#2752)
    * feat(汽水音乐):规则优化
    
    * Update com.luna.music.ts

--- src/apps/com.luna.music.ts
+++ src/apps/com.luna.music.ts
@@ -7,17 +7,18 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[id$="/tt_splash_skip_btn"]',
-      snapshotUrls: 'https://i.gkd.li/import/12514049',
+      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]', // 删除旧规则[id$="/tt_splash_skip_btn"]，等出现[id="com.byted.pangle.m:id/tt_splash_skip_btn"] 快照再适配
+      snapshotUrls: 'https://i.gkd.li/import/13533782',
     },
     {
       key: 2,
       name: 'VIP弹窗',
       desc: '直接关闭所有该样式弹窗',
-      matchLauncher: true,
+      quickFind: true,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
@@ -25,8 +26,8 @@ export default defineAppConfig({
           action: 'back',
           matches: '[id="com.luna.music:id/bullet_container"]',
           snapshotUrls: [
-            'https://i.gkd.li/import/13365087',
-            'https://i.gkd.li/import/13365085',
+            'https://i.gkd.li/import/13533795',
+            'https://i.gkd.li/import/13533797',
           ],
         },
       ],
