a30613cdbd2b3339c496ddaee7c582a5cd44be14
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-03 16:54:36 +0800

    feat(小米运动健康): 活动弹窗、更新弹窗 (#2767)
    * feat 活动弹窗
    
    * feat 更新弹窗

--- src/apps/com.mi.health.ts
+++ src/apps/com.mi.health.ts
@@ -24,5 +24,28 @@ export default defineAppConfig({
       rules: '[text="开启后台无限制"] + [id="com.mi.health:id/close_icon"]',
       snapshotUrls: 'https://i.gkd.li/import/13495035',
     },
+    {
+      key: 3,
+      name: '活动弹窗',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.xiaomi.fitness.main.MainActivity',
+          quickFind: true,
+          matches:
+            '[id="com.mi.health:id/cardview"] + [id="com.mi.health:id/dialog_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13537836',
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text^="发现新版本"] < * + * >n [text="放弃更新"]',
+      snapshotUrls: 'https://i.gkd.li/import/13537840',
+    },
   ],
 });
