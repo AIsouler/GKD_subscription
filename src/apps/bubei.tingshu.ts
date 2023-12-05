f7748636f587c05430e9c5897160fcfda44113ab
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-05 20:35:24 +0800

    feat(懒人听书): 更新弹窗 (#2829)

--- src/apps/bubei.tingshu.ts
+++ src/apps/bubei.tingshu.ts
@@ -29,5 +29,14 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 2,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text="发现新版本"] +2 * > [text="暂不升级"]',
+      snapshotUrls: 'https://i.gkd.li/import/13545953',
+    },
   ],
 });
