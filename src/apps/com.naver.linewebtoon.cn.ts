55164143c7b8dc5b53e4be1c08c6fff6088eedca
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-05 20:42:33 +0800

    feat(咚漫漫画): 开屏广告（补充规则） (#2838)
    * feat 开屏广告（补充规则）
    
    * feat 弹窗广告
    
    * feat 请求开启个性化推荐弹窗
    
    * Revert "feat 请求开启个性化推荐弹窗"
    
    This reverts commit 2181356e1aa3bf4ae15f8256e9f78dceb2c493ff.
    
    * Revert "feat 弹窗广告"
    
    This reverts commit 99940583a6672d53c2a16215bee860ff22245a7b.

--- src/apps/com.naver.linewebtoon.cn.ts
+++ src/apps/com.naver.linewebtoon.cn.ts
@@ -11,8 +11,18 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
-      snapshotUrls: 'https://i.gkd.li/import/13407119',
+      rules: [
+        {
+          key: 0,
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13407119',
+        },
+        {
+          key: 1,
+          matches: '[text="跳过"]',
+          snapshotUrls: 'https://i.gkd.li/import/13546265',
+        },
+      ],
     },
   ],
 });
