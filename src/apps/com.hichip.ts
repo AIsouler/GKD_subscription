a34538dc7b41957d83d071a5e387b0ca99772b9b
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-05 20:19:58 +0800

    feat(CamHi):开屏广告 (#2795)
    * feat(CamHi):开屏广告
    
    * Update com.hichip.ts
    
    close #2787

---
+++ src/apps/com.hichip.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.hichip',
+  name: 'CamHi',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13538761',
+    },
+  ],
+});
