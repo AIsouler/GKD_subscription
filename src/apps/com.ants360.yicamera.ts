8d413b772dbcda1d7ade6aaba96fde3a3f7e4ea4
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-05 20:33:17 +0800

    feat(小蚁摄像机): 弹窗广告 (#2823)
    close #2821

--- src/apps/com.ants360.yicamera.ts
+++ src/apps/com.ants360.yicamera.ts
@@ -21,9 +21,19 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'activity',
-      rules:
-        'FrameLayout[childCount=3] + FrameLayout[childCount=1][index=1] > ImageView',
-      snapshotUrls: 'https://i.gkd.li/import/13463241',
+      rules: [
+        {
+          name: '腾讯SDK',
+          matches:
+            'FrameLayout[childCount=3] + FrameLayout[childCount=1][index=1] > ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13463241',
+        },
+        {
+          matches:
+            'ImageView < @ViewGroup[clickable=true] < ViewGroup <<n FrameLayout[id="com.ants360.yicamera:id/ksad_tk_view"] ',
+          snapshotUrls: 'https://i.gkd.li/import/13543175',
+        },
+      ],
     },
   ],
 });
