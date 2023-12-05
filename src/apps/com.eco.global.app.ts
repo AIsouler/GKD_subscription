b4de9ba59f42861aa47a5a18c5ef7f2262f8fcfb
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-05 16:04:19 +0800

    feat(ECOVACS HOME):开屏广告 (#2792)
    close #2788

---
+++ src/apps/com.eco.global.app.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.eco.global.app',
+  name: 'ECOVACS HOME',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: 'ImageView[id="com.eco.global.app:id/adview_jump"]',
+      snapshotUrls: 'https://i.gkd.li/import/13538616',
+    },
+  ],
+});
