88f1287990657e2947ebe1fc98cedc721d5dc0a3
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-12-05 20:26:51 +0800

    feat(CC加速器): 加速页-分享抽奖浮窗（默认关闭） (#2803)
    * feat(CC加速器): 加速页-分享抽奖浮窗（默认关闭）
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/cn.ccspeed.ts
+++ src/apps/cn.ccspeed.ts
@@ -14,5 +14,22 @@ export default defineAppConfig({
       rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13334790',
     },
+    {
+      key: 1,
+      enable: false,
+      name: '加速页-分享抽奖浮窗',
+      activityIds: 'cn.ccspeed.app.MainActivity',
+      quickFind: true,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      actionMaximum: 1,
+      rules: [
+        {
+          matches:
+            'FrameLayout[id="cn.ccspeed:id/float_layout"] > ImageView - ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13539299',
+        },
+      ],
+    },
   ],
 });
