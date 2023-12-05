aa12b222b6436d55e799d2e765bb948fd40ac9b2
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-12-05 20:34:57 +0800

    feat(网速管家): 开屏广告 (#2828)
    close #2827

--- src/apps/cn.lezhi.speedtest.ts
+++ src/apps/cn.lezhi.speedtest.ts
@@ -5,6 +5,16 @@ export default defineAppConfig({
   name: '网速管家',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      matchTime: 10000,
+      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+      snapshotUrls: 'https://i.gkd.li/import/13544242',
+    },
+    {
       key: 1,
       name: '更新弹窗',
       activityIds: 'cn.lezhi.speedtest.main5.MainV5Activity',
