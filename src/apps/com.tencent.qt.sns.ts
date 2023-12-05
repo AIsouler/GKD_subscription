4b5e8521cd6cb365156f1e0d616cba60389a26a6
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-12-05 20:29:35 +0800

    feat(掌上穿越火线): 首页活动弹窗及更新弹窗 (#2813)
    close #2650

--- src/apps/com.tencent.qt.sns.ts
+++ src/apps/com.tencent.qt.sns.ts
@@ -10,5 +10,27 @@ export default defineAppConfig({
       activityIds: 'com.tencent.gamehelper.ui.main.WelcomeActivity',
       rules: '[id=`com.tencent.qt.sns:id/tv_timer`][text$=`跳过`]',
     },
+    {
+      key: 1,
+      name: '首页活动弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      matchTime: 10000,
+      rules: '[id="com.tencent.qt.sns:id/closebtn"]',
+      snapshotUrls: 'https://i.gkd.li/import/13497978',
+    },
+    {
+      key: 2,
+      enable: false,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      matchDelay: 10000,
+      rules:
+        '@[id="com.tencent.qt.sns:id/btnNegative"][text*="取消"] +2 [id="com.tencent.qt.sns:id/btnPositive"][text*="现在升级"]', //防止误触其它取消按钮
+      snapshotUrls: 'https://i.gkd.li/import/13497984',
+    },
   ],
 });
