2dc41e927ea867382fbba51d394c868442b53b51
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-12-05 20:41:34 +0800

    feat(123云盘): 应用内广告弹窗 (#2834)
    * feat(123云盘): 应用内广告弹窗
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.mfcloudcalculate.networkdisk.ts
+++ src/apps/com.mfcloudcalculate.networkdisk.ts
@@ -27,5 +27,17 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '应用内广告弹窗',
+      activityIds: 'com.mfcloudcalculate.networkdisk.activity.AdFreeActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'activity',
+      quickFind: true,
+      rules:
+        '[text*="关闭"][id="com.mfcloudcalculate.networkdisk:id/tv_ad_free_colse"]',
+      snapshotUrls: 'https://i.gkd.li/import/13546173',
+    },
   ],
 });
