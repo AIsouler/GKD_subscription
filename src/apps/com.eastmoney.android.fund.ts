5ad9cc0516f84113d7f73ff0205cc8dd23cbdb8b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-05 20:45:57 +0800

    feat(天天基金): 开屏广告（优化规则）、更新弹窗 (#2846)
    * feat 开屏广告（优化规则）
    
    * feat 更新弹窗

--- src/apps/com.eastmoney.android.fund.ts
+++ src/apps/com.eastmoney.android.fund.ts
@@ -7,9 +7,11 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds:
-        'com.eastmoney.android.fund.centralis.activity.FundRootActivity',
-      rules: '[id="com.eastmoney.android.fund:id/ad_ignore"][text^="跳过"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12781533',
     },
     {
@@ -32,6 +34,14 @@ export default defineAppConfig({
         '[desc="表示场内基金的最新价和折价率"] -(2) FrameLayout < FrameLayout + ImageView',
       ],
       snapshotUrls: 'https://i.gkd.li/import/12642387',
+    },
+    {
+      key: 3,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@FrameLayout[clickable=true] - * >2 [desc="立即升级"]',
+      snapshotUrls: 'https://i.gkd.li/import/13546927',
     },
   ],
 });
