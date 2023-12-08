7f3c902ef30297c3cdb9ea2d2c065ca39602537d
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-08 20:23:46 +0800

    feat(果冻宝盒): 广告弹窗 (#2898)
    close #2888

--- src/apps/com.guodongbaohe.app.ts
+++ src/apps/com.guodongbaohe.app.ts
@@ -14,5 +14,18 @@ export default defineAppConfig({
       rules: '[text*="跳过"][id="com.guodongbaohe.app:id/time"]',
       snapshotUrls: 'https://i.gkd.li/import/13543323',
     },
+    // key0-5作为开屏、更新、消息提醒等通用规则
+    {
+      enable: false,
+      key: 6,
+      name: '广告弹窗',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      activityIds: 'com.guodongbaohe.app.activity.AdvertisementTwoActivity',
+      rules: 'LinearLayout[id="com.guodongbaohe.app:id/close_dialog_lv"]',
+      snapshotUrls: 'https://i.gkd.li/import/13577877',
+    },
   ],
 });
