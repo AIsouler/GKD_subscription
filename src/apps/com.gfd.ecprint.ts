a1ad84649db510d40c4f21dc1dd1b97a985f2cc9
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-08 20:25:43 +0800

    feat(小白智慧打印):开屏广告 (#2906)
    close #2900

---
+++ src/apps/com.gfd.ecprint.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.gfd.ecprint',
+  name: '小白智慧打印',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+      snapshotUrls: 'https://i.gkd.li/import/13579102',
+    },
+  ],
+});
