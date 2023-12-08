c9b8d6339aab4a08173c09e7511859f22e40f6f4
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-12-08 21:16:54 +0800

    feat(Cellular-Z): 更新弹窗 (#2936)
    Co-authored-by: adproqwq <2757917031@qq.com>

--- src/apps/make.more.r2d2.cellular_z.ts
+++ src/apps/make.more.r2d2.cellular_z.ts
@@ -19,5 +19,22 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 2,
+      name: '更新弹窗',
+      resetMatch: 'app',
+      actionMaximum: 1,
+      matchTime: 10000,
+      quickFind: true,
+      rules: [
+        {
+          matches: [
+            '[id="make.more.r2d2.cellular_z:id/tv_alert_title"][text="发现新版本"]',
+            '[id="make.more.r2d2.cellular_z:id/btn_alert_left"][text="取消"]',
+          ], //防止误触其它取消按钮
+          snapshotUrls: 'https://i.gkd.li/import/13608563',
+        },
+      ],
+    },
   ],
 });
