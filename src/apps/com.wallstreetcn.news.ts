e6768ad03cdc1e2e405c55dd93ed53cbb78327cd
walklty <47232102+walklty@users.noreply.github.com>
2023-12-08 21:16:31 +0800

    fix(华尔街见闻): 开屏广告 (#2935)
    close #2917

--- src/apps/com.wallstreetcn.news.ts
+++ src/apps/com.wallstreetcn.news.ts
@@ -7,7 +7,10 @@ export default defineAppConfig({
     {
       key: 0,
       name: '启动-开屏广告',
-      activityIds: ['com.wallstreetcn.news.ProxyMainActivity'],
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
       rules: [
         {
           matches: '[id="com.wallstreetcn.news:id/iv_jump"]',
