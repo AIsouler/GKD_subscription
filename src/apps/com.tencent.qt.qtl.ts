edc34556d3710988515f3007081bad200f9015ff
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-12-08 21:18:32 +0800

    feat(掌上英雄联盟): 更新弹窗 (#2946)
    close #2944

--- src/apps/com.tencent.qt.qtl.ts
+++ src/apps/com.tencent.qt.qtl.ts
@@ -18,5 +18,14 @@ export default defineAppConfig({
         'https://i.gkd.li/import/13413724',
       ],
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      quickFind: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      rules: '[id="com.tencent.qt.qtl:id/btn_update_cancel"]',
+      snapshotUrls: 'https://i.gkd.li/import/13611286',
+    },
   ],
 });
