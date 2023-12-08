a35fea78bf473dd0af2ef9207b5e475ed8faf114
jacktpy <48015717+jacktpy@users.noreply.github.com>
2023-12-08 21:53:44 +0800

    feat(Pods King): 开屏广告 (#2932)
    * feat(Pods King): 开屏广告
    
    * Update com.msnothing.airpodsking.ts
    
    * chore(actions): check_format_lint
    
    * feat(Pods King): 开屏广告
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lisonge <i@songe.li>

--- src/apps/com.msnothing.airpodsking.ts
+++ src/apps/com.msnothing.airpodsking.ts
@@ -7,10 +7,21 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.msnothing.ad.splash.HomeSplashActivity',
-      matchLauncher: true,
-      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
-      snapshotUrls: 'https://i.gkd.li/import/12908449',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      matchTime: 10000,
+      rules: [
+        {
+          quickFind: false,
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: ['https://i.gkd.li/import/12908449'],
+        },
+        {
+          matches: '[text="跳过"]',
+          snapshotUrls: ['https://i.gkd.li/import/13607847'],
+        },
+      ],
     },
   ],
 });
