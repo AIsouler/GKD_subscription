3567ff59161263f8afbbd4798acd2dadf826c8c9
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-08 21:13:31 +0800

    feat(熊猫看书):开屏广告 (#2924)
    close #2922

---
+++ src/apps/com.nd.android.pandareader.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.nd.android.pandareader',
+  name: '熊猫看书',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13599744',
+    },
+  ],
+});
