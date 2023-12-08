eb77cd12792442eb10b6729bb58562429f829ff3
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-08 20:26:05 +0800

    feat(实用大师):广告弹窗 (#2907)
    close #2904

---
+++ src/apps/com.practical.master.toolsme.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.practical.master.toolsme',
+  name: '实用大师',
+  groups: [
+    {
+      key: 6,
+      name: '广告弹窗',
+      rules: [
+        {
+          name: '字节穿山甲SDK',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: 'Image[width<60][height<60] < @View <n View [text="反馈"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13581740',
+            'https://i.gkd.li/import/13582110',
+          ],
+        },
+      ],
+    },
+  ],
+});
