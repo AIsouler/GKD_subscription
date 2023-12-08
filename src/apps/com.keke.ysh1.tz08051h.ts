ca86b5a5f75151895492dab7b5d7c619b3a6143c
Tim.P <971920751@qq.com>
2023-12-08 20:00:29 +0800

    feat(可可影视): 开屏提示 (#2865)
    * 新增APP 可可影视
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.keke.ysh1.tz08051h.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.keke.ysh1.tz08051h',
+  name: '可可影视',
+  groups: [
+    {
+      key: 1,
+      name: '开屏提示',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: ['com.salmon.film.main.ui.MainActivity'],
+      rules: [
+        {
+          key: 1,
+          name: '网址更新',
+          quickFind: true,
+          matches:
+            'TextView[text="可可影视"] < LinearLayout >n @TextView[text*="知道了"]',
+          snapshotUrls: 'https://i.gkd.li/import/13548412',
+        },
+      ],
+    },
+  ],
+});
