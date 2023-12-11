d538f9e39d1ef66c61a4790b97691fc0036b056c
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 19:36:08 +0800

    feat(晓晓优选): 开屏广告（优化规则） (#2996)

--- src/apps/moc.nauxuoyoaixoaix.www.ts
+++ src/apps/moc.nauxuoyoaixoaix.www.ts
@@ -11,8 +11,18 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
-      snapshotUrls: 'https://i.gkd.li/import/13476862',
+      rules: [
+        {
+          key: 0,
+          matches: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13476862',
+        },
+        {
+          key: 1,
+          matches: '[text^="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13625367',
+        },
+      ],
     },
   ],
 });
