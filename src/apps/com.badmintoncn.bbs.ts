10da91205282748174e310caa6b41c3ba79e53cb
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 20:47:34 +0800

    feat(中羽在线): 弹窗广告 (#3085)

--- src/apps/com.badmintoncn.bbs.ts
+++ src/apps/com.badmintoncn.bbs.ts
@@ -17,5 +17,19 @@ export default defineAppConfig({
         'https://i.gkd.li/import/13379472',
       ],
     },
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '红包弹窗',
+          activityIds: 'net.duohuo.magappx.main.IndexTabActivity',
+          quickFind: true,
+          matches: '[id="com.badmintoncn.bbs:id/iv_close_ad"]',
+          snapshotUrls: 'https://i.gkd.li/import/13635224',
+        },
+      ],
+    },
   ],
 });
