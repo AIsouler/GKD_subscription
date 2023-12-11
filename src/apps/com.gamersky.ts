2587a6ec6c9a9298d5c12716eda75398091cc8be
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 21:14:05 +0800

    feat(游民星空):卡片式广告（补充规则） (#3097)

--- src/apps/com.gamersky.ts
+++ src/apps/com.gamersky.ts
@@ -26,9 +26,11 @@ export default defineAppConfig({
           name: '首页卡片广告',
           activityIds: 'com.gamersky.main.activity.LibMainActivity',
           quickFind: true,
-          matches:
-            '[id="com.gamersky:id/tap_video_ad_root"] > [id="com.gamersky:id/delete"]',
-          snapshotUrls: 'https://i.gkd.li/import/13451220',
+          matches: '[text="广告"] < * + [id="com.gamersky:id/delete"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13451220',
+            'https://i.gkd.li/import/13635580',
+          ],
         },
         {
           key: 1,
@@ -37,6 +39,13 @@ export default defineAppConfig({
             'com.gamersky.common.activity.LibDetailContentDetailActivity',
           matches: 'View[childCount=2] > [text="广告"] + View[clickable=true]',
           snapshotUrls: 'https://i.gkd.li/import/13451258',
+        },
+        {
+          key: 2,
+          activityIds:
+            'com.gamersky.common.activity.LibDetailContentDetailActivity',
+          matches: 'View[childCount=2] > [text="广告"] + View',
+          snapshotUrls: 'https://i.gkd.li/import/13635579',
         },
       ],
     },
