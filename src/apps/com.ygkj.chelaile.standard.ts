b9b563a7057186ec7c5eaa67bb5c7eeccb735aca
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 19:39:00 +0800

    feat(车来了): 卡片式广告（补充规则） (#2997)
    * feat: 应用内广告卡片
    
    * feat 开屏广告（优化规则）
    
    * feat 卡片式广告（补充规则）

--- src/apps/com.ygkj.chelaile.standard.ts
+++ src/apps/com.ygkj.chelaile.standard.ts
@@ -19,17 +19,29 @@ export default defineAppConfig({
     },
     {
       key: 1,
-      name: '应用内广告卡片',
-      activityIds: [
-        'dev.xesam.chelaile.app.module.func.SplashActivity',
-        'dev.xesam.chelaile.app.module.PanelHostActivity',
-      ],
-      rules:
-        'ImageView[id^="com.ygkj.chelaile.standard:id/cll_all_pic_close"][visibleToUser=true]',
-      snapshotUrls: [
-        'https://i.gkd.li/import/13062991',
-        'https://i.gkd.li/import/13062984',
-        'https://i.gkd.li/import/13464325',
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: [
+            'dev.xesam.chelaile.app.module.func.SplashActivity',
+            'dev.xesam.chelaile.app.module.PanelHostActivity',
+          ],
+          matches:
+            'ImageView[id^="com.ygkj.chelaile.standard:id/cll_all_pic_close"][visibleToUser=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13062991',
+            'https://i.gkd.li/import/13062984',
+            'https://i.gkd.li/import/13464325',
+          ],
+        },
+        {
+          key: 1,
+          activityIds: 'dev.xesam.chelaile.app.module.PanelHostActivity',
+          matches:
+            '[id="com.ygkj.chelaile.standard:id/cll_line_single_ad_close"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13625374',
+        },
       ],
     },
   ],
