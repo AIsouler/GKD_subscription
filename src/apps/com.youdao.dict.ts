3135a065e83a79e8a53c72cd7ee1faa2f30da435
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 20:17:45 +0800

    feat(网易有道词典): 开屏广告（优化规则）、弹窗广告（优化规则）、更新弹窗 (#3049)
    * feat 开屏广告（优化规则）
    
    * feat 弹窗广告（优化规则）
    
    * feat 更新弹窗

--- src/apps/com.youdao.dict.ts
+++ src/apps/com.youdao.dict.ts
@@ -3,12 +3,16 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'com.youdao.dict',
   name: '网易有道词典',
+  deprecatedKeys: [3],
   groups: [
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.youdao.dict.activity.DictSplashActivity',
-      rules: '[id="com.youdao.dict:id/skin_text"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text$="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12668545',
     },
     {
@@ -30,25 +34,31 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: '主页弹窗',
-      activityIds: [
-        'com.youdao.dict.activity.MainActivity',
-        'com.youdao.dict.activity.DictSplashActivity',
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '首页-弹窗广告',
+          activityIds: [
+            'com.youdao.dict.activity.MainActivity',
+            'com.youdao.dict.activity.DictSplashActivity',
+          ],
+          quickFind: true,
+          matches: '@ImageView[id="com.youdao.dict:id/iv_close_bt"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12893419',
+            'https://i.gkd.li/import/13263801',
+          ],
+        },
+        {
+          key: 1,
+          name: '学习页面-弹窗广告',
+          activityIds: 'com.youdao.dict.edu.main.MainPopDialog',
+          quickFind: true,
+          matches: '@ImageView[id="com.youdao.dict:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/12893450',
+        },
       ],
-      rules: '@ImageView[id="com.youdao.dict:id/iv_close_bt"]',
-      quickFind: true,
-      snapshotUrls: [
-        'https://i.gkd.li/import/12893419',
-        'https://i.gkd.li/import/13263801',
-      ],
-    },
-    {
-      key: 3,
-      name: '学习-弹窗',
-      activityIds: 'com.youdao.dict.edu.main.MainPopDialog',
-      rules: '@ImageView[id="com.youdao.dict:id/iv_close"]',
-      quickFind: true,
-      snapshotUrls: 'https://i.gkd.li/import/12893450',
     },
     {
       key: 4,
@@ -68,6 +78,16 @@ export default defineAppConfig({
       matchDelay: 10000,
       rules: '[id="com.youdao.dict:id/btn_never"][text*="不再提醒"]',
       snapshotUrls: 'https://i.gkd.li/import/13540941',
+    },
+    {
+      key: 6,
+      name: '更新弹窗',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules:
+        '[id="com.youdao.dict:id/tv_version"] + [id="com.youdao.dict:id/iv_close"]',
+      snapshotUrls: 'https://i.gkd.li/import/13627912',
     },
   ],
 });
