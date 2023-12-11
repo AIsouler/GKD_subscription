85353bc8775e0061de53379cff94a5662562e280
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 20:49:44 +0800

    feat(扫描全能王): 开屏广告（优化规则） (#3089)

--- src/apps/com.intsig.camscanner.ts
+++ src/apps/com.intsig.camscanner.ts
@@ -7,9 +7,16 @@ export default defineAppConfig({
     {
       key: -1,
       name: '开屏广告',
-      activityIds: 'com.intsig.camscanner.mainmenu.mainactivity.MainActivity',
-      rules: '[id="com.intsig.camscanner:id/tv_skip_ad"]',
-      snapshotUrls: 'https://i.gkd.li/import/12668795',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12668795',
+        'https://i.gkd.li/import/13635350',
+        'https://i.gkd.li/import/13635351',
+      ],
     },
     {
       key: 0,
