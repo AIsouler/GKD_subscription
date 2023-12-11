f90652c4ef05ff56e50304e9e74e2b28f597be5c
aoguai <34203474+aoguai@users.noreply.github.com>
2023-12-11 19:25:57 +0800

    feat(蓝岛): 通知弹窗 (#2970)

---
+++ src/apps/com.loyea.adnmb.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.loyea.adnmb',
+  name: '蓝岛',
+  groups: [
+    {
+      key: 5,
+      name: '通知弹窗',
+      enable: false,
+      quickFind: true,
+      activityIds: 'com.loyea.adnmb.activity.MainActivity',
+      rules: 'LinearLayout > Button[text="确定"]',
+      snapshotUrls: 'https://i.gkd.li/import/13623450',
+    },
+  ],
+});
