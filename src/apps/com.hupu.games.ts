03c14ac285b1adc6f23710a87dc875639bbb556f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 19:29:27 +0800

    feat(虎扑): 开屏广告（优化规则） (#2988)

--- src/apps/com.hupu.games.ts
+++ src/apps/com.hupu.games.ts
@@ -7,7 +7,9 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      desc: '开屏广告,任意界面切回APP广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules:
         '[id="com.byted.pangle:id/tt_splash_skip_btn"||id="com.hupu.games:id/tv_time"&&text*="跳过"]',
       snapshotUrls: [
