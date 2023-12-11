4a01e265513102f68e2faadab47c6016f6e829ad
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-12-11 20:41:23 +0800

    feat(TextNow): 设为默认应用提示 (#3067)
    close #3064

---
+++ src/apps/com.enflick.android.TextNow.ts
@@ -0,0 +1,29 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.enflick.android.TextNow',
+  name: 'TextNow',
+  groups: [
+    {
+      key: 1,
+      name: '设为默认应用提示',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[id="com.enflick.android.TextNow:id/btn_dismiss"][text="Not Now"]',
+          snapshotUrls: 'https://i.gkd.li/import/13630460',
+        },
+        {
+          preKeys: 0,
+          matches: '[id="android:id/button2"][text="SKIP"]',
+          snapshotUrls: 'https://i.gkd.li/import/13630463',
+        },
+      ],
+    },
+  ],
+});
