c5afd349c66be7463413f97489741b9cb77ffc44
lyc452 <108231863+lyc452@users.noreply.github.com>
2023-12-11 19:25:15 +0800

    fix(不背单词): 自动签到 (#2967)

--- src/apps/cn.com.langeasy.LangEasyLexis.ts
+++ src/apps/cn.com.langeasy.LangEasyLexis.ts
@@ -1,7 +1,7 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-  id: 'cn.com.langeasy.langeasylexis',
+  id: 'cn.com.langeasy.LangEasyLexis',
   name: '不背单词',
   groups: [
     {
@@ -10,8 +10,8 @@ export default defineAppConfig({
       name: '自动签到',
       rules: [
         {
-          activityIds: 'cn.com.langeasy.langeasylexis.activity.MainActivity',
-          matches: '[id="cn.com.langeasy.langeasylexis:id/bb_checkin"]',
+          activityIds: 'cn.com.langeasy.LangEasyLexis.activity.MainActivity',
+          matches: '[id="cn.com.langeasy.LangEasyLexis:id/bb_checkin"]',
           snapshotUrls: 'https://i.gkd.li/import/13610321',
         },
       ],
