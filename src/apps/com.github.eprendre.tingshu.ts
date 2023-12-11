fe1ea39dc3ccc2ccfd82a32564e456bdd04b3f91
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 19:30:31 +0800

    feat(我的听书): 弹窗广告 (#2989)

--- src/apps/com.github.eprendre.tingshu.ts
+++ src/apps/com.github.eprendre.tingshu.ts
@@ -36,5 +36,21 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 2,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '快手广告',
+          activityIds: 'com.github.eprendre.tingshu.ui.SplashActivity',
+          matches: [
+            'ViewGroup[childCount=2] > [text="广告"]',
+            'ViewGroup[childCount=3] > TextView + TextView + ImageView[clickable=true]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13625303',
+        },
+      ],
+    },
   ],
 });
