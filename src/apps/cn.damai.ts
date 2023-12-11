b419fa9820a189768360ccc599b44e318a9c3717
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 20:17:26 +0800

    feat(大麦): 开屏广告（优化规则）、弹窗广告 (#3048)
    * feat 开屏广告（优化规则）
    
    * feat 弹窗广告

--- src/apps/cn.damai.ts
+++ src/apps/cn.damai.ts
@@ -7,9 +7,24 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'cn.damai.launcher.splash.SplashMainActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: '[id="cn.damai:id/homepage_advert_pb"]',
       snapshotUrls: 'https://i.gkd.li/import/12472623',
+    },
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'cn.damai.homepage.MainActivity',
+          matches:
+            '[id="cn.damai:id/homepage_popup_window_image"] + [id="cn.damai:id/homepage_popup_window_close_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13627900',
+        },
+      ],
     },
   ],
 });
