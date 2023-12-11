04586d6538beae9026361761fba6c3ffd8da34d1
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-12-11 20:45:09 +0800

    feat(宝宝生活记录): 应用内广告弹窗 (#3079)
    close #3078

---
+++ src/apps/com.bluemobile.flutterbabylife.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.bluemobile.flutterbabylife',
+  name: '宝宝生活记录',
+  groups: [
+    {
+      key: 1,
+      name: '应用内广告弹窗',
+      matchTime: 10000,
+      actionMaximum: 1,
+      rules: [
+        {
+          key: 0,
+          name: '字节SDK',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: 'View[childCount=7] > View[index=1]',
+          snapshotUrls: 'https://i.gkd.li/import/13632639',
+        },
+      ],
+    },
+  ],
+});
