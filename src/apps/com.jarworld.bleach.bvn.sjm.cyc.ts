75a660b9a87c6fa5d7bb3b9264b84564bf23a5e9
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 20:50:52 +0800

    feat(次元城动漫): 广告弹窗（补充规则） (#3091)

--- src/apps/com.jarworld.bleach.bvn.sjm.cyc.ts
+++ src/apps/com.jarworld.bleach.bvn.sjm.cyc.ts
@@ -27,17 +27,29 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      activityIds: 'com.jarworld.bleach.bvn.sjm.cyc.MainActivity',
       rules: [
         {
+          key: 0,
+          activityIds: 'com.jarworld.bleach.bvn.sjm.cyc.MainActivity',
           quickFind: true,
           matches: '[text*="跳过"][text.length<=10]',
           snapshotUrls: 'https://i.gkd.li/import/13626949',
         },
         {
+          key: 1,
+          activityIds: 'com.jarworld.bleach.bvn.sjm.cyc.MainActivity',
           matches:
             '[id="com.jarworld.bleach.bvn.sjm.cyc:id/ksad_container"] ViewGroup[childCount=1] > @ViewGroup[clickable=true][childCount=1] > ImageView',
           snapshotUrls: 'https://i.gkd.li/import/13626950',
+        },
+        {
+          key: 2,
+          activityIds: 'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          matches: [
+            'WebView > View > View > [text="广告"]',
+            'ViewGroup[childCount=2] > @ViewGroup[childCount=1] > ImageView[childCount=0]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13635410',
         },
       ],
     },
