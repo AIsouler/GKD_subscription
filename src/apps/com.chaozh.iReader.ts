bad730b6bb0eb17b89a8fe77d459a4f46423c0fe
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-12-11 20:13:44 +0800

    feat(电子书): 权限弹窗 (#3042)

---
+++ src/apps/com.chaozh.iReader.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.chaozh.iReader',
+  name: 'VIVO 电子书',
+  groups: [
+    {
+      key: 1,
+      name: '权限弹窗',
+      quickFind: true,
+      matchTime: 10000,
+      rules: [
+        {
+          matches: 'Button[text="前往开启"] - Button[text="取消"]',
+          snapshotUrls: 'https://i.gkd.li/import/13627649',
+        },
+      ],
+    },
+  ],
+});
