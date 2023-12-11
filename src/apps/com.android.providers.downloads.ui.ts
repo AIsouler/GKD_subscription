7d0c60b70908c3c06cb7567e723c4e41cda3b446
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-12-11 20:42:12 +0800

    feat(下载管理): 更新弹窗 (#3072)
    close #3066

---
+++ src/apps/com.android.providers.downloads.ui.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.android.providers.downloads.ui',
+  name: '下载管理',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '@[text="以后再说"] + [text="立即更新"]',
+      snapshotUrls: 'https://i.gkd.li/import/13631769',
+    },
+  ],
+});
