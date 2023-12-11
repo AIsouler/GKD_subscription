d788687f02404c0e7a20932cfb8531162f8282ac
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 20:14:49 +0800

    feat(两步路户外助手): 开屏广告（优化规则）、更新弹窗（优化规则）、弹窗广告 (#3046)
    * feat 开屏广告（优化规则）
    
    * feat 更新弹窗（优化规则）
    
    * feat 弹窗广告

--- src/apps/com.lolaage.tbulu.tools.ts
+++ src/apps/com.lolaage.tbulu.tools.ts
@@ -7,19 +7,38 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.lolaage.tbulu.tools.ui.activity.WelcomeActivity',
-      matchLauncher: true,
       quickFind: true,
-      rules: '[id="com.lolaage.tbulu.tools:id/skipPart"]',
-      snapshotUrls: 'https://i.gkd.li/import/12882538',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12882538',
+        'https://i.gkd.li/import/13627860',
+      ],
     },
     {
       key: 1,
       name: '更新弹窗',
-      activityIds: 'com.lolaage.tbulu.tools.upgrade',
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules:
         '[id="com.lolaage.tbulu.tools:id/tvFullUpgrade"] + [id="com.lolaage.tbulu.tools:id/tvCancel"]',
       snapshotUrls: 'https://i.gkd.li/import/12882550',
+    },
+    {
+      key: 2,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '腾讯广告',
+          activityIds: 'com.lolaage.tbulu.tools.ui.activity.WelcomeActivity',
+          matches:
+            'ImageView -2 FrameLayout > FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: 'https://i.gkd.li/import/13627861',
+        },
+      ],
     },
   ],
 });
