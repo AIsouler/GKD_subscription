450d4d52a2a17863a1679c5d7965d5da4d2165bc
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-11 20:14:00 +0800

    feat(中国移动云盘): 开屏广告（优化规则）、更新弹窗（优化规则）、弹窗广告、悬浮广告、请求开启自动备份弹窗 (#3043)
    * feat 开屏广告（优化规则）
    
    * feat 更新弹窗（优化规则）
    
    * feat 弹窗广告
    
    * feat 悬浮广告
    
    * feat 请求开启自动备份弹窗

--- src/apps/com.chinamobile.mcloud.ts
+++ src/apps/com.chinamobile.mcloud.ts
@@ -7,17 +7,64 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.chinamobile.mcloud.client.ui.logo.LogoActivity',
-      rules: '[id="com.chinamobile.mcloud:id/btn_skip"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12774830',
     },
     {
       key: 1,
       name: '更新弹窗',
-      activityIds: 'com.chinamobile.mcloud.client.ui.setting.UpgradeActivity',
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules:
         '@[id="com.chinamobile.mcloud:id/bn_cancel"] + [id="com.chinamobile.mcloud:id/upgrade_title"]',
       snapshotUrls: 'https://i.gkd.li/import/12774833',
+    },
+    {
+      key: 2,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.chinamobile.mcloud.client.ui.MenuActivity',
+          quickFind: true,
+          matches:
+            '[id="com.chinamobile.mcloud:id/rl_container"] + [id="com.chinamobile.mcloud:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13627826',
+        },
+        {
+          key: 1,
+          activityIds: 'com.chinamobile.mcloud.client.ui.MenuActivity',
+          matches: '[text="马上领取"] +n View > Image[clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13627832',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '悬浮广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.chinamobile.mcloud.client.ui.MenuActivity',
+          quickFind: true,
+          matches:
+            '[id="com.chinamobile.mcloud:id/iv_logo"] + [id="com.chinamobile.mcloud:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13627834',
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '请求开启自动备份弹窗',
+      activityIds: 'com.chinamobile.mcloud.client.ui.MenuActivity',
+      quickFind: true,
+      rules:
+        '[text="开启自动备份"] +n [id="com.chinamobile.mcloud:id/tv_skip"]',
+      snapshotUrls: 'https://i.gkd.li/import/13627830',
     },
   ],
 });
