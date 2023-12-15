2f8bf44b25f84aa5ba6b6129277fe5360a513ae2
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-15 19:25:16 +0800

    feat(乔安智联):开屏广告 (#3127)
    close #3101

--- src/apps/com.jooan.qiaoanzhilian.ts
+++ src/apps/com.jooan.qiaoanzhilian.ts
@@ -7,14 +7,24 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: 'TextView[text^="跳过"][text.length<=10]',
-      snapshotUrls: [
-        'https://i.gkd.li/import/13197473',
-        'https://i.gkd.li/import/13223790',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13197473',
+            'https://i.gkd.li/import/13223790',
+          ],
+        },
+        {
+          actionMaximumKey: 0,
+          matches: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13636103',
+        },
       ],
     },
   ],
