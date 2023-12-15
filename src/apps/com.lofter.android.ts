25fa988fce45f0f7478599a7c11b42571b93cd6e
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-15 19:23:01 +0800

    feat(LOFTER):开屏广告 (#3122)
    close #3094

--- src/apps/com.lofter.android.ts
+++ src/apps/com.lofter.android.ts
@@ -7,12 +7,24 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.lofter.android.global.splash.HomeActivity',
-      rules: '[text^="跳过"][text.length<=10]',
-      quickFind: true,
-      snapshotUrls: [
-        'https://i.gkd.li/import/13189846',
-        'https://i.gkd.li/import/13189906',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          matches: '[text*="跳过"][text.length<=10]',
+          quickFind: true,
+          snapshotUrls: [
+            'https://i.gkd.li/import/13189846',
+            'https://i.gkd.li/import/13189906',
+          ],
+        },
+        {
+          actionMaximumKey: 0,
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13635533',
+        },
       ],
     },
   ],
