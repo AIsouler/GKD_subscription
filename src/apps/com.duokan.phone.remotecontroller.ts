c3407304794380dfd7c32547f83eab8b147ac243
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-15 19:27:29 +0800

    feat(万能遥控): 申请定位 (#3131)
    close #3106

--- src/apps/com.duokan.phone.remotecontroller.ts
+++ src/apps/com.duokan.phone.remotecontroller.ts
@@ -12,5 +12,18 @@ export default defineAppConfig({
       rules:
         'ImageView[id=`com.duokan.phone.remotecontroller:id/image_close_banner`]',
     },
+    {
+      key: 4,
+      name: '申请定位',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      activityIds:
+        'com.xiaomi.mitv.phone.remotecontroller.HoriWidgetMainActivityV2',
+      rules:
+        '[id="com.duokan.phone.remotecontroller:id/ll_permission"] [text="取消"]',
+      snapshotUrls: 'https://i.gkd.li/import/13642080',
+    },
   ],
 });
