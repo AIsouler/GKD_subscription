d3053fa14832d048e34a45da012aa9628f9aa8dc
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-15 20:37:50 +0800

    feat(天翼生活): 开屏广告 (#3185)

--- src/apps/com.ataaw.tianyi.ts
+++ src/apps/com.ataaw.tianyi.ts
@@ -5,6 +5,16 @@ export default defineAppConfig({
   name: '天翼生活',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13674443',
+    },
+    {
       key: 1,
       name: '版本升级',
       quickFind: true,
