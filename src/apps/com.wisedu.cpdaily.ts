7030ade9b2167aecf47221e5102669ac8d49ed96
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-15 19:26:43 +0800

    feat(今日校园):版本更新 (#3129)
    close #3103

---
+++ src/apps/com.wisedu.cpdaily.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wisedu.cpdaily',
+  name: '今日校园',
+  groups: [
+    {
+      key: 2,
+      name: '版本更新',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      activityIds: 'com.wisorg.wisedu.home.ui.HomeActivity',
+      rules:
+        '[id="com.wisedu.cpdaily:id/btn_update"] + [id="com.wisedu.cpdaily:id/tv_ignore"]',
+      snapshotUrls: 'https://i.gkd.li/import/13643565',
+    },
+  ],
+});
