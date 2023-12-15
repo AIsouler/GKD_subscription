42d6d339cccf459765cfbd1bda0054d09641ac88
xcz1997 <cappuxcz@outlook.com>
2023-12-15 19:19:31 +0800

    feat(软件包安装程序): 自动跳过协议 (#3040)

--- src/apps/com.samsung.android.packageinstaller.ts
+++ src/apps/com.samsung.android.packageinstaller.ts
@@ -5,18 +5,25 @@ export default defineAppConfig({
   name: '软件包安装程序',
   groups: [
     {
-      enable: false,
-      key: 1,
-      name: '请求开启反恶意软件提示弹窗',
-      activityIds: [
-        'pl.solidexplorer.SolidExplorer',
-        'com.android.packageinstaller.activity.InstallerActivity',
-      ],
+      name: '跳过安全使用协议',
+      key: 2,
+      desc: '自动点击略过并关闭反恶意软件协议',
+      enable: true,
       rules: [
+        {
+          key: 1,
+          matches: ['[text *="略过"]'],
+          snapshotUrls: ['https://i.gkd.li/import/13255641'],
+        },
         {
           key: 0,
           matches: ['[text^="是否打开反恶意软件"]', '[text="取消"]'],
-          snapshotUrls: 'https://i.gkd.li/import/13233686',
+          snapshotUrls: ['https://i.gkd.li/import/13233686'],
+        },
+        {
+          key: 2,
+          matches: ['[text^="打开应用程序保护?"]', '[text="取消"]'],
+          snapshotUrls: ['https://i.gkd.li/import/13650528'],
         },
       ],
     },
