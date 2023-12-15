45393f61c2eddc3081e49b891e15b4b3b5a2446a
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-15 19:35:14 +0800

    feat(360摄像机): 卡录像界面广告 (#3158)
    close #3145

--- src/apps/com.qihoo.camera.ts
+++ src/apps/com.qihoo.camera.ts
@@ -7,11 +7,31 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.qihoo.jia.ui.activity.SplashActivity',
-      matchLauncher: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       quickFind: true,
-      rules: '[text="跳过"]',
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12846783',
+    },
+    {
+      key: 6,
+      name: '卡录像界面广告',
+      actionMaximum: 1,
+      activityIds: 'com.qihoo.jia.playpage.RecordPlayActivity',
+      rules: [
+        {
+          name: '视频下方卡片式广告',
+          matches:
+            'ViewGroup[childCount=2] > ImageView[clickable=true] + ImageView[clickable=true]',
+        },
+        {
+          name: '云收藏弹窗广告',
+          matches:
+            'ViewGroup > @ImageView[clickable=true] -n TextView[text="云收藏"]',
+        },
+      ],
+      snapshotUrls: 'https://i.gkd.li/import/13630755',
     },
   ],
 });
