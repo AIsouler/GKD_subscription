946676b7da1eb9353ed4e585aa5fa92f617a7e7d
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-15 19:31:38 +0800

    feat(微店):首页红包弹窗 (#3138)
    close #3118

--- src/apps/com.koudai.weidian.buyer.ts
+++ src/apps/com.koudai.weidian.buyer.ts
@@ -7,9 +7,23 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.koudai.weidian.buyer.activity.SplashActivity',
-      rules: '[id="com.koudai.weidian.buyer:id/skip_view"||text="跳过"]',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '@[clickable=true] > [text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12506297',
+    },
+    {
+      key: 6,
+      name: '首页红包弹窗',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.vdian.android.wdb.main.ui.activity.MainTabsActivity',
+      rules:
+        'FrameLayout > FrameLayout > RelativeLayout > ImageView[clickable=true][index=1]',
+      snapshotUrls: 'https://i.gkd.li/import/13646151',
     },
   ],
 });
