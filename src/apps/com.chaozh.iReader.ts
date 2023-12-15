3fadb3aa9934de134033ccee0f3542366bf8bdbd
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-15 19:34:35 +0800

    feat(VIVO 电子书):开屏广告 (#3155)
    close #3147

--- src/apps/com.chaozh.iReader.ts
+++ src/apps/com.chaozh.iReader.ts
@@ -5,6 +5,16 @@ export default defineAppConfig({
   name: 'VIVO 电子书',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13659604',
+    },
+    {
       key: 1,
       name: '权限弹窗',
       quickFind: true,
