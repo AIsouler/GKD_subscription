25ead4d8124385b144adb65a352d1c5b4e01f758
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-24 20:34:33 +0800

    feat(youtube):视频播放-跳过广告 (#3319)
    close #3306

--- src/apps/com.google.android.youtube.ts
+++ src/apps/com.google.android.youtube.ts
@@ -7,14 +7,18 @@ export default defineAppConfig({
     {
       key: 0,
       name: '视频播放-跳过广告',
-      activityIds:
+      activityIds: [
         'com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity',
-      matchLauncher: true,
+        'com.google.android.youtube',
+      ],
       quickFind: true,
-      rules: '[id=`com.google.android.youtube:id/skip_ad_button`]',
+      rules: [
+        '[id="com.google.android.youtube:id/skip_ad_button"]',
+        '[id="com.google.android.youtube:id/modern_skip_ad_button"]',
+      ],
       snapshotUrls: [
         'https://i.gkd.li/import/12565261',
-        'https://i.gkd.li/import/13024980', // matchLauncher
+        'https://i.gkd.li/import/13705106', // rules:'[id="com.google.android.youtube:id/modern_skip_ad_button"]'
       ],
     },
     {
@@ -22,7 +26,6 @@ export default defineAppConfig({
       name: '视频播放-赞助商广告',
       activityIds:
         'com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity',
-      matchLauncher: true,
       rules: 'ImageView[desc="更多选项"] + @ImageView[desc="关闭广告面板"]',
       snapshotUrls: 'https://i.gkd.li/import/12877346',
     },
@@ -31,7 +34,6 @@ export default defineAppConfig({
       name: '首页-会员广告',
       activityIds:
         'com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity',
-      matchLauncher: true,
       rules:
         'ImageView < @Button < ViewGroup +3 Button[desc*="YouTube Premium"]',
       snapshotUrls: 'https://i.gkd.li/import/12877357',
