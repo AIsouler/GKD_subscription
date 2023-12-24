a547eea4f55b1f3e418877ba6c2ec480cf17f7ec
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 21:10:24 +0800

    feat(人人视频): 青少年模式、开屏广告（补充规则） (#3404)
    * feat 青少年模式
    
    * feat 开屏广告（补充规则）

--- src/apps/com.example.pptv.ts
+++ src/apps/com.example.pptv.ts
@@ -11,8 +11,27 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[text*="跳过"][text.length<=10]',
-      snapshotUrls: 'https://i.gkd.li/import/13669401',
+      rules: [
+        {
+          key: 0,
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13669401',
+        },
+        {
+          key: 1,
+          matches: '[id="com.example.pptv:id/view_count_shake"]',
+          snapshotUrls: 'https://i.gkd.li/import/13761160',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '青少年模式',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '@[text="我知道了"] - * > [text="未成年模式"]',
+      snapshotUrls: 'https://i.gkd.li/import/13761159',
     },
   ],
 });
