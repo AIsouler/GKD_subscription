94dd6d2f75a5c1c9cee1e2548ee8ae7699a6842c
aoguai <34203474+aoguai@users.noreply.github.com>
2023-12-24 20:15:56 +0800

    feat(海南航空): 开屏广告 (#3266)

--- src/apps/com.rytong.hnair.ts
+++ src/apps/com.rytong.hnair.ts
@@ -6,6 +6,20 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[id="com.rytong.hnair:id/btn_skip"]',
+          snapshotUrls: 'https://i.gkd.li/import/13695495',
+        },
+      ],
+    },
+    {
+      key: 1,
       name: '首页-弹窗广告',
       activityIds: 'com.hnair.airlines.ui.main.MainActivity',
       rules: 'ImageView[id*="com.rytong.hnair:id/iv_cloes"]',
