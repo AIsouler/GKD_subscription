0a0e45e3227f502ac66f739ee87779359de85c45
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 21:00:55 +0800

    feat(CliCli动漫): 弹窗广告 (#3387)

---
+++ src/apps/com.viva.time_todo.ts
@@ -0,0 +1,30 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.viva.time_todo',
+  name: 'CliCli动漫',
+  groups: [
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '腾讯广告',
+          activityIds: 'com.maoyun.guoguo.MainActivity',
+          matches:
+            'ImageView - LinearLayout - FrameLayout > FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: 'https://i.gkd.li/import/13759356',
+        },
+        {
+          key: 1,
+          name: '百度广告',
+          activityIds: 'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
+          matches:
+            '@ImageView[clickable=true] - RelativeLayout > RelativeLayout > ImageView + ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13761232',
+        },
+      ],
+    },
+  ],
+});
