ef93997bb084f128a63fe993a57049c7b252d3b8
xhemj <xhemj2680@163.com>
2023-12-24 21:15:11 +0800

    feat(滴滴出行): 打车页面广告 (#3414)
    Signed-off-by: xhemj <xhemj2680@163.com>

--- src/apps/com.sdu.didi.psnger.ts
+++ src/apps/com.sdu.didi.psnger.ts
@@ -20,5 +20,18 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '打车页面广告',
+      desc: '点击广告卡片左上角x',
+      rules: [
+        {
+          activityIds: 'com.didi.sdk.app.MainActivity',
+          matches:
+            'ImageView[id=`com.sdu.didi.psnger:id/second_entrance_img`] + @ImageView[id=`com.sdu.didi.psnger:id/left_close_img`]',
+          snapshotUrls: 'https://i.gkd.li/import/13760846',
+        },
+      ],
+    },
   ],
 });
