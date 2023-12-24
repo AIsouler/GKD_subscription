c1f1fc83f1bab768cfeac28f0911cddc9d380c8c
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 20:06:07 +0800

    feat(铁塔换电): 弹窗广告 (#3251)

---
+++ src/apps/com.chinatower.tthd.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.chinatower.tthd',
+  name: '铁塔换电',
+  groups: [
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.chinatower.fghd.customer.home.HomeNewActivity',
+          quickFind: true,
+          matches:
+            '[id="com.chinatower.tthd:id/fl_content_container"] + [id="com.chinatower.tthd:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13694901',
+        },
+      ],
+    },
+  ],
+});
