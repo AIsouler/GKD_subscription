5988844fcfa92beaf9c6fa4fc2592bcf5bd09a15
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 19:56:09 +0800

    feat(春木漫画): 弹窗广告 (#3221)

---
+++ src/apps/com.galaxy.qbyd.ts
@@ -0,0 +1,28 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.galaxy.qbyd',
+  name: '春木漫画',
+  groups: [
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '腾讯广告',
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Activity_T',
+            'com.aster.comic.app.view.MainActivity',
+          ],
+          matches:
+            'ImageView - FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13691104',
+            'https://i.gkd.li/import/13691103',
+          ],
+        },
+      ],
+    },
+  ],
+});
