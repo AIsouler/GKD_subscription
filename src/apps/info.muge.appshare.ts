372bebc1b294e1c766d9c0389b5ae8d38ab62d07
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 21:40:23 +0800

    feat(AppShare): 开屏广告（优化规则）、卡片式广告 (#3411)
    * feat 开屏广告（优化规则）
    
    * feat 卡片式广告

--- src/apps/info.muge.appshare.ts
+++ src/apps/info.muge.appshare.ts
@@ -12,14 +12,17 @@ export default defineAppConfig({
       resetMatch: 'app',
       rules: [
         {
+          key: 0,
           matches: '[id$="tt_splash_skip_btn"]',
           snapshotUrls: [
             'https://i.gkd.li/import/12683145',
-            'https://i.gkd.li/import/12683173', // activityId: 'com.miui.home.launcher.Launcher'
+            'https://i.gkd.li/import/12683173',
             'https://i.gkd.li/import/13702708',
           ],
         },
         {
+          key: 1,
+          quickFind: true,
           matches: '[text*="跳过"][text.length<=10]',
           snapshotUrls: [
             'https://i.gkd.li/import/12683168',
@@ -30,14 +33,21 @@ export default defineAppConfig({
       ],
     },
     {
-      key: 6,
-      name: '下载页-卡片广告',
-      matchTime: 10000,
-      actionMaximum: 1,
-      activityIds: 'info.muge.appshare.view.app.detail.AppDetailActivity',
-      rules:
-        'FrameLayout[childCount>1] > FrameLayout[childCount=1] > ImageView[width<90][height<90]',
-      snapshotUrls: 'https://i.gkd.li/import/13712716',
+      key: 1,
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 0,
+          name: '腾讯广告',
+          activityIds: 'info.muge.appshare.view.app.detail.AppDetailActivity',
+          matches:
+            '[id="info.muge.appshare:id/adContainer"] >n FrameLayout > ImageView - FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13761259',
+            'https://i.gkd.li/import/13712716',
+          ],
+        },
+      ],
     },
   ],
 });
