bd930613868593e6166e05c14a224e9776c004e8
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 20:12:34 +0800

    feat(Blued): 开屏广告（优化规则）、卡片式广告（优化规则） (#3253)
    * feat 开屏广告（优化规则）
    
    * feat 卡片式广告（优化规则）

--- src/apps/com.soft.blued.ts
+++ src/apps/com.soft.blued.ts
@@ -7,17 +7,28 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.blued.android.core.ui.TerminalActivity',
-      rules: '[id="com.soft.blued:id/fl_ad_content"] >n [text="跳过"]',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12777070',
     },
     {
       key: 1,
-      name: '交友页面-广告卡片',
-      activityIds: 'com.blued.android.core.ui.TerminalActivity',
-      rules:
-        '[id="com.soft.blued:id/ad_container"] >n [id="com.soft.blued:id/img_close"]',
-      snapshotUrls: 'https://i.gkd.li/import/12777097',
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.blued.android.core.ui.TerminalActivity',
+          matches:
+            '[id="com.soft.blued:id/layout_native_ad"] +(1,2) [id="com.soft.blued:id/img_close"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12777097',
+            'https://i.gkd.li/import/13694950',
+          ],
+        },
+      ],
     },
   ],
 });
