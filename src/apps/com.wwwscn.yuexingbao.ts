d5a857d56025ea7cfcd7c10f88f6464c13ef368a
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-24 20:42:12 +0800

    feat(悦通行): 开屏广告 (#3341)
    close #3338

--- src/apps/com.wwwscn.yuexingbao.ts
+++ src/apps/com.wwwscn.yuexingbao.ts
@@ -7,11 +7,18 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      desc: '字节广告SDK',
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
-      snapshotUrls: 'https://i.gkd.li/import/13239194',
+      rules: [
+        '[id$="tt_splash_skip_btn"]',
+        '@View[clickable=true][id=null][visibleToUser=true] +n TextView[clickable=true]',
+      ],
+      snapshotUrls: [
+        'https://i.gkd.li/import/13239194',
+        'https://i.gkd.li/import/13723967',
+      ],
     },
   ],
 });
