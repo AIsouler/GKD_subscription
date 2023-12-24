0dff4f45abcc2d8e02a6cd313f291c88f846b877
aoguai <34203474+aoguai@users.noreply.github.com>
2023-12-24 20:53:07 +0800

    feat(手机电视高清直播): 弹窗广告、浮窗广告、播放页面-弹窗广告(gkd-kit#3357) (#3364)
    close #3357

---
+++ src/apps/com.zbx.ct.tvzhibo.ts
@@ -0,0 +1,51 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.zbx.ct.tvzhibo',
+  name: '手机电视高清直播',
+  groups: [
+    {
+      key: 3,
+      name: '弹窗广告',
+      rules: [
+        {
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: '@Image[text.length=0] < View -n [text="反馈"]',
+          snapshotUrls: 'https://i.gkd.li/import/13739951',
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '浮窗广告',
+      desc: '会出现在更多页面、播放页面',
+      quickFind: true,
+      rules: [
+        {
+          activityIds: [
+            'com.tvmain.mvp.view.activity.TvMainActivity',
+            'com.tvmain.mvp.view.activity.LivePlayerActivity',
+          ],
+          matches: '[id="com.zbx.ct.tvzhibo:id/iv_close"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13740079',
+            'https://i.gkd.li/import/13740126',
+          ],
+        },
+      ],
+    },
+    {
+      key: 5,
+      name: '播放页面-弹窗广告',
+      rules: [
+        {
+          activityIds: 'com.tvmain.mvp.view.activity.VodPlayerActivity',
+          matches:
+            '@ImageView[text.length=null] < FrameLayout <n FrameLayout +n ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13739932',
+        },
+      ],
+    },
+  ],
+});
