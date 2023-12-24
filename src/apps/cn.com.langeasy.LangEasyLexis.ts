afc354698d5220ee1d0940b6f8eaa01dce497320
lyc452 <108231863+lyc452@users.noreply.github.com>
2023-12-24 20:58:02 +0800

    feat(不背单词): 右下角弹窗 (#3385)
    * fix(不背单词): 自动签到
    
    * Add files via upload
    
    * Update cn.com.langeasy.LangEasyLexis.ts

--- src/apps/cn.com.langeasy.LangEasyLexis.ts
+++ src/apps/cn.com.langeasy.LangEasyLexis.ts
@@ -8,11 +8,28 @@ export default defineAppConfig({
       enable: false,
       key: 1,
       name: '自动签到',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
       rules: [
         {
           activityIds: 'cn.com.langeasy.LangEasyLexis.activity.MainActivity',
           matches: '[id="cn.com.langeasy.LangEasyLexis:id/bb_checkin"]',
           snapshotUrls: 'https://i.gkd.li/import/13610321',
+        },
+      ],
+    },
+    {
+      key: 2,
+      enable: false,
+      name: '右下角弹窗',
+      quickFind: true,
+      matchTime: 10000,
+      rules: [
+        {
+          activityIds: 'cn.com.langeasy.LangEasyLexis.activity.MainActivity',
+          matches: '[id="cn.com.langeasy.LangEasyLexis:id/close_float_button"]',
+          snapshotUrls: 'https://i.gkd.li/import/13759025',
         },
       ],
     },
