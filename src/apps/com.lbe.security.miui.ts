d2a9db019708c9fdc770470c7d30ed882585f286
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 21:14:19 +0800

    feat(权限管理服务): 权限授予弹窗 (#3412)

---
+++ src/apps/com.lbe.security.miui.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.lbe.security.miui',
+  name: '权限管理服务',
+  groups: [
+    {
+      key: 1,
+      name: '权限授予弹窗',
+      desc: '自动点击【仅在使用中允许】',
+      activityIds:
+        'com.android.packageinstaller.permission.ui.GrantPermissionsActivity',
+      quickFind: true,
+      rules: '[text="仅在使用中允许"]',
+      snapshotUrls: 'https://i.gkd.li/import/13761264',
+    },
+  ],
+});
