c0c7b3fe641e445ff037275fa8ab2b5811958631
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 21:01:23 +0800

    feat(大众点评): 卡片式广告 (#3388)

--- src/apps/com.dianping.v1.ts
+++ src/apps/com.dianping.v1.ts
@@ -55,5 +55,19 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 5,
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 0,
+          name: '推荐页卡片广告',
+          activityIds: 'com.dianping.v1.NovaMainActivity',
+          matches:
+            '[desc="reculike_delete"][clickable=true][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13759369',
+        },
+      ],
+    },
   ],
 });
