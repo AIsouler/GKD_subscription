9fdd1299938fce24435453093a94784fb9672cab
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-24 20:29:33 +0800

    feat(识货):请求消息通知 (#3301)
    close #3285

--- src/apps/com.hupu.shihuo.ts
+++ src/apps/com.hupu.shihuo.ts
@@ -15,6 +15,17 @@ export default defineAppConfig({
       snapshotUrls: 'https://i.gkd.li/import/13685134',
     },
     {
+      key: 2,
+      name: '请求消息通知',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      activityIds: 'com.shizhi.shihuoapp.module.main.ui.main.MainActivity',
+      rules: '[text^="通知分类"] +n ImageView[id="com.hupu.shihuo:id/ivClose"]',
+      snapshotUrls: 'https://i.gkd.li/import/13704887',
+    },
+    {
       key: 3,
       name: '广告弹窗',
       desc: '点击左上角x关闭',
