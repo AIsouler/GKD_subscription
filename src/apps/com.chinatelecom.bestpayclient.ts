c8d1bca195e2ab8412646cc2126aea8ef3ca001c
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 20:24:19 +0800

    feat(翼支付): 弹窗广告（补充规则） (#3280)

--- src/apps/com.chinatelecom.bestpayclient.ts
+++ src/apps/com.chinatelecom.bestpayclient.ts
@@ -27,16 +27,15 @@ export default defineAppConfig({
     },
     {
       key: 3,
-      name: '广告弹窗',
-      quickFind: true,
-      matchTime: 10000,
+      name: '弹窗广告',
       actionMaximum: 1,
-      resetMatch: 'app',
-      activityIds: 'com.chinatelecom.bestpayclient.ui.MainActivity',
+      resetMatch: 'activity',
       rules: [
         {
           key: 0,
           name: '权益页',
+          activityIds: 'com.chinatelecom.bestpayclient.ui.MainActivity',
+          quickFind: true,
           matches:
             'Image < @View[clickable=true][index=1] <<n Dialog <<n [id="com.chinatelecom.bestpayclient:id/best_h5_container"]',
           snapshotUrls: 'https://i.gkd.li/import/13402692',
@@ -44,15 +43,17 @@ export default defineAppConfig({
         {
           key: 1,
           name: '首页1',
+          activityIds: 'com.chinatelecom.bestpayclient.ui.MainActivity',
+          quickFind: true,
           matches:
             '[id="com.chinatelecom.bestpayclient:id/iv_dialog_close_one"]',
           snapshotUrls: 'https://i.gkd.li/import/13455790',
         },
-
         {
           key: 2,
           name: '首页2',
           activityIds: 'com.alipay.mobile.nebulacore.ui.H5Activity',
+          quickFind: true,
           matches:
             '@Image[clickable=true][text!=null] < View[childCount=2] <<n FrameLayout[id="com.chinatelecom.bestpayclient:id/h5_pc_container"]',
           snapshotUrls: 'https://i.gkd.li/import/13626324',
@@ -60,9 +61,22 @@ export default defineAppConfig({
         {
           key: 3,
           name: '借贷页',
+          activityIds: 'com.chinatelecom.bestpayclient.ui.MainActivity',
+          quickFind: true,
           matches:
             '@View[clickable=true][text=""] <<n [id="com.chinatelecom.bestpayclient:id/best_h5_container"]',
           snapshotUrls: 'https://i.gkd.li/import/13455929',
+        },
+        {
+          key: 4,
+          name: '信用卡页面',
+          activityIds: 'com.alipay.mobile.nebulacore.ui.H5Activity',
+          matches:
+            '[id="com.chinatelecom.bestpayclient:id/h5_pc_container"] >n View[childCount=3] > @*[clickable=true] + * + *[childCount=0]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13696322',
+            'https://i.gkd.li/import/13696323',
+          ],
         },
       ],
     },
