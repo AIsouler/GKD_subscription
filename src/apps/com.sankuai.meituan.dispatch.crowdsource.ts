e06aeb068dc0226a3865e6f9a4dc95a0e8796156
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 20:06:36 +0800

    feat(美团众包): 弹窗广告 (#3252)

---
+++ src/apps/com.sankuai.meituan.dispatch.crowdsource.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.sankuai.meituan.dispatch.crowdsource',
+  name: '美团众包',
+  groups: [
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.meituan.banma.main.activity.MainActivity',
+          quickFind: true,
+          matches:
+            '[id="com.sankuai.meituan.dispatch.crowdsource:id/image_ad_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13694935',
+        },
+      ],
+    },
+  ],
+});
