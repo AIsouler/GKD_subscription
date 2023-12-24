49f57394c3bcee633186830ad9960bf5a62d7cb3
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 20:21:41 +0800

    feat(Showcaller): 弹窗广告 (#3278)

---
+++ src/apps/com.allinone.callerid.ts
@@ -0,0 +1,27 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.allinone.callerid',
+  name: 'Showcaller',
+  groups: [
+    {
+      key: 1,
+      name: '弹窗广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          matches: '[id="mys-content"] > View > View > Button[clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13696207',
+        },
+        {
+          key: 1,
+          matches: '[id="close-button"] > View > View > Image',
+          snapshotUrls: 'https://i.gkd.li/import/13696205',
+        },
+      ],
+    },
+  ],
+});
