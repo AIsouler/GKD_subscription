9ba9873df9c81f890cfefe8bd709bfffb5d8b749
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-24 20:20:53 +0800

    feat(作业精灵):弹窗广告 (#3275)
    close #3274

---
+++ src/apps/com.pcncn.jj.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.pcncn.jj',
+  name: '作业精灵',
+  groups: [
+    {
+      key: 6,
+      name: '弹窗广告',
+      desc: '腾讯广告SDK',
+      quickFind: true,
+      activityIds: 'com.pcncn.jj.act.AnswerDetail2Activity',
+      rules:
+        '[id="com.pcncn.jj:id/interact_ad_root"] [id="com.pcncn.jj:id/iv_close"]',
+      snapshotUrls: 'https://i.gkd.li/import/13695874',
+    },
+  ],
+});
