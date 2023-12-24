cdcdb9506bbebbdb5de2680049606a7398732785
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-24 20:35:38 +0800

    feat(剧多多):弹窗广告 (#3321)
    close #3308

---
+++ src/apps/com.juduoduo.app.ts
@@ -0,0 +1,29 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.juduoduo.app',
+  name: '剧多多',
+  groups: [
+    {
+      key: 6,
+      name: '弹窗广告',
+      desc: '快手广告SDK',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          matches:
+            'ImageView[width<90][height<90] < @ViewGroup[clickable=true] <<n [id="com.juduoduo.app:id/ksad_tk_view"]',
+          snapshotUrls: 'https://i.gkd.li/import/13705650',
+        },
+        {
+          matches:
+            '[id="com.juduoduo.app:id/ksad_tk_view"] @ViewGroup[clickable=true] > [text="跳过"]',
+          snapshotUrls: 'https://i.gkd.li/import/13705662',
+        },
+      ],
+    },
+  ],
+});
