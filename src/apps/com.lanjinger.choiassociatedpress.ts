ab8e4c8e4fef3587363c1d328bc4dee9689bc9c6
MiracleLau <zhiminemail@163.com>
2023-12-24 20:58:20 +0800

    feat(财联社): 优化开屏广告,添加首页弹窗广告 (#3372)

--- src/apps/com.lanjinger.choiassociatedpress.ts
+++ src/apps/com.lanjinger.choiassociatedpress.ts
@@ -3,16 +3,31 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'com.lanjinger.choiassociatedpress',
   name: '财联社',
+  quickFind: true,
   groups: [
     {
       key: 0,
       name: '开屏广告',
       matchTime: 10000,
-      quickFind: true,
       resetMatch: 'app',
+      enable: true,
+      activityIds: 'com.lanjinger.choiassociatedpress.ad.AdActivity',
       actionMaximum: 1,
-      rules: '[id="com.lanjinger.choiassociatedpress:id/iv_skip"]',
-      snapshotUrls: 'https://i.gkd.li/import/13627807',
+      rules: '[text="跳过"]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13627807',
+        'https://i.gkd.li/import/13748989',
+      ],
+    },
+    {
+      key: 1,
+      name: '首页-弹窗广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      enable: false,
+      activityIds: 'com.lanjinger.choiassociatedpress.main.OperateActivity',
+      rules: '@ImageView[id$="iv_back"][clickable=true]',
+      snapshotUrls: 'https://i.gkd.li/import/13749206',
     },
   ],
 });
