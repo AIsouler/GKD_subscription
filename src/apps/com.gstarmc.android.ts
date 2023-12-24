0f375765f849af6393a0e1864a500445af92657b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 21:06:49 +0800

    feat(CAD看图王): 开屏广告（补充规则） (#3401)

--- src/apps/com.gstarmc.android.ts
+++ src/apps/com.gstarmc.android.ts
@@ -7,9 +7,18 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.stone.app.ui.activity.AppSplashActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
+          key: 0,
+          quickFind: true,
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13761120',
+        },
+        {
+          key: 1,
           matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
           snapshotUrls: 'https://i.gkd.li/import/12855855',
         },
