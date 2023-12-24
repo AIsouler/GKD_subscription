26dd71de2fff3191de4961d8e9a9e20ab8347354
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 20:05:18 +0800

    feat(猫番阅读): 开屏广告 (#3248)

---
+++ src/apps/com.maofan.app.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.maofan.app',
+  name: '猫番阅读',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13694873',
+    },
+  ],
+});
