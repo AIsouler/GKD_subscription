0ce2641d02a2b21c7220a12f1f9784620163f9bf
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 21:00:28 +0800

    feat(漫天星): 开屏广告、弹窗广告（补充规则） (#3386)
    * feat 弹窗广告（补充规则）
    
    * feat 开屏广告

--- src/apps/ltd.sky.okay.cn.ts
+++ src/apps/ltd.sky.okay.cn.ts
@@ -5,6 +5,15 @@ export default defineAppConfig({
   name: '漫天星',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+      snapshotUrls: 'https://i.gkd.li/import/13759409',
+    },
+    {
       key: 1,
       name: '弹窗广告',
       rules: [
@@ -15,6 +24,16 @@ export default defineAppConfig({
           matches:
             'ImageView -2 FrameLayout > FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
           snapshotUrls: 'https://i.gkd.li/import/13625479',
+        },
+        {
+          key: 1,
+          name: '字节广告',
+          actionDelay: 1500,
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches:
+            '@Image[text=""] < View +n View > View > TextView[text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/import/13759331',
         },
       ],
     },
