ce788269bd22df31093a5802d4b3b90658170bc0
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-24 20:19:07 +0800

    feat(SteamPY): 版本更新 (#3270)
    close #3258

---
+++ src/apps/com.steampy.app.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.steampy.app',
+  name: 'SteamPY',
+  groups: [
+    {
+      key: 1,
+      name: '版本更新',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.steampy.app.activity.common.MainActivity',
+      rules:
+        '@[id="com.steampy.app:id/imgClose"] - RelativeLayout [id="com.steampy.app:id/tv_update"]',
+      snapshotUrls: 'https://i.gkd.li/import/13695519',
+    },
+  ],
+});
