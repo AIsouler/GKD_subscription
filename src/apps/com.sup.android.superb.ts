e5e4d311db4a5e5bf8ba25e5b8e717194ff77d60
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 19:55:34 +0800

    feat 请求开启通知权限弹窗 (#3219)

--- src/apps/com.sup.android.superb.ts
+++ src/apps/com.sup.android.superb.ts
@@ -5,6 +5,15 @@ export default defineAppConfig({
   name: '皮皮虾',
   groups: [
     {
+      key: 5,
+      name: '请求开启通知权限弹窗',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text="开启推送通知"] +2 * > [text="以后再说"]',
+      snapshotUrls: 'https://i.gkd.li/import/13691081',
+    },
+    {
       key: 6,
       name: '右上角红包悬浮窗',
       matchTime: 10000,
