2993618baed3c2779c48bf082c92f7f0b4d30e36
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 21:15:36 +0800

    feat(Keep): 青少年模式弹窗（优化规则）、首页广告弹窗（优化规则）、请求开启通知权限弹窗 (#3415)
    * feat 青少年模式弹窗（优化规则）
    
    * feat 首页广告弹窗（优化规则）
    
    * feat 请求开启通知权限弹窗

--- src/apps/com.gotokeep.keep.ts
+++ src/apps/com.gotokeep.keep.ts
@@ -25,20 +25,28 @@ export default defineAppConfig({
     {
       key: 1,
       name: '青少年模式弹窗',
-      activityIds:
-        'com.gotokeep.keep.refactor.business.main.activity.MainActivity',
-      rules:
-        '[text$="未成年人模式"] + ImageView + ViewGroup > [text="我知道了"]',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text$="未成年人模式"] +2 @[clickable=true] > [text="我知道了"]',
       snapshotUrls: 'https://i.gkd.li/import/12706097',
     },
     {
       key: 2,
       name: '首页广告弹窗',
-      activityIds:
-        'com.gotokeep.keep.refactor.business.main.activity.MainActivity',
-      rules:
-        '[id="com.gotokeep.keep:id/layoutLottie"] + [id="com.gotokeep.keep:id/imgCloseHomePageDialog"]',
-      snapshotUrls: 'https://i.gkd.li/import/12706102',
+      rules: [
+        {
+          key: 0,
+          activityIds:
+            'com.gotokeep.keep.refactor.business.main.activity.MainActivity',
+          matches:
+            '[id="com.gotokeep.keep:id/layoutLottie"] +(1,2) [id="com.gotokeep.keep:id/imgCloseHomePageDialog"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12706102',
+            'https://i.gkd.li/import/13761641',
+          ],
+        },
+      ],
     },
     {
       key: 3,
@@ -56,6 +64,15 @@ export default defineAppConfig({
         'com.gotokeep.keep.refactor.business.main.activity.MainActivity',
       rules: 'ImageView + LinearLayout > ImageView[id=null][clickable=true]',
       snapshotUrls: 'https://i.gkd.li/import/12706111',
+    },
+    {
+      key: 5,
+      name: '请求开启通知权限弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '@[text="暂不开启"] +2 [text="开启通知权限"]',
+      snapshotUrls: 'https://i.gkd.li/import/13761671',
     },
   ],
 });
