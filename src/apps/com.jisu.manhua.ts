5618fe2b90504592d260d007d95a36769a43f5b4
walklty <47232102+walklty@users.noreply.github.com>
2023-12-24 19:56:38 +0800

    feat(漫画人极速版): 弹窗广告 (#3228)
    close #3212

---
+++ src/apps/com.jisu.manhua.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.jisu.manhua',
+  name: '漫画人极速版',
+  groups: [
+    {
+      key: 1,
+      name: '弹窗广告',
+      quickFind: true,
+      activityIds: 'com.ilike.cartoon.activities.HomeActivity',
+      rules: [
+        {
+          matches:
+            '[id="com.jisu.manhua:id/iv_promotion"] + [id="com.jisu.manhua:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13688186',
+        },
+      ],
+    },
+  ],
+});
