f710c145e51dae66f47a7fcead667ce86f637310
lisonge <i@songe.li>
2023-12-28 20:16:51 +0800

    perf: 全局规则-开屏广告

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -4,10 +4,15 @@ import type { RawGlobalGroup } from './types';
 const diabledAppIds = [
   'com.android.systemui',
   'com.miui.aod',
+
+  // 在桌面禁用
   'com.miui.home',
   'com.android.launcher.Launcher',
   'com.bbk.launcher2.Launcher',
   'com.huawei.android.launcher.unihome.UniHomeLauncher',
+
+  // 在一些常见的应用中禁用
+  'com.tencent.mm',
 ];
 
 // 如果应用规则已有开屏广告一类的规则, 则在全局规则禁用此应用
@@ -21,7 +26,7 @@ const globalGroups: RawGlobalGroup[] = [
   {
     key: 0,
     name: '开屏广告',
-    actionMaximum: 1,
+    actionMaximum: 2,
     matchTime: 10000,
     resetMatch: 'app',
     actionCdKey: 0,
@@ -30,14 +35,12 @@ const globalGroups: RawGlobalGroup[] = [
       {
         key: 0,
         quickFind: true,
-        matches: '[text*="跳过"][text.length<10]',
-        action: 'clickCenter',
+        matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
       },
       {
-        key: 1,
+        key: -1,
         matches:
-          '[(text.length<10&&(text*="跳过"||text*="skip")) || id$="tt_splash_skip_btn" || desc*="跳过" || desc*="skip"]',
-        action: 'clickCenter',
+          '[childCount=0][visibleToUser=true][(text.length<10&&(text*="跳过"||text*="skip")) || id$="tt_splash_skip_btn" || vid*="skip" || desc*="跳过" || desc*="skip"]',
       },
     ],
     apps: diabledAppIds.map((id) => ({ id, enable: false })),
