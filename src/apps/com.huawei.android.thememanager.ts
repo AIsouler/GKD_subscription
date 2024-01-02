0fe0bc879abd3a1bafa1e730424bc2ca3efa9fb1
CallMeBill <callmebill@billw.cn>
2024-01-02 19:34:50 +0800

    feat(华为主题): 开屏广告 (#3420)
    close #3189

--- src/apps/com.huawei.android.thememanager.ts
+++ src/apps/com.huawei.android.thememanager.ts
@@ -11,10 +11,16 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[text^="跳过"][text.length<=10]',
-      snapshotUrls: [
-        'https://i.gkd.li/import/12657379',
-        'https://i.gkd.li/import/13069736',
+      rules: [
+        {
+          matches: '[text^="跳过"][text.length<=10]',
+          action: 'clickCenter',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12657379',
+            'https://i.gkd.li/import/13069736',
+            'https://i.gkd.li/import/13762181',
+          ],
+        },
       ],
     },
     {
