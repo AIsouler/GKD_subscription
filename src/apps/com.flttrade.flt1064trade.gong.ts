a2c14c06f6d302b505819248fd17ba4f600dfbd2
adproqwq <101449500+adproqwq@users.noreply.github.com>
2024-01-02 20:36:54 +0800

    feat(影视工场): 弹窗广告-腾讯SDK（补充规则） (#3560)
    Co-authored-by: adproqwq <2757917031@qq.com>

--- src/apps/com.flttrade.flt1064trade.gong.ts
+++ src/apps/com.flttrade.flt1064trade.gong.ts
@@ -21,9 +21,15 @@ export default defineAppConfig({
           key: 0,
           name: '腾讯广告',
           activityIds: 'com.flttrade.flt1064trade.gong.MainActivity',
-          matches:
+          matches: [
             'ImageView - LinearLayout - FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
-          snapshotUrls: 'https://i.gkd.li/import/13759472',
+            '[id="android:id/content"] >(4,5) @FrameLayout[index=1] > ImageView',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/import/13759472',
+            'https://i.gkd.li/import/13798323', //第二条 >5
+            'https://i.gkd.li/import/13798327', //第二条 >4
+          ],
         },
       ],
     },
