2069e01db81ea2102be22addf8854d08fb8e2dac
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2024-01-02 19:56:09 +0800

    fix(交管12123): 消息通知新ActivityId (#3492)
    * fix(交管12123): 消息通知新ActivityId
    
    * Update com.tmri.app.main.ts

--- src/apps/com.tmri.app.main.ts
+++ src/apps/com.tmri.app.main.ts
@@ -19,9 +19,15 @@ export default defineAppConfig({
       actionMaximum: 1,
       rules: [
         {
-          activityIds: 'com.tmri.app.ui.activity.main.MainTabHostNewActivity',
+          activityIds: [
+            'com.tmri.app.ui.activity.main.MainTabHostNewActivity',
+            'com.tmri.app.ui.activity.main.TmriNewActivity',
+          ],
           matches: 'ImageButton[id=\'com.tmri.app.main:id/btn_cancel\']',
-          snapshotUrls: 'https://i.gkd.li/import/13315944',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13315944', //com.tmri.app.ui.activity.main.MainTabHostNewActivity
+            'https://i.gkd.li/import/13779215', //com.tmri.app.ui.activity.main.TmriNewActivity
+          ],
         },
       ],
     },
