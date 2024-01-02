5ed9c34a4bcbbd38d8ffaf5a571b56e51aca37ee
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-01-02 20:23:17 +0800

    feat(百分网游戏盒子): 开屏广告（优化规则） (#3576)

--- src/apps/com.byfen.market.ts
+++ src/apps/com.byfen.market.ts
@@ -12,8 +12,14 @@ export default defineAppConfig({
       resetMatch: 'app',
       rules: [
         {
+          key: 0,
           matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
           snapshotUrls: 'https://i.gkd.li/import/13298944',
+        },
+        {
+          key: 1,
+          matches: '[id="com.byfen.market:id/ad_view"] >3 View[clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13800021',
         },
       ],
     },
