7c6078d007f5a33e5c04a1698ca6518335a46fcc
adproqwq <101449500+adproqwq@users.noreply.github.com>
2024-01-02 20:49:04 +0800

    feat(京喜): 底部申请定位浮窗 (#3619)
    close #3616

--- src/apps/com.jd.pingou.ts
+++ src/apps/com.jd.pingou.ts
@@ -11,12 +11,18 @@ export default defineAppConfig({
         'com.jd.jdlite.ad.ADActivity',
         'com.jd.jdlite.MainActivity',
       ],
-
       rules: '[desc="闪屏广告"] + [text$="跳过"]',
       snapshotUrls: [
         'https://i.gkd.li/import/12727342',
         'https://i.gkd.li/import/13062974',
       ],
+    },
+    {
+      key: 1,
+      name: '底部申请定位浮窗',
+      activityIds: 'com.jd.jdlite.MainFrameActivity',
+      rules: '[text*="开启定位授权"] +2 ImageView',
+      snapshotUrls: 'https://i.gkd.li/import/13804515',
     },
   ],
 });
