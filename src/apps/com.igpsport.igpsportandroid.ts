2cce1c8015ca49b9429b9151b399b0950755da53
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-01-02 20:11:09 +0800

    feat(iGPSPORT): 开屏广告（优化规则）、更新弹窗 (#3540)
    * feat 开屏广告（优化规则）
    
    * feat 更新弹窗

--- src/apps/com.igpsport.igpsportandroid.ts
+++ src/apps/com.igpsport.igpsportandroid.ts
@@ -7,9 +7,20 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.igpsport.globalapp.activity.SplashAdActivity',
-      rules: '[id="com.igpsport.igpsportandroid:id/tvSkip"]',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12818230',
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@[text="忽略本次"] + [text="立即升级"]',
+      snapshotUrls: 'https://i.gkd.li/import/13797203',
     },
   ],
 });
