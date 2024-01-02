40e8055dc18e9e194f6de9891a91ca25060f65f3
aoguai <34203474+aoguai@users.noreply.github.com>
2024-01-02 20:17:47 +0800

    feat(富民银行): 开屏广告(gkd-kit#3504) (#3551)
    close #3504

---
+++ src/apps/com.fbank.mobile.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.fbank.mobile',
+  name: '富民银行',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[id="com.fbank.mobile:id/iv_dumiao"]',
+          snapshotUrls: 'https://i.gkd.li/import/13797434',
+        },
+      ],
+    },
+  ],
+});
