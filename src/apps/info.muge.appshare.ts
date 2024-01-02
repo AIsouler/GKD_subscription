8e44618e7cc65fe5d804db338d01607d51a888ac
adproqwq <101449500+adproqwq@users.noreply.github.com>
2024-01-02 19:45:25 +0800

    feat(AppShare): 卡片式广告（补充规则） (#3445)
    close #3383

--- src/apps/info.muge.appshare.ts
+++ src/apps/info.muge.appshare.ts
@@ -40,11 +40,14 @@ export default defineAppConfig({
           key: 0,
           name: '腾讯广告',
           activityIds: 'info.muge.appshare.view.app.detail.AppDetailActivity',
-          matches:
+          matches: [
             '[id="info.muge.appshare:id/adContainer"] >n FrameLayout > ImageView - FrameLayout[childCount=1] > ImageView[childCount=0]',
+            '[id="info.muge.appshare:id/adContainer"] >n FrameLayout[desc*="dislike"]', //新规则
+          ],
           snapshotUrls: [
             'https://i.gkd.li/import/13761259',
             'https://i.gkd.li/import/13712716',
+            'https://i.gkd.li/import/13758909', //新规则
           ],
         },
       ],
