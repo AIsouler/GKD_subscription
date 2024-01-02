f72240422f75acfc435b951be6bffa80ad5df100
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2024-01-02 19:52:52 +0800

    feat(Soul):青少年模式弹窗 (#3470)
    Signed-off-by: jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>

--- src/apps/cn.soulapp.android.ts
+++ src/apps/cn.soulapp.android.ts
@@ -26,7 +26,6 @@ export default defineAppConfig({
       key: 1,
       name: '青少年模式弹窗',
       quickFind: true,
-      matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
