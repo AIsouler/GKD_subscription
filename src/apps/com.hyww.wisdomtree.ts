5daef343e5b7ca89e04dcef5aa2c7a79e8d47559
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-01-02 20:20:47 +0800

    feat(智慧树): 开屏广告、弹窗广告 (#3566)
    * feat 开屏广告
    
    * feat 弹窗广告

---
+++ src/apps/com.hyww.wisdomtree.ts
@@ -0,0 +1,32 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.hyww.wisdomtree',
+  name: '智慧树',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13799839',
+    },
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '百度广告',
+          activityIds: 'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
+          matches:
+            '@ImageView[clickable=true] + RelativeLayout > RelativeLayout[childCount=6] > ImageView[childCount=0][index=5]',
+          snapshotUrls: 'https://i.gkd.li/import/13799876',
+        },
+      ],
+    },
+  ],
+});
