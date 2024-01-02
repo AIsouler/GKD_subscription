228bb0c8c840e3a2877cf160c4a660118416f687
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-01-02 20:19:13 +0800

    feat(爱壹帆): 开屏广告（优化规则）、更新弹窗 (#3559)
    * feat 开屏广告（优化规则）
    
    * feat 更新弹窗

--- src/apps/com.cqcsy.ifvod.ts
+++ src/apps/com.cqcsy.ifvod.ts
@@ -7,9 +7,21 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.cqcsy.lgsp.main.SplashActivity',
-      rules: '[id="com.cqcsy.ifvod:id/skipTips"]',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12793228',
+    },
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '@[text="以后再说"] +2 [text="立即更新"]',
+      snapshotUrls: 'https://i.gkd.li/import/13798537',
     },
   ],
 });
