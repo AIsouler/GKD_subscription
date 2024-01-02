0d50854c8a100b4f718981e9efbb457deb2ef95a
aoguai <34203474+aoguai@users.noreply.github.com>
2024-01-02 20:12:59 +0800

    feat(邮储银行): 首页-浮窗广告 (#3543)

--- src/apps/com.yitong.mbank.psbc.ts
+++ src/apps/com.yitong.mbank.psbc.ts
@@ -31,5 +31,13 @@ export default defineAppConfig({
         '[id="com.yitong.mbank.psbc:id/iv_theme"] + [id="com.yitong.mbank.psbc:id/iv_theme_close_btn"]',
       snapshotUrls: 'https://i.gkd.li/import/12755516',
     },
+    {
+      key: 3,
+      name: '首页-浮窗广告',
+      activityIds:
+        'com.yitong.mbank.psbc.module.home.view.activity.MainActivity',
+      rules: '[id="com.yitong.mbank.psbc:id/img_close_float_window"]',
+      snapshotUrls: 'https://i.gkd.li/import/13797314',
+    },
   ],
 });
