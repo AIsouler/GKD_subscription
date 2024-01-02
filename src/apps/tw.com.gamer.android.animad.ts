481ca4a80caf0a2d6f60d856bea7a142d65fd14d
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-02 20:59:33 +0800

    feat(动画疯):年龄限制窗口 (#3654)
    close #3644

---
+++ src/apps/tw.com.gamer.android.animad.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'tw.com.gamer.android.animad',
+  name: '動畫瘋',
+  groups: [
+    {
+      key: 1,
+      name: '动画疯年龄限制',
+      desc: '点击同意',
+      activityIds: 'tw.com.gamer.android.animad.VideoActivity',
+      quickFind: true,
+      rules: [
+        {
+          matches:
+            '[id="tw.com.gamer.android.animad:id/agree_button"][text="同意"]',
+          snapshotUrls: 'https://i.gkd.li/import/13808757',
+        },
+      ],
+    },
+  ],
+});
