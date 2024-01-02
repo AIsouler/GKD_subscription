07212c17958011a17f81983410621ff1eea9bf3a
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-01-02 19:43:20 +0800

    feat(vivo 软件包安装程序): 自动安装应用（补充规则） (#3438)

--- src/apps/com.android.packageinstaller.ts
+++ src/apps/com.android.packageinstaller.ts
@@ -5,7 +5,6 @@ export default defineAppConfig({
   name: 'vivo 软件包安装程序',
   groups: [
     {
-      enable: false,
       key: 1,
       name: '自动安装应用',
       quickFind: true,
@@ -20,8 +19,16 @@ export default defineAppConfig({
           key: 1,
           preKeys: 0,
           activityIds: 'com.android.packageinstaller.NewInstallInstalling',
-          matches: '@LinearLayout > [text="完成"]',
+          matches: '@[clickable=true][childCount=1] > [text="完成"]',
           snapshotUrls: 'https://i.gkd.li/import/13206476',
+        },
+        {
+          key: 2,
+          preKeys: 0,
+          activityIds: 'com.android.packageinstaller.NewInstallInstalling',
+          matches:
+            '[id="com.android.packageinstaller:id/done_button"][text="完成"][clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13766420',
         },
       ],
     },
