667c12466f0a136106e3d67b81ca144a407836cb
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-01-02 20:07:57 +0800

    feat(皮皮虾): 青少年模式弹窗 (#3534)

--- src/apps/com.sup.android.superb.ts
+++ src/apps/com.sup.android.superb.ts
@@ -5,6 +5,15 @@ export default defineAppConfig({
   name: '皮皮虾',
   groups: [
     {
+      key: 1,
+      name: '青少年模式弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text$="青少年模式"] + [text="我知道了"]',
+      snapshotUrls: 'https://i.gkd.li/import/13796869',
+    },
+    {
       key: 5,
       name: '请求开启通知权限弹窗',
       quickFind: true,
