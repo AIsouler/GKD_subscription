2fff267bff0dba5b24bf7ae78b85e608259af942
aoguai <34203474+aoguai@users.noreply.github.com>
2024-01-02 20:36:35 +0800

    feat(youtube): 视频播放-跳过广告、视频播放-赞助商广告、订阅浮窗广告(gkd-kit#3520) (#3552)
    close #3520

--- src/apps/com.google.android.youtube.ts
+++ src/apps/com.google.android.youtube.ts
@@ -9,16 +9,26 @@ export default defineAppConfig({
       name: '视频播放-跳过广告',
       activityIds: [
         'com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity',
+        'com.google.android.apps.youtube.app.watchwhile.MainActivity',
         'com.google.android.youtube',
       ],
       quickFind: true,
       rules: [
-        '[id="com.google.android.youtube:id/skip_ad_button"]',
-        '[id="com.google.android.youtube:id/modern_skip_ad_button"]',
-      ],
-      snapshotUrls: [
-        'https://i.gkd.li/import/12565261',
-        'https://i.gkd.li/import/13705106', // rules:'[id="com.google.android.youtube:id/modern_skip_ad_button"]'
+        {
+          key: 0,
+          name: '类型1',
+          matches: '[id="com.google.android.youtube:id/skip_ad_button_text"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13797491',
+            'https://i.gkd.li/import/12565261',
+          ],
+        },
+        {
+          key: 1,
+          name: '类型2',
+          matches: '[id="com.google.android.youtube:id/modern_skip_ad_text"]',
+          snapshotUrls: ['https://i.gkd.li/import/13705106'],
+        },
       ],
     },
     {
@@ -26,8 +36,13 @@ export default defineAppConfig({
       name: '视频播放-赞助商广告',
       activityIds:
         'com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity',
-      rules: 'ImageView[desc="更多选项"] + @ImageView[desc="关闭广告面板"]',
-      snapshotUrls: 'https://i.gkd.li/import/12877346',
+      rules:
+        'ImageView[desc="更多选项"||desc^="More"] + @ImageView[desc="关闭广告面板"||desc^="Close"]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12877346',
+        'https://i.gkd.li/import/13797491',
+        'https://i.gkd.li/import/13705106',
+      ],
     },
     {
       key: 2,
@@ -37,6 +52,15 @@ export default defineAppConfig({
       rules:
         'ImageView < @Button < ViewGroup +3 Button[desc*="YouTube Premium"]',
       snapshotUrls: 'https://i.gkd.li/import/12877357',
+    },
+    {
+      key: 3,
+      name: '订阅浮窗广告',
+      activityIds:
+        'com.google.android.apps.youtube.app.watchwhile.MainActivity',
+      rules:
+        'ViewGroup[desc="免费试用 1 个月"] < * - * > ViewGroup[text="不用了，谢谢"]', // 缺少英文快照
+      snapshotUrls: 'https://i.gkd.li/import/13797512',
     },
   ],
 });
