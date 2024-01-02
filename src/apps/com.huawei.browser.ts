5b147541f4d94731f9fbec4cc0f71b3700ab0d3b
Huahvv12 <87047583+Huahvv12@users.noreply.github.com>
2024-01-02 19:29:37 +0800

    feat(华为浏览器): 规则添加 (#3237)
    * Update com.huawei.browser.ts
    
    新增关闭“开启资讯通知”弹窗
    
    * chore(actions): check_format_lint
    
    * Update com.huawei.browser.ts
    
    * chore(actions): check_format_lint
    
    * Update com.huawei.browser.ts
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.huawei.browser.ts
+++ src/apps/com.huawei.browser.ts
@@ -19,5 +19,19 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '开启资讯通知弹窗',
+      desc: '关闭资讯通知后，总是弹窗让打开',
+      rules: [
+        {
+          activityIds: 'com.huawei.browser.BrowserMainActivity',
+          matches: '[id="com.huawei.browser:id/close_push_guide_window"]',
+          exampleUrls:
+            'https://m.gkd.li/87047583/3982b64a-15a7-4c0c-b179-2ed82d6ea18e',
+          snapshotUrls: 'https://i.gkd.li/import/13692404',
+        },
+      ],
+    },
   ],
 });
