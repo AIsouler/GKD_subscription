a63892c0c0f135dc3e50bcb0cb17dab7e9417ba9
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-01-02 20:23:44 +0800

    feat(360家庭防火墙): 开屏广告（优化规则）、更新弹窗 (#3574)
    * feat 开屏广告（优化规则）
    
    * feat 更新弹窗

--- src/apps/com.qihoo.srouter.n300.ts
+++ src/apps/com.qihoo.srouter.n300.ts
@@ -11,12 +11,20 @@ export default defineAppConfig({
       actionMaximum: 1,
       resetMatch: 'app',
       quickFind: true,
-      rules: [
-        {
-          matches: '[id="com.qihoo.srouter.n300:id/tv_countdown"]',
-          snapshotUrls: 'https://i.gkd.li/import/13216607',
-        },
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13216607',
+        'https://i.gkd.li/import/13800007',
       ],
+    },
+    {
+      key: 1,
+      name: '弹窗广告',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[vid="app_update_later_button"]',
+      snapshotUrls: 'https://i.gkd.li/import/13800011',
     },
   ],
 });
