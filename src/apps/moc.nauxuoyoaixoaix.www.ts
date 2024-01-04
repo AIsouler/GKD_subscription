ce305184a927a912a813f4e8799c9eef2d2199d7
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-04 21:34:05 +0800

    feat(晓晓优选):开屏广告 (#3693)
    close #3690

---
+++ src/apps/moc.nauxuoyoaixoaix.www.ts
@@ -0,0 +1,36 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'moc.nauxuoyoaixoaix.www',
+  name: '晓晓优选',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      actionCdKey: 0,
+      actionMaximumKey: 0,
+      rules: [
+        {
+          key: 0,
+          matches: '[id$="tt_splash_skip_btn"] <<n [vid="adsFl"]',
+          snapshotUrls: 'https://i.gkd.li/import/13476862',
+        },
+        {
+          key: 1,
+          matches: ['[vid="adsFl"]', '[text^="跳过"][text.length<=10]'],
+          snapshotUrls: 'https://i.gkd.li/import/13625367',
+        },
+        {
+          key: 2,
+          matches:
+            '@View <3 FrameLayout <2 FrameLayout < FrameLayout < FrameLayout < [vid="adsFl"]',
+          snapshotUrls: 'https://i.gkd.li/import/13820450',
+        },
+      ],
+    },
+  ],
+});
