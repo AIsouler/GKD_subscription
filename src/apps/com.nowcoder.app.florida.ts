a65aad90d9e84e8093e85745db537a38fdcb9e79
Xenos Tian <1450846708@qq.com>
2024-01-14 22:32:12 +0800

    feat(牛客): 开屏广告 (#3784)
    * feat(牛客): 开屏广告
    
    * feat(牛客): 开屏广告
    
    * Update com.nowcoder.app.florida.ts
    
    Signed-off-by: 二刺螈 <i@songe.li>
    
    * Update com.nowcoder.app.florida.ts
    
    Signed-off-by: 二刺螈 <i@songe.li>
    
    * Update com.nowcoder.app.florida.ts
    
    Signed-off-by: 二刺螈 <i@songe.li>
    
    ---------
    
    Signed-off-by: 二刺螈 <i@songe.li>
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.nowcoder.app.florida.ts
@@ -0,0 +1,25 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.nowcoder.app.florida',
+  name: '牛客',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      actionCdKey: 0,
+      actionMaximumKey: 0,
+      rules: [
+        {
+          key: 0,
+          matches:
+            '@View <2 FrameLayout[childCount=3] <2 FrameLayout[childCount=2] < FrameLayout[childCount=1] < [vid="fl_splash_container"]',
+          snapshotUrls: 'https://i.gkd.li/import/13855443',
+        },
+      ],
+    },
+  ],
+});
