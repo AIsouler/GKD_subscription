526f35971319dc7de2ce3619024892f59cd9beae
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 22:51:05 +0800

    fix(招商银行):补充ActivityId (#3827)
    close #3826

--- src/apps/cmb.pb.ts
+++ src/apps/cmb.pb.ts
@@ -15,15 +15,18 @@ export default defineAppConfig({
     },
     {
       key: 11,
-      name: '请求定位权限弹窗',
+      name: '定位提示-请求定位权限弹窗',
       quickFind: true,
-      activityIds:
+      activityIds: [
         'cmb.pb.app.h5container.webviewcontainer.PBWebContainerActivity',
+        'cmb.pb.app.search.PBSearchWebActivity',
+      ],
       rules:
         '@[id="cmb.pb:id/ivDelete"][desc="关闭"] - FrameLayout > [id="cmb.pb:id/tvDescription"][text*="获取您的位置"]',
       snapshotUrls: [
         'https://i.gkd.li/import/12706029',
         'https://i.gkd.li/import/13248893',
+        'https://i.gkd.li/import/13897345',
       ],
     },
   ],
