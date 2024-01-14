89c8d82f6d7b3b96f5ca5ed732f6e34d8bb69e2a
adproqwq <101449500+adproqwq@users.noreply.github.com>
2024-01-14 20:52:15 +0800

    feat(埋堆堆): 青少年弹窗 (#3753)
    close #3751

---
+++ src/apps/com.tvbc.maiduidui.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tvbc.maiduidui',
+  name: '埋堆堆',
+  groups: [
+    {
+      key: 1,
+      name: '青少年弹窗',
+      quickFind: true,
+      activityIds: 'com.tvbc.maiduidui.MainHomeActivity',
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules: '@[vid="tv_ok"] - [vid="tv_open_child_model"]',
+      snapshotUrls: 'https://i.gkd.li/import/13847804',
+    },
+  ],
+});
