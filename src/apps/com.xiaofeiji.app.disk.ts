d2be13bd6ddee11eb4644fafa797433a778d0096
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 21:36:15 +0800

    feat(小飞机网盘):视频页广告 (#3791)
    * feat(小飞机网盘):视频页广告
    
    * feat:修改名称

---
+++ src/apps/com.xiaofeiji.app.disk.ts
@@ -0,0 +1,29 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xiaofeiji.app.disk',
+  name: '小飞机网盘',
+  groups: [
+    {
+      key: 1,
+      quickFind: true,
+      name: '分段广告',
+      desc: '点击X-点击不感兴趣',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.xiaofeiji.app.disk.MainActivity',
+          matches: '@Image[visibleToUser=true] < View + View >2 [text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/import/13857392',
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          activityIds: 'com.xiaofeiji.app.disk.MainActivity',
+          matches: '[id="android:id/content"] >5 [text="不感兴趣"]',
+          snapshotUrls: 'https://i.gkd.li/import/13857537',
+        },
+      ],
+    },
+  ],
+});
