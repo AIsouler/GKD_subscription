093043ae3db88fce8d4fe585d0fa483adbda62c5
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 22:47:29 +0800

    feat(汽车之家):右下角领福利悬浮窗 (#3817)
    close #3816

--- src/apps/com.cubic.autohome.ts
+++ src/apps/com.cubic.autohome.ts
@@ -12,5 +12,19 @@ export default defineAppConfig({
       rules: '[id="com.cubic.autohome:id/operate_show_exit_layout"]',
       snapshotUrls: 'https://i.gkd.li/import/12836324',
     },
+    {
+      key: 2,
+      quickFind: true,
+      name: '局部广告-右下角领福利悬浮窗',
+      desc: '点击X',
+      rules: [
+        {
+          activityIds: 'com.cubic.autohome.MainActivity',
+          matches:
+            '@ImageView[visibleToUser=true] < * <2 * - [id="android:id/tabhost"]',
+          snapshotUrls: 'https://i.gkd.li/import/13885414',
+        },
+      ],
+    },
   ],
 });
