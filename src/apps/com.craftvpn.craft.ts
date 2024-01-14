c7a67fc9b4c105d8e34868c02e34525b6f014e87
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 22:07:05 +0800

    feat(小牛加速器):开屏广告 (#3837)
    * feat(小牛加速器):开屏广告
    
    * chore(actions): check_format_lint
    
    * Update com.craftvpn.craft.ts
    
    Signed-off-by: 二刺螈 <i@songe.li>
    
    ---------
    
    Signed-off-by: 二刺螈 <i@songe.li>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.craftvpn.craft.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.craftvpn.craft',
+  name: '小牛加速器',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      desc: '点击X',
+      quickFind: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches:
+            '@View < View[childCount=2] < View[childCount=1] < View[childCount=1] < FrameLayout[childCount=1] < FrameLayout[childCount=1] < [id="android:id/content"]',
+          snapshotUrls: 'https://i.gkd.li/import/13914788',
+        },
+      ],
+    },
+  ],
+});
