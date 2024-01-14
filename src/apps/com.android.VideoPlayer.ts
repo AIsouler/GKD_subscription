1b8ca8eb24f515d0c76203023b40c2685ba9f71f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-01-14 21:17:35 +0800

    feat(i 视频): 青少年模式 (#3767)

---
+++ src/apps/com.android.VideoPlayer.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.android.VideoPlayer',
+  name: 'i 视频',
+  groups: [
+    {
+      key: 1,
+      name: '青少年模式',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[vid="child_mode_cancel_button"]',
+      snapshotUrls: 'https://i.gkd.li/import/13849807',
+    },
+  ],
+});
