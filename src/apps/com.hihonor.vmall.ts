f4598ea18ff427d7b63d636cdeef1e958871923b
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 23:05:21 +0800

    feat(荣耀商城):应用内广告弹窗 (#3869)

--- src/apps/com.hihonor.vmall.ts
+++ src/apps/com.hihonor.vmall.ts
@@ -6,8 +6,11 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '应用内广告弹窗',
-      activityIds: 'com.vmall.client.base.fragment.VmallWapActivity',
+      name: '全屏广告-应用内广告弹窗',
+      activityIds: [
+        'com.vmall.client.base.fragment.VmallWapActivity',
+        'com.vmall.client.splash.fragment.SplashActivity',
+      ],
       rules: '[id="com.hihonor.vmall:id/gift_close_iv"]',
       snapshotUrls: 'https://i.gkd.li/import/13060881',
     },
