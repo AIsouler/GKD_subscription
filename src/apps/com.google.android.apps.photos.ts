e8432d21b8d413c793186dc0e97828eb02e7d4e4
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 20:55:26 +0800

    feat(相册):优惠提示 (#3757)
    close #3474

--- src/apps/com.google.android.apps.photos.ts
+++ src/apps/com.google.android.apps.photos.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '取消更新',
+      name: '更新提示-取消更新',
       desc: '点击[以后再说]',
       activityIds: [
         'com.google.android.apps.photos.update.treatment.UpdateAppTreatmentPromoPageActivity',
@@ -17,6 +17,21 @@ export default defineAppConfig({
           matches:
             '[id=`com.google.android.apps.photos:id/negative_button`][text=`以后再说`]',
           snapshotUrls: 'https://i.gkd.li/import/13218940',
+        },
+      ],
+    },
+    {
+      key: 1,
+      quickFind: true,
+      name: '全屏广告-优惠提示',
+      desc: '点击X',
+      rules: [
+        {
+          activityIds:
+            'com.google.android.apps.photos.cloudstorage.paidfeatures.PaidFeaturesActivity',
+          matches:
+            '@[desc="转到上一层级"] < ViewGroup[id="com.google.android.apps.photos:id/toolbar"]',
+          snapshotUrls: 'https://i.gkd.li/import/13774247',
         },
       ],
     },
