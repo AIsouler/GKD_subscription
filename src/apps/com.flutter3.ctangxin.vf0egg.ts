9d4532457157fbb38a337c3784682396352eae1e
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2024-01-14 20:22:37 +0800

    feat(糖心Vlog): 广告弹窗 (#3732)
    * Update and rename com.flutter3.ctangxin.vf0egg.ts to com.flutter3.ctangxin.ef0eee.ts
    
    Signed-off-by: jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
    
    * Update com.flutter3.ctangxin.ef0eee.ts
    
    * Update com.flutter3.ctangxin.ef0eee.ts
    
    * Update com.flutter3.ctangxin.ef0eee.ts
    
    * Update com.flutter3.ctangxin.ef0eee.ts
    
    * Update com.flutter3.ctangxin.ef0eee.ts
    
    * Update com.flutter3.ctangxin.ef0eee.ts
    
    * Update com.flutter3.ctangxin.ef0eee.ts
    
    * Update and rename com.flutter3.ctangxin.ef0eee.ts to com.flutter3.ctangxin.vf0egg.ts
    
    ---------
    
    Signed-off-by: jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>

--- src/apps/com.flutter3.ctangxin.vf0egg.ts
+++ src/apps/com.flutter3.ctangxin.vf0egg.ts
@@ -1,7 +1,7 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-  id: 'com.flutter3.ctangxin.vf0egg',
+  id: 'com.flutter3.ctangxin.vf0egg', // 注意 appId 有随机后缀, 每个版本都变，需修改为你自己的 appId
   name: '糖心Vlog',
   groups: [
     {
@@ -22,14 +22,15 @@ export default defineAppConfig({
     },
     {
       key: 1,
-      name: '广告弹窗',
-
+      name: '全屏广告-广告弹窗', // 目前已叠加了三层广告
+      //matchDelay: 6500, // 待无法跳过的5s开屏广告过后，能够主动执行匹配
       matchTime: 10000,
-      actionMaximum: 1,
+      actionCd: 100,
       resetMatch: 'app',
       activityIds: ['com.example.flutter3_frame.MainActivity'],
       rules: [
         {
+          action: 'back',
           matches:
             'FrameLayout > View > View > View[index=0] > View > ImageView[childCount=1] + View[clickable=true && desc=null]',
           snapshotUrls: 'https://i.gkd.li/import/12836891',
@@ -38,18 +39,29 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: '公告弹窗',
-
+      name: '全屏广告-公告弹窗',
       matchTime: 10000,
-      actionMaximum: 1,
       resetMatch: 'app',
       activityIds: ['com.example.flutter3_frame.MainActivity'],
       rules: [
         {
-          matches: '@[desc="取消"][clickable=true] + [desc="确定"]',
+          action: 'back',
+          matches: '[desc="公告"] +2 [desc="取消"][clickable=true]',
           snapshotUrls: 'https://i.gkd.li/import/12836854',
         },
       ],
+    },
+    {
+      // key1、2的延迟工具，matchDelay如在key1、2，有时候开屏广告时切了后台，
+      // 后台恢复时，界面上的弹窗就得等6.5s了
+      key: 3,
+      name: '全屏广告-延迟工具(协助key1、2)',
+      desc: '5s开屏广告后主动触发匹配',
+      matchDelay: 6500, // 时间太小会导致低性能手机赶不上
+      matchTime: 10000,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: 'TextView[text="xxxxx"]',
     },
   ],
 });
