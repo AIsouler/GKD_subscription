00896763a3c107d473add87da2afb8887f73166c
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 22:54:28 +0800

    feat(系统服务组件):游戏加速广告弹窗 (#3836)
    * feat(系统服务组件):游戏加速广告弹窗
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/apps/com.miui.securityadd.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.miui.securityadd',
+  name: '系统服务组件',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-游戏加速广告弹窗',
+      desc: '点击X',
+      rules: [
+        {
+          activityIds: 'com.miui.gamebooster.GameBoosterRichWebActivity',
+          matches:
+            '[id="com.miui.securityadd:id/webViewContainer"] [text="活动图片"] +2 Image[clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13914659',
+        },
+      ],
+    },
+  ],
+});
