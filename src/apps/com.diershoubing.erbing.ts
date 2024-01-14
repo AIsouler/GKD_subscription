cf8889b1b495d232ad62f04a42e904fd8f13e531
adproqwq <101449500+adproqwq@users.noreply.github.com>
2024-01-14 22:19:06 +0800

    feat(二柄): 开屏广告 (#3807)
    close #3797

--- src/apps/com.diershoubing.erbing.ts
+++ src/apps/com.diershoubing.erbing.ts
@@ -5,6 +5,35 @@ export default defineAppConfig({
   name: '二柄',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          key: 0,
+          matches: '@[id$="tt_splash_skip_btn"] <<n [vid="containerV"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13378853',
+            'https://i.gkd.li/import/13546165',
+          ],
+        },
+        {
+          key: 1,
+          matches: '[text="跳过"]',
+          snapshotUrls: 'https://i.gkd.li/import/13538207',
+        },
+        {
+          key: 2,
+          matches:
+            '@View <2 FrameLayout[childCount=3] <2 FrameLayout[childCount=2] < [vid="containerV"]',
+          snapshotUrls: 'https://i.gkd.li/import/13857307',
+        },
+      ],
+    },
+    {
       key: 6,
       name: '分段广告',
       quickFind: true,
