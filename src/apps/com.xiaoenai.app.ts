d693d0130fbabb75b326c2e15ca0b04babf4e658
walklty <47232102+walklty@users.noreply.github.com>
2024-01-14 21:44:48 +0800

    feat(小恩爱): 开屏, 弹窗 (#3798)
    close #3796

--- src/apps/com.xiaoenai.app.ts
+++ src/apps/com.xiaoenai.app.ts
@@ -5,19 +5,39 @@ export default defineAppConfig({
   name: '小恩爱',
   groups: [
     {
-      key: 6,
-      name: '广告弹窗',
+      key: 0,
+      name: '开屏广告',
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      activityIds:
-        'com.xiaoenai.app.presentation.home.view.activity.HomeActivity',
+      quickFind: true,
+      rules: [
+        {
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13727905',
+        },
+        {
+          matches:
+            '@View <3 FrameLayout[childCount=4] <2 FrameLayout[childCount=2] < [vid="ad_view"]',
+          snapshotUrls: 'https://i.gkd.li/import/13867880',
+        },
+      ],
+    },
+    {
+      key: 6,
+      name: '全屏广告',
+      matchTime: 15000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           name: '腾讯广告SDK',
           matches:
             'FrameLayout[childCount>1] > FrameLayout[childCount=1] > ImageView[width<90][height<90]',
-          snapshotUrls: 'https://i.gkd.li/import/13728018',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13728018',
+            'https://i.gkd.li/import/13867878',
+          ],
         },
       ],
     },
