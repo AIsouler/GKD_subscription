39a701d4587b63e76d49a04aaffd82e23506c35d
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 22:12:59 +0800

    feat(网速管家):开屏广告 (#3820)
    close #3819

--- src/apps/cn.lezhi.speedtest.ts
+++ src/apps/cn.lezhi.speedtest.ts
@@ -3,8 +3,34 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'cn.lezhi.speedtest',
   name: '网速管家',
-  deprecatedKeys: [0],
   groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      matchTime: 10000,
+      quickFind: true,
+      rules: [
+        {
+          name: '字节SDK',
+          matches:
+            '@[id="com.byted.pangle:id/tt_splash_skip_btn"] <<n [id="cn.lezhi.speedtest:id/fl_splash_container"]',
+          snapshotUrls: 'https://i.gkd.li/import/13544242',
+        },
+        {
+          name: '腾讯SDK',
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13626049',
+        },
+        {
+          name: '腾讯SDK',
+          matches:
+            '@View <3 FrameLayout[childCount=4] <2 FrameLayout[childCount=2] < [vid="fl_splash_container"]',
+          snapshotUrls: 'https://i.gkd.li/import/13885906',
+        },
+      ],
+    },
     {
       key: 1,
       name: '更新弹窗',
