2b409a62cd6e73d19cffe8a3c3e5ff76651beb6c
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2024-01-14 20:23:15 +0800

    feat(VIVO 电子书): 通知弹窗 (#3729)
    * feat(VIVO 电子书): 通知弹窗
    
    * chore(actions): check_format_lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.chaozh.iReader.ts
+++ src/apps/com.chaozh.iReader.ts
@@ -17,5 +17,22 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 2,
+      name: '通知弹窗',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      activityIds: 'com.zhangyue.iReader.bookshelf.ui.ActivityBookShelf',
+      excludeActivityIds:
+        'com.zhangyue.iReader.read.ui.Activity_BookBrowser_TXT',
+      rules: [
+        {
+          matches:
+            '@ImageView[clickable=true] +(1,2) TextView[text="开启推送通知"]',
+          snapshotUrls: 'https://i.gkd.li/import/13837961',
+        },
+      ],
+    },
   ],
 });
