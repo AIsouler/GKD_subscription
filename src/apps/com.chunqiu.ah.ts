c4fcfeaea72e857eff103e4e03e3c0c74e434bda
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 21:28:07 +0800

    feat(AH视频): 公告弹窗、推广弹窗、广告弹窗、播放页广告 (#3777)
    * feat(AH视频):公告弹窗、推广弹窗、广告弹窗、播放页广告
    
    * chore(actions): check_format_lint
    
    * fix(AH视频):广告弹窗修复，播放页面补充规则
    
    * chore(actions): check_format_lint
    
    * fix:补充key值
    
    * feat:修改规则名称
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/apps/com.chunqiu.ah.ts
+++ src/apps/com.chunqiu.ah.ts
@@ -27,7 +27,7 @@ export default defineAppConfig({
     },
     {
       key: 1,
-      name: '弹窗广告',
+      name: '全屏广告-弹窗广告',
       quickFind: true,
       activityIds: 'com.androlua.LuaActivity',
       rules: [
@@ -39,7 +39,7 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: '卡片式广告',
+      name: '局部广告-卡片式广告',
       rules: [
         {
           key: 0,
@@ -48,6 +48,72 @@ export default defineAppConfig({
           matches:
             'ImageView < FrameLayout -2 FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
           snapshotUrls: 'https://i.gkd.li/import/13635499',
+        },
+      ],
+    },
+    {
+      key: 3,
+      quickFind: true,
+      name: '全屏广告-首页推广',
+      desc: '点击我已知晓',
+      rules: [
+        {
+          activityIds: 'com.androlua.LuaActivity',
+          matches:
+            '[id="com.chunqiu.ah:id/buttonPanel"] [text="我已知晓"][id="android:id/button2"]',
+          snapshotUrls: 'https://i.gkd.li/import/13852430',
+        },
+      ],
+    },
+    {
+      key: 4,
+      quickFind: true,
+      name: '全屏广告-公告',
+      desc: '点击不再提示',
+      rules: [
+        {
+          activityIds: 'com.androlua.LuaActivity',
+          matches: '[id="com.chunqiu.ah:id/buttonPanel"] [text="不再提示"]',
+          snapshotUrls: 'https://i.gkd.li/import/13852447',
+        },
+      ],
+    },
+    {
+      key: 5,
+      quickFind: true,
+      name: '全屏广告-首页广告弹窗',
+      desc: '点击X',
+      rules: [
+        {
+          activityIds: 'com.androlua.LuaActivity',
+          matches:
+            '@ImageView[visibleToUser=true] < * +3 * > [text="查看详情"]',
+          snapshotUrls: 'https://i.gkd.li/import/13852448',
+        },
+      ],
+    },
+    {
+      key: 6,
+      name: '局部广告-播放界面广告',
+      desc: '点击X',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.androlua.LuaActivity',
+          matches:
+            '[id="com.chunqiu.ah:id/ksad_container"] >n View[id="logoBox"] > TextView[clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13852535',
+        },
+        {
+          key: 1,
+          activityIds: 'com.androlua.LuaActivity',
+          matches:
+            '[id="android:id/content"] FrameLayout[childCount<=5] > FrameLayout[childCount=1] > ImageView[visibleToUser=true][index=0]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13852695',
+            'https://i.gkd.li/import/13852670',
+            'https://i.gkd.li/import/13852669',
+          ],
         },
       ],
     },
