1ea628b5fa7b522feeec2722b79236e1e08084a8
adproqwq <101449500+adproqwq@users.noreply.github.com>
2024-01-14 20:20:34 +0800

    feat(默往): 开屏广告 (#3715)
    close #3713

---
+++ src/apps/com.mostone.life.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.mostone.life',
+  name: '默往',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules: '[id="android:id/content"] >n FrameLayout[childCount=4] > View',
+      snapshotUrls: 'https://i.gkd.li/import/13832104',
+    },
+  ],
+});
