6af96469818211e73f04481ea71d87e5e0a8d09f
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 22:51:47 +0800

    feat(人卫):抽奖弹窗 (#3828)
    * feat(人卫):抽奖弹窗
    
    * chore(actions): check_format_lint
    
    * Update com.pmph.irenwei.ts
    
    Signed-off-by: 二刺螈 <i@songe.li>
    
    ---------
    
    Signed-off-by: 二刺螈 <i@songe.li>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>

---
+++ src/apps/com.pmph.irenwei.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.pmph.irenwei',
+  name: '人卫',
+  groups: [
+    {
+      key: 1,
+      quickFind: true,
+      name: '全屏广告-抽奖弹窗',
+      desc: '点击X',
+      rules: [
+        {
+          activityIds: 'com.pmph.main.ad.AdActivity',
+          matches:
+            '[id="com.pmph.irenwei:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13897421',
+        },
+      ],
+    },
+  ],
+});
