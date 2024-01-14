2917eafcf45ff4755592bf34d178b937a5b7bf8b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-01-14 20:25:25 +0800

    feat(数字人民币): 更新提示 (#3736)

---
+++ src/apps/cn.gov.pbc.dcep.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.gov.pbc.dcep',
+  name: '数字人民币',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[vid="upgrade_dialog_cancel"]',
+      snapshotUrls: 'https://i.gkd.li/import/13840408',
+    },
+  ],
+});
