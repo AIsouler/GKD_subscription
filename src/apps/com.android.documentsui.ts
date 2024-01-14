e01923aacacd8ad6c269365116c945bfff52ccfe
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 22:54:56 +0800

    feat(文件):使用此文件夹 (#3839)
    * feat(文件):使用此文件夹
    
    * feat:修改分类

--- src/apps/com.android.documentsui.ts
+++ src/apps/com.android.documentsui.ts
@@ -6,10 +6,22 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '允许第三方应用访问文件夹',
+      name: '功能类-允许第三方应用访问文件夹',
       activityIds: 'com.android.documentsui.picker.PickActivity',
       rules: '[text="取消"] + [text="允许"]',
       snapshotUrls: 'https://i.gkd.li/import/13801468',
+    },
+    {
+      key: 2,
+      quickFind: true,
+      name: '功能类-使用此文件夹',
+      rules: [
+        {
+          activityIds: 'com.android.documentsui.picker.PickActivity',
+          matches: '[text="使用此文件夹"]',
+          snapshotUrls: 'https://i.gkd.li/import/13916082',
+        },
+      ],
     },
   ],
 });
