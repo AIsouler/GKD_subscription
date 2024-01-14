cb2643816deb6f16997d67f486ed933d245beb1d
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-14 21:59:11 +0800

    feat(飞常准业内版):开屏广告 (#3853)
    close #3850

---
+++ src/apps/com.feeyo.vz.pro.cdm.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.feeyo.vz.pro.cdm',
+  name: '飞常准业内版',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      desc: '点击跳过',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          matches:
+            '@View <3 FrameLayout[childCount=4] <2 FrameLayout[childCount=2] < FrameLayout < [vid="splashAdContainer"]',
+          snapshotUrls: 'https://i.gkd.li/import/13926823',
+        },
+      ],
+    },
+  ],
+});
