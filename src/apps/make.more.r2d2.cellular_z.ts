b5a5bb8dd25c31ed2ac28dafe31d12aa8927020f
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2024-01-28 13:39:18 +0800

    feat(Cellular-Z): 开屏广告 (#3981)
    close #3978

--- src/apps/make.more.r2d2.cellular_z.ts
+++ src/apps/make.more.r2d2.cellular_z.ts
@@ -5,8 +5,39 @@ export default defineAppConfig({
   name: 'Cellular-Z',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      resetMatch: 'app',
+      actionMaximum: 1,
+      matchTime: 10000,
+      actionCdKey: 0,
+      actionMaximumKey: 0,
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          name: '全局规则-1',
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13987038',
+        },
+        {
+          key: 1,
+          name: '字节广告SDK',
+          matches:
+            'TextView - @View[clickable=true] <n FrameLayout <2 FrameLayout[childCount=2]', // 字节开屏通用规则
+          snapshotUrls: 'https://i.gkd.li/import/13978978',
+        },
+        {
+          key: 2,
+          name: '全局规则-2',
+          matches:
+            '[childCount=0][visibleToUser=true][(text.length<10&&(text*="跳过"||text*="跳過"||text*="skip"||text*="Skip")) || id$="tt_splash_skip_btn" || vid*="skip" || vid*="Skip" || (vid*="count" && vid*="down" && vid!*="download") || desc*="跳过" || desc*="skip"]',
+        },
+      ],
+    },
+    {
       key: 2,
-      name: '更新弹窗',
+      name: '更新提示',
       resetMatch: 'app',
       actionMaximum: 1,
       matchTime: 10000,
