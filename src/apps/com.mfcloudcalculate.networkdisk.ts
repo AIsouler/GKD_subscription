81dd83c6bd16cc56d9525df73e3eced77ea9b7fc
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-28 13:42:40 +0800

    feat(123云盘):开屏广告 (#3914)
    * feat(123云盘):开屏广告
    
    * chore(actions): check_format_lint
    
    * feat(123云盘):开屏广告
    
    * Update com.mfcloudcalculate.networkdisk.ts
    
    * Update com.mfcloudcalculate.networkdisk.ts
    
    Signed-off-by: 二刺螈 <i@songe.li>
    
    * chore(actions): check_format_lint
    
    ---------
    
    Signed-off-by: 二刺螈 <i@songe.li>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 二刺螈 <i@songe.li>

--- src/apps/com.mfcloudcalculate.networkdisk.ts
+++ src/apps/com.mfcloudcalculate.networkdisk.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '应用内广告弹窗',
+      name: '全屏广告-应用内广告弹窗',
       activityIds: 'com.mfcloudcalculate.networkdisk.activity.AdFreeActivity',
       matchTime: 10000,
       actionMaximum: 1,
@@ -15,6 +15,43 @@ export default defineAppConfig({
       rules:
         '[text*="关闭"][id="com.mfcloudcalculate.networkdisk:id/tv_ad_free_colse"]',
       snapshotUrls: 'https://i.gkd.li/import/13546173',
+    },
+    {
+      key: 2,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          matches:
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView <<n [id="com.mfcloudcalculate.networkdisk:id/frame_ad_splash_container"]',
+          snapshotUrls: 'https://i.gkd.li/import/14018247',
+        },
+        // 该应用存在特殊开屏广告，被全局规则排除，以下为之前的开屏广告规则
+        {
+          key: 1,
+          quickFind: true,
+          matches:
+            '[id="com.mfcloudcalculate.networkdisk:id/frame_ad_splash_container"] >n [text^="跳过"][text.length<=4]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12846434',
+            'https://i.gkd.li/import/13059834',
+          ],
+        },
+        {
+          key: 2,
+          quickFind: true,
+          matches:
+            '[id$="tt_splash_skip_btn"] <<n [id="com.mfcloudcalculate.networkdisk:id/frame_ad_splash_container"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13259303',
+            'https://i.gkd.li/import/13695497',
+          ],
+        },
+      ],
     },
   ],
 });
