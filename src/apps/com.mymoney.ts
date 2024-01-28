74c960eee201fa175bc6f859218164e27bf707c1
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-01-28 13:43:15 +0800

    feat(随手记): 开屏广告（补充规则） (#3902)

--- src/apps/com.mymoney.ts
+++ src/apps/com.mymoney.ts
@@ -14,9 +14,18 @@ export default defineAppConfig({
       desc: '点击跳过',
       rules: [
         {
+          key: 0,
           matches:
             '@View[clickable=true] <3 * <2 * < FrameLayout[id="com.mymoney:id/external_ad_content"]',
           snapshotUrls: 'https://i.gkd.li/import/13855760',
+        },
+        {
+          key: 1, //全局规则将该应用排除
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13939216',
+            'https://i.gkd.li/import/13939163',
+          ],
         },
       ],
     },
