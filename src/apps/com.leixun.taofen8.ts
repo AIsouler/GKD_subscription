1e8472971dcecd3a1c14f04ead28fba5ef9125ba
AIsouler <57941037+AIsouler@users.noreply.github.com>
2024-01-28 13:43:32 +0800

    feat(淘粉吧):开屏广告  (#3904)
    close #3897

---
+++ src/apps/com.leixun.taofen8.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.leixun.taofen8',
+  name: '淘粉吧',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          quickFind: true,
+          matches:
+            '@View[clickable=true] <<n [id="com.leixun.taofen8:id/fl_advert_container"]',
+          snapshotUrls: 'https://i.gkd.li/import/13937324',
+        },
+      ],
+    },
+  ],
+});
