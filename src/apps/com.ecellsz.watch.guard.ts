9c0ebad1c16bad217ef99c3864e6485e5ced0c65
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-01-28 13:22:23 +0800

    feat(乐康守护): 开屏广告 (#4067)
    close #4066

---
+++ src/apps/com.ecellsz.watch.guard.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.ecellsz.watch.guard',
+  name: '乐康守护',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      quickFind: true,
+      rules: 'View[clickable=true] <<n [vid="splash_container"]',
+      snapshotUrls: 'https://i.gkd.li/import/14039510',
+    },
+  ],
+});
