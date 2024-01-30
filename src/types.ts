48d5d5d4add2f4e25c5b2f395f49412f968f1a92
lisonge <i@songe.li>
2024-01-30 16:08:15 +0800

    chore: docs

--- src/types.ts
+++ src/types.ts
@@ -244,11 +244,12 @@ type RawGroupProps = RawCommonProps & {
   /**
    * 其它 group 的 key, 允许将目标组的所有 rule 添加到当前组的作用域
    *
-   * 假设 group1{key=1} 有一个 rule1{key=1}, group2{key=2} 有一个 rule2{key=2}
+   * 假设 group1{key=1} 有一个 rule1{key=11}, group2{key=2} 有 rule2{key=22}, rule3{key=23}
    *
-   * 如果 group1 的 scopeKeys=[2], 那么 rule1 的 preKeys/actionCdKey/actionMaximumKey 可以是 2
+   * 如果 group1 的 scopeKeys=[2] 并且 group2 没有被禁用, 那么 rule1 的 preKeys/actionCdKey/actionMaximumKey 可以是 11/22/23
    *
    * 如果存在相同 key 的 rule, 优先使用本组的 rule, 其次按 scopeKeys 的顺序查找其它组的 rule
+   *
    */
   scopeKeys?: IArray<number>;
 
