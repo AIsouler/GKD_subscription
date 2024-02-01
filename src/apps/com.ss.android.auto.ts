4da0873467bcceadd072ad4ee706483bbea6a30e
aisouler <zy43220@gmail.com>
2024-02-01 18:40:19 +0800

    Squashed commit of the following:
    commit 7ad0346e9dc91914d7224a8470b951e1390fe430
    Author: aisouler <zy43220@gmail.com>
    Date:   Fri Jan 26 14:36:57 2024 +0800
    
        feat:通知提示（优化规则）
    
    commit 1ec07195058b182be3cfef4bdd8ab986d73853ed
    Author: aisouler <zy43220@gmail.com>
    Date:   Wed Jan 24 15:40:52 2024 +0800
    
        feat(懂车帝):车型页面右下角直播悬浮窗、规则分类
    
    commit 5d7fdb866ba4ed39529feda9aa82be2a7dd455f4
    Author: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Date:   Wed Jan 17 07:41:25 2024 +0000
    
        chore(actions): check_format_lint
    
    commit 5a37f8708d64835b22578282b88f7450bbdff68b
    Author: aisouler <zy43220@gmail.com>
    Date:   Wed Jan 17 15:40:43 2024 +0800
    
        feat(懂车帝):分段广告（补充activityid）、规则修复

--- src/apps/com.ss.android.auto.ts
+++ src/apps/com.ss.android.auto.ts
@@ -7,10 +7,13 @@ export default defineAppConfig({
     {
       enable: false,
       key: 1,
-      name: '首页推荐卡片广告',
+      name: '分段广告-首页推荐卡片广告',
       desc: '需二次点击关闭原因',
       quickFind: true,
-      activityIds: 'com.ss.android.auto.activity.SplashActivity',
+      activityIds: [
+        'com.ss.android.auto.activity.SplashActivity',
+        'com.ss.android.auto.policy.AutoPrivacyActivity',
+      ],
       rules: [
         {
           key: 0,
@@ -18,26 +21,32 @@ export default defineAppConfig({
           matches:
             'TextView[text.length=1][id=null][clickable=false] < @FrameLayout[clickable=true][id!=null] - LinearLayout > [text="广告"]',
           // 貌似快照存在延迟导致屏幕与节点不对应
-          snapshotUrls: 'https://i.gkd.li/import/12660816',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12660816',
+            'https://i.gkd.li/import/13959610',
+          ],
         },
         {
           // 不与旧版本合并，尽量使用[clickable=true]，保证速度避免误触
-          preKeys: 1,
+          preKeys: 0,
           name: '点击【不感兴趣】1', // 懂车帝v7.8.4样式
           matches: '@ViewGroup[clickable=true] TextView[text="不感兴趣"]',
           snapshotUrls: 'https://i.gkd.li/import/13538627',
         },
         {
-          preKeys: 1,
-          name: '点击【不感兴趣】2', // 懂车帝v7.7.4样式
+          preKeys: 0,
+          name: '点击【不感兴趣】2', // 懂车帝v7.7.4样式、v7.8.0样式
           matches: '[text="不感兴趣"][clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/import/12711589',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12711589',
+            'https://i.gkd.li/import/13959613', //v7.8.0样式
+          ],
         },
       ],
     },
     {
       key: 2,
-      name: '升级弹窗',
+      name: '更新提示',
       quickFind: true,
       actionMaximum: 1,
       resetMatch: 'app',
@@ -51,7 +60,7 @@ export default defineAppConfig({
     },
     {
       key: 3,
-      name: '右下角悬浮窗',
+      name: '局部广告-右下角悬浮窗',
       quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
@@ -71,7 +80,7 @@ export default defineAppConfig({
     },
     {
       key: 4,
-      name: '文章底部卡片式广告',
+      name: '局部广告-文章底部卡片式广告',
       quickFind: true,
       activityIds:
         'com.ss.android.article.base.feature.detail2.view.NewDetailActivity',
@@ -81,7 +90,7 @@ export default defineAppConfig({
     },
     {
       key: 5,
-      name: '评论区信息流广告',
+      name: '局部广告-评论区信息流广告',
       desc: '像正常内容的广告',
       quickFind: true,
       activityIds: [
@@ -99,25 +108,42 @@ export default defineAppConfig({
     },
     {
       key: 6,
-      name: '请求推送通知弹窗',
+      name: '通知提示',
       quickFind: true,
       actionMaximum: 1,
       resetMatch: 'app',
       activityIds: [
         'com.ss.android.auto.activity.SplashActivity',
         'com.ss.android.auto.policy.AutoPrivacyActivity',
+        'com.bytedance.im.auto.conversation.activity.ConversationListActivity', // https://i.gkd.li/import/14060897
       ],
-      rules: '@TextView[clickable=true] + [text^="打开推送通知"]',
-      snapshotUrls: 'https://i.gkd.li/import/12840664',
+      rules: '@TextView[clickable=true] + [text*="推送通知"]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12840664',
+        'https://i.gkd.li/import/14060897',
+      ],
     },
     {
       key: 7,
-      name: '选车页卡片广告',
+      name: '局部广告-选车页卡片广告',
       quickFind: true,
       activityIds: 'com.ss.android.auto.activity.ConcernDetailActivity',
       rules:
         'TextView[text.length=1][id=null][clickable=false] < @FrameLayout[clickable=true][id!=null] -2 [text="广告"]',
       snapshotUrls: 'https://i.gkd.li/import/13686928',
+    },
+    {
+      key: 8,
+      name: '局部广告-车型页面右下角直播悬浮窗',
+      desc: '点击X',
+      quickFind: true,
+      rules: [
+        {
+          activityIds: 'com.ss.android.auto.activity.ConcernDetailActivity',
+          matches: 'FrameLayout[childCount=8] > TextView[text=""]',
+          snapshotUrls: 'https://i.gkd.li/import/14034740',
+        },
+      ],
     },
   ],
 });
