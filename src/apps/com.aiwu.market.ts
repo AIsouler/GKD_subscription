9407f5942206905df90428bf7bc6b4a386b4ad2f
aisouler <zy43220@gmail.com>
2024-02-01 18:25:54 +0800

    Squashed commit of the following:
    commit 77922241af95a310fa60fd20bb1dc8e4f3ea1f25
    Author: aisouler <zy43220@gmail.com>
    Date:   Wed Jan 24 13:55:30 2024 +0800
    
        feat(爱吾游戏宝盒):青少年模式

---
+++ src/apps/com.aiwu.market.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.aiwu.market',
+  name: '爱吾游戏宝盒',
+  groups: [
+    {
+      key: 1,
+      name: '青少年模式',
+      desc: '点击"我知道了"',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.aiwu.market.ui.activity.NewHomeActivity',
+          matches: '[vid="hintTextView2"] + [vid="confirmView"]',
+          snapshotUrls: 'https://i.gkd.li/import/14032203',
+        },
+      ],
+    },
+  ],
+});
