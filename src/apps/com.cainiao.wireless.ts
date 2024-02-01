c15ae2f9e3c18b5963ad902f1cf4e3adef4d691b
aisouler <zy43220@gmail.com>
2024-02-01 18:29:07 +0800

    Squashed commit of the following:
    commit a2070169a5277890ce9780ed3c89bf26aff525a6
    Author: aisouler <zy43220@gmail.com>
    Date:   Fri Jan 26 16:36:45 2024 +0800
    
        Update com.cainiao.wireless.ts
    
    commit 84f9ce7ef6fe26da8c09cd90d35079c3c8db5ccb
    Author: aisouler <zy43220@gmail.com>
    Date:   Wed Jan 24 14:32:37 2024 +0800
    
        feat:规则分类
    
    commit df733e46a8c6019d3e8d7ee9910de791641f3dfd
    Author: aisouler <zy43220@gmail.com>
    Date:   Wed Jan 24 14:29:06 2024 +0800
    
        feat(菜鸟):发现页新装试用弹窗

--- src/apps/com.cainiao.wireless.ts
+++ src/apps/com.cainiao.wireless.ts
@@ -7,7 +7,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '弹窗广告',
+      name: '全屏广告-弹窗广告',
       rules: [
         {
           key: 0,
@@ -32,7 +32,7 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: '包裹页面-地图底部横条广告',
+      name: '局部广告-包裹页面-地图底部横条广告',
       activityIds: [
         'com.taobao.cainiao.logistic.ui.view.LogisticDetailActivity',
       ],
@@ -41,7 +41,7 @@ export default defineAppConfig({
     },
     {
       key: 4,
-      name: '升级提示',
+      name: '更新提示',
       enable: false,
       quickFind: true,
       activityIds: [
@@ -52,7 +52,7 @@ export default defineAppConfig({
     },
     {
       key: 5,
-      name: '开启消息弹窗',
+      name: '通知提示',
       desc: '自动点击关闭。',
       enable: false,
       quickFind: true,
@@ -64,7 +64,7 @@ export default defineAppConfig({
     },
     {
       key: 6,
-      name: '请求好评弹窗',
+      name: '评价提示',
       activityIds: 'com.taobao.cainiao.logistic.ui.view.LogisticDetailActivity',
       rules: '@[text="取消"] + [text="好评"]',
       snapshotUrls: 'https://i.gkd.li/import/13692761',
@@ -78,6 +78,19 @@ export default defineAppConfig({
         'com.cainiao.wireless.homepage.view.activity.HomePageActivity',
       rules: '[vid="dialog_full_image_close"]',
       snapshotUrls: 'https://i.gkd.li/import/13842492',
+    },
+    {
+      key: 8,
+      name: '全屏广告-发现页新装试用弹窗',
+      desc: '点击X',
+      rules: [
+        {
+          activityIds:
+            'com.cainiao.wireless.homepage.view.activity.HomePageActivity',
+          matches: 'View[childCount=2][clickable=true] > [text="关闭"]',
+          snapshotUrls: 'https://i.gkd.li/import/14033859',
+        },
+      ],
     },
   ],
 });
