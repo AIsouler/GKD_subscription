cfaf6e4d0a5fab247f77bbbe345b3ff1c08e175d
aisouler <zy43220@gmail.com>
2024-02-01 18:34:27 +0800

    Squashed commit of the following:
    commit 743407ca6d030babd938211776eaf31c0782c09f
    Author: aisouler <zy43220@gmail.com>
    Date:   Thu Jan 25 15:31:27 2024 +0800
    
        feat(RayLink):功能类-退出软件时点击"退出程序"

--- src/apps/com.example.raylink_flutter.ts
+++ src/apps/com.example.raylink_flutter.ts
@@ -14,5 +14,17 @@ export default defineAppConfig({
       rules: '[desc="立即升级"] + [desc="以后再说"]',
       snapshotUrls: 'https://i.gkd.li/import/13659530',
     },
+    {
+      key: 2,
+      name: '功能类-退出软件时点击"退出程序"',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.example.raylink_flutter.MainActivity',
+          matches: '[vid="raylink_desktop_dialog_msg_bottombtn_left_tv"]',
+          snapshotUrls: 'https://i.gkd.li/import/14047538',
+        },
+      ],
+    },
   ],
 });
