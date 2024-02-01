6c3de8bc25f7e33ab06da829f13878b686709a93
aisouler <zy43220@gmail.com>
2024-02-01 18:36:42 +0800

    Squashed commit of the following:
    commit 997076346ad74027a388a8c7330bd75a432d7749
    Merge: 7f41b14c 53a0a876
    Author: aisouler <zy43220@gmail.com>
    Date:   Wed Jan 24 15:31:53 2024 +0800
    
        Merge branch 'com.lingan.seeyou' of https://github.com/AIsouler/subscription into com.lingan.seeyou
    
    commit 7f41b14c1c2a89159c08325ef087d196645b48e5
    Author: aisouler <zy43220@gmail.com>
    Date:   Wed Jan 24 15:31:37 2024 +0800
    
        Update com.lingan.seeyou.ts
    
    commit 53a0a876bbfdc8c9a2d60f55c14b57c4d079dffb
    Author: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Date:   Wed Jan 24 07:31:32 2024 +0000
    
        chore(actions): check_format_lint
    
    commit 1e0a359758ad7e23b1ff965671798755d4a6a01f
    Author: aisouler <zy43220@gmail.com>
    Date:   Wed Jan 24 15:30:55 2024 +0800
    
        feat(美柚):评论区上方卡片广告

---
+++ src/apps/com.lingan.seeyou.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.lingan.seeyou',
+  name: '美柚',
+  groups: [
+    {
+      key: 1,
+      name: '局部广告-评论区上方卡片广告',
+      desc: '点击X',
+      quickFind: true,
+      rules: [
+        {
+          activityIds:
+            'com.lingan.seeyou.ui.activity.community.topicdetail.TopicDetailActivity',
+          matches: '[id="com.lingan.seeyou:id/v_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/14034770',
+        },
+      ],
+    },
+  ],
+});
