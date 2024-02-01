96e85cd56b3251cc360ffcb0cbaeaf8c17f7f83a
aisouler <zy43220@gmail.com>
2024-02-01 18:36:18 +0800

    Squashed commit of the following:
    commit df3bbc4aa23659b5a80c876697651fb09207a674
    Author: aisouler <zy43220@gmail.com>
    Date:   Thu Jan 25 21:14:19 2024 +0800
    
        feat(识货):更新提示、规则分类

--- src/apps/com.hupu.shihuo.ts
+++ src/apps/com.hupu.shihuo.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 2,
-      name: '请求消息通知',
+      name: '通知提示-请求消息通知',
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
@@ -17,13 +17,29 @@ export default defineAppConfig({
     },
     {
       key: 3,
-      name: '广告弹窗',
+      name: '全屏广告-广告弹窗',
       desc: '点击左上角x关闭',
       activityIds:
         'com.shizhuang.duapp.modules.rn.mini.MiniReactActivity$MiniUITranslucentReactActivity',
       rules:
         '[id="com.hupu.shihuo:id/fragment_container"] >11 @ViewGroup[clickable=true] > ImageView',
       snapshotUrls: 'https://i.gkd.li/import/13115664',
+    },
+    {
+      key: 4,
+      name: '更新提示',
+      desc: '点击"以后再说"',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          activityIds: 'com.shizhi.shihuoapp.module.main.ui.main.MainActivity',
+          matches: '[text="发现新版本"] +2 ViewGroup > [text="以后再说"]',
+          snapshotUrls: 'https://i.gkd.li/import/14052208',
+        },
+      ],
     },
   ],
 });
