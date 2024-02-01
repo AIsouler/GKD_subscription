e9be03eee26f60feac592ff45757a9aca2605768
aisouler <zy43220@gmail.com>
2024-02-01 18:34:42 +0800

    Squashed commit of the following:
    commit 7bebfd467330ee1fe52b1019503d2639ac2e7042
    Author: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Date:   Wed Jan 24 08:51:25 2024 +0000
    
        chore(actions): check_format_lint
    
    commit 4fd13f62eae96319af32a3c136fb67698416d87f
    Author: aisouler <zy43220@gmail.com>
    Date:   Wed Jan 24 16:50:50 2024 +0800
    
        feat(影视):更新提示

---
+++ src/apps/com.fongmi.android.tv.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.fongmi.android.tv',
+  name: '影视',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.fongmi.android.tv.ui.activity.MainActivity',
+          matches:
+            '[id="com.fongmi.android.tv:id/buttonPanel"] >2 [text="取消"]',
+          snapshotUrls: 'https://i.gkd.li/import/14035154',
+        },
+      ],
+    },
+  ],
+});
