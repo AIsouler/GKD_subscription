e03dc312c9b2ecc049a9dca73b2ce8c3c99729e1
aisouler <zy43220@gmail.com>
2024-02-01 18:27:22 +0800

    Squashed commit of the following:
    commit 61efb8dbbbc7f0c267da3180d329fc9bb9cc2cae
    Author: aisouler <zy43220@gmail.com>
    Date:   Thu Jan 25 14:05:27 2024 +0800
    
        feat(应用安装器):优化规则
    
    commit 480b93bc17ea684595d53f0a59f9efa8ad7c3493
    Author: Adpro <101449500+adproqwq@users.noreply.github.com>
    Date:   Fri Jan 19 20:58:45 2024 +0800
    
        update: 合并规则
    
    commit 09aff09868536604448a7827417d20191c9c2217
    Author: adproqwq <adpro_qwq@outlook.com>
    Date:   Wed Jan 17 20:48:41 2024 +0800
    
        fix: 无法点击完成
    
    commit 5e9f900c7788c3020c8023fd5aba9cfcbe5343ae
    Author: adproqwq <adpro_qwq@outlook.com>
    Date:   Wed Jan 17 19:35:04 2024 +0800
    
        feat(软件包安装程序):  功能类-自动安装应用（优化规则）

--- src/apps/com.android.packageinstaller.ts
+++ src/apps/com.android.packageinstaller.ts
@@ -2,33 +2,35 @@ import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
   id: 'com.android.packageinstaller',
-  name: 'vivo 软件包安装程序',
+  name: '软件包安装程序',
   groups: [
     {
       key: 1,
-      name: '自动安装应用',
+      name: '功能类-自动安装应用',
       quickFind: true,
       rules: [
         {
           key: 0,
           activityIds: 'com.android.packageinstaller.PackageInstallerActivity',
-          matches: '@LinearLayout > [text="继续安装"]',
-          snapshotUrls: 'https://i.gkd.li/import/13206444',
+          matches: '[text="继续安装"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13206444',
+            'https://i.gkd.li/import/14046749',
+          ],
         },
         {
           key: 1,
-          preKeys: 0,
           activityIds: 'com.android.packageinstaller.NewInstallInstalling',
-          matches: '@[clickable=true][childCount=1] > [text="完成"]',
-          snapshotUrls: 'https://i.gkd.li/import/13206476',
-        },
-        {
-          key: 2,
-          preKeys: 0,
-          activityIds: 'com.android.packageinstaller.NewInstallInstalling',
-          matches:
-            '[id="com.android.packageinstaller:id/done_button"][text="完成"][clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/import/13766420',
+          matches: [
+            '@[clickable=true][childCount=1] > [text="完成"]',
+            '[id="com.android.packageinstaller:id/done_button"][clickable=true]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/import/13206476', //规则1
+            //规则2
+            'https://i.gkd.li/import/13766420',
+            'https://i.gkd.li/import/13962438', //text=null
+          ],
         },
       ],
     },
