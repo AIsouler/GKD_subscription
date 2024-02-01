633a63b30e822ba006dbc4e6ee089ed15b7d4488
aisouler <zy43220@gmail.com>
2024-02-01 18:44:14 +0800

    Squashed commit of the following:
    commit ca59f92fae9ba941fe22907736dd2f4df4d0faac
    Author: aisouler <zy43220@gmail.com>
    Date:   Fri Jan 19 19:59:05 2024 +0800
    
        feat(亿通行):出站后摇一摇广告

---
+++ src/apps/enfc.metro.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'enfc.metro',
+  name: '亿通行',
+  groups: [
+    {
+      key: 1,
+      quickFind: true,
+      name: '全屏广告-出站后摇一摇广告',
+      desc: '点击跳过',
+      rules: [
+        {
+          activityIds: 'enfc.metro.session.web.rn.RPReactActivity',
+          matches: '@View[clickable=true] - * [text="扭动手机"]',
+          snapshotUrls: 'https://i.gkd.li/import/13988597',
+        },
+      ],
+    },
+  ],
+});
