e3519fc9885d58162372656a91e201c42a244b0d
aisouler <zy43220@gmail.com>
2024-02-01 18:38:33 +0800

    Squashed commit of the following:
    commit 51b0f7081f7329e7311fec9e837a742c77af5a01
    Author: aisouler <zy43220@gmail.com>
    Date:   Wed Jan 17 13:20:59 2024 +0800
    
        feat(滴滴车主):全屏广告-广告弹窗

---
+++ src/apps/com.sdu.didi.gsui.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.sdu.didi.gsui',
+  name: '滴滴车主',
+  groups: [
+    {
+      key: 1,
+      quickFind: true,
+      name: '全屏广告-广告弹窗',
+      desc: '点击X',
+      rules: [
+        {
+          activityIds: 'com.sdu.didi.gsui.main.MainActivity',
+          matches: '[id="com.sdu.didi.gsui:id/main_dialog_banner_image_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13958887',
+        },
+      ],
+    },
+  ],
+});
