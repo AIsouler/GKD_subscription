594cc82c33503f2217604e594cfc6efc1e7cf28a
aisouler <zy43220@gmail.com>
2024-02-01 18:28:49 +0800

    Squashed commit of the following:
    commit 99b9d3a6d44f889be469cc285c3b9ad92e745382
    Author: aisouler <zy43220@gmail.com>
    Date:   Sat Jan 27 18:46:07 2024 +0800
    
        feat(糖豆):更新提示

---
+++ src/apps/com.bokecc.dance.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.bokecc.dance',
+  name: '糖豆',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      desc: '点击"以后再说"',
+      quickFind: true,
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules: [
+        {
+          activityIds: 'com.bokecc.dance.activity.MainActivity',
+          matches: '[id="com.bokecc.dance:id/tv_cancel"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/9156698c-76bb-4921-a39d-96a20c45260b',
+          snapshotUrls: 'https://i.gkd.li/import/14071977',
+        },
+      ],
+    },
+  ],
+});
