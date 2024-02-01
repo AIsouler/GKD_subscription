79a91bca4463273984fc3350a0c0df585669bc6d
aisouler <zy43220@gmail.com>
2024-02-01 18:28:32 +0800

    Squashed commit of the following:
    commit c9b0031fa43b5317cae153dd23d0bf323894f6f9
    Author: aisouler <zy43220@gmail.com>
    Date:   Wed Jan 24 15:01:24 2024 +0800
    
        feat(百度):红包弹窗（补充规则）
    
    commit d4456ad47a80d87566d896057452f818a83340c8
    Author: aisouler <zy43220@gmail.com>
    Date:   Fri Jan 19 19:50:08 2024 +0800
    
        feat(百度):红包弹窗（补充规则）

--- src/apps/com.baidu.searchbox.ts
+++ src/apps/com.baidu.searchbox.ts
@@ -7,13 +7,24 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '红包弹窗',
+      name: '全屏广告-红包弹窗',
       desc: '点击关闭',
       rules: [
         {
           activityIds: 'com.baidu.searchbox.MainActivity',
           matches: '[text^="看视频"] -2 ImageView[index=4]',
           snapshotUrls: 'https://i.gkd.li/import/13806848',
+        },
+        {
+          activityIds: 'com.baidu.searchbox.MainActivity',
+          matches:
+            '[id="android:id/content"] >2 LinearLayout[childCount=2] > ImageView[index=1]',
+          snapshotUrls: 'https://i.gkd.li/import/13988536',
+        },
+        {
+          activityIds: 'com.baidu.searchbox.MainActivity',
+          matches: 'ViewGroup[childCount=7] > ImageView[index=1]',
+          snapshotUrls: 'https://i.gkd.li/import/14034223',
         },
       ],
     },
