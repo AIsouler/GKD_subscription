05f5a2c281b3aafa3d2d3a0de04076e0bf65c80d
aisouler <zy43220@gmail.com>
2024-02-01 18:43:43 +0800

    Squashed commit of the following:
    commit 3c20591efd8150ab475173dc07e0112f388c04a7
    Author: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Date:   Thu Jan 18 10:04:04 2024 +0000
    
        chore(actions): check_format_lint
    
    commit 007b06c1f2d790767fa74c57c57770fdf797d733
    Author: aisouler <zy43220@gmail.com>
    Date:   Thu Jan 18 18:03:24 2024 +0800
    
        feat(微音乐):功能类-看视频免广告自动化

--- src/apps/com.zhouyu.music.ts
+++ src/apps/com.zhouyu.music.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '广告弹窗',
+      name: '全屏广告-广告弹窗',
       activityIds: 'com.zhouyu.music.activities.MainActivity',
       rules: '@[id=null][clickable=true] + View >n View [text$="广告"]',
       snapshotUrls: [
@@ -19,13 +19,44 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: '搜索底部广告',
+      name: '局部广告-搜索底部广告',
       quickFind: true,
-
       activityIds: 'com.zhouyu.music.activities.MainActivity',
       rules: '[id="com.zhouyu.music:id/ksad_ad_dislike"]',
       snapshotUrls: [
         'https://i.gkd.li/import/13245859', //launcher
+      ],
+    },
+    {
+      key: 3,
+      quickFind: true,
+      name: '功能类-看视频免广告自动化',
+      desc: '点击静音-倒计时结束后点击X-点击确定',
+      rules: [
+        {
+          key: 0,
+          name: '点击静音',
+          actionMaximum: 1, //由于静音前后控件没有区别，故设置最大触发次数
+          resetMatch: 'app', //之所以不设置activity是因为进入广告时会刷新两次activity，触发两次点击导致静音失效
+          activityIds: 'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          matches: '[id="com.zhouyu.music:id/ksad_video_sound_switch"]',
+          snapshotUrls: 'https://i.gkd.li/import/13974721',
+        },
+        {
+          key: 1,
+          name: '倒计时结束后点击右上角X',
+          activityIds: 'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          matches:
+            '[id="com.zhouyu.music:id/ksad_play_end_top_toolbar"] > [vid="ksad_end_close_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13974632',
+        },
+        {
+          key: 2,
+          name: '点击退出广告后的确定弹窗',
+          activityIds: 'com.zhouyu.music.activities.MainActivity',
+          matches: '[text="免广告成功"] +3 * > [text="确定"]',
+          snapshotUrls: 'https://i.gkd.li/import/13974655',
+        },
       ],
     },
   ],
