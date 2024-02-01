e080e69fd0ed0a4418435dd783bfb45926534fd8
aisouler <zy43220@gmail.com>
2024-02-01 18:26:41 +0800

    Squashed commit of the following:
    commit 74cebc5702685bc49f7b9bbbfe6f4627ff003f77
    Author: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Date:   Sat Jan 20 12:58:51 2024 +0000
    
        chore(actions): check_format_lint
    
    commit 9f6416968091b70959a60cfa98ae237d04d6fb94
    Author: aisouler <zy43220@gmail.com>
    Date:   Sat Jan 20 20:58:14 2024 +0800
    
        feat(全知识):通知提示、局部广告

---
+++ src/apps/com.allhistory.dls.marble.ts
@@ -0,0 +1,38 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.allhistory.dls.marble',
+  name: '全知识',
+  groups: [
+    {
+      key: 1,
+      name: '通知提示-请求通知弹窗',
+      desc: '点击暂不开启',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds:
+            'com.pwrd.dls.marble.other.notification.NotificationDialogActivity',
+          matches: '[vid="v_open"] + [vid="v_not"]',
+          snapshotUrls: 'https://i.gkd.li/import/13997956',
+        },
+      ],
+    },
+    {
+      key: 2,
+      quickFind: true,
+      name: '局部广告-左下角参与答题卡片',
+      desc: '点击X',
+      rules: [
+        {
+          activityIds: 'com.pwrd.dls.marble.moudle.homepage.HomePageActivity',
+          matches: '[id="com.allhistory.dls.marble:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13997954',
+        },
+      ],
+    },
+  ],
+});
