955cc6f9212d18a07d053e67af0bbe4e58de10aa
aisouler <zy43220@gmail.com>
2024-02-01 17:50:12 +0800

    Squashed commit of the following:
    commit b5e14662129a3071241dbd25c623d3214250421d
    Author: aisouler <zy43220@gmail.com>
    Date:   Thu Jan 18 18:14:12 2024 +0800
    
        feat(酷我音乐):酷我会员日弹窗

---
+++ src/apps/cn.kuwo.player.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.kuwo.player',
+  name: '酷我音乐',
+  groups: [
+    {
+      key: 1,
+      quickFind: true,
+      name: '全屏广告-酷我会员日弹窗',
+      rules: [
+        {
+          activityIds: 'cn.kuwo.player.activities.MainActivity',
+          matches: '[id="cn.kuwo.player:id/iv_business_personal_dia_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13974483',
+        },
+      ],
+    },
+  ],
+});
