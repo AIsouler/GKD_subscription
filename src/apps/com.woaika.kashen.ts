d13d548b156c684c8e96ae65099b5868621121c1
aisouler <zy43220@gmail.com>
2024-02-01 18:42:40 +0800

    Squashed commit of the following:
    commit 51977cd8d1bae45464a7fe2b0997cbbe4dc3ff98
    Author: aisouler <zy43220@gmail.com>
    Date:   Sat Jan 27 12:39:19 2024 +0800
    
        feat(我爱卡):定位提示、权限提示

---
+++ src/apps/com.woaika.kashen.ts
@@ -0,0 +1,43 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.woaika.kashen',
+  name: '我爱卡',
+  groups: [
+    {
+      key: 1,
+      name: '定位提示',
+      desc: '点击"暂不"',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.woaika.kashen.ui.WIKHomeActivity',
+          matches: '[id="android:id/button2"][text="暂不"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/ae172e3f-42e2-4de2-8bd5-de7eb624c359',
+          snapshotUrls: 'https://i.gkd.li/import/14070846',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '权限提示-请求位置、存储权限',
+      desc: '点击"我知道了"',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[vid="tvPermissionDescDialogConfirm"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/6faf1f01-5b77-414b-91d8-98dcb5673f7c',
+          snapshotUrls: 'https://i.gkd.li/import/14070836',
+        },
+      ],
+    },
+  ],
+});
