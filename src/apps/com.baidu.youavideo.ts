5bacfe7243d9c18b155a2ca0d3c3b83caa62f604
aisouler <zy43220@gmail.com>
2024-02-02 11:12:49 +0800

    feat(一刻相册):弹窗广告、规则分类

--- src/apps/com.baidu.youavideo.ts
+++ src/apps/com.baidu.youavideo.ts
@@ -7,7 +7,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 2,
-      name: '软件升级提醒',
+      name: '更新提示-软件升级提醒',
       activityIds: ['com.baidu.youavideo.home.view.HomeActivity'],
       quickFind: true,
       rules: [
@@ -20,7 +20,7 @@ export default defineAppConfig({
     },
     {
       key: 3,
-      name: '悬浮窗',
+      name: '局部广告-悬浮窗广告',
       desc: '关闭广告悬浮窗',
       activityIds: ['com.baidu.youavideo.home.view.HomeActivity'],
       quickFind: true,
@@ -33,7 +33,7 @@ export default defineAppConfig({
     },
     {
       key: 4,
-      name: '评价',
+      name: '评价提示',
       desc: '点击下次再说',
       activityIds: ['com.baidu.youavideo.home.view.HomeActivity'],
       quickFind: true,
@@ -46,7 +46,7 @@ export default defineAppConfig({
     },
     {
       key: 5,
-      name: '会员充值提示',
+      name: '全屏广告-会员充值提示',
       desc: '点击x关闭充值提示',
       activityIds: ['com.baidu.youavideo.home.view.HomeActivity'],
       quickFind: true,
@@ -60,21 +60,31 @@ export default defineAppConfig({
     },
     {
       key: 6,
-      name: '弹窗广告',
+      name: '全屏广告-弹窗广告',
       desc: '点击x关闭广告提示',
-      activityIds: ['com.baidu.youavideo.home.view.HomeActivity'],
       quickFind: true,
       rules: [
         {
+          key: 0,
+          activityIds: 'com.baidu.youavideo.home.view.HomeActivity',
           matches:
             '[id="com.baidu.youavideo:id/dialog_home_pop_close_button"] ',
           snapshotUrls: ['https://i.gkd.li/import/13048700'],
+        },
+        {
+          key: 1,
+          activityIds:
+            'com.baidu.youavideo.preview.ui.hometimeline.TimelinePreviewActivity',
+          matches: '[id="com.baidu.youavideo:id/iv_close"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/3aa0e107-e31f-4e5a-8969-1fcd8310281d',
+          snapshotUrls: 'https://i.gkd.li/import/14133595',
         },
       ],
     },
     {
       key: 7,
-      name: '请求开启通知权限弹窗',
+      name: '通知提示-请求开启通知权限弹窗',
       activityIds: 'com.baidu.youavideo.app.ui.HomeActivity',
       quickFind: true,
       rules:
@@ -83,7 +93,7 @@ export default defineAppConfig({
     },
     {
       key: 8,
-      name: '照片-底部浮窗广告',
+      name: '局部广告-照片页底部浮窗广告',
       activityIds: 'com.baidu.youavideo.home.view.HomeActivity',
       quickFind: true,
       rules:
