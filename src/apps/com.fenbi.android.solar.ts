3aab8e728a0d916b67fac86c990e0c8b28a1cbb9
aisouler <zy43220@gmail.com>
2024-02-02 15:20:23 +0800

    feat(小猿搜题):评价提示

--- src/apps/com.fenbi.android.solar.ts
+++ src/apps/com.fenbi.android.solar.ts
@@ -6,8 +6,8 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '升级提示',
-
+      name: '更新提示',
+      matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       quickFind: true,
@@ -18,6 +18,23 @@ export default defineAppConfig({
             '[id="com.fenbi.android.solar:id/img_close"]',
           ],
           snapshotUrls: 'https://i.gkd.li/import/13246056',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '评价提示',
+      desc: '点击"返回"',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          matches: '[id="com.fenbi.android.solar:id/tv_cancel"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/7864769f-542c-4f76-8b90-4d030b53651b',
+          snapshotUrls: 'https://i.gkd.li/import/14136539',
         },
       ],
     },
