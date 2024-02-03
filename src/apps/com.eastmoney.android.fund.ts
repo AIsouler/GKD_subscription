cde2bd39fe051d195dc1bf2d75fcdb552a7eae1e
aisouler <zy43220@gmail.com>
2024-02-03 10:54:17 +0800

    feat(天天基金):全屏广告

--- src/apps/com.eastmoney.android.fund.ts
+++ src/apps/com.eastmoney.android.fund.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '自选页面广告',
+      name: '局部广告-自选页面广告',
       activityIds:
         'com.eastmoney.android.fund.centralis.activity.FundRootActivity',
       rules: [
@@ -17,7 +17,7 @@ export default defineAppConfig({
     {
       enable: false,
       key: 2,
-      name: '自选页面缩写提示信息',
+      name: '功能类-自选页面缩写提示信息',
       activityIds:
         'com.eastmoney.android.fund.centralis.activity.FundRootActivity',
       rules: [
@@ -27,11 +27,28 @@ export default defineAppConfig({
     },
     {
       key: 3,
-      name: '更新弹窗',
+      name: '更新提示',
       actionMaximum: 1,
       resetMatch: 'app',
       rules: '@FrameLayout[clickable=true] - * >2 [desc="立即升级"]',
       snapshotUrls: 'https://i.gkd.li/import/13546927',
+    },
+    {
+      key: 4,
+      name: '全屏广告-广告弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          activityIds:
+            'com.eastmoney.android.fund.centralis.activity.FundRootActivity',
+          matches:
+            '@ImageView < FrameLayout <2 FrameLayout[childCount=2] <<n [id="com.eastmoney.android.fund:id/container"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/21743bbb-010a-410c-8ce1-57710d37722d',
+          snapshotUrls: 'https://i.gkd.li/import/14149452',
+        },
+      ],
     },
   ],
 });
