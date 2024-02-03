21005d9d855b9d231ada45bd80ef669ccbe96c4c
aisouler <zy43220@gmail.com>
2024-02-03 11:04:36 +0800

    feat(剪映):导出界面底部广告（补充规则）

--- src/apps/com.lemon.lv.ts
+++ src/apps/com.lemon.lv.ts
@@ -6,13 +6,22 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '导出界面底部广告',
+      name: '局部广告-导出界面底部广告',
       desc: '关闭完成导出后的下方广告',
       activityIds: 'com.vega.export.edit.view.ExportActivity',
       rules: [
         {
+          key: 0,
           matches: '@FlattenUIImage + LynxAdComponentView',
           snapshotUrls: 'https://i.gkd.li/import/12911010',
+        },
+        {
+          key: 1,
+          matches:
+            '[vid="exportAdContainer"] FrameLayout[childCount=7] > FlattenUIImage[index=6]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/bc46565b-16ac-4415-8165-13d075f28fcf',
+          snapshotUrls: 'https://i.gkd.li/import/14149461',
         },
       ],
     },
