5ff003f3dc185135694a6b13986c9329f7e932a6
aisouler <zy43220@gmail.com>
2024-02-05 00:48:38 +0800

    feat(剪映): 选择素材界面上方广告

--- src/apps/com.lemon.lv.ts
+++ src/apps/com.lemon.lv.ts
@@ -25,5 +25,13 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '局部广告-选择素材界面上方广告',
+      activityIds: 'com.vega.libcutsame.select.view.CutSameSelectMediaActivity',
+      rules:
+        '[vid="gallery_layout_below_category_view"] >3 [text="立即下载"] -n LynxFlattenUI[clickable=true]',
+      snapshotUrls: 'https://i.gkd.li/import/14122865',
+    },
   ],
 });
