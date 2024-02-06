aabb7ba79863ff690da7c769ab3f6496275ce80d
aisouler <zy43220@gmail.com>
2024-02-06 14:47:31 +0800

    feat(ToDesk): 卡片广告（优化规则）

--- src/apps/youqu.android.todesk.ts
+++ src/apps/youqu.android.todesk.ts
@@ -7,18 +7,24 @@ export default defineAppConfig({
     {
       key: 1,
       name: '局部广告-广告卡片',
-      desc: '设备列表和连接界面中的广告卡片',
+      desc: '关闭广告卡片',
+      quickFind: true,
       activityIds: [
         'youqu.android.todesk.activity.WelcomeActivity',
         'youqu.android.todesk.activity.MainActivity', //新增activityIds
       ],
-      quickFind: true,
-      rules:
-        '[id="youqu.android.todesk:id/sivDeviceBanner"] + [id="youqu.android.todesk:id/llClose"]',
-      snapshotUrls: [
-        'https://i.gkd.li/import/13228546',
-        'https://i.gkd.li/import/13256552', //新增快照
-        'https://i.gkd.li/import/13296380', //新增快照
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[id="youqu.android.todesk:id/ivAdClose" || id="youqu.android.todesk:id/llClose"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/7535a849-b4a7-4dd5-a6c1-4d3def042d70',
+          snapshotUrls: [
+            'https://i.gkd.li/import/14175556', // id="youqu.android.todesk:id/ivAdClose"
+            'https://i.gkd.li/import/13228546', // id="youqu.android.todesk:id/llClose"
+          ],
+        },
       ],
     },
   ],
