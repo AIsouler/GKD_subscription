9ecae7ede6b26d7f2fa6743ebae26df7bb351334
aisouler <zy43220@gmail.com>
2024-02-07 13:17:02 +0800

    feat(闪电侠): 开屏广告

---
+++ src/apps/app.esou.ts
@@ -0,0 +1,31 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'app.esou',
+  name: '闪电侠',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          matches:
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView',
+          exampleUrls:
+            'https://m.gkd.li/57941037/fe2a046b-1abe-4aba-a0df-8d9568211e0d',
+          snapshotUrls: 'https://i.gkd.li/import/14189313',
+        },
+        {
+          key: 1,
+          actionCdKey: 0,
+          matches:
+            '[childCount=0][visibleToUser=true][(text.length<10&&(text*="跳过"||text*="跳过"||text*="skip"||text*="Skip")) || id$="tt_splash_skip_btn" || vid*="skip" || vid*="Skip" || (vid*="count" && vid*="down" && vid!*="download") || desc*="跳过" || desc*="skip"]',
+        },
+      ],
+    },
+  ],
+});
