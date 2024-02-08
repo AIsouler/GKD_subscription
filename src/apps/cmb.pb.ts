043455192de11bb36832d0e2ded55dda37102d46
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-08 19:56:02 +0800

    feat(招商银行) 局部广告-首页活动悬浮广告

--- src/apps/cmb.pb.ts
+++ src/apps/cmb.pb.ts
@@ -6,6 +6,14 @@ export default defineAppConfig({
   deprecatedKeys: [0],
   groups: [
     {
+      key: 3,
+      name: '局部广告-首页活动悬浮广告',
+      quickFind: true,
+      activityIds: 'cmb.pb.app.mainframe.container.PBMainActivity',
+      rules: '[vid="float_window"] > [vid="ivCancle"][clickable=true]',
+      snapshotUrls: 'https://i.gkd.li/import/14208669',
+    },
+    {
       key: 10,
       name: '全屏广告-广告弹窗',
       activityIds:
