abaee39eb84abfc5d57dc5836b68889b147a8efd
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-08 20:05:23 +0800

    feat 定位提示-请求定位权限弹窗（优化规则）

--- src/apps/cmb.pb.ts
+++ src/apps/cmb.pb.ts
@@ -36,16 +36,14 @@ export default defineAppConfig({
       key: 11,
       name: '定位提示-请求定位权限弹窗',
       quickFind: true,
-      activityIds: [
-        'cmb.pb.app.h5container.webviewcontainer.PBWebContainerActivity',
-        'cmb.pb.app.search.PBSearchWebActivity',
-      ],
-      rules:
-        '@[id="cmb.pb:id/ivDelete"][desc="关闭"] - FrameLayout > [id="cmb.pb:id/tvDescription"][text*="获取您的位置"]',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@[desc="关闭"] - * > [text*="开启"][text*="定位"]',
       snapshotUrls: [
         'https://i.gkd.li/import/12706029',
         'https://i.gkd.li/import/13248893',
         'https://i.gkd.li/import/13897345',
+        'https://i.gkd.li/import/14208717',
       ],
     },
   ],
