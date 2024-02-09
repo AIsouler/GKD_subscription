fbc5f76b0d7b3dd535652caf1181132631aa3a52
aisouler <zy43220@gmail.com>
2024-02-09 11:47:47 +0800

    feat((biubiu加速器): 开屏广告

--- src/apps/com.mymoney.ts
+++ src/apps/com.mymoney.ts
@@ -7,11 +7,11 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      desc: '点击跳过',
       quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      desc: '点击跳过',
       rules: [
         {
           key: 0,
---
+++ src/apps/com.njh.biubiu.ts
@@ -0,0 +1,32 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.njh.biubiu',
+  name: 'biubiu加速器',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      desc: '点击跳过',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          matches:
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView',
+          exampleUrls:
+            'https://m.gkd.li/57941037/5c2f4dfe-d7ff-415d-81f0-bca6b5b628cb',
+          snapshotUrls: 'https://i.gkd.li/import/14214392',
+        },
+        {
+          key: 1,
+          actionCdKey: 0,
+          matches:
+            '[childCount=0][visibleToUser=true][(text.length<10&&(text*="跳过"||text*="跳過"||text*="skip"||text*="Skip")) || id$="tt_splash_skip_btn" || vid*="skip" || vid*="Skip" || (vid*="count" && vid*="down" && vid!*="download") || desc*="跳过" || desc*="skip"]',
+        },
+      ],
+    },
+  ],
+});
