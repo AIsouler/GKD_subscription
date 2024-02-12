834241dc4a4b7507bda82ddb7bb095e0b645fe85
aisouler <zy43220@gmail.com>
2024-02-12 12:15:43 +0800

    fix(QQ): 聊天页面广告（修复误触）

--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -527,14 +527,15 @@ export default defineAppConfig({
         {
           key: 0,
           name: '集福卡活动卡片',
+          quickFind: true,
           activityIds: 'com.tencent.mobileqq.activity.SplashActivity',
-          matches:
-            'FrameLayout[childCount=2] >4 FrameLayout[childCount=2] > FrameLayout[index=1] > RelativeLayout[childCount=4] > TextView[index=1][text!*="正在语音通话"] +2 ImageView',
+          matches: '@ImageView[clickable=true] -2 [text$="会员月卡"]',
           exampleUrls:
             'https://m.gkd.li/57941037/3bf709fc-eefc-4d66-9a00-475738934cd7',
           snapshotUrls: [
             'https://i.gkd.li/import/14178667',
             'https://i.gkd.li/import/14193973', // 误触界面
+            'https://i.gkd.li/import/14235146', // 误触界面
           ],
         },
         {
