518cb65473cbe9d5b273b48b2f91c45cc89c6394
aisouler <zy43220@gmail.com>
2024-02-12 14:06:58 +0800

    feat(OmoFun): 广告弹窗（优化规则）、通知提示-公告弹窗

--- src/apps/com.xifeng.fun.ts
+++ src/apps/com.xifeng.fun.ts
@@ -11,8 +11,28 @@ export default defineAppConfig({
       rules: [
         {
           activityIds: 'com.xifeng.fun.MainActivity',
-          matches: 'FrameLayout[childCount=6] >2 ImageView[visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/import/14050836',
+          matches:
+            '[id="android:id/content"] >2 FrameLayout[childCount>2] >(1,2) FrameLayout[childCount>4] >2 ImageView',
+          snapshotUrls: [
+            'https://i.gkd.li/import/14050836',
+            'https://i.gkd.li/import/14235476',
+            'https://i.gkd.li/import/14235478',
+            'https://i.gkd.li/import/14235479',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '通知提示-公告弹窗',
+      desc: '点击"我知道了"',
+      rules: [
+        {
+          activityIds: 'com.xifeng.fun.MainActivity',
+          matches: '[desc="我知道了"][clickable=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/971c3631-a9cc-4aec-992e-6cf1166eea1b',
+          snapshotUrls: 'https://i.gkd.li/import/14235411',
         },
       ],
     },
