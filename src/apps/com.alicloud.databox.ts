5ced00c11d5114a5aebd9cc9ee62d4ff456f4121
aisouler <zy43220@gmail.com>
2024-02-12 12:47:31 +0800

    feat(阿里云盘): 自动签到（补充activityid）、全屏广告-活动弹窗（优化规则）

--- src/apps/com.alicloud.databox.ts
+++ src/apps/com.alicloud.databox.ts
@@ -7,22 +7,28 @@ export default defineAppConfig({
     {
       key: 0,
       name: '功能类-自动签到',
-      activityIds: 'com.alicloud.databox.MainActivity',
+      activityIds: [
+        'com.alicloud.databox.MainActivity',
+        'com.alicloud.databox.navigation.NavigationFragmentContainerActivity',
+      ],
       rules: [
         {
           key: 0,
           name: '自动点击签到',
-          matches: ['[id="com.alicloud.databox:id/tvTaskAction"][text="领取"]'],
-          snapshotUrls: 'https://i.gkd.li/import/12929318',
           quickFind: true,
+          matches: '[id="com.alicloud.databox:id/tvTaskAction"][text="领取"]',
+          snapshotUrls: 'https://i.gkd.li/import/12929318',
         },
         {
           key: 1,
           preKeys: [0],
           name: '在签到后，关闭弹窗',
-          matches: ['[id="com.alicloud.databox:id/ivClose"]'],
-          snapshotUrls: 'https://i.gkd.li/import/13038304',
           quickFind: true,
+          matches: '[id="com.alicloud.databox:id/ivClose"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13038304',
+            'https://i.gkd.li/import/14235221', // com.alicloud.databox.navigation.NavigationFragmentContainerActivity
+          ],
         },
       ],
     },
@@ -30,8 +36,12 @@ export default defineAppConfig({
       key: 1,
       name: '全屏广告-活动弹窗',
       activityIds: 'com.alicloud.databox.MainActivity',
-      rules: '[text^="好运盲盒"] >n View > Image + TextView[clickable=true]',
-      snapshotUrls: 'https://i.gkd.li/import/13228610',
+      rules:
+        '[id="com.alicloud.databox:id/webContainer"] >5 View[childCount=3] > TextView[index=1]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13228610',
+        'https://i.gkd.li/import/14235204',
+      ],
     },
     {
       key: 2,
