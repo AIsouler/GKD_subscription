bec8ca4156976b524e03c90f1e41acdb81c94ec4
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-15 15:31:29 +0800

    feat(转转) 更新弹窗

---
+++ src/apps/com.wuba.zhuanzhuan.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wuba.zhuanzhuan',
+  name: '转转',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@[text="下次再说"] +2 [text="立即升级"]',
+      snapshotUrls: 'https://i.gkd.li/import/14292849',
+    },
+  ],
+});
