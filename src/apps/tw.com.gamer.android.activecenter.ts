238bad7fc0c28e837b964efa6279e91481d5e584
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-15 15:18:02 +0800

    feat(巴哈姆特) 开屏广告

---
+++ src/apps/tw.com.gamer.android.activecenter.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'tw.com.gamer.android.activecenter',
+  name: '巴哈姆特',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[desc="Interstitial close button"][clickable=true]',
+      snapshotUrls: 'https://i.gkd.li/import/14292691',
+    },
+  ],
+});
