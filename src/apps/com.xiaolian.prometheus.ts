fc1e8f29df916f67355ae5af0cc572da4af2218c
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-15 15:29:06 +0800

    feat(智慧笑联) 更新弹窗、全屏广告-关注公众号弹窗

---
+++ src/apps/com.xiaolian.prometheus.ts
@@ -0,0 +1,25 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xiaolian.prometheus',
+  name: '智慧笑联',
+  groups: [
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '@[vid="close_rel"] - * > [text="发现新版本"]',
+      snapshotUrls: 'https://i.gkd.li/import/14292788',
+    },
+    {
+      key: 2,
+      name: '全屏广告-关注公众号弹窗',
+      activityIds: 'com.xiaolian.home.ui.HomeActivity',
+      quickFind: true,
+      rules: '@[vid="close_img"] + [text="关注"]',
+      snapshotUrls: 'https://i.gkd.li/import/14292789',
+    },
+  ],
+});
