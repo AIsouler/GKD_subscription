401f5d9d7357a4d88d1f96f5786dfa6c47e536b1
aisouler <zy43220@gmail.com>
2024-02-15 20:44:54 +0800

    feat(777影视): 全屏广告-广告弹窗（补充规则）

--- src/apps/com.changyou.app.qi.ts
+++ src/apps/com.changyou.app.qi.ts
@@ -10,12 +10,22 @@ export default defineAppConfig({
       desc: '点击关闭',
       rules: [
         {
+          key: 0,
           quickFind: true,
           activityIds: 'com.changyou.app.qi.MainActivity',
           matches: '[vid="ksad_auto_close_btn"]',
           exampleUrls:
             'https://m.gkd.li/57941037/687f8201-8b15-491e-9c51-f3af0dc00260',
           snapshotUrls: 'https://i.gkd.li/import/14228685',
+        },
+        {
+          key: 1,
+          activityIds: 'com.changyou.app.qi.MainActivity',
+          matches:
+            '[id="android:id/content"] >2 FrameLayout[childCount=4] > FrameLayout[childCount=5] > FrameLayout[index=2][visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/c65e146e-4f2a-42f0-b233-4e04152d851d',
+          snapshotUrls: 'https://i.gkd.li/import/14296913',
         },
       ],
     },
