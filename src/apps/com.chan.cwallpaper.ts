a409ad7881723910741fd6243dd09dfa1e8ef3fc
aisouler <zy43220@gmail.com>
2024-02-16 16:34:59 +0800

    feat(图凌): 全屏广告-会员回馈弹窗

--- src/apps/com.chan.cwallpaper.ts
+++ src/apps/com.chan.cwallpaper.ts
@@ -14,5 +14,20 @@ export default defineAppConfig({
       rules: '[vid="activate_button"]',
       snapshotUrls: 'https://i.gkd.li/import/14293734',
     },
+    {
+      key: 1,
+      name: '全屏广告-会员回馈弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.chan.cwallpaper.module.main.MainActivity',
+          matches: '[vid="ivClose"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/8150c450-bcf8-4e8b-8236-4c3abebbeef3',
+          snapshotUrls: 'https://i.gkd.li/import/14308433',
+        },
+      ],
+    },
   ],
 });
