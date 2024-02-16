16e6103776e102f5d513203fc3c24a573a5fdca2
aisouler <zy43220@gmail.com>
2024-02-16 13:01:19 +0800

    feat(初音社): 全屏广告-谷歌广告

---
+++ src/apps/mikuclub.app.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'mikuclub.app',
+  name: '初音社',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-谷歌广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          activityIds: 'com.google.android.gms.ads.AdActivity',
+          matches: '[text^="关闭广告"] + View >2 Image[text=""]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/2fa2793e-187e-44e6-91e4-6a5a355568c6',
+          snapshotUrls: 'https://i.gkd.li/import/14306002',
+        },
+      ],
+    },
+  ],
+});
