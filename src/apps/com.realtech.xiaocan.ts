26199de0cebdcc0597bd5172e46290cde847fd3f
aisouler <zy43220@gmail.com>
2024-02-16 15:02:15 +0800

    feat(小蚕霸王餐): 开屏广告（优化规则）、全屏广告（补充规则）

--- src/apps/com.realtech.xiaocan.ts
+++ src/apps/com.realtech.xiaocan.ts
@@ -23,19 +23,12 @@ export default defineAppConfig({
         },
         {
           key: 2,
-          quickFind: true,
           matches:
-            '@[id$="tt_splash_skip_btn"] <<n [id="com.realtech.xiaocan:id/splash_ad_container"]',
-          snapshotUrls: 'https://i.gkd.li/import/12854728',
-        },
-        {
-          key: 3,
-          quickFind: true,
-          matches:
-            '@View[visibleToUser=true][clickable=true] <n FrameLayout[childCount=4||childCount=3] <<n [id="com.realtech.xiaocan:id/splash_ad_container"]',
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView',
           snapshotUrls: [
             'https://i.gkd.li/import/13694858',
             'https://i.gkd.li/import/13794403',
+            'https://i.gkd.li/import/12854728',
           ],
         },
       ],
@@ -50,6 +43,16 @@ export default defineAppConfig({
           matches:
             'View[childCount=3] > View + ImageView + ImageView[clickable=true][childCount=0]',
           snapshotUrls: 'https://i.gkd.li/import/13694864',
+        },
+        {
+          key: 1,
+          name: '签到后抽奖弹窗',
+          activityIds: 'com.realtech.xiaocan.MainActivity',
+          matches:
+            '[id="android:id/content"] >5 View[childCount=6] > ImageView[index=5][clickable=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/2d3008c8-0ef3-4165-bd2c-325f4b602f17',
+          snapshotUrls: 'https://i.gkd.li/import/14290847',
         },
       ],
     },
