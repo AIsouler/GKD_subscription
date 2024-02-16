ff4af947f1aea1511133fdccfa2fd7f5ae14283a
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-16 20:04:36 +0800

    feat(快手极速版) 全屏广告-朋友推荐弹窗

--- src/apps/com.kuaishou.nebula.ts
+++ src/apps/com.kuaishou.nebula.ts
@@ -12,5 +12,13 @@ export default defineAppConfig({
       rules: '[id="com.kuaishou.nebula:id/positive"][text="我知道了"]',
       snapshotUrls: 'https://i.gkd.li/import/13196316',
     },
+    {
+      key: 2,
+      name: '全屏广告-朋友推荐弹窗',
+      activityIds: 'com.yxcorp.gifshow.HomeActivity',
+      quickFind: true,
+      rules: '@[vid="close_btn"] + [text="朋友推荐"]',
+      snapshotUrls: 'https://i.gkd.li/import/14310639',
+    },
   ],
 });
