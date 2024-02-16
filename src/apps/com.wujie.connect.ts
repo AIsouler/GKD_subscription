84d2358f3858dcaf893e64cf50b10282e9e12b79
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-16 19:45:17 +0800

    feat(无界趣连) 更新提示

---
+++ src/apps/com.wujie.connect.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wujie.connect',
+  name: '无界趣连',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[vid="no_update_tv"]',
+      snapshotUrls: 'https://i.gkd.li/import/14296122',
+    },
+  ],
+});
