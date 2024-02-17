8635b92639ee6a43036fd98daf5807360d6c0db2
aisouler <zy43220@gmail.com>
2024-02-17 16:46:55 +0800

    feat(Cimoc): 全屏广告-广告弹窗

---
+++ src/apps/com.cimoc.haleydu.ts
@@ -0,0 +1,28 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.cimoc.haleydu',
+  name: 'Cimoc',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-广告弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.haleydu.cimoc.ui.activity.MainActivity',
+          matches:
+            '[id="android:id/content"] >3 FrameLayout[childCount>4] >2 ImageView[visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/fe71e192-83bd-4db7-aed4-a01851b6f0bd',
+          snapshotUrls: [
+            'https://i.gkd.li/import/14318224',
+            'https://i.gkd.li/import/14318235',
+            'https://i.gkd.li/import/14318236',
+          ],
+        },
+      ],
+    },
+  ],
+});
