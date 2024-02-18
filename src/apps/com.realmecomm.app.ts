4f98fed03b9c6f6c8168d1df1f6a1ce75444ca41
aisouler <zy43220@gmail.com>
2024-02-18 12:34:13 +0800

    feat(realme社区): 功能类-自动签到

---
+++ src/apps/com.realmecomm.app.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.realmecomm.app',
+  name: 'realme社区',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-自动签到',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.android.realme2.home.view.MainActivity',
+          matches: '[text="签到"][clickable=true][visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/18a5751c-0bbd-475d-be6a-5803307111cb',
+          snapshotUrls: 'https://i.gkd.li/import/14321155',
+        },
+      ],
+    },
+  ],
+});
