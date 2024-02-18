904d3de295cbce728c035f2843dedcafcdc8798d
aisouler <zy43220@gmail.com>
2024-02-18 17:42:42 +0800

    feat(可可影视): 通知提示-公告弹窗

---
+++ src/apps/com.kk.ysk3.tz12063k.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.kk.ysk3.tz12063k',
+  name: '可可影视',
+  groups: [
+    {
+      key: 1,
+      name: '通知提示-公告弹窗',
+      desc: '点击[朕知道了]',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.salmon.film.main.ui.MainActivity',
+          matches: '[text="朕知道了"][clickable=true][visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/c93d9be8-559a-46aa-8416-f52a7c99df4b',
+          snapshotUrls: 'https://i.gkd.li/import/14321907',
+        },
+      ],
+    },
+  ],
+});
