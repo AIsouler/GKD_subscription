3aaa2a0d5bfec11b2c884c31caa53aca068b9ccb
aisouler <zy43220@gmail.com>
2024-02-18 17:40:02 +0800

    feat(星影视频): 功能类-[您还未登录]弹窗

---
+++ src/apps/cn.nlyuming.duanju.ting.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.nlyuming.duanju.ting',
+  name: '星影视频',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-[您还未登录]弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          activityIds: 'cn.nlyuming.duanju.ting.MainActivity',
+          matches:
+            '[id="android:id/content"] >5 FrameLayout[childCount=3] >2 ImageView[visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/3041769f-b644-460b-b0de-9c2a84518849',
+          snapshotUrls: 'https://i.gkd.li/import/14321891',
+        },
+      ],
+    },
+  ],
+});
