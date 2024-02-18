7ef1adb10fdd56a5a8073bb687fef5ca381608ae
aisouler <zy43220@gmail.com>
2024-02-18 11:31:40 +0800

    feat(小米云服务): 全屏广告-广告弹窗（补充规则）

--- src/apps/com.miui.cloudservice.ts
+++ src/apps/com.miui.cloudservice.ts
@@ -6,11 +6,29 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '全屏广告-云存储空间不足弹窗',
-      activityIds:
-        'com.miui.cloudservice.hybrid.SignDeductDialogHybridActivity',
-      rules: 'View > [text="以后再说"]',
-      snapshotUrls: 'https://i.gkd.li/import/12847374',
+      name: '全屏广告-广告弹窗',
+      rules: [
+        {
+          key: 0,
+          name: '云存储空间不足弹窗',
+          activityIds:
+            'com.miui.cloudservice.hybrid.SignDeductDialogHybridActivity',
+          matches: '[text="以后再说"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/df5f7c28-a1fa-4d1b-87b8-2f592e5a8423',
+          snapshotUrls: 'https://i.gkd.li/import/12847374',
+        },
+        {
+          key: 1,
+          name: '会员打折弹窗',
+          quickFind: true,
+          activityIds: 'com.miui.cloudservice.ad.AdPopupActivity',
+          matches: '[vid="ad_popup_close_iv"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/fbb8a877-4e69-4d72-b31d-85ad87ab34af',
+          snapshotUrls: 'https://i.gkd.li/import/14320937',
+        },
+      ],
     },
   ],
 });
