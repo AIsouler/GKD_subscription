b19b52b4b6212fb2adc682ae781cb6aac303358d
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-18 16:46:49 +0800

    feat(酷狗概念版) 青少年模式、全屏广告-VIP弹窗、分段广告-我的页面-右侧悬浮反馈图标

---
+++ src/apps/com.kugou.android.lite.ts
@@ -0,0 +1,63 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.kugou.android.lite',
+  name: '酷狗概念版',
+  groups: [
+    {
+      key: 1,
+      name: '青少年模式',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text="开启青少年模式"] + [text="知道了"]',
+      snapshotUrls: 'https://i.gkd.li/import/14321697',
+    },
+    {
+      key: 2,
+      name: '全屏广告-VIP弹窗',
+      rules: [
+        {
+          key: 0,
+          name: '新人限时优惠弹窗',
+          activityIds: 'com.kugou.android.app.setting.YoungModeTipsActivity',
+          quickFind: true,
+          matches: '[text^="新人限时开通"] +3 [text="放弃优惠"]',
+          snapshotUrls: 'https://i.gkd.li/import/14321698',
+        },
+        {
+          key: 1,
+          name: '组队抽奖弹窗',
+          activityIds: 'com.kugou.android.app.setting.YoungModeTipsActivity',
+          matches:
+            '[id="android:id/content"] > ViewGroup[childCount=3] > ImageView + ImageView + ImageView[clickable=true][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/import/14321700',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '分段广告-我的页面-右侧悬浮反馈图标',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.kugou.android.app.MediaActivity',
+          matches:
+            'FrameLayout > RelativeLayout > @ImageView[vid!=null][clickable=true][visibleToUser=true] +(2,3) ImageView[vid!=null]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/14321713',
+            'https://i.gkd.li/import/14321718',
+          ],
+        },
+        {
+          key: 1,
+          preKeys: 0,
+          activityIds: 'com.kugou.android.app.MediaActivity',
+          quickFind: true,
+          matches: '[text="是否取消？"] +4 * > [text="确认"]',
+          snapshotUrls: 'https://i.gkd.li/import/14321724',
+        },
+      ],
+    },
+  ],
+});
