35f3ca51d117eb9d34e3f04795ae205be15f8594
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-19 22:58:28 +0800

    feat(手机性能排行) 更新提示

---
+++ src/apps/com.nasoft.socmark.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.nasoft.socmark',
+  name: '手机性能排行',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@[text="取消"] + [text="更新"]',
+      snapshotUrls: 'https://i.gkd.li/import/14332576',
+    },
+  ],
+});
