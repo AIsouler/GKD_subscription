55120a992f09d6b1e6fbb852b7fbde55e27b4490
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-19 22:55:16 +0800

    feat(一个木函) 更新提示

---
+++ src/apps/com.One.WoodenLetter.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.One.WoodenLetter',
+  name: '一个木函',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="新版本"] <2 * +2 * > [text="忽略"]',
+      snapshotUrls: 'https://i.gkd.li/import/14332536',
+    },
+  ],
+});
