d594118556bd626d7e4e7c9124d60063b2ab329a
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-19 22:47:49 +0800

    feat(小米换机) 更新提示

---
+++ src/apps/com.miui.huanji.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.miui.huanji',
+  name: '小米换机',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@[text="下次再说"] + [text="立即升级"]',
+      snapshotUrls: 'https://i.gkd.li/import/14332463',
+    },
+  ],
+});
