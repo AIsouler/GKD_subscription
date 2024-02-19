6f28cae253369357e4b532b5a8aa6c3d7c0d796e
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-19 23:04:41 +0800

    feat(腾讯会议) 更新提示

---
+++ src/apps/com.tencent.wemeet.app.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.tencent.wemeet.app',
+  name: '腾讯会议',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="腾讯会议新版本"] +3 * > [text="下次安装"]',
+      snapshotUrls: 'https://i.gkd.li/import/14332625',
+    },
+  ],
+});
