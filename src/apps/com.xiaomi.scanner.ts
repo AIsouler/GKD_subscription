bab64414d8050597d7ba73a4b646dbb6523e6e4d
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-20 19:56:17 +0800

    quickFind

--- src/apps/com.xiaomi.scanner.ts
+++ src/apps/com.xiaomi.scanner.ts
@@ -9,6 +9,7 @@ export default defineAppConfig({
       name: '更新提示',
       actionMaximum: 1,
       resetMatch: 'app',
+      quickFind: true,
       rules: '@[text="以后再说"] + [text="立即更新"]',
       snapshotUrls: 'https://i.gkd.li/import/14332513',
     },
