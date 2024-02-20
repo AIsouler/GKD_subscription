87ef77176267d2c6103907649a7bf43a99f873d4
aisouler <zy43220@gmail.com>
2024-02-20 17:12:26 +0800

    feat(小红帽): 全屏广告-广告弹窗

---
+++ src/apps/com.didi.live.spring.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.didi.live.spring',
+  name: '小红帽',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-广告弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          activityIds:
+            'com.qennnsad.aknkaksd.presentation.ui.MainContainerActivity',
+          matches: '[vid="dialog_hall_iv_close"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/0d0a2a0a-b9de-4f30-9e25-a0e3b3c111d6',
+          snapshotUrls: 'https://i.gkd.li/import/14341955',
+        },
+      ],
+    },
+  ],
+});
