b3695c8def13946e250afe283f369009f379a4f7
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-20 19:54:12 +0800

    quickFind

--- src/apps/com.tencent.wemeet.app.ts
+++ src/apps/com.tencent.wemeet.app.ts
@@ -9,6 +9,7 @@ export default defineAppConfig({
       name: '更新提示',
       actionMaximum: 1,
       resetMatch: 'app',
+      quickFind: true,
       rules: '[text^="腾讯会议新版本"] +3 * > [text="下次安装"]',
       snapshotUrls: 'https://i.gkd.li/import/14332625',
     },
