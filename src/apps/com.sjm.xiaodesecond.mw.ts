8687e9861084a15c1285305c136303f277d39797
aisouler <zy43220@gmail.com>
2024-02-21 13:24:32 +0800

    feat(喵物次元): 全屏广告-广告弹窗（补充规则）

--- src/apps/com.sjm.xiaodesecond.mw.ts
+++ src/apps/com.sjm.xiaodesecond.mw.ts
@@ -7,9 +7,10 @@ export default defineAppConfig({
     {
       key: 1,
       name: '全屏广告-广告弹窗',
-      desc: '点击关闭',
+      desc: '点击关闭/跳过',
       rules: [
         {
+          key: 0,
           activityIds: 'com.sjm.xiaodesecond.mw.MainActivity',
           matches:
             '[id="android:id/content"] >(3,4) FrameLayout[childCount>3 && childCount!=5] >2 ImageView[visibleToUser=true]',
@@ -18,6 +19,31 @@ export default defineAppConfig({
           snapshotUrls: [
             'https://i.gkd.li/import/14353263',
             'https://i.gkd.li/import/14345580',
+          ],
+        },
+        {
+          key: 1,
+          quickFind: true,
+          activityIds: 'com.sjm.xiaodesecond.mw.MainActivity',
+          matches: '@[clickable=true] > [text="跳过"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/a52ba4bb-0838-4c08-b771-78205786b016',
+          snapshotUrls: [
+            'https://i.gkd.li/import/14203087',
+            'https://i.gkd.li/import/14354395',
+          ],
+        },
+        {
+          key: 2,
+          quickFind: true,
+          activityIds: 'com.sjm.xiaodesecond.mw.MainActivity',
+          matches:
+            '@ViewGroup[clickable=true][childCount=1] < ViewGroup +2 ViewGroup[childCount=2] > [text="广告"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/46e28728-570e-4fef-9c38-d561813d1c63',
+          snapshotUrls: [
+            'https://i.gkd.li/import/14203600',
+            'https://i.gkd.li/import/14203327',
           ],
         },
       ],
