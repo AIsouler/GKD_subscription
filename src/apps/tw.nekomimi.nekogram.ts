03b35749d6243f5f12c43bb156a4607617f521f5
aisouler <zy43220@gmail.com>
2024-02-22 17:52:34 +0800

    feat(Nekogram): 优化规则

--- src/apps/tw.nekomimi.nekogram.ts
+++ src/apps/tw.nekomimi.nekogram.ts
@@ -9,8 +9,11 @@ export default defineAppConfig({
       name: '更新提示',
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '@[desc="稍后提醒我"] -3 * >2 [text="更新 Nekogram"]',
-      snapshotUrls: 'https://i.gkd.li/import/14229236',
+      rules: '@[desc$="稍后提醒我"] -3 * >2 [text="更新 Nekogram"]',
+      snapshotUrls: [
+        'https://i.gkd.li/i/14229236',
+        'https://i.gkd.li/i/14372061',
+      ],
     },
   ],
 });
