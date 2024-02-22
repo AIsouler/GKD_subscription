8f3beef60d3c5cd45ed29447698f344bf4602062
aisouler <zy43220@gmail.com>
2024-02-22 17:41:28 +0800

    feat(米游社): 功能类-原神自动签到

--- src/apps/com.mihoyo.hyperion.ts
+++ src/apps/com.mihoyo.hyperion.ts
@@ -39,5 +39,47 @@ export default defineAppConfig({
         'https://i.gkd.li/import/13721776', // 已打卡
       ],
     },
+    {
+      key: 4,
+      name: '功能类-原神自动签到',
+      desc: '点击签到-关闭弹窗-返回',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.mihoyo.hyperion.web2.MiHoYoWebActivity',
+      rules: [
+        {
+          key: 0,
+          name: '点击签到',
+          matches:
+            '[text="米游社原神每日签到"] >4 View[childCount=11] > @View[childCount=3][clickable=true] > Image[index=0]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/6654679e-58a9-4d6c-85a3-11bd2f82c15e',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14369790', // 签到前
+            'https://i.gkd.li/i/14371469', // 签到后，避免在此页面误触
+          ],
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          name: '关闭签到后的弹窗',
+          matches:
+            '[text="米游社原神每日签到"] >3 TextView[index=2][clickable=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/b02eca70-f7d3-4169-99d6-9906c534392a',
+          snapshotUrls: 'https://i.gkd.li/i/14371439',
+        },
+        {
+          preKeys: 1,
+          key: 2,
+          name: '返回上一个页面',
+          matches:
+            '[text="米游社原神每日签到"] - View > TextView[clickable=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/c232ba4c-d50e-4887-85ab-91739928119e',
+          snapshotUrls: 'https://i.gkd.li/i/14371469',
+        },
+      ],
+    },
   ],
 });
