47d097fa2a696cc4e2ce153baa254d9dc8c1f370
aisouler <zy43220@gmail.com>
2024-02-22 12:45:38 +0800

    feat(美图秀秀): 全屏广告-VIP弹窗

--- src/apps/com.mt.mtxx.mtxx.ts
+++ src/apps/com.mt.mtxx.mtxx.ts
@@ -14,5 +14,20 @@ export default defineAppConfig({
       rules: '[text="升级新版客户端"] - [text="取消"][clickable=true]',
       snapshotUrls: 'https://i.gkd.li/import/13238352',
     },
+    {
+      key: 2,
+      name: '全屏广告-VIP弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.mt.mtxx.mtxx.MainActivity',
+          matches: '[vid="iv_close"][clickable=true][visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/d98355c6-1308-4ed1-bf29-56499e22446d',
+          snapshotUrls: 'https://i.gkd.li/i/14368853',
+        },
+      ],
+    },
   ],
 });
