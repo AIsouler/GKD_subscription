59db447798bc946465d957cd56fb2166cc80c5d3
aisouler <zy43220@gmail.com>
2024-02-24 22:09:02 +0800

    feat(小米商城): 全屏广告-广告弹窗（优化规则）

--- src/apps/com.xiaomi.shop.ts
+++ src/apps/com.xiaomi.shop.ts
@@ -6,11 +6,26 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '全屏广告-首页弹窗广告',
-      activityIds: 'com.xiaomi.shop.plugin.homepage.utils.AdDialog',
-      rules:
-        'LinearLayout[childCount=2] > ImageView[id=null] + ImageView[id=null][clickable=true]',
-      snapshotUrls: ['https://i.gkd.li/i/12649330'],
+      name: '全屏广告-广告弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          activityIds: [
+            'com.xiaomi.shop2.activity.MainActivity',
+            'com.xiaomi.shop.plugin.homepage.utils.AdDialog',
+          ],
+          matches:
+            '@ImageView[index=1][clickable=true] <2 LinearLayout[childCount=2] < RelativeLayout < [id="android:id/content"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/85d8e8e9-b438-4904-86e2-4cb53735a70c',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14393039',
+            'https://i.gkd.li/i/12649330',
+          ],
+        },
+      ],
     },
   ],
 });
