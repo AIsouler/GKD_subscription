3fc037b2e37f5a5631737a20b09772921fb5cae4
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-24 17:29:25 +0800

    feat(转转) 更新提示（补充规则）

--- src/apps/com.wuba.zhuanzhuan.ts
+++ src/apps/com.wuba.zhuanzhuan.ts
@@ -8,9 +8,21 @@ export default defineAppConfig({
       key: 1,
       name: '更新提示',
       actionMaximum: 1,
+      actionMaximumKey: 0,
       resetMatch: 'app',
-      rules: '@[text="下次再说"] +2 [text="立即升级"]',
-      snapshotUrls: 'https://i.gkd.li/i/14292849',
+      quickFind: true,
+      rules: [
+        {
+          key: 0,
+          matches: '@[text="下次再说"] +2 [text="立即升级"]',
+          snapshotUrls: 'https://i.gkd.li/i/14292849',
+        },
+        {
+          key: 1,
+          matches: '@[text="残忍拒绝"] +2 [text="立即安装"]',
+          snapshotUrls: 'https://i.gkd.li/i/14392301',
+        },
+      ],
     },
   ],
 });
