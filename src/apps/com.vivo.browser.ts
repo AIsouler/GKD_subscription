1548f2a2ed4aec173817df9e7fd9bdc4c0dcb044
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-02-24 17:40:18 +0800

    feat(vivo浏览器) 局部广告-天气详情页面

--- src/apps/com.vivo.browser.ts
+++ src/apps/com.vivo.browser.ts
@@ -11,5 +11,17 @@ export default defineAppConfig({
       rules: '[id="com.vivo.browser:id/dialog_button_right"][text="继续浏览"]',
       snapshotUrls: 'https://i.gkd.li/i/12847431',
     },
+    {
+      key: 2,
+      name: '局部广告-天气详情页面',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.vivo.browser.lightweb.LightWebNomalStartActivity',
+          matches: '[text$="广告"] +5 * > Image',
+          snapshotUrls: 'https://i.gkd.li/i/14392332',
+        },
+      ],
+    },
   ],
 });
