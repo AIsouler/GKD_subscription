868c5dcf79680cb59618e1db44a71282606dd2f4
AIsouler <zy43220@gmail.com>
2024-02-28 19:54:49 +0800

    feat(京东云无线宝): 全屏广告-广告弹窗

---
+++ src/apps/com.jdcloud.mt.smartrouter.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.jdcloud.mt.smartrouter',
+  name: '京东云无线宝',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-广告弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          matches: '[vid="iv_close"][clickable=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/26521f31-ee9b-4bab-8275-fab75a76c96e',
+          snapshotUrls: 'https://i.gkd.li/i/14432168',
+        },
+      ],
+    },
+  ],
+});
