da8c7df92c711f6708e9a70fda8a6873b16d2f86
AIsouler <zy43220@gmail.com>
2024-02-28 23:25:35 +0800

    feat(呆萌价): 开屏广告（GKD1.7有效）

---
+++ src/apps/com.lxkj.dmhw.ts
@@ -0,0 +1,30 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.lxkj.dmhw',
+  name: '呆萌价',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          action: 'clickCenter',
+          position: {
+            left: 'width * 0.8981',
+            top: 'width * 0.1629',
+          },
+          matches: '[id="com.lxkj.dmhw:id/animation_view"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/c523fe10-484f-4bdb-b331-51745e494af1',
+          snapshotUrls: 'https://i.gkd.li/i/13246178',
+        },
+      ],
+    },
+  ],
+});
