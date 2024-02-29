5c70b9290431ee586b211213a368dbd49e991c85
AIsouler <zy43220@gmail.com>
2024-02-29 16:19:02 +0800

    feat(微博轻享版): 优化规则

--- src/apps/com.weico.international.ts
+++ src/apps/com.weico.international.ts
@@ -8,30 +8,31 @@ export default defineAppConfig({
       key: 0,
       name: '分段广告-信息流广告',
       desc: '出现在主页,搜索页',
-      activityIds: [
-        'com.weico.international.activity.MainFragmentActivity',
-        'com.weico.international.ui.search.SearchActivity',
-      ],
-      exampleUrls:
-        'https://github.com/gkd-kit/subscription/assets/38517192/e713a2ca-5048-486a-874f-dd876d53c49b',
       rules: [
         {
-          preKeys: [1],
+          key: 0,
+          name: '点击广告卡片右上角[x]',
+          quickFind: true,
+          activityIds: [
+            'com.weico.international.activity.MainFragmentActivity',
+            'com.weico.international.ui.search.SearchActivity',
+          ],
+          matches: '[id="com.weico.international:id/item_timeline_ad_action"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/12857202',
+            'https://i.gkd.li/i/14444744',
+          ],
+        },
+        {
+          preKeys: [0],
+          key: 1,
           name: '点击不感兴趣',
+          activityIds:
+            'com.google.android.material.bottomsheet.BottomSheetDialog',
           matches: '@View > [text="不感兴趣"]',
           snapshotUrls: [
             'https://i.gkd.li/i/12505755',
             'https://i.gkd.li/i/12505764',
-          ],
-        },
-        {
-          key: 0,
-          name: '点击广告卡片右上角[x]',
-          matches: '[id="com.weico.international:id/item_timeline_ad_action"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12505753',
-            'https://i.gkd.li/i/12505763',
-            'https://i.gkd.li/i/12857202',
           ],
         },
       ],
