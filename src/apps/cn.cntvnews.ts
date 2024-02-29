e302f826b8a463998381687ef778a5daa882934c
AIsouler <zy43220@gmail.com>
2024-02-29 16:24:32 +0800

    feat(央视新闻): 功能类-个性化推荐弹窗

--- src/apps/cn.cntvnews.ts
+++ src/apps/cn.cntvnews.ts
@@ -21,5 +21,22 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 2,
+      name: '功能类-个性化推荐弹窗',
+      desc: '点击[取消]',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.torch.open.homeimpl.HomeActivity',
+          matches: '[text="是否开启个性化推荐？"] +3 * > [text="取消"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/b3ee21aa-429d-4407-9b70-acecb222c354',
+          snapshotUrls: 'https://i.gkd.li/i/14444961',
+        },
+      ],
+    },
   ],
 });
