82f318533d27f8ecd787efc29aa2545f9f19f990
AIsouler <zy43220@gmail.com>
2024-02-29 14:50:49 +0800

    feat(天猫精灵): 全屏广告-超级会员弹窗（GKD1.7有效）

--- src/apps/com.alibaba.ailabs.tg.ts
+++ src/apps/com.alibaba.ailabs.tg.ts
@@ -16,5 +16,26 @@ export default defineAppConfig({
         '[id="com.alibaba.ailabs.tg:id/ll_download"] + [id="com.alibaba.ailabs.tg:id/va_dialog_cancel"]',
       snapshotUrls: 'https://i.gkd.li/i/13296332',
     },
+    {
+      key: 2,
+      name: '全屏广告-超级会员弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          action: 'clickCenter',
+          position: {
+            left: 'width * 0.9444',
+            top: 'width * 1.0583',
+          },
+          activityIds:
+            'com.alibaba.ailabs.flutter.container.FlutterPaymentActivity',
+          matches: 'FrameLayout > LinearLayout > [id="android:id/content"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/4a823639-f400-4249-81ea-f920d09317f4',
+          snapshotUrls: 'https://i.gkd.li/i/14443874',
+        },
+      ],
+    },
   ],
 });
