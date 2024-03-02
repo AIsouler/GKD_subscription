97664ce708881f2d4386e5822b1171380b81b3e1
AIsouler <zy43220@gmail.com>
2024-03-02 11:58:41 +0800

    feat(抖音): 功能类-发现通讯录好友弹窗

--- src/apps/com.ss.android.ugc.aweme.mobile.ts
+++ src/apps/com.ss.android.ugc.aweme.mobile.ts
@@ -14,5 +14,22 @@ export default defineAppConfig({
         '[id="com.ss.android.ugc.aweme.mobile:id/teen_mode_title"] +n [id="com.ss.android.ugc.aweme.mobile:id/bt_i_know"]',
       snapshotUrls: 'https://i.gkd.li/i/13255513',
     },
+    {
+      key: 2,
+      name: '功能类-发现通讯录好友弹窗',
+      desc: '点击[暂时不要]',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.ss.android.ugc.aweme.main.MainActivity',
+          matches: '@[text="暂时不要"] +2 [text="发现好友"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/af8632c4-f5fd-403a-aca9-daf92ec6cda1',
+          snapshotUrls: 'https://i.gkd.li/i/14467944',
+        },
+      ],
+    },
   ],
 });
