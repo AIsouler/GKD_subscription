391ab81713197e22256f94f12dafa8847ba5ad2c
AIsouler <zy43220@gmail.com>
2024-03-04 20:39:28 +0800

    feat(烟花动漫): 全屏广告

---
+++ src/apps/com.viva.timeplan.p5.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.viva.timeplan.p5',
+  name: '烟花动漫',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-广告弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          activityIds: 'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
+          matches: '[id="android:id/content"] >5 ImageView[clickable=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/3bfe1e95-56ed-43f7-b40c-e523953e71f6',
+          snapshotUrls: 'https://i.gkd.li/i/14481496',
+        },
+      ],
+    },
+  ],
+});
