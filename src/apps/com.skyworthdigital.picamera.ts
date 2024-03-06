e0749010e9920f36bde6cb45b355f0929ca28206
AIsouler <zy43220@gmail.com>
2024-03-06 22:59:19 +0800

    feat(创维智慧云): 开屏广告

---
+++ src/apps/com.skyworthdigital.picamera.ts
@@ -0,0 +1,34 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.skyworthdigital.picamera',
+  name: '创维智慧云',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      actionMaximumKey: 0,
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          matches: '[text*="跳过"][text.length<=10]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/07c9f980-aa0f-4044-a58d-299a2a358857',
+          snapshotUrls: 'https://i.gkd.li/i/13497656',
+        },
+        {
+          key: 1,
+          matches:
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView',
+          exampleUrls:
+            'https://m.gkd.li/57941037/3b5cba55-7024-4d3e-a21c-7b1d540515bc',
+          snapshotUrls: 'https://i.gkd.li/i/14511463',
+        },
+      ],
+    },
+  ],
+});
