160def17777276fa85c1e0e4d1a53e442a1c9bcf
AIsouler <zy43220@gmail.com>
2024-03-07 14:40:47 +0800

    feat(鄂汇办): 开屏广告（GKD1.7有效）

---
+++ src/apps/cn.cty.hbzw.ts
@@ -0,0 +1,29 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.cty.hbzw',
+  name: '鄂汇办',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          action: 'clickCenter',
+          position: {
+            left: 'width * 0.8676',
+            top: 'width * -1.9094',
+          },
+          quickFind: true,
+          matches: '[vid="iv_ad_image"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/9cf76d31-3b11-45dc-be9c-c2e9cbf26c23',
+          snapshotUrls: 'https://i.gkd.li/i/14519367',
+        },
+      ],
+    },
+  ],
+});
