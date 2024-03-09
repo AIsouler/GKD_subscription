1de65cff3de9ea6aeb8b8cd986f5a1c2b4928d54
AIsouler <zy43220@gmail.com>
2024-03-09 14:45:40 +0800

    feat(邻里邦): 功能类-自动签到

---
+++ src/apps/cn.timesneighborhood.app.c.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.timesneighborhood.app.c',
+  name: '邻里邦',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-自动签到',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.zkty.modules.engine.activity.XEngineWebActivity',
+          matches: '[text="立即签到"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/e02dac87-41dc-45eb-bf7e-8daa0b69f517',
+          snapshotUrls: 'https://i.gkd.li/i/14545251',
+        },
+      ],
+    },
+  ],
+});
