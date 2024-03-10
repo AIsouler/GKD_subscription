59d6c91f0338321ce113726bbb4080922cd9708d
AIsouler <zy43220@gmail.com>
2024-03-10 16:10:21 +0800

    fix(微博轻享版): 开屏广告（小窗模式下全局规则触发无效）

--- src/apps/com.weico.international.ts
+++ src/apps/com.weico.international.ts
@@ -5,6 +5,26 @@ export default defineAppConfig({
   name: '微博轻享版',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          matches: '@[clickable=true] + * > [text="跳过"][visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/e1253f77-cfdd-42ea-b6c2-05732a9e347a',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14549281',
+            'https://i.gkd.li/i/14549328', // 小窗模式下全局规则触发无效
+          ],
+        },
+      ],
+    },
+    {
       key: 0,
       name: '分段广告-信息流广告',
       desc: '出现在主页,搜索页',
