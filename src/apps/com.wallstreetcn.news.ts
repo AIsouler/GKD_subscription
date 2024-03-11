bd6aea4cf890ee78439fc53580bc9b2c2ba373d7
AIsouler <zy43220@gmail.com>
2024-03-11 11:01:24 +0800

    feat(华尔街见闻): 开屏广告

--- src/apps/com.wallstreetcn.news.ts
+++ src/apps/com.wallstreetcn.news.ts
@@ -5,6 +5,22 @@ export default defineAppConfig({
   name: '华尔街见闻',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          quickFind: true,
+          matches: '[text="广告"] +2 [text^="关闭"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/40e483a8-61e7-45a0-ac44-df1f472e2863',
+          snapshotUrls: 'https://i.gkd.li/i/14525329',
+        },
+      ],
+    },
+    {
       key: 10,
       name: '局部广告-卡片广告',
       desc: '点击关闭',
