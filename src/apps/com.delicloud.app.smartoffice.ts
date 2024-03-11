7e3c531ded5d3164e3a572444c05443afccdaf2d
AIsouler <zy43220@gmail.com>
2024-03-11 11:19:14 +0800

    feat(得力e+): 开屏广告（补充规则）

--- src/apps/com.delicloud.app.smartoffice.ts
+++ src/apps/com.delicloud.app.smartoffice.ts
@@ -7,12 +7,21 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
+      actionMaximumKey: 0,
       rules: [
         {
-          quickFind: true,
+          key: 0,
+          matches: '[text="跳过"][childCount=0][visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/23262900-c52d-494a-9b7d-bb821025665a',
+          snapshotUrls: 'https://i.gkd.li/i/14553055',
+        },
+        {
+          key: 1,
           matches:
             'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView <<n [vid="splash_container"]',
           snapshotUrls: [
