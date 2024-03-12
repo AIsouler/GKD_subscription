bfe1db8939654eccda9a2ab3d97b97569fbcd4b1
AIsouler <zy43220@gmail.com>
2024-03-12 20:49:26 +0800

    feat(Clousx6): 通知提示-公告弹窗

---
+++ src/apps/com.example.robortx.clousx6F.ts
@@ -0,0 +1,25 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.example.robortx.clousx6F',
+  name: 'Clousx6',
+  groups: [
+    {
+      key: 1,
+      name: '通知提示-公告弹窗',
+      desc: '点击关闭',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          quickFind: true,
+          matches: '@[vid="iv_close"] + * >2 [text="上一条"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/a59e4c30-29e3-4d7e-9c05-f7e844ff6ac3',
+          snapshotUrls: 'https://i.gkd.li/i/14571096',
+        },
+      ],
+    },
+  ],
+});
