25de0ee9c6916a4be72da8f8d5154e36c7fd85d4
AIsouler <zy43220@gmail.com>
2024-03-12 22:13:31 +0800

    feat(三星阅读): 开屏广告

---
+++ src/apps/com.mci.smagazine.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+export default defineAppConfig({
+  id: 'com.mci.smagazine',
+  name: '三星阅读',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          quickFind: true,
+          action: 'clickCenter',
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/6654a799-c853-4221-b058-8278949574cc',
+          snapshotUrls: 'https://i.gkd.li/i/14573324',
+        },
+      ],
+    },
+  ],
+});
