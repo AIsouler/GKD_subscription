844f4b0c1ffe0c4e7d76aa38f974f7a305c179e2
AIsouler <zy43220@gmail.com>
2024-03-13 19:42:05 +0800

    fix(云闪付): 局部广告-右侧悬浮广告失效

--- src/apps/com.unionpay.ts
+++ src/apps/com.unionpay.ts
@@ -40,16 +40,21 @@ export default defineAppConfig({
     {
       key: 9,
       name: '局部广告-右侧悬浮广告',
-      // matchTime: 10000, 部分广告不在10s内
       actionMaximum: 1,
       resetMatch: 'app',
-      quickFind: true,
-      activityIds: ['com.unionpay.activity.UPActivityMain'],
       rules: [
         {
+          quickFind: true,
+          position: {
+            left: 'width * 0.8893',
+            top: 'width * 0.4607',
+          },
+          activityIds: 'com.unionpay.activity.UPActivityMain',
           matches:
-            '[id="com.unionpay:id/frog_float"] >2 [id="com.unionpay:id/close_view"][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/i/12695699',
+            '@[vid="frog_float"][visibleToUser=true] >2 [vid="close_view"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/0f4f6a7f-55ce-4f87-a4cb-97e9c7107359',
+          snapshotUrls: 'https://i.gkd.li/i/14586427',
         },
       ],
     },
