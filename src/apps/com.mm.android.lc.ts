751bf29bd5b1065ba9e63ea435dcaf2a6fcf9929
AIsouler <zy43220@gmail.com>
2024-03-13 21:24:18 +0800

    feat(乐橙): 开屏广告

---
+++ src/apps/com.mm.android.lc.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.mm.android.lc',
+  name: '乐橙',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          quickFind: true,
+          matches: '[vid="advert_countdown_view"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/b30750e1-92ca-452b-8467-a0074989ec92',
+          snapshotUrls: 'https://i.gkd.li/i/14586621',
+        },
+      ],
+    },
+  ],
+});
