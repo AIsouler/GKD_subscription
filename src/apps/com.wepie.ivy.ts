0295901722dddbe85d2960ac704cbaa4fc77dc14
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-03-14 21:36:52 +0800

    feat(青藤之恋) 评价提示-好评弹窗

---
+++ src/apps/com.wepie.ivy.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wepie.ivy',
+  name: '青藤之恋',
+  groups: [
+    {
+      key: 1,
+      name: '评价提示-好评弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          key: 0,
+          matches: '[text="我要吐槽"] + [text="五星好评"]',
+          action: 'back',
+          snapshotUrls: 'https://i.gkd.li/i/14602933',
+        },
+      ],
+    },
+  ],
+});
