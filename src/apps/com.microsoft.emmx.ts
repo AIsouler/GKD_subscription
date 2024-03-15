cc9c951052393f2d918dc9fa5c30b95e2e8391ea
AIsouler <zy43220@gmail.com>
2024-03-15 19:57:28 +0800

    feat(Edge): 功能类-关闭标签页、功能类-[前往CSDN APP阅读全文]弹窗

--- src/apps/com.microsoft.emmx.ts
+++ src/apps/com.microsoft.emmx.ts
@@ -29,5 +29,34 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 5,
+      name: '功能类-关闭标签页',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'org.chromium.chrome.browser.ChromeTabbedActivity',
+          matches: '[text="关闭标签页"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/81f9a737-a111-4630-898b-4f2b5de0623b',
+          snapshotUrls: 'https://i.gkd.li/i/14614992',
+        },
+      ],
+    },
+    {
+      key: 6,
+      name: '功能类-[前往CSDN APP阅读全文]弹窗',
+      desc: '点击[继续]',
+      rules: [
+        {
+          activityIds: 'org.chromium.chrome.browser.ChromeTabbedActivity',
+          matches:
+            '[text="前往CSDN APP阅读全文"] < * > [desc="继续"][clickable=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/fea75b5a-010d-4f67-8998-6cf794eda975',
+          snapshotUrls: 'https://i.gkd.li/i/14610025',
+        },
+      ],
+    },
   ],
 });
