6949005890c0d58ccb62ddcdfd60287fe3229c52
AIsouler <zy43220@gmail.com>
2024-03-15 21:28:53 +0800

    feat(大麦): 开屏广告

--- src/apps/cn.damai.ts
+++ src/apps/cn.damai.ts
@@ -6,12 +6,20 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '开屏广告',
+      name: '开屏广告', // 该软件开屏广告非常特殊，需使用clickCenter并在设置里开启[shizuku模拟点击]才能跳过
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[id="cn.damai:id/homepage_advert_pb"]',
-      snapshotUrls: 'https://i.gkd.li/i/12472623',
+      rules: [
+        {
+          quickFind: true,
+          action: 'clickCenter',
+          matches: '[vid="homepage_advert_pb"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/6680a0b8-0796-4165-88fc-1f16cd6c67dd',
+          snapshotUrls: 'https://i.gkd.li/i/14585402',
+        },
+      ],
     },
     {
       key: 1,
