6025fd3248e8583e42d377df1aa4a69efa8a9b4c
AIsouler <zy43220@gmail.com>
2024-03-15 18:46:25 +0800

    feat(抖音): 定位提示

--- src/apps/com.ss.android.ugc.aweme.mobile.ts
+++ src/apps/com.ss.android.ugc.aweme.mobile.ts
@@ -31,5 +31,23 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 3,
+      name: '定位提示-请求位置权限弹窗',
+      desc: '点击[以后再说]',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.ss.android.ugc.aweme.main.MainActivity',
+          matches: '[text^="你还没有开启位置权限"] +2 * > [text="以后再说"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/546e45cf-d151-47f4-830e-0f1b7ac21794',
+          snapshotUrls: 'https://i.gkd.li/i/14614184',
+        },
+      ],
+    },
   ],
 });
