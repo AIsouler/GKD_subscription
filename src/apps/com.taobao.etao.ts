c5515a63e8fe42f1f941231dcb515c87179172f4
AIsouler <zy43220@gmail.com>
2024-03-16 13:00:36 +0800

    feat(一淘): 全屏广告（补充规则）

--- src/apps/com.taobao.etao.ts
+++ src/apps/com.taobao.etao.ts
@@ -34,6 +34,15 @@ export default defineAppConfig({
             'https://i.gkd.li/i/13670025',
           ],
         },
+        {
+          key: 1,
+          activityIds: 'com.taobao.etao.app.homev4.HomeV4Activity',
+          matches:
+            '[text="一淘-首页红包升级-2312wf"] >5 View[childCount=3] > View[index=0][visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/db5c7761-3a8b-4bc6-a61c-65dd30f61b9f',
+          snapshotUrls: 'https://i.gkd.li/i/14622468',
+        },
       ],
     },
     {
