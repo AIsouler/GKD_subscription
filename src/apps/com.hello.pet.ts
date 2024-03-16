25af5d0186ccf0b5a4f0c48ff9fd97a15a53ab73
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-03-16 14:15:52 +0800

    feat(街猫) 通知提示-请求开启通知权限弹窗

---
+++ src/apps/com.hello.pet.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.hello.pet',
+  name: '街猫',
+  groups: [
+    {
+      key: 1,
+      name: '通知提示-请求开启通知权限弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text^="开启通知"] +2 [text="取消"]',
+      snapshotUrls: 'https://i.gkd.li/i/14622639',
+    },
+  ],
+});
