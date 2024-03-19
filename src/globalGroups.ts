575f9b94bc1413101e87eeaedf00399fb0dfd410
AIsouler <zy43220@gmail.com>
2024-03-19 17:24:13 +0800

    perf: 全局规则

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -37,15 +37,13 @@ const globalGroups: RawGlobalGroup[] = [
     matchTime: 10000,
     actionMaximum: 1,
     resetMatch: 'app',
-    actionCdKey: 0,
-    actionMaximumKey: 0,
     rules: [
       {
         key: 0,
         matches: [
-          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][height>0 && width>0][(text*="内测" || text*="测试版" || text*="新版" || text*="更新" || text*="升级" || text*="体验" || text*="內測" || text*="測試版" || text*="升級" || text*="體驗" || text*="Update" || text*="Upgrade" || text*="Experience") && text!*="自动" && text!*="自動" && (text!*="成功" && text!*="失败" && text!*="失敗" && (text!*="已" || text*="已知") && text!*="检查更新")]',
-          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][height>0 && width>0][(((text*="立即" || text*="马上" || text*="并" || text*="现在" || text*="立刻" || text*="抢先" || text*="在线")&&(text*="更新" || text*="下载" || text*="安装" || text*="升级" || text*="查看" || text*="体验")) || text="更新" || text="下载" || text="安装" || text="升级" || text="确定" || text="好,升级") && text.length<6]',
-          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][height>0 && width>0][text^="不再" || text$="再说" || text$="拒绝" || text$="再想想" || text^="忽略" || text^="暂不" || text^="放弃" || text^="取消" || text$="不要" || text$="再說" || text$="暫不" || text$="拒絕" || text$="Later" || text^="Ignore" || text^="Not now" || text^="Cancel" || vid^="close" || vid^="Close" || vid^="closeIv" || vid*="_close" || vid*="_Close" || vid$="close" || vid$="Close" || vid$="cancel" || vid$="Cancel" || vid$="cancle" || vid$="Cancle"]',
+          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][(text*="内测" || text*="测试版" || text*="新版" || text*="更新" || text*="升级" || text*="体验" || text*="內測" || text*="測試版" || text*="升級" || text*="體驗" || text*="Update" || text*="Upgrade" || text*="Experience") && text!*="自动" && text!*="自動" && (text!*="成功" && text!*="失败" && text!*="失敗" && (text!*="已" || text*="已知") && text!*="检查更新")]',
+          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][(((text*="立即" || text*="马上" || text*="并" || text*="现在" || text*="立刻" || text*="抢先" || text*="在线")&&(text*="更新" || text*="下载" || text*="安装" || text*="升级" || text*="查看" || text*="体验")) || text="更新" || text="下载" || text="安装" || text="升级" || text="确定" || text="好,升级") && text.length<6]',
+          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][text^="不再" || text$="再说" || text$="拒绝" || text$="再想想" || text^="忽略" || text^="暂不" || text^="放弃" || text^="取消" || text$="不要" || text$="再說" || text$="暫不" || text$="拒絕" || text$="Later" || text^="Ignore" || text^="Not now" || text^="Cancel" || vid^="close" || vid^="Close" || vid^="closeIv" || vid*="_close" || vid*="_Close" || vid$="close" || vid$="Close" || vid$="cancel" || vid$="Cancel" || vid$="cancle" || vid$="Cancle"]',
         ],
       },
     ],
@@ -66,8 +64,8 @@ const globalGroups: RawGlobalGroup[] = [
       {
         key: 0,
         matches: [
-          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][height>0 && width>0][((text*="青少年" || text*="未成年") && text*="模式" || text*="儿童模式") && text.length<15 || ((desc*="青少年" || desc*="未成年") && desc*="模式") && desc.length<15]',
-          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][height>0 && width>0][text*="知道了" || text*="关闭" || text="我已知晓" || desc*="知道了" || desc*="关闭"]',
+          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][((text*="青少年" || text*="未成年") && text*="模式" || text*="儿童模式") && text.length<15 || ((desc*="青少年" || desc*="未成年") && desc*="模式") && desc.length<15]',
+          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][text*="知道了" || text*="关闭" || text="我已知晓" || desc*="知道了" || desc*="关闭"]',
         ],
       },
     ],
