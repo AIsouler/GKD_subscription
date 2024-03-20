42584718a6d51c1ab7353e4e8c76e7c102ed66d7
AIsouler <zy43220@gmail.com>
2024-03-20 21:42:19 +0800

    feat(水果派): 开屏广告

---
+++ src/apps/com.ruYUVE.bgGRHf.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.ruYUVE.bgGRHf',
+  name: '水果派',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          action: 'clickCenter',
+          forcedTime: 10000, // 该软件开屏广告5秒后才出现跳过按钮
+          matches: '[desc="关闭广告"]',
+          snapshotUrls: 'https://i.gkd.li/i/14667107',
+        },
+      ],
+    },
+  ],
+});
