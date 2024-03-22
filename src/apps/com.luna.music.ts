57552b0ccc50aed6459cad1e92bb4b168b3f2691
AIsouler <zy43220@gmail.com>
2024-03-22 18:03:16 +0800

    feat(汽水音乐): 评价提示

--- src/apps/com.luna.music.ts
+++ src/apps/com.luna.music.ts
@@ -103,5 +103,22 @@ export default defineAppConfig({
       rules: '[text="立得全天畅听"][clickable=true]',
       snapshotUrls: 'https://i.gkd.li/i/14237527',
     },
+    {
+      key: 10,
+      name: '评价提示-评分弹窗',
+      desc: '使用返回关闭弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          quickFind: true,
+          action: 'back',
+          matches: '[text="为汽水音乐评分"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/a7e53af0-8b84-4619-b369-69b949ab2ce4',
+          snapshotUrls: 'https://i.gkd.li/i/14720841',
+        },
+      ],
+    },
   ],
 });
