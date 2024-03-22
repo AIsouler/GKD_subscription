78546da342742b49816dc2fa55272d5ea4542653
AIsouler <zy43220@gmail.com>
2024-03-22 20:54:35 +0800

    feat(NP管理器): 全屏广告-广告弹窗

---
+++ src/apps/com.wn.app.np.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wn.app.np',
+  name: 'NP管理器',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-广告弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          activityIds: 'player.normal.np.activity.CommonEditActivity',
+          matches:
+            '[id="android:id/content"] FrameLayout[index=2][childCount=1] > ImageView',
+          exampleUrls:
+            'https://m.gkd.li/57941037/b75f4ba1-7e40-4ea2-9f65-20b61f018720',
+          snapshotUrls: 'https://i.gkd.li/i/14722550',
+        },
+      ],
+    },
+  ],
+});
