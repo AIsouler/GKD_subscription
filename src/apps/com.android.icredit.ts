1b42ad4fd13d6c488c8b60c75a4cd8d1ca5cc5b3
AIsouler <zy43220@gmail.com>
2024-03-22 20:51:52 +0800

    feat(企查查): 全屏广告-VIP弹窗

---
+++ src/apps/com.android.icredit.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.android.icredit',
+  name: '企查查',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-VIP弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          activityIds: 'com.android.icredit.ui.MainMenuActivity',
+          matches: '[text$="限时返场"] >3 Image',
+          exampleUrls:
+            'https://m.gkd.li/57941037/eb232325-12e4-495f-a9f3-5f853d5e66f9',
+          snapshotUrls: 'https://i.gkd.li/i/14720494',
+        },
+      ],
+    },
+  ],
+});
