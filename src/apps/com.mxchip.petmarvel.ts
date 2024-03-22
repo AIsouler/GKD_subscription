c3ffb5affe4cb6910711c77fd2dc8b3f1c87b142
AIsouler <zy43220@gmail.com>
2024-03-22 14:09:45 +0800

    feat(滴宠生活): 开屏广告、全屏广告

--- src/apps/com.mxchip.petmarvel.ts
+++ src/apps/com.mxchip.petmarvel.ts
@@ -5,6 +5,23 @@ export default defineAppConfig({
   name: '滴宠生活',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          position: {
+            left: 'width * 0.8379',
+            top: 'width * 0.2055',
+          },
+          matches: 'FrameLayout[depth=0]',
+          snapshotUrls: 'https://i.gkd.li/i/14717741',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '局部广告-左上角卡片弹窗',
       desc: '点击X',
@@ -14,6 +31,21 @@ export default defineAppConfig({
           matches:
             'View[childCount=19] > View[index=1] > Image[clickable=true]',
           snapshotUrls: 'https://i.gkd.li/i/13989316',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '全屏广告-广告弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.mxchip.petmarvel.MainActivity',
+          matches: '[vid="iv_close"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/de4ea9ad-c85f-4ace-bd4e-7b7fc9adbc2a',
+          snapshotUrls: 'https://i.gkd.li/i/14717743',
         },
       ],
     },
