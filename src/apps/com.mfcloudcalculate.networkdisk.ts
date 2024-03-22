91f72b844b5676c86f1cfbc990ccf3e2b7bdb719
AIsouler <zy43220@gmail.com>
2024-03-22 20:31:15 +0800

    perf(123云盘): 全屏广告

--- src/apps/com.mfcloudcalculate.networkdisk.ts
+++ src/apps/com.mfcloudcalculate.networkdisk.ts
@@ -6,15 +6,24 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '全屏广告-应用内广告弹窗',
-      activityIds: 'com.mfcloudcalculate.networkdisk.activity.AdFreeActivity',
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'activity',
-      quickFind: true,
-      rules:
-        '[text*="关闭"][id="com.mfcloudcalculate.networkdisk:id/tv_ad_free_colse"]',
-      snapshotUrls: 'https://i.gkd.li/i/13546173',
+      name: '全屏广告-广告弹窗',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+            'com.mfcloudcalculate.networkdisk.activity.AdFreeActivity',
+          ],
+          matches: '[id$="iv_ad_free_gif"] + * > [text="关闭"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/4cafd5fd-b5ed-4df1-b9f2-f443f53a7166',
+          snapshotUrls: [
+            'https://i.gkd.li/i/13546173',
+            'https://i.gkd.li/i/14696860',
+          ],
+        },
+      ],
     },
     {
       key: 2,
