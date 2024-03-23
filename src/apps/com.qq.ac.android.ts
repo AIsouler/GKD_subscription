94e0176053fad83125721cdf7b1fb56227fb527b
AIsouler <zy43220@gmail.com>
2024-03-23 13:46:01 +0800

    feat(腾讯动漫): 局部广告-信息流广告

--- src/apps/com.qq.ac.android.ts
+++ src/apps/com.qq.ac.android.ts
@@ -21,7 +21,24 @@ export default defineAppConfig({
       activityIds: 'com.qq.ac.android.reader.comic.ComicReaderActivity',
       rules:
         '[id="com.qq.ac.android:id/operation_ad_view"] > [id="com.qq.ac.android:id/btn_close"]',
-      snapshotUrls: ['https://i.gkd.li/i/12707756'],
+      snapshotUrls: 'https://i.gkd.li/i/12707756',
+    },
+    {
+      key: 3,
+      name: '局部广告-信息流广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          key: 0,
+          name: '漫画章节之间的广告',
+          quickFind: true,
+          activityIds: 'com.qq.ac.android.reader.comic.ComicReaderActivity',
+          matches: '[vid="iv_del"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/1be7564d-f6c2-429d-9cb0-3d923adafc8a',
+          snapshotUrls: 'https://i.gkd.li/i/14730335',
+        },
+      ],
     },
   ],
 });
