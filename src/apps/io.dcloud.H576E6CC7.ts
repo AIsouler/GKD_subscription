63c090396fdee6c51a18ae0a079bd587578616ab
AIsouler <zy43220@gmail.com>
2024-03-24 08:55:57 +0800

    feat(鱼泡网): 局部广告-卡片广告（补充规则）

--- src/apps/io.dcloud.H576E6CC7.ts
+++ src/apps/io.dcloud.H576E6CC7.ts
@@ -10,12 +10,21 @@ export default defineAppConfig({
       desc: '点击关闭',
       rules: [
         {
+          key: 0,
           quickFind: true,
           activityIds: 'com.yupao.entry.MainActivity',
           matches: '[vid="ad_close"]',
           exampleUrls:
             'https://m.gkd.li/57941037/42273686-b44a-4964-80a5-84b55e6ad75c',
           snapshotUrls: 'https://i.gkd.li/i/14731764',
+        },
+        {
+          key: 1,
+          activityIds: 'com.yupao.entry.MainActivity',
+          matches: '[text="广告"] <2 View +3 View > TextView[clickable=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/123997dd-4783-4aac-a787-e02daccd43da',
+          snapshotUrls: 'https://i.gkd.li/i/14732000',
         },
       ],
     },
