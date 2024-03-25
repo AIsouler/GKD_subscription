d5fd59207c7f6730ac8a5a0e4fd613858cb54803
AIsouler <zy43220@gmail.com>
2024-03-25 11:55:19 +0800

    feat(中国移动): 局部广告-右侧悬浮小图标（补充规则）

--- src/apps/com.greenpoint.android.mc10086.activity.ts
+++ src/apps/com.greenpoint.android.mc10086.activity.ts
@@ -76,20 +76,30 @@ export default defineAppConfig({
       key: 12,
       name: '局部广告-右侧悬浮小图标',
       quickFind: true,
-      activityIds: 'com.mc10086.cmcc.view.tabs.AppTabFragment',
       rules: [
         {
           key: 0,
+          activityIds: 'com.mc10086.cmcc.view.tabs.AppTabFragment',
           matches:
             '[id="com.greenpoint.android.mc10086.activity:id/close_btn"]',
           snapshotUrls: 'https://i.gkd.li/i/12662265',
         },
         {
           key: 1,
+          activityIds: 'com.mc10086.cmcc.view.tabs.AppTabFragment',
           matches: '[vid="close_btn_bottom"]',
           exampleUrls:
             'https://m.gkd.li/57941037/276c7811-52f7-4379-8782-f9fb0b6cec1c',
           snapshotUrls: 'https://i.gkd.li/i/14570369',
+        },
+        {
+          key: 2,
+          activityIds: 'com.cmccit.webview.ac.CommonHtml5Activity',
+          matches:
+            '[text="余量查询"] > View > View > @Image[clickable=true][text!=null] <<n [vid="patmentwebview"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/69c6bf73-d901-4816-a9da-e4bcb0f4b9c6',
+          snapshotUrls: 'https://i.gkd.li/i/14738550',
         },
       ],
     },
