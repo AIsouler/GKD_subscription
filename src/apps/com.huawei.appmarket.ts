3e7967041797720277e2d75fcfe21c7d4deab0de
AIsouler <zy43220@gmail.com>
2024-03-25 11:59:33 +0800

    feat(华为应用市场): 功能类-应用更新页面-点击[查看更多]

--- src/apps/com.huawei.appmarket.ts
+++ src/apps/com.huawei.appmarket.ts
@@ -30,5 +30,22 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 3,
+      name: '功能类-应用更新页面-点击[查看更多]',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds:
+            'com.huawei.appmarket.service.appmgr.view.activity.AppUpdateActivity',
+          matches: '[text="查看更多"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/e2283b1d-3041-49c5-aca2-7c165c83ce37',
+          snapshotUrls: 'https://i.gkd.li/i/14738587',
+        },
+      ],
+    },
   ],
 });
