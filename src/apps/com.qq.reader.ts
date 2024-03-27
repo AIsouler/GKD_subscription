3f36beb2440f264bbca39f240a0415a6874aa94f
AIsouler <zy43220@gmail.com>
2024-03-27 11:39:17 +0800

    feat(QQ阅读): 分段广告-阅读页面广告

--- src/apps/com.qq.reader.ts
+++ src/apps/com.qq.reader.ts
@@ -43,5 +43,41 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 5,
+      name: '分段广告-阅读页面广告',
+      desc: '点击关闭-点击[关闭本条广告]',
+      quickFind: true,
+      activityIds: 'com.qq.reader.activity.ReaderPageActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '@[clickable=true] > [text="关闭广告"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/70e15d7d-0911-44e7-b0d6-a8e74c00b8b1',
+          snapshotUrls: 'https://i.gkd.li/i/14767123',
+        },
+        {
+          key: 1,
+          matches: '[vid="adv_close"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/eb2bca3c-abbc-4666-b505-cdaeeec6a839',
+          snapshotUrls: 'https://i.gkd.li/i/14767126',
+        },
+
+        // 预留key
+        {
+          preKeys: [0, 1],
+          key: 50,
+          matches: '[vid="view_close_ad_btn_bg"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/c985da66-c2cc-4400-bb61-8f7ee6dbba11',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14767165',
+            'https://i.gkd.li/i/14767124',
+          ],
+        },
+      ],
+    },
   ],
 });
