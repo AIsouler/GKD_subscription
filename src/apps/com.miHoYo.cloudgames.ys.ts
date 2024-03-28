b34fb8d77ba18943b1fc9200441df900e60d0359
AIsouler <zy43220@gmail.com>
2024-03-28 12:54:36 +0800

    feat(云·原神): 功能类-自动点击[使用流量进行游戏]

---
+++ src/apps/com.miHoYo.cloudgames.ys.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.miHoYo.cloudgames.ys',
+  name: '云·原神',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-自动点击[使用流量进行游戏]',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.mihoyo.cloudgame.main.MiHoYoCloudMainActivity',
+          matches: '[text="使用流量进行游戏"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/84c18536-b3a4-4f6e-99b2-264c1a36beb5',
+          snapshotUrls: 'https://i.gkd.li/i/14783168',
+        },
+      ],
+    },
+  ],
+});
