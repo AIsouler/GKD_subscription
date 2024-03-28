4c564c17cdac34e2fcf225fb981239f5d92f352a
AIsouler <zy43220@gmail.com>
2024-03-28 13:24:03 +0800

    feat(云·星穹铁道): 功能类-自动点击[使用流量进行游戏]

---
+++ src/apps/com.miHoYo.cloudgames.hkrpg.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.miHoYo.cloudgames.hkrpg',
+  name: '云·星穹铁道',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-自动点击[使用流量进行游戏]',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.mihoyo.cloudgame.main.MiHoYoCloudMainActivity',
+          matches: '[text="使用流量进行游戏"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/f33b53ea-32a2-48b3-a4a8-54bfa8b1fdd8',
+          snapshotUrls: 'https://i.gkd.li/i/14784161',
+        },
+      ],
+    },
+  ],
+});
