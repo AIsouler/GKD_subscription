2b6d9092c980b62cd3241d553a063328d2ddafb7
AIsouler <zy43220@gmail.com>
2024-03-29 21:31:00 +0800

    feat(菜鸟): 全屏广告（补充规则）

--- src/apps/com.cainiao.wireless.ts
+++ src/apps/com.cainiao.wireless.ts
@@ -28,16 +28,23 @@ export default defineAppConfig({
             'View[childCount=4] > Image + View + Image + View[text.length>0][clickable=true]',
           snapshotUrls: 'https://i.gkd.li/i/13042279',
         },
+        {
+          key: 2,
+          quickFind: true,
+          activityIds:
+            'com.cainiao.wireless.homepage.view.activity.HomePageActivity',
+          matches:
+            'View[clickable=true] > @[text="关闭"] + [text="活动"] <<n [vid="browser_fragment_layout"]',
+          snapshotUrls: 'https://i.gkd.li/i/14033859',
+        },
       ],
     },
     {
       key: 2,
       name: '局部广告-包裹页面-地图底部横条广告',
-      activityIds: [
-        'com.taobao.cainiao.logistic.ui.view.LogisticDetailActivity',
-      ],
+      activityIds: 'com.taobao.cainiao.logistic.ui.view.LogisticDetailActivity',
       rules: 'View[desc="立即查看"] +2 ImageView[id=null]',
-      snapshotUrls: ['https://i.gkd.li/i/12914450'],
+      snapshotUrls: 'https://i.gkd.li/i/12914450',
     },
     {
       key: 4,
@@ -47,7 +54,7 @@ export default defineAppConfig({
       resetMatch: 'app',
       quickFind: true,
       rules: '[text="确认"] - [text="取消"]',
-      snapshotUrls: ['https://i.gkd.li/i/13042207'],
+      snapshotUrls: 'https://i.gkd.li/i/13042207',
     },
     {
       key: 5,
@@ -58,7 +65,7 @@ export default defineAppConfig({
       resetMatch: 'app',
       quickFind: true,
       rules: '[id="com.cainiao.wireless:id/gg_dialog_base_close"]',
-      snapshotUrls: ['https://i.gkd.li/i/13068573'],
+      snapshotUrls: 'https://i.gkd.li/i/13068573',
     },
     {
       key: 6,
