0ce62a5c403063774eb2343e7cbb7eacbe1fefcf
AIsouler <zy43220@gmail.com>
2024-03-30 14:52:08 +0800

    feat(南方航空): 功能类-长辈版弹窗

--- src/apps/com.csair.mbp.ts
+++ src/apps/com.csair.mbp.ts
@@ -10,7 +10,25 @@ export default defineAppConfig({
       quickFind: true,
       activityIds: 'com.csair.mbp.CSMBPActivity',
       rules: '[id="com.csair.mbp:id/close_btn"]',
-      snapshotUrls: ['https://i.gkd.li/i/13197497'],
+      snapshotUrls: 'https://i.gkd.li/i/13197497',
+    },
+    {
+      key: 4,
+      name: '功能类-长辈版弹窗',
+      desc: '点击[暂不开启]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.csair.mbp.CSMBPActivity',
+          matches: '[text="开启长辈版"] -2 [text="暂不开启"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/8744f8c6-7f9a-4f7b-a5c7-68de1701cd7b',
+          snapshotUrls: 'https://i.gkd.li/i/14810700',
+        },
+      ],
     },
   ],
 });
