f77b7e707ddf87e1f88d88de5075a168ba5aa66c
AIsouler <zy43220@gmail.com>
2024-04-01 19:28:30 +0800

    perf: 百度网盘

--- src/apps/com.baidu.netdisk.ts
+++ src/apps/com.baidu.netdisk.ts
@@ -3,66 +3,76 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'com.baidu.netdisk',
   name: '百度网盘',
-  deprecatedKeys: [0],
+  deprecatedKeys: [0, 3, 4, 5, 7, 10, 11],
   groups: [
     {
       key: 1,
-      name: '全屏广告-活动弹窗',
-      desc: '关闭各种活动弹窗信息',
+      name: '全屏广告-广告弹窗',
+      desc: '点击关闭',
       quickFind: true,
       rules: [
         {
-          key: 1,
-          name: '一刻相册推广弹窗',
-          activityIds: 'com.baidu.netdisk.ui.MainActivity',
-          matches:
-            '[id="com.baidu.netdisk:id/cl_content"] - [id="com.baidu.netdisk:id/iv_close"]',
-          snapshotUrls: ['https://i.gkd.li/i/12642505'],
+          key: 3,
+          activityIds: [
+            'com.baidu.netdisk.business.guide.dialog.lifeproduct.',
+            'com.baidu.netdisk.ui.MainActivity',
+            'com.baidu.netdisk.ui.Navigate',
+          ],
+          matches: '[vid="iv_close"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/12642505',
+            'https://i.gkd.li/i/12783106',
+            'https://i.gkd.li/i/12923937',
+            'https://i.gkd.li/i/13806852',
+            'https://i.gkd.li/i/14730106',
+            'https://i.gkd.li/i/14822799',
+          ],
         },
         {
-          key: 2,
-          name: 'VIP弹窗',
-          activityIds: 'com.baidu.netdisk.business.guide.dialog.lifeproduct.', // LifeV10GuideDialog
+          key: 4,
+          name: '相册页面激活无限空间弹窗',
+          activityIds:
+            'com.baidu.netdisk.cloudimage.ui.album.AlbumGuideOneImageDialog',
           matches:
-            '[id="com.baidu.netdisk:id/view_content_bg2"] - [id="com.baidu.netdisk:id/iv_close"]',
-          snapshotUrls: ['https://i.gkd.li/i/12923937'],
+            '@ImageView[id="com.baidu.netdisk:id/close_btn"] + ImageView[id="com.baidu.netdisk:id/bg_image"]',
+          snapshotUrls: 'https://i.gkd.li/i/12648987',
         },
       ],
     },
     {
       key: 2,
-      name: '局部广告-首页卡片广告',
+      name: '局部广告-卡片广告',
+      desc: '点击关闭',
       activityIds: 'com.baidu.netdisk.ui.MainActivity',
-      quickFind: true,
-      rules: '[id="com.baidu.netdisk:id/banner_item_close"]',
-      snapshotUrls: 'https://i.gkd.li/i/12706544',
-    },
-    {
-      key: 3,
-      name: '局部广告-首页热门广告',
-      activityIds: 'com.baidu.netdisk.ui.MainActivity',
-      quickFind: true,
-      rules:
-        '[id="com.baidu.netdisk:id/vf_content"] + [id="com.baidu.netdisk:id/close"]',
-      snapshotUrls: 'https://i.gkd.li/i/12706544',
-    },
-    {
-      key: 4,
-      name: '局部广告-"我的"页面限时福利',
-      activityIds: 'com.baidu.netdisk.ui.MainActivity',
-      quickFind: true,
-      rules: '@TextView + [text="专属福利"]',
-      snapshotUrls: 'https://i.gkd.li/i/12706549',
-    },
-    {
-      key: 5,
-      name: '全屏广告-相册页面激活无限空间弹窗',
-      quickFind: true,
-      activityIds:
-        'com.baidu.netdisk.cloudimage.ui.album.AlbumGuideOneImageDialog',
-      rules:
-        '@ImageView[id="com.baidu.netdisk:id/close_btn"] + ImageView[id="com.baidu.netdisk:id/bg_image"]',
-      snapshotUrls: 'https://i.gkd.li/i/12648987',
+      rules: [
+        {
+          key: 0,
+          name: '首页特惠广告',
+          quickFind: true,
+          matches: '[id="com.baidu.netdisk:id/banner_item_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/12706544',
+        },
+        {
+          key: 1,
+          name: '首页热门广告',
+          quickFind: true,
+          matches:
+            '[id="com.baidu.netdisk:id/vf_content"] + [id="com.baidu.netdisk:id/close"]',
+          snapshotUrls: 'https://i.gkd.li/i/12706544',
+        },
+        {
+          key: 2,
+          name: '[我的]页面限时福利',
+          matches: '@TextView + [text="专属福利"]',
+          snapshotUrls: 'https://i.gkd.li/i/12706549',
+        },
+        {
+          key: 3,
+          name: '续费横幅提示',
+          matches: 'View[desc="续费"] + ImageView',
+          snapshotUrls: 'https://i.gkd.li/i/12924036',
+        },
+      ],
     },
     {
       key: 6,
@@ -71,21 +81,8 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[text="立即更新"] -n [text="下次再说"]', //使用ID会导致误触（例如删除确认https://i.gkd.li/i/13069049）
+      rules: '[text="立即更新"] -2 [text="下次再说"]', //使用ID会导致误触（例如删除确认https://i.gkd.li/i/13069049）
       snapshotUrls: 'https://i.gkd.li/i/12863984',
-    },
-    {
-      key: 7,
-      name: '局部广告-续费横幅提示',
-      desc: '关闭续费横幅提示',
-      quickFind: true,
-      activityIds: 'com.baidu.netdisk.ui.MainActivity',
-      rules: [
-        {
-          matches: 'View[desc="续费"] + ImageView',
-          snapshotUrls: 'https://i.gkd.li/i/12924036',
-        },
-      ],
     },
     {
       key: 8,
@@ -96,7 +93,7 @@ export default defineAppConfig({
       resetMatch: 'app',
       quickFind: true,
       rules: 'ImageView[id="com.baidu.netdisk:id/dialog_cancel"]', //单独使用ID会导致误触（例如删除确认https://i.gkd.li/i/13069049）
-      snapshotUrls: ['https://i.gkd.li/i/12923936'],
+      snapshotUrls: 'https://i.gkd.li/i/12923936',
     },
     {
       key: 9,
@@ -108,34 +105,8 @@ export default defineAppConfig({
       snapshotUrls: 'https://i.gkd.li/i/14317054',
     },
     {
-      key: 10,
-      name: '全屏广告-看视频免费享极速下载弹窗',
-      desc: '自动点击x按钮',
-      quickFind: true,
-      activityIds: 'com.baidu.netdisk.ui.MainActivity',
-      rules:
-        'ViewGroup > [id="com.baidu.netdisk:id/background_image"] +n [id="com.baidu.netdisk:id/iv_close"]',
-      snapshotUrls: 'https://i.gkd.li/i/12783106',
-    },
-    {
-      key: 11,
-      name: '全屏广告-幸运券包弹窗',
-      desc: '自动点击关闭',
-      quickFind: true,
-      activityIds: 'com.baidu.netdisk.ui.MainActivity',
-      rules: [
-        {
-          matches: '[vid="iv_close"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/13806852',
-            'https://i.gkd.li/i/14730106',
-          ],
-        },
-      ],
-    },
-    {
       key: 12,
-      name: '局部广告-右下角悬浮卡片',
+      name: '局部广告-悬浮广告',
       desc: '点击关闭',
       rules: [
         {
