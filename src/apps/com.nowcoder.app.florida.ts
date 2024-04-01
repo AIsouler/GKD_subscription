94fab45f7987f6693a63c09c4e0170d04e24d8e8
AIsouler <zy43220@gmail.com>
2024-04-01 13:33:38 +0800

    feat(牛客): 开屏广告（补充规则）

--- src/apps/com.nowcoder.app.florida.ts
+++ src/apps/com.nowcoder.app.florida.ts
@@ -7,16 +7,22 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
+      actionMaximumKey: 0,
       rules: [
         {
           key: 0,
-          quickFind: true,
           matches:
             'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView <<n [id="android:id/content"]',
           snapshotUrls: 'https://i.gkd.li/i/13855443',
+        },
+        {
+          key: 1,
+          matches: '[text="跳过"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/14819443',
         },
       ],
     },
