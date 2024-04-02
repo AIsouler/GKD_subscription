3e7ec3d8afe3f22e2898d3299d604a122557762c
AIsouler <zy43220@gmail.com>
2024-04-02 20:07:50 +0800

    feat(99手游加速器): 开屏广告

--- src/apps/com.excean.na.ts
+++ src/apps/com.excean.na.ts
@@ -5,6 +5,22 @@ export default defineAppConfig({
   name: '99手游加速器',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules: [
+        {
+          key: 0,
+          matches:
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView <<n [id="android:id/content"]',
+          snapshotUrls: 'https://i.gkd.li/i/14835263',
+        },
+      ],
+    },
+    {
       key: 1,
       quickFind: true,
       name: '局部广告-卡片广告',
