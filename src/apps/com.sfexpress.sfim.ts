cff795796238f3b9e81df8463801bb6baa46530c
AIsouler <zy43220@gmail.com>
2024-04-03 13:20:12 +0800

    perf(丰声): 功能类-登录确认

--- src/apps/com.sfexpress.sfim.ts
+++ src/apps/com.sfexpress.sfim.ts
@@ -6,18 +6,32 @@ export default defineAppConfig({
   groups: [
     {
       key: 10,
-      name: '功能类-CAS安全认证-自动登录',
+      name: '功能类-登录确认',
       desc: '自动点击"登录"按钮',
-      activityIds: [
-        'com.sfexpress.sfim.scan.view.SfScanActivity',
-        'com.sfexpress.sfim.microservice.commonservice.ui.MicroAppContainerActivity',
-        'com.sfexpress.sfim.sdkuikit.widget.dialog.NoticeDialog',
-      ],
-      rules: '[text="CAS安全认证"] >n [text="登录"]',
-      snapshotUrls: [
-        'https://i.gkd.li/i/12745239',
-        'https://i.gkd.li/i/12745240',
-        'https://i.gkd.li/i/12745241',
+      quickFind: true,
+      rules: [
+        {
+          key: 0,
+          activityIds: [
+            'com.sfexpress.sfim.scan.view.SfScanActivity',
+            'com.sfexpress.sfim.microservice.commonservice.ui.MicroAppContainerActivity',
+            'com.sfexpress.sfim.sdkuikit.widget.dialog.NoticeDialog',
+          ],
+          matches: '@[text="登录"] <<n [vid="flContainer"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/12745239',
+            'https://i.gkd.li/i/12745240',
+            'https://i.gkd.li/i/12745241',
+            'https://i.gkd.li/i/14830687',
+          ],
+        },
+        {
+          key: 1,
+          activityIds:
+            'com.sfexpress.sfim.middleplatform.ui.SFMiddlePlatformLoginActivity',
+          matches: '[text="登录"]',
+          snapshotUrls: 'https://i.gkd.li/i/14830682',
+        },
       ],
     },
   ],
