0c49b13d7a350e2c1c16d6e39fc6f6282377f04b
AIsouler <zy43220@gmail.com>
2024-04-05 17:34:49 +0800

    feat(快手极速版): 全屏广告-红包弹窗

--- src/apps/com.kuaishou.nebula.ts
+++ src/apps/com.kuaishou.nebula.ts
@@ -20,5 +20,20 @@ export default defineAppConfig({
       rules: '@[vid="close_btn"] + [text="朋友推荐"]',
       snapshotUrls: 'https://i.gkd.li/i/14310639',
     },
+    {
+      key: 3,
+      name: '全屏广告-红包弹窗',
+      rules: [
+        {
+          key: 1,
+          quickFind: true,
+          activityIds: 'com.yxcorp.gifshow.HomeActivity',
+          matches: '@ImageView[clickable=true] -2 [text$="免费红包"]',
+          exampleUrls:
+            'https://m.gkd.li/101449500/f7bbd1db-f519-4ff9-96cb-4cb5b2f483a2',
+          snapshotUrls: 'https://i.gkd.li/i/14879912',
+        },
+      ],
+    },
   ],
 });
