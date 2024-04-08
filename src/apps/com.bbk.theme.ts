fe98e2555d4a2176b39214c9889b523112a24f61
AIsouler <zy43220@gmail.com>
2024-04-08 19:20:31 +0800

    feat(i 主题): 全屏广告、功能类-[添加主题卡片至桌面]弹窗

---
+++ src/apps/com.bbk.theme.ts
@@ -0,0 +1,37 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.bbk.theme',
+  name: 'i 主题',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-推广弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.bbk.theme.Theme',
+          matches: '[vid="card_close_img"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/8207ec80-6e9d-4b9e-9880-7370d826c405',
+          snapshotUrls: 'https://i.gkd.li/i/14904298',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '功能类-[添加主题卡片至桌面]弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          matches: '@[vid="cancel"] + * > [text="智慧桌面"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/45df70c2-55bc-428a-91cf-40dff23ae2af',
+          snapshotUrls: 'https://i.gkd.li/i/14904036',
+        },
+      ],
+    },
+  ],
+});
