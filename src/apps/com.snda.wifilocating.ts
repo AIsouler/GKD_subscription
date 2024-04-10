f049eea4cac4948aa7b5751d6e96a1818ecbb64b
AIsouler <zy43220@gmail.com>
2024-04-10 13:14:55 +0800

    feat(WiFi万能钥匙): 全屏广告（补充规则）

--- src/apps/com.snda.wifilocating.ts
+++ src/apps/com.snda.wifilocating.ts
@@ -7,22 +7,29 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '全屏广告-"好看"页面广告弹窗',
-      desc: '点击X',
-      quickFind: true,
+      name: '全屏广告-广告弹窗',
+      desc: '点击关闭',
       rules: [
         {
+          key: 0,
+          quickFind: true,
           activityIds: 'com.lantern.start.main.activity.HomeMainActivity',
           matches: '[vid="interstitial_close_iv"]',
           snapshotUrls: 'https://i.gkd.li/i/14032794',
         },
         {
+          key: 1,
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
           matches: '[text="反馈"] + View[visibleToUser=true]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/e6a94c34-6580-4bf5-8c44-16c9c9534be4',
           snapshotUrls: 'https://i.gkd.li/i/14256643',
+        },
+        {
+          key: 2,
+          quickFind: true,
+          activityIds: 'com.zm.wfsdk.core.activity.WfInterstitialActivity',
+          matches: '[vid="wf_interstitial_close_iv"]',
+          snapshotUrls: 'https://i.gkd.li/i/14927523',
         },
       ],
     },
