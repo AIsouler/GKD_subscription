0edf86d67aec6ebd6b39ec6cf4d1a63db1ca8f3a
AIsouler <zy43220@gmail.com>
2024-04-12 22:03:19 +0800

    perf(酷安): 分段广告

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -47,7 +47,7 @@ export default defineAppConfig({
     {
       key: 0,
       name: '分段广告-卡片广告',
-      desc: '点击卡片右上角按钮->免广告-点击不感兴趣->选择关闭原因-点击不感兴趣',
+      desc: '点击卡片右上角按钮->免广告-点击关闭->选择关闭原因-点击不感兴趣',
       quickFind: true,
       activityIds: [
         'com.coolapk.market.view.main.MainActivity', // 缺少快照
@@ -59,8 +59,7 @@ export default defineAppConfig({
         {
           key: 1,
           name: '点击右上角x按钮',
-          matches:
-            '[id="com.coolapk.market:id/ad_time_view"||id="com.coolapk.market:id/top_text_view"||id="com.coolapk.market:id/ad_text_view"] +n [id="com.coolapk.market:id/close_view"]',
+          matches: '[id="com.coolapk.market:id/close_view"]',
           snapshotUrls: [
             'https://i.gkd.li/i/12707506',
             'https://i.gkd.li/i/12642094',
@@ -72,27 +71,20 @@ export default defineAppConfig({
         {
           preKeys: [1],
           key: 2,
-          name: '去广告/免广告-点击不感兴趣',
-          matches:
-            'Button[text$="广告"] <n LinearLayout[childCount=2] > Button[text="不感兴趣"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12707509',
-            'https://i.gkd.li/i/12642132',
-            'https://i.gkd.li/i/12642155',
-            'https://i.gkd.li/i/12774753',
-          ],
+          name: '去广告/免广告-点击关闭',
+          matches: '[text*="广告"] <n * > [text="关闭"]',
+          snapshotUrls: 'https://i.gkd.li/i/14959519',
         },
         {
           preKeys: [1, 2],
           key: 3,
           name: '选择关闭原因-点击不感兴趣',
-          matches: ['@LinearLayout > TextView[text="不感兴趣"]'],
+          matches: '@LinearLayout > TextView[text="不感兴趣"]',
           snapshotUrls: [
             'https://i.gkd.li/i/12472633',
             'https://i.gkd.li/i/12655713',
             'https://i.gkd.li/i/12660759',
             'https://i.gkd.li/i/12706437',
-            'https://i.gkd.li/i/13786886', // 没有id
           ],
         },
       ],
