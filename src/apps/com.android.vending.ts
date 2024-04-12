58df4825297631fb8bb145501e3f5c3fb986877e
AIsouler <zy43220@gmail.com>
2024-04-12 20:39:51 +0800

    feat(Google Play 商店): 全屏广告

--- src/apps/com.android.vending.ts
+++ src/apps/com.android.vending.ts
@@ -17,5 +17,20 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 2,
+      name: '全屏广告-[体验 Google Play Pass]弹窗',
+      desc: '点击[以后再说]',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.google.android.finsky.activities.MainActivity',
+          matches: '[text^="体验 Google Play Pass"] +3 * > [text="以后再说"]',
+          snapshotUrls: 'https://i.gkd.li/i/14958783',
+        },
+      ],
+    },
   ],
 });
