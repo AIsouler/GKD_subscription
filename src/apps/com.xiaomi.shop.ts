2117ddd98a53f48d710c277ca3929579367e13c5
AIsouler <zy43220@gmail.com>
2024-04-13 10:30:35 +0800

    perf(小米商城): 全屏广告

--- src/apps/com.xiaomi.shop.ts
+++ src/apps/com.xiaomi.shop.ts
@@ -11,18 +11,15 @@ export default defineAppConfig({
       rules: [
         {
           key: 0,
-          quickFind: true,
           activityIds: [
             'com.xiaomi.shop2.activity.MainActivity',
             'com.xiaomi.shop.plugin.homepage.utils.AdDialog',
           ],
           matches:
-            '@ImageView[index=1][clickable=true] <2 LinearLayout[childCount=2] < RelativeLayout < [id="android:id/content"]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/85d8e8e9-b438-4904-86e2-4cb53735a70c',
+            '[id="android:id/content"] > RelativeLayout > LinearLayout[childCount=2] > ImageView[index=1][clickable=true]',
           snapshotUrls: [
-            'https://i.gkd.li/i/14393039',
             'https://i.gkd.li/i/12649330',
+            'https://i.gkd.li/i/14393039',
           ],
         },
       ],
