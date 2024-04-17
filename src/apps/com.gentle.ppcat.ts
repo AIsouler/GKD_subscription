b0cbc61f2355caa2b7d3bfc29958b90167ae6a03
AIsouler <zy43220@gmail.com>
2024-04-17 18:39:13 +0800

    fix(皮皮喵): 开屏广告失效

--- src/apps/com.gentle.ppcat.ts
+++ src/apps/com.gentle.ppcat.ts
@@ -10,11 +10,11 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      actionMaximumKey: 0,
       rules: [
         {
           key: 0,
-          matches: '@FrameLayout - LinearLayout [text$="惊喜奖励"]',
+          quickFind: true,
+          matches: '@FrameLayout - LinearLayout >2 [text$="惊喜奖励"]',
           snapshotUrls: [
             'https://i.gkd.li/i/12777048',
             'https://i.gkd.li/i/12858015',
@@ -22,8 +22,9 @@ export default defineAppConfig({
         },
         {
           key: 1,
+          quickFind: true,
           matches: '[text="放弃奖励离开"]',
-          snapshotUrls: 'https://i.gkd.li/i/12777058',
+          snapshotUrls: 'https://i.gkd.li/i/15008493',
         },
         {
           key: 2,
@@ -33,11 +34,7 @@ export default defineAppConfig({
         {
           key: 3,
           matches: '[desc="关闭喵"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12777059',
-            'https://i.gkd.li/i/12777054', // activityId: 'com.gentle.ppcat.MainActivity'
-            'https://i.gkd.li/i/13797494', // activityId: 'com.hihonor.android.launcher.drawer.DrawerLauncher'
-          ],
+          snapshotUrls: 'https://i.gkd.li/i/13797494',
         },
       ],
     },
