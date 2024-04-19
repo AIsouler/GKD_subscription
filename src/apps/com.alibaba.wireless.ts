eba5ac18ecd394d906c3eb198c8bcbee7e910925
AIsouler <zy43220@gmail.com>
2024-04-19 11:48:18 +0800

    fix(阿里巴巴): 更新提示误触

--- src/apps/com.alibaba.wireless.ts
+++ src/apps/com.alibaba.wireless.ts
@@ -24,8 +24,7 @@ export default defineAppConfig({
         },
         {
           key: 1,
-          matches:
-            '@ImageView[visibleToUser=true] < * - * > [text*="立即更新"]',
+          matches: '[vid="update_pop_cancel_btn"]',
           snapshotUrls: 'https://i.gkd.li/i/15004486',
         },
       ],
