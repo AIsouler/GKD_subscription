ee353940a34869d8d1a942ea2c0e69a11ea34e31
AIsouler <zy43220@gmail.com>
2024-04-27 17:23:00 +0800

    feat: applist

--- src/globalDefaultApps.ts
+++ src/globalDefaultApps.ts
@@ -188,6 +188,7 @@ export const openAdWhiteListAppIDs = new Set([
   'com.bbk.appstore', // vivo应用商店
   'com.huawei.appmarket', // 华为应用市场
   'com.miui.player', // 小米音乐
+  'com.tencent.southpole.appstore', // 黑鲨应用市场
 ]);
 
 // 更新提示白名单
