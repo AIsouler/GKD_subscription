9c3905adbf4fcec30d3fd71c7e84edc06b73971b
AIsouler <zy43220@gmail.com>
2024-04-28 19:23:50 +0800

    feat(掌上道聚城): 开屏广告

--- src/apps/com.tencent.djcity.ts
+++ src/apps/com.tencent.djcity.ts
@@ -5,6 +5,22 @@ export default defineGkdApp({
   name: '掌上道聚城',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          action: 'clickCenter',
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/15137209',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '全屏广告-弹窗广告',
       desc: '点击关闭',
