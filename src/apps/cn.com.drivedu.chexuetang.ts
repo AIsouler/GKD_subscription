1fbdaca377e2fd3a416a61ea4759e67b95d6e723
AIsouler <zy43220@gmail.com>
2024-04-29 19:21:49 +0800

    feat(车学堂): 局部广告-卡片广告（补充规则）

--- src/apps/cn.com.drivedu.chexuetang.ts
+++ src/apps/cn.com.drivedu.chexuetang.ts
@@ -8,15 +8,25 @@ export default defineGkdApp({
       key: 1,
       name: '局部广告-卡片广告',
       desc: '点击关闭',
+      quickFind: true,
       rules: [
         {
-          quickFind: true,
+          key: 0,
           activityIds: 'cn.com.drivedu.chexuetang.main.activity.MainActivity_',
           matches:
             '[desc^="dislike"] > @View[clickable=true][visibleToUser=true] <<n [vid="fragment_ad"]',
           exampleUrls:
             'https://m.gkd.li/57941037/16020d11-8e5b-4fb5-8e57-6baaa13ce6a9',
           snapshotUrls: 'https://i.gkd.li/i/15137765',
+        },
+        {
+          key: 1,
+          activityIds: 'cn.com.drivedu.chexuetang.main.activity.MainActivity_',
+          matches:
+            'FrameLayout[childCount=5] > FrameLayout > @ImageView[visibleToUser=true] <<n [vid="fragment_ad"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/402dc635-354e-4c4b-a6b9-0eebeecfee0d',
+          snapshotUrls: 'https://i.gkd.li/i/15148368',
         },
       ],
     },
