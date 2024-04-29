b06350875f1263a3bfe28eb5645b0f15654ef7c8
AIsouler <zy43220@gmail.com>
2024-04-29 12:58:33 +0800

    feat(电池容量检测管理): 开屏广告（补充规则）

--- src/apps/com.zlfcapp.batterymanager.ts
+++ src/apps/com.zlfcapp.batterymanager.ts
@@ -7,12 +7,19 @@ export default defineGkdApp({
     {
       key: 1,
       name: '开屏广告',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
+      actionMaximumKey: 0,
       rules: [
         {
-          quickFind: true,
+          key: 0,
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/15140838',
+        },
+        {
+          key: 1,
           matches:
             'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView <<n [id="android:id/content"]',
           snapshotUrls: 'https://i.gkd.li/i/13960341',
