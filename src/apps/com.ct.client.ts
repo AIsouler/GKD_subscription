c9d6e698e129bb2345b964ac353af929b93628d2
AIsouler <zy43220@gmail.com>
2024-05-04 20:03:16 +0800

    feat(中国电信): 通知提示（补充规则）

--- src/apps/com.ct.client.ts
+++ src/apps/com.ct.client.ts
@@ -80,8 +80,20 @@ export default defineGkdApp({
       actionMaximum: 1,
       resetMatch: 'app',
       // quickFind: true, 实机测试使用无法跳过
-      rules: 'ImageView[id="com.ct.client:id/msg_close"]',
-      snapshotUrls: 'https://i.gkd.li/i/13043522',
+      rules: [
+        {
+          key: 0,
+          matches: 'ImageView[id="com.ct.client:id/msg_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/13043522',
+        },
+        {
+          key: 1,
+          quickFind: true,
+          activityIds: 'com.ct.client.activity.MainActivity',
+          matches: '[text^="开启消息通知"] <n * > [vid="ivClose"]',
+          snapshotUrls: 'https://i.gkd.li/i/15209597',
+        },
+      ],
     },
     {
       key: 6,
