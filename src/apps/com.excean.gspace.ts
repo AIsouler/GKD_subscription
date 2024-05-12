0959fddf00d2671ae7b763aa2d2166a7930bd1d7
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-05-12 09:30:24 +0800

    feat(OurPlay): 全屏广告-弹窗广告

--- src/apps/com.excean.gspace.ts
+++ src/apps/com.excean.gspace.ts
@@ -8,25 +8,33 @@ export default defineGkdApp({
       key: 1,
       name: '全屏广告-弹窗广告',
       forcedTime: 10000,
-      activityIds: [
-        'com.excelliance.kxqp.splash.SplashActivity',
-        'com.excelliance.kxqp.gs.main.MainActivity',
-      ],
       rules: [
         {
           key: 0,
+          activityIds: 'com.excelliance.kxqp.splash.SplashActivity',
           matches:
             'FrameLayout[childCount=2] > LinearLayout[childCount=2] + FrameLayout[childCount=1] > ImageView',
           snapshotUrls: 'https://i.gkd.li/i/13302890',
         },
         {
           key: 1,
+          activityIds: [
+            'com.excelliance.kxqp.splash.SplashActivity',
+            'com.excelliance.kxqp.gs.main.MainActivity',
+          ],
           matches:
             '[id="android:id/content"] >(3,4) FrameLayout[childCount>4] > FrameLayout[childCount=1] > ImageView',
           snapshotUrls: [
             'https://i.gkd.li/i/13447122',
             'https://i.gkd.li/i/14862348',
           ],
+        },
+        {
+          key: 2,
+          quickFind: true,
+          activityIds: 'com.excelliance.kxqp.gs.main.MainActivity',
+          matches: '[vid="close_render_ad"]',
+          snapshotUrls: 'https://i.gkd.li/i/15284762',
         },
       ],
     },
