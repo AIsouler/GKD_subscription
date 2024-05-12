f15a250e7c2b709c7fbd89ece397ff73ce463162
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-05-12 09:26:22 +0800

    feat(小爱音箱): 权限提示-请求开启定位权限弹窗

--- src/apps/com.xiaomi.mico.ts
+++ src/apps/com.xiaomi.mico.ts
@@ -12,5 +12,15 @@ export default defineGkdApp({
         '[id="com.xiaomi.mico:id/adImage"] + [id="com.xiaomi.mico:id/ivCloseAd"]',
       snapshotUrls: 'https://i.gkd.li/i/12745621',
     },
+    {
+      key: 2,
+      name: '权限提示-请求开启定位权限弹窗',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '@[text="取消"] < * - * >2 [text*="开启位置服务"]',
+      snapshotUrls: 'https://i.gkd.li/i/15284753',
+    },
   ],
 });
