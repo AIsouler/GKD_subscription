589be438c8d18a02a3b67bda7580c2821148bad6
AIsouler <zy43220@gmail.com>
2024-05-17 11:17:23 +0800

    feat(视趣): 全屏广告-弹窗广告'

---
+++ src/apps/com.sqsp.movie.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.sqsp.movie',
+  name: '视趣',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches:
+            '[text="反馈"] -2 @View[visibleToUser=true][childCount=1] > Image[text=""]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/640c42bf-5279-4ab5-a3bc-e264a99af2ac',
+          snapshotUrls: 'https://i.gkd.li/i/15342445',
+        },
+      ],
+    },
+  ],
+});
