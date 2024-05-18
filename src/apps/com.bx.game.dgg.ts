0271083b22c0c989cef01ea4ed9d115ed530e14a
AIsouler <zy43220@gmail.com>
2024-05-18 19:13:41 +0800

    feat(看剧): 全屏广告（补充规则）、通知提示-[温馨提示]弹窗

--- src/apps/com.bx.game.dgg.ts
+++ src/apps/com.bx.game.dgg.ts
@@ -14,9 +14,39 @@ export default defineGkdApp({
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
           matches: '[text="反馈"] -2 @View > Image[childCount=0][text=""]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/e1cfbd61-4592-4330-b550-8ac9c0783947',
           snapshotUrls: 'https://i.gkd.li/i/15335006',
+        },
+        {
+          key: 1,
+          quickFind: true,
+          activityIds: 'com.anythink.basead.ui.ATPortraitTranslucentActivity',
+          matches: '[vid="anythink_myoffer_btn_close_id"]',
+          snapshotUrls: 'https://i.gkd.li/i/15362931',
+        },
+        {
+          key: 2,
+          quickFind: true,
+          activityIds: 'com.bx.game.dgg.MainActivity',
+          matches:
+            'ImageView < @ViewGroup[clickable=true] < ViewGroup +2 * > [text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/i/15362927',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '通知提示-[温馨提示]弹窗',
+      desc: '点击[我知道了]',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.bx.game.dgg.MainActivity',
+          matches: '[desc="我知道了"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/8a825661-5595-4577-835c-2a0b2d41bc14',
+          snapshotUrls: 'https://i.gkd.li/i/15362930',
         },
       ],
     },
