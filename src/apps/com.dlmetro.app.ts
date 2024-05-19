054c032ae7ec8e8c6d0e1e1274e4d624b66185df
AIsouler <zy43220@gmail.com>
2024-05-19 19:59:56 +0800

    feat(大连交通e出行): 全屏广告-弹窗广告

---
+++ src/apps/com.dlmetro.app.ts
@@ -0,0 +1,23 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.dlmetro.app',
+  name: '大连交通e出行',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.bwton.metro.tabindicator.business.MainTabActivity',
+          matches: '[vid="iv_close"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/78f79ab0-c224-40b8-9cae-e4a3366660db',
+          snapshotUrls: 'https://i.gkd.li/i/15369247',
+        },
+      ],
+    },
+  ],
+});
