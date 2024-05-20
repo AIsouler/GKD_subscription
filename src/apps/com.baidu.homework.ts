0032ce1128a95f146da1e484eff060b190dbdc4e
AIsouler <zy43220@gmail.com>
2024-05-20 16:11:46 +0800

    feat(作业帮): 全屏广告-弹窗广告

---
+++ src/apps/com.baidu.homework.ts
@@ -0,0 +1,25 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.baidu.homework',
+  name: '作业帮',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          matchTime: 10000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          activityIds: 'com.baidu.homework.activity.index.IndexActivity',
+          matches: '[vid="ad_dialog_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/15374440',
+        },
+      ],
+    },
+  ],
+});
