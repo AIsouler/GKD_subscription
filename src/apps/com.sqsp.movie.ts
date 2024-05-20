9df4120630d933e81fb0006b51f91467aa7a850f
AIsouler <zy43220@gmail.com>
2024-05-20 16:01:02 +0800

    perf(视趣): 全屏广告

--- src/apps/com.sqsp.movie.ts
+++ src/apps/com.sqsp.movie.ts
@@ -13,10 +13,13 @@ export default defineGkdApp({
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
           matches:
-            '[text="反馈"] -2 @View[visibleToUser=true][childCount=1] > Image[text=""]',
+            '[text="反馈"] -(2,4) @View[visibleToUser=true][childCount=1] > Image[text=""]',
           exampleUrls:
             'https://m.gkd.li/57941037/640c42bf-5279-4ab5-a3bc-e264a99af2ac',
-          snapshotUrls: 'https://i.gkd.li/i/15342445',
+          snapshotUrls: [
+            'https://i.gkd.li/i/15342445',
+            'https://i.gkd.li/i/15374240',
+          ],
         },
       ],
     },
