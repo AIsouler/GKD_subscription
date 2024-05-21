44b7b59b90b54d23bdd23ba52aa49838150203b7
LingEmberMaple <69679868+LingEmberMaple@users.noreply.github.com>
2024-05-21 09:46:28 +0000

    feat(系统更新)!: renamed [更新提示-MIUI更新弹窗] to [更新提示-智能更新弹窗] and optimized performance

--- src/apps/com.android.updater.ts
+++ src/apps/com.android.updater.ts
@@ -6,15 +6,16 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
-      name: '更新提示-MIUI更新弹窗',
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      rules:
-        '[id="com.android.updater:id/alertTitle"][text="开启智能更新"] < * +2 * > Button[id="android:id/button2"][text="暂不开启"]',
+      name: '更新提示-智能更新弹窗',
+      quickFind: true,
+      rules: [
+        '[vid="alertTitle"][text="开启智能更新"]',
+        '[id="android:id/button2"][text="暂不开启"]',
+      ],
       snapshotUrls: [
         'https://i.gkd.li/i/12715712',
         'https://i.gkd.li/i/12749906',
+        'https://i.gkd.li/i/15386937', // quickFind
       ],
     },
   ],
