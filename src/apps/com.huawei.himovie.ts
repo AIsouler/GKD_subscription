e4c05a2e38edcc5bfa82987e819e0e75c11505b6
AIsouler <zy43220@gmail.com>
2024-06-02 10:29:10 +0800

    perf(华为视频): 开屏广告

--- src/apps/com.huawei.himovie.ts
+++ src/apps/com.huawei.himovie.ts
@@ -7,19 +7,14 @@ export default defineGkdApp({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
-          action: 'clickCenter',
-          matches: '[id="com.huawei.hms.ads.uiengine:id/hiad_skip_text"]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/0fdba1cd-ea19-4e7e-9a1e-14781fdb5242',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12814007',
-            'https://i.gkd.li/i/12819351',
-          ],
+          matches: '[vid="hiad_skip_text"]',
+          snapshotUrls: 'https://i.gkd.li/i/15523945',
         },
       ],
     },
