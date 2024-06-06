f18efc6173168b14418f678efe358ed4a4eb6e46
AIsouler <zy43220@gmail.com>
2024-06-06 15:26:27 +0800

    feat(交管12123): 功能类-确认退出

--- src/apps/com.tmri.app.main.ts
+++ src/apps/com.tmri.app.main.ts
@@ -21,5 +21,18 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '功能类-确认退出',
+      desc: '点击[确认]',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.tmri.app.ui.activity.main.MainTabHostNewActivity',
+          matches: ['[text="是否确认退出?"]', '[text="确认"]'],
+          snapshotUrls: 'https://i.gkd.li/i/15597285',
+        },
+      ],
+    },
   ],
 });
