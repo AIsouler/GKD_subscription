5e308209f296e4b89a5b9eb0307d40407e3dd654
AIsouler <zy43220@gmail.com>
2024-06-08 11:01:05 +0800

    feat(快对): 开屏广告

--- src/apps/com.kuaiduizuoye.scan.ts
+++ src/apps/com.kuaiduizuoye.scan.ts
@@ -5,6 +5,29 @@ export default defineGkdApp({
   name: '快对',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      actionMaximumKey: 0,
+      rules: [
+        {
+          key: 0,
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/15746254',
+        },
+        {
+          key: 1,
+          action: 'clickCenter',
+          matches:
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true][visibleToUser=true] + TextView[text=null] <<n [id="android:id/content"]',
+          snapshotUrls: 'https://i.gkd.li/i/15741399',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '全屏广告-首页弹窗广告',
       activityIds: 'com.hihonor.android.launcher.drawer.DrawerLauncher',
