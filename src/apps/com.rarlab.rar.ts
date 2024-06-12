d19ac869aeb90c53b5c6521930026ecc20bace27
david082321 <7046617+david082321@users.noreply.github.com>
2024-06-12 16:58:46 +0800

    全屏广告-订购 RAR 弹窗 适配繁体 (#241)
    Signed-off-by: david082321 <7046617+david082321@users.noreply.github.com>
    Co-authored-by: AIsouler <zy43220@gmail.com>

--- src/apps/com.rarlab.rar.ts
+++ src/apps/com.rarlab.rar.ts
@@ -8,9 +8,14 @@ export default defineGkdApp({
       key: 10,
       name: '全屏广告-订购 RAR 弹窗',
       desc: '自动点击"放弃"',
+      quickFind: true,
       activityIds: 'com.rarlab.rar.MainActivity',
-      rules: '[text="订购"] + [text="放弃"]',
-      snapshotUrls: 'https://i.gkd.li/i/12781596',
+      rules:
+        '[text~="(订购|訂閱)"] + [id="android:id/button1"][text~="(放弃|解除)"]',
+      snapshotUrls: [
+        'https://i.gkd.li/i/12781596',
+        'https://i.gkd.li/i/15801328',
+      ],
     },
   ],
 });
