45f2317e71e43f290619088015c2ac6237741f0a
AIsouler <zy43220@gmail.com>
2024-06-15 21:21:06 +0800

    feat(CamHipro): 局部广告-卡片广告

---
+++ src/apps/com.hichip.campro.ts
@@ -0,0 +1,21 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.hichip.campro',
+  name: 'CamHipro',
+  groups: [
+    {
+      key: 1,
+      name: '局部广告-卡片广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'main.MainActivity',
+          matches: '[vid="iv_close_ad"]',
+          snapshotUrls: 'https://i.gkd.li/i/15847732',
+        },
+      ],
+    },
+  ],
+});
