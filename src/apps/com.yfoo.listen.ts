1c500edbc506281c36aa3ca2c93f9979fd8be030
AIsouler <zy43220@gmail.com>
2024-06-16 18:23:20 +0800

    feat(聆听音乐): 全屏广告-弹窗广告

---
+++ src/apps/com.yfoo.listen.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.yfoo.listen',
+  name: '聆听音乐',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-弹窗广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          activityIds: 'com.yfoo.listenx.activity.HomeActivity',
+          matches: '@[text="取消"] < * > [text="下载"]',
+          snapshotUrls: 'https://i.gkd.li/i/15856014',
+        },
+      ],
+    },
+  ],
+});
