3a8f13565e6b10f00b4e20ff8b5da678f3e4fb6d
AIsouler <zy43220@gmail.com>
2024-06-17 14:20:51 +0800

    feat(华为阅读): 开屏广告

---
+++ src/apps/com.huawei.hwread.dz.ts
@@ -0,0 +1,23 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.huawei.hwread.dz',
+  name: '华为阅读',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          action: 'clickCenter', // https://github.com/AIsouler/GKD_subscription/issues/246
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/15862073',
+        },
+      ],
+    },
+  ],
+});
