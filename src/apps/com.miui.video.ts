9fea3730ddb66c9e7d276227c5f7aa483d8680ae
AIsouler <zy43220@gmail.com>
2024-07-01 21:03:04 +0800

    feat(小米视频): 分段广告-卡片广告（补充规则）

--- src/apps/com.miui.video.ts
+++ src/apps/com.miui.video.ts
@@ -8,12 +8,22 @@ export default defineGkdApp({
       key: 1,
       name: '分段广告-卡片广告',
       desc: '该广告的第二段需开启“智能服务”的对应规则才能关闭',
+      quickFind: true,
       rules: [
         {
-          quickFind: true,
+          key: 0,
           activityIds:
             'com.miui.video.feature.detail.NewShortVideoDetailActivity',
           matches: '[vid="v_banner_bottom_right"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/cd25f4a5-64db-4471-8eea-3d84a34ffb72',
+          snapshotUrls: 'https://i.gkd.li/i/16044569',
+        },
+        {
+          key: 1,
+          activityIds:
+            'com.miui.video.feature.detail.NewShortVideoDetailActivity',
+          matches: '[vid="iv_ad"][visibleToUser=true]',
           exampleUrls:
             'https://m.gkd.li/57941037/cd25f4a5-64db-4471-8eea-3d84a34ffb72',
           snapshotUrls: 'https://i.gkd.li/i/16044569',
