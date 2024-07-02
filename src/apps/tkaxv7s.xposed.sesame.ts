ef9d49d4cd99adafac633529fd3272236748271f
AIsouler <zy43220@gmail.com>
2024-07-02 09:04:38 +0800

    feat(芝麻粒): 通知提示-提示弹窗

---
+++ src/apps/tkaxv7s.xposed.sesame.ts
@@ -0,0 +1,26 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'tkaxv7s.xposed.sesame',
+  name: '芝麻粒',
+  groups: [
+    {
+      key: 1,
+      name: '通知提示-提示弹窗',
+      desc: '点击[我知道了]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'tkaxv7s.xposed.sesame.ui.MainActivity',
+          matches: '[text="我知道了"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/75d1d639-5234-4ad4-865d-2b1617994a16',
+          snapshotUrls: 'https://i.gkd.li/i/16060201',
+        },
+      ],
+    },
+  ],
+});
