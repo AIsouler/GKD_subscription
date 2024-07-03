af8d1c2c5005e91ad62625245f7680d440dd11b6
AIsouler <zy43220@gmail.com>
2024-07-03 16:06:44 +0800

    feat(Google Play 商店): 全屏广告-弹窗广告（补充规则）

--- src/apps/com.android.vending.ts
+++ src/apps/com.android.vending.ts
@@ -45,6 +45,13 @@ export default defineGkdApp({
           matches: '[text="以后再说"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/15335350',
         },
+        {
+          key: 2,
+          quickFind: true,
+          activityIds: 'com.google.android.finsky.activities.MainActivity',
+          matches: ['[text*="推荐内容"]', '[text="不用了"]'],
+          snapshotUrls: 'https://i.gkd.li/i/16079813',
+        },
       ],
     },
   ],
