52371f9e9f3203e2fea922ec5004d8e01bcdd5a1
AIsouler <zy43220@gmail.com>
2024-07-04 18:24:27 +0800

    feat(云服务): 更新提示

---
+++ src/apps/com.heytap.cloud.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.heytap.cloud',
+  name: '云服务',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      desc: '点击关闭',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.heytap.cloud.home.ui.CloudSettingsActivity2',
+          matches: ['[text="检测到新版本"]', '[vid="cancel"]'],
+          snapshotUrls: 'https://i.gkd.li/i/16097208',
+        },
+      ],
+    },
+  ],
+});
