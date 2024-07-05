827cd7cfdb858b400d90d0fec4e93de2b55fc8dc
AIsouler <zy43220@gmail.com>
2024-07-05 15:39:14 +0800

    feat(RQrun): 功能类-自动签到

---
+++ src/apps/cn.runningquotient.rq.ts
@@ -0,0 +1,41 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'cn.runningquotient.rq',
+  name: 'RQrun',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-自动签到',
+      desc: '点击右侧[签到福利]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'cn.runningquotient.rq.page.common_page.MainActivity',
+          matches: '[vid="iv_main_checkin_entry"]',
+          snapshotUrls: 'https://i.gkd.li/i/16105502',
+        },
+        {
+          preKeys: [0],
+          key: 1,
+          activityIds:
+            'cn.runningquotient.rq.page.common_page.CommonContainerActivity',
+          matches: '@[text="我知道了"] <<n [vid="common_h5_container"]',
+          snapshotUrls: 'https://i.gkd.li/i/16105938',
+        },
+        {
+          preKeys: [1],
+          key: 2,
+          activityIds:
+            'cn.runningquotient.rq.page.common_page.CommonContainerActivity',
+          matches: '[vid="iv_setting_header_view_left"]',
+          snapshotUrls: 'https://i.gkd.li/i/16105942',
+        },
+      ],
+    },
+  ],
+});
