1c82fd10ae20aff5738f0b0638b11687e658d92f
AIsouler <zy43220@gmail.com>
2024-07-07 12:49:06 +0800

    perf(点心云): 开屏广告

--- src/apps/com.dianxinai.mobile.ts
+++ src/apps/com.dianxinai.mobile.ts
@@ -7,16 +7,14 @@ export default defineGkdApp({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
-          matches:
-            '@ViewGroup[clickable=true] > TextView[text$="s" && text.length=2]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/a4b9c558-1d1f-4964-9545-95533253c9d1',
-          snapshotUrls: 'https://i.gkd.li/i/12847518',
+          matches: '[text$="s"][text.length=2]',
+          snapshotUrls: 'https://i.gkd.li/i/16115075',
         },
       ],
     },
