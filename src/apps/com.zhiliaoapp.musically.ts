28805108ec57b1bd4693fc4befa9a0b178331fac
AIsouler <zy43220@gmail.com>
2024-07-12 17:33:59 +0800

    feat(TikTok): 全屏广告-弹窗广告（补充规则）

--- src/apps/com.zhiliaoapp.musically.ts
+++ src/apps/com.zhiliaoapp.musically.ts
@@ -8,15 +8,21 @@ export default defineGkdApp({
       key: 1,
       name: '全屏广告-弹窗广告',
       desc: '点击关闭',
+      quickFind: true,
       rules: [
         {
-          quickFind: true,
+          key: 0,
           activityIds: 'com.ss.android.ugc.aweme.main.MainActivity',
           matches:
             'RelativeLayout[childCount=2] > @ImageView[index=1][clickable=true] <<n [id="android:id/custom"]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/13316f83-eaca-4081-9757-33730338a033',
           snapshotUrls: 'https://i.gkd.li/i/15144780',
+        },
+        {
+          key: 1,
+          activityIds: 'com.ss.android.ugc.aweme.main.MainActivity',
+          matches:
+            '@UIView[clickable=true] -3 FlattenUIText[text="Get products"]',
+          snapshotUrls: 'https://i.gkd.li/i/16190364',
         },
       ],
     },
