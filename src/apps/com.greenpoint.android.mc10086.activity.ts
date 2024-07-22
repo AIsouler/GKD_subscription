93d267b2ae2bef8ccab2786bd6c13e67db45ade9
AIsouler <zy43220@gmail.com>
2024-07-22 22:23:42 +0800

    feat(中国移动): 开屏广告

--- src/apps/com.greenpoint.android.mc10086.activity.ts
+++ src/apps/com.greenpoint.android.mc10086.activity.ts
@@ -5,6 +5,20 @@ export default defineGkdApp({
   name: '中国移动',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/16335136',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '更新提示',
       matchTime: 10000,
