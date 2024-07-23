ab65913d5b09672dce9a7fc0174275237e04fd17
AIsouler <zy43220@gmail.com>
2024-07-23 09:46:42 +0800

    feat(汽水音乐): 全屏广告-VIP弹窗（补充规则）

--- src/apps/com.luna.music.ts
+++ src/apps/com.luna.music.ts
@@ -57,13 +57,15 @@ export default defineGkdApp({
           key: 0,
           name: '底部半屏弹窗',
           action: 'back', // 使用点击方式有概率无效
-          matches: 'FlattenUIText[text="开会员听整月" || text="购买汽水会员"]',
+          matches:
+            'FlattenUIText[text="开会员听整月" || text="购买汽水会员" || text="试听中，开通VIP听全曲"]',
           snapshotUrls: [
             'https://i.gkd.li/i/13533795',
             'https://i.gkd.li/i/13660652',
             'https://i.gkd.li/i/13533797',
             'https://i.gkd.li/i/14767233',
             'https://i.gkd.li/i/16280954',
+            'https://i.gkd.li/i/16342691',
             'https://i.gkd.li/i/13613296', // 避免在此页面误触
           ],
         },
