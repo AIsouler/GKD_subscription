875416611bd183df17a572389fe63c6157808e76
AIsouler <zy43220@gmail.com>
2024-07-24 13:01:07 +0800

    feat(携程旅行): 全屏广告-弹窗广告

--- src/apps/ctrip.android.view.ts
+++ src/apps/ctrip.android.view.ts
@@ -27,5 +27,23 @@ export default defineGkdApp({
       rules: '[text*="用户已升级"] < * + [desc="关闭"]',
       snapshotUrls: 'https://i.gkd.li/i/13218347',
     },
+    {
+      key: 2,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          matchTime: 10000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          activityIds:
+            'ctrip.android.publicproduct.home.view.CtripHomeActivity',
+          matches:
+            '@ImageView[clickable=true] < RelativeLayout + RelativeLayout >5 [desc^="广告"]',
+          exampleUrls: 'https://e.gkd.li/00628604-fab9-432c-a70b-343c8c889c23',
+          snapshotUrls: 'https://i.gkd.li/i/16357287',
+        },
+      ],
+    },
   ],
 });
