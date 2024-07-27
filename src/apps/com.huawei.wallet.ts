c0547a3d34b93601dc6208b8cf02aaf0ea2d6608
AIsouler <zy43220@gmail.com>
2024-07-27 11:05:05 +0800

    feat(华为钱包): 全屏广告-小额免密支付弹窗

--- src/apps/com.huawei.wallet.ts
+++ src/apps/com.huawei.wallet.ts
@@ -20,5 +20,20 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '全屏广告-小额免密支付弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds:
+            'com.huawei.petalpay.userapp.function.withhold.view.activity.PetalPayGuideSmallFreePwdActivity',
+          matches: ['[text="开通小额免密支付"]', '[vid="title_close_btn"]'],
+          exampleUrls: 'https://e.gkd.li/7fcdf7d8-d9db-4b30-840d-3f656f645fe3',
+          snapshotUrls: 'https://i.gkd.li/i/16397908',
+        },
+      ],
+    },
   ],
 });
