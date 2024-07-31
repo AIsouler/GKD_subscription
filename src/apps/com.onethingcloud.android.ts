c2c31964d119f8bc6bd96eaf59e590844d82743d
AIsouler <zy43220@gmail.com>
2024-07-31 19:30:43 +0800

    perf(网心云): 全屏广告-弹窗广告

--- src/apps/com.onethingcloud.android.ts
+++ src/apps/com.onethingcloud.android.ts
@@ -7,13 +7,18 @@ export default defineGkdApp({
     {
       key: 1,
       name: '全屏广告-弹窗广告',
-      activityIds: 'com.onethingcloud.android.MainActivity',
-      rules:
-        '[id="android:id/content"] >(3,4) View[childCount=2] >2 View[childCount=3] > ImageView[index=2][clickable=true]',
-      snapshotUrls: [
-        'https://i.gkd.li/i/12841171',
-        'https://i.gkd.li/i/14035418', // 误触快照
-        'https://i.gkd.li/i/14766665',
+      rules: [
+        {
+          activityIds: 'com.onethingcloud.android.MainActivity',
+          matches:
+            '[id="android:id/content"] >(3,4) View[childCount=2] >2 View[childCount=2 || childCount=3] > ImageView[index=parent.childCount.minus(1)][clickable=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/12841171',
+            'https://i.gkd.li/i/14035418', // 误触快照
+            'https://i.gkd.li/i/14766665',
+            'https://i.gkd.li/i/16438609',
+          ],
+        },
       ],
     },
   ],
