3b14b59340f9fb7562ad034116c7e1288b12fa4c
AIsouler <zy43220@gmail.com>
2024-08-01 18:31:21 +0800

    feat(央视体育): 局部广告-视频暂停时的广告

--- src/apps/com.cctv.cctv5ultimate.ts
+++ src/apps/com.cctv.cctv5ultimate.ts
@@ -19,5 +19,21 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '局部广告-视频暂停时的广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds:
+            'com.cctv.cctv5ultimate.videodetail.activity.VideoDetailActivity',
+          matches:
+            '@ImageView[clickable=true][childCount=0] + LinearLayout > [text="广告"]',
+          exampleUrls: 'https://e.gkd.li/26bf9fcc-9dda-4465-a4d5-3fd56a3addde',
+          snapshotUrls: 'https://i.gkd.li/i/16454650',
+        },
+      ],
+    },
   ],
 });
