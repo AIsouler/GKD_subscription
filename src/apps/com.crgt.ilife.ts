c1847fcc8d7ac6b2532b68dadff78e5ebbffc241
AIsouler <zy43220@gmail.com>
2024-08-01 12:39:07 +0800

    feat(国铁吉讯): 局部广告-卡片广告

---
+++ src/apps/com.crgt.ilife.ts
@@ -0,0 +1,22 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.crgt.ilife',
+  name: '国铁吉讯',
+  groups: [
+    {
+      key: 1,
+      name: '局部广告-卡片广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          activityIds: 'com.crgt.ilife.ui.main.MainPageView',
+          matches:
+            'FrameLayout[childCount=2] > FrameLayout[childCount=2] > FrameLayout[childCount=3] > @FrameLayout[childCount=1] > ImageView[childCount=0][text=null]',
+          exampleUrls: 'https://e.gkd.li/3026504f-acb7-4f58-b171-ab664b2caf27',
+          snapshotUrls: 'https://i.gkd.li/i/16450332',
+        },
+      ],
+    },
+  ],
+});
