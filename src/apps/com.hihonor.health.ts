e41f3ed3f6c31cd432f4de644a773a10c182d9d6
AIsouler <zy43220@gmail.com>
2024-08-01 18:15:11 +0800

    feat(荣耀运动健康): 全屏广告-弹窗广告

--- src/apps/com.hihonor.health.ts
+++ src/apps/com.hihonor.health.ts
@@ -10,10 +10,24 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
-          activityIds: 'com.hihonor.health.main.MainActivity',
           fastQuery: true,
+          activityIds: 'com.hihonor.health.main.MainActivity',
           matches: '[id="com.hihonor.health:id/iv_close_filled"]',
           snapshotUrls: 'https://i.gkd.li/i/13759407',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.hihonor.health.main.MainActivity',
+          matches: '[vid="btn_close"][desc="关闭弹窗"]',
+          exampleUrls: 'https://e.gkd.li/1ab101bd-59cb-46c0-bbb1-7a0a9dccf79a',
+          snapshotUrls: 'https://i.gkd.li/i/16454150',
         },
       ],
     },
