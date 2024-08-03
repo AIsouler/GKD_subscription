dd2a925e2184463a4a68e6e9b9bacf29ee5cfcac
AIsouler <zy43220@gmail.com>
2024-08-03 08:48:38 +0800

    feat(我的荣耀): 全屏广告-弹窗广告

--- src/apps/com.hihonor.phoneservice.ts
+++ src/apps/com.hihonor.phoneservice.ts
@@ -5,6 +5,20 @@ export default defineGkdApp({
   name: '我的荣耀',
   groups: [
     {
+      key: 1,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.hihonor.phoneservice.main.MainActivity',
+          matches: '[vid="ads_cancel"]',
+          exampleUrls: 'https://e.gkd.li/539acb69-5c9c-4c4b-9757-d189b6852f89',
+          snapshotUrls: 'https://i.gkd.li/i/16476928',
+        },
+      ],
+    },
+    {
       key: 10,
       name: '权限提示-定位权限',
       actionMaximum: 1,
