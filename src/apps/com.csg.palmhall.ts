379d46067f4fb7eadc68a353dadbee73add3218a
AIsouler <zy43220@gmail.com>
2024-08-03 15:33:29 +0800

    feat(南网在线): 通知提示-公告弹窗

--- src/apps/com.csg.palmhall.ts
+++ src/apps/com.csg.palmhall.ts
@@ -11,14 +11,28 @@ export default defineGkdApp({
       actionMaximum: 1,
       resetMatch: 'app',
       rules: '@[text="关闭"] + [text="升级"]',
-      snapshotUrls: ['https://i.gkd.li/i/12700060'],
+      snapshotUrls: 'https://i.gkd.li/i/12700060',
     },
     {
       key: 2,
       name: '全屏广告-首页弹窗广告',
-      activityIds: ['com.csg.palmhall.MainActivity'],
+      activityIds: 'com.csg.palmhall.MainActivity',
       rules: 'View > Image +(3) [text="不再显示"][clickable=true]',
-      snapshotUrls: ['https://i.gkd.li/i/12700075'],
+      snapshotUrls: 'https://i.gkd.li/i/12700075',
+    },
+    {
+      key: 3,
+      name: '通知提示-公告弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          activityIds: 'com.csg.palmhall.MainActivity',
+          matches:
+            '[text$="公告"] - View[childCount=2] > Image[childCount=0][text=""]',
+          exampleUrls: 'https://e.gkd.li/0ecc2a4e-6ab9-4c00-80fe-bbd23d181be8',
+          snapshotUrls: 'https://i.gkd.li/i/16478559',
+        },
+      ],
     },
   ],
 });
