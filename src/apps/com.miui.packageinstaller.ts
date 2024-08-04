d770cc5a71fe5d9d6d56955c12965c192a25633c
AIsouler <zy43220@gmail.com>
2024-08-04 18:35:15 +0800

    feat(应用包管理组件): 功能类-放弃开启安全守护

--- src/apps/com.miui.packageinstaller.ts
+++ src/apps/com.miui.packageinstaller.ts
@@ -5,6 +5,28 @@ export default defineGkdApp({
   name: '应用包管理组件',
   groups: [
     {
+      key: 7,
+      name: '功能类-放弃开启安全守护',
+      fastQuery: true,
+      activityIds:
+        'com.miui.packageInstaller.ui.normalmode.InstallProgressActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '[text="30天内不再提示"][checked=false]',
+          exampleUrls: 'https://e.gkd.li/fe6682cd-a27a-4183-8717-15fd373bafcf',
+          snapshotUrls: 'https://i.gkd.li/i/16487140',
+        },
+        {
+          preKeys: [0],
+          key: 1,
+          matches: '[text="放弃"]',
+          exampleUrls: 'https://e.gkd.li/859dac99-62ca-4876-bdfb-c84795254111',
+          snapshotUrls: 'https://i.gkd.li/i/16487142',
+        },
+      ],
+    },
+    {
       key: 8,
       name: '功能类-安装来源不可信',
       desc: '点击[授权本次安装]',
