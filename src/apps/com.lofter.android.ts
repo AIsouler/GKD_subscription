f978ea6bd1afce0310d673fe7f66724bfee9f804
AIsouler <zy43220@gmail.com>
2024-08-05 18:09:24 +0800

    feat(LOFTER): 局部广告-悬浮广告

--- src/apps/com.lofter.android.ts
+++ src/apps/com.lofter.android.ts
@@ -26,5 +26,23 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 1,
+      name: '局部广告-悬浮广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          matchTime: 10000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          activityIds: 'com.lofter.android.global.home.TabHomeActivity',
+          matches:
+            '[vid="global_ad_layout"] > [vid="close"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/0559eada-7899-4c8a-8634-ef3f55227492',
+          snapshotUrls: 'https://i.gkd.li/i/16494241',
+        },
+      ],
+    },
   ],
 });
