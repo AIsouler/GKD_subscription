0774a79b76f705c51bd642b8d79c44d5c8c4d4e5
AIsouler <zy43220@gmail.com>
2024-08-06 07:47:42 +0800

    feat(淘票票): 开屏广告

---
+++ src/apps/com.taobao.movie.android.ts
@@ -0,0 +1,23 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.taobao.movie.android',
+  name: '淘票票',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/b0b7bd5e-89fc-4079-b17f-1b1cf4938658',
+          snapshotUrls: 'https://i.gkd.li/i/16498740',
+        },
+      ],
+    },
+  ],
+});
