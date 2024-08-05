6343ce45a43d97c917d988f6c1ee048b8c4a506e
AIsouler <zy43220@gmail.com>
2024-08-05 13:17:34 +0800

    feat(高德地图): 开屏广告

--- src/apps/com.autonavi.minimap.ts
+++ src/apps/com.autonavi.minimap.ts
@@ -5,6 +5,21 @@ export default defineGkdApp({
   name: '高德地图',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/983cc392-1b01-4172-bc1d-2df001baeb3d',
+          snapshotUrls: 'https://i.gkd.li/i/16490985',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '更新提示',
       matchTime: 10000,
