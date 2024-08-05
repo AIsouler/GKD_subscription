88752465e20a85f4610662c5a30210d505dcf808
AIsouler <zy43220@gmail.com>
2024-08-05 20:32:35 +0800

    feat(百度输入法定制版): 开屏广告

---
+++ src/apps/com.baidu.input_huawei.ts
@@ -0,0 +1,26 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.baidu.input_huawei',
+  name: '百度输入法定制版',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/0754949d-3440-4a0f-9b2e-949c6850b292',
+          snapshotUrls: [
+            'https://i.gkd.li/i/16493628',
+            'https://i.gkd.li/i/16493619',
+          ],
+        },
+      ],
+    },
+  ],
+});
