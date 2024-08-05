356c805afd0bd5012046ed56cf75032126a87db1
AIsouler <zy43220@gmail.com>
2024-08-05 19:15:29 +0800

    perf(华为运动健康): 功能类-请求开启[实时推送步数]弹窗

--- src/apps/com.huawei.health.ts
+++ src/apps/com.huawei.health.ts
@@ -68,17 +68,22 @@ export default defineGkdApp({
     },
     {
       key: 5,
-      name: '功能类-请求开启“实时推送步数”弹窗',
+      name: '功能类-请求开启[实时推送步数]弹窗',
       desc: '点击取消',
       rules: [
         {
           fastQuery: true,
           activityIds: 'com.huawei.health.MainActivity',
-          matches:
-            '[text*="实时推送步数"] <2 * < * +2 * >4 [text="取消"][clickable=true]',
+          matches: [
+            '[text*="实时推送步数" || text*="通知栏实时显示步数"]',
+            '[text="取消"]',
+          ],
           exampleUrls:
             'https://m.gkd.li/57941037/7e9b7131-35ae-49dd-badf-13b6dae3aa96',
-          snapshotUrls: 'https://i.gkd.li/i/14321121',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14321121',
+            'https://i.gkd.li/i/16494762',
+          ],
         },
       ],
     },
