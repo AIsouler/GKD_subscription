628ad98c37c7a23dd428776015b5237ff52f3017
AIsouler <zy43220@gmail.com>
2024-08-06 22:27:06 +0800

    perf(云空间): 全屏广告-[升级云空间]弹窗

--- src/apps/com.huawei.hidisk.ts
+++ src/apps/com.huawei.hidisk.ts
@@ -7,24 +7,22 @@ export default defineGkdApp({
     {
       key: 1,
       name: '全屏广告-[升级云空间]弹窗',
-      desc: '点击[放弃]/[以后再说]',
+      desc: '点击[放弃]/[以后再说]/[关闭]',
       fastQuery: true,
-      activityIds:
-        'com.huawei.android.hicloud.ui.activity.BackupNotificationActivity',
       rules: [
         {
-          key: 0,
-          matches: '[text="免费升级云空间"] +2 * > [text="放弃"]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/0b2dc8b3-a9b4-48ed-a680-d01d7945cd46',
-          snapshotUrls: 'https://i.gkd.li/i/14881774',
-        },
-        {
           key: 1,
-          matches: '[text="以后再说"]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/dd870248-a785-4fd7-81bb-8366c3169340',
-          snapshotUrls: 'https://i.gkd.li/i/14881775',
+          activityIds:
+            'com.huawei.android.hicloud.ui.activity.BackupNotificationActivity',
+          matches: [
+            '[text="免费升级云空间" || text="云空间不够了"]',
+            '[text="放弃" || text="以后再说"]',
+          ],
+          exampleUrls: 'https://e.gkd.li/5a445ec2-e3bf-445a-be82-7c91036bd05a',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14881774',
+            'https://i.gkd.li/i/14881775',
+          ],
         },
         {
           key: 2,
@@ -32,6 +30,7 @@ export default defineGkdApp({
             'com.huawei.android.hicloud.ui.activity.GuideWebViewActivity',
           matches:
             '@[clickable=true] + [text^="equityDistri"] <<n [vid="guid_webview_mian"]',
+          exampleUrls: 'https://e.gkd.li/a541bce4-3fb3-412a-8c51-d93f4eb78848',
           snapshotUrls: [
             'https://i.gkd.li/i/15523963',
             'https://i.gkd.li/i/16511513',
