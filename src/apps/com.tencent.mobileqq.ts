7280126e07cece32cdab3a27a775d622191a9520
PianCat <50688679+PianCat@users.noreply.github.com>
2024-08-09 10:49:35 +0800

    对QQ近期常见的聊天页面关键词广告进行适配 (#301)
    Signed-off-by: PianCat <50688679+PianCat@users.noreply.github.com>
    Co-authored-by: AIsouler <zy43220@gmail.com>

--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -589,15 +589,22 @@ export default defineGkdApp({
         },
         {
           key: 1,
-          name: '开福袋浮层广告',
+          name: '关键词广告',
           fastQuery: true,
-          activityIds:
+          activityIds: [
             'com.tencent.mobileqq.profilecard.activity.FriendProfileCardActivity',
+            'com.tencent.mobileqq.activity.ChatActivity',
+            'com.tencent.mobileqq.activity.SplashActivity',
+          ],
           matches:
-            'FrameLayout > FrameLayout > FrameLayout[childCount=2] > TextView[text="跳过"]',
+            'FrameLayout[childCount=2] >2 FrameLayout[childCount=2] >3 FrameLayout[childCount=2] > [text="跳过" || text="关闭"][clickable=true]',
           exampleUrls:
             'https://m.gkd.li/57941037/4cf5bc02-d2c3-4ca4-833e-522a194e3131',
-          snapshotUrls: 'https://i.gkd.li/i/14183188',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14183188',
+            'https://i.gkd.li/i/16549500',
+            'https://i.gkd.li/i/16555184',
+          ],
         },
       ],
     },
