92a5bfdeca55e02c644a6d81d8f701789167a730
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2024-08-09 10:01:13 +0800

    feat(掌阅): 更新提示 (#300)
    Signed-off-by: jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>

---
+++ src/apps/com.chaozh.iReaderFree.ts
@@ -0,0 +1,18 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.chaozh.iReaderFree',
+  name: '掌阅',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[vid="dialog_public_top_bar_title_close"]',
+      snapshotUrls: 'https://i.gkd.li/i/16549332',
+    },
+  ],
+});
