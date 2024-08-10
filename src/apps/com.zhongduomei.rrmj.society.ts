da25a8813240dbf3be445e5254996c7752af5b8f
AIsouler <zy43220@gmail.com>
2024-08-10 19:18:17 +0800

    feat(人人视频): 开屏广告

---
+++ src/apps/com.zhongduomei.rrmj.society.ts
@@ -0,0 +1,23 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.zhongduomei.rrmj.society',
+  name: '人人视频',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/6765a074-2482-42d7-97a2-23d5789078d9',
+          snapshotUrls: 'https://i.gkd.li/i/16569340',
+        },
+      ],
+    },
+  ],
+});
