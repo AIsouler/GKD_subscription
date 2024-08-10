06e6a458a95d5e104dc3fc7471d490b6af815d9c
AIsouler <zy43220@gmail.com>
2024-08-10 18:29:56 +0800

    feat(掌上英雄联盟): 开屏广告

--- src/apps/com.tencent.qt.qtl.ts
+++ src/apps/com.tencent.qt.qtl.ts
@@ -5,6 +5,22 @@ export default defineGkdApp({
   name: '掌上英雄联盟',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          action: 'clickCenter',
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/8e923c51-48e7-431c-abf8-e564761b772e',
+          snapshotUrls: 'https://i.gkd.li/i/16568456',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '更新提示',
       fastQuery: true,
