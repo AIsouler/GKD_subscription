9da36a9f72e2e6fc013a5ec6e21d181cdd9fcb4b
AIsouler <zy43220@gmail.com>
2024-08-12 20:34:39 +0800

    feat(掌上英雄联盟): 全屏广告-弹窗广告

--- src/apps/com.tencent.qt.qtl.ts
+++ src/apps/com.tencent.qt.qtl.ts
@@ -30,5 +30,19 @@ export default defineGkdApp({
       rules: '[id="com.tencent.qt.qtl:id/btn_update_cancel"]',
       snapshotUrls: 'https://i.gkd.li/i/13611286',
     },
+    {
+      key: 2,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.tencent.zone.main.MainZoneHomeActivity',
+          matches: '[vid="notice_close_pic"]',
+          exampleUrls: 'https://e.gkd.li/d1747892-c416-4877-a706-fb3c16e5b049',
+          snapshotUrls: 'https://i.gkd.li/i/16581606',
+        },
+      ],
+    },
   ],
 });
