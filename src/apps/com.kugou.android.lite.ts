a4bc7c750a97e0012302892ad70f9b2cf08372fc
AIsouler <zy43220@gmail.com>
2024-08-14 13:29:27 +0800

    feat(酷狗概念版): 全屏广告-弹窗广告、功能类-播放页听歌领VIP

--- src/apps/com.kugou.android.lite.ts
+++ src/apps/com.kugou.android.lite.ts
@@ -68,5 +68,41 @@ export default defineGkdApp({
       rules: '@ImageView - * > [text="立即升级"]',
       snapshotUrls: 'https://i.gkd.li/i/14622566',
     },
+    {
+      key: 5,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.kugou.android.app.MediaActivity',
+          matches: '@[text*="跳过"] + [text*="广告"]',
+          exampleUrls: 'https://e.gkd.li/64dd159b-bff5-4dff-a4b9-d63f5b34acb7',
+          snapshotUrls: 'https://i.gkd.li/i/16407631',
+        },
+      ],
+    },
+    {
+      key: 6,
+      name: '功能类-播放页听歌领VIP',
+      fastQuery: true,
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.kugou.android.app.MediaActivity',
+          matches: '@LinearLayout[clickable=true] > TextView[text="听歌领VIP"]',
+          exampleUrls: 'https://e.gkd.li/df927124-4e7a-43bd-8d42-3eb428eecf78',
+          snapshotUrls: 'https://i.gkd.li/i/16554695',
+        },
+        {
+          key: 1,
+          activityIds: 'com.kugou.android.app.MediaActivity',
+          matches:
+            '@ImageView[clickable=true] - FrameLayout >2 [text="立即领取福利"]',
+          exampleUrls: 'https://e.gkd.li/51fbe6aa-d0da-4f08-aae0-2eed4e6c0ad1',
+          snapshotUrls: 'https://i.gkd.li/i/16594001',
+        },
+      ],
+    },
   ],
 });
