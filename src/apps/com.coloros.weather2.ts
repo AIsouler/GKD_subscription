c947301b9bf1eb1913a5aed6f14c9a5a29a9b047
AIsouler <zy43220@gmail.com>
2024-08-16 10:17:20 +0800

    perf(天气): 分段广告-卡片广告

--- src/apps/com.coloros.weather2.ts
+++ src/apps/com.coloros.weather2.ts
@@ -6,16 +6,23 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '局部广告-卡片广告',
+      name: '分段广告-卡片广告',
       desc: '点击关闭',
+      fastQuery: true,
+      activityIds: 'com.oplus.weather.main.view.WeatherMainActivity',
       rules: [
         {
-          fastQuery: true,
-          activityIds: 'com.oplus.weather.main.view.WeatherMainActivity',
+          key: 0,
           matches: '[vid="feed_close"]',
           exampleUrls:
             'https://m.gkd.li/57941037/fc08493c-dec2-4534-9f61-3627aa337b30',
           snapshotUrls: 'https://i.gkd.li/i/14258190',
+        },
+        {
+          preKeys: [0],
+          matches: '@[clickable=true] > [text="不感兴趣"]',
+          exampleUrls: 'https://e.gkd.li/9137d262-7fda-4b57-a0bc-b9fa60634a22',
+          snapshotUrls: 'https://i.gkd.li/i/16631878',
         },
       ],
     },
