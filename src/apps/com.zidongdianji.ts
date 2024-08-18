e70d4c5e377bf2d9b4e3cd1d884b8027d6746db7
AIsouler <zy43220@gmail.com>
2024-08-18 08:35:18 +0800

    perf(自动点击器): 分段广告

--- src/apps/com.zidongdianji.ts
+++ src/apps/com.zidongdianji.ts
@@ -7,18 +7,21 @@ export default defineGkdApp({
     {
       key: 1,
       name: '分段广告-卡片广告',
+      fastQuery: true,
       activityIds: 'com.autoclicker.clicker.MainActivity',
       rules: [
         {
           key: 1,
           matches:
-            'Image < @View[visibleToUser=true] + View > View > [text="广告"]',
-          snapshotUrls: 'https://i.gkd.li/i/14782886',
+            'WebView >3 View[childCount=2] > View[childCount=4] > View[childCount=1] > @Image[childCount=0][visibleToUser=true][text=""] <<n [vid="native_ad_container_top" || vid="rv_record"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14782886',
+            'https://i.gkd.li/i/16646833',
+          ],
         },
         {
           preKeys: [1],
           key: 2,
-          fastQuery: true,
           matches: '@[clickable=true] > [text="不感兴趣"]',
           snapshotUrls: 'https://i.gkd.li/i/14783475',
         },
