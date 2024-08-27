2da5cc4d6cf418ae956f8e8c11ebb4ffc38c42fa
DeepChirp <66902050+DeepChirp@users.noreply.github.com>
2024-08-27 20:07:01 +0800

    feat(扫描全能王): 开屏广告-免费试用 (#319)

--- src/apps/com.intsig.camscanner.ts
+++ src/apps/com.intsig.camscanner.ts
@@ -12,5 +12,16 @@ export default defineGkdApp({
         '[id="com.intsig.camscanner:id/card_ad_tag"] + [id="com.intsig.camscanner:id/card_close"]',
       snapshotUrls: 'https://i.gkd.li/i/12668813',
     },
+    {
+      key: 2,
+      name: '开屏广告-免费试用',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      fastQuery: true,
+      rules: '[id="com.intsig.camscanner:id/iv_cancel"]',
+      exampleUrls: 'https://e.gkd.li/5f628895-e214-4a0b-8344-14f3d8c20807',
+      snapshotUrls: 'https://i.gkd.li/i/16761285',
+    },
   ],
 });
