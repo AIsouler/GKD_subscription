ac3065bfed052c44538d3e6461c354f1b38a78a5
DeepChirp <66902050+DeepChirp@users.noreply.github.com>
2024-08-27 19:16:14 +0800

    feat(小猿搜题): 全屏广告-年卡广告 (#316)

--- src/apps/com.fenbi.android.solar.ts
+++ src/apps/com.fenbi.android.solar.ts
@@ -38,5 +38,20 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 3,
+      name: '全屏广告-年卡广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      fastQuery: true,
+      rules: [
+        {
+          matches: '[id="com.fenbi.android.solar:id/v_firework_close"]',
+          exampleUrls: 'https://e.gkd.li/874cdabf-8e49-4c6d-8fd0-48effd6e44e9',
+          snapshotUrls: 'https://i.gkd.li/i/16759322',
+        },
+      ],
+    },
   ],
 });
