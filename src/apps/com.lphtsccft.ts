150e1b19220ad820ed6aa270ccb887830aa14585
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-08-27 19:11:39 +0800

    feat(涨乐财富通): 更新提示（补充规则） (#317)

--- src/apps/com.lphtsccft.ts
+++ src/apps/com.lphtsccft.ts
@@ -23,8 +23,19 @@ export default defineGkdApp({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[text*="升级体验"] +2 LinearLayout > [text="取消"]',
-      snapshotUrls: 'https://i.gkd.li/i/12699308',
+      fastQuery: true,
+      rules: [
+        {
+          key: 0,
+          matches: '[text*="升级体验"] +2 LinearLayout > [text="取消"]',
+          snapshotUrls: 'https://i.gkd.li/i/12699308',
+        },
+        {
+          key: 1,
+          matches: '[vid="iv_update_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/16760030',
+        },
+      ],
     },
     {
       key: 3,
