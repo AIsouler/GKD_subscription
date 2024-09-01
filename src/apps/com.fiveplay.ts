a161fe689ad1635385522ea7af85162c601349f4
ThisDumpling <51235466+ThisDumpling@users.noreply.github.com>
2024-09-01 16:01:07 +0800

    feat(5EPlay): 局部广告-首页右下角浮窗 (#331)

--- src/apps/com.fiveplay.ts
+++ src/apps/com.fiveplay.ts
@@ -21,5 +21,20 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '局部广告-首页右下角浮窗',
+      desc: '点击关闭',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          matches: '[id="com.fiveplay:id/iv_ad_float_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/16825082',
+        },
+      ],
+    },
   ],
 });
