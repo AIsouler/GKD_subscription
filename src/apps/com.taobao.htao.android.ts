a883ac63f69c2cd8ccaae8b011ee8ade2312c8fd
AIsouler <zy43220@gmail.com>
2024-09-03 10:52:36 +0800

    perf: 优化

--- src/apps/com.taobao.htao.android.ts
+++ src/apps/com.taobao.htao.android.ts
@@ -6,13 +6,13 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '局部广告-打开淘宝',
+      name: '功能类-商品详情页点击右上角[打开淘宝]',
       fastQuery: true,
       activityIds: 'com.taobao.htao.android.detail.DetailActivity',
       rules: [
         {
           matches:
-            '@TextView[visibleToUser=true][text=""] < View[id=null][childCount=1] <<n WebView[text="- 天猫Tmall.com"] <<4 FrameLayout[vid="detail_root"]',
+            '@[visibleToUser=true][text="打开淘宝"] <<n [vid="detail_swipe_to_refresher"]',
           exampleUrls: 'https://e.gkd.li/661fca21-45e3-4e89-8ccc-b8bafb1bab02',
           snapshotUrls: 'https://i.gkd.li/i/16842507',
         },
