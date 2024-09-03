2993435afbabed3d86c5cc53420b787eee54ef62
DeepChirp <1434323966@qq.com>
2024-09-03 10:33:05 +0800

    perf(淘宝Lite): 局部广告-打开淘宝[使用`fastQuery`]

--- src/apps/com.taobao.htao.android.ts
+++ src/apps/com.taobao.htao.android.ts
@@ -7,11 +7,12 @@ export default defineGkdApp({
     {
       key: 1,
       name: '局部广告-打开淘宝',
+      fastQuery: true,
       activityIds: 'com.taobao.htao.android.detail.DetailActivity',
       rules: [
         {
           matches:
-            'WebView[text="- 天猫Tmall.com"] >2 View[id=null][childCount=1] > TextView[visibleToUser=true]',
+            '@TextView[visibleToUser=true][text=""] < View[id=null][childCount=1] <<n WebView[text="- 天猫Tmall.com"] <<4 FrameLayout[vid="detail_root"]',
           exampleUrls: 'https://e.gkd.li/661fca21-45e3-4e89-8ccc-b8bafb1bab02',
           snapshotUrls: 'https://i.gkd.li/i/16842507',
         },
