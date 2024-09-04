3578938377e82c969b97ff9ad7e22f4b5734f012
ThisDumpling <51235466+ThisDumpling@users.noreply.github.com>
2024-09-04 11:29:09 +0800

    feat(SteamPY): 功能类-开屏时优惠券自动领取 (#339)
    Co-authored-by: AIsouler <zy43220@gmail.com>

--- src/apps/com.steampy.app.ts
+++ src/apps/com.steampy.app.ts
@@ -15,5 +15,25 @@ export default defineGkdApp({
         '@[id="com.steampy.app:id/imgClose"] - RelativeLayout [id="com.steampy.app:id/tv_update"]',
       snapshotUrls: 'https://i.gkd.li/i/13695519',
     },
+    {
+      key: 2,
+      name: '功能类-首页优惠券弹窗自动点击领取/使用',
+      fastQuery: true,
+      matchTime: 10000,
+      rules: [
+        {
+          activityIds: '.activity.common.MainActivity',
+          matches: '[vid="coupon_right"]',
+          exampleUrls: [
+            'https://e.gkd.li/2e87decb-54ba-4f8b-9330-f037c220d2d9',
+            'https://e.gkd.li/b2e256c2-c606-430c-afa9-3cd02f4d56c2',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/16860018',
+            'https://i.gkd.li/i/16860017',
+          ],
+        },
+      ],
+    },
   ],
 });
