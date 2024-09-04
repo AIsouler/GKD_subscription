fbf55544a7938d74faba137ab9d094387efe16a6
ThisDumpling <2326900941@qq.com>
2024-09-04 10:27:06 +0800

    feat(SteamPY): 功能类-开屏优惠券领取

--- src/apps/com.steampy.app.ts
+++ src/apps/com.steampy.app.ts
@@ -15,5 +15,15 @@ export default defineGkdApp({
         '@[id="com.steampy.app:id/imgClose"] - RelativeLayout [id="com.steampy.app:id/tv_update"]',
       snapshotUrls: 'https://i.gkd.li/i/13695519',
     },
+    {
+      key: 2,
+      name: '功能类-开屏优惠券领取/使用',
+      fastQuery: true,
+      rules: '[id="com.steampy.app:id/coupon_right"]',
+      snapshotUrls: [
+        'https://i.gkd.li/i/16860018',
+        'https://i.gkd.li/i/16860017',
+      ],
+    },
   ],
 });
