94a1816c4686d37f8485130a72330e25f5a5246b
ThisDumpling <2326900941@qq.com>
2024-09-05 21:43:04 +0800

    fix(每日赛车): 局部广告-新闻页面广告

--- src/apps/com.romielf.mrsc.ts
+++ src/apps/com.romielf.mrsc.ts
@@ -6,17 +6,30 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '局部广告-新闻页面摇一摇广告',
+      name: '局部广告-新闻页面广告',
       desc: '点击准确率灵敏度不太高',
       activityIds: 'com.romielf.mrsc.MainActivity',
       rules: [
         {
+          key: 1,
           matches: '@Image < View -n View >2 View[text="广告"]',
           snapshotUrls: 'https://i.gkd.li/i/12667539',
         },
         {
+          key: 2,
           matches: '[id="com.byted.pangle:id/tt_item_tv"][text="不感兴趣"]', // 字节SDK广告
           snapshotUrls: 'https://i.gkd.li/i/12667549',
+        },
+        {
+          key: 3,
+          activityIds: 'com.romielf.mrsc.MainActivity',
+          matches: '[text="广告"] <<4 FrameLayout +2 FrameLayout > View',
+          snapshotUrls: 'https://i.gkd.li/i/16894284',
+        },
+        {
+          preKeys: 3,
+          matches: '[text="不感兴趣"]',
+          snapshotUrls: 'https://i.gkd.li/i/16894284',
         },
       ],
     },
