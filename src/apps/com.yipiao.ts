c3a0828232a9c74ddc414f29b2c2822724703f20
luoxuebinfei <39854534+luoxuebinfei@users.noreply.github.com>
2024-09-06 00:11:30 +0800

    add(智行火车票): 跳过智行火车票的开屏广告
    Signed-off-by: luoxuebinfei <39854534+luoxuebinfei@users.noreply.github.com>

---
+++ src/apps/com.yipiao.ts
@@ -0,0 +1,22 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.yipiao',
+  name: '智行火车票',
+  groups: [
+    {
+      name: '开屏广告',
+      key: 0,
+      actionMaximum: 1,
+      order: -10,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['[text^="跳过"][text.length<=10]'],
+          snapshotUrls: ['https://i.gkd.li/i/16896514'],
+        },
+      ],
+    },
+  ],
+});
