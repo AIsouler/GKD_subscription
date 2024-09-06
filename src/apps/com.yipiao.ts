4097328446522e37f74a6245e2c3e8d0d12f239e
AIsouler <zy43220@gmail.com>
2024-09-06 09:56:51 +0800

    feat(智行火车票): 开屏广告

---
+++ src/apps/com.yipiao.ts
@@ -0,0 +1,26 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.yipiao',
+  name: '智行火车票',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          position: {
+            left: 'width * 0.8759',
+            top: 'width * 2.0519',
+          },
+          matches: '[desc="广告素材"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/375bf789-95f6-4646-b433-3d3b25dda58a',
+          snapshotUrls: 'https://i.gkd.li/i/16896514',
+        },
+      ],
+    },
+  ],
+});
