c8b152dcf690fca3fc55c0eb303966a3165d1df4
AIsouler <zy43220@gmail.com>
2024-09-11 09:44:47 +0800

    perf: 主题

--- src/apps/com.huawei.android.thememanager.ts
+++ src/apps/com.huawei.android.thememanager.ts
@@ -26,6 +26,7 @@ export default defineGkdApp({
     {
       key: 1,
       name: '局部广告-主题详情页“下应用领主题”横幅',
+      fastQuery: true,
       activityIds: [
         'com.huawei.android.thememanager.mvp.view.activity.onlinetheme.OnlineThemePreviewActivity',
         'com.huawei.android.thememanager.mvp.view.activity.onlinewallpaper.OnlineWallpaperPreviewActivity',
@@ -42,32 +43,30 @@ export default defineGkdApp({
     },
     {
       key: 2,
-      name: '局部广告-主题详情页广告',
-      activityIds: [
-        'com.huawei.android.thememanager.mvp.view.activity.onlinetheme.OnlineThemePreviewActivity',
-        'com.huawei.android.thememanager.mvp.view.activity.onlinewallpaper.OnlineWallpaperPreviewActivity',
-        'com.huawei.android.thememanager.mvp.view.activity.onlinefont.OnlineFontPreviewActivity',
-        'com.huawei.android.thememanager.mvp.view.activity.paster.PasterDetailActivity',
-        'com.huawei.android.thememanager.mvp.view.activity.onlinetheme.LocalThemePreviewActivity',
-      ],
+      name: '分段广告-主题详情页广告',
+      fastQuery: true,
       rules: [
         {
+          key: 0,
+          activityIds: [
+            'com.huawei.android.thememanager.mvp.view.activity.onlinetheme.OnlineThemePreviewActivity',
+            'com.huawei.android.thememanager.mvp.view.activity.onlinewallpaper.OnlineWallpaperPreviewActivity',
+            'com.huawei.android.thememanager.mvp.view.activity.onlinefont.OnlineFontPreviewActivity',
+            'com.huawei.android.thememanager.mvp.view.activity.paster.PasterDetailActivity',
+            'com.huawei.android.thememanager.mvp.view.activity.onlinetheme.LocalThemePreviewActivity',
+          ],
           matches:
-            '[id="com.huawei.hms.ads.uiengine:id/unlike_iv"||id="com.huawei.android.thememanager:id/download_pps_close"||id="com.huawei.android.thememanager:id/pps_close"]',
+            '[id="com.huawei.hms.ads.uiengine:id/unlike_iv" || id="com.huawei.android.thememanager:id/download_pps_close" || vid="pps_close"]',
           snapshotUrls: [
             'https://i.gkd.li/i/12647248',
             'https://i.gkd.li/i/12647292',
             'https://i.gkd.li/i/12647367',
+            'https://i.gkd.li/i/16956672',
           ],
         },
-      ],
-    },
-    {
-      key: 3,
-      name: '局部广告-主题详情页广告关闭弹窗',
-      activityIds: ['com.huawei.openalliance.ad.activity.FeedbackActivity'],
-      rules: [
         {
+          preKeys: [0],
+          activityIds: 'com.huawei.openalliance.ad.activity.FeedbackActivity',
           matches: '[text="直接关闭"]',
           snapshotUrls: 'https://i.gkd.li/i/12647268',
         },
@@ -76,6 +75,7 @@ export default defineGkdApp({
     {
       key: 4,
       name: '分段广告-主题详情信息流广告',
+      fastQuery: true,
       activityIds: [
         'com.huawei.android.thememanager.mvp.view.activity.onlinetheme.OnlineThemePreviewActivity',
         'com.huawei.android.thememanager.mvp.view.activity.onlinewallpaper.OnlineWallpaperPreviewActivity',
@@ -85,6 +85,7 @@ export default defineGkdApp({
       ],
       rules: [
         {
+          key: 0,
           matches:
             '[id="com.huawei.android.thememanager:id/pps_lable_top"||id="com.huawei.android.thememanager:id/pps_img_video_close"]',
           snapshotUrls: [
@@ -93,6 +94,7 @@ export default defineGkdApp({
           ],
         },
         {
+          preKeys: [0],
           matches:
             '[id="com.huawei.android.thememanager:id/tv_btn_uninterested"]',
           snapshotUrls: 'https://i.gkd.li/i/12647326',
@@ -116,6 +118,7 @@ export default defineGkdApp({
       activityIds: ['com.huawei.android.thememanager.HwThemeManagerActivity'],
       rules: [
         {
+          key: 0,
           matches:
             '[id="com.huawei.android.thememanager:id/announce_ad_close_btn"||id="com.huawei.android.thememanager:id/pps_lable_top"||id="com.huawei.android.thememanager:id/pps_img_video_close"||id="com.huawei.android.thememanager:id/pps_label_ll"]',
           snapshotUrls: [
@@ -126,6 +129,7 @@ export default defineGkdApp({
           ],
         },
         {
+          preKeys: [0],
           matches:
             '[id="com.huawei.android.thememanager:id/tv_btn_uninterested"]',
           snapshotUrls: 'https://i.gkd.li/i/12647655',
