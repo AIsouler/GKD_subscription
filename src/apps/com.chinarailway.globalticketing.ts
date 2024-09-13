bc2e693d8faf84d0aaab76b1b06381b1044c9906
DeepChirp <66902050+DeepChirp@users.noreply.github.com>
2024-09-13 10:00:00 +0800

    feat(铁路12306): 权限提示-定位权限 (#357)
    Co-authored-by: AIsouler <zy43220@gmail.com>

---
+++ src/apps/com.chinarailway.globalticketing.ts
@@ -0,0 +1,25 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.chinarailway.globalticketing',
+  name: '铁路12306',
+  groups: [
+    {
+      key: 1,
+      name: '权限提示-定位权限',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: '.com.MobileTicket.ui.activity.MainActivity',
+          matches:
+            '[text^="开启定位权限"] +2 [vid="fl_close"] > [vid="img_close"]',
+          exampleUrls: 'https://e.gkd.li/95c8443e-babc-4d96-a3cd-00692c2acf30',
+          snapshotUrls: 'https://i.gkd.li/i/16987292',
+        },
+      ],
+    },
+  ],
+});
