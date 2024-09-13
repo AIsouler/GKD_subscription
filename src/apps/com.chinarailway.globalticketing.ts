bd3b714787b43ab0d87130e1b5e76d149d53b566
AIsouler <zy43220@gmail.com>
2024-09-13 09:56:54 +0800

    perf: 优化

--- src/apps/com.chinarailway.globalticketing.ts
+++ src/apps/com.chinarailway.globalticketing.ts
@@ -8,10 +8,18 @@ export default defineGkdApp({
       key: 1,
       name: '权限提示-定位权限',
       fastQuery: true,
-      activityIds: '.com.MobileTicket.ui.activity.MainActivity',
-      rules: '[text^="开启定位权限"] +n [vid="fl_close"] > [vid="img_close"]',
-      exampleUrls: 'https://e.gkd.li/95c8443e-babc-4d96-a3cd-00692c2acf30',
-      snapshotUrls: 'https://i.gkd.li/i/16987292',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: '.com.MobileTicket.ui.activity.MainActivity',
+          matches:
+            '[text^="开启定位权限"] +2 [vid="fl_close"] > [vid="img_close"]',
+          exampleUrls: 'https://e.gkd.li/95c8443e-babc-4d96-a3cd-00692c2acf30',
+          snapshotUrls: 'https://i.gkd.li/i/16987292',
+        },
+      ],
     },
   ],
 });
