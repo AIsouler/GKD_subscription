704e8305b113359da564c0430c6552d8a06bdc9c
AIsouler <zy43220@gmail.com>
2024-09-14 18:54:31 +0800

    perf: 驾考宝典

--- src/apps/com.handsgo.jiakao.android.ts
+++ src/apps/com.handsgo.jiakao.android.ts
@@ -46,19 +46,18 @@ export default defineGkdApp({
         {
           key: 0,
           name: '字节广告',
+          fastQuery: true,
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
-          matches: [
-            '[desc^="logoad"] > [text="广告"]',
-            '[desc^="webview-close"] > View[clickable=true]',
-          ],
+          matches:
+            '@View[clickable=true][childCount=0] < FrameLayout[desc^="webview-close"] +4 FrameLayout >3 [text="广告"]',
           snapshotUrls: 'https://i.gkd.li/i/13476039',
         },
         {
           key: 1,
           name: '课程广告',
-          activityIds: 'com.handsgo.jiakao.android.main.activity.MainActivity',
           fastQuery: true,
+          activityIds: 'com.handsgo.jiakao.android.main.activity.MainActivity',
           matches:
             '[id="com.handsgo.jiakao.android:id/adsdk__ids_cta_layout"] - * > [id="com.handsgo.jiakao.android:id/close"]',
           snapshotUrls: 'https://i.gkd.li/i/13523033',
@@ -69,12 +68,16 @@ export default defineGkdApp({
       key: 3,
       name: '权限提示-通知权限',
       fastQuery: true,
-      matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules:
-        '[text="开启专属学习资料推送"] + [id="com.handsgo.jiakao.android:id/closeIv"]',
-      snapshotUrls: 'https://i.gkd.li/i/13520296',
+      rules: [
+        {
+          activityIds: 'com.handsgo.jiakao.android.main.activity.MainActivity',
+          matches: '[text="开启专属学习资料推送"] + [vid="closeIv"]',
+          exampleUrls: 'https://e.gkd.li/9a9aad9b-052b-4e00-9926-d4a213f96372',
+          snapshotUrls: 'https://i.gkd.li/i/17001986',
+        },
+      ],
     },
   ],
 });
