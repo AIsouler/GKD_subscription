b33c8caae21595868ba4ef6243f8f973e36fb20f
AIsouler <zy43220@gmail.com>
2024-09-22 10:51:16 +0800

    feat(悠洗): 全屏广告-弹窗广告（补充规则）

--- src/apps/com.zhishan.washer.ts
+++ src/apps/com.zhishan.washer.ts
@@ -25,6 +25,7 @@ export default defineGkdApp({
       desc: '点击关闭',
       rules: [
         {
+          key: 0,
           matchTime: 10000,
           actionMaximum: 1,
           resetMatch: 'app',
@@ -34,6 +35,18 @@ export default defineGkdApp({
             '[text="反馈"] + View[text=""][childCount=1] > Image[text=""][childCount=0][visibleToUser=true]',
           exampleUrls: 'https://e.gkd.li/355f6e11-c21f-4cb2-b103-1a56032da15c',
           snapshotUrls: 'https://i.gkd.li/i/17002026',
+        },
+        {
+          key: 1,
+          matchTime: 10000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches:
+            '[text="反馈"] -4 View[text=""][childCount=1] > Image[text=""][childCount=0][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/13cb561b-cfd5-44a8-b8ed-8bc1500b73a8',
+          snapshotUrls: 'https://i.gkd.li/i/17088067',
         },
       ],
     },
