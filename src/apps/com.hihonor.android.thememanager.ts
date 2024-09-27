0abb37654bc2f0a86c3a36b3d843b66631616717
AIsouler <zy43220@gmail.com>
2024-09-27 10:23:11 +0800

    feat(主题): 全屏广告-弹窗广告

--- src/apps/com.hihonor.android.thememanager.ts
+++ src/apps/com.hihonor.android.thememanager.ts
@@ -20,5 +20,22 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          matchTime: 10000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          activityIds: '.HwThemeManagerActivity',
+          matches: '[vid="mtDialogClose"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/7ceeb371-e7a5-4c94-8afd-d2c5b56744a6',
+          snapshotUrls: 'https://i.gkd.li/i/17158049',
+        },
+      ],
+    },
   ],
 });
