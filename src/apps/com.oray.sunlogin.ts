3ecf05211f1451bfb69ce03d6d15cb93a38d83d7
AIsouler <zy43220@gmail.com>
2024-09-27 18:28:42 +0800

    feat(向日葵远程控制): 局部广告-卡片广告

--- src/apps/com.oray.sunlogin.ts
+++ src/apps/com.oray.sunlogin.ts
@@ -51,13 +51,20 @@ export default defineGkdApp({
     },
     {
       key: 2,
-      name: '功能类-设备列表-卡片广告',
-      activityIds: 'com.oray.sunlogin.application.Main',
+      name: '局部广告-卡片广告',
       fastQuery: true,
-      rules: '[id="com.oray.sunlogin:id/fl_close_advertise"]',
-      snapshotUrls: 'https://i.gkd.li/i/13207785',
-      exampleUrls:
-        'https://github.com/gkd-kit/inspect/assets/38517192/071e5512-e1aa-4a8c-8440-009d6f0f35ff',
+      rules: [
+        {
+          activityIds: 'com.oray.sunlogin.application.Main',
+          matches:
+            '[id="com.oray.sunlogin:id/fl_close_advertise" || vid="iv_dislike"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/2cad526b-1691-45e9-826b-eb92299cb8ee',
+          snapshotUrls: [
+            'https://i.gkd.li/i/13207785',
+            'https://i.gkd.li/i/17159821',
+          ],
+        },
+      ],
     },
     {
       key: 3,
