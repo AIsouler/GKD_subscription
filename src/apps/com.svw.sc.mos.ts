ac2021975d15bd482f3c4d1a4f3d1dddf736d06c
AIsouler <zy43220@gmail.com>
2024-10-26 13:33:07 +0800

    feat(上汽大众): 全屏广告-弹窗广告 (#428)、局部广告-悬浮广告 (#429)

---
+++ src/apps/com.svw.sc.mos.ts
@@ -0,0 +1,42 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.svw.sc.mos',
+  name: '上汽大众',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          matchTime: 10000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          activityIds: '.main.ui.home.ui.AppStartDialogActivity',
+          matches: '[vid="iv_appstart_close"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/0acf720d-1752-449f-bbe6-357896f1bb2e',
+          snapshotUrls: 'https://i.gkd.li/i/17528776',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '局部广告-悬浮广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          matchTime: 10000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          activityIds: '.main.ui.MainActivity',
+          matches: '[vid="clyt_floating_close"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/fbbc311e-cfe1-468a-88c1-20633622a34e',
+          snapshotUrls: 'https://i.gkd.li/i/17528781',
+        },
+      ],
+    },
+  ],
+});
