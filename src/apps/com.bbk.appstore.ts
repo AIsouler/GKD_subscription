213433d43029885ba4fe62028aa147e1b63f40bb
AIsouler <zy43220@gmail.com>
2024-10-29 20:30:42 +0800

    perf(应用商店): 权限提示-通知权限 (#435)

--- src/apps/com.bbk.appstore.ts
+++ src/apps/com.bbk.appstore.ts
@@ -13,18 +13,31 @@ export default defineGkdApp({
       resetMatch: 'app',
       rules: [
         {
-          key: 0,
-          matches: '[id="com.bbk.appstore:id/upgrade_necessary_btn_high_new"]',
-          snapshotUrls: 'https://i.gkd.li/i/13198101',
-        },
-        {
           key: 1,
+          activityIds: '.ui.AppStoreTabActivity',
           matches: ['[text*="通知"]', '[text="取消"]'],
           snapshotUrls: [
             'https://i.gkd.li/i/13198234',
             'https://i.gkd.li/i/13246971',
             'https://i.gkd.li/i/13884356',
           ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '全屏广告-热门应用推荐',
+      desc: '点击[跳过，进入首页]',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.upgrade.UpgradeNecessaryActivity',
+          matches: '[text="跳过，进入首页"]',
+          exampleUrls: 'https://e.gkd.li/eb96ca80-2e68-4f41-ac0f-3c82092034d7',
+          snapshotUrls: 'https://i.gkd.li/i/13198101',
         },
       ],
     },
