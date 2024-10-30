f45989a32dcacb95f57e0084382ce7ff93fca512
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2024-10-31 04:33:28 +0800

    feat(淘宝): 开屏广告
    Signed-off-by: jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>

--- src/apps/com.taobao.taobao.ts
+++ src/apps/com.taobao.taobao.ts
@@ -5,6 +5,20 @@ export default defineGkdApp({
   name: '淘宝',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      priorityTime: 10000,
+      rules: '@[vid="close" || vid="ll_close"] [text*="跳过"][text.length<=10][visibleToUser=true]',
+      snapshotUrls: [
+        'https://i.gkd.li/i/17576743',
+        'https://i.gkd.li/i/17578451',
+      ],
+    },
+    {
       key: 1,
       name: '全屏广告-弹窗广告',
       rules: [
