fd6955545c2b07cc10b1879b1d1f95eabefe60a3
AIsouler <zy43220@gmail.com>
2024-11-03 18:56:33 +0800

    perf: 优化

--- src/apps/com.jingdong.app.mall.ts
+++ src/apps/com.jingdong.app.mall.ts
@@ -10,16 +10,23 @@ export default defineGkdApp({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
+      actionMaximumKey: 0,
       priorityTime: 10000,
       rules: [
         {
+          key: 0,
+          fastQuery: true,
           excludeActivityIds: 'com.jd.lib.search.view.Activity.SearchActivity',
           matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/16323111',
-            'https://i.gkd.li/i/16323115', // 防止误触
-            'https://i.gkd.li/i/17602356', // 京东v13.6.1 "跳过" 节点不支持fastQuery
-          ],
+          snapshotUrls: 'https://i.gkd.li/i/16323111',
+          excludeSnapshotUrls: 'https://i.gkd.li/i/16323115',
+        },
+        {
+          key: 1,
+          excludeActivityIds: 'com.jd.lib.search.view.Activity.SearchActivity',
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/17602356', // "跳过" 节点不支持fastQuery
+          excludeSnapshotUrls: 'https://i.gkd.li/i/16323115',
         },
       ],
     },
