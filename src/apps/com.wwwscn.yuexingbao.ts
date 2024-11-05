bfe3f98795eadca621be6cb0d2359e624d0623d7
AIsouler <zy43220@gmail.com>
2024-11-05 21:10:59 +0800

    feat(悦通行): 功能类-扫码确认解锁 (#467)

--- src/apps/com.wwwscn.yuexingbao.ts
+++ src/apps/com.wwwscn.yuexingbao.ts
@@ -24,5 +24,19 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 1,
+      name: '功能类-扫码确认解锁',
+      desc: '扫码成功后点击[确认并解锁]',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.ui.qrcode.ScanResultActivity',
+          matches: ['[text="扫码成功"]', '[text="确认并解锁"]'],
+          exampleUrls: 'https://e.gkd.li/31a62ba3-9329-4abe-ab30-99006ffa04d4',
+          snapshotUrls: 'https://i.gkd.li/i/17632893',
+        },
+      ],
+    },
   ],
 });
