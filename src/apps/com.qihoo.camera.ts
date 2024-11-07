e51c3e9f06b267655323e9f86644b85edfe48586
AIsouler <zy43220@gmail.com>
2024-11-07 13:18:03 +0800

    feat(360摄像机): 全屏广告-弹窗广告 (#472)

--- src/apps/com.qihoo.camera.ts
+++ src/apps/com.qihoo.camera.ts
@@ -6,22 +6,34 @@ export default defineGkdApp({
   groups: [
     {
       key: 6,
-      name: '局部广告-卡录像界面广告',
-      actionMaximum: 1,
-      activityIds: 'com.qihoo.jia.playpage.RecordPlayActivity',
+      name: '局部广告-卡片广告',
       rules: [
         {
-          name: '视频下方卡片广告',
-          matches:
-            'ViewGroup[childCount=2] > ImageView[clickable=true] + ImageView[clickable=true]',
-        },
-        {
+          actionMaximum: 1,
+          activityIds: 'com.qihoo.jia.playpage.RecordPlayActivity',
           name: '云收藏弹窗广告',
           matches:
             'ViewGroup > @ImageView[clickable=true] -n TextView[text="云收藏"]',
+          snapshotUrls: 'https://i.gkd.li/i/13630755',
         },
       ],
-      snapshotUrls: 'https://i.gkd.li/i/13630755',
+    },
+    {
+      key: 7,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.qihoo.jia.ui.activity.TabMainActivity',
+          matches:
+            '[id="android:id/content"] >2 FrameLayout[childCount=3] > FrameLayout[childCount=5] > FrameLayout[childCount=1] > ImageView[childCount=0]',
+          exampleUrls: 'https://e.gkd.li/c88cdec7-e6da-4bbf-821c-76459d0daff9',
+          snapshotUrls: 'https://i.gkd.li/i/17656326',
+        },
+      ],
     },
   ],
 });
