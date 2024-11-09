b141a00aa81f678a59d7ec547c09887fff80985b
AIsouler <zy43220@gmail.com>
2024-11-09 20:04:20 +0800

    perf(支付宝): 功能类-支付后自动点击完成 (#481)

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -173,11 +173,15 @@ export default defineGkdApp({
             'com.alipay.android.msp.ui.views.MspContainerActivity',
             'com.alipay.android.phone.businesscommon.ucdp.nfc.activity.NResPageActivity',
           ],
-          matches: ['[text="支付成功" || text="充值成功"]', '[text="完成"]'],
+          matches: [
+            '[text="支付成功" || text="充值成功" || text="提交成功"]',
+            '[text="完成"]',
+          ],
           snapshotUrls: [
             'https://i.gkd.li/i/14008852',
             'https://i.gkd.li/i/16328809',
             'https://i.gkd.li/i/17503266',
+            'https://i.gkd.li/i/17686972',
           ],
         },
       ],
