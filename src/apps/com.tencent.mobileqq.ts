a2fb2b69ac84316a99417f993507b6d27bf982c6
zhendery <zhendery@qq.com>
2024-11-20 13:37:45 +0800

    feat(QQ): 局部广告-说说详情广告 (#514)
    Co-authored-by: AIsouler <zy43220@gmail.com>

--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -776,11 +776,18 @@ export default defineGkdApp({
         {
           key: 3,
           name: '好友动态详情页广告',
-          activityIds: 'com.qzone.reborn.base.QZoneTransparentShellActivity',
+          activityIds: [
+            'com.qzone.reborn.base.QZoneTransparentShellActivity',
+            'com.qzone.reborn.base.QZoneShellActivity',
+          ],
           fastQuery: true,
-          matches: '@[desc="关闭广告"][visibleToUser=true] -3 [text^="活动"]',
+          matches:
+            '@[desc="关闭广告"][visibleToUser=true] <4 RelativeLayout <2 LinearLayout <2 LinearLayout < FrameLayout <(4,5) RecyclerView < FrameLayout - FrameLayout >2 [text="详情"]',
           exampleUrls: 'https://e.gkd.li/b78a3e44-3bd9-445d-9199-e989269c2be3',
-          snapshotUrls: 'https://i.gkd.li/i/17009847',
+          snapshotUrls: [
+            'https://i.gkd.li/i/17009847',
+            'https://i.gkd.li/i/17815694',
+          ],
         },
       ],
     },
