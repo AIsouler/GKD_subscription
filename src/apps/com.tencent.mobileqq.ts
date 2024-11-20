59f848f22af683545807cec54c967b258e729c70
AIsouler <zy43220@gmail.com>
2024-11-20 17:58:59 +0800

    feat(QQ): 局部广告-动态页-卡片广告 (补充快照)

--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -782,11 +782,12 @@ export default defineGkdApp({
           ],
           fastQuery: true,
           matches:
-            '@[desc="关闭广告"][visibleToUser=true] <4 RelativeLayout <2 LinearLayout <2 LinearLayout < FrameLayout <(n) RecyclerView < FrameLayout - FrameLayout >2 [text="详情"]',
+            '@[desc="关闭广告"][visibleToUser=true] <4 RelativeLayout <2 LinearLayout <2 LinearLayout < FrameLayout <n RecyclerView < FrameLayout - FrameLayout >2 [text="详情"]',
           exampleUrls: 'https://e.gkd.li/b78a3e44-3bd9-445d-9199-e989269c2be3',
           snapshotUrls: [
             'https://i.gkd.li/i/17009847',
             'https://i.gkd.li/i/17815694',
+            'https://i.gkd.li/i/17827969',
           ],
         },
       ],
