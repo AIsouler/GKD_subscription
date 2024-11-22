9af4b3b3fd3879bcd985e87c4148ed60e520dc23
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2024-11-23 00:18:16 +0800

    feat(移动爱家): 全屏广告-首页、局部广告-首页
    Signed-off-by: jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>

---
+++ src/apps/com.cmri.universalapp.ts
@@ -0,0 +1,38 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.cmri.universalapp',
+  name: '移动爱家',
+  groups: [
+    {
+      key: 5,
+      name: '全屏广告-首页',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[vid="ivCancelDlg"][visibleToUser=true]',
+      snapshotUrls: 'https://i.gkd.li/i/17872852',
+    },
+    {
+      key: 6,
+      name: '局部广告-首页',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      activityIds: 'com.cmri.universalapp.SmartMainProxyActivity',
+      rules: [
+        {
+          name: '横幅式',
+          matches: '[vid="close_banner"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/17872961',
+        },
+        {
+          name: '卡片信息流式',
+          matches: '[vid="iv_close"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/17872961',
+        },
+      ],
+    },
+  ],
+});
