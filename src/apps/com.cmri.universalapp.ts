416a332d213e6721508ad0973da8ac81b3107acc
AIsouler <zy43220@gmail.com>
2024-11-23 13:49:51 +0800

    perf: 优化

--- src/apps/com.cmri.universalapp.ts
+++ src/apps/com.cmri.universalapp.ts
@@ -6,28 +6,34 @@ export default defineGkdApp({
   groups: [
     {
       key: 5,
-      name: '全屏广告-首页',
+      name: '全屏广告-弹窗广告',
       fastQuery: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[vid="ivCancelDlg"][visibleToUser=true]',
-      snapshotUrls: 'https://i.gkd.li/i/17872852',
+      rules: [
+        {
+          matches: '[vid="ivCancelDlg"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/17872852',
+        },
+      ],
     },
     {
       key: 6,
-      name: '局部广告-首页',
+      name: '局部广告-卡片广告',
       fastQuery: true,
       matchTime: 10000,
       actionMaximum: 1,
       activityIds: 'com.cmri.universalapp.SmartMainProxyActivity',
       rules: [
         {
+          key: 0,
           name: '横幅式',
           matches: '[vid="close_banner"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/17872961',
         },
         {
+          key: 1,
           name: '卡片信息流式',
           matches: '[vid="iv_close"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/17872961',
