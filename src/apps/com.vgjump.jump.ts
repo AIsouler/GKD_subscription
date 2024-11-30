063d80a3d5631682da684c7acf098ca55d1fd877
zhendery <zhendery@qq.com>
2024-11-30 13:18:25 +0800

    feat(Jump): 卡片广告 (#548)
    Co-authored-by: AIsouler <zy43220@gmail.com>

--- src/apps/com.vgjump.jump.ts
+++ src/apps/com.vgjump.jump.ts
@@ -56,5 +56,25 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '局部广告-卡片广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: [
+            '.ui.game.detail.GameDetailActivity',
+            '.ui.content.detail.ContentDetailActivity',
+          ],
+          matches: '[vid="ivSDKClose"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/d68c487b-ce63-4d9f-afc3-cc94b3490a33',
+          snapshotUrls: [
+            'https://i.gkd.li/i/17949888',
+            'https://i.gkd.li/i/17949885',
+          ],
+        },
+      ],
+    },
   ],
 });
