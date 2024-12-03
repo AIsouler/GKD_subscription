d6875451c170f351384641273d58c1c498177574
AIsouler <zy43220@gmail.com>
2024-12-03 12:34:05 +0800

    feat(TIM): 功能类-自动点击查看原图、功能类-发送图片时自动勾选[原图] (补充activityid #557)

--- src/apps/com.tencent.tim.ts
+++ src/apps/com.tencent.tim.ts
@@ -10,12 +10,17 @@ export default defineGkdApp({
       rules: [
         {
           fastQuery: true,
-          activityIds:
+          activityIds: [
             'com.tencent.mobileqq.richmediabrowser.AIOGalleryActivity',
+            'com.tencent.richframework.gallery.QQGalleryActivity',
+          ],
           matches: '[text^="查看原图"]',
           exampleUrls:
             'https://m.gkd.li/57941037/894c5e7b-aab1-452c-a2a7-67a605506c89',
-          snapshotUrls: 'https://i.gkd.li/i/15197486',
+          snapshotUrls: [
+            'https://i.gkd.li/i/15197486',
+            'https://i.gkd.li/i/17987327',
+          ],
         },
       ],
     },
@@ -25,11 +30,17 @@ export default defineGkdApp({
       rules: [
         {
           fastQuery: true,
-          activityIds: 'com.tencent.mobileqq.activity.SplashActivity',
+          activityIds: [
+            'com.tencent.mobileqq.activity.SplashActivity',
+            'com.tencent.qqnt.qbasealbum.WinkHomeActivity',
+          ],
           matches: '@[checked=false] + [text="原图"]',
           exampleUrls:
             'https://m.gkd.li/57941037/57b4e39f-ba48-4e57-a5de-3a461a583e60',
-          snapshotUrls: 'https://i.gkd.li/i/15209768',
+          snapshotUrls: [
+            'https://i.gkd.li/i/15209768',
+            'https://i.gkd.li/i/17987325',
+          ],
         },
       ],
     },
