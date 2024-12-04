13e02ed2a14cd25c84d9ce835b86ea0f3ee3061b
zhendery <zhendery@qq.com>
2024-12-04 21:14:41 +0800

    feat(Jump): 局部广告新增信息流广告 (#559)

--- src/apps/com.vgjump.jump.ts
+++ src/apps/com.vgjump.jump.ts
@@ -58,7 +58,7 @@ export default defineGkdApp({
     },
     {
       key: 2,
-      name: '局部广告-卡片广告',
+      name: '局部广告-信息流广告/卡片广告',
       desc: '点击关闭',
       rules: [
         {
@@ -66,12 +66,17 @@ export default defineGkdApp({
           activityIds: [
             '.ui.game.detail.GameDetailActivity',
             '.ui.content.detail.ContentDetailActivity',
+            '.ui.main.MainActivity',
           ],
           matches: '[vid="ivSDKClose"][visibleToUser=true]',
-          exampleUrls: 'https://e.gkd.li/d68c487b-ce63-4d9f-afc3-cc94b3490a33',
+          exampleUrls: [
+            'https://e.gkd.li/388a915b-5b91-4a4a-8bd3-330b346b2864',
+            'https://e.gkd.li/d68c487b-ce63-4d9f-afc3-cc94b3490a33',
+          ],
           snapshotUrls: [
             'https://i.gkd.li/i/17949888',
             'https://i.gkd.li/i/17949885',
+            'https://i.gkd.li/i/18004837',
           ],
         },
       ],
