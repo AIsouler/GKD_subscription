ae04d2a5b623fe6c1ea57bd1e3c3ae5afa15e6e5
AIsouler <zy43220@gmail.com>
2024-12-11 19:24:29 +0800

    feat(嘀嗒出行): 开屏广告 (补充规则 #589)

--- src/apps/com.didapinche.booking.ts
+++ src/apps/com.didapinche.booking.ts
@@ -10,9 +10,20 @@ export default defineGkdApp({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
+      actionMaximumKey: 0,
       priorityTime: 10000,
       rules: [
         {
+          key: 0,
+          fastQuery: true,
+          activityIds: '.home.activity.StartActivity',
+          matches:
+            '@[text.length<10][text*="跳过"][visibleToUser=true] <<n [vid="flCSJAdContainer"]',
+          exampleUrls: 'https://e.gkd.li/155b2ddf-0bd2-4963-b35d-c7aa40a575e7',
+          snapshotUrls: 'https://i.gkd.li/i/18094436',
+        },
+        {
+          key: 1,
           matches:
             'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true][text=null][visibleToUser=true] + TextView[visibleToUser=true][text=null][index=parent.childCount.minus(1)]',
           snapshotUrls: [
