5ac9562fae5454247afb93610939194bf8e4be65
AIsouler <zy43220@gmail.com>
2024-12-12 17:35:56 +0800

    feat(趣智校园): 全屏广告-弹窗广告 (补充规则 #608)

--- src/apps/com.klcxkj.zqxy.ts
+++ src/apps/com.klcxkj.zqxy.ts
@@ -41,7 +41,8 @@ export default defineGkdApp({
           name: '吉欣广告',
           fastQuery: true,
           activityIds: 'com.klcxkj.zqxy.ui.splash.SplashActivity',
-          matches: '@ViewGroup < ViewGroup +2 ViewGroup > [text="吉欣广告"]',
+          matches:
+            '@ViewGroup[clickable=true] < ViewGroup +2 ViewGroup > [text$="广告"]',
           snapshotUrls: 'https://i.gkd.li/i/13274838',
         },
         {
@@ -52,6 +53,14 @@ export default defineGkdApp({
             '@ImageView[childCount=0][text=null][visibleToUser=true] < FrameLayout[childCount=1] <3 FrameLayout[childCount=3] < FrameLayout[childCount=2] +5 FrameLayout[childCount=4] > [text^="扭动或点击"]',
           exampleUrls: 'https://e.gkd.li/d31ed6be-1211-4d34-855c-3f454f8f4b3f',
           snapshotUrls: 'https://i.gkd.li/i/13274836',
+        },
+        {
+          key: 7,
+          fastQuery: true,
+          activityIds: '.ui.main.MainActivity',
+          matches: '[vid="ad_close"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/9c8af961-db98-4ef7-ad96-eff44f908481',
+          snapshotUrls: 'https://i.gkd.li/i/18108931',
         },
       ],
     },
