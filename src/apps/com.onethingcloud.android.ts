e74f0ad9f6ab95c48606e4f8c94fb0a705b8dc0f
AIsouler <zy43220@gmail.com>
2024-12-13 12:22:15 +0800

    feat(网心云): 开屏广告 (#587)

--- src/apps/com.onethingcloud.android.ts
+++ src/apps/com.onethingcloud.android.ts
@@ -5,6 +5,24 @@ export default defineGkdApp({
   name: '网心云',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      priorityTime: 10000,
+      rules: [
+        {
+          fastQuery: true,
+          action: 'clickCenter', // https://github.com/AIsouler/GKD_subscription/issues/587
+          matches:
+            '@View[desc*="跳过"][desc.length<10][visibleToUser=true] < View < View < View < View < FrameLayout < [id="android:id/content"]',
+          exampleUrls: 'https://e.gkd.li/0d0560e5-2b7d-4599-8336-a5254d5e13ea',
+          snapshotUrls: 'https://i.gkd.li/i/18094505',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '全屏广告-弹窗广告',
       matchTime: 10000,
