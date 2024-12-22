a470b9fcd2994070d6f7c0302051163c35bfb4b5
DeepChirp <66902050+DeepChirp@users.noreply.github.com>
2024-12-22 22:47:00 +0800

    feat(TradingView): 全屏广告

--- src/apps/com.tradingview.tradingviewapp.ts
+++ src/apps/com.tradingview.tradingviewapp.ts
@@ -11,10 +11,23 @@ export default defineGkdApp({
       rules: [
         {
           fastQuery: true,
-          activityIds: 'com.tradingview.tradingviewapp.root.view.RootActivity',
+          activityIds: '.root.view.RootActivity',
           matches: '@[clickable=true][text="关闭广告"] <<n [vid="chart_wbv"]',
           exampleUrls: 'https://e.gkd.li/3798ab1b-2cd3-471e-8303-b8124746e9d8',
           snapshotUrls: 'https://i.gkd.li/i/16725697',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '全屏广告',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.root.view.RootActivity',
+          matches: '[vid="friday_fl_layout"] > [vid="friday_iv_close"]',
+          exampleUrls: 'https://e.gkd.li/9cc35e84-1b27-4aa5-a74b-ace7cf42d943',
+          snapshotUrls: 'https://i.gkd.li/i/18222211',
         },
       ],
     },
