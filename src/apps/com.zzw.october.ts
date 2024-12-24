ed23bd3245d732fd1e64606a0ade41958f0d8c50
AIsouler <zy43220@gmail.com>
2024-12-24 13:07:17 +0800

    feat(志愿汇): 全屏广告-弹窗广告 (补充规则 #646)

--- src/apps/com.zzw.october.ts
+++ src/apps/com.zzw.october.ts
@@ -7,17 +7,25 @@ export default defineGkdApp({
     {
       key: 2,
       name: '全屏广告-弹窗广告',
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
       rules: [
         {
           key: 1,
           fastQuery: true,
-          activityIds: 'com.zzw.october.MainActivity',
+          matchTime: 10000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          activityIds: '.MainActivity',
           matches:
             '[id="com.zzw.october:id/interact_ad_root"] >n ImageView[id="com.zzw.october:id/iv_close"]',
           snapshotUrls: 'https://i.gkd.li/i/12842675',
+        },
+        {
+          key: 2,
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches: '[vid="ad_close"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/2033b8e2-8234-486d-ba8a-e7d140215d88',
+          snapshotUrls: 'https://i.gkd.li/i/18235996',
         },
       ],
     },
