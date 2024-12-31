1be30730f3891f8e902e7fa2c080692d7ec6bb42
minglu7 <104314597+minglu7@users.noreply.github.com>
2024-12-31 09:56:04 +0800

    Update com.taitedi.hdd.ayi.ts
    Signed-off-by: minglu7 <104314597+minglu7@users.noreply.github.com>

--- src/apps/com.taitedi.hdd.ayi.ts
+++ src/apps/com.taitedi.hdd.ayi.ts
@@ -5,12 +5,12 @@ export default defineGkdApp({
   name: '爱其意',
   groups: [
     {
-      key: 0,
-      name: '开屏广告',
+      key: 7,
+      name: '全屏广告',
       exampleUrls: [
         'https://github.com/gkd-kit/subscription/assets/38517192/57787554-0443-4bc0-9f29-1759aae07b9b',
       ],
-      activityIds: ['.MainActivity'],
+      activityIds: ['.MainActivity', '.MainActivity'],
       rules: [
         {
           matches: 'View[desc="我知道了"]',
@@ -18,7 +18,7 @@ export default defineGkdApp({
         },
         {
           matches:
-            'ImageView[height=28][width=28][name="android.widget.ImageView"]',
+            'ImageView[height=28 || height=56][width=28 || width=56][name="android.widget.ImageView"]',
           snapshotUrls: 'https://i.gkd.li/i/18259775',
         },
       ],
