b2a4f4e3c4fb5b3b69ffe6b75efdbf696822d391
minglu7 <104314597+minglu7@users.noreply.github.com>
2025-01-01 20:05:00 +0800

    删除多余规则，增加校验逻辑
    Signed-off-by: minglu7 <104314597+minglu7@users.noreply.github.com>

--- src/apps/com.taitedi.hdd.ayi.ts
+++ src/apps/com.taitedi.hdd.ayi.ts
@@ -7,19 +7,12 @@ export default defineGkdApp({
     {
       key: 7,
       name: '全屏广告',
-      exampleUrls: [
-        'https://github.com/gkd-kit/subscription/assets/38517192/57787554-0443-4bc0-9f29-1759aae07b9b',
-      ],
-      activityIds: ['.MainActivity', '.MainActivity'],
+      activityIds: ['.MainActivity'],
       rules: [
         {
-          matches: 'View[desc="我知道了"]',
-          snapshotUrls: ['https://i.gkd.li/i/18261037'],
-        },
-        {
           matches:
-            'ImageView[height=28 || height=56][width=28 || width=56][name="android.widget.ImageView"]',
-          snapshotUrls: 'https://i.gkd.li/i/18259775',
+            'ImageView[height <= 60][width <=60][name="android.widget.ImageView"]',
+          snapshotUrls: 'https://i.gkd.li/i/18285341',
         },
       ],
     },
