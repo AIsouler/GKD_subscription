d815ecd46558b97694ca68dee712654587efa3f1
minglu7 <1347866672@qq.com>
2025-01-03 10:44:22 +0800

    Update com.alcidae.smarthome.ts
    Signed-off-by: minglu7 <1347866672@qq.com>

--- src/apps/com.alcidae.smarthome.ts
+++ src/apps/com.alcidae.smarthome.ts
@@ -1,16 +1,16 @@
-{
+import { defineGkdApp } from '@gkd-kit/define';
+export default defineGkdApp({
   id: 'com.alcidae.smarthome',
   name: '海雀',
   groups: [
     {
       key: 1,
-      name: '全屏广告-商家活动',
+      name: '通知提示-首页通知弹窗',
       desc: '点击[不再提醒]',
-	    fastQuery: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-	    snapshotUrls: 'https://i.gkd.li/i/18291310',
+	  snapshotUrls: 'https://i.gkd.li/i/18291310',
       rules: [
 		{
           activityIds: 'com.danale.video.mainpage.main.MainActivity',
@@ -19,15 +19,13 @@
         {
           activityIds: 'com.danale.video.mainpage.main.MainActivity',
           matches: '@TextView[text="不再提醒"] - @CheckBox[id="com.alcidae.smarthome:id/cb_not_show"][visibleToUser=true] < LinearLayout[childCount=2] <<n FrameLayout',
-        }
+        },
       ],
     },
     {
       name: '通知提示',
       key: 4,
-	    desc: '拒绝开启通知',
-	    fastQuery: true,
-      matchTime: 10000,
+	  desc: '拒绝开启通知'
       snapshotUrls: [
         'https://i.gkd.li/i/18059150'
       ],
@@ -40,4 +38,4 @@
       ]
     }
   ],
-}
+});
