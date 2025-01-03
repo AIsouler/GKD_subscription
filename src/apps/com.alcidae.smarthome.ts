57f64d6d7d53002243613692763f4d393d621d7b
minglu7 <1347866672@qq.com>
2025-01-03 10:40:57 +0800

    feat: 关闭海雀提示与活动通知
    Signed-off-by: minglu7 <1347866672@qq.com>

---
+++ src/apps/com.alcidae.smarthome.ts
@@ -0,0 +1,43 @@
+{
+  id: 'com.alcidae.smarthome',
+  name: '海雀',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-商家活动',
+      desc: '点击[不再提醒]',
+	    fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+	    snapshotUrls: 'https://i.gkd.li/i/18291310',
+      rules: [
+		{
+          activityIds: 'com.danale.video.mainpage.main.MainActivity',
+          matches: '@LinearLayout[childCount=2] + @ImageButton[id="com.alcidae.smarthome:id/ibtn_close"][visibleToUser=true][clickable=true] <<n FrameLayout[id="android:id/content"]',
+        },
+        {
+          activityIds: 'com.danale.video.mainpage.main.MainActivity',
+          matches: '@TextView[text="不再提醒"] - @CheckBox[id="com.alcidae.smarthome:id/cb_not_show"][visibleToUser=true] < LinearLayout[childCount=2] <<n FrameLayout',
+        }
+      ],
+    },
+    {
+      name: '通知提示',
+      key: 4,
+	    desc: '拒绝开启通知',
+	    fastQuery: true,
+      matchTime: 10000,
+      snapshotUrls: [
+        'https://i.gkd.li/i/18059150'
+      ],
+      rules: [
+        {
+          matches: [
+            'TextView[id="com.alcidae.smarthome:id/danale_info_dialog_cancel_btn"][text="取消"]'
+          ]
+        }
+      ]
+    }
+  ],
+}
