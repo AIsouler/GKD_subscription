dfab3ebaa5f818de3f8d37fde59cdddf8995c30e
AIsouler <zy43220@gmail.com>
2025-01-03 15:17:03 +0800

    feat(中国工商银行): 评价提示 (#676)

--- src/apps/com.icbc.ts
+++ src/apps/com.icbc.ts
@@ -12,9 +12,24 @@ export default defineGkdApp({
       resetMatch: 'app',
       rules: [
         {
-          activityIds: 'com.icbc.activity.main.MainActivity',
+          activityIds: '.activity.main.MainActivity',
           matches: '[id="com.icbc:id/tv_time"][text*="跳过"][text.length<=6]',
           snapshotUrls: 'https://i.gkd.li/i/13330431',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '评价提示',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.activity.web.ICBCWebView',
+          matches: '[text="下次再说"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/85e48265-4269-4122-a70f-5361698954c0',
+          snapshotUrls: 'https://i.gkd.li/i/18296892',
         },
       ],
     },
