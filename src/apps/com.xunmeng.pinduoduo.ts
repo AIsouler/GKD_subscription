e7e231064f0eadf42b78a1346af691e56395fe73
AIsouler <zy43220@gmail.com>
2025-01-10 16:21:03 +0800

    feat: 补充activityid

--- src/apps/com.xunmeng.pinduoduo.ts
+++ src/apps/com.xunmeng.pinduoduo.ts
@@ -280,8 +280,10 @@ export default defineGkdApp({
           activityIds: [
             '.ui.activity.HomeActivity',
             '.activity.NewPageActivity',
+            '.ui.activity.MainFrameActivity',
           ],
-          matches: '[text="及时获取物流消息" || text="开启通知" || text="允许通知"]',
+          matches:
+            '[text="及时获取物流消息" || text="开启通知" || text="允许通知"]',
           snapshotUrls: [
             'https://i.gkd.li/i/15004580',
             'https://i.gkd.li/i/15048085',
