a3056561c1d3f87eae456ccdf927445ab96c324d
AIsouler <zy43220@gmail.com>
2025-01-10 19:56:17 +0800

    perf: 优化

--- src/apps/com.ziroom.ziroomcustomer.ts
+++ src/apps/com.ziroom.ziroomcustomer.ts
@@ -6,41 +6,34 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '全屏弹窗广告',
-      fastQuery: true,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
-          key: 0,
-          name: '点击叉号关闭',
-          activityIds: [
-            '.MainActivity',
-          ],
-          matches: ['[text*="本弹窗由自如推送"] + * > [vid="tv_ok"]'],
-          snapshotUrls: [
-            'https://i.gkd.li/i/18373198',
-          ],
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches: '[vid="tv_ok"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/c6507af4-1bb2-4c39-ab5d-3d04ec391291',
+          snapshotUrls: 'https://i.gkd.li/i/18373198',
         },
       ],
     },
     {
       key: 2,
-      name: '推荐好友弹窗',
-      fastQuery: true,
+      name: '全屏广告-推荐好友弹窗',
+      desc: '点击关闭',
       actionMaximum: 1,
       resetMatch: 'app',
-      activityIds: [
-        '.MainActivity',
-      ],
       rules: [
         {
-          key: 0,
-          name: '点击叉号关闭',
-          matches: ['[text*="向亲朋好友推荐自如"] + [vid="iv_nps_close"]'],
-          snapshotUrls: [
-            'https://i.gkd.li/i/18373218',
-          ],
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches: '[vid="iv_nps_close"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/808f8c61-211f-4592-abe5-21467c133ba6',
+          snapshotUrls: 'https://i.gkd.li/i/18373218',
         },
       ],
     },
