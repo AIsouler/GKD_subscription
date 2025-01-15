9284a6d24177acd7fac852d348c164eed1f9c8fe
AIsouler <zy43220@gmail.com>
2025-01-15 17:39:02 +0800

    perf: 优化

--- src/apps/com.wanbangauto.chargepile.ts
+++ src/apps/com.wanbangauto.chargepile.ts
@@ -5,35 +5,37 @@ export default defineGkdApp({
   name: '星星充电',
   groups: [
     {
-      key: 7,
-      name: '全屏广告-开通广告',
-      desc: '点击取消',
+      key: 1,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
       fastQuery: true,
-      activityIds: '.ui.newhome.MainActivity',
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
-          matches:
-            'FrameLayout[id="android:id/content"] > * > LinearLayout[childCount=2] > LinearLayout[vid="banner"] + @ImageView[vid="image_close"]',
+          activityIds: '.ui.newhome.MainActivity',
+          matches: '@[vid="image_close"][visibleToUser=true]',
           exampleUrls: 'https://e.gkd.li/ce4838fb-0f0d-4b20-bab3-565bd80bbc93',
           snapshotUrls: 'https://i.gkd.li/i/18421090',
         },
       ],
     },
     {
-      key: 9,
+      key: 2,
       name: '功能类-签到提示',
-      desc: '点击【忽略】',
+      desc: '点击[忽略]',
       fastQuery: true,
-      activityIds: '.ui.newhome.MainActivity',
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
-          matches: ['[text^="小星提醒您"]', '[text="忽略"]'],
+          activityIds: '.ui.newhome.MainActivity',
+          matches: [
+            '[text^="小星提醒您"][visibleToUser=true]',
+            '[text="忽略"][visibleToUser=true]',
+          ],
           exampleUrls: 'https://e.gkd.li/542507c4-57dd-46f5-b354-78cdbacb3191',
           snapshotUrls: 'https://i.gkd.li/i/18421092',
         },
