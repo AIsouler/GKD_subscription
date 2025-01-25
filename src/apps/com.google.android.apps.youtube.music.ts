6be9d9c0b444c5264d8a8b3555ae035a88ee158e
AIsouler <zy43220@gmail.com>
2025-01-25 13:57:00 +0800

    feat(YouTube Music): 全屏广告-会员广告 (#742)

--- src/apps/com.google.android.apps.youtube.music.ts
+++ src/apps/com.google.android.apps.youtube.music.ts
@@ -18,5 +18,19 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '全屏广告-会员广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.activities.MusicActivity',
+          matches: '@[desc="Close"][visibleToUser=true] <<n [vid="custom"]',
+          exampleUrls: 'https://e.gkd.li/42b2cb6d-b1e2-4cb1-84d8-399939263711',
+          snapshotUrls: 'https://i.gkd.li/i/18542111',
+        },
+      ],
+    },
   ],
 });
