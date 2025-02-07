2a45e6df4a6ad6fe6a064e6bb27fccde49811221
DoubleZ <masteryi_z@163.com>
2025-02-07 17:20:01 +0800

    feat: add rule3

--- src/apps/com.qinlin.edoor.ts
+++ src/apps/com.qinlin.edoor.ts
@@ -80,6 +80,15 @@ export default defineGkdApp({
             'https://i.gkd.li/i/18703827',
           ],
         },
+        {
+          key: 5,
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches:
+            '@FrameLayout <2 FrameLayout <2 FrameLayout <2 FrameLayout - FrameLayout > [text="扭动或点击立刻开玩"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/a2685f57-ec36-44a1-9529-f10d7fb481ef',
+          snapshotUrls: 'https://i.gkd.li/i/18704056',
+        },
       ],
     },
     {
