50267ab66bdf1b1d9cdf4675e9a51899346701f4
Sam <541861+heximing@users.noreply.github.com>
2025-03-26 00:25:33 -0400

    Update com.google.android.apps.photos.ts
    refer https://github.com/AIsouler/GKD_subscription/issues/754
    经测试，只要加上`.home.HomeActivity`，无论听不听歌，都能正常触发。
    现在发现一个小问题，用这个APP的时候，偶尔会有小掉帧。骁龙8E的手机，不应该啊。加回`[visibleToUser=true]` 再观察观察是不是这个原因。
    
    Signed-off-by: Sam <541861+heximing@users.noreply.github.com>

--- src/apps/com.google.android.apps.photos.ts
+++ src/apps/com.google.android.apps.photos.ts
@@ -17,7 +17,7 @@ export default defineGkdApp({
             '.update.treatment.UpdateAppTreatmentPromoPageActivity',
             '.home.HomeActivity',
           ],
-          matches: '[text="以后再说" || text="Not now"]', // [visibleToUser=true] https://github.com/AIsouler/GKD_subscription/issues/754
+          matches: '[text="以后再说" || text="Not now"][visibleToUser=true]', // https://github.com/AIsouler/GKD_subscription/issues/754
           snapshotUrls: [
             'https://i.gkd.li/i/13218940',
             'https://i.gkd.li/i/18522070',
