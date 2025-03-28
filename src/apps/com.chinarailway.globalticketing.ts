5708ed7b08f2cde6e669a3f14186143cf448a06b
DeepChirp <66902050+DeepChirp@users.noreply.github.com>
2025-03-28 13:17:23 +0800

    feat: 铁路12306-其他-关闭公告 (#791)
    Co-authored-by: AIsouler <zy43220@gmail.com>

--- src/apps/com.chinarailway.globalticketing.ts
+++ src/apps/com.chinarailway.globalticketing.ts
@@ -20,5 +20,21 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '通知提示-关闭底部公告横幅',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.MobileTicket.ui.activity.MainActivity',
+          matches: '[vid="iv_tip_close"][desc^="关闭公告"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/be03fa78-7c6f-4a05-8370-93a5de9a246e',
+          snapshotUrls: 'https://i.gkd.li/i/19497545',
+        },
+      ],
+    },
   ],
 });
