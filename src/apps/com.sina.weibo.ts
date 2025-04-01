df4a56bd3cecb37984f5e716a0efc47d26ec3cc2
AIsouler <zy43220@gmail.com>
2025-04-01 22:27:46 +0800

    perf: 微博 #814

--- src/apps/com.sina.weibo.ts
+++ src/apps/com.sina.weibo.ts
@@ -101,56 +101,40 @@ export default defineGkdApp({
       name: '分段广告-信息流广告',
       desc: '点击关闭-点击[不感兴趣]/[不想看到此类内容]',
       fastQuery: true,
+      activityIds: [
+        '.feed.DetailWeiboActivity',
+        '.feed.detailrefactor.DetailPageActivity',
+        '.story.gallery.feed.StoryFeedCommentsActivity2',
+      ],
       rules: [
         {
           key: 0,
-          activityIds: [
-            'com.sina.weibo.feed.DetailWeiboActivity',
-            '.feed.detailrefactor.DetailPageActivity',
-          ],
           matches: '[vid="ll_close"][visibleToUser=true]',
           snapshotUrls: [
             'https://i.gkd.li/i/13852321',
             'https://i.gkd.li/i/19526000',
+            'https://i.gkd.li/i/19552278',
           ],
         },
         {
           key: 1,
-          activityIds: 'com.sina.weibo.feed.DetailWeiboActivity',
           matches: '[vid="corner_marker_view"] >2 [vid="right_top_tag"]',
           snapshotUrls: 'https://i.gkd.li/i/14210775',
         },
         {
-          key: 2,
-          position: {
-            left: 'width * 0.9537',
-            top: 'width * 0.0611',
-          },
-          activityIds: 'com.sina.weibo.MainTabActivity',
-          matches:
-            'LinearLayout + LinearLayout[visibleToUser=true][childCount=6] > @ViewGroup > ViewGroup > FrameLayout > ImageView',
-          snapshotUrls: [
-            'https://i.gkd.li/i/14729705',
-            'https://i.gkd.li/i/14753916', // 避免在此页面误触
-          ],
-        },
-        {
           key: 3,
-          activityIds: 'com.sina.weibo.feed.DetailWeiboActivity',
           matches: [
             '[vid="left_img_ad_tag"][visibleToUser=true]',
             '[vid="close"][visibleToUser=true]',
           ],
-          exampleUrls: 'https://e.gkd.li/039f312b-0d6a-4316-9737-ead761e8e194',
           snapshotUrls: 'https://i.gkd.li/i/16828094',
         },
 
         // 预留key
         {
-          preKeys: [0, 1, 2, 3],
+          preKeys: [0, 1, 3],
           key: 90,
           matchRoot: true,
-          activityIds: 'com.sina.weibo.feed.DetailWeiboActivity',
           anyMatches: [
             '[text="不感兴趣"][clickable=true]',
             '@LinearLayout[clickable=true] >2 [text="不感兴趣" || text="不想看到此类内容"]',
@@ -160,6 +144,8 @@ export default defineGkdApp({
             'https://i.gkd.li/i/13958782',
             'https://i.gkd.li/i/16785777',
             'https://i.gkd.li/i/14730274',
+            'https://i.gkd.li/i/19551451',
+            'https://i.gkd.li/i/19552282',
           ],
         },
       ],
