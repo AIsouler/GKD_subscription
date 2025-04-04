881c8b671eb21328bfe9153af1d36e78f0b51c27
AIsouler <zy43220@gmail.com>
2025-04-04 19:55:43 +0800

    feat: biubiu加速器-全屏广告-弹窗广告 #829

--- src/apps/com.njh.biubiu.ts
+++ src/apps/com.njh.biubiu.ts
@@ -42,5 +42,19 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 1,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.njh.ping.speedup.detail.fragment.LaunchVPNActivity',
+          matches: '[vid="iv_listitem_dislike"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/13a828c7-b94d-463f-bceb-a92fa7ff069a',
+          snapshotUrls: 'https://i.gkd.li/i/19603369',
+        },
+      ],
+    },
   ],
 });
