022bb6730ec8297d7ffd0c31aa683693ce27eead
AIsouler <zy43220@gmail.com>
2025-04-08 14:13:37 +0800

    feat: rules #835 #840

--- src/apps/com.mi.poketrade.ts
+++ src/apps/com.mi.poketrade.ts
@@ -19,5 +19,23 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '全屏广告-订阅广告',
+      desc: '点击关闭',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches:
+            '@ImageView[clickable=true][childCount=0][visibleToUser=true] <2 View[getChild(5).desc="Subscribe"] < View < View < View < FrameLayout < FrameLayout < [id="android:id/content"]',
+          exampleUrls: 'https://e.gkd.li/19371ab4-5b1a-4efb-925a-883252fd50d0',
+          snapshotUrls: 'https://i.gkd.li/i/19643520',
+        },
+      ],
+    },
   ],
 });
