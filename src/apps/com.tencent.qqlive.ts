a142a49c5d8c85723d72061b4e12868bdf93a170
AIsouler <zy43220@gmail.com>
2025-04-09 21:33:49 +0800

    feat: 腾讯视频 #842

--- src/apps/com.tencent.qqlive.ts
+++ src/apps/com.tencent.qqlive.ts
@@ -24,149 +24,76 @@ export default defineGkdApp({
     {
       key: 2,
       name: '更新提示',
+      fastQuery: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
           activityIds: '.ona.update.trunk.client.TrunkUpdateActivity',
-          matches:
-            '[text^="有新版本"] + FrameLayout + LinearLayout + [text="暂不升级"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12700486',
-            'https://i.gkd.li/i/13799951',
-          ],
+          matches: '[text="暂不升级"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/19666787',
         },
       ],
     },
     {
       key: 3,
       name: '分段广告-卡片广告',
+      activityIds: [
+        '.ona.activity.SplashHomeActivity',
+        '.ona.activity.VideoDetailActivity',
+      ],
       rules: [
         {
           key: 0,
-          name: '首页顶部卡片广告',
-          activityIds: 'com.tencent.qqlive.ona.activity.SplashHomeActivity',
+          fastQuery: true,
           matches:
-            'FrameLayout - RelativeLayout > RelativeLayout > FrameLayout + ImageView[clickable=true][childCount=0][visibleToUser=true]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12700299',
-            'https://i.gkd.li/i/12700302',
-            'https://i.gkd.li/i/13685929', // 限定 visibleToUser=true 防止误触
-          ],
+            '@ImageView[childCount=0][clickable=true][visibleToUser=true] - FrameLayout >4 ViewGroup > [vid="player_sub_title_view"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/d0fb9740-defa-437a-882e-b3c5d6a2d231',
+          snapshotUrls: 'https://i.gkd.li/i/19666783',
         },
         {
           key: 1,
-          name: '首页顶部背景广告',
-          activityIds: 'com.tencent.qqlive.ona.activity.SplashHomeActivity',
-          matches: '[text="关闭广告"][clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/i/12700518',
+          fastQuery: true,
+          matches:
+            '[desc="the ad tag"] < @FrameLayout[clickable=true] < RelativeLayout < RelativeLayout +(1,2) * >(4,5) [text*="应用" || text*="立即" || text*="下载" || text*="了解" || text*="查看" || text*="详情" || text="去微信看看"][text.length<6][visibleToUser=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/19667010',
+            'https://i.gkd.li/i/19667126',
+            'https://i.gkd.li/i/19667028',
+            'https://i.gkd.li/i/19667404',
+            'https://i.gkd.li/i/19667413',
+            'https://i.gkd.li/i/19667494',
+            'https://i.gkd.li/i/19667496',
+            'https://i.gkd.li/i/19667620',
+            'https://i.gkd.li/i/19667647',
+          ],
         },
         {
           key: 2,
-          name: '个人中心页卡片广告',
-          activityIds: [
-            'com.tencent.qqlive.ona.activity.SplashHomeActivity',
-            'com.tencent.qqlive.ona.offline.client.group.DownloadGroupActivity',
-          ],
-          matches:
-            'ViewGroup > TextView + LinearLayout[childCount=2] + ImageView[clickable=true]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12700175',
-            'https://i.gkd.li/i/13759380',
-          ],
-        },
-        {
-          key: 3,
-          name: '个人中心页顶部背景广告',
-          activityIds: 'com.tencent.qqlive.ona.activity.SplashHomeActivity',
-          matches:
-            'RelativeLayout > FrameLayout + @ImageView[clickable=true] + ImageView + ImageView',
-          snapshotUrls: 'https://i.gkd.li/i/12777344',
-        },
-        {
-          key: 4,
-          name: '点击右上角[广告]',
-          activityIds: [
-            'com.tencent.qqlive.ona.activity.SplashHomeActivity',
-            'com.tencent.qqlive.ona.activity.VideoDetailActivity',
-          ],
-          matches:
-            'RelativeLayout[id=null] > @FrameLayout[clickable=true][id!=null] > ImageView[desc="the ad tag"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12737313',
-            'https://i.gkd.li/i/13685842',
-          ],
-        },
-        {
-          key: 5,
-          name: '点击右下角关闭',
-          matches:
-            'ImageView[childCount=0] < * < RelativeLayout + FrameLayout + RelativeLayout > RelativeLayout > RelativeLayout > ImageView',
-          snapshotUrls: 'https://i.gkd.li/i/13426421',
-        },
-        {
-          key: 6,
           fastQuery: true,
-          matches: '[id="com.tencent.qqlive:id/feed_icon"][clickable=true]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/42013a93-fb12-4747-92e0-95f5028eb8e5',
-          snapshotUrls: 'https://i.gkd.li/i/14318802',
-        },
-        // 以下是配合本规则组内其他key使用的规则，反馈界面的规则都是一样的
-        {
-          preKeys: [6],
-          key: 96,
-          fastQuery: true,
+          activityIds: '.ona.activity.SplashHomeActivity',
           matches:
-            '[id="com.tencent.qqlive:id/ad_feed_back_dislike"][clickable=true]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/8746fdc8-828e-42bb-9160-8e67b7af2dc7',
-          snapshotUrls: 'https://i.gkd.li/i/14318811',
+            '@ImageView[text=null][desc=null][clickable=true][visibleToUser=true][width<250 && height<150] - RelativeLayout >2 [text="了解详情"]',
+          snapshotUrls: 'https://i.gkd.li/i/19666328',
         },
         {
-          key: 97,
-          name: '广告反馈卡片-点击不感兴趣',
-          activityIds: 'com.tencent.qqlive.ona.activity.SplashHomeActivity',
-          matches: '@FrameLayout[clickable=true] >3 [text="不感兴趣"]',
-          snapshotUrls: 'https://i.gkd.li/i/13695084',
-        },
-        {
-          key: 98,
-          name: '广告反馈卡片-选择原因',
+          preKeys: [0, 1, 2],
+          key: 50,
           fastQuery: true,
-          activityIds: [
-            'com.tencent.qqlive.qaduikit.common.dialog.feedback.variable.QAdFeedbackVariableDislikeItemDialog',
-            'com.tencent.qqlive.ona.activity.SplashHomeActivity',
-            'com.tencent.qqlive.ona.activity.VideoDetailActivity',
-            'com.tencent.qqlive.ona.offline.client.group.DownloadGroupActivity',
-          ],
-          matches:
-            '[text="关闭这条广告的原因"] +(2) RecyclerView > [text="不感兴趣"]',
+          matches: '[text="不感兴趣"][visibleToUser=true]',
           snapshotUrls: [
-            'https://i.gkd.li/i/12700303',
-            'https://i.gkd.li/i/12829866',
-            'https://i.gkd.li/i/13685871',
-            'https://i.gkd.li/i/13703219',
+            'https://i.gkd.li/i/19667198',
+            'https://i.gkd.li/i/19667244',
           ],
         },
         {
-          preKeys: [98],
-          key: 99,
-          name: '广告反馈卡片-确认原因',
+          preKeys: [50],
           fastQuery: true,
-          activityIds: [
-            'com.tencent.qqlive.qaduikit.common.dialog.feedback.variable.QAdFeedbackVariableDislikeItemDialog',
-            'com.tencent.qqlive.ona.activity.SplashHomeActivity',
-            'com.tencent.qqlive.ona.activity.VideoDetailActivity',
-            'com.tencent.qqlive.ona.offline.client.group.DownloadGroupActivity',
-          ],
-          matches:
-            '[text="关闭这条广告的原因"] + [text="确认"][clickable=true]',
+          matches: '[text="确认"][visibleToUser=true]',
           snapshotUrls: [
-            'https://i.gkd.li/i/12700210',
-            'https://i.gkd.li/i/13685877',
-            'https://i.gkd.li/i/13703298',
+            'https://i.gkd.li/i/19667102',
+            'https://i.gkd.li/i/19666759',
           ],
         },
       ],
@@ -177,24 +104,6 @@ export default defineGkdApp({
       desc: '自动点击 跳过/关闭广告',
       activityIds: 'com.tencent.qqlive.ona.activity.VideoDetailActivity',
       rules: [
-        {
-          key: 0,
-          name: '全屏广告',
-          fastQuery: true,
-          matches:
-            '@[text$="跳过广告"][clickable=true] < LinearLayout > [text="VIP可关闭该广告"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12700407',
-            'https://i.gkd.li/i/12700433',
-          ],
-        },
-        {
-          key: 1,
-          name: '左下角悬浮广告',
-          matches:
-            'FrameLayout[childCount=2] > FrameLayout > RelativeLayout > View[id!=null][clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/i/13043079',
-        },
         {
           key: 2,
           name: '全屏广告2',
@@ -207,10 +116,11 @@ export default defineGkdApp({
           name: '居中广告-1',
           fastQuery: true,
           matches:
-            '@ImageView[clickable=true][childCount=0] - * >(3,4) [text^="摇动或点击"][visibleToUser=true]',
+            '@ImageView[clickable=true][childCount=0][width<150&&height<150] - * >(3,4) [text^="摇动或点击" || text*="应用" || text*="立即" || text*="下载" || text*="了解" || text*="查看" || text*="详情" || text="去微信看看"][visibleToUser=true]',
           snapshotUrls: [
             'https://i.gkd.li/i/18476380',
             'https://i.gkd.li/i/18476384',
+            'https://i.gkd.li/i/19667827',
           ],
         },
         {
@@ -230,11 +140,10 @@ export default defineGkdApp({
     },
     {
       key: 6,
-      name: '全屏广告-首页-弹窗广告',
+      name: '全屏广告-弹窗广告',
       fastQuery: true,
       matchTime: 10000,
       actionMaximum: 1,
-      resetMatch: 'app',
       rules: [
         {
           key: 0,
@@ -268,6 +177,21 @@ export default defineGkdApp({
           matches: '[text="暂不需要，稍后领取"][visibleToUser=true]',
           exampleUrls: 'https://e.gkd.li/3256ed8e-752c-426d-9d64-26c9fdea9b09',
           snapshotUrls: 'https://i.gkd.li/i/17542869',
+        },
+        {
+          key: 4,
+          activityIds: '.ona.activity.VideoDetailActivity',
+          matches: '[text="放弃权益"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/37d3d077-1d3b-4b4d-87c2-4e0fa00d423e',
+          snapshotUrls: 'https://i.gkd.li/i/19666924',
+        },
+        {
+          key: 5,
+          activityIds: '.ona.activity.SplashHomeActivity',
+          matches:
+            '@ImageView[clickable=true][childCount=0][visibleToUser=true][width<200 && height<200] <2 RelativeLayout < RelativeLayout < RelativeLayout < [id="android:id/content"]',
+          exampleUrls: 'https://e.gkd.li/afb17212-1dcb-4d07-a9ce-b4f775ccd6fc',
+          snapshotUrls: 'https://i.gkd.li/i/19666714',
         },
       ],
     },
@@ -307,23 +231,47 @@ export default defineGkdApp({
             'https://m.gkd.li/57941037/1b7518c9-4ca7-4905-8929-6f0130abf19f',
           snapshotUrls: 'https://i.gkd.li/i/14358913',
         },
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: '.ona.activity.SplashHomeActivity',
+          matches:
+            '@ImageView[childCount=0][clickable=true][visibleToUser=true][width<250 && height<150] <3 RelativeLayout < FrameLayout <2 FrameLayout < FrameLayout <2 ViewGroup < FrameLayout < FrameLayout < FrameLayout < FrameLayout < FrameLayout <5 [id="android:id/content"]',
+          exampleUrls: 'https://e.gkd.li/0ea465ad-e4e4-4af5-92a4-2d71e44845f4',
+          snapshotUrls: 'https://i.gkd.li/i/19667104',
+        },
       ],
     },
     {
       key: 22,
       name: '局部广告-悬浮广告',
       desc: '点击关闭',
+      matchTime: 10000,
+      actionMaximum: 1,
       rules: [
         {
+          key: 0,
           fastQuery: true,
-          matchTime: 10000,
-          actionMaximum: 1,
-          resetMatch: 'app',
-          activityIds: '.ona.activity.SplashHomeActivity',
+          activityIds: [
+            '.ona.activity.SplashHomeActivity',
+            '.ona.activity.VideoDetailActivity',
+          ],
           matches:
-            '@ImageView[clickable=true][visibleToUser=true] + [text="免费看"]',
+            '@ImageView[childCount=0][clickable=true][visibleToUser=true] + [text^="免费看"][visibleToUser=true]',
           exampleUrls: 'https://e.gkd.li/5f6447c1-39f6-4711-a907-fdf36f502ec6',
-          snapshotUrls: 'https://i.gkd.li/i/17542913',
+          snapshotUrls: [
+            'https://i.gkd.li/i/17542913',
+            'https://i.gkd.li/i/19667010',
+          ],
+        },
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: '.ona.activity.VideoDetailActivity',
+          matches:
+            '@ImageView[id=null][text=null][desc=null][clickable=true][visibleToUser=true][width<150&&height<150] <2 FrameLayout[desc="float_widget_activity"] < FrameLayout <3 FrameLayout <3 [id="android:id/content"]',
+          exampleUrls: 'https://e.gkd.li/8cac3233-7ead-4f5e-9126-51a195788276',
+          snapshotUrls: 'https://i.gkd.li/i/19667028',
         },
       ],
     },
