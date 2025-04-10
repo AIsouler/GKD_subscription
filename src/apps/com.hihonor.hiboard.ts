49e933e3a991e213d6ce3dea1d367e6c9840a949
AIsouler <zy43220@gmail.com>
2025-04-10 12:26:41 +0800

    feat: 负一屏-分段广告-卡片广告 #847

---
+++ src/apps/com.hihonor.hiboard.ts
@@ -0,0 +1,30 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.hihonor.hiboard',
+  name: '负一屏',
+  groups: [
+    {
+      key: 1,
+      name: '分段广告-卡片广告',
+      fastQuery: true,
+      activityIds:
+        'com.hihonor.feed.ui.weather.nested.NestedWeatherDetailActivity',
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[vid="ll_ad_flag_layout"] > [vid="iv_close"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/3ca8edb5-ba16-406c-8319-7bc5389f6e31',
+          snapshotUrls: 'https://i.gkd.li/i/19670830',
+        },
+        {
+          preKeys: [0],
+          matches: '[text="不感兴趣"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/eaea2f1e-741a-4171-9cc1-8a2c86848d93',
+          snapshotUrls: 'https://i.gkd.li/i/19670664',
+        },
+      ],
+    },
+  ],
+});
