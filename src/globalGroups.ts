149368a7f3dd3b7dc2da33921f4bf97231e9cdcd
AIsouler <zy43220@gmail.com>
2025-04-11 16:35:08 +0800

    feat: add snapshotUrls

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -43,6 +43,10 @@ export default defineGkdGlobalGroups([
           '@View[text=null][clickable=true][childCount=0][visibleToUser=true][width<200&&height<200] +(1,2) TextView[index=parent.childCount.minus(1)][text=null] <n FrameLayout[childCount>2] >(7,8,9,10) [text*="第三方应用" || text*="扭动手机" || text*="点击或上滑"][visibleToUser=true]',
           'FrameLayout > FrameLayout[childCount>2] > @View[text=null][clickable=true][childCount=0][visibleToUser=true][width<200&&height<200] +(1,2) TextView[index=parent.childCount.minus(1)][text=null][visibleToUser=true]',
         ],
+        snapshotUrls: [
+          'https://i.gkd.li/i/19685971', // +(1,2)
+          'https://i.gkd.li/i/19701216', // +(1,2)
+        ],
       },
     ],
     apps: [...appList.openAdBlackListAppIDs]
