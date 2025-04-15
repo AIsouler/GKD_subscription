825c8bd7908da95da41375c387e535df36946fcb
AIsouler <zy43220@gmail.com>
2025-04-15 15:05:38 +0800

    perf: 凤凰新闻-分段广告-信息流广告

--- src/apps/com.ifeng.news2.ts
+++ src/apps/com.ifeng.news2.ts
@@ -7,44 +7,38 @@ export default defineGkdApp({
     {
       key: 1,
       name: '分段广告-信息流广告',
-      activityIds: 'com.ifeng.news2.activity.IfengTabMainActivity',
+      fastQuery: true,
+      activityIds: [
+        '.activity.IfengTabMainActivity',
+        '.activity.DocDetailActivity',
+      ],
       rules: [
         {
           key: 1,
           matches:
-            '[id="com.ifeng.news2:id/adv_label"] + [id="com.ifeng.news2:id/normal_item_delete_wrap"]',
-          snapshotUrls: 'https://i.gkd.li/i/12705500',
+            '@[vid="img_feedback_left_of_image"] < LinearLayout -2 LinearLayout >2 [vid="adv_label"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/63216ca1-4f7c-4a95-9b9e-7ac4c894f0c0',
+          snapshotUrls: 'https://i.gkd.li/i/19752573',
         },
         {
           key: 2,
           matches:
-            '@[id="com.ifeng.news2:id/img_feedback_left_of_image"] - LinearLayout >(2) [id="com.ifeng.news2:id/adv_label"]',
-          snapshotUrls: 'https://i.gkd.li/i/12705508',
+            '[vid="iv_item_del" || vid="doc_detail_titleimg_ad_delete" || vid="del_click"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/0a199f54-70f9-45ae-8025-896b688dc8e6',
+          snapshotUrls: [
+            'https://i.gkd.li/i/19752570',
+            'https://i.gkd.li/i/19752567',
+            'https://i.gkd.li/i/19752561',
+          ],
         },
         {
-          key: 3,
-          matches:
-            '[id="com.ifeng.news2:id/adv_icon"] + LinearLayout[id="com.ifeng.news2:id/normal_item_delete_wrap"]',
-          snapshotUrls: 'https://i.gkd.li/i/12705511',
-        },
-        {
-          key: 4,
-          matches:
-            '[id="com.ifeng.news2:id/adv_icon"] + TextView + [id="com.ifeng.news2:id/normal_item_delete_wrap"]',
-          snapshotUrls: 'https://i.gkd.li/i/12705518',
-        },
-        {
-          key: 5,
-          matches:
-            '[id="com.ifeng.news2:id/adv_label"] + [id="com.ifeng.news2:id/iv_item_del"]',
-          snapshotUrls: 'https://i.gkd.li/i/12705520',
-        },
-        {
-          preKeys: [1, 2, 3, 4, 5],
-          key: 9,
-          matches:
-            '@[id="com.ifeng.news2:id/dialog_dislike_feedback_normal_item_linearlayout"] > [text="不感兴趣"]',
-          snapshotUrls: 'https://i.gkd.li/i/12705505',
+          preKeys: [1, 2],
+          matches: '@[clickable=true] > [text="不感兴趣"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/e182591a-492d-49ec-8820-20bea10495d9',
+          snapshotUrls: [
+            'https://i.gkd.li/i/19752582',
+            'https://i.gkd.li/i/19752559',
+          ],
         },
       ],
     },
