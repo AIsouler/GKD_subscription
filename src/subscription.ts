c9f8a110f9b568e9cbecd77ebadaf83d62bcc6ef
AIsouler <zy43220@gmail.com>
2025-04-21 20:59:25 +0800

    perf: categories enable

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,17 +1,14 @@
 import { defineGkdSubscription } from '@gkd-kit/define';
-import categories from './categories';
-import globalGroups from './globalGroups';
-import { RawApp, RawAppGroup } from '@gkd-kit/api';
 import { batchImportApps } from '@gkd-kit/tools';
-import { OPEN_AD_ORDER } from './globalGroups';
+import categories from './categories';
+import globalGroups, { OPEN_AD_ORDER } from './globalGroups';
+import { RawApp, RawAppGroup } from '@gkd-kit/api';
 
 const apps = await batchImportApps(`${import.meta.dirname}/apps`);
 const rawApps: RawApp[] = [];
 apps.forEach((appConfig) => {
   appConfig.groups?.forEach((g: RawAppGroup) => {
-    if (!g.name.startsWith('开屏广告')) {
-      g.enable = false;
-    } else {
+    if (g.name.startsWith('开屏广告')) {
       g.order = OPEN_AD_ORDER;
     }
   });
