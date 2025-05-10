788e59a42f850ef01d02dcafbe13b21a2a9691e8
zmtlwzy <49909526+zmtlwzy@users.noreply.github.com>
2025-05-10 15:42:52 +0800

    feat: 多屏互动-全屏弹窗广告-新增规则 (#899)
    Signed-off-by: zmtlwzy <49909526+zmtlwzy@users.noreply.github.com>
    Co-authored-by: AIsouler <zy43220@gmail.com>

--- src/apps/com.tnscreen.main.ts
+++ src/apps/com.tnscreen.main.ts
@@ -28,12 +28,22 @@ export default defineGkdApp({
       desc: '点击关闭',
       rules: [
         {
+          key: 0,
           activityIds:
             'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
           matches:
             '[text="反馈"] +(1,2) View[childCount=1] > Image[childCount=0][text=""][width<60 && height<60]',
           exampleUrls: 'https://e.gkd.li/be4513a1-fdf8-4889-a65d-55e2e0664afc',
           snapshotUrls: 'https://i.gkd.li/i/18371332',
+        },
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: 'com.tcl.tcast.snapshot.view.ShotPicActivity',
+          matches:
+            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null][parent.childCount>3] +n FrameLayout >(1,2) [text^="立即" || text="查看详情" || text="了解更多" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
+          exampleUrls: 'https://e.gkd.li/93834d55-142f-4371-ae5b-db65b168db7f',
+          snapshotUrls: 'https://i.gkd.li/i/20123436',
         },
       ],
     },
