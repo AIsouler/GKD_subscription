66989e87866bdd3919fc0855468ff298a1f7b59b
AIsouler <zy43220@gmail.com>
2025-05-12 13:10:56 +0800

    feat: 智慧树家长端-全屏广告 #895

--- src/apps/com.zhx.wisdomtree.ts
+++ src/apps/com.zhx.wisdomtree.ts
@@ -71,6 +71,33 @@ export default defineGkdApp({
           exampleUrls: 'https://e.gkd.li/f6c1c4ec-56d8-4c02-a66d-42a005bb196c',
           snapshotUrls: 'https://i.gkd.li/i/20095481',
         },
+        {
+          key: 2,
+          fastQuery: true,
+          activityIds: 'net.hyww.wisdomtree.parent.common.MainActivity',
+          matches:
+            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null] <2 FrameLayout[childCount=5] + FrameLayout[childCount=2] > [text^="立即" || text="查看详情" || text="了解更多" || text="去逛逛" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
+          exampleUrls: 'https://e.gkd.li/c72c44bc-a16a-4929-8aa6-457893c1254f',
+          snapshotUrls: 'https://i.gkd.li/i/20154496',
+        },
+        {
+          key: 3,
+          fastQuery: true,
+          activityIds: 'net.hyww.wisdomtree.parent.common.MainActivity',
+          matches:
+            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null][parent.childCount>3] +n FrameLayout >(1,2) [text^="立即" || text="查看详情" || text="了解更多" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
+          exampleUrls: 'https://e.gkd.li/91f3f0d3-d424-4e06-99a2-8e31dd71df41',
+          snapshotUrls: 'https://i.gkd.li/i/20154499',
+        },
+        {
+          key: 4,
+          fastQuery: true,
+          activityIds: 'net.hyww.wisdomtree.parent.common.MainActivity',
+          matches:
+            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null] - FrameLayout[childCount>2] >3 [text^="立即" || text$="应用" || text="了解更多" || text="查看详情"]',
+          exampleUrls: 'https://e.gkd.li/c24edc78-83d6-4a73-a555-fd69635a92b8',
+          snapshotUrls: 'https://i.gkd.li/i/20154500',
+        },
       ],
     },
   ],
