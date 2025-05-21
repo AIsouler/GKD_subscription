5b1cfbda0ff211d7adbe69317da713b7480744bd
AIsouler <zy43220@gmail.com>
2025-05-21 14:26:10 +0800

    fix: 百度贴吧-局部广告误触 #918

--- src/apps/com.baidu.tieba.ts
+++ src/apps/com.baidu.tieba.ts
@@ -241,8 +241,10 @@ export default defineGkdApp({
           key: 2,
           name: '评论区左侧悬浮广告',
           activityIds: '.pb.pb.main.PbActivity',
-          matches: 'LinearLayout[childCount=2] > @ImageView + [text="广告"]',
+          matches:
+            'FrameLayout[getChild(1).getChild(1).text!=null] + @FrameLayout[clickable=true] > LinearLayout[childCount=2] > ImageView + [text="广告"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/13296280',
+          excludeSnapshotUrls: 'https://i.gkd.li/i/20361318',
         },
         {
           key: 3,
