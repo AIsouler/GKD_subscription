8c324f00f2d9c58659b00522d9843bffbbc58e23
AIsouler <zy43220@gmail.com>
2025-06-05 12:35:57 +0800

    feat: 欢律-更新提示 #953

--- src/apps/com.heytap.headset.ts
+++ src/apps/com.heytap.headset.ts
@@ -14,8 +14,34 @@ export default defineGkdApp({
       rules: [
         {
           activityIds: '.component.about.FindAppUpgradeActivity',
-          matches: '[vid="tv_app_upgrade_cancel"]',
-          snapshotUrls: 'https://i.gkd.li/i/14971296',
+          matches: [
+            '[text="发现新版本"][visibleToUser=true]',
+            '[text="取消"][visibleToUser=true]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/14971296',
+            'https://i.gkd.li/i/20601909',
+          ],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '更新提示-耳机固件更新弹窗',
+      desc: '点击取消',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: '.component.detail.DeviceDetailActivity',
+          matches: [
+            '[text="耳机固件更新"][visibleToUser=true]',
+            '[text="取消"][visibleToUser=true]',
+          ],
+          exampleUrls: 'https://e.gkd.li/2336ee00-71e3-4e5e-b6e1-d81ea412b67b',
+          snapshotUrls: 'https://i.gkd.li/i/20601911',
         },
       ],
     },
