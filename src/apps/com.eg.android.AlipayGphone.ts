3e5c707a99318f8bce2c97f95de034686e9697a0
Jesse Senior <jessesenior@outlook.com>
2025-06-11 12:41:46 +0800

    Update com.eg.android.AlipayGphone.ts
    Signed-off-by: Jesse Senior <jessesenior@outlook.com>

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -250,12 +250,13 @@ export default defineGkdApp({
         {
           fastQuery: true,
           activityIds: 'com.alipay.android.msp.ui.views.MspContainerActivity',
-          matches: ['[text="同意协议并开通"]', '[text="30天不再提醒"]'],
+          anyMatches: ['[text="关闭"]', '[text="不感兴趣"]', '[text="30天不再提醒"]'],
           exampleUrls:
             'https://m.gkd.li/57941037/8daecaeb-35f0-4f3a-8d5a-a19098a992d5',
           snapshotUrls: [
             'https://i.gkd.li/i/14060628',
             'https://i.gkd.li/i/15050300',
+            'https://i.gkd.li/i/20684144',
           ],
         },
       ],
