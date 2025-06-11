b3ea810c5246c4731e05ca627ef13b5ebc168578
AIsouler <zy43220@gmail.com>
2025-06-11 19:31:05 +0800

    feat: ZAKER-开屏广告 #968

--- src/apps/com.myzaker.ZAKER_Phone.ts
+++ src/apps/com.myzaker.ZAKER_Phone.ts
@@ -11,9 +11,17 @@ export default defineGkdApp({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
+      actionMaximumKey: 0,
       priorityTime: 10000,
       rules: [
         {
+          key: 0,
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/0c69945f-ac0e-44a8-a0a3-95544e53849f',
+          snapshotUrls: 'https://i.gkd.li/i/20689993',
+        },
+        {
+          key: 1,
           position: {
             left: 'width * 0.9009',
             top: 'width * 0.075',
