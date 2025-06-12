2ec918c8b36c66f9c621df5d2fa223cb1eb7f346
AIsouler <zy43220@gmail.com>
2025-06-12 18:59:39 +0800

    perf: matches

--- src/apps/com.miHoYo.cloudgames.Nap.ts
+++ src/apps/com.miHoYo.cloudgames.Nap.ts
@@ -1,4 +1,4 @@
-import { defineGkdApp } from '@gkd-kit/define'
+import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'com.miHoYo.cloudgames.Nap',
@@ -11,20 +11,18 @@ export default defineGkdApp({
         {
           fastQuery: true,
           activityIds: 'com.mihoyo.cloudgame.main.MiHoYoCloudMainActivity',
-          anyMatches: [
-            '[text="使用流量进行游戏"]',
-            '[text="继续使用移动网络"]'
-          ],
+          matches:
+            '[text="使用流量进行游戏" || text="继续使用移动网络"][visibleToUser=true]',
           exampleUrls: [
             'https://e.gkd.li/6936d19b-bd2a-46d4-8284-93f0b2238359',
-            'https://e.gkd.li/f1d57678-ce9a-4384-b7ea-077e55b2aa49'
+            'https://e.gkd.li/f1d57678-ce9a-4384-b7ea-077e55b2aa49',
           ],
           snapshotUrls: [
             'https://i.gkd.li/i/20705751',
-            'https://i.gkd.li/i/20705824'
+            'https://i.gkd.li/i/20705824',
           ],
         },
       ],
     },
   ],
-})
+});
