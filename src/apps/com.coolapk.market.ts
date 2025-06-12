f05c48fb86280f0b893e482e3fbe50567b276055
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2025-06-12 20:27:58 +0800

    perf(酷安): 权限提示-通知权限

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -115,9 +115,11 @@ export default defineGkdApp({
       resetMatch: 'app',
       rules: [
         {
-          activityIds: '.view.main.MainActivity',
           matches: '[text="去开启"] - [text="以后再说"]',
-          snapshotUrls: 'https://i.gkd.li/i/13296465',
+          snapshotUrls: [
+            'https://i.gkd.li/i/13296465',
+            'https://i.gkd.li/i/20708984',
+          ],
         },
       ],
     },
