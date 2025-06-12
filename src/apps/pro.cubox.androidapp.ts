0bb838aa9ab488e006771dad2f8262e4ffb52da2
AIsouler <zy43220@gmail.com>
2025-06-12 22:00:03 +0800

    perf: 优化

--- src/apps/pro.cubox.androidapp.ts
+++ src/apps/pro.cubox.androidapp.ts
@@ -2,20 +2,22 @@ import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'pro.cubox.androidapp',
-  name: 'Cubox稍后读',
+  name: 'Cubox',
   groups: [
     {
       key: 1,
-      name: '加入会员弹窗',
-      desc: '点击[取消]',
-      fastQuery: true,
+      name: '全屏广告-会员广告',
+      desc: '点击关闭',
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
+          fastQuery: true,
           activityIds: '.ui.pro.ProAccountActivity',
-          matches: ['@[clickable=true][childCount=0][name="android.view.View"] - [text*="支付宝" && text*="微信"]'],
+          matches:
+            '@View[clickable=true][childCount=0][visibleToUser=true][index=parent.childCount.minus(1)] - [text*="支付"] <5 View[childCount=6] < [vid="newCV"]',
+          exampleUrls: 'https://e.gkd.li/a51b01f3-d690-4bdf-9db1-f40e05212b11',
           snapshotUrls: 'https://i.gkd.li/i/20708841',
         },
       ],
