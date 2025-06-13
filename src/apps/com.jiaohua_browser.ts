acc914e12ad2bc8e3638e56d2065056d8647c4cf
AIsouler <zy43220@gmail.com>
2025-06-13 10:59:57 +0800

    perf: 优化

--- src/apps/com.jiaohua_browser.ts
+++ src/apps/com.jiaohua_browser.ts
@@ -8,14 +8,18 @@ export default defineGkdApp({
       key: 1,
       name: '全屏广告',
       desc: '关闭选择路线后的全屏广告',
+      matchTime: 10000,
       actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
+          fastQuery: true,
           activityIds: '.MainActivity',
           matches:
-            '[childCount=2] > [name="android.view.ViewGroup"] + [name="android.view.ViewGroup"] > [name="android.widget.TextView"][text=""][clickable=false][visibleToUser=true]',
+            'ViewGroup[childCount=2] > @ViewGroup[clickable=true][visibleToUser=true] > [text=""]',
           exampleUrls: 'https://e.gkd.li/447bea41-2771-4a57-8436-82da37d0b3b2',
           snapshotUrls: 'https://i.gkd.li/i/20709992',
+          excludeSnapshotUrls: 'https://i.gkd.li/i/20718812',
         },
       ],
     },
@@ -25,12 +29,13 @@ export default defineGkdApp({
       desc: '关闭底部横幅广告',
       rules: [
         {
+          fastQuery: true,
           activityIds: '.MainActivity',
-          action: 'clickCenter',
           matches:
-            '[text="AD"] <<7 [name="android.view.ViewGroup"] + [name="android.view.ViewGroup"] > [name="android.widget.TextView"][text=""][clickable=false][visibleToUser=true]',
+            'ViewGroup[getChild(0).getChild(0).getChild(0).getChild(0).getChild(1).text="AD"] + @ViewGroup[clickable=false] > [text=""][visibleToUser=true]',
           exampleUrls: 'https://e.gkd.li/002589d0-660e-4658-8e20-42c1ac8710fc',
           snapshotUrls: 'https://i.gkd.li/i/20713583',
+          excludeSnapshotUrls: 'https://i.gkd.li/i/20709992',
         },
       ],
     },
@@ -38,15 +43,17 @@ export default defineGkdApp({
       key: 3,
       name: '功能类-路线选择',
       desc: '选择路线，优先顺畅>普通>缓慢',
+      matchTime: 10000,
       actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           fastQuery: true,
           activityIds: '.MainActivity',
           anyMatches: [
-            '[text="请选择线路"] +(2,3,4,5,6) [name="android.view.ViewGroup"] > [text="顺畅"]',
-            '[text="请选择线路"] +(2,3,4,5,6) [name="android.view.ViewGroup"] > [text="普通"]',
-            '[text="请选择线路"] +(2,3,4,5,6) [name="android.view.ViewGroup"] > [text="缓慢"]',
+            '@ViewGroup[clickable=true][visibleToUser=true] > [text="顺畅" || text="順暢"]',
+            '@ViewGroup[clickable=true][visibleToUser=true] > [text="普通"]',
+            '@ViewGroup[clickable=true][visibleToUser=true] > [text="缓慢"]',
           ],
           exampleUrls: [
             'https://e.gkd.li/68004e70-6b78-41f2-b329-ff600038db94',
@@ -57,6 +64,7 @@ export default defineGkdApp({
             'https://i.gkd.li/i/20711163',
             'https://i.gkd.li/i/20713674',
             'https://i.gkd.li/i/20713715',
+            'https://i.gkd.li/i/20718557',
           ],
         },
       ],
@@ -64,16 +72,21 @@ export default defineGkdApp({
     {
       key: 4,
       name: '功能类-点击[我保证我已满18岁]',
-      actionMaximum: 6,
-      actionDelay: 500,
-      actionCd: 500,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           fastQuery: true,
           activityIds: '.MainActivity',
-          matches: '[text="我保证我已满18岁！确定进入！"]',
+          matches:
+            '@ViewGroup[childCount=1][clickable=true][visibleToUser=true] > [text^="我保证我已满18岁" || text^="我保證我已滿18歲"]',
           exampleUrls: 'https://e.gkd.li/f890ac7e-8626-4fc3-85c1-83d6776cb3f4',
-          snapshotUrls: 'https://i.gkd.li/i/20711995',
+          snapshotUrls: [
+            'https://i.gkd.li/i/20711995',
+            'https://i.gkd.li/i/20718577',
+          ],
+          excludeSnapshotUrls: 'https://i.gkd.li/i/20713533',
         },
       ],
     },
