1e6dcec44f85136d1a8c2c5085f95d04f81b921a
AIsouler <zy43220@gmail.com>
2025-06-16 09:50:30 +0800

    feat: name

--- src/apps/com.miui.securitycenter.ts
+++ src/apps/com.miui.securitycenter.ts
@@ -2,7 +2,7 @@ import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'com.miui.securitycenter',
-  name: '小米手机管家',
+  name: '安全服务/小米手机管家',
   groups: [
     {
       key: 10,
