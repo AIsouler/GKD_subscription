7857a26c5d7af1adb21c16044913783c288da743
AIsouler <zy43220@gmail.com>
2025-06-22 19:18:38 +0800

    feat: PICO-更新提示 #1000

---
+++ src/apps/com.picovr.assistantphone.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.picovr.assistantphone',
+  name: 'PICO',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.ui.MainActivity',
+          matches: '[vid="update_check_cancel_btn"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/b63e9ce4-e333-4702-abd5-d2355f83da1f',
+          snapshotUrls: 'https://i.gkd.li/i/20852432',
+        },
+      ],
+    },
+  ],
+});
