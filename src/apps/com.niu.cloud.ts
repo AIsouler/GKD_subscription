5739abb1f55cea7fa2cc5688716b5e3f5993295c
AIsouler <zy43220@gmail.com>
2025-06-26 14:42:03 +0800

    feat: 小牛电动-更新提示-固件更新弹窗 #1014

---
+++ src/apps/com.niu.cloud.ts
@@ -0,0 +1,28 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.niu.cloud',
+  name: '小牛电动',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示-固件更新弹窗',
+      desc: '点击[以后再说]',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.main.MainActivityNew',
+          matches: [
+            '[text="发现新固件"][visibleToUser=true]',
+            '[text="以后再说"][visibleToUser=true]',
+          ],
+          exampleUrls: 'https://e.gkd.li/b486cc0b-3ac2-4be3-a38d-c16c952f4fce',
+          snapshotUrls: 'https://i.gkd.li/i/20919078',
+        },
+      ],
+    },
+  ],
+});
