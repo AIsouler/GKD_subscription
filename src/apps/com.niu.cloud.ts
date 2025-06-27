d54172540218badd4fd833d654017847162545db
AIsouler <zy43220@gmail.com>
2025-06-27 13:26:21 +0800

    feat: 小牛电动-全屏广告 #1018

--- src/apps/com.niu.cloud.ts
+++ src/apps/com.niu.cloud.ts
@@ -24,5 +24,22 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.main.MainActivityNew',
+          matches: '[vid="closeDialogIv"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/78f2afd2-de8b-46b5-ae6c-6df58862f00e',
+          snapshotUrls: 'https://i.gkd.li/i/20939822',
+        },
+      ],
+    },
   ],
 });
