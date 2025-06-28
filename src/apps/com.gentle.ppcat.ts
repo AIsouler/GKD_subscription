75b1ee3b834861532750ebf6b532ac7c8d4dbe3b
AIsouler <zy43220@gmail.com>
2025-06-28 10:23:58 +0800

    feat: 皮皮喵-全屏广告

--- src/apps/com.gentle.ppcat.ts
+++ src/apps/com.gentle.ppcat.ts
@@ -39,5 +39,30 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 1,
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches:
+            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null][parent.childCount>3] +n FrameLayout >(1,2) [text^="立即" || text="查看详情" || text="了解更多" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
+          exampleUrls: 'https://e.gkd.li/68c0ad3a-0f25-43bb-a6d4-ab45d49e8db7',
+          snapshotUrls: 'https://i.gkd.li/i/20950433',
+        },
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches:
+            '@ImageView[childCount=0][visibleToUser=true][width<100 && height<100] < FrameLayout[childCount=1] <2 FrameLayout[childCount=3] < FrameLayout < FrameLayout < FrameLayout < FrameLayout < [id="android:id/content"]',
+          exampleUrls: 'https://e.gkd.li/7002f432-f9cb-45c1-a120-5458b94782b3',
+          snapshotUrls: 'https://i.gkd.li/i/20950403',
+        },
+      ],
+    },
   ],
 });
