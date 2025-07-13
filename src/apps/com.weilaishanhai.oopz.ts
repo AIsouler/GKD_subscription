9b1e7043364cdb71a1ab4480b65c086e8b48165d
ZX夏夜之风 <snwcreations@qq.com>
2025-07-13 19:42:15 +0800

    perf: Oopz-开屏广告 (#1044)
    Co-authored-by: AIsouler <zy43220@gmail.com>

--- src/apps/com.weilaishanhai.oopz.ts
+++ src/apps/com.weilaishanhai.oopz.ts
@@ -13,6 +13,7 @@ export default defineGkdApp({
       priorityTime: 10000,
       rules: [
         {
+          key: 0,
           fastQuery: true,
           matches:
             '[name!$="RelativeLayout"] > [text*="跳过"][index=0][text.length<10][visibleToUser=true]',
@@ -23,6 +24,14 @@ export default defineGkdApp({
             'https://i.gkd.li/i/21202366',
           ],
           excludeSnapshotUrls: 'https://i.gkd.li/i/21202513', // 虚假跳过按钮 https://github.com/AIsouler/GKD_subscription/issues/1043
+        },
+        {
+          preKeys: [0], // 处理开屏广告的第二段
+          fastQuery: true,
+          matches:
+            '@ImageView[id="com.wangmai.allmodules:id/wm_image_clear"] <2 RelativeLayout[childCount=2] < FrameLayout < [vid="wm_reward_main"]',
+          exampleUrls: 'https://e.gkd.li/c1299341-66bf-4702-885c-3fbe5d648675',
+          snapshotUrls: 'https://i.gkd.li/i/21202940',
         },
       ],
     },
