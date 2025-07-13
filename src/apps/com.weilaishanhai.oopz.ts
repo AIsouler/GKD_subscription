4e3c8a822ffd6de3c946e4059c96cb6d3a13e4a8
AIsouler <zy43220@gmail.com>
2025-07-13 19:41:20 +0800

    perf: 优化

--- src/apps/com.weilaishanhai.oopz.ts
+++ src/apps/com.weilaishanhai.oopz.ts
@@ -13,6 +13,7 @@ export default defineGkdApp({
       priorityTime: 10000,
       rules: [
         {
+          key: 0,
           fastQuery: true,
           matches:
             '[name!$="RelativeLayout"] > [text*="跳过"][index=0][text.length<10][visibleToUser=true]',
@@ -24,20 +25,12 @@ export default defineGkdApp({
           ],
           excludeSnapshotUrls: 'https://i.gkd.li/i/21202513', // 虚假跳过按钮 https://github.com/AIsouler/GKD_subscription/issues/1043
         },
-      ],
-    },
-    {
-      key: 2,
-      name: '分段广告',
-      desc: '有的广告即使点击跳过后仍然会强制进入，此规则用于处理这种情况',
-      activityIds: 'com.wangmai.appsdkdex.WMPortraitActivity',
-      exampleUrls: 'https://e.gkd.li/c1299341-66bf-4702-885c-3fbe5d648675',
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      rules: [
         {
-          matches: '[id="com.wangmai.allmodules:id/wm_image_clear"]',
+          preKeys: [0], // 处理开屏广告的第二段
+          fastQuery: true,
+          matches:
+            '@ImageView[id="com.wangmai.allmodules:id/wm_image_clear"] <2 RelativeLayout[childCount=2] < FrameLayout < [vid="wm_reward_main"]',
+          exampleUrls: 'https://e.gkd.li/c1299341-66bf-4702-885c-3fbe5d648675',
           snapshotUrls: 'https://i.gkd.li/i/21202940',
         },
       ],
