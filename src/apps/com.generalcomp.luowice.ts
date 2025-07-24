360dcb05396c02fd4db46a33090c5f9d8974b929
chenxj2 <chenxj2@grand-trade.com>
2025-07-24 16:29:53 +0800

    feat: 添加镭威视云APP

---
+++ src/apps/com.generalcomp.luowice.ts
@@ -0,0 +1,19 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.generalcomp.luowice',
+  name: '镭威视云',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      activityIds: 'com.generalcomp.luowice.activity.SplashActivity',
+      rules: [
+        {
+          matches: '[text$="跳过"]',
+          snapshotUrls: 'https://i.gkd.li/i/21400248',
+        },
+      ],
+    },
+  ],
+});
