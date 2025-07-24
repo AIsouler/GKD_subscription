e263e3220dd8420d04e54d42f06d34a51d7d5e5e
chenxj2 <chenxj2@grand-trade.com>
2025-07-24 16:57:00 +0800

    feat: 镭威视云-开屏广告

--- src/apps/com.generalcomp.luowice.ts
+++ src/apps/com.generalcomp.luowice.ts
@@ -7,11 +7,18 @@ export default defineGkdApp({
     {
       key: 1,
       name: '开屏广告',
-      activityIds: 'com.generalcomp.luowice.activity.SplashActivity',
+      activityIds: [
+        'com.generalcomp.luowice.activity.SplashActivity',
+        'com.juanvision.modulelogin.activity.SplashActivity',
+      ],
       rules: [
         {
           matches: '[text$="跳过"]',
           snapshotUrls: 'https://i.gkd.li/i/21400248',
+        },
+        {
+          matches: '[text^="跳过"]',
+          snapshotUrls: 'https://i.gkd.li/i/21404199',
         },
       ],
     },
