8b95da4e940205955381390b05a80099f9db6ccd
AIsouler <zy43220@gmail.com>
2025-08-10 20:45:38 +0800

    feat: 百词斩-开屏广告 #1095

--- src/apps/com.jiongji.andriod.card.ts
+++ src/apps/com.jiongji.andriod.card.ts
@@ -5,6 +5,25 @@ export default defineGkdApp({
   name: '百词斩',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      priorityTime: 10000,
+      rules: [
+        {
+          fastQuery: true,
+          excludeActivityIds:
+            'com.baicizhan.client.business.webview.ui.BczWebActivity',
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/8612bbae-7a50-4730-8071-1c47e35c27ca',
+          snapshotUrls: 'https://i.gkd.li/i/21705324',
+          excludeSnapshotUrls: 'https://i.gkd.li/i/21705338',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '全屏广告-活动弹窗',
       fastQuery: true,
