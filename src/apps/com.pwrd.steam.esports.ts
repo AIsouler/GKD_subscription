4329f1fbc63f3ef96ac3f0422dcf4ad53a30bf2b
AIsouler <zy43220@gmail.com>
2025-08-16 20:17:18 +0800

    feat: 完美世界电竞-开屏广告 #1104

--- src/apps/com.pwrd.steam.esports.ts
+++ src/apps/com.pwrd.steam.esports.ts
@@ -5,6 +5,22 @@ export default defineGkdApp({
   name: '完美世界电竞',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      priorityTime: 10000,
+      rules: [
+        {
+          fastQuery: true,
+          matches: '[vid="rl_skip"][visibleToUser=true]', // 需点击 clickable=true 的节点才有效 https://github.com/AIsouler/GKD_subscription/issues/1104
+          exampleUrls: 'https://e.gkd.li/b7adf229-631c-4c27-a07b-eb6e0e1854af',
+          snapshotUrls: 'https://i.gkd.li/i/21808124',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '权限提示-通知权限',
       desc: '点击关闭',
