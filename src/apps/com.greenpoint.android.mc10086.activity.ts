6256f4fb9f19d98e6b8f8e23284a9f7e870c172d
AIsouler <zy43220@gmail.com>
2025-08-17 17:47:25 +0800

    fix: 中国移动-开屏广告 #1109

--- src/apps/com.greenpoint.android.mc10086.activity.ts
+++ src/apps/com.greenpoint.android.mc10086.activity.ts
@@ -16,6 +16,7 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
+          action: 'clickCenter', // https://github.com/AIsouler/GKD_subscription/issues/1109
           matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/16335136',
         },
