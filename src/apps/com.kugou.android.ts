ca5d427b2a9faf8ef4d9714ac963dfa08992499c
AIsouler <zy43220@gmail.com>
2025-09-13 19:45:07 +0800

    perf: 酷狗音乐-会员广告 #1155

--- src/apps/com.kugou.android.ts
+++ src/apps/com.kugou.android.ts
@@ -121,9 +121,12 @@ export default defineGkdApp({
           key: 2,
           activityIds: '.app.MediaActivity',
           matches:
-            '@[text="退出页面"][visibleToUser=true] + View > [text="你的会员已过期"]',
+            '@[clickable=true][childCount=0] < View <(1,2) View <(1,2) View < WebView < WebView < FrameLayout < FrameLayout < RelativeLayout < RelativeLayout < FrameLayout[id!="android:id/content"] < FrameLayout < [parent=null]',
           exampleUrls: 'https://e.gkd.li/f32ed6b7-e396-45a5-9849-f8a8ae99cb99',
-          snapshotUrls: 'https://i.gkd.li/i/18070974',
+          snapshotUrls: [
+            'https://i.gkd.li/i/18070974',
+            'https://i.gkd.li/i/22310624',
+          ],
         },
       ],
     },
