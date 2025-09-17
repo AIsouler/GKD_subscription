ba5309c9468c8f07b2387a26d6dd4da77b0a4656
ZYX <74281356+zhyx490991014@users.noreply.github.com>
2025-09-17 14:55:13 +0800

    Update com.oplus.appdetail.ts
    Signed-off-by: ZYX <74281356+zhyx490991014@users.noreply.github.com>

--- src/apps/com.oplus.appdetail.ts
+++ src/apps/com.oplus.appdetail.ts
@@ -33,6 +33,33 @@ export default defineGkdApp({
             'https://i.gkd.li/i/13054849',
           ],
         },
+        {
+          key: 2,
+          name: '点击[查看详情]',
+          activityIds: 'com.oplus.appdetail.model.guide.ui.InstallGuideActivity',
+          matches: '[id="com.oplus.appdetail:id/tv_result_desc"]',
+          position: {
+            right: 'width*0.27',
+            top: 'height/2',
+          },
+          snapshotUrls: [
+            'https://i.gkd.li/i/22377287',
+          ],
+        },
+        {
+          key: 3,
+          name: '勾选[已悉知应用风险]',
+          activityIds: 'com.oplus.appdetail.model.guide.ui.InstallGuideActivity',
+          matches: '[id="com.oplus.appdetail:id/coui_security_alert_dialog_checkbox"]',
+          prekey: 2,
+          position: {
+            left: 'width*0.1',
+            top: 'height/2',
+          },
+          snapshotUrls: [
+            'https://i.gkd.li/i/22377327',
+          ],
+        },
       ],
     },
   ],
