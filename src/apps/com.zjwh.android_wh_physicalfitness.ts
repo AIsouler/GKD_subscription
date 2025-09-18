c23ded5d56b84a2a990149c74eab2a8366717ead
天方夜谭 <tianfangyetan01@qq.com>
2025-09-18 09:31:03 +0800

    perf: 去除首页弹窗广告匹配时间和次数
    此类广告可能会在使用过程中多次出现

--- src/apps/com.zjwh.android_wh_physicalfitness.ts
+++ src/apps/com.zjwh.android_wh_physicalfitness.ts
@@ -14,6 +14,69 @@ export default defineGkdApp({
         {
           key: 0,
           fastQuery: true,
+          activityIds: '.activity.HomeActivity',
+          matches:
+            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null] <2 FrameLayout[childCount=5] + FrameLayout[childCount=2] > [text^="立即" || text="查看详情" || text="了解更多" || text="去逛逛" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
+          exampleUrls: 'https://e.gkd.li/a7753ec9-5382-42ee-9a41-b4e2b61b9050',
+          snapshotUrls: 'https://i.gkd.li/i/17355585',
+        },
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: ['.activity.HomeActivity', '.ui.AdActivity'],
+          matches:
+            'ImageView[childCount=0] < @ViewGroup[clickable=true][childCount=1] < ViewGroup +(3,5) ViewGroup[childCount=2] > [text="广告"]',
+          exampleUrls: 'https://e.gkd.li/cb21dc41-bcca-47ec-ae0a-df9aedde48b5',
+          snapshotUrls: [
+            'https://i.gkd.li/i/12826124',
+            'https://i.gkd.li/i/13228216',
+            'https://i.gkd.li/i/13601132',
+          ],
+        },
+        {
+          key: 2,
+          fastQuery: true,
+          activityIds: 'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
+          matches:
+            '@ImageView[clickable=true] - RelativeLayout > [text="反馈"]',
+          exampleUrls: 'https://e.gkd.li/6882bcf4-1aa1-47e1-b7ad-299d4aa7bfd9',
+          snapshotUrls: 'https://i.gkd.li/i/13554229',
+        },
+        {
+          key: 3,
+          activityIds:
+            'com.zjwh.android_wh_physicalfitness.activity.SplashActivity',
+          matches:
+            '@[text="跳过"][clickable=true] +3 RelativeLayout >2 [text="点击跳转至第三方页面"]',
+          snapshotUrls: 'https://i.gkd.li/i/12673349',
+        },
+        {
+          key: 4,
+          fastQuery: true,
+          activityIds: '.activity.HomeActivity',
+          matches:
+            '@ImageView[childCount=0] < FrameLayout[childCount=1] <2 FrameLayout[childCount=2] <2 FrameLayout[childCount=2] <2 FrameLayout[childCount=2] - FrameLayout > [text^="扭动或点击"]',
+          exampleUrls: 'https://e.gkd.li/2debed48-37f2-43ec-8b0f-3a5e06c23ed1',
+          snapshotUrls: 'https://i.gkd.li/i/17378546',
+        },
+        {
+          key: 5,
+          fastQuery: true,
+          activityIds: '.activity.HomeActivity',
+          matches:
+            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null][parent.childCount>3] +n FrameLayout >(1,2) [text^="立即" || text="查看详情" || text="了解更多" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
+          exampleUrls: 'https://e.gkd.li/f640d0c2-197d-45ef-98ff-58a04920bd2d',
+          snapshotUrls: 'https://i.gkd.li/i/17358027',
+        },
+      ],
+    },{
+      key: 2,
+      name: '首页-弹窗广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
           activityIds: [
             '.activity.HomeActivity',
             '.ui.AdActivity',
@@ -27,64 +90,7 @@ export default defineGkdApp({
             'https://i.gkd.li/i/13166472',
           ],
         },
-        {
-          key: 1,
-          fastQuery: true,
-          activityIds: '.activity.HomeActivity',
-          matches:
-            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null] <2 FrameLayout[childCount=5] + FrameLayout[childCount=2] > [text^="立即" || text="查看详情" || text="了解更多" || text="去逛逛" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
-          exampleUrls: 'https://e.gkd.li/a7753ec9-5382-42ee-9a41-b4e2b61b9050',
-          snapshotUrls: 'https://i.gkd.li/i/17355585',
-        },
-        {
-          key: 2,
-          fastQuery: true,
-          activityIds: ['.activity.HomeActivity', '.ui.AdActivity'],
-          matches:
-            'ImageView[childCount=0] < @ViewGroup[clickable=true][childCount=1] < ViewGroup +(3,5) ViewGroup[childCount=2] > [text="广告"]',
-          exampleUrls: 'https://e.gkd.li/cb21dc41-bcca-47ec-ae0a-df9aedde48b5',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12826124',
-            'https://i.gkd.li/i/13228216',
-            'https://i.gkd.li/i/13601132',
-          ],
-        },
-        {
-          key: 3,
-          fastQuery: true,
-          activityIds: 'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
-          matches:
-            '@ImageView[clickable=true] - RelativeLayout > [text="反馈"]',
-          exampleUrls: 'https://e.gkd.li/6882bcf4-1aa1-47e1-b7ad-299d4aa7bfd9',
-          snapshotUrls: 'https://i.gkd.li/i/13554229',
-        },
-        {
-          key: 4,
-          activityIds:
-            'com.zjwh.android_wh_physicalfitness.activity.SplashActivity',
-          matches:
-            '@[text="跳过"][clickable=true] +3 RelativeLayout >2 [text="点击跳转至第三方页面"]',
-          snapshotUrls: 'https://i.gkd.li/i/12673349',
-        },
-        {
-          key: 5,
-          fastQuery: true,
-          activityIds: '.activity.HomeActivity',
-          matches:
-            '@ImageView[childCount=0] < FrameLayout[childCount=1] <2 FrameLayout[childCount=2] <2 FrameLayout[childCount=2] <2 FrameLayout[childCount=2] - FrameLayout > [text^="扭动或点击"]',
-          exampleUrls: 'https://e.gkd.li/2debed48-37f2-43ec-8b0f-3a5e06c23ed1',
-          snapshotUrls: 'https://i.gkd.li/i/17378546',
-        },
-        {
-          key: 6,
-          fastQuery: true,
-          activityIds: '.activity.HomeActivity',
-          matches:
-            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null][parent.childCount>3] +n FrameLayout >(1,2) [text^="立即" || text="查看详情" || text="了解更多" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
-          exampleUrls: 'https://e.gkd.li/f640d0c2-197d-45ef-98ff-58a04920bd2d',
-          snapshotUrls: 'https://i.gkd.li/i/17358027',
-        },
-      ],
+      ]
     },
   ],
 });
