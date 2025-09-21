f1394ee1617fe02027c750815ead260e1e8fbb84
天方夜谭 <tianfangyetan01@qq.com>
2025-09-21 22:35:24 +0800

    feat: 易校园-功能类-打开应用自动扫一扫

---
+++ src/apps/cn.com.yunma.school.app.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'cn.com.yunma.school.app',
+  name: '易校园',
+  groups: [
+    {
+      key: 9,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      name: '功能类-打开应用自动扫一扫',
+      desc: '仅触发一次',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.yunma.app.ui.main.MainActivity',
+          matches: '@[vid="fl_scan" && clickable=true] > [vid="iv_scan"]',
+          snapshotUrls: 'https://i.gkd.li/i/22451914',
+        },
+      ],
+    },
+  ],
+});
