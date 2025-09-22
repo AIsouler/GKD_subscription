2d83acfecd2671692125578a2603dc6de70e35fc
天方夜谭 <152615144+tianfangyetan1@users.noreply.github.com>
2025-09-22 19:58:01 +0800

    feat: 易校园-功能类-打开应用自动扫一扫 (#1179)
    Co-authored-by: AIsouler <zy43220@gmail.com>

---
+++ src/apps/cn.com.yunma.school.app.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'cn.com.yunma.school.app',
+  name: '易校园',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-打开应用后自动点击右上角扫一扫',
+      desc: '仅开屏后触发一次',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.yunma.app.ui.main.MainActivity',
+          matches: '[vid="fl_scan"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/22451914',
+        },
+      ],
+    },
+  ],
+});
