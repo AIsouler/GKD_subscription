be8c505ea42300374ac35b12044d5dbd7e322445
AIsouler <zy43220@gmail.com>
2025-10-02 19:50:51 +0800

    feat: 小蚁摄像机 #1202

--- src/apps/com.ants360.yicamera.ts
+++ src/apps/com.ants360.yicamera.ts
@@ -24,19 +24,29 @@ export default defineGkdApp({
         },
         {
           key: 2,
-          activityIds: '.activity.MainActivity',
+          activityIds: [
+            '.activity.MainActivity',
+            'com.xiaoyi.yiplayer.ui.PlayerActivity',
+          ],
           matches: '@[vid="ivClose"] - [text="专属福利"][visibleToUser=true]',
           exampleUrls: 'https://e.gkd.li/a7ba5126-27dd-450d-abf9-c3a8b6938727',
-          snapshotUrls: 'https://i.gkd.li/i/18410909',
+          snapshotUrls: [
+            'https://i.gkd.li/i/18410909',
+            'https://i.gkd.li/i/22652775',
+          ],
         },
         {
           key: 3,
-          activityIds: 'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
+          activityIds: [
+            'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
+            'com.yd.saas.base.inner.interstitial.YdInterstitialActivity',
+          ],
           matches: '[vid="iv_close"][visibleToUser=true]',
           exampleUrls: 'https://e.gkd.li/1af231f8-bb98-450e-83d1-5bd067802de3',
           snapshotUrls: [
             'https://i.gkd.li/i/19514818',
             'https://i.gkd.li/i/19722625',
+            'https://i.gkd.li/i/22652776',
           ],
         },
         {
@@ -79,12 +89,31 @@ export default defineGkdApp({
       desc: '点击关闭',
       rules: [
         {
+          key: 0,
           fastQuery: true,
           activityIds: '.activity.MainActivity',
           matches:
             '@ImageView[childCount=0][visibleToUser=true] < FrameLayout[childCount=1] - LinearLayout[childCount=2] > [text="下载应用" || text="立即下载" || text="查看详情" || text="领取优惠" || text="进入小程序" || text="了解更多"][visibleToUser=true]',
           exampleUrls: 'https://e.gkd.li/c9e67d26-d65f-4e24-9f62-cc29f29cff9b',
           snapshotUrls: 'https://i.gkd.li/i/18406158',
+        },
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: '.activity.MainActivity',
+          matches:
+            '@[clickable=true][id="com.kwad.dy.sdk:id/ksad_ad_dislike"] <<n [vid="ad_container"]',
+          exampleUrls: 'https://e.gkd.li/2d358b3b-6a8e-4ebf-813b-03d01d18d5f1',
+          snapshotUrls: 'https://i.gkd.li/i/22652771',
+        },
+        {
+          key: 2,
+          fastQuery: true,
+          activityIds: 'com.xiaoyi.yiplayer.ui.PlayerActivity',
+          matches:
+            'FrameLayout > FrameLayout > FrameLayout[childCount=1] > @ImageView[childCount=0][width<50 && height<50] <<n [vid="adContainer"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/2c38cc0d-06e6-4075-86f8-cbb9c2453840',
+          snapshotUrls: 'https://i.gkd.li/i/22652772',
         },
       ],
     },
