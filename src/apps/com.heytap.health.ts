30cc8d7ea4a30e396f3e9165ae2f59cd1d9ec421
天方夜谭 <tianfangyetan01@qq.com>
2025-10-06 22:36:30 +0800

    feat: OPPO健康-健康周报

--- src/apps/com.heytap.health.ts
+++ src/apps/com.heytap.health.ts
@@ -35,5 +35,19 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 3,
+      name: '通知提示-健康周报',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.main.MainActivity',
+          matches: ['[text="您的健康周报已生成"]', '[text="关闭"]'],
+          snapshotUrls: 'https://i.gkd.li/i/22724032',
+        },
+      ],
+    },
   ],
 });
