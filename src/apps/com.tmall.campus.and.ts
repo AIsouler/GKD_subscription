7c7843238fdabbd7b1bd09ddedf53282cc9c3073
天方夜谭 <152615144+tianfangyetan1@users.noreply.github.com>
2025-10-12 20:01:30 +0800

    feat: 天猫校园-卡片广告 等2个更新 (#1249)

--- src/apps/com.tmall.campus.and.ts
+++ src/apps/com.tmall.campus.and.ts
@@ -29,10 +29,30 @@ export default defineGkdApp({
       rules: [
         {
           fastQuery: true,
-          activityIds:
+          activityIds: [
             'com.tmall.campus.scancode.activity.ToolsCaptureActivity',
+            'com.tmall.campus.bizwebview.ui.CampusWebActivity',
+          ],
           matches: '[vid="iv_close_ad"]',
-          snapshotUrls: 'https://i.gkd.li/i/22558370',
+          snapshotUrls: [
+            'https://i.gkd.li/i/22558370',
+            'https://i.gkd.li/i/22871032',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '功能类-打开应用自动点击扫一扫',
+      desc: '仅开屏后触发一次',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.tmall.campus.and.main.MainActivity',
+          matches: '[vid="iv_top_scan"]',
+          snapshotUrls: 'https://i.gkd.li/i/22871147',
         },
       ],
     },
