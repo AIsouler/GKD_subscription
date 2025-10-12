2bbb90d026e0e9c2291bd5957a680f4d375970c2
AIsouler <zy43220@gmail.com>
2025-10-12 19:13:25 +0800

    feat: 腾讯视频-局部广告 #1248

--- src/apps/com.tencent.qqlive.ts
+++ src/apps/com.tencent.qqlive.ts
@@ -308,6 +308,14 @@ export default defineGkdApp({
             'https://i.gkd.li/i/21327634',
           ],
         },
+        {
+          key: 2,
+          activityIds: '.ona.activity.VideoDetailActivity',
+          matches:
+            'RecyclerView > RelativeLayout > ComposeView > View > View[clickable=true][childCount=0][visibleToUser=true][index=parent.childCount.minus(1)]',
+          exampleUrls: 'https://e.gkd.li/23606b5d-a8e9-4848-9b5a-059e7ecb08af',
+          snapshotUrls: 'https://i.gkd.li/i/22870972',
+        },
       ],
     },
     {
