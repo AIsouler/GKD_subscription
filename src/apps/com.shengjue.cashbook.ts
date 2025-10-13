3d67e9a018fad538fa16a366d82071b429e0fd9f
AIsouler <zy43220@gmail.com>
2025-10-13 19:06:47 +0800

    feat: 章鱼记账-全屏广告

---
+++ src/apps/com.shengjue.cashbook.ts
@@ -0,0 +1,23 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.shengjue.cashbook',
+  name: '章鱼记账',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.hjq.demo.ui.activity.SplashActivity',
+          matches:
+            'ImageView[childCount=0] < @ViewGroup[clickable=true][childCount=1][visibleToUser=true] <n ViewGroup < ViewGroup - ViewGroup >2 [text="广告"]',
+          exampleUrls: 'https://e.gkd.li/2828ac84-f5e0-44c0-aab8-a12bff976fca',
+          snapshotUrls: 'https://i.gkd.li/i/22882021',
+        },
+      ],
+    },
+  ],
+});
