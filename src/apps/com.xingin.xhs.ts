e470bc7d5fda30bd84bfc06bb2b73d4d768ddd6a
AIsouler <zy43220@gmail.com>
2025-10-16 22:38:47 +0800

    perf: 优化

--- src/apps/com.xingin.xhs.ts
+++ src/apps/com.xingin.xhs.ts
@@ -24,7 +24,7 @@ export default defineGkdApp({
           excludeMatches:
             '[text="首页" || text="选择兴趣推荐更精准"][visibleToUser=true]',
           matches:
-            '[text*="跳过" || text="Skip Ad 1"][text.length<10][vid!="tv_user_name"][visibleToUser=true]',
+            '[text*="跳过" || text^="Skip Ad"][text.length<10][vid!="tv_user_name"][visibleToUser=true]',
           exampleUrls: 'https://e.gkd.li/9733ada7-4961-4a9f-b48d-3398ecd05508',
           snapshotUrls: [
             'https://i.gkd.li/i/17452158',
