f34197f97957ed86d2f3b85418a5520483f74f2e
AIsouler <zy43220@gmail.com>
2025-10-17 18:41:17 +0800

    perf: 软件包安装程序-自动安装应用 #1276

--- src/apps/com.android.packageinstaller.ts
+++ src/apps/com.android.packageinstaller.ts
@@ -8,14 +8,20 @@ export default defineGkdApp({
       key: 1,
       name: '功能类-自动安装应用',
       fastQuery: true,
+      forcedTime: 10000,
+      activityIds: [
+        '.PackageInstallerActivity',
+        '.FlymePackageInstallerActivity',
+        '.PackageInterceptActivity',
+        '.NewInstallInstalling',
+        '.InstallSuccess',
+        '.oplus.InstallAppProgress',
+        '.DeleteStagedFileOnResult',
+      ],
       rules: [
         {
           key: 0,
           name: '点击[继续安装]',
-          activityIds: [
-            'com.android.packageinstaller.PackageInstallerActivity',
-            'com.android.packageinstaller.FlymePackageInstallerActivity',
-          ],
           matches: '[text="继续安装"]',
           snapshotUrls: [
             'https://i.gkd.li/i/13206444',
@@ -26,12 +32,6 @@ export default defineGkdApp({
         {
           key: 1,
           name: '点击[完成]',
-          activityIds: [
-            'com.android.packageinstaller.NewInstallInstalling',
-            'com.android.packageinstaller.InstallSuccess',
-            'com.android.packageinstaller.oplus.InstallAppProgress',
-            'com.android.packageinstaller.FlymePackageInstallerActivity',
-          ],
           matches: '[text="完成"]',
           snapshotUrls: [
             'https://i.gkd.li/i/13206476',
@@ -45,19 +45,13 @@ export default defineGkdApp({
         {
           key: 2,
           name: '点击[安装]',
-          fastQuery: true,
-          activityIds: 'com.android.packageinstaller.DeleteStagedFileOnResult',
+          activityIds: '.DeleteStagedFileOnResult',
           matches: '[vid="confirm_bottom_button_layout"]',
           snapshotUrls: 'https://i.gkd.li/i/14228348',
         },
         {
           key: 3,
           name: '勾选[已了解此应用未经检测，可能存在风险]',
-          fastQuery: true,
-          activityIds: [
-            'com.android.packageinstaller.PackageInstallerActivity',
-            '.PackageInterceptActivity',
-          ],
           matches:
             '[vid="checkbox" || vid="deleted_file_state_cb"][checked=false]',
           snapshotUrls: [
@@ -67,15 +61,10 @@ export default defineGkdApp({
         },
         {
           key: 4,
-          fastQuery: true,
           position: {
             left: 'width * 1.5394',
             top: 'height * 0.5',
           },
-          activityIds: [
-            '.PackageInstallerActivity',
-            '.DeleteStagedFileOnResult',
-          ],
           excludeMatches: '[text="继续安装" || text="完成"]',
           matches:
             'LinearLayout[childCount=1] > Button[text="取消"][childCount=0]',
