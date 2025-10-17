d4c10b8224a481f39702d9639d9e6dd4028ca183
天方夜谭 <tianfangyetan01@qq.com>
2025-10-17 12:05:23 +0800

    feat: Telegram-添加生日提示

--- src/apps/org.telegram.messenger.web.ts
+++ src/apps/org.telegram.messenger.web.ts
@@ -21,5 +21,17 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '其他-添加生日提示',
+      desc: '点击关闭',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'org.telegram.ui.LaunchActivity',
+          matches: '@ImageView[clickable=true] - * >2 [text^="添加您的生日"]',
+        },
+      ],
+    },
   ],
 });
