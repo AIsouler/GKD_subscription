bd0484058224aa73c820f56a0be30a2350b73403
AIsouler <zy43220@gmail.com>
2025-10-20 19:21:33 +0800

    perf: 哔哩哔哩-开屏广告 #1280

--- src/apps/tv.danmaku.bili.ts
+++ src/apps/tv.danmaku.bili.ts
@@ -10,11 +10,12 @@ export default defineGkdApp({
       matchRoot: true,
       fastQuery: true,
       //matchTime: 10000, 从桌面小组件进入哔哩哔哩观看视频后，退出返回到哔哩哔哩首页时会跳出开屏广告
-      actionMaximum: 1,
+      actionMaximum: 2, // 可能连续出现两次 https://github.com/AIsouler/GKD_subscription/issues/1280
       resetMatch: 'app',
       priorityTime: 10000,
       rules: [
         {
+          actionCd: 500,
           matches: '[vid="count_down" || vid="skip"][visibleToUser=true]', // [text*="跳过"] 可能会误触搜索框
           snapshotUrls: [
             'https://i.gkd.li/i/16187624',
