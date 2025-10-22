5ea4116fa4154bacbd94a612a4cccec654089e42
AIsouler <zy43220@gmail.com>
2025-10-22 19:00:03 +0800

    feat: 掌阅-开屏广告 #1283

--- src/apps/com.chaozh.iReaderFree.ts
+++ src/apps/com.chaozh.iReaderFree.ts
@@ -5,6 +5,23 @@ export default defineGkdApp({
   name: '掌阅',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      priorityTime: 10000,
+      rules: [
+        {
+          fastQuery: true,
+          action: 'clickCenter', // clickNode无效
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/84df4bd7-bdf9-4b34-bb3c-57dfe9c09454',
+          snapshotUrls: 'https://i.gkd.li/i/23046697',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '更新提示',
       fastQuery: true,
