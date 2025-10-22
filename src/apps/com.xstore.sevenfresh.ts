642e99e0b57d567ed5d7ecb7e65362629193cebd
AIsouler <zy43220@gmail.com>
2025-10-22 13:34:40 +0800

    feat: 七鲜-全屏广告

---
+++ src/apps/com.xstore.sevenfresh.ts
@@ -0,0 +1,25 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.xstore.sevenfresh',
+  name: '七鲜',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告',
+      desc: '点击关闭',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.modules.MainActivity',
+          matches: '[vid="closed_img"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/fd3abb40-d339-49cf-a6c8-83eedf1451b4',
+          snapshotUrls: 'https://i.gkd.li/i/23021853',
+        },
+      ],
+    },
+  ],
+});
