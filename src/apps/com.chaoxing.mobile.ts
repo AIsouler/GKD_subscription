f2fbdac913405d47942e2fc7813ebd304c30bf9f
AIsouler <zy43220@gmail.com>
2025-10-22 13:52:24 +0800

    feat: 学习通 #1284

--- src/apps/com.chaoxing.mobile.ts
+++ src/apps/com.chaoxing.mobile.ts
@@ -5,6 +5,47 @@ export default defineGkdApp({
   name: '学习通',
   groups: [
     {
+      key: 1,
+      name: '功能类-提示没有网络时点击[重试/确定]',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.player.course.CoursePlayerWithNoteActivity',
+          matches: [
+            '[text^="请在有网络时进行学习"][visibleToUser=true]',
+            '[text="重试" || text="确定"][visibleToUser=true]',
+          ],
+          exampleUrls: 'https://e.gkd.li/17573fa6-2143-4b41-9d61-e922117c9bc7',
+          snapshotUrls: [
+            'https://i.gkd.li/i/23046734',
+            'https://i.gkd.li/i/23046852',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '功能类-提示非WIFI环境时点击[继续]，使用流量播放',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: [
+            '.player.course.CoursePlayerWithNoteActivity',
+            'com.chaoxing.fanya.aphone.ui.chapter.detail.ui.ChapterDetailActivity',
+          ],
+          matches: [
+            '[text^="正在使用非WiFi环境"][visibleToUser=true]',
+            '[text="继续"][visibleToUser=true]',
+          ],
+          exampleUrls: 'https://e.gkd.li/d4a8ec6d-031f-4030-b111-51a2f37bd39d',
+          snapshotUrls: [
+            'https://i.gkd.li/i/23046887',
+            'https://i.gkd.li/i/23046892',
+          ],
+        },
+      ],
+    },
+    {
       key: 10,
       name: '权限提示-通知权限',
       desc: '点击关闭',
