911c48106b35c59ad6e9ffe0c9b07a602f1bfc27
天方夜谭 <tianfangyetan01@qq.com>
2025-10-25 17:40:51 +0800

    feat: 淘宝-消息页面热门活动卡片

--- src/apps/com.taobao.taobao.ts
+++ src/apps/com.taobao.taobao.ts
@@ -95,11 +95,16 @@ export default defineGkdApp({
       rules: [
         {
           fastQuery: true,
-          activityIds: 'com.taobao.tao.welcome.Welcome',
-          matches: 'View[desc.length>0] +2n FrameLayout > TextView[text="퀺"]',
+          activityIds: [
+            'com.taobao.tao.welcome.Welcome',
+            'com.taobao.tao.TBMainActivity',
+          ],
+          matches:
+            '@LinearLayout[clickable=true] + * ->1 [desc.length>0 || text.length>0] +(n+1) FrameLayout > TextView[text="퀺"]',
           snapshotUrls: [
             'https://i.gkd.li/i/12642795',
             'https://i.gkd.li/i/13197877',
+            'https://i.gkd.li/i/23140899',
           ],
         },
       ],
