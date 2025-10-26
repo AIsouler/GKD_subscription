3b68275eead04ce7e738835e9487da4b4de58e69
天方夜谭 <152615144+tianfangyetan1@users.noreply.github.com>
2025-10-26 23:26:17 +0800

    feat: WE Learn-离开考试页面弹窗 (#1329)

---
+++ src/apps/com.sflep.course.ts
@@ -0,0 +1,25 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.sflep.course',
+  name: 'WE Learn',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-离开考试页面弹窗',
+      desc: '点击确定',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.activity.ClassActivity',
+          matchTime: 10000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          matches:
+            '@[text="确认"][clickable=true] < * - [text^="系统检查到你离开了考试页面"] <n Dialog <n * <<3 [vid="pp_about_content"]',
+          snapshotUrls: 'https://i.gkd.li/i/23150673',
+        },
+      ],
+    },
+  ],
+});
