b7af80ac2d64587c216da51e057bc07c584e6fb5
AIsouler <zy43220@gmail.com>
2025-10-28 19:39:47 +0800

    fix: 掌阅-开屏广告 #1331

--- src/apps/com.chaozh.iReaderFree.ts
+++ src/apps/com.chaozh.iReaderFree.ts
@@ -15,9 +15,13 @@ export default defineGkdApp({
         {
           fastQuery: true,
           action: 'clickCenter', // clickNode无效
-          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          matches:
+            '[name!$="ViewGroup"] > [text*="跳过"][text.length<10][visibleToUser=true]',
           exampleUrls: 'https://e.gkd.li/84df4bd7-bdf9-4b34-bb3c-57dfe9c09454',
-          snapshotUrls: 'https://i.gkd.li/i/23046697',
+          snapshotUrls: [
+            'https://i.gkd.li/i/23046697',
+            'https://i.gkd.li/i/23186001',
+          ],
         },
       ],
     },
