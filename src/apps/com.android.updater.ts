3de399ed934ee5a80bfe17bddc7ffb0db871de9b
天方夜谭 <152615144+tianfangyetan1@users.noreply.github.com>
2025-10-28 19:44:20 +0800

    feat: 系统更新-流量更新点击继续下载 等2个更新 (#1338)

--- src/apps/com.android.updater.ts
+++ src/apps/com.android.updater.ts
@@ -6,7 +6,7 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
-      name: '更新提示-智能更新弹窗',
+      name: '功能类-不开启自动更新',
       fastQuery: true,
       matchTime: 10000,
       actionMaximum: 1,
@@ -23,6 +23,33 @@ export default defineGkdApp({
             'https://i.gkd.li/i/15386869',
             'https://i.gkd.li/i/18469146',
           ],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '功能类-流量更新点击继续下载',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.android.updater.UpdateActivity',
+          matches: [
+            '[text^="您当前正在使用数据网络，继续下载可能会产生流量费用"]',
+            '[text="继续下载"]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/i/23182185',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '功能类-不开启流量自动更新',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.android.updater.UpdateActivity',
+          matches: ['[text="开启数据网络自动下载"]', '[text="不开启"]'],
+          snapshotUrls: 'https://i.gkd.li/i/23182175',
         },
       ],
     },
