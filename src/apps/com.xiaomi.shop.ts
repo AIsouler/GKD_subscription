d2f32f3cb4f9d75735be811e45cf645c7514073a
天方夜谭 <tianfangyetan01@qq.com>
2025-10-29 17:52:37 +0800

    perf: snapshot

--- src/apps/com.xiaomi.shop.ts
+++ src/apps/com.xiaomi.shop.ts
@@ -80,6 +80,7 @@ export default defineGkdApp({
           activityIds: 'com.xiaomi.miot.store.mirn.RNMainActivity',
           matches:
             'ImageView < @ViewGroup[clickable=true] - [text^="请对"][text$="进行评价"]',
+          snapshotUrls: 'https://i.gkd.li/i/23206873',
         },
       ],
     },
