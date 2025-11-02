b3c30bd8102221c744f6a4f339cf351f4860c3f2
天方夜谭 <tianfangyetan01@qq.com>
2025-11-02 19:45:27 +0800

    feat: 运动世界-局部广告

--- src/apps/com.zjwh.android_wh_physicalfitness.ts
+++ src/apps/com.zjwh.android_wh_physicalfitness.ts
@@ -164,6 +164,22 @@ export default defineGkdApp({
           matches: '[vid="ksad_ad_dislike"]',
           snapshotUrls: 'https://i.gkd.li/i/23054142',
         },
+        {
+          key: 3,
+          fastQuery: true,
+          activityIds: '.mvi.home.HomeActivity',
+          matches:
+            '@ImageView[width<70 && height<70][childCount=0] < FrameLayout[childCount=1] - LinearLayout > [text="查看详情"]',
+          snapshotUrls: 'https://i.gkd.li/i/23289399',
+        },
+        {
+          key: 4,
+          fastQuery: true,
+          activityIds: '.mvi.home.HomeActivity',
+          matches:
+            '@ImageView[width<70 && height<70][childCount=0] < FrameLayout[childCount=1] - FrameLayout[childCount=1] > TextView[childCount=0][text.length>0] <<n [vid="fl_native"]',
+          snapshotUrls: 'https://i.gkd.li/i/23289402',
+        },
       ],
     },
     {
