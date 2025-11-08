6f537edf57dc9e852221b31ab9a931b5710b02d7
AIsouler <zy43220@gmail.com>
2025-11-08 18:53:58 +0800

    feat: Battery Guru-全屏广告 #1406

--- src/apps/com.paget96.batteryguru.ts
+++ src/apps/com.paget96.batteryguru.ts
@@ -18,5 +18,27 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '全屏广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.google.android.gms.ads.AdActivity',
+          matches:
+            '[id="ad_container"] >3 [id="video_container"] >3 Button[clickable=true][childCount=0][id=null][text=""][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/40c72a7b-3e68-496f-a993-43ab9c759cbd',
+          snapshotUrls: 'https://i.gkd.li/i/23417592',
+        },
+        {
+          key: 1,
+          activityIds: '.activities.IntroActivity',
+          matches:
+            '@Button[clickable=true][childCount=0][id=null] < View[childCount=1] <3 View - [id="app-interstitial-slot"] >2 [id="abgcp"]',
+          snapshotUrls: 'https://i.gkd.li/i/23416244',
+        },
+      ],
+    },
   ],
 });
