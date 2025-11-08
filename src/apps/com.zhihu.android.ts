ca192bc4d5284c0e399676b2954393a4aa0a38c6
AIsouler <zy43220@gmail.com>
2025-11-08 21:51:39 +0800

    perf: 知乎-分段广告 #1407

--- src/apps/com.zhihu.android.ts
+++ src/apps/com.zhihu.android.ts
@@ -120,11 +120,12 @@ export default defineGkdApp({
             '.app.ui.activity.MainActivity',
           ],
           matches:
-            '@[clickable=true] >(1,3) [text$="不感兴趣" || text^="屏蔽作者"]',
+            '@[clickable=true] >(1,3) [text$="不感兴趣" || text^="屏蔽作者" || text="不喜欢该内容"]',
           snapshotUrls: [
             'https://i.gkd.li/i/13849689',
             'https://i.gkd.li/i/17237940',
             'https://i.gkd.li/i/17268849',
+            'https://i.gkd.li/i/23431871',
           ],
         },
       ],
