9cd2f47fb4705f5c716807adf0e71b3cd2db3708
AIsouler <zy43220@gmail.com>
2025-11-10 21:30:45 +0800

    perf: 优化

--- src/apps/com.android.systemui.ts
+++ src/apps/com.android.systemui.ts
@@ -6,42 +6,29 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '功能类-允许网易云音乐共享整个屏幕',
+      name: '功能类-自动允许网易云音乐共享整个屏幕',
+      fastQuery: true,
+      activityIds:
+        '.mediaprojection.permission.MediaProjectionPermissionActivity',
       rules: [
         {
           key: 1,
-          fastQuery: true,
-          activityIds:
-            '.mediaprojection.permission.MediaProjectionPermissionActivity',
           matches: [
             '[text="要与“网易云音乐”共享屏幕吗？"]',
-            '[text="共享一个应用"] < [vid="screen_share_mode_spinner"][clickable=true]',
+            '[vid="screen_share_mode_spinner"][clickable=true]',
           ],
           snapshotUrls: 'https://i.gkd.li/i/23451390',
         },
         {
           preKeys: [1],
           key: 2,
-          fastQuery: true,
-          activityIds:
-            '.mediaprojection.permission.MediaProjectionPermissionActivity',
-          matches: [
-            '[text="共享一个应用"]',
-            '@[clickable=true] > [text="共享整个屏幕"]',
-          ],
+          matches: '@[clickable=true] > [text="共享整个屏幕"]',
           snapshotUrls: 'https://i.gkd.li/i/23451403',
         },
         {
           preKeys: [2],
           key: 3,
-          fastQuery: true,
-          activityIds:
-            '.mediaprojection.permission.MediaProjectionPermissionActivity',
-          matches: [
-            '[text="要与“网易云音乐”共享屏幕吗？"]',
-            '[text="共享整个屏幕"] < [vid="screen_share_mode_spinner"]',
-            'Button[text="共享屏幕"][clickable=true]',
-          ],
+          matches: 'Button[text="共享屏幕"][clickable=true]',
           snapshotUrls: 'https://i.gkd.li/i/23451416',
         },
       ],
@@ -49,41 +36,28 @@ export default defineGkdApp({
     {
       key: 2,
       name: '功能类-允许白描共享整个屏幕',
+      fastQuery: true,
+      activityIds:
+        '.mediaprojection.permission.MediaProjectionPermissionActivity',
       rules: [
         {
           key: 1,
-          fastQuery: true,
-          activityIds:
-            '.mediaprojection.permission.MediaProjectionPermissionActivity',
           matches: [
             '[text="要与“白描”共享屏幕吗？"]',
-            '[text="共享一个应用"] < [vid="screen_share_mode_spinner"][clickable=true]',
+            '[vid="screen_share_mode_spinner"][clickable=true]',
           ],
           snapshotUrls: 'https://i.gkd.li/i/23451390',
         },
         {
           preKeys: [1],
           key: 2,
-          fastQuery: true,
-          activityIds:
-            '.mediaprojection.permission.MediaProjectionPermissionActivity',
-          matches: [
-            '[text="共享一个应用"]',
-            '@[clickable=true] > [text="共享整个屏幕"]',
-          ],
+          matches: '@[clickable=true] > [text="共享整个屏幕"]',
           snapshotUrls: 'https://i.gkd.li/i/23451403',
         },
         {
           preKeys: [2],
           key: 3,
-          fastQuery: true,
-          activityIds:
-            '.mediaprojection.permission.MediaProjectionPermissionActivity',
-          matches: [
-            '[text="要与“白描”共享屏幕吗？"]',
-            '[text="共享整个屏幕"] < [vid="screen_share_mode_spinner"]',
-            'Button[text="共享屏幕"][clickable=true]',
-          ],
+          matches: 'Button[text="共享屏幕"][clickable=true]',
           snapshotUrls: 'https://i.gkd.li/i/23451416',
         },
       ],
