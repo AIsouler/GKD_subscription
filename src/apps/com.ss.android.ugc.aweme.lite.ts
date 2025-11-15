9123bbbdd4d40ba69c1a8ed846a935a54d173206
AIsouler <zy43220@gmail.com>
2025-11-15 13:59:46 +0800

    perf: 优化

--- src/apps/com.ss.android.ugc.aweme.lite.ts
+++ src/apps/com.ss.android.ugc.aweme.lite.ts
@@ -71,13 +71,13 @@ export default defineGkdApp({
     },
     {
       key: 11,
-      name: '全屏广告-提现后-借贷弹窗-x掉',
+      name: '全屏广告',
       rules: [
         {
-          fastQuery: false,
           activityIds:
             'com.ss.android.ugc.aweme.live.LiveDummyHybridTransparentActivity',
-          matches: '[text="同意协议并查看额度"] < * - * < * -4 Image[index=0]',
+          matches:
+            '@Image[clickable=true][text!=null][width<100 && height<100] +4 View >2 [text="同意协议并查看额度"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/23558501',
         },
       ],
