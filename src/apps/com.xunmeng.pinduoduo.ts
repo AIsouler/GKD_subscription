954bfc6f5fa6c536f05556c3eb83a2da1ad70104
AIsouler <zy43220@gmail.com>
2025-11-20 19:38:59 +0800

    perf: 拼多多-全屏广告

--- src/apps/com.xunmeng.pinduoduo.ts
+++ src/apps/com.xunmeng.pinduoduo.ts
@@ -34,7 +34,7 @@ export default defineGkdApp({
           excludeMatches:
             '[text="我的订单" || text="聊天"][bottom<500][visibleToUser=true]',
           matches:
-            '[text="开心收下" || text="去抢购" || text="立即抽免单" || text="去刮奖" || text="立即领取" || text="去领大额金币" || text="送你大额现金" || text*="红包助手" || text="刮刮卡发来的消息通知" || text="立即充值"][visibleToUser=true]',
+            '[text="开心收下" || text="去抢购" || text="立即抽免单" || text="去刮奖" || text="立即领取" || text="去领大额金币" || text="送你大额现金" || text*="红包助手" || text="刮刮卡发来的消息通知" || text="立即充值" || text="打款金额"][top>600][visibleToUser=true]',
           snapshotUrls: [
             'https://i.gkd.li/i/13625441',
             'https://i.gkd.li/i/13761182',
