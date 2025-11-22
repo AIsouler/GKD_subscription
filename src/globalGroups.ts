6b62a13b1b72a502d179de5a0fcf4b1b21e78905
AIsouler <zy43220@gmail.com>
2025-11-22 14:11:18 +0800

    perf: 开屏广告-全局

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -26,7 +26,7 @@ export default defineGkdGlobalGroups([
         excludeMatches:
           '([text*="搜索" || text="历史记录" || text$="在搜"][text.length>3 && text.length<7][visibleToUser=true]) || ([text="设置" || text="Submit" || text*="阅读并同意" || text$="登录" || text="选好了" || text="书签" || text="NEXT"][visibleToUser=true]) || ([text^="选择"][text*="偏好" || text*="行业"][text.length<10][visibleToUser=true])',
         anyMatches: [
-          '[text*="跳过"][text.length<10][width<400 && height<200][visibleToUser=true]',
+          '@[text*="跳过"][text.length<10][width<400 && height<200][visibleToUser=true] <<n [parent=null]',
           '@[name$="View" || name$="LinearLayout"][clickable=true][childCount<2][width<300 && height<150] - [text="互动广告"][visibleToUser=true]',
           '[childCount=0][visibleToUser=true][width<400 && height<200][(text.length<10 && (text*="跳过" || text*="跳 过" || text*="跳過" || text~="(?is).*skip.*") && text!*="视频") || (vid~="(?is).*skip.*" && vid!~="(?is).*video.*" && !(text="帮助") && !(text="取消") && !(text*="退出")) || id$="tt_splash_skip_btn" || (desc.length<10 && (desc*="跳过" || desc*="跳過" || desc~="(?is).*skip.*"))]',
         ],
