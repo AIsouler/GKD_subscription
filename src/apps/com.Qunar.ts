aba7e1d5342d1dd2ec314b87ee79e2b4cd5edd48
AIsouler <zy43220@gmail.com>
2025-11-25 19:16:50 +0800

    feat: 去哪儿旅行-更新提示

--- src/apps/com.Qunar.ts
+++ src/apps/com.Qunar.ts
@@ -22,5 +22,21 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '更新提示',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.mqunar.atom.attemper.pupgrade.PUpgradeActivity',
+          matches: '[text="残忍拒绝"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/068a6e67-572f-4cd5-a4db-ba63d760cbd0',
+          snapshotUrls: 'https://i.gkd.li/i/23742676',
+        },
+      ],
+    },
   ],
 });
