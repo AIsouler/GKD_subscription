e6748e369d76a0c96fcb12d8c9b44e5d534cb55a
AIsouler <zy43220@gmail.com>
2025-11-27 13:47:35 +0800

    feat: rm deprecatedKeys

--- scripts/check.ts
+++ scripts/check.ts
@@ -1,11 +1,8 @@
 import subscription from '../src/subscription';
 import { checkSubscription, checkApiVersion } from '@gkd-kit/tools';
-import { checkDeprecatedGroupKeys } from '../src/appDeprecatedKeys';
 
 await checkApiVersion();
 
 checkSubscription(subscription);
-
-checkDeprecatedGroupKeys(subscription.apps!);
 
 export default subscription;
