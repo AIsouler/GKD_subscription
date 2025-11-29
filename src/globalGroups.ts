034880722611b1a93210741a0283bb01fbb4e12f
AIsouler <zy43220@gmail.com>
2025-11-29 20:55:53 +0800

    perf: 开屏广告-全局 #1511

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -15,6 +15,7 @@ export default defineGkdGlobalGroups([
     matchTime: 10000,
     actionMaximum: 2,
     resetMatch: 'app',
+    forcedTime: 10000,
     actionCdKey: 0,
     actionMaximumKey: 0,
     priorityTime: 10000,
@@ -26,7 +27,7 @@ export default defineGkdGlobalGroups([
         excludeMatches:
           '([text*="搜索" || text="历史记录" || text$="在搜"][text.length>3 && text.length<7][visibleToUser=true]) || ([text="Submit" || text*="阅读并同意" || text="书签" || text="NEXT"][visibleToUser=true]) || ([text$="设置" || text^="选择" || text*="登录" || text$="选好了" || text^="下一步" || text^="完成"][text.length<10][visibleToUser=true])',
         anyMatches: [
-          '@[text*="跳过"][text.length<10][width<400 && height<200][visibleToUser=true] <<n [parent=null]',
+          '[text*="跳过"][text.length<10][width<400 && height<200][visibleToUser=true]',
           '@[name$="View" || name$="LinearLayout"][clickable=true][childCount<2][width<300 && height<150] - [text="互动广告"][visibleToUser=true]',
           '[childCount=0][visibleToUser=true][width<400 && height<200][(text.length<10 && (text*="跳过" || text*="跳 过" || text*="跳過" || text~="(?is).*skip.*") && text!*="视频") || (vid~="(?is).*skip.*" && vid!~="(?is).*video.*" && !(text="帮助") && !(text="取消") && !(text*="退出")) || id$="tt_splash_skip_btn" || (desc.length<10 && (desc*="跳过" || desc*="跳過" || desc~="(?is).*skip.*"))]',
         ],
