2aad77439573c83098a7ae7267544b4d78019e3b
zkj124578963 <1809400941@qq.com>
2025-11-29 23:58:46 +0800

    灯塔加速器app跳过公告

--- src/apps/com.v2ray.bizer.ts
+++ src/apps/com.v2ray.bizer.ts
@@ -1,33 +1,25 @@
 import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
-  id: 'world.letsgo.booster.android.pro',
-  name: '快连 VPN',
+  id: 'com.v2ray.bizer',
+  name: '灯塔加速器',
   groups: [
     {
       key: 1,
-      name: '通知提示-各种提示弹窗',
+      name: '通知提示-公告弹窗',
+      desc: '点击 X 关闭弹窗',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
-          fastQuery: true,
-          activityIds: [
-            'world.letsgo.booster.android.pages.home.HomeActivity',
-            'world.letsgo.booster.android.pages.guide.GuideActivity',
-          ],
-          matches:
-            '[text="我会设置" || text="快快收下" || text="知道了"][visibleToUser=true]',
-          exampleUrls: [
-            'https://e.gkd.li/0fe731a4-bf4c-4d35-a157-c1304e8df4ba',
-            'https://e.gkd.li/efeb874b-f3bf-4899-a069-387d020eb1f5',
-            'https://e.gkd.li/5868f38e-b929-4423-adfc-82b05ea1733c',
-          ],
-          snapshotUrls: [
-            'https://i.gkd.li/i/23619426',
-            'https://i.gkd.li/i/23619427',
-            'https://i.gkd.li/i/23619428',
-          ],
+          activityIds: 'com.github.kr328.main.HomeActivity',
+          matches: '[id$="close"]',
+          exampleUrls: 'https://i.gkd.li/snapshot/1764428898825',
+          snapshotUrls: 'https://i.gkd.li/i/23834355',
         },
       ],
     },
   ],
-});
+},
+);
