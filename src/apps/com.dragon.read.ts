7c573ea8596fdaca2d32a5f53e4379d4e4bc6ec3
AIsouler <zy43220@gmail.com>
2025-12-17 19:09:57 +0800

    perf: 优化

--- src/apps/com.dragon.read.ts
+++ src/apps/com.dragon.read.ts
@@ -248,15 +248,8 @@ export default defineGkdApp({
           key: 2,
           name: '底部横幅广告-点击[x]',
           matches:
-            '@ViewGroup[clickable=true][focusable=true][visibleToUser=true] +n [desc~="(?is)[0-9].*" || desc^="文明" || desc^="值得"]',
-          exampleUrls: [
-            'https://e.gkd.li/ab2021a9-8e5c-4d2a-8df1-8c6aff4e38f6',
-            'https://e.gkd.li/e899dbef-3b71-4422-a7a1-a2e6a110d23a',
-            'https://e.gkd.li/91bd37ce-b007-43bd-b7af-83eca67d253e',
-            'https://e.gkd.li/584da5b9-62d4-427e-9e2c-351310ca31fb',
-            'https://e.gkd.li/cc3d8ef7-1427-41f1-befc-8bff396553dd',
-            'https://e.gkd.li/ae2776e7-a6da-49ea-b3b2-c42a1734e6f8',
-          ],
+            'ViewGroup[childCount>4] > @ViewGroup[childCount=1][clickable=true][focusable=true][visibleToUser=true][width<50 && height<50] +n [desc!=null]',
+          exampleUrls: 'https://e.gkd.li/ab2021a9-8e5c-4d2a-8df1-8c6aff4e38f6',
           snapshotUrls: [
             'https://i.gkd.li/i/24189866',
             'https://i.gkd.li/i/24189900',
@@ -268,7 +261,6 @@ export default defineGkdApp({
         },
         {
           preKeys: [0, 1, 2],
-          key: 3,
           name: '点击[不感兴趣]/[关闭此广告]',
           matches: '[text="不感兴趣" || text="关闭此广告"]',
           exampleUrls: 'https://e.gkd.li/e04bcb90-ad61-43d9-97e9-b4f6e3873320',
