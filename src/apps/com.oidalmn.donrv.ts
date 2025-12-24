40216cd4c5548a8c0843cbc17f53261016e4f720
AIsouler <zy43220@gmail.com>
2025-12-24 19:53:39 +0800

    perf: 适配 fastQuery

--- src/apps/com.oidalmn.donrv.ts
+++ src/apps/com.oidalmn.donrv.ts
@@ -7,15 +7,14 @@ export default defineGkdApp({
     {
       key: 1,
       name: '全屏广告-弹窗广告',
-      desc: '点击[x]',
+      desc: '点击关闭',
       rules: [
         {
-          activityIds: 'com.oidalmn.donrv.MainActivity',
-          matches: '@ViewGroup[desc="top_close_button"][clickable=true]',
-          exampleUrls: [
-            'https://e.gkd.li/0e4b7af0-5708-4be7-b1e9-87b469d9cadb',
-            'https://e.gkd.li/8a5057fb-d112-4045-8ebd-8e88fa3d2820',
-          ],
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches:
+            '@[desc="top_close_button"] < ViewGroup[childCount=1] <n ViewGroup >(2,3) [text="广告"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/0e4b7af0-5708-4be7-b1e9-87b469d9cadb',
           snapshotUrls: [
             'https://i.gkd.li/i/24325415',
             'https://i.gkd.li/i/24325538',
