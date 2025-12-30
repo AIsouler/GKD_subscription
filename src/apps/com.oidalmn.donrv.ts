7703da6778502de798c5b94f83ecc571a1d7325e
13432832724 <l2979007648@163.com>
2025-12-30 13:09:30 +0800

    feat: 番喜视频 开屏广告

--- src/apps/com.oidalmn.donrv.ts
+++ src/apps/com.oidalmn.donrv.ts
@@ -5,6 +5,23 @@ export default defineGkdApp({
   name: '番喜视频',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      desc: '仅为全局开屏广告的单独补充',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      priorityTime: 10000,
+      rules: [
+        {
+          matches:
+            'FrameLayout[index=parent.childCount.minus(1)] > ImageView[width>52 && width<64][height>52 && height<64][top>1000]',
+          snapshotUrls: 'https://i.gkd.li/i/24377039', //v1.8.7 广告的x号在下半方屏幕的
+          activityIds: '.MainActivity',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '全屏广告-弹窗广告',
       desc: '点击关闭',
