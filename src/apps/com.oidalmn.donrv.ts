b02f82ef02eb3403e92aa4a766d1c9f14c7e1891
AIsouler <zy43220@gmail.com>
2025-12-30 20:19:00 +0800

    feat: 番喜视频-全屏广告 #1648

--- src/apps/com.oidalmn.donrv.ts
+++ src/apps/com.oidalmn.donrv.ts
@@ -10,6 +10,7 @@ export default defineGkdApp({
       desc: '点击关闭',
       rules: [
         {
+          key: 0,
           fastQuery: true,
           activityIds: '.MainActivity',
           matches:
@@ -19,6 +20,15 @@ export default defineGkdApp({
             'https://i.gkd.li/i/24325415',
             'https://i.gkd.li/i/24325538',
           ],
+        },
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches:
+            '@ImageView[childCount=0][width<80 && height<80] < FrameLayout[childCount=1] - FrameLayout >3 [text$="了解更多内容"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/84da57a5-611d-4330-99b7-ca977d48da78',
+          snapshotUrls: 'https://i.gkd.li/i/24377039',
         },
       ],
     },
