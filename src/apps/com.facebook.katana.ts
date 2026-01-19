2253afa30707c0969759aa21212c70823392c258
13432832724 <l2979007648@163.com>
2026-01-19 22:46:19 +0800

    fix: Facebook 功能类-自动点击[查看翻译]

--- src/apps/com.facebook.katana.ts
+++ src/apps/com.facebook.katana.ts
@@ -7,11 +7,14 @@ export default defineGkdApp({
     {
       key: 1,
       name: '功能类-自动点击[查看翻译]',
+      activityIds: [
+        '.immersiveactivity.ImmersiveActivity',
+        '.activity.FbMainTabActivity',
+      ],
       rules: [
         {
           key: 0,
           fastQuery: true,
-          activityIds: '.activity.FbMainTabActivity',
           matches:
             '@ViewGroup[clickable=true] > [visibleToUser=true][text="查看翻译"] <<n [id="android:id/list"]',
           exampleUrls: 'https://e.gkd.li/8801f1f5-ddcb-45b2-9f19-c82e9d1271f1',
@@ -21,10 +24,12 @@ export default defineGkdApp({
           key: 1,
           matchRoot: true,
           actionCd: 300,
-          activityIds: '.immersiveactivity.ImmersiveActivity',
           matches: '[text="查看翻译"][clickable=true][visibleToUser=true]',
           exampleUrls: 'https://e.gkd.li/a111f9dc-8604-45f5-ba0c-22606890c338',
-          snapshotUrls: 'https://i.gkd.li/i/24712867',
+          snapshotUrls: [
+            'https://i.gkd.li/i/24712867',
+            'https://i.gkd.li/i/24717051',
+          ],
         },
       ],
     },
