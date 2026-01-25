64b90c2b8d883c22d91c8b2782af7edac4df13dd
cjy0812 <121001029+cjy0812@users.noreply.github.com>
2026-01-26 07:59:37 +0800

    fix: 修改格式规范错误

--- src/apps/com.xianfengniao.vanguardbird.ts
+++ src/apps/com.xianfengniao.vanguardbird.ts
@@ -6,21 +6,28 @@ export default defineGkdApp({
   groups: [
     {
       key: 9,
-      name: '功能类-首页-今日任务-自动点击签到',
-      activityIds: [
-        'com.xianfengniao.vanguardbird.ui.common.activity.MainActivity',
-      ],
+      name: '功能类-自动点击签到',
+      desc: '首页-今日任务or个人-点击签到',
       rules: [
         {
+          key: 1,
+          fastQuery: true,
+          activityIds: '.ui.common.activity.MainActivity',
           matches: '[text="签到"]',
           snapshotUrls: 'https://i.gkd.li/i/24839618',
         },
         {
+          key: 2,
+          fastQuery: true,
+          activityIds: '.ui.common.activity.MainActivity',
           matches:
             '[id="com.xianfengniao.vanguardbird:id/tv_time"][text="点击签到"]',
           snapshotUrls: 'https://i.gkd.li/i/24839732',
         },
         {
+          key: 3,
+          fastQuery: true,
+          activityIds: '.ui.common.activity.MainActivity',
           matches: '[id="com.xianfengniao.vanguardbird:id/tv_cancel"]',
           snapshotUrls: ['https://i.gkd.li/i/24839655'],
         },
