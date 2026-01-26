69f6b8a6cee4fc8bc888f3a295939391cfdafefd
AIsouler <zy43220@gmail.com>
2026-01-26 21:11:29 +0800

    perf: 书旗小说-局部广告 #1758

--- src/apps/com.shuqi.controller.ts
+++ src/apps/com.shuqi.controller.ts
@@ -12,9 +12,15 @@ export default defineGkdApp({
           key: 0,
           fastQuery: true,
           activityIds: 'com.shuqi.home.MainActivity',
-          matches: '[vid="promotion_close"][visibleToUser=true]',
+          anyMatches: [
+            '[vid="promotion_close"][visibleToUser=true]',
+            '[visibleToUser=true][vid="promotion_close"]',
+          ],
           exampleUrls: 'https://e.gkd.li/a634cd5a-7b84-44ad-b65b-99f5e8769eeb',
-          snapshotUrls: 'https://i.gkd.li/i/23831955',
+          snapshotUrls: [
+            'https://i.gkd.li/i/23831955',
+            'https://i.gkd.li/i/24859977', // 不可快速查询
+          ],
         },
       ],
     },
