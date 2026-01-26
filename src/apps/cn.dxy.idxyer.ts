d2d6c24818ce20103525f23ffaaf3f353889cf8d
cjy0812 <121001029+cjy0812@users.noreply.github.com>
2026-01-27 03:54:21 +0800

    feat: 丁香园 功能组等
    Tip: key8为模板多留出来的，并非修改原规则
    
    新增：
    8.功能类-自动点击查看原图
    9.权限提示-开启悬浮小窗播放权限
    10.功能类-病例视频自动慢速x0.5
    11.功能类-病例视频自动慢速x0.8
    12.功能类-病例视频自动倍速x1.5
    13.功能类-病例视频自动倍速x2.0
    14.功能类-课程视频自动倍速x1.2
    15.功能类-课程视频自动倍速x1.5
    16.功能类-课程视频自动倍速x2.0

--- src/apps/cn.dxy.idxyer.ts
+++ src/apps/cn.dxy.idxyer.ts
@@ -145,16 +145,196 @@ export default defineGkdApp({
     },
     {
       key: 8,
-      name: '功能类-药品说明书-表格全屏自动横屏观看',
+      name: '功能类-自动点击查看原图',
       fastQuery: true,
-      activityIds: ['cn.dxy.drugscomm.business.ebm.EbmContentComposeActivity'],
+      activityIds: ['.common.image.GalleryActivity'],
       rules: [
         {
-          matches: '@TextView[text="横屏"]',
+          matches: '[vid="iv_item_bottom_ll"]',
           snapshotUrls: [
-            'https://i.gkd.li/i/24860315',
-            'https://i.gkd.li/i/24860318',
+            'https://i.gkd.li/i/24865469',
+            'https://i.gkd.li/i/24865435',
+            'https://i.gkd.li/i/24865429',
           ],
+        },
+      ],
+    },
+    {
+      key: 9,
+      name: '权限提示-开启悬浮小窗播放权限',
+      desc: '点击取消',
+      fastQuery: true,
+      activityIds: ['.common.WebViewActivity'],
+      rules: [
+        {
+          matches: [
+            '[text="开启悬浮小窗播放功能"]',
+            '[text="取消"]'
+          ],
+          snapshotUrls: 'https://i.gkd.li/i/24865283'
+        },
+      ],
+    },
+    {
+      key: 10,
+      name: '功能类-病例视频自动慢速x0.5',
+      desc: '适合CT,MRI影像学深入分析',
+      fastQuery: true,
+      //activityIds: ['.biz.post.PostDetailActivity'], //考虑视频不止在一个界面id
+      rules: [
+        {
+          key: 0,
+          name: "调出倍速栏",
+          matches: '@Button[text="Playback Rate"][clickable=true][width<80 && height<67]',
+        },
+        {
+          key: 1,
+          name: "使用x0.5",
+          matches: '@MenuItem[text="0.5x"][clickable=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/24865795',
+            'https://i.gkd.li/i/24865666',
+          ]
+        }
+      ],
+    },
+    {
+      key: 11,
+      name: '功能类-病例视频自动慢速x0.8',
+      desc: '适合CT,MRI影像学深入分析',
+      fastQuery: true,
+      //activityIds: ['.biz.post.PostDetailActivity'], //考虑视频不止在一个界面id
+      rules: [
+        {
+          key: 0,
+          name: "调出倍速栏",
+          matches: '@Button[text="Playback Rate"][clickable=true][width<80 && height<67]',
+        },
+        {
+          key: 1,
+          name: "使用x0.8",
+          matches: '@MenuItem[text="0.8x"][clickable=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/24865795',
+            'https://i.gkd.li/i/24865666',
+          ]
+        }
+      ],
+    },
+    {
+      key: 12,
+      name: '功能类-病例视频自动倍速x1.5',
+      desc: '提高观看效率',
+      fastQuery: true,
+      //activityIds: ['.biz.post.PostDetailActivity'], //考虑视频不止在一个界面id
+      rules: [
+        {
+          key: 0,
+          name: "调出倍速栏",
+          matches: '@Button[text="Playback Rate"][clickable=true][width<80 && height<67]',
+        },
+        {
+          key: 1,
+          name: "使用x1.5",
+          matches: '@MenuItem[text="1.5x"][clickable=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/24865795',
+            'https://i.gkd.li/i/24865666',
+          ]
+        }
+      ],
+    },
+    {
+      key: 13,
+      name: '功能类-病例视频自动倍速x2.0',
+      desc: '极限观看效率',
+      fastQuery: true,
+      //activityIds: ['.biz.post.PostDetailActivity'], //考虑视频不止在一个界面id
+      rules: [
+        {
+          key: 0,
+          name: "调出倍速栏",
+          matches: '@Button[text="Playback Rate"][clickable=true][width<80 && height<67]',
+        },
+        {
+          key: 1,
+          name: "使用x2.0",
+          matches: '@MenuItem[text="2.0x"][clickable=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/24865795',
+            'https://i.gkd.li/i/24865666',
+          ]
+        }
+      ],
+    },
+    {
+      key: 14,
+      name: '功能类-课程视频自动倍速x1.2',
+      desc: '我们导师再也不卡壳啦',
+      fastQuery: true,
+      activityIds: ['.openclass.biz.video.detail.VideoDetailActivity'], 
+      rules: [
+        {
+          matches: '[vid="play_speed"]',
+          action: 'click',
+          snapshotUrls: 'https://i.gkd.li/i/24866929',
+        },
+      ],
+    },
+    {
+      key: 15,
+      name: '功能类-课程视频自动倍速x1.5',
+      desc: '显著提高讲师效率',
+      fastQuery: true,
+      activityIds: ['.openclass.biz.video.detail.VideoDetailActivity'], 
+      snapshotUrls: 'https://i.gkd.li/i/24866929',
+      rules: [
+        {
+          key: 0,
+          name: "第一次点击",
+          matches: '[vid="play_speed"]',
+          action: 'click',
+          actionDelay: 100, 
+        },
+        {
+          key: 1,
+          name: "第二次点击",
+          preKeys: [0], 
+          matches: '[vid="play_speed"]',
+          action: 'click',
+          actionDelay: 100,        
+        },
+      ],
+    },
+    {
+      key: 16,
+      name: '功能类-课程视频自动倍速x2.0',
+      desc: '导师讲到冒烟不带停',
+      fastQuery: true,
+      activityIds: ['.openclass.biz.video.detail.VideoDetailActivity'], 
+      snapshotUrls: 'https://i.gkd.li/i/24866758',
+      rules: [
+        {
+          key: 0,
+          name: "第一次点击",
+          matches: '[vid="play_speed"]',
+          action: 'click',
+          actionDelay: 100, 
+        },
+        {
+          key: 1,
+          name: "第二次点击",
+          preKeys: [0], 
+          matches: '[vid="play_speed"]',
+          action: 'click',
+          actionDelay: 100,        
+        },
+        {
+          key: 2,
+          name: "第三次点击",
+          preKeys: [1], 
+          matches: '[vid="play_speed"]',
+          action: 'click',
         },
       ],
     },
