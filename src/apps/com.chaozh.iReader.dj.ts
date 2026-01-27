c0d16b1fce30c8e16f0232187be206396423ff5a
cjy0812 <121001029+cjy0812@users.noreply.github.com>
2026-01-28 05:16:47 +0800

    fix:三修config

--- src/apps/com.chaozh.iReader.dj.ts
+++ src/apps/com.chaozh.iReader.dj.ts
@@ -12,8 +12,8 @@ export default defineGkdApp({
       activityIds: 'com.zhangyue.iReader.read.ui.Activity_BookBrowser_TXT',
       rules: [
         {
-          matches: 
-          '@ImageView[id="com.zhangyue.module.ad:id/ad_close_2"][width=45 && height=24]',
+          matches:
+            '@ImageView[id="com.zhangyue.module.ad:id/ad_close_2"][width=45 && height=24]',
           snapshotUrls: [
             'https://i.gkd.li/i/24879639',
             'https://i.gkd.li/i/24879692',
@@ -45,8 +45,8 @@ export default defineGkdApp({
       activityIds: 'com.zhangyue.iReader.ui.activity.ActivityContainer',
       rules: [
         {
-          matches: 
-          '[id="com.zhangyue.module.ad:id/close"][text="关闭"][width=66 && height=45]',
+          matches:
+            '[id="com.zhangyue.module.ad:id/close"][text="关闭"][width=66 && height=45]',
           snapshotUrls: [
             'https://i.gkd.li/i/24885529',
             'https://i.gkd.li/i/24884414',
@@ -64,8 +64,8 @@ export default defineGkdApp({
           key: 0,
           name: '中间单容器单广告',
           activityIds: 'com.zhangyue.iReader.read.ui.Activity_BookBrowser_TXT',
-          matches: 
-          '@ImageView[id="com.zhangyue.module.ad:id/close"][width<25 && height<25]',
+          matches:
+            '@ImageView[id="com.zhangyue.module.ad:id/close"][width<25 && height<25]',
           snapshotUrls: [
             'https://i.gkd.li/i/24882824',
             'https://i.gkd.li/i/24882944',
@@ -94,8 +94,8 @@ export default defineGkdApp({
       key: 4,
       name: '局部广告-短剧底部广告',
       fastQuery: true,
-      activityIds: 
-      'com.zhangyue.app.shortplay.player.ui.activity.EpisodesSetPlayActivity',
+      activityIds:
+        'com.zhangyue.app.shortplay.player.ui.activity.EpisodesSetPlayActivity',
       rules: [
         {
           matches: '[id="com.zhangyue.module.ad:id/close_ad"]',
