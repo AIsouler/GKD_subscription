83b2d14039f12f9628cd4372eb16a05dce655326
Canghaizhifeng <121001029+cjy0812@users.noreply.github.com>
2026-01-28 19:14:57 +0800

    feat: 番喜视频 全屏广告-首页悬浮广告 (#1783)
    Co-authored-by: AIsouler <zy43220@gmail.com>

---
+++ src/apps/com.syjc.app.ts
@@ -0,0 +1,30 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.syjc.app',
+  name: '番喜视频',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告',
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches:
+            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null][parent.childCount>3] <n FrameLayout >(2,3) [text^="立即" || text$="详情" || text^="了解" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
+          snapshotUrls: 'https://i.gkd.li/i/24908992',
+        },
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches:
+            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null] - FrameLayout[childCount>2] >3 [text^="立即" || text$="应用" || text="了解更多" || text="查看详情"]',
+          snapshotUrls: 'https://i.gkd.li/i/24892194',
+        },
+      ],
+    },
+  ],
+});
