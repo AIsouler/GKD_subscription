0f439977bed467e9a8c7b5d3291b001f20b0c92b
AIsouler <zy43220@gmail.com>
2026-01-28 19:16:35 +0800

    perf: 番喜视频-全屏广告

--- src/apps/com.syjc.app.ts
+++ src/apps/com.syjc.app.ts
@@ -11,6 +11,7 @@ export default defineGkdApp({
         {
           key: 0,
           fastQuery: true,
+          actionDelay: 300,
           activityIds: '.MainActivity',
           matches:
             '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null][parent.childCount>3] <n FrameLayout >(2,3) [text^="立即" || text$="详情" || text^="了解" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
@@ -19,6 +20,7 @@ export default defineGkdApp({
         {
           key: 1,
           fastQuery: true,
+          actionDelay: 300,
           activityIds: '.MainActivity',
           matches:
             '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null] - FrameLayout[childCount>2] >3 [text^="立即" || text$="应用" || text="了解更多" || text="查看详情"]',
