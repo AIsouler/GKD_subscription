6aca3922680c55dc214fb3c2b5ea7da9cd0b8ab1
AIsouler <zy43220@gmail.com>
2026-01-31 16:21:20 +0800

    feat: 看护家-全屏广告 #1798

--- src/apps/com.nursinghome.monitor.ts
+++ src/apps/com.nursinghome.monitor.ts
@@ -21,6 +21,21 @@ export default defineGkdApp({
           exampleUrls: 'https://e.gkd.li/12316698-b92f-4a78-be55-5d9a90700f4a',
           snapshotUrls: 'https://i.gkd.li/i/17504953',
         },
+        {
+          key: 2,
+          fastQuery: true,
+          activityIds: 'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
+          matches:
+            '@ImageView[clickable=true][childCount=0] + RelativeLayout >2 [text="反馈"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/24979384',
+        },
+        {
+          key: 3,
+          fastQuery: true,
+          activityIds: 'com.ja.adx.qiming.ad.activity.InterstitialActivity',
+          matches: '[vid="qiming_widget_iv_close"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/24979391',
+        },
       ],
     },
   ],
