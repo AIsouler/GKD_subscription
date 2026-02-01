9d40cb459527016967e4955aafd29157e04dbffa
cjy0812 <121001029+cjy0812@users.noreply.github.com>
2026-02-02 05:05:15 +0800

    fix: 将desc属性改为name

--- src/apps/com.openai.chatgpt.ts
+++ src/apps/com.openai.chatgpt.ts
@@ -10,11 +10,12 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
-          desc: 'GPT免费额度用尽底部悬浮提示',
+          name: 'GPT免费额度用尽底部悬浮提示',
           fastQuery: true,
           activityIds: '.MainActivity',
-          matches: '[text="获取 Plus"] < View[index=2] - View[index=1] > [desc="关闭"][clickable=false]',
-          snapshotUrls: 'https://i.gkd.li/i/24996012'
+          matches:
+            '[text="获取 Plus"] < View[index=2] - View[index=1] > [desc="关闭"][clickable=false]',
+          snapshotUrls: 'https://i.gkd.li/i/24996012',
         },
       ],
     },
