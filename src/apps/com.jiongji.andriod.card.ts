09885254819d71d3bcf0711f8787e6f79e7c1402
AIsouler <zy43220@gmail.com>
2026-02-01 15:31:08 +0800

    perf: 优化

--- src/apps/com.jiongji.andriod.card.ts
+++ src/apps/com.jiongji.andriod.card.ts
@@ -57,13 +57,13 @@ export default defineGkdApp({
     },
     {
       key: 4,
-      name: '通知提示-关闭顶栏提醒💡',
+      name: '通知提示-关闭顶栏提醒',
       rules: [
         {
           fastQuery: true,
           activityIds: 'com.baicizhan.main.activity.MainTabActivity',
           matches:
-            '[desc="word_plan_tab"] > FrameLayout > ViewGroup > ImageView +2 ImageView',
+            '[desc="word_plan_tab"] > FrameLayout[index=2] >2 TextView[text!=null] + ImageView[clickable=true][childCount=0][index=parent.childCount.minus(1)]',
           snapshotUrls: [
             'https://i.gkd.li/i/24887224',
             'https://i.gkd.li/i/24983406',
