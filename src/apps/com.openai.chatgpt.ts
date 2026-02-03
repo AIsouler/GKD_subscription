9d6d78e2d042043622573cfc0ba853e028451113
AIsouler <zy43220@gmail.com>
2026-02-03 15:51:22 +0800

    perf: 优化

--- src/apps/com.openai.chatgpt.ts
+++ src/apps/com.openai.chatgpt.ts
@@ -6,15 +6,13 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '通知提示',
+      name: '通知提示-套餐限额提示',
       rules: [
         {
           key: 0,
-          name: 'GPT免费额度用尽底部悬浮提示',
-          fastQuery: true,
           activityIds: '.MainActivity',
           matches:
-            '[text="获取 Plus"] < View[index=2] - View[index=1] > [desc="关闭"][clickable=false]',
+            '[text*="套餐限额"] + @View[clickable=true] > [desc="关闭"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/24996012',
         },
       ],
